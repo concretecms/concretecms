c8cd5b0da80647dfd7388fea9bbe04f50c66f10a
Andrew Embler <andrew@concrete5.org>
2008-08-26 22:47:48 +0000

    userdata mod work
    git-svn-id: http://svn.concrete5.org/svn/concrete5@33 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 29f0a5dc17ff9174f8594658f7d9a9c89d47f2de

--- branches/userdata_mod/web/concrete/helpers/form.php
+++ branches/userdata_mod/web/concrete/helpers/form.php
@@ -98,11 +98,11 @@ class FormHelper {
 			$_array = true;
 		}
 
-		if ($isChecked && $_SERVER['REQUEST_METHOD'] != 'POST') {
+		if ($isChecked && (!isset($_REQUEST[$_field]))) {
 			$checked = true;
-		} else if ($_POST[$_field] == $value) {
+		} else if ($_REQUEST[$_field] == $value) {
 			$checked = true;
-		} else if (is_array($_POST[$_field]) && in_array($value, $_POST[$_field])) {
+		} else if (is_array($_REQUEST[$_field]) && in_array($value, $_REQUEST[$_field])) {
 			$checked = true;
 		}
 			
@@ -130,11 +130,11 @@ class FormHelper {
 			}
 		} else {
 			if (is_array($a[1])) {
-				$innerValue .= $_POST[$a[0]];
+				$innerValue .= $_REQUEST[$a[0]];
 				$miscFields = $a[1];
 			} else {
 				// we ignore this second value if a post is set with this guy in it
-				$innerValue = (isset($_POST[$a[0]])) ? $_POST[$a[0]] : $a[1];
+				$innerValue = (isset($_REQUEST[$a[0]])) ? $_REQUEST[$a[0]] : $a[1];
 			}
 		}
 		
@@ -169,7 +169,7 @@ class FormHelper {
 			}
 		}
 		
-		if ($valueOrArray == $value && !isset($_POST[$key]) || (isset($_POST[$key]) && $_POST[$key] == $value)) {
+		if ($valueOrArray == $value && !isset($_REQUEST[$key]) || (isset($_REQUEST[$key]) && $_REQUEST[$key] == $value)) {
 			$str .= 'checked="checked" ';
 		}
 		
@@ -202,11 +202,11 @@ class FormHelper {
 			}
 		} else {
 			if (is_array($a[1])) {
-				$str .= 'value="' . $_POST[$a[0]] . '" ';
+				$str .= 'value="' . $_REQUEST[$a[0]] . '" ';
 				$miscFields = $a[1];
 			} else {
 				// we ignore this second value if a post is set with this guy in it
-				$val = (isset($_POST[$a[0]])) ? $_POST[$a[0]] : $a[1];
+				$val = (isset($_REQUEST[$a[0]])) ? $_REQUEST[$a[0]] : $a[1];
 				$str .= 'value="' . $val . '" ';
 			}
 		}
@@ -246,11 +246,11 @@ class FormHelper {
 			}
 		} else {
 			if (is_array($a[1])) {
-				$str .= 'value="' . $_POST[$a[0]] . '" ';
+				$str .= 'value="' . $_REQUEST[$a[0]] . '" ';
 				$miscFields = $a[1];
 			} else {
 				// we ignore this second value if a post is set with this guy in it
-				$val = (isset($_POST[$a[0]])) ? $_POST[$a[0]] : $a[1];
+				$val = (isset($_REQUEST[$a[0]])) ? $_REQUEST[$a[0]] : $a[1];
 				$str .= 'value="' . $val . '" ';
 			}
 		}
