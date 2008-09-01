a1952746f7cef940df8a008cac75836bf2a52694
Andrew Embler <andrew@concrete5.org>
2008-09-01 16:54:26 +0000

    updating loader, removing tutorials, etc
    git-svn-id: http://svn.concrete5.org/svn/concrete5@44 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/api/generate.php
+++ trunk/api/generate.php
@@ -6,8 +6,6 @@ passthru("svn export --force ../web/concrete/helpers/ ./source/helpers");
 passthru("svn export --force ../web/concrete/jobs/ ./source/jobs");
 passthru("svn export --force -N ../web/concrete/libraries/ ./source/libraries");
 passthru("svn export --force ../web/concrete/models/ ./source/models");
-passthru("svn export --force ../web/concrete/packages/ ./source/packages");
-passthru("svn export --force ../web/concrete/single_pages/ ./source/single_pages");
 passthru("svn export --force ../web/concrete/tutorials/ ./source/tutorials");
 
 passthru("rsync -ave ssh ./source/ andrew@concrete5.org:/home/andrew/generate_api/source/");
