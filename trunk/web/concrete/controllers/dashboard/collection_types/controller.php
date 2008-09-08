84f3a53ac3cac7150e1d33a203adc9a349c842be
Ryan Tyler <ryan@concrete5.org>
2008-09-08 23:25:59 +0000

    delete page type from dash
    git-svn-id: http://svn.concrete5.org/svn/concrete5@60 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 0e5765e6511fcb4d5a755ed572c8ad042c0a2006

---
+++ trunk/web/concrete/controllers/dashboard/collection_types/controller.php
@@ -0,0 +1,36 @@
+<?php
+class DashboardCollectionTypesController extends Controller {
+
+
+public function view() {
+
+}
+
+public function delete($ctID) {
+	$db = Loader::db();
+
+	// check to make sure we 
+	$pageCount = $db->getOne("SELECT COUNT(*) FROM Pages WHERE cIsTemplate = 0 and ctID = ?",array($ctID));
+		
+	if($pageCount == 0) {
+		$template_cID = $db->getOne("SELECT cID FROM Pages WHERE cIsTemplate = 1 and ctID = ?",array($ctID));
+		
+		if($template_cID) {
+			$template = Page::getByID($template_cID);
+			if($template->getCollectionID() > 1) {
+				$template->delete();	
+			}
+		}
+		
+		$db->query("DELETE FROM PageTypes WHERE ctID = ?",array($ctID));
+		$db->query("DELETE FROM PageTypeAttributes WHERE ctID = ?",array($ctID));
+		$this->redirect("/dashboard/collection_types");
+	} else {
+		$this->set("message","The page type could not be deleted because pages of that type exist in the system.<br/>You must delete all pages of this type before deleting this page type.");
+	}
+	
+
+}
+
+} // end class def 
+?>
