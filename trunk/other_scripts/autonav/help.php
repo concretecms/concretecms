9eff2996d00f2aeffb4aaa9833b6f9ba112a3b78
Andrew Embler <andrew@concrete5.org>
2008-09-23 14:55:10 +0000

    meh
    git-svn-id: http://svn.concrete5.org/svn/concrete5@81 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: db97ac88f183d76faec081ba458826247d75d3ae

---
+++ trunk/other_scripts/autonav/help.php
@@ -0,0 +1,31 @@
+<?
+	$GLOBALS['ccmStatusHelp']['autonav'] = "
+	<p>Use <strong>Auto-Navigation</strong> to create lists of links for use in site navigation and site maps.
+	<p>
+	<strong>Sorting</strong><br/>
+	Determines the order items are displayed for the site’s visitor.
+	</p>
+	<p>
+	<strong>Auto-Nav Type</strong><br/>
+	Determines the type of list generated.
+	<ol>
+		<li>Top Level<br/>Creates a list of links to pages at the very top level/home page of the website.</li>
+		<li>Current Level<br/>Creates a list of links of all the pages at the same level as the current page.</li>
+		<li>One Level Above<br/>Creates a list of links of all the pages one level above the current page.</li>
+		<li>One Level Below<br/>Creates a list of links of all the pages one level below the current page.</li>
+		<li>Trail to Current Item<br/>Creates a list of links including all pages, from the top to the current level, that lead to this page.</li>
+	</ol>
+	</p>
+	<p>
+	<strong>Sub-Items</strong><br/>
+	Determines whether or not sub-pages will be displayed in the navigation.
+	<ol>
+		<li>Don't display sub-items<br/>No sub-pages will be shown in the navigation listing.</li>
+		<li>Current Node (Partial)<br/>Only displays the sub-pages of the current page.</li>
+		<li>Display all sub-items<br/>Displays all sub-pages.</li>
+	</ol>
+	</p>
+	
+	";
+	$GLOBALS['ccmStatusTitle']['autonav'] = "Auto-Navigation";
+?>
