01410f708ee07f3594154d28bca963ba349d870c
Andrew Embler <andrew@concrete5.org>
2008-09-29 19:51:44 +0000

    updating more docs
    git-svn-id: http://svn.concrete5.org/svn/concrete5@106 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 6be424017a63c641adf9e9aa9380c98a986ee45e

--- trunk/web/concrete/libraries/update.php
+++ trunk/web/concrete/libraries/update.php
@@ -1,6 +1,20 @@
 <?
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
+
+/**
+ * @package Utilities
+ * @author Andrew Embler <andrew@concrete5.org>
+ * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
+ * @license    http://www.concrete5.org/license/     MIT License
+ */
+
+/**
+ * @package Utilities
+ * @author Andrew Embler <andrew@concrete5.org>
+ * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
+ * @license    http://www.concrete5.org/license/     MIT License
+ */
 class Update {
 	
 	public function getLatestAvailableVersionNumber() {
