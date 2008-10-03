2c5a49a1ef02f164a12aca6dee6d35651aafea7f
Andrew Embler <andrew@concrete5.org>
2008-10-03 00:19:09 +0000

    discussion changes
    git-svn-id: http://svn.concrete5.org/svn/concrete5@123 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/models/discussion.php
+++ trunk/web/models/discussion.php
@@ -2,6 +2,7 @@
 
 class DiscussionModel extends Page {
 	
+	const CTHANDLE = 'discussion';
 	public function load($obj) {
 		if ($obj instanceof Page) {
 			$dm = DiscussionModel::getByID($obj->getCollectionID(), $obj->getVersionID());
@@ -21,6 +22,14 @@ class DiscussionModel extends Page {
 	 * @todo add paging
 	 */
 	public function getPosts() {
-		
+		Loader::model('discussion_post');
+		$db = Loader::db();
+		$v = array(DiscussionPostModel::CTHANDLE, $this->getCollectionID());
+		$r = $db->Execute("select Pages.cID from Pages inner join Collections on Collections.cID = Pages.cID inner join PageTypes on Pages.ctID = PageTypes.ctID where PageTypes.ctHandle = ? and cParentID = ? order by cDateAdded desc", $v);
+		$posts = array();
+		while ($row = $r->fetchRow()) {
+			$posts[] = DiscussionPostModel::getByID($row['cID']);
+		}
+		return $posts;
 	}
 }
--- trunk/web/page_types/discussion.php
+++ trunk/web/page_types/discussion.php
@@ -1,1 +1,41 @@
-TEST
+<div class="discussion">
+
+<?
+$nav = Loader::helper('navigation');
+foreach($posts as $p) { 
+	
+	$title = $p->getSubject();
+	
+	?>
+
+	<h3><a href="<?=$nav->getLinkToCollection($p)?>"><?=$title?></a></h3>
+	<div class="discussion-info">
+		by
+		<span class="discussion-author"><?=$p->getUserName()?></span>
+	</div>
+</div>
+		
+		
+<? } ?>
+
+<br/><br/>
+
+<? if ($this->controller->getTask() == 'add') { ?>
+
+	<form method="post" action="<?=$this->action('add')?>">
+	<div>
+	<?= $form->label('subject', 'Topic'); ?>
+	<?= $form->text('subject') ?>
+	</div>
+	
+	<div>
+	<?= $form->label('message', 'Message'); ?>
+	<?= $form->textarea('Message') ?>
+	</div>
+	
+	<?=$form->submit('post', 'Post Message') ?>
+	
+	</form>
+<? } else { ?>
+	<a href="<?=$this->action('add')?>">Add Discussion</a>
+<? } ?>
