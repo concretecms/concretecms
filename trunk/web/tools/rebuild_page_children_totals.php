babc4b89341712b29c22856d1307c226f08d46a1
Andrew Embler <andrew@concrete5.org>
2008-10-06 19:19:10 +0000

    fixing totals
    git-svn-id: http://svn.concrete5.org/svn/concrete5@132 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/tools/rebuild_page_children_totals.php
+++ trunk/web/tools/rebuild_page_children_totals.php
@@ -5,13 +5,11 @@ Loader::model('page_statistics');
 
 // set all cTotalChildren records to zero
 $db->Execute("update PageStatisticsSummary set cTotalChildren = 0");
-
+$db->Execute('update Pages set cChildren = 0');
 
 // retrieve all the pages that DON'T have any children
 $r = $db->Execute("select cID from Pages where cIsTemplate = 0");
 while ($row = $r->fetchRow()) {
-
 	// foreach one of these, we grab all the pages, and increment the parent totals by one
-	PageStatistics::incrementViewsForParents($row['cID']);
-	
+	PageStatistics::incrementParents($row['cID']);	
 }
