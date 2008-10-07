cdd27fe2533c18e6138aa20db7f4a223c116e03b
Andrew Embler <andrew@concrete5.org>
2008-10-07 18:06:10 +0000

    updating db
    git-svn-id: http://svn.concrete5.org/svn/concrete5@134 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/db/discussion_summary_1006.sql
+++ trunk/db/discussion_summary_1006.sql
@@ -1,3 +1,11 @@
-create table PageStatisticsSummary ( cID int unsigned not null default 0, cViews int unsigned not null default 0, cTotalChildren int unsigned not null default 0, primary key (cID));
+drop table if exists PageStatisticsSummary;
+drop table if exists DiscussionSummary;
 
-alter table PageStatisticsSummary rename DiscussionSummary;
+CREATE TABLE `DiscussionSummary` (
+  `cID` int(10) unsigned NOT NULL default '0',
+  `totalViews` int(10) unsigned NOT NULL default '0',
+  `totalTopics` int(10) unsigned NOT NULL default '0',
+  `totalPosts` int(10) unsigned NOT NULL default '0',
+  `lastPostCID` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`cID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
