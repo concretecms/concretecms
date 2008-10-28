cebccb0d877d0e462590339238e7ccb503c09801
Andrew Embler <andrew@concrete5.org>
2008-10-28 16:33:26 +0000

    making startup directory multilingual
    git-svn-id: http://svn.concrete5.org/svn/concrete5@179 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 6f1a61dfc305cdb8da414e627be10a6920980832

--- trunk/web/concrete/startup/file_access_check.php
+++ trunk/web/concrete/startup/file_access_check.php
@@ -6,6 +6,6 @@ if ($config_check_failed) {
 	$sp = preg_replace('/[^A-Za-z(\.)]/i', '', $_SERVER['PHP_SELF']);
 	
 	if ($sp == DIRNAME_APP . DISPATCHER_FILENAME_CORE) {
-		die("You may not access this file directly.");
+		die(_("Access Denied."));
 	}
 }
