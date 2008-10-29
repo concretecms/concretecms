9f0c6c324fafae2afaf719d659e4efa68cd2c7ff
Andrew Embler <andrew@concrete5.org>
2008-10-29 04:03:35 +0000

    minor fixes to guestbook block
    git-svn-id: http://svn.concrete5.org/svn/concrete5@189 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/guestbook/edit.php
+++ trunk/web/concrete/blocks/guestbook/edit.php
@@ -11,5 +11,5 @@
 <input type="radio" name="displayGuestBookForm" value="0" <?=($displayGuestBookForm?"":"checked=\"checked\"") ?> /> <?=t('No')?><br /><br />
 
 <?=t('Authentication Required to Post')?><br/>
-<input type="radio" name="authenticationRequired" value="0" <?=($authenticationRequired?"":"checked=\"checked\"") ?> /> <?=t('Email Only (checked by default)')?><br />
+<input type="radio" name="authenticationRequired" value="0" <?=($authenticationRequired?"":"checked=\"checked\"") ?> /> <?=t('Email Only')?><br />
 <input type="radio" name="authenticationRequired" value="1" <?=($authenticationRequired?"checked=\"checked\"":"") ?> /> <?=t('Users must login to C5')?><br /><br />
