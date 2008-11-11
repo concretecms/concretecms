314bea02413cf73484e69bfb15566c739ad54d23
Andrew Embler <andrew@concrete5.org>
2008-11-11 23:33:03 +0000

    updating language in main page controls for date picker
    git-svn-id: http://svn.concrete5.org/svn/concrete5@242 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: cbca1daf722c8963a9fb9c3aa92577e33e1b08b7

--- trunk/web/concrete/elements/page_controls.php
+++ trunk/web/concrete/elements/page_controls.php
@@ -63,6 +63,10 @@ if (isset($cp)) {
 <script type="text/javascript" src="<?=ASSETS_URL_JAVASCRIPT?>/jquery.form.2.0.2.js"></script>
 <script type="text/javascript" src="<?=ASSETS_URL_JAVASCRIPT?>/jquery.ui.1.5.2.no_datepicker.js"></script>
 <script type="text/javascript" src="<?=ASSETS_URL_JAVASCRIPT?>/jquery.ui.1.6b.datepicker.js"></script>
+<? if (LANGUAGE != 'en') { ?>
+	<script type="text/javascript" src="<?=ASSETS_URL_JAVASCRIPT?>/i18n/ui.datepicker-<?=LANGUAGE?>.js"></script>
+<? } ?>
+
 
 <script type="text/javascript" src="<?=ASSETS_URL_JAVASCRIPT?>/ccm.dialog.js"></script>
 <script type="text/javascript" src="<?=ASSETS_URL_JAVASCRIPT?>/ccm.base.js"></script>
