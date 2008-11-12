9f72cb6d84bf87a666bd783e84bd47758ddc35db
Ryan Tyler <ryan@concrete5.org>
2008-11-12 18:15:53 +0000

    task #1214 - added extension checking to file uploading
    git-svn-id: http://svn.concrete5.org/svn/concrete5@249 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/tools/al_upload_process_multiple.php
+++ trunk/web/concrete/tools/al_upload_process_multiple.php
@@ -11,12 +11,17 @@ if (!$cp->canRead()) {
 require_once(DIR_FILES_BLOCK_TYPES_CORE . '/library_file/controller.php');
 
 if (isset($_FILES['Filedata'])) {
-	$bt = BlockType::getByHandle('library_file');
-	$data = array();
-	$data['file'] = $_FILES['Filedata']['tmp_name'];
-	$data['name'] = $_FILES['Filedata']['name'];
-	$nb = $bt->add($data);
-	echo(t("File Uploaded"));
+	$fh = Loader::helper('file');
+	if($fh->hasAllowedExtension($_FILES['Filedata']['name'])) { // check type
+		$bt = BlockType::getByHandle('library_file');
+		$data = array();
+		$data['file'] = $_FILES['Filedata']['tmp_name'];
+		$data['name'] = $_FILES['Filedata']['name'];
+		$nb = $bt->add($data);
+		echo(t("File Uploaded"));
+	} else {
+		echo(t('Invalid file extension.'));
+	}
 } else {
 	echo(t('Error: No files sent.'));
 }
