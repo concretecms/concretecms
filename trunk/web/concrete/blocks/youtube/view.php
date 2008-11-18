420d550a8c8a6fb132579f778c12c8a9d38c5b7e
Tony Trupp <tony@concretecms.com>
2008-11-18 01:17:32 +0000

    #1249: quicktime detect?
    git-svn-id: http://svn.concrete5.org/svn/concrete5@265 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/youtube/view.php
+++ trunk/web/concrete/blocks/youtube/view.php
@@ -2,9 +2,18 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $url = parse_url($videoURL);
 parse_str($url['query'], $query);
-?>
-<object width="425" height="344">
+global $c;
+ 
+$vWidth=425;
+$vHeight=344;
+if ($c->isEditMode()) { ?>
+	<div style="width:<?=$vWidth?>px; height:<?=$vHeight?>px; background: #ddd; color:#333; border:1px solid #888; text-align:center; font-size:14px; padding:0px;">
+		<div style="padding:8px; padding-top: <?=round($vHeight/2)-10?>px;"><?=t('Content disabled in edit mode.')?></div>
+	</div>
+<? }else{ ?>
+<object width="<?=$vWidth?>" height="<?=$vHeight?>">
 	<param name="movie" value="http://www.youtube.com/v/<?=$query['v']?>&hl=en"></param>
 	<param name="wmode" value="transparent"></params>
 	<embed src="http://www.youtube.com/v/<?=$query['v']?>&hl=en" type="application/x-shockwave-flash" wmode="transparent" width="425" height="344"></embed>
 </object>
+<? } ?>
