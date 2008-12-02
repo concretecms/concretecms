f706bf8c5512fe6a88a0e57813d7d3973b1d9acc
Tony Trupp <tony@concretecms.com>
2008-12-02 19:47:54 +0000

    git-svn-id: http://svn.concrete5.org/svn/concrete5@317 b0551a0c-1e16-4222-a7d5-975db1aca215
    Former-commit-id: 3d10593934477194f1f5ceeef02ec83000afb506

--- branches/marketplace_theme_support/concrete/tools/preview_external_theme_iframe.php
+++ branches/marketplace_theme_support/concrete/tools/preview_external_theme_iframe.php
@@ -1,5 +1,2 @@
-<style>
-#previewExternalIFrame{ width:100%; border:0px; height:400px; }
-</style>
-<iframe id="previewExternalIFrame" src="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/preview_external_theme.php?themeCID=<?=intval($_REQUEST['themeCID'])?>&previewCID=<?=intval($_REQUEST['previewCID'])?>&themeHandle=<?=$_REQUEST['themeHandle']?>&ctID=<?=intval($_REQUEST['ctID'])?>">
+<iframe id="previewExternalIFrame" style="width:100%; border:0px; height:420px" src="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/preview_external_theme.php?themeCID=<?=intval($_REQUEST['themeCID'])?>&previewCID=<?=intval($_REQUEST['previewCID'])?>&themeHandle=<?=$_REQUEST['themeHandle']?>&ctID=<?=intval($_REQUEST['ctID'])?>">
 </iframe>
