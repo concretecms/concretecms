4d47902d1f39922ee41fbb69bc89e5f1095be273
Andrew Embler <andrew@concrete5.org>
2008-12-20 16:10:51 +0000

    some tweaks to make css handler more robust
    git-svn-id: http://svn.concrete5.org/svn/concrete5@405 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/tools/css.php
+++ trunk/web/concrete/tools/css.php
@@ -5,22 +5,24 @@ $v = View::getInstance();
 $au = $co->getAuxiliaryData();
 if (isset($au->theme) && isset($au->file)) {
 	$pt = PageTheme::getByHandle($au->theme);
-	if ($_REQUEST['mode'] == 'preview') {
-	 	$val = Cache::get('preview_theme_style', $pt->getThemeID());
-		if (is_array($val)) {
-			$values = $pt->mergeStylesFromPost($val);
-			$pt->outputStyleSheet($au->file, $values);
-			exit;
+	if (is_object($pt)) {
+		if (file_exists($pt->getThemeDirectory() . '/' . $au->file)) {
+			if ($_REQUEST['mode'] == 'preview') {
+				$val = Cache::get('preview_theme_style', $pt->getThemeID());
+				if (is_array($val)) {
+					$values = $pt->mergeStylesFromPost($val);
+					$pt->outputStyleSheet($au->file, $values);
+					exit;
+				}
+			}
+			$stat = filemtime($pt->getThemeDirectory() . '/' . $au->file);
+			$style = Cache::get($au->theme, $au->file, $stat);
+			if ($style == '') {
+				$style = $pt->parseStyleSheet($au->file);
+				Cache::set($au->theme, $au->file, $style, 10800);
+			}
+		
+			print $style;
 		}
 	}
-	
-	$stat = filemtime($pt->getThemeDirectory() . '/' . $au->file);
-	$style = Cache::get($au->theme, $au->file, $stat);
-	if ($style == '') {
-		$style = $pt->parseStyleSheet($au->file);
-		Cache::set($au->theme, $au->file, $style, 10800);
-	}
-
-	print $style;
-
 }
