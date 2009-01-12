1f6a739eed912798c4742feaaf49ff55aa214a18
Andrew Embler <andrew@concrete5.org>
2009-01-12 05:50:08 +0000

    fixing display order cache bug in dashboard
    git-svn-id: http://svn.concrete5.org/svn/concrete5@464 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: e959fae5b195fac1dbb1af7cd2ca6da92b6531c9

--- trunk/web/concrete/tools/dashboard/sitemap_update.php
+++ trunk/web/concrete/tools/dashboard/sitemap_update.php
@@ -10,8 +10,8 @@ $db = Loader::db();
 if (isset($_REQUEST['cID']) && is_array($_REQUEST['cID'])) {
 	foreach($_REQUEST['cID'] as $displayOrder => $cID) {
 		$v = array($displayOrder, $cID);
-		$db->query("update Pages set cDisplayOrder = ? where cID = ?", $v);
-		
+		$c = Page::getByID($cID);
+		$c->updateDisplayOrder($displayOrder);
 	}
 }
 
