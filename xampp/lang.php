fb828a0b0cf14436281790b6fae072362aed0a33
Kyle Wukawitz <wukawitz@gmail.com>
2009-01-29 06:32:01 +0000

    git-svn-id: http://svn.concrete5.org/svn/concrete5@517 b0551a0c-1e16-4222-a7d5-975db1aca215

--- xampp/lang.php
+++
@@ -1,6 +0,0 @@
-<?
-	$fp=fopen("lang.tmp","w");
-	fwrite($fp,basename($_SERVER['QUERY_STRING']));
-	fclose($fp);
-	header("Location: index.php");
-?>
--- xampp/mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (@mysql_connect("localhost", "pma", "")) {
-		echo "OK";
-	} else {
-		echo "NOK";
-	}
-?>
