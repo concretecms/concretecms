25f1a4ddd15e48a5c43e73fe59dafd30ac486f7f
Alan Storm <astorm@alanstorm.com>
2009-01-31 01:16:35 +0000

    Missed
    git-svn-id: http://svn.concrete5.org/svn/concrete5@538 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/helpers/validation/ip.php
+++ trunk/web/concrete/helpers/validation/ip.php
@@ -53,7 +53,7 @@
 			return t("Unable to complete action: your IP address has been banned. Please contact the administrator of this site for more information.");
 		}	
 		
-		public function logSignupRequest() {		
+		public function logSignupRequest($ignoreConfig=false) {		
 			if (Config::get('IP_BAN_LOCK_IP_ENABLE') == 1) {
 				$signupRequest = new SignupRequest();
 				$signupRequest->ipFrom = ip2long($this->getRequestIP());
@@ -61,60 +61,64 @@
 			}
 		}
 		
-		public function signupRequestThreshholdReached() {
-			$db = Loader::db();
-			$threshold_attempts  = Config::get('IP_BAN_LOCK_IP_ATTEMPTS');
-			$threshhold_seconds = Config::get('IP_BAN_LOCK_IP_TIME');
-			$ip = ip2long($this->getRequestIP());			
-			$q = 'SELECT count(ipFrom) as count
-			FROM SignupRequests 
-			WHERE ipFrom = ? 
-			AND UNIX_TIMESTAMP(date_access) > (UNIX_TIMESTAMP(now()) - ?)';			
-			$v = Array($ip, $threshhold_seconds);
-			
-			$rs = $db->execute($q,$v);			
-			$row = $rs->fetchRow();
-			if ($row['count'] >= $threshold_attempts) {
-				return true;
-			}
-			else{
-				return false;
+		public function signupRequestThreshholdReached($ignoreConfig=false) {
+			if ($ignoreConfig || Config::get('IP_BAN_LOCK_IP_ENABLE') == 1) {		
+				$db = Loader::db();
+				$threshold_attempts  = Config::get('IP_BAN_LOCK_IP_ATTEMPTS');
+				$threshhold_seconds = Config::get('IP_BAN_LOCK_IP_TIME');
+				$ip = ip2long($this->getRequestIP());			
+				$q = 'SELECT count(ipFrom) as count
+				FROM SignupRequests 
+				WHERE ipFrom = ? 
+				AND UNIX_TIMESTAMP(date_access) > (UNIX_TIMESTAMP(now()) - ?)';			
+				$v = Array($ip, $threshhold_seconds);
+				
+				$rs = $db->execute($q,$v);			
+				$row = $rs->fetchRow();
+				if ($row['count'] >= $threshold_attempts) {
+					return true;
+				}
+				else{
+					return false;
+				}
 			}
 		}
 		
-		public function createIPBan($ip=false) {
-			$ip = ($ip) ? $ip : $this->getRequestIP();			
-			$ip = ip2long($ip);
-			$time = time() + (60 * 60 * 24 * 1);     //ban for a day
-			Loader::model('user_banned_ip');
-			$ban = new UserBannedIP();
-			$ban->ipFrom 	= $ip;
-			$ban->ipTo 		= 0;
-			$ban->banCode	= UserBannedIp::IP_BAN_CODE_REGISTRATION_THROTTLE;
-			$ban->expires	= $time;
-			$ban->isManual	= 0;
-			try{
-				$ban->save();
-			}
-			catch (Exception $e) {
-				//AdoDB active record has problems with no primary key tables
-				//if a duplicate key, update the expired 
-				//if (strpos ( $e->getMsg(), string needle [, int offset] )
-				if ($e->getCode() == 1062) {		//1602 is duplicate entry key
-					$db = Loader::db();
-					
-					$q = 'UPDATE UserBannedIPs 
-					SET 
-					expires = ?
-					WHERE 
-					(ipFrom = ? AND ipTo = 0)
-					AND
-					NOT (expires = 0)';
-					
-					$time 	= time() + (60 * 60 * 24 * 1);
-					$ip		= $ip;
-					$v = array($time,$ip);					
-					$db->execute($q,$v);
+		public function createIPBan($ip=false,$ignoreConfig=false) {
+			if ($ignoreConfig || Config::get('IP_BAN_LOCK_IP_ENABLE') == 1) {		
+				$ip = ($ip) ? $ip : $this->getRequestIP();			
+				$ip = ip2long($ip);
+				$time = time() + (60 * 60 * 24 * 1);     //ban for a day
+				Loader::model('user_banned_ip');
+				$ban = new UserBannedIP();
+				$ban->ipFrom 	= $ip;
+				$ban->ipTo 		= 0;
+				$ban->banCode	= UserBannedIp::IP_BAN_CODE_REGISTRATION_THROTTLE;
+				$ban->expires	= $time;
+				$ban->isManual	= 0;
+				try{
+					$ban->save();
+				}
+				catch (Exception $e) {
+					//AdoDB active record has problems with no primary key tables
+					//if a duplicate key, update the expired 
+					//if (strpos ( $e->getMsg(), string needle [, int offset] )
+					if ($e->getCode() == 1062) {		//1602 is duplicate entry key
+						$db = Loader::db();
+						
+						$q = 'UPDATE UserBannedIPs 
+						SET 
+						expires = ?
+						WHERE 
+						(ipFrom = ? AND ipTo = 0)
+						AND
+						NOT (expires = 0)';
+						
+						$time 	= time() + (60 * 60 * 24 * 1);
+						$ip		= $ip;
+						$v = array($time,$ip);					
+						$db->execute($q,$v);
+					}
 				}
 			}
 		}
