72c57558ce984941b1ed362068877d3fddcba5e2
Andrew Embler <andrew@concrete5.org>
2009-02-02 21:20:12 +0000

    adding new shit
    git-svn-id: http://svn.concrete5.org/svn/concrete5@545 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 534b7c9964f9f234e9e3acecd92d7449a250bbf9

---
+++ branches/development/5.3.0/concrete/helpers/concrete/file.php
@@ -0,0 +1,65 @@
+<?
+/**
+ * @package Helpers
+ * @category Concrete
+ * @author Andrew Embler <andrew@concrete5.org>
+ * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
+ * @license    http://www.concrete5.org/license/     MIT License
+ */
+
+/**
+ * Internal functions useful for working with files.
+ * @package Helpers
+ * @category Concrete
+ * @author Andrew Embler <andrew@concrete5.org>
+ * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
+ * @license    http://www.concrete5.org/license/     MIT License
+ */
+
+	defined('C5_EXECUTE') or die(_("Access Denied."));
+	class ConcreteFileHelper {
+	
+		/** 
+		 * Given a file's prefix and its name, returns a path to the.
+		 * Can optionally create the path if this is the first time doing this operation for this version.
+		 */
+		public function getSystemPath($prefix, $filename, $createDirectories = false) {
+			return $this->mapSystemPath($prefix, $filename, $createDirectories);
+		}
+		
+		public function getThumbnailSystemPath($prefix, $filename, $level, $createDirectories = false) {
+			switch($level) {
+				case 2:
+					$base = DIR_FILES_UPLOADED_THUMBNAILS_LEVEL2;
+					break;
+				case 3:
+					$base = DIR_FILES_UPLOADED_THUMBNAILS_LEVEL3;
+					break;
+				default: // level 1
+					$base = DIR_FILES_UPLOADED_THUMBNAILS;
+					break;
+			}
+			
+			return $this->mapSystemPath($prefix, $filename, $createDirectories, $base);
+		}
+		
+		private function mapSystemPath($prefix, $filename, $createDirectories = false, $base = DIR_FILES_UPLOADED) {
+			$d1 = substr($prefix, 0, 4);
+			$d2 = substr($prefix, 4, 4);
+			$d3 = substr($prefix, 8);
+			
+			if ($createDirectories) {
+				if (!is_dir($base . '/' . $d1 . '/' . $d2 . '/' . $d3)) {
+					mkdir($base . '/' . $d1 . '/' . $d2 . '/' . $d3, 0777, TRUE);
+				}
+			}
+			
+			$path = $base . '/' . $d1 . '/' . $d2 . '/' . $d3 . '/' . $filename;
+			return $path;
+		}
+	
+
+	
+	}
+	
+?>
---
+++ branches/development/5.3.0/concrete/models/file_attributes.php
@@ -0,0 +1,11 @@
+<?
+
+class FileAttributeKey {
+
+	// reserved keys
+	const K_WIDTH = 'width';
+	const K_HEIGHT = 'height';
+	
+	
+	
+}
