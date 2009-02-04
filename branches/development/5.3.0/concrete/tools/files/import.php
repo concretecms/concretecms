d6ae11981eb0b14314185f159a9129456c7ff2fd
Andrew Embler <andrew@concrete5.org>
2009-02-04 18:54:42 +0000

    updating import
    git-svn-id: http://svn.concrete5.org/svn/concrete5@565 b0551a0c-1e16-4222-a7d5-975db1aca215

---
+++ branches/development/5.3.0/concrete/tools/files/import.php
@@ -0,0 +1,35 @@
+<ul class="ccm-dialog-tabs" id="ccm-file-import-tabs">
+<li class="ccm-nav-active"><a href="javascript:void(0)" id="ccm-file-upload-multiple"><?=t('Upload Multiple')?></a></li>
+<li><a href="javascript:void(0)" id="ccm-file-add-incoming"><?=t('Add Incoming')?></a></li>
+<li><a href="javascript:void(0)" id="ccm-file-add-remote"><?=t('Add Remote Files')?></a></li>
+</ul>
+
+
+<script type="text/javascript">
+var ccm_fiActiveTab = "ccm-file-upload-multiple";
+
+$("#ccm-file-import-tabs a").click(function() {
+	$("li.ccm-nav-active").removeClass('ccm-nav-active');
+	$("#" + ccm_fiActiveTab + "-tab").hide();
+	ccm_fiActiveTab = $(this).attr('id');
+	$(this).parent().addClass("ccm-nav-active");
+	$("#" + ccm_fiActiveTab + "-tab").show();
+});
+
+</script>
+
+<div id="ccm-file-upload-multiple-tab">
+<h1>Upload Multiple Files</h1>
+Form Here
+</div>
+
+<div id="ccm-file-add-incoming-tab" style="display: none">
+<h1>Add Files from Incoming Directory</h1>
+Form Here
+</div>
+
+<div id="ccm-file-add-remote-tab" style="display: none">
+<h1>Add Remote Files</h1>
+Form Here
+</div>
+
