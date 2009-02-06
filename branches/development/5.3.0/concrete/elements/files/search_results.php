50c40be4b715cecf12ca20ee2be10f585a47e4a2
Alan Storm <astorm@alanstorm.com>
2009-02-06 18:55:48 +0000

    Task 1452
    git-svn-id: http://svn.concrete5.org/svn/concrete5@583 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: c2dbfac06a6503c87b0d0104850dee9d73842d5a

--- branches/development/5.3.0/concrete/elements/files/search_results.php
+++ branches/development/5.3.0/concrete/elements/files/search_results.php
@@ -1,3 +1,10 @@
+<script type="text/javascript">
+	var CCM_STAR_STATES = {
+		'unstarred':'star_grey.png',
+		'starred':'star_yellow.png'
+	};
+	var CCM_STAR_ACTION    = 'files/star.php';
+</script>
 <div id="ccm-file-list-wrapper">
 <?
 	$fileList->displaySummary();
@@ -14,7 +21,8 @@
 			</select>
 			</th>
 			<th>Type</th>
-			<th><a href="<?=$fileList->getSortByURL('fvTitle')?>"><?=t('Title')?></a></th>
+			<th class="ccm-starred">&nbsp;</th>			
+			<th class="ccm-filename"><a href="<?=$fileList->getSortByURL('fvTitle')?>"><?=t('Title')?></a></th>
 			<th><a href="<?=$fileList->getSortByURL('fvDateAdded')?>"><?=t('Date Added')?></a></th>
 			<th><a href="<?=$fileList->getSortByURL('fvSize')?>"><?=t('Size')?></a></th>
 			<th><a href="<?=$fileList->getSortByURL('fvAuthorName')?>"><?=t('Uploaded By')?></a></th>
@@ -37,7 +45,9 @@
 			<td><input type="checkbox" /></td>
 			<td class="ccm-file-list-thumbnail"><?=$fv->getThumbnail(1)?></td>
 			<td><?=$fv->getType()?></td>
-			<td><?=$txt->highlightSearch(wordwrap($fv->getTitle(), 25, "\n", true), $keywords)?></td>
+			<!-- star_yellow.png -->
+			<td class="ccm-starred"><img src="<?=ASSETS_URL_IMAGES?>/icons/star_grey.png" height="16" width="16" border="0" class="ccm-star" /></td>			
+			<td class="ccm-filename"><?=$txt->highlightSearch(wordwrap($fv->getTitle(), 25, "\n", true), $keywords)?></td>
 			<td><?=date('M d, Y g:ia', strtotime($f->getDateAdded()))?></td>
 			<td><?=$fv->getSize()?></td>
 			<td><?=$txt->highlightSearch($fv->getAuthorName(), $keywords)?></td>
