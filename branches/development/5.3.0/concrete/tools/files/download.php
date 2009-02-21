ee34dce6ab871249227e3defcac3b7f6957a091b
Andrew Embler <andrew@concrete5.org>
2009-02-21 18:33:59 +0000

    adding in view file support
    git-svn-id: http://svn.concrete5.org/svn/concrete5@651 b0551a0c-1e16-4222-a7d5-975db1aca215

---
+++ branches/development/5.3.0/concrete/tools/files/download.php
@@ -0,0 +1,19 @@
+<?
+defined('C5_EXECUTE') or die(_("Access Denied."));
+$c = Page::getByPath("/dashboard/mediabrowser");
+$cp = new Permissions($c);
+$u = new User();
+$form = Loader::helper('form');
+if (!$cp->canRead()) {
+	die(_("Access Denied."));
+}
+
+$f = File::getByID($_REQUEST['fID']);
+if (isset($_REQUEST['fvID'])) {
+	$fv = $f->getVersion($_REQUEST['fvID']);
+} else {
+	$fv = $f->getApprovedVersion();
+}
+
+$ci = Loader::helper('file');
+$ci->forceDownload($fv->getPath());
