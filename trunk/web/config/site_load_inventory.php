bdf4eb91e38152845960676482fd4f8fd6bcf298
Kenneth Feldt <kfeldt@cholabris.com>
2009-02-27 07:24:13 +0000

    Adding first cut of load inventory - DO NOT USE (yet)
    git-svn-id: http://svn.concrete5.org/svn/concrete5@673 b0551a0c-1e16-4222-a7d5-975db1aca215

---
+++ trunk/web/config/site_load_inventory.php
@@ -0,0 +1,13 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'C5DBADMIN');
+define('DB_PASSWORD', 'C5PWRD');
+define('DB_DATABASE', 'C5DB');
+define ('INV_FILENAME','BarefootInventory.txt');
+define ('INV_FTP_HOST','localhost');
+define ('INV_FTP_LOGIN','kfeldt');
+define ('INV_SPOOL_DIR','/var/www/html/public/concrete5.2.0/spool/');
+define('BASE_URL', 'http://localhost');
+define('DIR_REL', '/concrete5.2.0');
+define('PASSWORD_SALT', 'c8MYPqJBO1a4t0Dm7rvbKo5yCkIQzpfSnGxl62sVUZueFjhNw39ATWXgELHRi');
+?>
