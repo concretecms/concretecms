1c5710ea06c77c4ad06f880b33bb1ed8c039ac35
Andrew Embler <andrew@concrete5.org>
2009-03-09 16:02:59 +0000

    addressing session fixation vulnerability, indexExists() refactoring
    git-svn-id: http://svn.concrete5.org/svn/concrete5@714 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/startup/session.php
+++ trunk/web/concrete/startup/session.php
@@ -8,10 +8,10 @@ session_set_cookie_params(0, str_replace(' ','%20',DIR_REL) . '/');
 ini_set('session.gc_maxlifetime', SESSION_MAX_LIFETIME);
 
 //if we've set the _postSID variable, we populate session_id using it
-if (isset($_REQUEST['ccm-session'])) {
-	session_id($_REQUEST['ccm-session']);
-} else if (isset($_REQUEST['sessionIDOverride'])) {
-	session_id($_REQUEST['sessionIDOverride']);
+if (isset($_POST['ccm-session'])) {
+	session_id($_POST['ccm-session']);
+} else if (isset($_POST['sessionIDOverride'])) {
+	session_id($_POST['sessionIDOverride']);
 }
 
 session_name(SESSION);
