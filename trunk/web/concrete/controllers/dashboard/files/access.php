0e197e1eb45aa3e15ffebf014eb1731c7280ff82
Andrew Embler <andrew@concrete5.org>
2009-03-11 18:42:29 +0000

    upgrades
    git-svn-id: http://svn.concrete5.org/svn/concrete5@735 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/controllers/dashboard/files/access.php
+++ trunk/web/concrete/controllers/dashboard/files/access.php
@@ -2,7 +2,7 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 class DashboardFilesAccessController extends Controller {
 
-	var $helpers = array('form','concrete/interface','validation/token'); 
+	var $helpers = array('form','concrete/interface','validation/token', 'concrete/file');
 	
 	public function view($updated=false) {
 		$helper_file = Loader::helper('concrete/file');
@@ -10,10 +10,10 @@ class DashboardFilesAccessController extends Controller {
 		$file_access_file_types = Config::get('UPLOAD_FILE_EXTENSIONS_ALLOWED');
 		//is nothing's been defined, display the constant value
 		if (!$file_access_file_types) {
-			$file_access_file_types = $helper_file->unSerializeUploadFileExtentions(UPLOAD_FILE_EXTENSIONS_ALLOWED);
+			$file_access_file_types = $helper_file->unserializeUploadFileExtensions(UPLOAD_FILE_EXTENSIONS_ALLOWED);
 		}
 		else {
-			$file_access_file_types = $helper_file->unSerializeUploadFileExtentions($file_access_file_types);		
+			$file_access_file_types = $helper_file->unserializeUploadFileExtensions($file_access_file_types);		
 		}
 		$file_access_file_types = join(', ',$file_access_file_types);		
 		$this->set('file_access_file_types', $file_access_file_types);		
@@ -35,7 +35,7 @@ class DashboardFilesAccessController extends Controller {
 		}
 		
 		$types = preg_split('{,}',$this->post('file-access-file-types'),null,PREG_SPLIT_NO_EMPTY);
-		$types = $helper_file->serializeUploadFileExtentions($types);
+		$types = $helper_file->serializeUploadFileExtensions($types);
 		Config::save('UPLOAD_FILE_EXTENSIONS_ALLOWED',$types);
 		$this->redirect('/dashboard/files/access','extensions-saved');
 	}
--- trunk/web/concrete/single_pages/dashboard/files/access.php
+++ trunk/web/concrete/single_pages/dashboard/files/access.php
@@ -3,14 +3,90 @@
 		<form method="post" id="file-access-extensions" action="<?=$this->url('/dashboard/files/access', 'file_access_extensions')?>">
 			<?=$validation_token->output('file_access_extensions');?>
 			<p>
-			Only files with the following extensions will be allowed.
+			<?=t('Only files with the following extensions will be allowed.
 			Separate extensions with commas. Periods and spaces will be
-			ignored. 
+			ignored.')?>
 			</p>
-			<?=$form->textarea('file-access-file-types',$file_access_file_types,array('rows'=>'2','cols'=>'40','style'=>'width:99%'));?>
+			<?=$form->textarea('file-access-file-types',$file_access_file_types,array('rows'=>'3','style'=>'width:99%'));?>
 			<?php		
 				$b1 = $concrete_interface->submit(t('Save'), 'file-access-extensions');
 				print $concrete_interface->buttons($b1);
 			?>		
 		</form>
 	</div>
+	
+	<h1><span><?=t('Permissions')?></span></h1>
+	<div class="ccm-dashboard-inner">
+		<form method="post" id="file-access-extensions" action="<?=$this->url('/dashboard/files/access', 'file_access_extensions')?>">
+			<?=$validation_token->output('file_permissions');?>
+
+			<p>
+			<?=t('Add users or groups to the grid below to determine access to the file manager.');?>
+			</p>
+			
+			<div class="ccm-file-permissions-entity">
+			
+			<h2>Admin Group</h2>
+
+			<table border="0" cellspacing="0" cellpadding="0" id="ccm-file-permissions-grid">
+			<tr>
+				<th><?=t('View')?></th>
+				<td><?=$form->radio('canView[]', 'ALL', true)?> <?=('All')?></td>
+				<td><?=$form->radio('canView[]', 'MINE')?> <?=('Mine')?></td>
+				<td><?=$form->radio('canView[]', 'NONE')?> <?=('None')?></td>
+				</td>
+			</tr>
+			<tr>
+				<th><?=t('Edit')?></th>
+				<td><?=$form->radio('canEdit[]', 'ALL', true)?> <?=('All')?></td>
+				<td><?=$form->radio('canEdit[]', 'MINE')?> <?=('Mine')?></td>
+				<td><?=$form->radio('canEdit[]', 'NONE')?> <?=('None')?></td>
+				</td>
+			</tr>
+			<tr>
+				<th><?=t('Admin')?></th>
+				<td><?=$form->radio('canAdmin[]', 'ALL', true)?> <?=('All')?></td>
+				<td><?=$form->radio('canAdmin[]', 'MINE')?> <?=('Mine')?></td>
+				<td><?=$form->radio('canAdmin[]', 'NONE')?> <?=('None')?></td>
+				</td>
+			</tr>
+			<tr>
+				<th><?=t('Add')?></th>
+				<td><?=$form->radio('canAdd[]', 'ALL', true)?> <?=('All')?></td>
+				<td><?=$form->radio('canAdd[]', 'NONE')?> <?=('None')?></td>
+				<td><?=$form->radio('canAdd[]', 'CUSTOM')?> <?=('Custom')?></td>
+			</tr>
+			<tr>
+				<th>&nbsp;</th>
+				<td colspan="3"><br/><strong><?=t('Allowed File Types')?></strong>
+				
+				<div style="border: 1px solid #ddd; padding: 4px; height: 120px">
+				<?
+				$extensions = $concrete_file->getAllowedFileExtensions();
+				?>
+				</div>
+				
+				</td>
+			</tr>
+			</table>
+			
+			</div>
+			
+
+				<a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/user_group_selector" id="ug-selector" dialog-width="600" dialog-title="<?=t('Choose User/Group')?>"  dialog-height="400" class="dialog-launch"><?=t('Add Group or User')?></a>
+			
+			
+			
+
+		</form>
+	</div>
+	
+	<script type="text/javascript">
+	
+	
+	$(function() {	
+		$("#ug-selector").dialog();	
+		ccm_setupGridStriping('ccm-file-permissions-grid');
+	});
+	
+	</script>
