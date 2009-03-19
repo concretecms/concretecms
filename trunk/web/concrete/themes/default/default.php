c2a8d30c73fbfc29f3944df4bbb8a76d69ebffb4
Andrew Embler <andrew@concrete5.org>
2009-03-19 16:49:17 +0000

    ratings
    git-svn-id: http://svn.concrete5.org/svn/concrete5@759 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/themes/default/default.php
+++ trunk/web/concrete/themes/default/default.php
@@ -2,36 +2,6 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $this->inc('elements/header.php'); ?>
 
-<div class="ccm-rating-display" style="display: block">
-    <input class="star {split:2}" disabled="true" type="radio" value="0.5"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="1.0"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="1.5"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="2.0"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="2.5"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="3.0"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="3.5" checked="checked"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="4.0"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="4.5"/>
-    <input class="star {split:2}" disabled="true" type="radio" value="5.0"/>
-</div>
-<div class="ccm-rating-active" style="display: none">
-    <input class="star" name="ccm-rating-star" type="radio" value="1.0"/>
-    <input class="star" name="ccm-rating-star" type="radio" value="2.0"/>
-    <input class="star" name="ccm-rating-star" type="radio" value="3.0"/>
-    <input class="star" name="ccm-rating-star" type="radio" value="4.0"/>
-    <input class="star" name="ccm-rating-star" type="radio" value="5.0"/>
-</div>
-
-<script type="text/javascript">
-$(function() {
-	$('.ccm-rating-display').hover(function() {
-		$('.ccm-rating-display').hide();
-		$('.ccm-rating-active').show();
-	}, function() {
-	
-	});
-});
-</script>
 	<div id="central">
 		<div id="sidebar">
 			<?
