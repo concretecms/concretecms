5464ca5e0935f65855f4a8bc5783486df4e94f30
Andrew Embler <andrew@concrete5.org>
2009-03-24 01:43:16 +0000

    fixing page editmode javascript bug
    git-svn-id: http://svn.concrete5.org/svn/concrete5@779 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 57d016e874f8645f1003b8e60b7dc6f07fd368de

--- trunk/web/concrete/elements/header_required.php
+++ trunk/web/concrete/elements/header_required.php
@@ -115,7 +115,7 @@ if (is_object($cp)) {
 
 	if ($this->editingEnabled()) {
 		//Loader::element('page_controls_menu', array('cp' => $cp, 'c' => $c));
-		$this->addHeaderItem('<script type="text/javascript" src="' . REL_DIR_FILES_TOOLS_REQUIRED . '/page_controls_menu_js?' . $_SERVER['QUERY_STRING'] . '"></script>'); 
+		$this->addHeaderItem('<script type="text/javascript" src="' . REL_DIR_FILES_TOOLS_REQUIRED . '/page_controls_menu_js?cID=' . $c->getCollectionID() . '&btask=' . $_REQUEST['btask'] . '"></script>'); 
 	}
 }
 
