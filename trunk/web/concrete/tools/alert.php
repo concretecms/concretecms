705422381cdc6f704cd8263c039699bbe27b088c
Todd Crowe <toddbcrowe@gmail.com>
2009-03-25 19:41:51 +0000

    Task 1564: checkin new fix for alert box; copy copy/paste bug in availabe themes function.
    git-svn-id: http://svn.concrete5.org/svn/concrete5@793 b0551a0c-1e16-4222-a7d5-975db1aca215

---
+++ trunk/web/concrete/tools/alert.php
@@ -0,0 +1,8 @@
+<?
+Loader::library('view');
+?>
+
+<div id="ccm-popup-alert">
+	<div id="ccm-popup-alert-message"></div>
+	<a href="javascript:void(0)" class="ccm-button-left ccm-dialog-close"><span><?=t('Okay')?></span></a>
+</div>
