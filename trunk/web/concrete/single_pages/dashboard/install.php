964e9025f5c0ae7d2619eedc397092ce7ed91e83
Todd Crowe <toddbcrowe@gmail.com>
2009-03-25 20:12:16 +0000

    Task 1590: remove third column/themes.
    git-svn-id: http://svn.concrete5.org/svn/concrete5@794 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/single_pages/dashboard/install.php
+++ trunk/web/concrete/single_pages/dashboard/install.php
@@ -29,7 +29,7 @@ if ($_REQUEST['bt_installed']) {
 	$message = t('Add-On Installed');
 }
 
-/* Load installed and available blocks, packages, and themes.
+/* Load installed and available blocks and packages.
  */
 $ci = Loader::helper('concrete/urls');
 $ch = Loader::helper('concrete/interface');
@@ -41,9 +41,8 @@ $pkgAvailableArray = Package::getAvailablePackages();
 $installedArray = $btArray;
 $availableArray = array_merge($btAvailableArray, $pkgAvailableArray);
 ksort($availableArray);
-$themesArray = PageTheme::getAvailableThemes();
 
-/* Load featured add-ons and themes from the marketplace.
+/* Load featured add-ons from the marketplace.
  */
 Loader::model('collection_attributes');
 $db = Loader::db();
@@ -52,12 +51,10 @@ $isFeaturedKeyId = CollectionAttributeKey::getByHandle('is_featured_remotely');
 
 if(ENABLE_MARKETPLACE_SUPPORT){
 	$blocksHelper = Loader::helper('concrete/marketplace/blocks');
-	$themesHelper = Loader::helper('concrete/marketplace/themes');
 
 	$featuredBlocks = $blocksHelper->getPreviewableList();
-	$featuredThemes = $themesHelper->getPreviewableList();
 }else{
-    $featuredBlocks = $featuredThemes = array();
+    $featuredBlocks = array();
 }
 
 ?>
@@ -111,9 +108,8 @@ function logoutSuccess() {
 
 	<div id="ccm-module-wrapper">
 	<div style="width: 778px">
-	<div style="float: left; width: 520px">
 
-	<div class="ccm-module" style="width: 250px; margin-bottom: 0px">
+	<div class="ccm-module" style="width: 320px; margin-bottom: 0px">
 
 		<h1><span><?=t('Currently Installed')?></span></h1>
 		<div class="ccm-dashboard-inner">
@@ -157,10 +153,20 @@ function logoutSuccess() {
 			
 	</div>
 
-	<div class="ccm-module" style="width: 250px; margin-bottom: 0px">
+	<div class="ccm-module" style="width: 380px; margin-bottom: 0px">
 
 		<h1><span><?=t('New')?></span></h1>
 		<div class="ccm-dashboard-inner">
+
+		<? if (!UserInfo::isRemotelyLoggedIn()) { ?>
+			<p>You aren't currently signed in to the marketplace.</p>
+			<p><a href="#" onclick="ccmPopupLogin.show('', loginSuccess, '', 1)">Click here to sign in or create an account.</a></p>
+		<? } else { ?>
+			<p><?=t('You are currently signed in to the marketplace as');?>
+          	  <a href="<?=CONCRETE5_ORG_URL ?>/profile/-/<?=UserInfo::getRemoteAuthUserId() ?>/" ><?=UserInfo::getRemoteAuthUserName() ?></a>
+			  <?=t('(Not your account? <a href="#" onclick="ccm_support.signOut(logoutSuccess)">Sign Out</a>)')?></p>
+		<? } ?>
+		<hr />
 
 		<? if (count($availableArray) == 0) { ?>
 
@@ -197,103 +203,6 @@ function logoutSuccess() {
 
 		<? } ?>
 		</div>
-
-	</div>
-
-	<div class="ccm-module" style="width: 250px; margin-top: 10px; margin-bottom: 0px">
-
-		<h1><span><?=t('Available Themes')?></span></h1>
-		<div class="ccm-dashboard-inner">
-
-		<? if (count($themesArray) == 0) { ?>
-			<p><?=t('No themes are available to install.')?></p>
-		<? } ?>
-
-		<? foreach ($themesArray as $t) { ?>
-			<div class="ccm-block-type">
-			<table>
-			<tr>
-				<td class="ccm-template-content" colspan="2"><h3><?=$t->getThemeName()?></h3></td>
-			</tr>
-			<tr>
-				<td class="ccm-template-content" colspan="2"><?=$t->getThemeThumbnail()?></td>
-			</tr>
-			<tr>
-				<td style="color: #aaa; padding: 2px 0 6px"><?=$t->getThemeDescription()?></td>
-				<td style="vertical-align: bottom"><?=$ch->button(t("Install"), $this->url('/dashboard/pages/themes','install', $t->getThemeHandle()), "right");?></td>
-			</tr>
-			</table>
-			</div>
-		<? } ?>
-
-		</div>
-
-	</div>
-
-	</div>
-	<div style="float: left; width: 258px">
-
-	<div class="ccm-module" style="width: 248px; margin-bottom: 0px; clear: left">
-
-		<h1><span><?=t('Marketplace')?></span></h1>
-		<div class="ccm-dashboard-inner">
-		<? if (!UserInfo::isRemotelyLoggedIn()) { ?>
-			<p>You aren't currently signed in to the marketplace.</p>
-			<p><a onclick="ccmPopupLogin.show('', loginSuccess, '', 1)">Click here to sign in or create an account.</a></p>
-		<? } else { ?>
-			<p><?=t('You are currently signed in to the marketplace as');?>
-          	  <a href="<?=CONCRETE5_ORG_URL ?>/profile/-/<?=UserInfo::getRemoteAuthUserId() ?>/" ><?=UserInfo::getRemoteAuthUserName() ?></a>
-			  <?=t('(Not your account? <a onclick="ccm_support.signOut(logoutSuccess)">Sign Out</a>)')?></p>
-			<br/><h2><?=t('Featured Add-Ons')?></h2>
-			<div style="margin:0px; padding:0px;  height:auto" >	
-			<? foreach ($featuredBlocks as $fb) { ?>
-				<div class="ccm-block-type">
-				<table>
-				<tr>
-					<td colspan="2"><p class="ccm-block-type-inner" style="background-image: url(<?=$fb->getRemoteIconURL()?>)"><?=$fb->btName?></p></td>
-				</tr>
-				<tr>
-					<td style="color: #aaa; padding: 2px 0 6px"><?=$fb->btDescription?></td>
-				<? $file = $fb->getRemoteFileURL();
-				   if (!empty($file) && intval($fb->getPrice()) == 0) { ?>
-					<td style="vertical-align: bottom"><?=$ch->button(t("Install"), View::url('/dashboard/install', 'remote_addon', $fb->getHandle()), "right");?></td>
-				<? } else { ?>
-					<td style="vertical-align: bottom"><?=$ch->button(t("Download"), $fb->getRemoteURL(), "right");?></td>
-				<? } ?>
-				</tr>
-				</table>
-				</div>
-			<? } ?>
-			</div>
-
-			<br/><h2><?=t('Featured Themes')?></h2>
-			<div style="margin:0px; padding:0px;  height:auto" >	
-			<? foreach ($featuredThemes as $ft) { ?>
-				<div class="ccm-block-type">
-				<table>
-				<tr>
-					<td class="ccm-template-content" colspan="2"><h3><?=$ft->getThemeName()?></h3></td>
-				</tr>
-				<tr>
-					<td class="ccm-template-content" colspan="2"><img src="<?=$ft->getThemeThumbnail()?>"></td>
-				</tr>
-				<tr>
-					<td style="color: #aaa; padding: 2px 0 6px"><?=$ft->getThemeDescription()?></td>
-				<? $file = $ft->getRemoteFileURL();
-				   if (!empty($file) && intval($ft->getPrice()) == 0) { ?>
-					<td style="vertical-align: bottom"><?=$ch->button(t("Install"), View::url('/dashboard/install', 'remote_theme', $ft->getHandle()), "right");?></td>
-				<? } else { ?>
-					<td style="vertical-align: bottom"><?=$ch->button(t("Download"), $ft->getRemoteURL(), "right");?></td>
-				<? } ?>
-				</tr>
-				</table>
-				</div>
-			<? } ?>
-			</div>
-		<? } ?>
-		</div>
-
-	</div>
 
 	</div>
 
