cf0249120bbda1a16b961a4e76920170e0cec614
Tony Trupp <tony@concretecms.com>
2009-03-31 03:51:10 +0000

    http://www.concrete5.org/index.php?cID=8232
    git-svn-id: http://svn.concrete5.org/svn/concrete5@828 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: de69f2b91dc2c94eef68cde5befe3f461213d637

--- trunk/web/concrete/elements/files/search_form_advanced.php
+++ trunk/web/concrete/elements/files/search_form_advanced.php
@@ -41,133 +41,127 @@ foreach($s1 as $s) {
 }
 
 ?>
+
 <? $form = Loader::helper('form'); ?>
-<div id="ccm-file-manager-advanced">
 
-<div id="ccm-file-search-advanced-fields">
-
-<div id="ccm-file-search-field-base-elements" style="display: none">
-
-	<span class="ccm-file-search-option" search-field="size">
-	<?=$form->text('size_from', array('style' => 'width: 30px'))?>
-	<?=t('to')?>
-	<?=$form->text('size_to', array('style' => 'width: 30px'))?>
-	KB
-	</span>
-
-	<span class="ccm-file-search-option"  search-field="type">
-	<?=$form->select('type', $types)?>
-	</span>
-
-	<span class="ccm-file-search-option"  search-field="extension">
-	<?=$form->select('extension', $extensions)?>
-	</span>
-
-	<span class="ccm-file-search-option"  search-field="date_added">
-	<?=$form->text('date_from', array('style' => 'width: 86px'))?>
-	<?=t('to')?>
-	<?=$form->text('date_to', array('style' => 'width: 86px'))?>
-	</span>
+<div id="ccm-file-search-advanced-fields" >
 	
-	<? foreach($searchFieldAttributes as $sfa) { ?>
-		<span class="ccm-file-search-option ccm-file-search-option-type-<?=strtolower($sfa->getAttributeKeyType())?>" search-field="<?=$sfa->getAttributeKeyID()?>">
-		<?=$sfa->outputSearchHTML()?>
+	<div id="ccm-file-search-field-base-elements" style="display: none">
+	
+		<span class="ccm-file-search-option" search-field="size">
+		<?=$form->text('size_from', array('style' => 'width: 30px'))?>
+		<?=t('to')?>
+		<?=$form->text('size_to', array('style' => 'width: 30px'))?>
+		KB
 		</span>
-	<? } ?>	
-</div>
-
-
-<form method="get" class="ccm-dashboard-file-search" action="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/files/search_results">
-	<input type="hidden" name="search" value="1" />
-<?	/** 
-	 * Here are all the things that could be passed through the asset library that we need to account for, as hidden form fields
-	 */
-	print $form->hidden('fType'); 
-?>	
-	<div style="position: relative">
-		<? if ($_REQUEST['fType'] != false) { ?>
-			<div class="ccm-file-manager-pre-filter"><?=t('Only displaying %s files.', FileType::getGenericTypeText($_REQUEST['fType']))?></div>
-		<? } ?>
-
-		<h2><?=t('Search')?></h2>
-		<img src="<?=ASSETS_URL_IMAGES?>/throbber_white_16.gif" width="16" height="16" id="ccm-file-search-loading" />
-
-	</div>
 	
-	<div id="ccm-file-search-advanced-fields-inner">
-	<div class="ccm-file-search-field">
-
-	<table border="0" cellspacing="0" cellpadding="0">
-	<tr>
-		<td width="100%">
-		<?=$form->text('fKeywords', array('style' => 'width:200px')); ?>
-		</td>
-	</tr>
-	</table>
-	</div>
-
-	<div class="ccm-file-search-field">
-	<table border="0" cellspacing="0" cellpadding="0" width="100%">
-	<tr>
-		<td style="white-space: nowrap" align="right"><div style="width: 85px; padding-right:5px"><?=t('Results Per Page')?></div></td>
-		<td width="100%">
-			<?=$form->select('fNumResults', array(
-				'10' => '10',
-				'25' => '25',
-				'50' => '50',
-				'100' => '100',
-				'500' => '500'
-			), false, array('style' => 'width:65px'))?>
-		</td>
-		<td>&nbsp;</td>
-
-	</tr>	
-	</table>
-	</div>
+		<span class="ccm-file-search-option"  search-field="type">
+		<?=$form->select('type', $types)?>
+		</span>
 	
+		<span class="ccm-file-search-option"  search-field="extension">
+		<?=$form->select('extension', $extensions)?>
+		</span>
 	
-	
-	<div class="ccm-file-search-field">
-	<table border="0" cellspacing="0" cellpadding="0" width="100%">
-	<tr>
-		<td style="white-space: nowrap" align="right"><div style="width: 85px; padding-right:5px"><?=t('Found in Set')?></div></td>
-		<td width="100%">
-			<?=$form->select('fSet', $sets, false, array('style' => 'width:95px'))?>
-		</td>
-		<td><a href="javascript:void(0)" id="ccm-file-search-add-option"><img src="<?=ASSETS_URL_IMAGES?>/icons/add.png" width="16" height="16" /></a></td>
-
-	</tr>	
-	</table>
-	</div>
-	
-	
-	<div id="ccm-file-search-field-base">
+		<span class="ccm-file-search-option"  search-field="date_added">
+		<?=$form->text('date_from', array('style' => 'width: 86px'))?>
+		<?=t('to')?>
+		<?=$form->text('date_to', array('style' => 'width: 86px'))?>
+		</span>
 		
-		<table border="0" cellspacing="0" cellpadding="0">
-		<tr>
-		<td valign="top" style="padding-right: 4px">
-		<?=$form->select('fvField', $searchFields, array('style' => 'width: 85px'));
-		?>
-		<input type="hidden" value="" class="ccm-file-selected-field" name="fvSelectedField[]" />
-		</td>
-		<td width="100%" valign="top" class="ccm-file-selected-field-content">
-		<?=t('Select Search Field.')?>
-		</td>
-		<td valign="top">
-		<a href="javascript:void(0)" class="ccm-file-search-remove-option"><img src="<?=ASSETS_URL_IMAGES?>/icons/remove_minus.png" width="16" height="16" /></a>
-		</td>
-		</tr></table>
+		<? foreach($searchFieldAttributes as $sfa) { ?>
+			<span class="ccm-file-search-option ccm-file-search-option-type-<?=strtolower($sfa->getAttributeKeyType())?>" search-field="<?=$sfa->getAttributeKeyID()?>">
+			<?=$sfa->outputSearchHTML()?>
+			</span>
+		<? } ?>	
 	</div>
 	
-	<div id="ccm-file-search-fields-wrapper">
 	
-	</div>
+	<form method="get" class="ccm-dashboard-file-search" action="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/files/search_results">
+		<input type="hidden" name="search" value="1" />
+	<?	/** 
+		 * Here are all the things that could be passed through the asset library that we need to account for, as hidden form fields
+		 */
+		print $form->hidden('fType'); 
+	?>	
+		<div id="ccm-search-box-title">
+			<? if ($_REQUEST['fType'] != false) { ?>
+				<div class="ccm-file-manager-pre-filter"><?=t('Only displaying %s files.', FileType::getGenericTypeText($_REQUEST['fType']))?></div>
+			<? } ?>
 	
-	<div id="ccm-file-search-fields-submit">
-		<?=$form->submit('ccm-search-files', 'Search')?>
-	</div>
-	</div>
-
-</form>	
+			<img src="<?=ASSETS_URL_IMAGES?>/throbber_white_16.gif" width="16" height="16" id="ccm-file-search-loading" />
+			
+			<h2><?=t('Search')?></h2>
+			
+		</div>
+		
+		<div id="ccm-file-search-advanced-fields-inner">
+			<div class="ccm-file-search-field">
+				<table border="0" cellspacing="0" cellpadding="0">
+				<tr>
+					<td width="100%">
+					<?=$form->text('fKeywords', array('style' => 'width:200px')); ?>
+					</td>
+				</tr>
+				</table>
+			</div>
+		
+			<div class="ccm-file-search-field">
+				<table border="0" cellspacing="0" cellpadding="0" width="100%">
+				<tr>
+					<td style="white-space: nowrap" align="right"><div style="width: 85px; padding-right:5px"><?=t('Results Per Page')?></div></td>
+					<td width="100%">
+						<?=$form->select('fNumResults', array(
+							'10' => '10',
+							'25' => '25',
+							'50' => '50',
+							'100' => '100',
+							'500' => '500'
+						), false, array('style' => 'width:65px'))?>
+					</td>
+					<td>&nbsp;</td>			
+				</tr>	
+				</table>
+			</div>
+			
+			<div class="ccm-file-search-field">
+				<table border="0" cellspacing="0" cellpadding="0" width="100%">
+					<tr>
+						<td style="white-space: nowrap" align="right"><div style="width: 85px; padding-right:5px"><?=t('Found in Set')?></div></td>
+						<td width="100%">
+							<?=$form->select('fSet', $sets, false, array('style' => 'width:95px'))?>
+						</td>
+						<td><a href="javascript:void(0)" id="ccm-file-search-add-option"><img src="<?=ASSETS_URL_IMAGES?>/icons/add.png" width="16" height="16" /></a></td>
+				
+					</tr>	
+				</table>
+			</div>
+			
+			<div id="ccm-file-search-field-base">				
+				<table border="0" cellspacing="0" cellpadding="0">
+					<tr>
+						<td valign="top" style="padding-right: 4px">
+						<?=$form->select('fvField', $searchFields, array('style' => 'width: 85px'));
+						?>
+						<input type="hidden" value="" class="ccm-file-selected-field" name="fvSelectedField[]" />
+						</td>
+						<td width="100%" valign="top" class="ccm-file-selected-field-content">
+						<?=t('Select Search Field.')?>
+						</td>
+						<td valign="top">
+						<a href="javascript:void(0)" class="ccm-file-search-remove-option"><img src="<?=ASSETS_URL_IMAGES?>/icons/remove_minus.png" width="16" height="16" /></a>
+						</td>
+					</tr>
+				</table>
+			</div>
+			
+			<div id="ccm-file-search-fields-wrapper">			
+			</div>
+			
+			<div id="ccm-file-search-fields-submit">
+				<?=$form->submit('ccm-search-files', 'Search')?>
+			</div>
+		</div>
+	
+	</form>	
 </div>
-
--- trunk/web/concrete/tools/files/search_dialog.php
+++ trunk/web/concrete/tools/files/search_dialog.php
@@ -12,23 +12,31 @@ $cnt = Loader::controller('/dashboard/files/search');
 $fileList = $cnt->getRequestedSearchResults();
 $files = $fileList->getPage();
 $pagination = $fileList->getPagination();
+?>
 
-Loader::element('files/search_form_advanced'); ?>
-
-
-<div id="ccm-file-search-advanced-fields-gutter">&nbsp;</div>
-
-<div id="ccm-file-search-advanced-results-wrapper">
-
-<? Loader::element('files/upload_single'); ?>
-
-<div id="ccm-file-search-results">
-
-<? Loader::element('files/search_results', array('fileSelector' => true, 'files' => $files, 'fileList' => $fileList, 'pagination' => $pagination)); ?>
-
-</div>
-
-</div>
+<div id="ccm-file-manager-advanced" >
+	
+	<table id="ccm-file-manager-table" >
+		<tr>
+			<td>
+				<? Loader::element('files/search_form_advanced'); ?>
+			</td>		
+			<? /* <div id="ccm-file-search-advanced-fields-gutter">&nbsp;</div> */ ?>		
+			<td>	
+				<div id="ccm-file-search-advanced-results-wrapper">
+				
+					<? Loader::element('files/upload_single'); ?>
+					
+					<div id="ccm-file-search-results">
+					
+						<? Loader::element('files/search_results', array('fileSelector' => true, 'files' => $files, 'fileList' => $fileList, 'pagination' => $pagination)); ?>
+					
+					</div>
+				
+				</div> 	
+			</td>	
+		</tr>
+	</table>
 
 </div>
 
@@ -38,3 +46,4 @@ $(function() {
 	ccm_activateFileManager();
 });
 </script>';
+?>
