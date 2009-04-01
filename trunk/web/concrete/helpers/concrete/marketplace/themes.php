bf57f01dd27bbf0d265e0e754fadfd2eb96a9458
Todd Crowe <toddbcrowe@gmail.com>
2009-04-01 18:38:39 +0000

    Task 1617: Improve error handling.  Better method for merging block lists.
    git-svn-id: http://svn.concrete5.org/svn/concrete5@857 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 1672701f3a0370d0ed1efc9de315ceaf638677c1

--- trunk/web/concrete/helpers/concrete/marketplace/themes.php
+++ trunk/web/concrete/helpers/concrete/marketplace/themes.php
@@ -6,12 +6,14 @@ class ConcreteMarketplaceThemesHelper  {
 
 	function getPreviewableList($filterInstalled=true) {
 		if (!function_exists('mb_detect_encoding')) {
-			return false;
+			return array();
 		}
 		
 		$pageThemes = Cache::get('marketplace_theme_list', false, false, true);
 		if (!is_array($pageThemes)) {
 			$fh = Loader::helper('file'); 
+			if (!$fh) return array();
+
 			// Retrieve the URL contents 
 			$xml = $fh->getContents(MARKETPLACE_THEME_LIST_WS);
 			$pageThemes=array();
@@ -19,12 +21,16 @@ class ConcreteMarketplaceThemesHelper  {
 				// Parse the returned XML file
 				$enc = mb_detect_encoding($xml);
 				$xml = mb_convert_encoding($xml, 'UTF-8', $enc);
-				$xmlObj = new SimpleXMLElement($xml);
-				foreach($xmlObj->theme as $theme){
-					$pgTheme = new PageThemeRemote();
-					$pgTheme->loadFromXML($theme);
-					$pageThemes[]=$pgTheme;
-				}
+
+				try {
+					libxml_use_internal_errors(true);
+					$xmlObj = new SimpleXMLElement($xml);
+					foreach($xmlObj->theme as $theme) {
+						$pgTheme = new PageThemeRemote();
+						$pgTheme->loadFromXML($theme);
+						$pageThemes[]=$pgTheme;
+					}
+				} catch (Exception $e) {}
 			}
 			Cache::set('marketplace_theme_list', false, $pageThemes, MARKETPLACE_CONTENT_LATEST_THRESHOLD, true);		
 		} 
@@ -32,13 +38,15 @@ class ConcreteMarketplaceThemesHelper  {
 		if ($filterInstalled && is_array($pageThemes)) {
 			Loader::model('page_theme');
 			$handles = PageTheme::getInstalledHandles();
-			$ptList = array();
-			foreach($pageThemes as $pt) {
-				if (!in_array($pt->getHandle(), $handles)) {
-					$ptList[] = $pt;
+			if (is_array($handles)) {
+				$ptList = array();
+				foreach($pageThemes as $pt) {
+					if (!in_array($pt->getHandle(), $handles)) {
+						$ptList[] = $pt;
+					}
 				}
+				$pageThemes = $ptList;
 			}
-			$pageThemes = $ptList;
 		}
 
 		return $pageThemes;
