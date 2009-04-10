2316eed36493ed3dc6d781a79bd8b00c8f152987
Andrew Embler <andrew@concrete5.org>
2009-04-10 19:08:21 +0000

    adding choose to the select menu
    git-svn-id: http://svn.concrete5.org/svn/concrete5@909 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/tools/files/get_data.php
+++ trunk/web/concrete/tools/files/get_data.php
@@ -3,26 +3,44 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 $u = new User();
 $form = Loader::helper('form');
 
-$resp = array();
+$respw = array();
 
-$f = File::getByID($_REQUEST['fID']);
-$fp = new Permissions($f);
-if (!$fp->canRead()) {
+$fileIDs = array();
+$files = array();
+if (is_array($_REQUEST['fID'])) {
+	$fileIDs = $_REQUEST['fID'];
+} else {
+	$fileIDs[] = $_REQUEST['fID'];
+}
+
+foreach($fileIDs as $fID) {
+	$f = File::getByID($fID);
+	$fp = new Permissions($f);
+	if ($fp->canRead()) {
+		$files[] = $f;
+	}
+}
+
+if (count($files) == 0) {
 	die(_("Access Denied."));
 }
 
-$resp['error'] = false;
-$resp['filePathDirect'] = $f->getRelativePath();
-$resp['filePathInline'] = View::url('/download_file', 'view_inline', $f->getFileID());
-$resp['filePath'] = View::url('/download_file', 'view', $f->getFileID());
-$resp['title'] = $f->getTitle();
-$resp['fileName'] = $f->getFilename();
-$resp['thumbnailLevel1'] = $f->getThumbnailSRC(1);
-$resp['thumbnailLevel2'] = $f->getThumbnailSRC(2);
-$resp['thumbnailLevel3'] = $f->getThumbnailSRC(3);
-$resp['width'] = $f->getAttribute("width");
-$resp['height'] = $f->getAttribute("height");
-$resp['fID'] = $f->getFileID();
+$i = 0;
+foreach($files as $f) {
+	$resp[$i]['error'] = false;
+	$resp[$i]['filePathDirect'] = $f->getRelativePath();
+	$resp[$i]['filePathInline'] = View::url('/download_file', 'view_inline', $f->getFileID());
+	$resp[$i]['filePath'] = View::url('/download_file', 'view', $f->getFileID());
+	$resp[$i]['title'] = $f->getTitle();
+	$resp[$i]['fileName'] = $f->getFilename();
+	$resp[$i]['thumbnailLevel1'] = $f->getThumbnailSRC(1);
+	$resp[$i]['thumbnailLevel2'] = $f->getThumbnailSRC(2);
+	$resp[$i]['thumbnailLevel3'] = $f->getThumbnailSRC(3);
+	$resp[$i]['width'] = $f->getAttribute("width");
+	$resp[$i]['height'] = $f->getAttribute("height");
+	$resp[$i]['fID'] = $f->getFileID();
+	$i++;
+}
 
 $h = Loader::helper('json');
 print $h->encode($resp);
