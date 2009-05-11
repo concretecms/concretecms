f56e6c3936332a675c1fc327c295bcf702e16269
Andrew Embler <andrew@concrete5.org>
2009-05-11 19:07:35 +0000

    fixing inc() in add templates
    git-svn-id: http://svn.concrete5.org/svn/concrete5@995 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/google_map/add.php
+++ trunk/web/concrete/blocks/google_map/add.php
@@ -2,6 +2,4 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $mapObj=$controller;
 
-?>
-
-<? include($this->getBlockPath() .'/form_setup_html.php'); ?> 
+$this->inc('form_setup_html.php'); 
--- trunk/web/concrete/blocks/html/add.php
+++ trunk/web/concrete/blocks/html/add.php
@@ -3,4 +3,4 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 $controllerObj=$controller;
 ?>
 
-<? include($this->getBlockPath().'/form_setup_html.php'); ?> 
+<? $this->inc('form_setup_html.php');
