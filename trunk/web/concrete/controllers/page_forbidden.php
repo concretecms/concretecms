6b8a1384a04aa09be2a85ad95172b52125c58500
Andrew Embler <andrew@concrete5.org>
2009-05-12 20:16:12 +0000

    redirecting you to restricted page on login
    git-svn-id: http://svn.concrete5.org/svn/concrete5@997 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 481d43fe8e84c6b2dc08880ed71603d70df30eb1

--- trunk/web/concrete/controllers/page_forbidden.php
+++ trunk/web/concrete/controllers/page_forbidden.php
@@ -8,6 +8,14 @@ class PageForbiddenController extends LoginController {
 	
 	
 	public function view() {
+		$v = View::getInstance();
+		$c = $v->getCollectionObject();
+		if (is_object($c)) {
+			$cID = $c->getCollectionID();
+			if($cID) { 
+				$this->forward($cID); // set the intended url
+			}
+		}
 		parent::view();
 		$this->render('/login');
 	}
