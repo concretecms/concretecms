c6bf0dd4a6fc9eeaafb93c96de089c469e29fb1e
Andrew Embler <andrew@concrete5.org>
2009-05-17 16:45:19 +0000

    making non canonnical paths redirect 301
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1007 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: eca1ab03df6595506fdb19ed2345c92b1fc5b2c1

--- trunk/web/concrete/dispatcher.php
+++ trunk/web/concrete/dispatcher.php
@@ -133,7 +133,7 @@
 		## Check maintenance mode
 		require(dirname(__FILE__) . '/startup/maintenance_mode_check.php');
 		
-		## Check to see whether this is an external alias. If so we go there.
+		## Check to see whether this is an external alias or a header 301 redirect. If so we go there.
 		include(dirname(__FILE__) . '/startup/external_link.php');
 		
 		## Get a permissions object for this particular collection.
