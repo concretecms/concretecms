ca4c4ec5f4744abcee7c0d9ac07665ece217fa93
Tony Trupp <tony@concretecms.com>
2009-06-15 20:55:28 +0000

    change to autonav view, so selected path tree of nodes will get nav-path-selected class
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1054 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/autonav/view.php
+++ trunk/web/concrete/blocks/autonav/view.php
@@ -6,6 +6,20 @@
 	
 	$nh = Loader::helper('navigation');
 	
+	//this will create an array of parent cIDs 
+	$inspectC=$c;
+	$selectedPathCIDs=array( $inspectC->getCollectionID() );
+	$parentCIDnotZero=true;	
+	while($parentCIDnotZero){
+		$cParentID=$inspectC->cParentID;
+		if(!intval($cParentID)){
+			$parentCIDnotZero=false;
+		}else{
+			$selectedPathCIDs[]=$cParentID;
+			$inspectC=Page::getById($cParentID);
+		}
+	} 	
+	
 	foreach($aBlocks as $ni) {
 		$_c = $ni->getCollectionObject();
 		if (!$_c->getCollectionAttributeValue('exclude_nav')) {
@@ -39,7 +53,9 @@
 			}
 
 			if ($c->getCollectionID() == $_c->getCollectionID()) { 
-				echo('<li class="nav-selected"><a class="nav-selected" href="' . $pageLink . '">' . $ni->getName() . '</a>');
+				echo('<li class="nav-selected nav-path-selected"><a class="nav-selected nav-path-selected" href="' . $pageLink . '">' . $ni->getName() . '</a>');
+			} elseif ( in_array($_c->getCollectionID(),$selectedPathCIDs) ) { 
+				echo('<li class="nav-path-selected"><a class="nav-path-selected" href="' . $pageLink . '">' . $ni->getName() . '</a>');
 			} else {
 				echo('<li><a href="' . $pageLink . '">' . $ni->getName() . '</a>');
 			}	
