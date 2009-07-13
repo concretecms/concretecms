aad8abe970166b75518034c5a406ea4ab5653aa1
Ryan Tyler <ryan@concrete5.org>
2009-07-13 23:16:45 +0000

    added databases size waring to log db queries
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1109 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 3dfed53bf6203202b1b0694047fda283dfcf68b2

--- trunk/web/concrete/single_pages/dashboard/settings.php
+++ trunk/web/concrete/single_pages/dashboard/settings.php
@@ -101,7 +101,8 @@ $h = Loader::helper('concrete/interface'); ?>
 	<div class="ccm-dashboard-radio"><?=$form->checkbox('ENABLE_LOG_ERRORS', 1, $enable_log_errors)?> <?=t('Log Application Exceptions')?></div>
 	<div class="ccm-dashboard-description"><?=t('Saves application exceptions to logs.')?></div>
 	<div class="ccm-dashboard-radio"><?=$form->checkbox('ENABLE_LOG_DATABASE_QUERIES', 1, $enable_log_database_queries)?> <?=t('Log Database Activity')?></div>
-	<div class="ccm-dashboard-description"><?=t('Logs SQL queries for application profiling.')?></div>
+	<div class="ccm-dashboard-description"><?=t('Logs SQL queries for application profiling.')?><br />
+		<?=t('Warning: this may make your database huge!');?></div>
 	<div class="ccm-dashboard-radio"><?=$form->checkbox('ENABLE_LOG_EMAILS', 1, $enable_log_emails)?> <?=t('Log Emails Sent')?></div>
 	<div class="ccm-dashboard-description">
 		<?=t('Enables saving records of emails being sent out. This will save records even if actual email delivery is disabled on your site.')?>
