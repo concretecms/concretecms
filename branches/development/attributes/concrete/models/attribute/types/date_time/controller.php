73c48f069e3540ee6598fc241a2d7c278165f66d
Andrew Embler <andrew@concrete5.org>
2009-08-05 18:06:16 +0000

    removing form
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1161 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 43e1b8890f8ff159c6895804442e3131dbd1008b

--- branches/development/attributes/concrete/models/attribute/types/date_time/controller.php
+++ branches/development/attributes/concrete/models/attribute/types/date_time/controller.php
@@ -1,7 +1,7 @@
 <?
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
-class DateAttributeTypeController extends AttributeTypeController  {
+class DateTimeAttributeTypeController extends AttributeTypeController  {
 
 	public $helpers = array('form');
 	
@@ -13,7 +13,7 @@ class DateAttributeTypeController extends AttributeTypeController  {
 		$akDateDisplayMode = $this->post('akDateDisplayMode');
 				
 		// now we have a collection attribute key object above.
-		$db->Replace('atDateSettings', array(
+		$db->Replace('atDateTimeSettings', array(
 			'akID' => $ak->getAttributeKeyID(), 
 			'akDateDisplayMode' => $akDateDisplayMode
 		), array('akID'), true);
@@ -23,6 +23,17 @@ class DateAttributeTypeController extends AttributeTypeController  {
 		$this->load();
 	}
 	
+	public function form() {
+		$dt = Loader::helper('form/date_time');
+		print $dt->datetime($this->field('value'), $caValue);
+	}
+
+	public function getValue() {
+		$db = Loader::db();
+		$value = $db->GetOne("select value from atDateTime where avID = ?", array($this->getAttributeValueID()));
+		return $value;
+	}
+	
 	protected function load() {
 		$ak = $this->getAttributeKey();
 		if (!is_object($ak)) {
@@ -30,11 +41,28 @@ class DateAttributeTypeController extends AttributeTypeController  {
 		}
 		
 		$db = Loader::db();
-		$row = $db->GetRow('select akDateDisplayMode from atDateSettings where akID = ?', $ak->getAttributeKeyID());
+		$row = $db->GetRow('select akDateDisplayMode from atDateTimeSettings where akID = ?', $ak->getAttributeKeyID());
 		$this->akDateDisplayMode = $row['akDateDisplayMode'];
 
 		$this->set('akDateDisplayMode', $this->akDateDisplayMode);
 	}
 	
+	public function deleteKey() {
+		$db = Loader::db();
+		$arr = $this->attributeKey->getAttributeValueIDList();
+		foreach($arr as $id) {
+			$db->Execute('delete from atDateTime where avID = ?', array($id));
+		}
+	}
+	
+	public function saveForm($data) {
+		$db = Loader::db();
+		$this->saveValue($data['value']);
+	}
+	
+	public function deleteValue() {
+		$db = Loader::db();
+		$db->Execute('delete from atDateTime where avID = ?', array($this->getAttributeValueID()));
+	}
 
 }
