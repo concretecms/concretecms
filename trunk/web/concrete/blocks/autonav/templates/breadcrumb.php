b880533b834ae39811779bdeba71e371e5a0b55f
Andrew Embler <andrew@concrete5.org>
2009-08-07 16:05:40 +0000

    making breadcrumb template better
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1174 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/autonav/templates/breadcrumb.php
+++ trunk/web/concrete/blocks/autonav/templates/breadcrumb.php
@@ -2,13 +2,17 @@
 	defined('C5_EXECUTE') or die(_("Access Denied."));
 	$aBlocks = $controller->generateNav();
 	$c = Page::getCurrentPage();
+	$i = 0;
 	foreach($aBlocks as $ni) {
 		$_c = $ni->getCollectionObject();
-		if (!$_c->getCollectionAttributeValue('exclude_nav')) {			
+		if (!$_c->getCollectionAttributeValue('exclude_nav')) {	
+			if ($i > 0) {
+				print ' <span class="ccm-autonav-breadcrumb-sep">&gt;</span> ';
+			}
 			if ($c->getCollectionID() == $_c->getCollectionID()) { 
 				echo($ni->getName());
 			} else {
-				echo('<a href="' . $ni->getURL() . '">' . $ni->getName() . '</a> <span class="ccm-autonav-breadcrumb-sep">&gt;</span> ');
+				echo('<a href="' . $ni->getURL() . '">' . $ni->getName() . '</a>');
 			}	
 			$lastLevel = $thisLevel;
 			$i++;
@@ -16,6 +20,3 @@
 	}
 	
 	$thisLevel = 0;
-	echo("</p>");
-
-?>
