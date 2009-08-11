0ad60bc892161e3e08147c530a923e6dd1e10729
Andrew Embler <andrew@concrete5.org>
2009-08-11 18:50:14 +0000

    review fixins
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1192 b0551a0c-1e16-4222-a7d5-975db1aca215

--- branches/development/attributes/concrete/libraries/database_indexed_search.php
+++ branches/development/attributes/concrete/libraries/database_indexed_search.php
@@ -137,18 +137,7 @@ class IndexedSearch {
 				$v = array($row['cID'], $c->getCollectionName(), $c->getCollectionDescription(), $c->getCollectionPath(), $c->getCollectionDatePublic(), $this->getBodyContentFromPage($c));
 				$db->Execute("insert into PageSearchIndex (cID, cName, cDescription, cPath, cDatePublic, content) values (?, ?, ?, ?, ?, ?)", $v);
 				unset($v);
-				/*
-				$attributes=$c->getSetCollectionAttributes();
-				foreach($attributes as $attribute){
-					if ($attribute->isCollectionAttributeKeySearchable()) {
-						$val = $db->GetOne("select value from CollectionAttributeValues where cID = ? and cvID = ? and akID = ?", array($c->getCollectionID(), $c->getVersionID(), $attribute->getCollectionAttributeKeyID()));
-						$v = array($row['cID'], $attribute->getAttributeKeyID(), $attribute->getAttributeKeyHandle(), $val);
-						$db->Execute("insert into PageSearchIndexAttributes (cID, akID, akHandle, value) values (?, ?, ?, ?)", $v);
-					}
-				}
-				
-				unset($v);
-				*/
+				$c->reindex();
 				$num++;
 			}
 			
