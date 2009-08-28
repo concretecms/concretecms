e4c5481d36096b973b8ece1f3ceb16c02d292e57
Andrew Embler <andrew@concrete5.org>
2009-08-28 21:17:02 +0000

    git-svn-id: http://svn.concrete5.org/svn/concrete5@1235 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/startup/url_check.php
+++ trunk/web/concrete/startup/url_check.php
@@ -6,3 +6,8 @@ if (REDIRECT_TO_BASE_URL == true && (BASE_URL != 'http://' . $_SERVER['HTTP_HOST
 	header('Location: ' . BASE_URL . $_SERVER['REQUEST_URI']);
 	exit;
 }
+
+if (REDIRECT_TO_BASE_URL == true && strpos($_SERVER['REQUEST_URI'], '%7E') !== false) {
+	header('Location: ' . BASE_URL . str_replace('%7E', '~', $_SERVER['REQUEST_URI']));
+	exit;
+}
