9e66398ccb7d0b433129bc53e039ebc6e07549a2
Ryan Tyler <ryan@concrete5.org>
2009-09-22 14:21:08 +0000

    task# 2286 - changed debug level for depreciated function calls in php 5.3.0
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1285 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: fd79efebed3ea21c952f29733685cea652f2827a

--- trunk/web/concrete/startup/debug_logging.php
+++ trunk/web/concrete/startup/debug_logging.php
@@ -3,11 +3,11 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 $debug_level = Config::get('SITE_DEBUG_LEVEL');
 switch($debug_level) {
 	case DEBUG_DISPLAY_ERRORS:
-		error_reporting(E_ALL ^ E_NOTICE);
+		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED); // E_DEPRECATED required for php 5.3.0 because of depreciated function calls in 3rd party libs (adodb).
 		ini_set('display_errors', 1);
 		break;
 	default:
-		error_reporting(E_ALL ^ E_NOTICE);
+		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 		ini_set('display_errors', 0);
 		break;
 }
