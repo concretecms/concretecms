937b5db98f49303997f715995b4e56c049f33686
Ryan Tyler <ryan@concrete5.org>
2009-09-23 16:22:39 +0000

    fixed error_reporting for pre php 5.3.0 sites
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1290 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 85fbb842ad2db9d2b280e383b4d20028e6735e5f

--- trunk/web/concrete/startup/debug_logging.php
+++ trunk/web/concrete/startup/debug_logging.php
@@ -3,11 +3,19 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 $debug_level = Config::get('SITE_DEBUG_LEVEL');
 switch($debug_level) {
 	case DEBUG_DISPLAY_ERRORS:
-		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED); // E_DEPRECATED required for php 5.3.0 because of depreciated function calls in 3rd party libs (adodb).
+		if(defined("E_DEPRECATED")) {
+			error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED); // E_DEPRECATED required for php 5.3.0 because of depreciated function calls in 3rd party libs (adodb).
+		} else {
+			error_reporting(E_ALL ^ E_NOTICE);
+		}
 		ini_set('display_errors', 1);
 		break;
 	default:
-		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+		if(defined("E_DEPRECATED")) {
+			error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+		} else {
+			error_reporting(E_ALL ^ E_NOTICE);
+		}
 		ini_set('display_errors', 0);
 		break;
 }
