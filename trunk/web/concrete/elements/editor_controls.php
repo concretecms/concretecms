4a3e2147b408ff23ecc97cca9334c881d3e10842
Todd Crowe <toddbcrowe@gmail.com>
2009-09-24 01:36:48 +0000

    Task 2295: Add support for opening the sitemap overlay directly into a new dialog.
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1298 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/elements/editor_controls.php
+++ trunk/web/concrete/elements/editor_controls.php
@@ -4,7 +4,11 @@
 <ul>
 <li ccm-file-manager-field="rich-text-editor-image"><a class="ccm-file-manager-launch" onclick="ccm_editorCurrentAuxTool='image'; setBookMark();return false;" href="#"><?=t('Add Image')?></a></li>
 <li><a class="ccm-file-manager-launch" onclick="ccm_editorCurrentAuxTool='file'; setBookMark();return false;" href="#"><?=t('Add File')?></a></li>
+<?php if (isset($mode) && $mode == 'full') {?>
+<li><a href="#" onclick="setBookMark();sitemapOverlay();" class="dialog-launch" dialog-modal="false" ><?=t('Insert Link to Page')?></a></li>
+<?php } else {?>
 <li><a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/sitemap_overlay.php?sitemap_mode=select_page" onclick="setBookMark();" class="dialog-launch" dialog-modal="false" ><?=t('Insert Link to Page')?></a></li>
+<?php } ?>
 <li><a style="float: right" href="<?=View::url('/dashboard/settings')?>"><?=t('Customize Toolbar')?></a></li>
 </ul>
 </div>
@@ -14,3 +18,14 @@
 </div>
 
 <div class="ccm-spacer">&nbsp;</div>
+<script type="text/javascript">
+function sitemapOverlay() {
+    $.fn.dialog.open({
+        title: 'Choose A Page',
+        href: CCM_TOOLS_PATH + '/sitemap_overlay.php?sitemap_mode=select_page',
+        width: '550',
+        modal: false,
+        height: '400'
+    });
+};
+</script
