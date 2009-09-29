ce86c02e36041f51295f20a80fbcdaa8be01063d
Matias Gertel <matias@codium.co.nz>
2009-09-29 22:35:12 +0000

    added custom template
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1322 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 9272d8d94a5990647350478a181d348b6009a8db

---
+++ trunk/web/concrete/blocks/page_list/templates/custom.php
@@ -0,0 +1,52 @@
+<?
+	defined('C5_EXECUTE') or die(_("Access Denied."));
+	$textHelper = Loader::helper("text"); 
+	// now that we're in the specialized content file for this block type, 
+	// we'll include this block type's class, and pass the block to it, and get
+	// the content
+	
+	if (count($cArray) > 0) { ?>
+	<div class="ccm-page-list">
+	
+	<?
+	for ($i = 0; $i < count($cArray); $i++ ) {
+		$cobj = $cArray[$i]; 
+		$title = $cobj->getCollectionName(); ?>
+	
+	<h3 class="ccm-page-list-title"><a href="<?=$nh->getLinkToCollection($cobj)?>"><?=$title?></a></h3>
+	<? if ($cobj->getCollectionTypeHandle()=="Press Release") { ?>
+		<h4><? echo $cobj->getCollectionAttributeValue('Press_Release_Type'); ?> - for release on <? echo strftime("%x %l:%M%p",strtotime($cobj->getCollectionAttributeValue('Release_Date'))); ?></h4>
+	<? } ?>
+	<div class="ccm-page-list-description">
+		<?
+		if(!$controller->truncateSummaries){
+			echo $cobj->getCollectionDescription();
+		}else{
+			echo $textHelper->shorten($cobj->getCollectionDescription(),$controller->truncateChars);
+		}
+		?>
+	</div>
+	
+<?  } 
+	if(!$previewMode && $controller->rss) { 
+			$btID = $b->getBlockTypeID();
+			$bt = BlockType::getByID($btID);
+			$uh = Loader::helper('concrete/urls');
+			$rssUrl = $controller->getRssUrl($b);
+			?>
+			<div class="rssIcon">
+				<a href="<?=$rssUrl?>" target="_blank"><img src="<?=$uh->getBlockTypeAssetsURL($bt, 'rss.png')?>" width="14" height="14" /></a>
+				
+			</div>
+			<link href="<?=$rssUrl?>" rel="alternate" type="application/rss+xml" title="<?=$controller->rssTitle?>" />
+		<? 
+	} 
+	?>
+</div>
+<? } 
+	
+	if ($paginate && $num > 0 && is_object($pl)) {
+		$pl->displayPaging();
+	}
+	
+?>
