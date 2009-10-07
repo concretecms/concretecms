a4d48938215ab1fc59d064708a093a1c37251820
Tony Trupp <tony@concretecms.com>
2009-10-07 16:06:18 +0000

    email vaidation fix #2393
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1356 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: c198603ee04b0b7a5312732156a8edf560d20ff0

--- trunk/web/concrete/helpers/validation/strings.php
+++ trunk/web/concrete/helpers/validation/strings.php
@@ -18,61 +18,62 @@
  * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
  * @license    http://www.concrete5.org/license/     MIT License
  */
- 	defined('C5_EXECUTE') or die(_("Access Denied."));
-	class ValidationStringsHelper {	
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+class ValidationStringsHelper {	
+
 	
-		
-		/**
-		 * Validates an email address
-		 * @param string $address
-		 * @return bool $isvalid
-		 */
-		public function email($em) {
-			return preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/", $em);
-		}
-		
-		/**
-		 * Returns true on whether the passed field is completely alpha-numeric
-		 * @param string $field
-		 * @return bool
-		 */
-		public function alphanum($field, $allow_spaces = false) {
-			if($allow_spaces) {
-				return !preg_match("/[^A-Za-z0-9 ]/", $field);
-			} else {
-				return !preg_match('/[^A-Za-z0-9]/', $field);
-			}
-		}
-		
-		/**
-		 * Returns false if the string is empty (including trim())
-		 * @param string $field
-		 * @return bool
-		 */
-		public function notempty($field) {
-			return (trim($field) != '');
-		}	
-		
-		/** 
-		 * Returns true on whether the passed string is larger or equal to the passed length
-		 * @param string $str
-		 * @param int $length
-		 * @return bool
-		 */
-		public function min($str, $num) {
-			return strlen(trim($str)) >= $num;
-		}
-		
-		/** 
-		 * Returns true on whether the passed is smaller or equal to the passed length
-		 * @param string $str
-		 * @param int $length
-		 * @return bool
-		 */
-		public function max($str, $num) {
-			return strlen(trim($str)) <= $num;
-		}
-	
+	/**
+	 * Validates an email address
+	 * @param string $address
+	 * @return bool $isvalid
+	 */
+	public function email($em) {
+		return preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]*\.)+[a-zA-Z]{2,6}$/", $em);
 	}
 	
+	/**
+	 * Returns true on whether the passed field is completely alpha-numeric
+	 * @param string $field
+	 * @return bool
+	 */
+	public function alphanum($field, $allow_spaces = false) {
+		if($allow_spaces) {
+			return !preg_match("/[^A-Za-z0-9 ]/", $field);
+		} else {
+			return !preg_match('/[^A-Za-z0-9]/', $field);
+		}
+	}
+	
+	/**
+	 * Returns false if the string is empty (including trim())
+	 * @param string $field
+	 * @return bool
+	 */
+	public function notempty($field) {
+		return (trim($field) != '');
+	}	
+	
+	/** 
+	 * Returns true on whether the passed string is larger or equal to the passed length
+	 * @param string $str
+	 * @param int $length
+	 * @return bool
+	 */
+	public function min($str, $num) {
+		return strlen(trim($str)) >= $num;
+	}
+	
+	/** 
+	 * Returns true on whether the passed is smaller or equal to the passed length
+	 * @param string $str
+	 * @param int $length
+	 * @return bool
+	 */
+	public function max($str, $num) {
+		return strlen(trim($str)) <= $num;
+	}
+
+}
+
 ?>
