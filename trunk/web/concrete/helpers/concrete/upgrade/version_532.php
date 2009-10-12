8c94c5178e4154acb2d02d15a3a48a6bb7b8a19a
Andrew Embler <andrew@concrete5.org>
2009-10-12 16:56:16 +0000

    fixing upgrade bug
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1378 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 16ae33d37466b4874a72ae64e3aa7c918d9134e2

--- trunk/web/concrete/helpers/concrete/upgrade/version_532.php
+++ trunk/web/concrete/helpers/concrete/upgrade/version_532.php
@@ -91,7 +91,9 @@ class ConcreteUpgradeVersion532Helper {
 		$db = Loader::db();
 		
 		Cache::disableLocalCache();
-		Loader::model('collection_attributes');
+		Loader::model('attribute/categories/collection');
+		Loader::model('attribute/categories/file');
+		Loader::model('attribute/categories/user');
 		$collectionErrors = array();
 		$fileErrors = array();
 		$userErrors = array();
@@ -234,7 +236,6 @@ class ConcreteUpgradeVersion532Helper {
 	
 	protected function upgradeCollectionAttributes() {
 		$messages = array();
-		Loader::model('attribute/categories/collection');
 		$db = Loader::db();
 		$r = $db->Execute('select _CollectionAttributeKeys.* from _CollectionAttributeKeys order by _CollectionAttributeKeys.akID asc');
 		while ($row = $r->FetchRow()) {
@@ -316,7 +317,6 @@ class ConcreteUpgradeVersion532Helper {
 
 	protected function upgradeFileAttributes() {
 		$messages = array();
-		Loader::model('attribute/categories/file');
 		$db = Loader::db();
 		$r = $db->Execute('select _FileAttributeKeys.* from _FileAttributeKeys order by fakID asc');
 		while ($row = $r->FetchRow()) {
@@ -391,7 +391,6 @@ class ConcreteUpgradeVersion532Helper {
 
 	protected function upgradeUserAttributes() {
 		$messages = array();
-		Loader::model('attribute/categories/user');
 		$db = Loader::db();
 		$r = $db->Execute('select _UserAttributeKeys.* from _UserAttributeKeys order by displayOrder asc');
 		while ($row = $r->FetchRow()) {
