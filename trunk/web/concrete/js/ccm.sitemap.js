1162be8067c44c7110683cb26e72b54b6d8aedd1
Andrew Embler <andrew@concrete5.org>
2009-10-19 18:05:02 +0000

    fixing sitemap target bug we introduced where copying page was impossible
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1392 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 1b14b65fd77d2efaf8caee34a6887bcb0bb6c3cc

--- trunk/web/concrete/js/ccm.sitemap.js
+++ trunk/web/concrete/js/ccm.sitemap.js
@@ -330,24 +330,28 @@ selectMoveCopyTarget = function(destCID) {
 	var dialog_url = CCM_TOOLS_PATH + '/dashboard/sitemap_drag_request.php?origCID=' + origCID + '&sitemap_mode=' + CCM_SITEMAP_MODE + '&destCID=' + destCID;
 	var dialog_height = 350;
 	var dialog_width = 350;
-
-	if (CCM_NODE_ACTION == '<none>') {
-		if (CCM_TARGET_ID != '') {
-			$('#'+CCM_TARGET_ID).val(destCID);
+	
+	try {
+		if (CCM_NODE_ACTION == '<none>') {
+			if (CCM_TARGET_ID != '') {
+				$('#'+CCM_TARGET_ID).val(destCID);
+			}
+			$.fn.dialog.closeTop();
+			return;
 		}
-		$.fn.dialog.closeTop();
-		return;
+	
+		if (CCM_NODE_ACTION != '')
+			dialog_url = CCM_NODE_ACTION+'?destCID='+destCID;
+		if (CCM_DIALOG_TITLE != '')
+			dialog_title = CCM_DIALOG_TITLE;
+		if (CCM_DIALOG_HEIGHT != '')
+			dialog_height = CCM_DIALOG_HEIGHT;
+		if (CCM_DIALOG_WIDTH != '')
+			dialog_width = CCM_DIALOG_WIDTH;
+	} catch(e) {
+	
 	}
-
-	if (CCM_NODE_ACTION != '')
-		dialog_url = CCM_NODE_ACTION+'?destCID='+destCID;
-	if (CCM_DIALOG_TITLE != '')
-		dialog_title = CCM_DIALOG_TITLE;
-	if (CCM_DIALOG_HEIGHT != '')
-		dialog_height = CCM_DIALOG_HEIGHT;
-	if (CCM_DIALOG_WIDTH != '')
-		dialog_width = CCM_DIALOG_WIDTH;
-		
+	
 	$.fn.dialog.open({
 		title: dialog_title,
 		href: dialog_url,
@@ -356,7 +360,7 @@ selectMoveCopyTarget = function(destCID) {
 		height: dialog_height,
 		onClose: function() {
 			//$("#tree").fadeIn(200);
-			if (CCM_TARGET_ID != '') {
+			if (typeof(CCM_TARGET_ID) != "undefined" && CCM_TARGET_ID != '') {
 				$('#'+CCM_TARGET_ID).val(destCID);
 			}
 			if (tr_moveCopyMode == true) {
