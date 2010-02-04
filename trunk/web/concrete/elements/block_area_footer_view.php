d73448e24bf9d5f7b8a3efc5901dad3705960a19
Andrew Embler <andrew@concrete5.org>
2010-02-04 17:09:24 +0000

    more style work
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1524 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 4301ed5be15a159476c9f3b958fe3f6d24e9c692

--- trunk/web/concrete/elements/block_area_footer_view.php
+++ trunk/web/concrete/elements/block_area_footer_view.php
@@ -1,4 +1,7 @@
 <?
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
+$c = Page::getCurrentPage();
+$areaStyle = $c->getAreaCustomStyleRule($a);
+
 if($areaStyle && $areaStyle->getCustomStyleRuleID() ){ ?></div><? } ?>
