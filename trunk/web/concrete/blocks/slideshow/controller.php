b72c339795b9fa5bfe8472d8a111f40e63aa79a8
Andrew Embler <andrew@concrete5.org>
2010-02-06 17:20:14 +0000

    slideshow bug fix
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1542 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/slideshow/controller.php
+++ trunk/web/concrete/blocks/slideshow/controller.php
@@ -32,36 +32,10 @@ class SlideshowBlockController extends BlockController {
 		);
 	}
 	
-	function __construct($obj = null) {		
-		parent::__construct($obj);
-		$this->db = Loader::db();
-		if ($this->fsID == 0) {
-			$this->loadImages();
-		} else {
-			$this->loadFileSet();
-		}
-		$this->randomizeImages();	
-		$this->set('defaultFadeDuration', $this->defaultFadeDuration);
-		$this->set('defaultDuration', $this->defaultDuration);
-		$this->set('fadeDuration', $this->fadeDuration);
-		$this->set('duration', $this->duration);
-		$this->set('minHeight', $this->minHeight);
-		$this->set('fsID', $this->fsID);
-		$this->set('fsName', $this->getFileSetName());
-		$this->set('images', $this->images);
-		$this->set('playback', $this->playback);
-		$type = ($this->fsID > 0) ? 'FILESET' : 'CUSTOM';
-		$this->set('type', $type);
-		$this->set('bID', $this->bID);			
-	}	
-	
-	function view() { }
-	
-	function on_page_view() { }
-	
 	function getFileSetName(){
 		$sql = "SELECT fsName FROM FileSets WHERE fsID=".intval($this->fsID);
-		return $this->db->getOne($sql); 
+		$db = Loader::db();
+		return $db->getOne($sql); 
 	}
 
 	function loadFileSet(){
@@ -119,32 +93,79 @@ class SlideshowBlockController extends BlockController {
 			$this->playback='ORDER';
 		if(intval($this->bID)==0) return array();
 		$sql = "SELECT * FROM btSlideshowImg WHERE bID=".intval($this->bID).' ORDER BY '.$sortChoices[$this->playback];
-		$this->images=$this->db->getAll($sql); 
+		$db = Loader::db();
+		$this->images=$db->getAll($sql); 
 	}
 	
 	function delete(){
-		$this->db->query("DELETE FROM btSlideshowImg WHERE bID=".intval($this->bID));		
+		$db = Loader::db();
+		$db->query("DELETE FROM btSlideshowImg WHERE bID=".intval($this->bID));		
 		parent::delete();
+	}
+	
+	function loadBlockInformation() {
+		if ($this->fsID == 0) {
+			$this->loadImages();
+		} else {
+			$this->loadFileSet();
+		}
+		$this->randomizeImages();	
+		$this->set('defaultFadeDuration', $this->defaultFadeDuration);
+		$this->set('defaultDuration', $this->defaultDuration);
+		$this->set('fadeDuration', $this->fadeDuration);
+		$this->set('duration', $this->duration);
+		$this->set('minHeight', $this->minHeight);
+		$this->set('fsID', $this->fsID);
+		$this->set('fsName', $this->getFileSetName());
+		$this->set('images', $this->images);
+		$this->set('playback', $this->playback);
+		$type = ($this->fsID > 0) ? 'FILESET' : 'CUSTOM';
+		$this->set('type', $type);
+		$this->set('bID', $this->bID);				
+	}
+	
+	function view() {
+		$this->loadBlockInformation();
+	}
+
+	function add() {
+		$this->loadBlockInformation();
+	}
+	
+	function edit() {
+		$this->loadBlockInformation();
+	}
+	
+	function duplicate($nbID) {
+		parent::duplicate($nbID);
+		$this->loadBlockInformation();
+		$db = Loader::db();
+		foreach($this->images as $im) {
+			$db->Execute('insert into btSlideshowImg (bID, fID, url, duration, fadeDuration, groupSet, position, imgHeight) values (?, ?, ?, ?, ?, ?, ?, ?)', 
+				array($nbID, $im['fID'], $im['url'], $im['duration'], $im['fadeDuration'], $im['groupSet'], $im['position'], $im['imgHeight'])
+			);		
+		}
 	}
 	
 	function save($data) { 
 		$args['playback'] = isset($data['playback']) ? trim($data['playback']) : 'ORDER';
+		$db = Loader::db();
 		
 		if( $data['type'] == 'FILESET' && $data['fsID'] > 0){
 			$args['fsID'] = $data['fsID'];
 			$args['duration'] = $data['duration'][0];
 			$args['fadeDuration'] = $data['fadeDuration'][0];
 
-			$files = $this->db->getAll("SELECT fv.fID FROM FileSetFiles fsf, FileVersions fv WHERE fsf.fsID = " . $data['fsID'] .
+			$files = $db->getAll("SELECT fv.fID FROM FileSetFiles fsf, FileVersions fv WHERE fsf.fsID = " . $data['fsID'] .
 			         " AND fsf.fID = fv.fID AND fvIsApproved = 1");
 			
 			//delete existing images
-			$this->db->query("DELETE FROM btSlideshowImg WHERE bID=".intval($this->bID));
+			$db->query("DELETE FROM btSlideshowImg WHERE bID=".intval($this->bID));
 		} else if( $data['type'] == 'CUSTOM' && count($data['imgFIDs']) ){
 			$args['fsID'] = 0;
 
 			//delete existing images
-			$this->db->query("DELETE FROM btSlideshowImg WHERE bID=".intval($this->bID));
+			$db->query("DELETE FROM btSlideshowImg WHERE bID=".intval($this->bID));
 			
 			//loop through and add the images
 			$pos=0;
@@ -152,7 +173,7 @@ class SlideshowBlockController extends BlockController {
 				if(intval($imgFID)==0 || $data['fileNames'][$pos]=='tempFilename') continue;
 				$vals = array(intval($this->bID),intval($imgFID), trim($data['url'][$pos]),intval($data['duration'][$pos]),intval($data['fadeDuration'][$pos]),
 						intval($data['groupSet'][$pos]),intval($data['imgHeight'][$pos]),$pos);
-				$this->db->query("INSERT INTO btSlideshowImg (bID,fID,url,duration,fadeDuration,groupSet,imgHeight,position) values (?,?,?,?,?,?,?,?)",$vals);
+				$db->query("INSERT INTO btSlideshowImg (bID,fID,url,duration,fadeDuration,groupSet,imgHeight,position) values (?,?,?,?,?,?,?,?)",$vals);
 				$pos++;
 			}
 		}
