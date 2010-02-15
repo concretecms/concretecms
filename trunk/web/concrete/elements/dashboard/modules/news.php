482c2fd5e0971e981d1b8fa239f77fdac13ec125
Andrew Embler <andrew@concrete5.org>
2010-02-15 18:33:25 +0000

    alkfj
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1588 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/elements/dashboard/modules/news.php
+++ trunk/web/concrete/elements/dashboard/modules/news.php
@@ -3,12 +3,12 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 foreach($posts as $item) { ?>
 	
 	<div class="post">
-	<h4><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h4>
-	<h5><?php echo $item->get_date('F jS'); ?></h5>
-	<?php echo $item->get_description(); ?>
+	<h4><a href="<?php echo $item->getSystemNotificationURL(); ?>"><?php echo $item->getSystemNotificationTitle(); ?></a></h4>
+	<h5><?php echo date(t('F jS'), strtotime($item->getSystemNotificationDateTime())); ?></h5>
+	<?php echo $item->getSystemNotificationDescription(); ?>
 	</div>
 <? } ?>
 
 <h2><?=t('Read More')?></h2>
 
-<p><?=t('Read more C5 news <a href="%s">at the C5 Developer Center</a>', $feed_read_more)?>.</p>
+<p><?=t('Read more concrete5 news <a href="%s">at the concrete5 Developer Center</a>', $feed_read_more)?>.</p>
--- trunk/web/concrete/models/system_notification.php
+++ trunk/web/concrete/models/system_notification.php
@@ -9,10 +9,18 @@ class SystemNotification extends Object {
 	const SN_TYPE_ADDON_UPDATE = 20;
 	const SN_TYPE_ADDON_MESSAGE = 22;
 	
+	public function getSystemNotificationURL() {return $this->snURL;}
+	public function getSystemNotificationTitle() {return $this->snTitle;}
+	public function getSystemNotificationDescription() {return $this->snDescription;}
+	public function getSystemNotificationBody() {return $this->snBody;}
+	public function getSystemNotificationDateTime() {return $this->snDateTime;}
+	public function isSystemNotificationNew() {return $this->snIsNew;}
+	public function isSystemNotificationArchived() {return $this->snIsArchived;}
+	
 	public static function add($typeID, $title, $description, $body, $url) {
 		$db = Loader::db();
 		$date = Loader::helper('date')->getLocalDateTime();
-		$db->Execute('insert into SystemNotifications (snTypeID, snTitle, snDescription, snBody, snURL, snDatetime, snIsNew) values (?, ?, ?, ?, ?, ?, ?)', array(
+		$db->Execute('insert into SystemNotifications (snTypeID, snTitle, snDescription, snBody, snURL, snDateTime, snIsNew) values (?, ?, ?, ?, ?, ?, ?)', array(
 			$typeID, $title, $description, $body, $url, $date, 1
 		));	
 	}
@@ -23,15 +31,46 @@ class SystemNotification extends Object {
 		$cnt = $db->GetOne('select count(snID) from SystemNotifications where snURL = ?', array($post->get_permalink()));
 		if ($cnt == 0) {
 			// otherwise we already have this
-			$db->Execute('insert into SystemNotifications (snTypeID, snTitle, snDescription, snBody, snURL, snDatetime, snIsNew) values (?, ?, ?, ?, ?, ?, ?)', array(
+			$db->Execute('insert into SystemNotifications (snTypeID, snTitle, snDescription, snBody, snURL, snDateTime, snIsNew) values (?, ?, ?, ?, ?, ?, ?)', array(
 				$type, $post->get_title(), $post->get_description(), $post->get_content(), $post->get_permalink(), $post->get_date('Y-m-d H:i:s'), 1
 			));
 		}	
 	}
-
-	public static function getListByType($typeID) {
-
-		
+	
+	public static function getByID($snID) {
+		$db = Loader::db();
+		$row = $db->GetRow('select * from SystemNotifications where snID = ?', array($snID));
+		if (is_array($row) && $row['snID']) {
+			$sn = new SystemNotification();
+			$sn->setPropertiesFromArray($row);
+			return $sn;
+		}
 	}
 
+}
+
+class SystemNotificationList extends DatabaseItemList {
+	
+	public function filterByType($type) {
+		$db = Loader::db();
+		$this->filter('sn.snTypeID', $type);
+	}
+	
+	function __construct() {
+		$this->setQuery("select sn.snID from SystemNotifications sn");
+		$this->sortBy('snDateTime', 'desc');
+	}
+
+	public function get($itemsToGet = 0, $offset = 0) {
+		$r = parent::get($itemsToGet, $offset);
+		$posts = array();
+		foreach($r as $snID) {
+			$sn = SystemNotification::getByID($snID);
+			if (is_object($sn)) {
+				$posts[] = $sn;
+			}
+		}
+		return $posts;
+	}
+	
 }
