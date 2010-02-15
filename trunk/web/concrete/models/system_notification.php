8cd5927fab082933225a56194f060a3774b1df14
Andrew Embler <andrew@concrete5.org>
2010-02-15 00:59:09 +0000

    update tweaks
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1583 b0551a0c-1e16-4222-a7d5-975db1aca215

---
+++ trunk/web/concrete/models/system_notification.php
@@ -0,0 +1,17 @@
+<?
+
+class SystemNotification extends Object {
+
+	const SN_TYPE_CORE_UPDATE = 10;
+	const SN_TYPE_CORE_MESSAGE_HELP = 11;
+	const SN_TYPE_CORE_MESSAGE_NEWS = 12;
+	const SN_TYPE_CORE_MESSAGE_OTHER = 19;
+	const SN_TYPE_ADDON_UPDATE = 20;
+	const SN_TYPE_ADDON_MESSAGE = 22;
+	
+	
+
+
+
+
+}
