5b59138db5a2c17f951ff14084e0aece7b4db249
Andrew Embler <andrew@concrete5.org>
2010-02-15 01:54:29 +0000

    updates
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1584 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: 1fec37aeef221b69b70d2d4ce6bff97dfcfc7362

--- trunk/web/concrete/models/system_notification.php
+++ trunk/web/concrete/models/system_notification.php
@@ -9,7 +9,13 @@ class SystemNotification extends Object {
 	const SN_TYPE_ADDON_UPDATE = 20;
 	const SN_TYPE_ADDON_MESSAGE = 22;
 	
-	
+	public static function add($typeID, $text, $additionalText, $url) {
+		$db = Loader::db();
+		$date = Loader::helper('date')->getLocalDateTime();
+		$db->Execute('insert into SystemNotifications (snTypeID, snNotificationText, snNotificationAdditionalText, snURL, snDatetime, snIsNew) values (?, ?, ?, ?, ?, ?)', array(
+			$typeID, $text, $additionalText, $url, $date, 1
+		));	
+	}
 
 
 
