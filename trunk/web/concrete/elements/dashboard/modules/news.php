242a5f8ff237abec374c9590fbc64cd23b543fad
Andrew Embler <andrew@concrete5.org>
2010-02-15 22:56:28 +0000

    more update work
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1590 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: bfee718d59a495ea3f6a31952eca2f4538ed8070

--- trunk/web/concrete/elements/dashboard/modules/news.php
+++ trunk/web/concrete/elements/dashboard/modules/news.php
@@ -1,7 +1,10 @@
 <?
 defined('C5_EXECUTE') or die(_("Access Denied."));
-Loader::element('dashboard/notification_list', array('notifications' => $notifications, 'isDashboardModule' => true));
-?>
+if ($total > 0) { 
+	Loader::element('dashboard/notification_list', array('notifications' => $notifications, 'isDashboardModule' => true));
+} else { ?>
+	<p><?=t('There are no notifications.')?></p>
+<? } ?>
 
 <? if ($total > 1) { ?>
 	<p><a href="<?=View::url('/dashboard/system/notifications')?>"><?=t('View all %s notifications &gt;', $total)?></a></p>
