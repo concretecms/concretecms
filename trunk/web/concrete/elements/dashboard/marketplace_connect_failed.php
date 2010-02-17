de81a098f6c55052037bb6d12d03e8b92d759eca
Andrew Embler <andrew@concrete5.org>
2010-02-17 00:03:18 +0000

    marketplace
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1599 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/elements/dashboard/marketplace_connect_failed.php
+++ trunk/web/concrete/elements/dashboard/marketplace_connect_failed.php
@@ -1,19 +1,34 @@
 <?
 defined('C5_EXECUTE') or die(_("Access Denied."));
-$h = Loader::helper('concrete/interface');
-?>
-
-<?=t('Your site is <strong>not</strong> connected to the concrete5 community.')?>
-<?
 $mi = Marketplace::getInstance();
-if ($mi->hasConnectionError()) { 
-	switch($mi->getConnectionError()) {
-		default:
-			print $mi->getConnectionError();
-			break;
-	}
-}
-?>
-<br/><br/>
+if ($mi->hasConnectionError() && $mi->getConnectionError() == Marketplace::E_MARKETPLACE_SUPPORT_MANUALLY_DISABLED) { ?>
+	<p><?=t('Marketplace integration disabled in configuration file.')?></p>
 
-<? print $h->button(t('Connect to Community'), $this->url('/dashboard/settings/marketplace'))?>
+<? } else {
+
+	$h = Loader::helper('concrete/interface');
+	?>
+	
+	<?=t('Your site is <strong>not</strong> connected to the concrete5 community.')?>
+	<?
+	if ($mi->hasConnectionError()) { ?>
+		<div class="ccm-error"><br/>
+		<?
+		switch($mi->getConnectionError()) {
+			default:
+				case Marketplace::E_INVALID_BASE_URL:
+					print t('The base URL of your site does not match a registered instance of the site. Please click below to authenticate your site again.');
+					
+					break;
+	
+		}
+		?>
+		</div>
+		<?
+	}
+	?>
+	
+	<br/>
+	
+	<? print $h->button(t('Connect to Community'), $this->url('/dashboard/settings/marketplace'))?>
+<? } ?>
