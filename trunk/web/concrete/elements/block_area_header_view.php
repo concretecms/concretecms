705e4aba509308657c08522770dcc6e555d1e21e
Tony Trupp <tony@concretecms.com>
2010-02-26 08:41:11 +0000

    layout - bug fix-> insert block into nestled area styles
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1662 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/elements/block_area_header_view.php
+++ trunk/web/concrete/elements/block_area_header_view.php
@@ -5,5 +5,5 @@ $c = Page::getCurrentPage();
 $areaStyle = $c->getAreaCustomStyleRule($a);
 
 if (is_object($areaStyle)) { ?>
-	<div id="<?=$areaStyle->getCustomStyleRuleCSSID(true)?>" class="<?=$areaStyle->getCustomStyleRuleClassName() ?> ccm-area-styles" >
+	<div id="<?=$areaStyle->getCustomStyleRuleCSSID(true)?>" class="<?=$areaStyle->getCustomStyleRuleClassName() ?> ccm-area-styles ccm-area-styles-a<?=$a->getAreaID()?>" >
 <? } ?>
