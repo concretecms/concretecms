68c44aa1d3bbe64a401cd4d218aa9b3a361e93a2
Tony Trupp <tony@concretecms.com>
2010-03-17 05:39:48 +0000

    layout delete block options panel continued
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1742 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: dd4385bcc014bd7a0c444c9e7503ae5369724018

--- trunk/web/concrete/js/ccm.layout.js
+++ trunk/web/concrete/js/ccm.layout.js
@@ -227,17 +227,16 @@ function ccmLayout( cvalID, layout_id, area, locked ){
 		this.layoutWrapper.find('.ccm-block').each(function(i,el){
 			if(el.style.display!='none')  hasBlocks=1;													
 		})
-		if( hasBlocks ){   
-			$.fn.dialog.open({
-				title: ccmi18n.deleteLayoutOptsTitle,
-				href:  CCM_TOOLS_PATH + '/layout_services.php?cID=' + CCM_CID + '&arHandle=' + this.area + '&layoutID=' + this.layout_id +  '&task=deleteOpts',
-				width: '340px',
-				modal: false,
-				height: '120px'
-			});			
-		}else{
-			if( confirm(ccmi18n.deleteLayoutNoBlocksConfirmMsg) ) this.deleteLayout(1); 
-		}
+		var dialogHeight=(hasBlocks)?'110px':'50px';
+		
+		$.fn.dialog.open({
+			title: ccmi18n.deleteLayoutOptsTitle,
+			href:  CCM_TOOLS_PATH + '/layout_services.php?cID=' + CCM_CID + '&arHandle=' + this.area + '&layoutID=' + this.layout_id +  '&task=deleteOpts&hasBlocks='+hasBlocks,
+			width: '340px',
+			modal: false,
+			height: dialogHeight
+		});	
+			
 	}
 	
 	this.deleteLayout=function(deleteBlocks){   
