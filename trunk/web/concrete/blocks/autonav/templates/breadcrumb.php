c48ac60254edf5f75fc6b221b6358a7c3a072fd3
Andrew Embler <andrew@concrete5.org>
2010-03-19 16:21:26 +0000

    making breadcrumb respect replace link with first in nav
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1745 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/blocks/autonav/templates/breadcrumb.php
+++ trunk/web/concrete/blocks/autonav/templates/breadcrumb.php
@@ -2,17 +2,32 @@
 	defined('C5_EXECUTE') or die(_("Access Denied."));
 	$aBlocks = $controller->generateNav();
 	$c = Page::getCurrentPage();
+	$nh = Loader::helper('navigation');
 	$i = 0;
 	foreach($aBlocks as $ni) {
 		$_c = $ni->getCollectionObject();
 		if (!$_c->getCollectionAttributeValue('exclude_nav')) {	
+
+			$pageLink = false;
+			
+			if ($_c->getCollectionAttributeValue('replace_link_with_first_in_nav')) {
+				$subPage = $_c->getFirstChild();
+				if ($subPage instanceof Page) {
+					$pageLink = $nh->getLinkToCollection($subPage);
+				}
+			}
+			
+			if (!$pageLink) {
+				$pageLink = $ni->getURL();
+			}
+
 			if ($i > 0) {
 				print ' <span class="ccm-autonav-breadcrumb-sep">&gt;</span> ';
 			}
 			if ($c->getCollectionID() == $_c->getCollectionID()) { 
 				echo($ni->getName());
 			} else {
-				echo('<a href="' . $ni->getURL() . '">' . $ni->getName() . '</a>');
+				echo('<a href="' . $pageLink . '">' . $ni->getName() . '</a>');
 			}	
 			$lastLevel = $thisLevel;
 			$i++;
