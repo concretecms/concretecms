51aa51d05353b38d3ac9a2e76bed29eb55eda51d
Andrew Embler <andrew@concrete5.org>
2010-03-26 22:17:26 +0000

    google map fix
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1758 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: df00b9189b77c6f0982e8a92c4a91a53ab35e051

--- trunk/web/concrete/blocks/google_map/edit.php
+++ trunk/web/concrete/blocks/google_map/edit.php
@@ -1,6 +1,4 @@
 <?
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $mapObj=$controller;
-?>
-
-<? include($this->getBlockPath() .'/form_setup_html.php'); ?> 
+$this->inc('form_setup_html.php',array( 'mapObj' => $controller ));
