ae41d2accdd83521bfeee26fad054d09561615da
Andrew Embler <andrew@concrete5.org>
2010-03-29 20:28:26 +0000

    updates
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1764 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/startup/updated_core_check.php
+++ trunk/web/concrete/startup/updated_core_check.php
@@ -1,6 +1,7 @@
 <?
 
-$td = substr(dirname(__FILE__), 0, strpos(dirname(__FILE__), DIRECTORY_SEPARATOR . 'startup'));
-if (defined('DIRNAME_APP_UPDATED') && $td != DIR_BASE . DIRECTORY_SEPARATOR . DIRNAME_UPDATES . DIRECTORY_SEPARATOR . DIRNAME_APP_UPDATED . DIRECTORY_SEPARATOR . DIRNAME_APP) {
+if (defined('DIRNAME_APP_UPDATED') && (!isset($GLOBALS['APP_UPDATED_PASSTHRU']) || $GLOBALS['APP_UPDATED_PASSTHRU'] == false)) {
+	$GLOBALS['APP_UPDATED_PASSTHRU'] = true;
 	require(DIR_BASE . '/' . DIRNAME_UPDATES . '/' . DIRNAME_APP_UPDATED . '/' . DIRNAME_APP . '/' . 'dispatcher.php');
+	exit;
 }
