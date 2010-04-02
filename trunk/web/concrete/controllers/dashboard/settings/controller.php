db782ddf99c95b798bbcb22ff9b1d2ccf4ce006e
Andrew Embler <andrew@concrete5.org>
2010-04-02 16:01:20 +0000

    improving environment
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1780 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/controllers/dashboard/settings/controller.php
+++ trunk/web/concrete/controllers/dashboard/settings/controller.php
@@ -469,11 +469,100 @@ class DashboardSettingsController extends Controller {
 				$phpinfo[end(array_keys($phpinfo))][] = $match[2];
 			}
 		}
+		
+		$environmentMessage = '# ' . t('concrete5 Version') . "\n" . APP_VERSION . "\n\n";
+		$environmentMessage .= '# ' . t('concrete5 Packages') . "\n";
+		$pla = PackageList::get();
+		$pl = $pla->getPackages();
+		$packages = array();
+		foreach($pl as $p) {
+			if ($p->isPackageInstalled()) {
+				$packages[] =$p->getPackageName() . ' (' . $p->getPackageVersion() . ')';
+			}			
+		}
+		if (count($packages) > 0) {
+			natcasesort($packages);
+			$environmentMessage .= implode(', ', $packages);
+			$environmentMessage .= ".\n";
+		} else {
+			$environmentMessage .= t('None') . "\n";
+		}
+		$environmentMessage .= "\n";
+		
+		// overrides
+		$environmentMessage .= '# ' . t('concrete5 Overrides') . "\n";
+		$fh = Loader::helper('file');
+		$overrides = array();
+		$ovBlocks = $fh->getDirectoryContents(DIR_FILES_BLOCK_TYPES);
+		$ovControllers = $fh->getDirectoryContents(DIR_FILES_CONTROLLERS);
+		$ovElements = $fh->getDirectoryContents(DIR_FILES_ELEMENTS);
+		$ovHelpers = $fh->getDirectoryContents(DIR_HELPERS);
+		$ovJobs = $fh->getDirectoryContents(DIR_FILES_JOBS);
+		$ovCSS = $fh->getDirectoryContents(DIR_BASE . '/' . DIRNAME_CSS);
+		$ovJS = $fh->getDirectoryContents(DIR_BASE . '/' . DIRNAME_JAVASCRIPT);
+		$ovLng = $fh->getDirectoryContents(DIR_BASE . '/' . DIRNAME_LANGUAGES);
+		$ovLibs = $fh->getDirectoryContents(DIR_LIBRARIES);
+		$ovMail = $fh->getDirectoryContents(DIR_FILES_EMAIL_TEMPLATES);
+		$ovModels = $fh->getDirectoryContents(DIR_MODELS);
+		$ovSingle = $fh->getDirectoryContents(DIR_FILES_CONTENT);
+		$ovThemes = $fh->getDirectoryContents(DIR_FILES_THEMES);
+		$ovTools = $fh->getDirectoryContents(DIR_FILES_TOOLS);
 
-		$environmentMessage = '# ' . t('PHP Version') . "\n" . PHP_VERSION . "\n\n";
+		foreach($ovBlocks as $ovb) {
+			$overrides[] = DIRNAME_BLOCKS . '/' . $ovb;
+		}
+		foreach($ovControllers as $ovb) {
+			$overrides[] = DIRNAME_CONTROLLERS . '/' . $ovb;
+		}
+		foreach($ovElements as $ovb) {
+			$overrides[] = DIRNAME_ELEMENTS . '/' . $ovb;
+		}
+		foreach($ovHelpers as $ovb) {
+			$overrides[] = DIRNAME_HELPERS . '/' . $ovb;
+		}
+		foreach($ovJobs as $ovb) {
+			$overrides[] = DIRNAME_JOBS . '/' . $ovb;
+		}
+		foreach($ovJS as $ovb) {
+			$overrides[] = DIRNAME_JAVASCRIPT . '/' . $ovb;
+		}
+		foreach($ovCSS as $ovb) {
+			$overrides[] = DIRNAME_CSS . '/' . $ovb;
+		}
+		foreach($ovLng as $ovb) {
+			$overrides[] = DIRNAME_LANGUAGES . '/' . $ovb;
+		}
+		foreach($ovLibs as $ovb) {
+			$overrides[] = DIRNAME_LIBRARIES . '/' . $ovb;
+		}
+		foreach($ovMail as $ovb) {
+			$overrides[] = DIRNAME_MAIL_TEMPLATES . '/' . $ovb;
+		}
+		foreach($ovModels as $ovb) {
+			$overrides[] = DIRNAME_MODELS . '/' . $ovb;
+		}
+		foreach($ovSingle as $ovb) {
+			$overrides[] = DIRNAME_PAGES . '/' . $ovb;
+		}
+		foreach($ovThemes as $ovb) {
+			$overrides[] = DIRNAME_THEMES . '/' . $ovb;
+		}
+		foreach($ovTools as $ovb) {
+			$overrides[] = DIRNAME_TOOLS . '/' . $ovb;
+		}
+
+		if (count($overrides) > 0) {
+			$environmentMessage .= implode(', ', $overrides);
+			$environmentMessage .= "\n";
+		} else {
+			$environmentMessage .= t('None') . "\n";
+		}
+		$environmentMessage .= "\n";
+		
 		$environmentMessage .= '# ' . t('Server Software') . "\n" . $_SERVER['SERVER_SOFTWARE'] . "\n\n";
 		$environmentMessage .= '# ' . t('Server API') . "\n" . php_sapi_name() . "\n\n";
-		$environmentMessage .= '# ' . t('Loaded Extensions') . "\n";
+		$environmentMessage .= '# ' . t('PHP Version') . "\n" . PHP_VERSION . "\n\n";
+		$environmentMessage .= '# ' . t('PHP Extensions') . "\n";
 		if (function_exists('get_loaded_extensions')) {
 			$gle = @get_loaded_extensions();
 			natcasesort($gle);
@@ -483,7 +572,7 @@ class DashboardSettingsController extends Controller {
 			$environmentMessage .= t('Unable to determine.') . "\n";
 		}
 
-		$environmentMessage .= "\n# " . t('Limits') . "\n";
+		$environmentMessage .= "\n# " . t('PHP Settings') . "\n";
 
 		foreach($phpinfo as $name => $section) {
 			foreach($section as $key => $val) {
