0d54745cc23a857c2de77b6ad5460d9ad4eff10c
Andrew Embler <andrew@concrete5.org>
2010-04-14 22:07:56 +0000

    update tweawk
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1806 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: ba14df39ea6ec518565036b050427af24a72d3bd

--- trunk/web/concrete/startup/updated_core_check.php
+++ trunk/web/concrete/startup/updated_core_check.php
@@ -2,7 +2,7 @@
 
 if (defined('DIRNAME_APP_UPDATED') && (!isset($GLOBALS['APP_UPDATED_PASSTHRU']) || $GLOBALS['APP_UPDATED_PASSTHRU'] == false)) {
 	$GLOBALS['APP_UPDATED_PASSTHRU'] = true;
-	if (is_dir(DIR_BASE . '/' . DIRNAME_UPDATES)) {
+	if (is_dir(DIR_BASE . '/' . DIRNAME_UPDATES . '/' . DIRNAME_APP_UPDATED)) {
 		require(DIR_BASE . '/' . DIRNAME_UPDATES . '/' . DIRNAME_APP_UPDATED . '/' . DIRNAME_APP . '/' . 'dispatcher.php');
 	} else {
 		require(DIRNAME_UPDATES . '/' . DIRNAME_APP_UPDATED . '/' . DIRNAME_APP . '/' . 'dispatcher.php');
