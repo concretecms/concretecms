4e7792e6c890aa3156f89a3f8522571cd7c9a984
Andrew Embler <andrew@concrete5.org>
2010-06-12 22:19:16 +0000

    more cache work and css tweaks
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1891 b0551a0c-1e16-4222-a7d5-975db1aca215

--- branches/development/5.4.1/concrete/libraries/model.php
+++ branches/development/5.4.1/concrete/libraries/model.php
@@ -15,7 +15,10 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 class Model extends ADOdb_Active_Record {
 
 
-
+	public function __construct() {
+	 	$db = Loader::db();
+	 	parent::__construct();
+	}		 
 
 
 }
