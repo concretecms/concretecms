a2e40b9d8d442efbcc8efbf54c84eee300beb589
Andrew Embler <andrew@concrete5.org>
2010-07-06 17:52:49 +0000

    toString in attribute reindex
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1946 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: c2f8cd295cb2a657f6e6472de4ba37e531f601f7

--- trunk/web/concrete/models/attribute/key.php
+++ trunk/web/concrete/models/attribute/key.php
@@ -304,6 +304,9 @@ class AttributeKey extends Object {
 			} else {
 				$column = 'ak_' . $akHandle;
 				if (isset($columns[strtoupper($column)])) {
+					if (is_object($value)) {
+						$value = $value->__toString();
+					}
 					$columnHeaders[$column] = $value;
 				}
 			}
