2a2a5db06fb7ce9b5a9a9ab48b24a9f08c56f67d
Andrew Embler <andrew@concrete5.org>
2010-07-07 19:06:18 +0000

    updating germany state provinces
    git-svn-id: http://svn.concrete5.org/svn/concrete5@1951 b0551a0c-1e16-4222-a7d5-975db1aca215
    
    Former-commit-id: bbb00e9ee6d01877b8230f3591d17335c20dfdf1

--- trunk/web/concrete/helpers/lists/states_provinces.php
+++ trunk/web/concrete/helpers/lists/states_provinces.php
@@ -101,6 +101,25 @@ class ListsStatesProvincesHelper {
 		'VIC' => 'Victoria',
 		'WA' => 'Western Australia',
 	),
+	
+	'DE' => array(
+      'BW' => 'Baden-W&uuml;rttemberg',
+      'BY' => 'Bayern',
+      'BE' => 'Berlin',
+      'BB' => 'Brandenburg',
+      'HB' => 'Bremen',
+      'HH' => 'Hamburg',
+      'HE' => 'Hessen',
+      'MV' => 'Mecklenburg-Vorpommern',
+      'NI' => 'Niedersachsen',
+      'NW' => 'Nordrhein-Westfalen',
+      'RP' => 'Rheinland-Pfalz',
+      'SL' => 'Saarland',
+      'SN' => 'Sachsen',
+      'ST' => 'Sachsen-Anhalt',
+      'SH' => 'Schleswig-Holstein',
+      'TH' => 'Th&uuml;ringen'   
+   ),
 
 	'UK' => array(
 		'ANGLES' => 'Anglesey',
