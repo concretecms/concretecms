c2eff81a72f76ca4c1fb8d457fcc757b5df6e16f
Ryan Tyler <ryan@concrete5.org>
2010-09-21 17:42:10 +0000

    removed page heading block, coded heading into blog_entry page type
    git-svn-id: http://svn.concrete5.org/svn/concrete5@2043 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/page_types/blog_entry.php
+++ trunk/web/concrete/page_types/blog_entry.php
@@ -3,6 +3,10 @@
 	<?php $ai = new Area('Blog Post Header'); $ai->display($c); ?>
 </div>
 <div class="pageSection">
+	<h1><?php echo $c->getCollectionName(); ?></h1>
+	<p class="meta"><?php echo t('Posted by')?> <?php echo $c->getVersionObject()->getVersionAuthorUserName(); ?> on <?php echo $c->getCollectionDatePublic('F j, Y'); ?></p>		
+</div>
+<div class="pageSection">
 	<?php $as = new Area('Blog Summary'); $as->display($c); ?>
 </div>
 <div class="pageSection">
