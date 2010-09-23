619551bb7aeff4f1097442239b4a90710bf7ea5e
Andrew Embler <andrew@concrete5.org>
2010-09-23 19:02:07 +0000

    tweaks to blog templateS
    git-svn-id: http://svn.concrete5.org/svn/concrete5@2054 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/page_types/blog_entry.php
+++ trunk/web/concrete/page_types/blog_entry.php
@@ -7,8 +7,11 @@
 	<p class="meta"><?php echo t('Posted by')?> <?php echo $c->getVersionObject()->getVersionAuthorUserName(); ?> on <?php echo $c->getCollectionDatePublic('F j, Y'); ?></p>		
 </div>
 <div class="pageSection">
-	<?php $as = new Area('Blog Summary'); $as->display($c); ?>
+	<?php $as = new Area('Main'); $as->display($c); ?>
 </div>
 <div class="pageSection">
-	<?php $a = new Area('Main'); $a->display($c); ?>
+	<?php $a = new Area('Blog Post More'); $a->display($c); ?>
+</div>
+<div class="pageSection">
+	<?php $ai = new Area('Blog Post Footer'); $ai->display($c); ?>
 </div>
