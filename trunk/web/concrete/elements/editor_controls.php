7cc295b619a62bd0d633c9f30bc0cc57a30310b8
Andrew Embler <andrew@concrete5.org>
2010-09-30 21:51:53 +0000

    http://www.concrete5.org/developers/beta/beta-bugs/5-4-1-development/tinymce-editor-bugs-for-textarea-custom-attribute-with-richtext-/
    git-svn-id: http://svn.concrete5.org/svn/concrete5@2076 b0551a0c-1e16-4222-a7d5-975db1aca215

--- trunk/web/concrete/elements/editor_controls.php
+++ trunk/web/concrete/elements/editor_controls.php
@@ -5,7 +5,7 @@
 <li ccm-file-manager-field="rich-text-editor-image"><a class="ccm-file-manager-launch" onclick="ccm_editorCurrentAuxTool='image'; setBookMark();return false;" href="#"><?=t('Add Image')?></a></li>
 <li><a class="ccm-file-manager-launch" onclick="ccm_editorCurrentAuxTool='file'; setBookMark();return false;" href="#"><?=t('Add File')?></a></li>
 <?php if (isset($mode) && $mode == 'full') {?>
-<li><a href="#" onclick="setBookMark();ccmEditorSitemapOverlay();" class="dialog-launch" dialog-modal="false" ><?=t('Insert Link to Page')?></a></li>
+<li><a href="#" onclick="setBookMark();ccmEditorSitemapOverlay();"><?=t('Insert Link to Page')?></a></li>
 <?php } else {?>
 <li><a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/sitemap_overlay.php?sitemap_mode=select_page" onclick="setBookMark();" class="dialog-launch" dialog-modal="false" ><?=t('Insert Link to Page')?></a></li>
 <?php } ?>
