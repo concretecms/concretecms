8dd43dc084ea7dffd07104016a69ca7ecd8d8916
Mnkras <mnkras@gmail.com>
2011-03-31 05:12:38 +0800

    New Task Permission to delete Users
    Former-commit-id: 9a50f8ce0e8e33a6c12d363b1a58b8b65b0f24a5

--- web/concrete/helpers/concrete/upgrade/version_542.php
+++ web/concrete/helpers/concrete/upgrade/version_542.php
@@ -20,6 +20,18 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 class ConcreteUpgradeVersion542Helper {
 
+	public function run() {
+		$db = Loader::db();
+		$cnt = $db->GetOne('select count(*) from TaskPermissions where tpHandle = ?', array('delete_user'));
+		if ($cnt < 1) {
+			$g3 = Group::getByID(ADMIN_GROUP_ID);
+			$tip = TaskPermission::addTask('delete_user', t('Delete Users'), false);
+			if (is_object($g3)) {
+				$tip->addAccess($g3);
+			}
+		}
+	}
+	
 	public function prepare() {
 		// we install the updated schema just for tables that matter
 		Package::installDB(dirname(__FILE__) . '/db/version_542.xml');
