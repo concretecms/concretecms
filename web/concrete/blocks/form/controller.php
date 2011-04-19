61511c903e13f7e296f5b5ea1b0eb8b0fb2a4c12
jack <jack.lightbody@gmail.com>
2011-04-18 18:18:19 -0700

    commit d9ba6d19a45cb79fe7c7 [formerly 5cd87161376dd8e468aa] added to form controller
    Former-commit-id: 0de1cdf0a458dfcb0dad6c7737b09ed199ca6749

--- web/concrete/blocks/form/controller.php
+++ web/concrete/blocks/form/controller.php
@@ -714,7 +714,7 @@ class MiniSurvey{
 					return $html;
 					
 				case 'text':
-					$val=($_REQUEST['Question'.$msqID])?$_REQUEST['Question'.$msqID]:'';
+					$val=($_REQUEST['Question'.$msqID])?Loader::helper('text')->entities($_REQUEST['Question'.$msqID]):'';
 					return '<textarea name="Question'.$msqID.'" cols="'.$questionData['width'].'" rows="'.$questionData['height'].'" style="width:95%">'.$val.'</textarea>';
 				case 'url':
 					$val=($_REQUEST['Question'.$msqID])?$_REQUEST['Question'.$msqID]:'';
