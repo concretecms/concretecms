8e0b2bf915d8bf84c9969be736341109eafe6388
Ryan Tyler <ryan@concrete5.org>
2011-05-02 13:42:39 -0700

    added the new thumbnail picker + integrated it into the avatar edit page

--- web/concrete/single_pages/profile/avatar.php
+++ web/concrete/single_pages/profile/avatar.php
@@ -25,7 +25,23 @@
            	ThumbnailBuilder_onSaveCompleted = function() { 
 				alert("<?php echo t('User Profile picture saved.')?>");
 				window.location.href="<?php echo $this->url('/profile/avatar')?>";
-			}
+			};
+
+           	/* <?php /* flashvars - options for the avatar/thumb picker  
+			upload -- whether to enable or disable the upload button (default is "true")
+			webcam -- whether to enable or disable the webcam button (default is "true")
+			format -- whether to use "jpg", "png" or "auto" when encoding (default is "auto")
+			imagePath -- which image should be preloaded into the editor
+			overlayPath -- an optional image to provide chrome over the top of thumbnails
+			redirectPath -- an optional path to redirect to once an image has been saved
+			savePath -- an optional path for saving images ($_POST["thumbnail"], base64) instead of saving locally
+			rounding -- an optional amount of pixel rounding on thumbnail edges (will output transparent corners)
+			width -- the width of the thumbnail
+			quality -- the quality of the output image when using the JPEG encoder (default is 80)
+			height -- the height of the thumbnail
+			backgroundColor -- the color to use when tinting the background of the editor (default is 0xFFFFFF)
+			tint -- the amount of strength to apply when tinting the background of the editor (default is 0)
+			*/ ?> */
 			
             $(function(){   
                 var params = { 
@@ -37,9 +53,9 @@
                     width: '<?php echo AVATAR_WIDTH?>',
                     height: '<?php echo AVATAR_HEIGHT?>',
                     image: '<?php echo $av->getImagePath($ui)?>',
-                    save: "<?php echo $this->url($c->getCollectionPath(), 'save_thumb')?>"
+                    save: "<?php echo $this->url($c->getCollectionPath(), 'save_thumb')?>"    
                 };
-				swfobject.embedSWF ("<?php echo ASSETS_URL_FLASH?>/thumbnail_editor_2.swf", "profile-avatar", "500", "400", "10,0,0,0", "includes/expressInstall.swf", flashvars, params);
+				swfobject.embedSWF ("<?php echo ASSETS_URL_FLASH?>/thumbnail_editor_3.swf", "profile-avatar", "500", "400", "10,0,0,0", "includes/expressInstall.swf", flashvars, params);
         
            		
            });
