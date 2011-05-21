a3d37f81f3945c240d6f0732aab6eb9ca38b52e7
Ryan Tyler <ryan@concrete5.org>
2011-05-20 17:26:15 -0700

    Added view all tags option to tag block + comments to file version
    Former-commit-id: de933e20a3f96330ce70014b348ad6e6fd18349d

--- web/concrete/blocks/image/edit.php
+++ web/concrete/blocks/image/edit.php
@@ -1,5 +1,4 @@
-<? 
-defined('C5_EXECUTE') or die("Access Denied.");
+<?php defined('C5_EXECUTE') or die("Access Denied.");
 $includeAssetLibrary = true; 
 $assetLibraryPassThru = array(
 	'type' => 'image'
--- web/concrete/blocks/tags/controller.php
+++ web/concrete/blocks/tags/controller.php
@@ -6,10 +6,14 @@ class TagsBlockController extends BlockController {
 	protected $btInterfaceWidth = "500";
 	protected $btInterfaceHeight = "350";
 	
-	protected $btCacheBlockOutput = false;
+	protected $btCacheBlockOutput = true;
 	protected $btCacheBlockOutputOnPost = false;
 	protected $btCacheBlockOutputForRegisteredUsers = false;
+	
 	public $attributeHandle = 'tags';
+	public $displayMode = 'page';
+	public $cloudCount = 10;
+	public $helpers =  array('navigation');	
 	
 	/** 
 	 * Used for localization. If we want to localize the name/description we have to include this
@@ -29,22 +33,52 @@ class TagsBlockController extends BlockController {
 	protected function loadAttribute() {
 		Loader::model('attribute/categories/collection');
 		$ak = CollectionAttributeKey::getByHandle($this->attributeHandle);
-		$this->set('ak',$ak);
+		return $ak;
 	}
 	
 	public function edit() { 
-		$this->loadAttribute();
+		$ak = $this->loadAttribute();
+		$this->set('ak',$ak);
 	}
 	
 	public function view() {
-		$this->loadAttribute();	
+		$ak = $this->loadAttribute();
+		if($this->displayMode == "cloud") {
+			$type = $ak->getAttributeType();
+			$controller = $type->getController();
+			$controller->setAttributeKey($ak);
+			$items = $controller->getOptions();
+			$options = new SelectAttributeTypeOptionList();
+			if($this->cloudCount > 0 && $items instanceof SelectAttributeTypeOptionList && $items->count()) {
+				$i = 1;
+				foreach($items as $item) {
+					$options->add($item); 
+					if($i >=$this->cloudCount) { break;}
+					$i++;
+				}
+			} else {
+				$options = $items;
+			}
+		} else {
+			$c = Page::getCurrentPage();
+			$av = $c->getAttributeValueObject($ak);
+			$controller = $ak->getController();
+			$options = $c->getAttribute($ak->getAttributeKeyHandle());
+		}
+		
+		if($this->targetCID > 0) {
+			$target = Page::getByID($this->targetCID);
+			$this->set('target',$target);
+		}		
+		$this->set('options',$options);
+		$this->set('akc',$controller);
+		$this->set('ak',$ak);
 	}
 	
 	public function save($args) {
-		$this->loadAttribute();
+		$ak = $this->loadAttribute();
 		$c = Page::getByID($_REQUEST['cID'], 'RECENT');
 		$nvc = $c->getVersionToModify();
-		$ak = $this->get('ak');
 		$ak->saveAttributeForm($nvc);
 		$nvc->refreshCache();
 		parent::save($args);
