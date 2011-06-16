010b7ee7b32d8c037f4a446372f5aad165cb9239
Andrew Embler <andrew@concrete5.org>
2011-06-16 16:29:54 -0700

    Youtube and Google Map edit mode tweaks; file block trim() added to validate; let having() support free-form strings

--- web/concrete/blocks/google_map/view.php
+++ web/concrete/blocks/google_map/view.php
@@ -3,7 +3,7 @@
 $c = Page::getCurrentPage();
 if ($c->isEditMode()) { ?>
 	<div class="ccm-edit-mode-disabled-item" style="height: 400px">
-		<div style="padding: 80px 0px 0px 0px"><?=t('Content disabled in edit mode.')?></div>
+		<div style="padding: 80px 0px 0px 0px"><?=t('Google Map disabled in edit mode.')?></div>
 	</div>
 <? } else { ?>
 	<style type="text/css">
