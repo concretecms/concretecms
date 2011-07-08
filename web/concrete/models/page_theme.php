340fcb2b24222739859281949013c7904e1cbb46
jack <jack.lightbody@gmail.com>
2011-07-08 13:55:55 -0700

    allowed quoted url in theme

--- web/concrete/models/page_theme.php
+++ web/concrete/models/page_theme.php
@@ -270,7 +270,9 @@ class PageTheme extends Object {
 			$contents = $fh->getContents($this->getThemeDirectory() . '/' . $file);
 			
 			// replace all url( instances with url starting with path to theme
-			$contents = str_replace('url(', 'url(' . $this->getThemeURL() . '/', $contents);
+			$contents = preg_replace('/(url\(\')(.*)/', '$1' . $this->getThemeURL() . '/$2', $contents);
+                        $contents = preg_replace('/(url\(")(.*)/', '$1' . $this->getThemeURL() . '/$2', $contents);
+                        $contents = preg_replace('/(url\((?![\'"]))(.*)/', '$1' . $this->getThemeURL() . '/$2', $contents);
 			
 			
 			// load up all tokens from the db for this stylesheet.
