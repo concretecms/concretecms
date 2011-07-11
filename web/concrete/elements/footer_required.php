b4909253afccd5d65ebc53495094aed061741c9f
Joseph Dill <jmdill@elevatestudios.com>
2011-07-10 21:56:30 -0500

    outputFooterItems in elements/footer_required
    Former-commit-id: e937258ec0b7e510ed601cba8b0a486c11ffbecd

--- web/concrete/elements/footer_required.php
+++ web/concrete/elements/footer_required.php
@@ -4,5 +4,6 @@ if (!isset($disableTrackingCode) || $disableTrackingCode == false) {
 	echo Config::get('SITE_TRACKING_CODE');
 }
 
-// not working yet
-// print $this->outputFooterItems();
+print $this->controller->outputFooterItems();
+
+?>
