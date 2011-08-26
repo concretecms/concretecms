111c14bc754609e3a3c68f0d638fa4771bcd4e1b
Andrew Embler <andrew@concrete5.org>
2011-08-26 07:59:46 -0700

    Fixing regressions in youtube video block caused by update

--- web/concrete/blocks/youtube/view.php
+++ web/concrete/blocks/youtube/view.php
@@ -14,7 +14,7 @@ if (!$vHeight) {
 
 if ($c->isEditMode()) { ?>
 	<div class="ccm-edit-mode-disabled-item" style="width:<?php echo $vWidth; ?>px; height:<?php echo $vHeight; ?>px;">
-		<div style="padding:8px 0px; padding-top: <?php echo round($vHeight/2)-10; ?>px;"><?php echo t('Content disabled in edit mode.'); ?></div>
+		<div style="padding:8px 0px; padding-top: <?php echo round($vHeight/2)-10; ?>px;"><?php echo t('YouTube Video disabled in edit mode.'); ?></div>
 	</div>
 <? } elseif ($vPlayer==1) { ?>
 	
@@ -36,7 +36,7 @@ if ($c->isEditMode()) { ?>
 		<script type="text/javascript">
 		//<![CDATA[
 		params = {
-			wmode:  "transparent",
+			wmode:  "transparent"
 		};
 		flashvars = {};
 		swfobject.embedSWF('http://www.youtube.com/v<?=$url['path']?>&amp;hl=en', 'youtube<?php echo $bID?>', '<?php echo $vWidth; ?>', '<?php echo $vHeight; ?>', '8.0.0', false, flashvars, params);
@@ -46,7 +46,7 @@ if ($c->isEditMode()) { ?>
 		<script type="text/javascript">
 		//<![CDATA[
 		params = {
-			wmode:  "transparent",
+			wmode:  "transparent"
 		};
 		flashvars = {};
 		swfobject.embedSWF('http://www.youtube.com/v/<?=$query['v']?>&amp;hl=en', 'youtube<?php echo $bID?>', '<?php echo $vWidth; ?>', '<?php echo $vHeight; ?>', '8.0.0', false, flashvars, params);
