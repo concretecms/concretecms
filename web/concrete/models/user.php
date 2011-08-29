7b04705400cd2294ad6cf813fb021fcda6844f83
Andrew Embler <andrew@concrete5.org>
2011-08-29 14:08:36 -0700

    more work on session regenerate id for IE
    Former-commit-id: ded21a6d730a15e358b5a580cc86af29d29f45d8

--- web/concrete/models/user.php
+++ web/concrete/models/user.php
@@ -66,6 +66,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 		protected static function regenerateSession() {
 			$tmpSession = $_SESSION; 
 			session_write_close(); 
+			setcookie(session_name(), session_id(), time()-100000);
 			session_id(sha1(mt_rand())); 
 			session_start(); 
 			$_SESSION = $tmpSession; 
