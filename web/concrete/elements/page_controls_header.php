6c1a059a852f8dd87079c3ed0e2f5e79097a05af
Andrew Embler <andrew@concrete5.org>
2011-10-06 11:39:07 -0700

    jQuery UI datepicker uses LANGUAGE instead of ACTIVE_LOCALE
    Former-commit-id: f1cd07ac24da628ca75e7c77182b865ac2061ba2

--- web/concrete/elements/page_controls_header.php
+++ web/concrete/elements/page_controls_header.php
@@ -38,7 +38,7 @@ $this->addHeaderItem($html->javascript('jquery.rating.js'));
 $this->addHeaderItem($html->javascript('jquery.colorpicker.js'));
 	
 if (ACTIVE_LOCALE != 'en_US') {
-	$dlocale = str_replace('_', '-', ACTIVE_LOCALE);
+	$dlocale = str_replace('_', '-', LANGUAGE);
 	$this->addHeaderItem($html->javascript('i18n/ui.datepicker-' . $dlocale . '.js'));
 	$this->addHeaderItem('<script type="text/javascript">$(function() { jQuery.datepicker.setDefaults({dateFormat: \'yy-mm-dd\'}); });</script>');
 }
