bd2cd9c6dd3d1c516bb4bf18d62cc0e3f003cc8c
Jeramy Spurgeon <jedspurg@iupui.edu>
2011-10-31 03:48:01 +0800

    Added debug controller page / updated single page
    Former-commit-id: d853bab52928d62dcf320a93048ca7f702138d68

--- web/concrete/single_pages/dashboard/system/developer/debug.php
+++ web/concrete/single_pages/dashboard/system/developer/debug.php
@@ -1,1 +1,50 @@
 <? defined('C5_EXECUTE') or die("Access Denied.");?>
+
+<?php echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Debug Level'), false, 'span12 offset2', false)?>
+
+<form method="post" id="debug-form" action="<?php echo $this->url('/dashboard/system/developer/debug', 'update_debug')?>">
+	<div class="ccm-pane-body">
+	<?php echo $this->controller->token->output('update_debug')?>
+	
+	
+	<span class="label notice"><?php echo t('Note: these are global settings. If enabled, PHP errors will be displayed to all visitors of the site.')?></span>
+    
+    <div class="clearfix"></div>
+    
+    <div class="input">
+    
+    <ul class="inputs-list">
+    <li>
+    <label>
+	<input type="radio" name="debug_level" value="<?php echo DEBUG_DISPLAY_PRODUCTION?>" <?php  if ($debug_level == DEBUG_DISPLAY_PRODUCTION) { ?> checked <?php  } ?> /> <?php echo t('Production')?> 
+    </label>
+    <span class="help-block">
+       <?php echo t('PHP errors and database exceptions will be suppressed.')?>
+    </span>
+    </li>
+    
+    
+              
+	<li>
+    <label>
+	<input type="radio" name="debug_level" value="<?php echo DEBUG_DISPLAY_ERRORS?>" <?php  if ($debug_level == DEBUG_DISPLAY_ERRORS) { ?> checked <?php  } ?> /> <?php echo t('Development')?> 
+    </label>
+    <span class="help-block">
+       <?php echo t('PHP errors and database exceptions will be displayed.')?>
+    </span>
+    </li>
+
+</div>
+
+<div class="clearfix"></div>
+
+    <div class="ccm-pane-footer">
+        <?
+        print $interface->submit(t('Set Debug Level'), 'debug-form', 'left','primary');
+        ?>
+    
+    </div>
+	
+
+</form>
+<?php echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false);?> 
