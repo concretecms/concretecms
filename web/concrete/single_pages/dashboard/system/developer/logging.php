02671f853764e97ad772c2b5a663a4f06651f1aa
Andrew Embler <andrew@concrete5.org>
2011-10-31 13:29:28 -0700

    updating .form-stacked class to work better with dashboard panes; tweaks to logging

--- web/concrete/single_pages/dashboard/system/developer/logging.php
+++ web/concrete/single_pages/dashboard/system/developer/logging.php
@@ -2,28 +2,33 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $dh = Loader::helper('concrete/dashboard');
 
-echo $dh->getDashboardPaneHeaderWrapper(t('Logging'), t('Enables saving records of emails being sent out. This will save records even if actual email delivery is disabled on your site.'), 'span6 offset4', false);
+echo $dh->getDashboardPaneHeaderWrapper(t('Logging'), t('Enables saving records of emails being sent out. This will save records even if actual email delivery is disabled on your site.'), 'span10 offset3', false);
 ?>
 
-<form method="post" action="<?php echo $this->action('update_logging'); ?>">
+<form method="post" class="form-stacked" action="<?php echo $this->action('update_logging'); ?>">
 	<div class="ccm-pane-body">	
 		<?php echo $this->controller->token->output('update_logging'); ?>
 		
 		<div class="clearfix">
+			<div class="input">
+			<ul class="inputs-list">
+			<li>
 			<label for="ENABLE_LOG_ERRORS">
 				<?php echo $fh->checkbox('ENABLE_LOG_ERRORS', 1, $intLogErrors) ?>
 			
 				<span><?php echo t('Log Application Exceptions'); ?></span>
-			</label>		
-		</div>
-		
-		<div class="clearfix">
-			<label for="ENABLE_LOG_EMAILS">
+			</label>	
+			</li>
+			<li><label for="ENABLE_LOG_EMAILS">
 				<?php echo $fh->checkbox('ENABLE_LOG_EMAILS', 1, $intLogEmails) ?>
 			
 				<span><?php echo t('Log Emails Sent'); ?></span>
-			</label>		
+			</label>
+			</li>
+			</ul>
+			</div>
 		</div>		
+
 	</div>
 
 	<div class="ccm-pane-footer">	
