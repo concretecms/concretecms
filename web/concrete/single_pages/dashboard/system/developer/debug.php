7155113e3721ea598fb928961bd02941b1f7f7f5
Andrew Embler <andrew@concrete5.org>
2011-10-31 10:49:32 -0700

    Updating debug settings to fix some mis-nested DIVs, random stuff

--- web/concrete/single_pages/dashboard/system/developer/debug.php
+++ web/concrete/single_pages/dashboard/system/developer/debug.php
@@ -3,14 +3,14 @@
 <?php echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Debug Level'), false, 'span12 offset2', false)?>
 
 <form method="post" id="debug-form" action="<?php echo $this->url('/dashboard/system/developer/debug', 'update_debug')?>">
-	<div class="ccm-pane-body">
+<div class="ccm-pane-body">
 	<?php echo $this->controller->token->output('update_debug')?>
 	
 	
-	<span class="label notice"><?php echo t('Note: these are global settings. If enabled, PHP errors will be displayed to all visitors of the site.')?></span>
+	<div class="block-message alert-message info"><?php echo t('Note: these are global settings. If enabled, PHP errors will be displayed to all visitors of the site.')?></div>
     
-    <div class="clearfix"></div>
-    
+	<div class="clearfix">
+	
     <div class="input">
     
     <ul class="inputs-list">
@@ -22,9 +22,6 @@
        <?php echo t('PHP errors and database exceptions will be suppressed.')?>
     </span>
     </li>
-    
-    
-              
 	<li>
     <label>
 	<input type="radio" name="debug_level" value="<?php echo DEBUG_DISPLAY_ERRORS?>" <?php  if ($debug_level == DEBUG_DISPLAY_ERRORS) { ?> checked <?php  } ?> /> <?php echo t('Development')?> 
@@ -33,18 +30,18 @@
        <?php echo t('PHP errors and database exceptions will be displayed.')?>
     </span>
     </li>
-
+    </ul>
+    
+	</div>
+	</div>
+	
 </div>
 
-<div class="clearfix"></div>
+<div class="ccm-pane-footer">
+	<?
+	print $interface->submit(t('Set Debug Level'), 'debug-form', 'left','primary');
+	?>
 
-    <div class="ccm-pane-footer">
-        <?
-        print $interface->submit(t('Set Debug Level'), 'debug-form', 'left','primary');
-        ?>
-    
-    </div>
-	
-
+</div>
 </form>
 <?php echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false);?> 
