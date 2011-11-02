c1c7f7b3e754857631ffed50ab81b81b5e645c32
Andrew Embler <andrew@concrete5.org>
2011-11-02 14:33:04 -0700

    updating twitter bootstrap forms on timezone

--- web/concrete/single_pages/dashboard/system/appearance/timezone.php
+++ web/concrete/single_pages/dashboard/system/appearance/timezone.php
@@ -5,9 +5,15 @@ print $d->getDashboardPaneHeaderWrapper(t('Timezone'), false, 'span8 offset4', f
 <form method="post" id="user-timezone-form" action="<?php echo $this->action('update') ?>">
      <?php echo $this->controller->token->output('update_timezone')?>
      <div class="ccm-pane-body">
-          <div class="ccm-dashboard-checkbox"><input type="checkbox" name="user_timezones" value="1" style="vertical-align: middle" <?php if ($user_timezones) { ?> checked <?php } ?> /> <?php echo t('Enable user defined time zones.') ?></div>
-          <div class="ccm-dashboard-description"><?php echo t('Allows site members to display date/time information in their time zone.') ?></div>
+     <label><?=t('Time Zone')?></label>
+     <div class="input">
+     <ul class="inputs-list">
+     <li><label><input type="checkbox" name="user_timezones" value="1" <?php if ($user_timezones) { ?> checked <?php } ?> /> <span><?php echo t('Enable user defined time zones.') ?></span></label></li>
+     </ul>
+    <div class="help-block"><p><?php echo t('Allows site members to display date/time information in their time zone.') ?></p></div>
      </div>
+	 </div>
+     
      <div class="ccm-pane-footer">
           <?
           print $interface->submit(t('Save'), 'user-timezone-form', 'left', 'primary');
