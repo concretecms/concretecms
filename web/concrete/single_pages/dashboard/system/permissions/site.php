731c5d3b1180d868709a114c1b5a7552d6d078f8
Christiaan <anotherhero@gmail.com>
2011-11-02 15:15:23 +0100

    Use Twitter Bootstrap form styling
    Former-commit-id: 2dc48d1850f5503953d725c0e3937b8f7ade8e48

--- web/concrete/single_pages/dashboard/system/permissions/site.php
+++ web/concrete/single_pages/dashboard/system/permissions/site.php
@@ -1,1 +1,69 @@
-<? defined('C5_EXECUTE') or die("Access Denied.");
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+/* @var $h ConcreteDashboardHelper */
+$h = Loader::helper('concrete/dashboard');
+/* @var $ih ConcreteInterfaceHelper */
+$ih = Loader::helper('concrete/interface');
+/* @var $form FormHelper */
+$form = Loader::helper('form');
+?>
+<?=$h->getDashboardPaneHeaderWrapper(t('Site Permissions'), false, false, false);?>
+<form id="site-permissions-form" action="<?=$this->action('save')?>" method="post">
+<div class="ccm-pane-body">
+	<?=$this->controller->token->output('site_permissions_code')?>
+
+<?if(PERMISSIONS_MODEL != 'simple'):?>
+<p>
+<?=t('Your Concrete site does not use the simple permissions model. You must change your permissions for each specific page and content area.')?>
+</p>
+<?else:?>
+	<div class="clearfix">
+		<?=$form->label('view', t('Viewing Permissions'))?>
+		<div class="input">
+			<ul class="inputs-list">
+				<li>
+					<label>
+						<?=$form->radio('view', 'ANYONE', $guestCanRead)?>
+						<span><?=t('Public')?> - <?=t('Anyone may view the website.')?></span>
+					</label>
+				</li>
+				<li>
+					<label>
+						<?=$form->radio('view', 'USERS', $registeredCanRead)?>
+						<span><?=t('Members')?> - <?=t('Only registered users may view the website.')?></span>
+					</label>
+				</li>
+				<li>
+					<label>
+						<?=$form->radio('view', 'PRIVATE', !$guestCanRead && !$registeredCanRead)?>
+						<span><?=t('Private')?> - <?=t('Only the administrative group may view the website.')?></span>
+					</label>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="clearfix">
+		<?=$form->label('gID', t('Edit Access'))?>
+		<div class="input">
+			<ul class="inputs-list">
+				<?foreach($gArray as $g):?>
+				<li>
+					<label>
+						<?=$form->checkbox('gID[]', $g->getGroupID(), $g->canWrite())?>
+						<span><?=$g->getGroupName()?></span>
+					</label>
+				</li>
+				<?endforeach?>
+			</ul>
+			<span class="help-block"><?=t('Choose which users and groups may edit your site. Note: These settings can be overridden on specific pages.')?></span>
+		</div>
+	</div>
+<?endif?>
+</div>
+<div class="ccm-pane-footer">
+<?
+	$submit = $ih->submit( t('Save'), 'site-permissions-form', 'right', 'primary');
+	print $submit;
+?>
+</div>
+</form>
+<?=$h->getDashboardPaneFooterWrapper(false);?>
