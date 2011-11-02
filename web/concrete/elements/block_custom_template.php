a9359d6e1cc0a62b3bca7ab6929b3ad96bb5e27b
Ant Rodgers <arcanepain@gmail.com>
2011-11-02 17:07:48 +0000

    Minor Bootstrap-y Custom Block Template dialogue styling
    Little things...use 'em if you like! Looks a little prettier, used some Bootstrap styles (stacked form, <div class='clearfix'><label></label><div><select /></div></div>), added a little inline padding to the .ccm-ui wrapper. Can't say I like how the Bootstrap styles are out-denting (if that's a word) in the body area versus the dialogue header/titles (looks like 13px left padding, versus 20px for the header in this case), so went with the 20px indenting of the form-stacked class instead.
    
    
    Former-commit-id: 0f544d2516669bc38439ba20525b61b9988f8f92

--- web/concrete/elements/block_custom_template.php
+++ web/concrete/elements/block_custom_template.php
@@ -12,10 +12,8 @@ if ($b->getBlockTypeHandle() == BLOCK_HANDLE_SCRAPBOOK_PROXY) {
 $templates = $bt->getBlockTypeCustomTemplates();
 $txt = Loader::helper('text');
 ?>
-<div class="ccm-ui">
-<form method="post" id="ccmCustomTemplateForm" action="<?=$b->getBlockUpdateInformationAction()?>&amp;rcID=<?=intval($rcID) ?>">
-	
-	<strong><?=t('Custom Template')?></strong>:<br/>
+<div class="ccm-ui" style="padding-top:10px;">
+<form method="post" id="ccmCustomTemplateForm" action="<?=$b->getBlockUpdateInformationAction()?>&amp;rcID=<?=intval($rcID) ?>" class="form-stacked clearfix">
 	
 	<? if (count($templates) == 0) { ?>
 	
@@ -25,19 +23,25 @@ $txt = Loader::helper('text');
 		</div>
 
 	<? } else { ?>
-	
-		<select name="bFilename">
-			<option value="">(<?=t('None selected')?>)</option>
-			<? foreach($templates as $tpl) { ?>
-				<option value="<?=$tpl?>" <? if ($b->getBlockFilename() == $tpl) { ?> selected <? } ?>><?	
-					if (strpos($tpl, '.') !== false) {
-						print substr($txt->unhandle($tpl), 0, strrpos($tpl, '.'));
-					} else {
-						print $txt->unhandle($tpl);
-					}
-					?></option>		
-			<? } ?>
-		</select>
+		
+    	<div class="clearfix">
+            <label for="bFilename"><?=t('Custom Template')?></label>
+            <div class="input">
+                <select id="bFilename" name="bFilename" class="xlarge">
+                    <option value="">(<?=t('None selected')?>)</option>
+                    <? foreach($templates as $tpl) { ?>
+                        <option value="<?=$tpl?>" <? if ($b->getBlockFilename() == $tpl) { ?> selected <? } ?>><?	
+                            if (strpos($tpl, '.') !== false) {
+                                print substr($txt->unhandle($tpl), 0, strrpos($tpl, '.'));
+                            } else {
+                                print $txt->unhandle($tpl);
+                            }
+                            ?></option>		
+                    <? } ?>
+                </select>
+            </div>
+        </div>
+        
 		<div class="ccm-buttons dialog-buttons">
 			<a href="#" class="btn ccm-dialog-close ccm-button-left cancel"><span><em class="ccm-button-close"><?=t('Cancel')?></em></span></a>
 			<a href="javascript:void(0)" onclick="$('#ccmCustomTemplateForm').submit()" class="ccm-button-right accept primary btn"><span><?=t('Update')?></span></a>
