d0bc194506c430f4a10f99e417ecd5d8e790caaa
Ant Rodgers <arcanepain@gmail.com>
2011-11-02 00:31:31 +0000

    Creation of separate Add Page Type page/dialogue for Dashboard
    Pretty much got this! Addition of new 'add.php' single page + new controller for it. Add page stuff cut out of the original view.php, and dashboard.xml in install > base (hopefully) correctly updated for the new page. Only issue -- errors don't seem to be rendering! Can't get my head around why this is as $error looks like it's being populated correctly.

--- web/concrete/single_pages/dashboard/pages/types/view.php
+++ web/concrete/single_pages/dashboard/pages/types/view.php
@@ -4,6 +4,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $ih = Loader::helper('concrete/interface');
 $cap = Loader::helper('concrete/dashboard');
 $valt = Loader::helper('validation/token');
+$valc = Loader::helper('concrete/validation');
 $form = Loader::helper('form');
 $ctArray = CollectionType::getList();
 $args['section'] = 'collection_types';
@@ -37,7 +38,7 @@ if ($_REQUEST['task'] == 'edit') {
 	}
 }
 
-if ($_POST['task'] == 'add' || $_POST['update']) {
+if ($_POST['update']) {
 	$ctName = $_POST['ctName'];
 	$ctHandle = $_POST['ctHandle'];
 	
@@ -49,7 +50,7 @@ if ($_POST['task'] == 'add' || $_POST['update']) {
 		$error[] = t("Name required.");
 	}
 	
-	if (!$valt->validate('add_or_update_page_type')) {
+	if (!$valt->validate('update_page_type')) {
 		$error[] = $valt->getErrorMessage();
 	}
 	
@@ -60,10 +61,7 @@ if ($_POST['task'] == 'add' || $_POST['update']) {
 	
 	if (count($error) == 0) {
 		try {
-			if ($_POST['task'] == 'add') {
-				$nCT = CollectionType::add($_POST);
-				$this->controller->redirect('/dashboard/pages/types?created=1');
-			} else if (is_object($ct)) {
+			if (is_object($ct)) {
 				$ct->update($_POST);
 				$this->controller->redirect('/dashboard/pages/types?updated=1');
 			}		
@@ -74,9 +72,7 @@ if ($_POST['task'] == 'add' || $_POST['update']) {
 	}
 }
 
-if ($_REQUEST['created']) { 
-	$message = t('Page Type added.');
-} else if ($_REQUEST['updated']) {
+if ($_REQUEST['updated']) {
 	$message = t('Page Type updated.');
 }
 
@@ -91,7 +87,7 @@ if ($ctEditMode) {
     <?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Edit Page Type').'<span class="label" style="position:relative;top:-3px;left:12px;">'.t('* required field').'</span>', false, false, false);?>
     
     <form method="post" id="update_page_type" action="<?=$this->url('/dashboard/pages/types/')?>">
-	<?=$valt->output('add_or_update_page_type')?>
+	<?=$valt->output('update_page_type')?>
     <?=$form->hidden('ctID', $_REQUEST['ctID']); ?>
     <?=$form->hidden('task', 'edit'); ?>
     <?=$form->hidden('update', '1'); ?>
@@ -259,160 +255,6 @@ if ($ctEditMode) {
     
     <!-- END: Delete Page Type pane -->
     
-<? 
-} else if ($_REQUEST['task'] == 'add') {  ?>
-	
-    <!-- START: Add Page Type pane -->
-    
-    <?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Add Page Type').'<span class="label" style="position:relative;top:-3px;left:12px;">'.t('* required field').'</span>', false, false, false);?>
-	
-    <form method="post" id="add_page_type" action="<?=$this->url('/dashboard/pages/types/')?>">
-	<?=$valt->output('add_or_update_page_type')?>
-    <?=$form->hidden('task', 'add'); ?>
-	
-    <div class="ccm-pane-body">
-    
-        <table border="0" cellspacing="0" cellpadding="0">
-            <thead>
-                <tr>
-                    <th class="header"><?=t('Name')?> <span class="required">*</span></th>
-                    <th class="header"><?=t('Handle')?> <span class="required">*</span></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <?=$form->text('ctName', $_POST['ctName'], array('class' => 'span9'))?>
-                    </td>
-                    <td>
-                        <?=$form->text('ctHandle', $_POST['ctHandle'], array('class' => 'span6'))?>
-                    </td>
-                </tr>
-			</tbody>
-		</table>
-        
-        <table border="0" cellspacing="0" cellpadding="0">
-            <thead>
-                <tr>
-                    <th class="subheader">
-					
-					<?=t('Icon')?>
-                    <?
-                        if (!is_object($pageTypeIconsFS)) {
-                            print '<span style="margin-left: 4px; color: #aaa">';
-                            print t('(To add your own page type icons, create a file set named "%s" and add files to that set)', t('Page Type Icons'));
-                            print '</span>';
-                        } else {
-                            print '<span style="margin-left: 4px; color: #aaa">';
-                            print t('(Pulling icons from file set "%s". Icons will be displayed at %s x %s.)', t('Page Type Icons'), COLLECTION_TYPE_ICON_WIDTH, COLLECTION_TYPE_ICON_HEIGHT);
-                            print '</span>';
-                        }
-                    ?>
-            
-                    </th>
-                </tr>
-			</thead>
-            <tbody>
-                <tr>
-                    <td>
-                    <? 
-                    $first = true;
-                    foreach($icons as $ic) { 
-                        if(is_object($ic)) {
-                            $fv = $ic->getApprovedVersion(); 
-                            $checked = false;
-                            if (isset($_POST['ctIcon']) && $_POST['ctIcon'] == $ic->getFileID()) {
-                                $checked = 'checked';
-                            } else {
-                                if ($first) { 
-                                    $checked = 'checked';
-                                }
-                            }
-                            $first = false;
-                            ?>
-                            <label style="white-space: nowrap; margin: 10px 20px 10px 0; float:left;">
-                            <input type="radio" name="ctIcon" value="<?= $ic->getFileID() ?>" style="vertical-align: middle" <?=$checked?> />
-                            <img src="<?= $fv->getRelativePath(); ?>" width="<?=COLLECTION_TYPE_ICON_WIDTH?>" height="<?=COLLECTION_TYPE_ICON_HEIGHT?>" style="vertical-align: middle" />
-                            </label>
-                        <? 
-                        } else {
-                            $checked = false;
-                            if (isset($_POST['ctIcon']) && $_POST['ctIcon'] == $ic) {
-                                $checked = 'checked';
-                            } else {
-                                if ($first) { 
-                                    $checked = 'checked';
-                                }
-                            }
-                            $first = false;
-                            ?>
-                            <label style="white-space: nowrap; margin: 10px 20px 10px 0; float:left;">
-                            <input type="radio" name="ctIcon" value="<?= $ic ?>" style="vertical-align: middle" <?=$checked?> />
-                                <img src="<?=REL_DIR_FILES_COLLECTION_TYPE_ICONS.'/'.$ic;?>" width="<?=COLLECTION_TYPE_ICON_WIDTH?>" height="<?=COLLECTION_TYPE_ICON_HEIGHT?>" style="vertical-align: middle" />
-                            </label>
-                        <?
-                        }
-                    
-                    } ?>
-                    </td>
-                </tr>
-			</tbody>
-		</table>
-        
-        <table border="0" cellspacing="0" cellpadding="0">
-            <thead>
-                <tr>
-                    <th colspan="3" class="subheader"><?=t('Default Attributes to Display')?></th>
-                </tr>
-			</thead>
-            <tbody>
-                <?
-                    $attribs = CollectionAttributeKey::getList();
-                    $i = 0;
-                    foreach($attribs as $ak) { 
-                    if ($i == 0) { ?>
-                        <tr class="inputs-list">
-                    <? } ?>
-                    
-                        <td width="33%">
-                            <label>
-                                <input type="checkbox" name="akID[]" value="<?=$ak->getAttributeKeyID()?>" />
-                                <span><?=$ak->getAttributeKeyName()?></span>
-                            </label>
-                        </td>
-                    
-                    <? $i++;
-                    
-                    if ($i == 3) { ?>
-                    </tr>
-                    <? 
-                    $i = 0;
-                    }
-                    
-                }
-                
-                if ($i < 3 && $i > 0) {
-                    for ($j = $i; $j < 3; $j++) { ?>
-                        <td>&nbsp;</td>
-                    <? }
-                ?></tr>
-        	<? } ?>
-        	</tbody>
-        </table>
-	
-	</div>
-    
-    <div class="ccm-pane-footer">
-        <? print $ih->submit(t('Add Page Type'), 'add_page_type', 'right', 'primary'); ?>
-        <? print $ih->button(t('Cancel'), $this->url('/dashboard/pages/types'), 'left'); ?>
-    </div>
-    
-    </form>
-    
-    <?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false)?>
-    
-    <!-- END Add Page Type pane -->
-
 <? } else { ?>
 	
     <!-- START: Default Page Types pane -->
@@ -479,7 +321,7 @@ if ($ctEditMode) {
     </div>
     
     <div class="ccm-pane-footer">
-        <? print $ih->button(t('Add a Page Type'), $this->url('/dashboard/pages/types?task=add'), 'left', 'primary'); ?>
+        <? print $ih->button(t('Add a Page Type'), $this->url('/dashboard/pages/types/add'), 'left', 'primary'); ?>
     </div>
     
     <?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false)?>
