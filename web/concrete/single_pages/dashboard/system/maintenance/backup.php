0bfbe5e38009575aa0ef68485c9c1ab401113818
Andrew Embler <andrew@concrete5.org>
2011-11-03 14:09:31 -0700

    backup tweaks

--- web/concrete/single_pages/dashboard/system/maintenance/backup.php
+++ web/concrete/single_pages/dashboard/system/maintenance/backup.php
@@ -8,7 +8,12 @@ defined('C5_EXECUTE') or die("Access Denied.");
 		//   $('body').append($('#confirmDelete'+dNum)); 
 		$('#confirmDelete').clone().attr('id', 'confirmDelete'+dNum).appendTo('body');
 		var alink = $('#confirmDelete' + dNum + ' input[name=backup_file]').val(strFn); 
-		$('#confirmDelete' + dNum).dialog({width: 500, height: 200, title: "<?= t("Confirm Delete"); ?>"}); 
+		$('#confirmDelete' + dNum).dialog({width: 500, height: 200, title: "<?= t("Confirm Delete"); ?>", buttons:[{}], 'open': function() {
+			$(this).parent().find('.ui-dialog-buttonpane').addClass("ccm-ui").html('');
+			$(this).find('.dialog-buttons').appendTo($(this).parent().find('.ui-dialog-buttonpane'));
+			$(this).find('.dialog-buttons').remove();
+			}
+		});
 		dNum++;
 	}
 
@@ -19,7 +24,12 @@ defined('C5_EXECUTE') or die("Access Denied.");
 		$('#confirmRestore').clone().attr('id', 'confirmRestore'+rNum).appendTo('body');
 		var alink = $('#confirmRestore' + rNum + ' input[name=backup_file]').val(strFn); 
 		$('#confirmRestore' + rNum + ' .confirmActionBtn a').attr('href',alink); 
-		$('#confirmRestore' + rNum).dialog({width:500, height: 200, title: "<?= t("Are you sure?"); ?>"})  
+		$('#confirmRestore' + rNum).dialog({width:500, height: 200, title: "<?= t("Are you sure?"); ?>", buttons:[{}], 'open': function() {
+			$(this).parent().find('.ui-dialog-buttonpane').addClass("ccm-ui").html('');
+			$(this).find('.dialog-buttons').appendTo($(this).parent().find('.ui-dialog-buttonpane'));
+			$(this).find('.dialog-buttons').remove();
+			}
+		});
 		rNum++;
 	}
 	$(document).ready(function () {
@@ -49,118 +59,97 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 <!--Dialog -->
 <div id="confirmDelete" style="display:none" class="ccm-ui">
-	<form method="post" action="<?= $this->action('delete_backup') ?>">
 		<p><?= t('This action <strong>cannot be undone</strong>. Are you sure?') ?></p>
-		<div class="ccm-buttons well">
+		<div class="dialog-buttons">
+			<form method="post" action="<?= $this->action('delete_backup') ?>" style="display: inline">
 			<input type="hidden" name="backup_file" value="" />
-			<?= $interface->button_js(t('Cancel'), "jQuery.fn.dialog.closeTop();", 'left'); ?>
-			<?= $interface->submit('Delete Backup', false, 'right'); ?>
-		</div> 
+			<?= $interface->submit('Delete Backup', false, 'right', 'error'); ?>
 	</form>
+		</div> 
 </div>
 
 <!-- End of Dialog //-->
 
 <!--Dialog -->
 <div id="confirmRestore" style="display:none" class="ccm-ui">
-	<form method="post" action="<?= $this->action('restore_backup') ?>">	
 		<p><?= t('This action <strong>cannot be undone</strong>. Are you sure?') ?></p>
-		<div class="ccm-buttons well">
+		<div class="dialog-buttons">
+			<form method="post" action="<?= $this->action('restore_backup') ?>" style="display: inline">	
 			<input type="hidden" name="backup_file" value="" />
-			<?= $interface->button_js(t('Cancel'), "jQuery.fn.dialog.closeTop();", 'left'); ?>
-			<?= $interface->submit('Restore Backup', false, 'right'); ?>
+			<?= $interface->submit('Restore Backup', false, 'right', 'primary'); ?>
+		</form>
 		</div> 
-	</form>
 </div>
 
 <!-- End of Dialog //-->
-
-<script type="text/javascript">
-	$(document).ready( function() { 
-		$('a.dialog-launch').click( function() {
-			$.fn.dialog.open({ href: $(this).attr('href'),modal:false });
-
-			return false;
-      
-		});
-	});
-
-</script>
 
 
 <?
 $tp = new TaskPermission();
 if ($tp->canBackup()) {
 	?>
-	<?= Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Existing Backups'), false, 'span12 offset2', false) ?>
-	<div class="ccm-pane-body">
+	<?= Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Backup Database'), false, 'span14 offset1') ?>
+		<h3><?=t('Existing Backups')?></h3>
 		<?php
 		if (count($backups) > 0) {
 			?>
-			<br/>
 			<table class="zebra-striped" cellspacing="1" cellpadding="0" border="0">
 				<thead>
 					<tr>
 						<th><?= t('Date') ?></th>
 						<th><?= t('File') ?></th>
-						<th>&nbsp;</th>
-						<th>&nbsp;</th>
-						<th>&nbsp;</th>
+						<th colspan="3"></th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php foreach ($backups as $arr_bkupInf) { ?>
 						<tr> 
-							<td style="white-space: nowrap"><?= date(DATE_APP_GENERIC_MDYT_FULL, strtotime($arr_bkupInf['date'])) ?></td>
-							<td width="100%"><?= $arr_bkupInf['file']; ?></td>
-							<td><?= $interface->button_js(t('Download'), 'window.location.href=\'' . $this->action('download', $arr_bkupInf['file']) . '\''); ?></td>
-							<td>
-								<? print $interface->button_js(t("Restore"), "confirmRestore('" . $arr_bkupInf['file'] . "')"); ?>
-							</td>
-							<td>
-								<? print $interface->button_js(t("Delete"), "confirmDelete('" . $arr_bkupInf['file'] . "')"); ?>
+							<td width="50%" style="white-space: nowrap"><?= date(DATE_APP_GENERIC_MDYT_FULL, strtotime($arr_bkupInf['date'])) ?></td>
+							<td width="50%"><?= $arr_bkupInf['file']; ?></td>
+							<td style="white-space: nowrap">
+								<?= $interface->button_js(t('Download'), 'window.location.href=\'' . $this->action('download', $arr_bkupInf['file']) . '\'', 'left', 'small'); ?>
+								
+								<? print $interface->button_js(t("Restore"), "confirmRestore('" . $arr_bkupInf['file'] . "')", 'left','small'); ?>
+								
+								<? print $interface->button_js(t("Delete"), "confirmDelete('" . $arr_bkupInf['file'] . "')",'left','small'); ?>
 							</td>
 						</tr>
 					<? } ?>
 				</tbody>
 			</table>
+
+
 		<?php } else { ?>
 			<p><?= t('You have no backups available.') ?></p>
 		<? } ?>
-	</div>
-	<?= Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false); ?>
-	<?
-	$crypt = Loader::helper('encryption');
-	?>
-	<br />
-	<?= Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Create New Backup'), false, 'span12 offset2', false) ?>
-	<div class="ccm-pane-body">
 
-		<form method="post" action="<?= $this->action('run_backup') ?>">
-			<div class="ccm-buttons well">
-				<div style="float: left;"><?= $interface->submit(t("Run Backup"), false, "left") ?></div>
-				<? if ($crypt->isAvailable()) { ?>
-				<label><input type="checkbox" name="useEncryption" id="useEncryption" value="1" />
-							<span><?= t('Use Encryption') ?></span></label>
-						<? } else { ?>
-				<label><input type="checkbox" value="0" disabled />
-							<span><?= t('Use Encryption') ?></span></label>
-						<? } ?>
-				<br class="clearfix" />
-			</div>
-		</form>
+			<?
+				$crypt = Loader::helper('encryption');
+			?>
+			<h3><?=t('Create new Backup')?></h3>
+				<form method="post" action="<?= $this->action('run_backup') ?>">
+					<div class="ccm-buttons well">
+						<div style="float: left;"><?= $interface->submit(t("Run Backup"), false, "left") ?></div>
+						<? if ($crypt->isAvailable()) { ?>
+						<label><input type="checkbox" name="useEncryption" id="useEncryption" value="1" />
+									<span><?= t('Use Encryption') ?></span></label>
+								<? } else { ?>
+						<label><input type="checkbox" value="0" disabled />
+									<span><?= t('Use Encryption') ?></span></label>
+								<? } ?>
+						<br class="clearfix" />
+					</div>
+				</form>
+		
+				<h2><?= t('Important Information about Backup & Restore') ?></h2>
+		
+				<p><?= t('Running a backup will create a database export file and store it on your server. Encryption is only advised if you plan on storing the backup on the server indefinitely. This is <strong>not recommended</strong>. After running backup, download the file and make sure that the entire database was saved correctly. If any error messages appear during the backup process, do <b>not</b> attempt to restore from that backup.') ?></p>
 
-		<h2><?= t('Important Information about Backup & Restore') ?></h2>
-
-		<p><?= t('Running a backup will create a database export file and store it on your server. Encryption is only advised if you plan on storing the backup on the server indefinitely. This is <strong>not recommended</strong>. After running backup, download the file and make sure that the entire database was saved correctly. If any error messages appear during the backup process, do <b>not</b> attempt to restore from that backup.') ?></p>
-
-	</div>
-	<?= Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false); ?>
+	<?= Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(); ?>
+	
 <? } else { ?>
-	<?= Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Backup'), false, 'span6 offset6', false) ?>
-	<div class="ccm-pane-body">
-		<p><?= t('You do not have permission to create or administer backups.') ?></p>
-	</div>
-	<?= Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false); ?>
+	<?= Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Backup'), false, 'span6 offset6') ?>
+	<p><?= t('You do not have permission to create or administer backups.') ?></p>
+	<?= Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(); ?>
 <? } ?>
 </div>
