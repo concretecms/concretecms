926425b625ec70ab9a45765a465dc47f4c824c85
Ant Rodgers <arcanepain@gmail.com>
2011-11-04 00:30:32 +0000

    Themes pages into Bootstrap / C5 styles.
    Have some fun here! Just updated the styles to Bootstrap-friendly in some cases...played it a little fast and loose with what was there for the 'Inspect' page. Extra CSS tweaks above and beyond Bootstrap are, again, inline...will need to be added to .less > .css in due course, assmuming approved. One bug -- whilst i've dropped with width of the pages (span12 & span14), the success message on install of theme, etc... is full width. Looks a little odd is all.

--- web/concrete/single_pages/dashboard/pages/themes/view.php
+++ web/concrete/single_pages/dashboard/pages/themes/view.php
@@ -1,5 +1,7 @@
 <?
 defined('C5_EXECUTE') or die("Access Denied.");
+
+// HELPERS
 $bt = Loader::helper('concrete/interface');
 $valt = Loader::helper('validation/token');
 
@@ -7,78 +9,141 @@ $alreadyActiveMessage = t('This theme is currently active on your site.');
 
 ?>
 
-<h1><span><?=t('Themes')?></span></h1>
-<div class="ccm-dashboard-inner">
-	
+<?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Themes'), false, 'span12 offset2');?>
 	
 	<? if (isset($activate_confirm)) { ?>
-	<strong><?=t('Are you sure you wish to activate this theme? Any custom theme selections across your site will be reset.')?></strong>
-	<br/><br/>
-	<?=$bt->button(t("Yes, activate this theme."), $activate_confirm, "left");?>
-	<?=t('or')?> <a href="<?=$this->url('/dashboard/pages/themes/')?>"><?=t('Cancel')?></a>
-	
-	<div class="ccm-spacer">&nbsp;</div>
+    
+    <div class="alert-message block-message error">
+		
+        <h5>
+			<strong><?=t('Are you sure you want to activate this theme?')?></strong>
+		</h5>
+
+		<p>
+        	<em><?=t('Any custom theme selections across your site will be reset.')?></em>
+		</p>
+		
+        <div class="alert-actions clearfix" style="margin-top:15px;">
+			<?=$bt->button(t("Yes, activate this theme."), $activate_confirm, 'left', 'primary');?>
+            <?=$bt->button(t('Cancel'), $this->url('/dashboard/pages/themes/'), 'left');?>
+        </div>
+        
+    </div>
 	
 	<? } else { ?>
 	
-	<div style="margin:0px; padding:0px; width:100%; height:auto" >	
-	<table border="0" cellspacing="0" cellpadding="0" id="ccm-template-list">
+	<table width="100%" border="0" cellspacing="0" cellpadding="0" class="zebra-striped">
+    	<thead>
+        	<tr>
+            	<th <? if (count($tArray) !== 0) { ?>colspan="2"<? } ?>>
+                	<?=t('Currently Installed')?>
+				</th>
+            </tr>
+		</thead>
 	<?
 	if (count($tArray) == 0) { ?>
-	<tr>
-		<td colspan="5"><?=t('No themes are available.')?></td>
-	</tr>
-	<? } else {
-		foreach ($tArray as $t) { ?>
-		<tr <? if ($siteThemeID == $t->getThemeID()) { ?> class="ccm-theme-active" <? } ?>>
-			<td><?=$t->getThemeThumbnail()?></td>
-			<td class="ccm-template-content">
-			<h2><?=$t->getThemeName()?></h2>
-			<?=$t->getThemeDescription()?>
-			<br/><br/>
-			<? if ($siteThemeID == $t->getThemeID()) { ?>
-				<?=$bt->button_js(t("Activate"), "alert('" . $alreadyActiveMessage . "')", "left", "ccm-button-inactive");?>
-			<? } else { ?>
-				<?=$bt->button(t("Activate"), $this->url('/dashboard/pages/themes','activate', $t->getThemeID()), "left");?>
-			<? } ?>
-			<?=$bt->button_js(t("Preview"), "ccm_previewInternalTheme(1, " . intval($t->getThemeID()) . ",'" . addslashes(str_replace(array("\r","\n",'\n'),'',$t->getThemeName())) . "')", "left");?>
-			<?=$bt->button(t("Inspect"), $this->url('/dashboard/pages/themes/inspect', $t->getThemeID()), "left");?>
-			<?=$bt->button(t("Customize"), $this->url('/dashboard/pages/themes/customize', $t->getThemeID()), "left");?>
-			
-			<?=$bt->button(t("Remove"), $this->url('/dashboard/pages/themes', 'remove', $t->getThemeID(), $valt->generate('remove')), "left");?>
-			</td>
-		</tr>
-		<? }
-	} ?>
+		
+        <tbody>
+            <tr>
+                <td><p><?=t('No themes are installed.')?></p></td>
+            </tr>        
+		</tbody>
+    
+	<? } else { ?>
+    
+    	<tbody>
+		
+		<? foreach ($tArray as $t) { ?>        
+        
+            <tr <? if ($siteThemeID == $t->getThemeID()) { ?> class="ccm-theme-active" <? } ?>>
+                
+                <td>
+					<div class="ccm-themes-thumbnail" style="padding:4px;background-color:#FFF;border-radius:3px;border:1px solid #DDD;">
+						<?=$t->getThemeThumbnail()?>
+					</div>
+				</td>
+                
+                <td width="100%" style="vertical-align:middle;">
+                
+                    <p class="ccm-themes-name"><strong><?=$t->getThemeName()?></strong></p>
+                    <p class="ccm-themes-description"><em><?=$t->getThemeDescription()?></em></p>
+                    
+                    <div class="ccm-themes-button-row clearfix">
+                    <? if ($siteThemeID == $t->getThemeID()) { ?>
+                        <?=$bt->button_js(t("Activate"), "alert('" . $alreadyActiveMessage . "')", 'left', 'primary ccm-button-inactive', array('disabled'=>'disabled'));?>
+                    <? } else { ?>
+                        <?=$bt->button(t("Activate"), $this->url('/dashboard/pages/themes','activate', $t->getThemeID()), 'left', 'primary');?>
+                    <? } ?>
+                        <?=$bt->button_js(t("Preview"), "ccm_previewInternalTheme(1, " . intval($t->getThemeID()) . ",'" . addslashes(str_replace(array("\r","\n",'\n'),'',$t->getThemeName())) . "')", 'left');?>
+                        <?=$bt->button(t("Inspect"), $this->url('/dashboard/pages/themes/inspect', $t->getThemeID()), 'left');?>
+                        <?=$bt->button(t("Customize"), $this->url('/dashboard/pages/themes/customize', $t->getThemeID()), 'left');?>
+                    
+                        <?=$bt->button(t("Remove"), $this->url('/dashboard/pages/themes', 'remove', $t->getThemeID(), $valt->generate('remove')), 'right', 'error');?>
+                    </div>
+                
+                </td>
+            </tr>
+            
+		<? } // END FOREACH ?>
+        
+        </tbody>
+        
+	<? } // END 'ELSE' INSTALLED LISTING ?>
+    
+    </table>
+    
+    <!-- END CURRENTLY INSTALLED -->
+    
 	<? 
 	if (count($tArray2) > 0) { ?>
-	<tr>
-		<td colspan="2" class="header"><br/><h2><?=t('Themes Available to Install')?></h2></td>
-	</tr>
+    
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="zebra-striped">
+    	<thead>
+        	<tr>
+            	<th colspan="2"><?=t('Themes Available to Install')?></th>
+            </tr>
+		</thead>
+        <tbody>
 
-	<? foreach ($tArray2 as $t) { ?>
-		<tr>
-			<td><?=$t->getThemeThumbnail()?></td>
-			<td class="ccm-template-content">
-			<h3><?=$t->getThemeName()?></h3>
-			<?=$t->getThemeDescription()?>
-			<br/><br/>
-			<?=$bt->button(t("Install"), $this->url('/dashboard/pages/themes','install', $t->getThemeHandle()), "left");?>
-			
-		</tr>
-		<? }
-	} ?>
+		<? foreach ($tArray2 as $t) { ?>
+            <tr>
+                
+                <td>
+					<div class="ccm-themes-thumbnail" style="padding:4px;background-color:#FFF;border-radius:3px;border:1px solid #DDD;">
+						<?=$t->getThemeThumbnail()?>
+					</div>
+				</td>
+                
+                <td width="100%" style="vertical-align:middle;">
+                <p class="ccm-themes-name"><strong><?=$t->getThemeName()?></strong></p>
+                <p class="ccm-themes-description"><em><?=$t->getThemeDescription()?></em></p>
+                
+                <div class="ccm-themes-button-row clearfix">
+                <?=$bt->button(t("Install"), $this->url('/dashboard/pages/themes','install',$t->getThemeHandle()),'left','primary');?>
+                </div>
+                </td>
+                
+            </tr>
+        <? } // END FOREACH ?>
+        
+        </tbody>
 	</table>
-	</div>
+    
+    <!-- END AVAILABLE TO INSTALL -->
+			
+	<? } // END 'IF AVAILABLE' CHECK ?>
+    
+    <? if (ENABLE_MARKETPLACE_SUPPORT == true) { ?>
 
-	<? } ?>
-	
-	</div>
-	
-<? if (ENABLE_MARKETPLACE_SUPPORT == true) { ?>
+	<div class="well" style="padding:10px 20px;">
+        <h3><?=t('Looking for more themes?')?></h3>
+        <p><a href="<?=$this->url('/dashboard/install', 'browse', 'themes')?>"><?=t("Download more themes from the concrete5.org marketplace.")?></a></p>
+    </div>
+    
+    <? } ?>
 
-<h1><span><?=t('More Themes')?></span></h1>
-<div class="ccm-dashboard-inner">
-<a href="<?=$this->url('/dashboard/install', 'browse', 'themes')?>"><?=t("Download more themes from the concrete5.org marketplace.")?></a>
-</div>
-<? } ?>
+	<?
+	} // END 'ELSE' DEFAULT LISTING
+	?>
+	
+	<?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper()?>
