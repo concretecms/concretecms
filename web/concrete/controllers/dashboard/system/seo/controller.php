4724ccfd39c6f7c92d38f1ad14ae5e0896c7d8c9
Brad Beebe <beebs93@gmail.com>
2011-11-06 13:02:17 -0800

    Updated main SEO controller for issue #126
    Updated controller to redirect to Pretty URLs page

--- web/concrete/controllers/dashboard/system/seo/controller.php
+++ web/concrete/controllers/dashboard/system/seo/controller.php
@@ -2,23 +2,14 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 
 class DashboardSystemSeoController extends DashboardBaseController{
-
-	public $helpers = array('form'); 
-	
-	public function view(){
-		$categories = array();
-		$c = Page::getCurrentPage();
-		$children = $c->getCollectionChildrenArray(true);
-		
-		foreach($children as $cID) {
-			$nc = Page::getByID($cID, 'ACTIVE');
-			$ncp = new Permissions($nc);
-			if($ncp->canRead()){
-				$categories[] = $nc;	
-			}
-		}				
-		
-		$this->set('categories', $categories);
+	/**
+	* Dashboard view - automatically redirects to a default
+	* page in the category
+	*
+	* @return void
+	*/
+	public function view() {
+		$this->redirect('/dashboard/system/seo/urls');
 	}
 }
 ?>
