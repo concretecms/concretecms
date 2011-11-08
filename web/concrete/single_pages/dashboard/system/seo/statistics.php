5b1ebfe762c6055c9c17d475bae3b02cddc06334
Andrew Embler <andrew@concrete5.org>
2011-11-08 09:34:32 -0800

    modify statistics to have a label tag
    Former-commit-id: 13720227d36859dcea4cdba1f03047ffdf8fc0bf

--- web/concrete/single_pages/dashboard/system/seo/statistics.php
+++ web/concrete/single_pages/dashboard/system/seo/statistics.php
@@ -2,12 +2,21 @@
 $form = Loader::helper('form');
 echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Statistics'), t("Tracks page views in concrete5. Disabling this may increase site performance, but you will have to get statistics information from elsewhere."), 'span7 offset5', false); ?>
 
-	<form method="post" id="url-form" action="<?php echo $this->action('')?>">
+	<form method="post" class="form-stacked" id="url-form" action="<?php echo $this->action('')?>">
 		<?=$this->controller->token->output('update_statistics')?>
 		<div class="ccm-pane-body">
 			<div class="clearfix">
+				<label></label>
+				<div class="input">
+				<ul class="inputs-list">
+				<li>
+				<label>
 				<?=$form->checkbox('STATISTICS_TRACK_PAGE_VIEWS', 1, STATISTICS_TRACK_PAGE_VIEWS); ?>
 				<span><?=t('Track page view statistics.');?></span>
+				</label>
+				</li>
+				</ul>
+				</div>
 			</div>
 		</div>
 		<div class="ccm-pane-footer">
