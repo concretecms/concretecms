b34b4d67a432e69fa348293b168ba2f5cb0a33d8
Andrew Embler <andrew@concrete5.org>
2011-11-09 14:30:48 -0800

    work on the upgrade process
    Former-commit-id: 9e59114bd8779872a77e187c4d9205a0bee4019d

--- web/concrete/helpers/concrete/upgrade/version_550.php
+++ web/concrete/helpers/concrete/upgrade/version_550.php
@@ -29,7 +29,7 @@ class ConcreteUpgradeVersion550Helper {
 		}
 		$columns = $db->MetaColumns('Pages');
 		if (!isset($columns['CISACTIVE'])) {
-			$db->Execute('alter table Pages add column cIsActive tinyint(1) not null default 0');
+			$db->Execute('alter table Pages add column cIsActive tinyint(1) not null default 1');
 			$db->Execute('alter table Pages add index (cIsActive)');
 			$db->Execute('update Pages set cIsActive = 1');
 		}
@@ -54,11 +54,70 @@ class ConcreteUpgradeVersion550Helper {
 		}
 
 		// Install new block types
-		
-		// install new page types 
+		$this->installBlockTypes();
 
-		// Migrate the dashboard
+		// install stacks, trash and drafts
+		$this->installSinglePages();
 		
+		// move the old dashboard
+		$dashboard = Page::getByPath('/dashboard');
+		$dashboard->moveToTrash();
+		
+		// install new dashboard + page types		
+		$this->installDashboard();
+		
+		// TODO - migrate non core pages out of the dashboard into where we think they should go.
+		
+	}
+	
+	public function installSinglePages() {
+		Loader::model('single_page');
+		$spl = SinglePage::add('/!trash');
+		$spl->update(array('cName' => t('Trash')));
+		$spl->moveToRoot();
+		$spl = SinglePage::add('/!stacks');
+		$spl->update(array('cName' => t('Stacks')));
+		$spl->moveToRoot();
+		$spl = SinglePage::add('/!drafts');
+		$spl->update(array('cName' => t('Drafts')));
+		$spl->moveToRoot();
+	}
+	
+	public function installBlockTypes() {
+		$bt = BlockType::getByHandle('core_scrapbook_display');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('core_scrapbook_display');			
+		}
+		$bt = BlockType::getByHandle('core_stack_display');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('core_stack_display');			
+		}
+		$bt = BlockType::getByHandle('dashboard_app_status');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('dashboard_app_status');			
+		}
+		$bt = BlockType::getByHandle('dashboard_featured_addon');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('dashboard_featured_addon');			
+		}
+		$bt = BlockType::getByHandle('dashboard_featured_theme');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('dashboard_featured_theme');			
+		}
+		$bt = BlockType::getByHandle('dashboard_form_summary');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('dashboard_form_summary');			
+		}
+		$bt = BlockType::getByHandle('dashboard_newsflow_latest');
+		if (!is_object($bt)) {
+			BlockType::installBlockType('dashboard_newsflow_latest');			
+		}
+	}
+	
+	public function installDashboard() {
+		Loader::library('content/importer');
+		$ci = new ContentImporter();
+		$ci->importContentFile(DIR_BASE_CORE. '/config/install/base/dashboard.xml');
 	}
 	
 	public function prepare() {
