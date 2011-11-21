bb97494ee56957e98512d46781200bb023b19797
Andrew Embler <andrew@concrete5.org>
2011-11-21 09:24:37 -0800

    Fixing conflicts
    Former-commit-id: 2c9405e08435d052f8b9a90ff9bf9e051267762b

--- web/concrete/blocks/page_list/view.php
+++ web/concrete/blocks/page_list/view.php
@@ -1,56 +1,80 @@
-<?
-	defined('C5_EXECUTE') or die("Access Denied.");
-	$textHelper = Loader::helper("text"); 
-	// now that we're in the specialized content file for this block type, 
-	// we'll include this block type's class, and pass the block to it, and get
-	// the content
-	
-	if (count($cArray) > 0) { ?>
-	<div class="ccm-page-list">
-	
-	<?
-	for ($i = 0; $i < count($cArray); $i++ ) {
-		$cobj = $cArray[$i]; 
-		$target = $cobj->getAttribute('nav_target');
-
-		if ($cobj->getCollectionPointerExternalLink() != '') {
-			if ($cobj->openCollectionPointerExternalLinkInNewWindow()) {
-				$target = "_blank";
-			}
-		}
-
-		$title = $textHelper->entities($cobj->getCollectionName()); ?>
-	
-	<h3 class="ccm-page-list-title"><a <? if ($target != '') { ?> target="<?=$target?>" <? } ?> href="<?=$nh->getLinkToCollection($cobj)?>"><?=$title?></a></h3>
-	<div class="ccm-page-list-description">
-		<?
-		if(!$controller->truncateSummaries){
-			echo $textHelper->entities($cobj->getCollectionDescription());
-		}else{
-			echo $textHelper->entities($textHelper->shorten($cobj->getCollectionDescription(),$controller->truncateChars));
-		}
-		?>
-	</div>
-	
-<?  } 
-	if(!$previewMode && $controller->rss) { 
-			$btID = $b->getBlockTypeID();
-			$bt = BlockType::getByID($btID);
-			$uh = Loader::helper('concrete/urls');
-			$rssUrl = $controller->getRssUrl($b);
-			?>
-			<div class="ccm-page-list-rss-icon">
-				<a href="<?=$rssUrl?>" target="_blank"><img src="<?=$uh->getBlockTypeAssetsURL($bt, 'rss.png')?>" width="14" height="14" alt="<?php echo t('RSS Icon')?>" title="<?php echo t('RSS Feed')?>" /></a>
-			</div>
-			<link href="<?=BASE_URL . $rssUrl?>" rel="alternate" type="application/rss+xml" title="<?=$textHelper->entities($controller->rssTitle)?>" />
-		<? 
-	} 
-	?>
-</div>
-<? } 
-	
-	if ($paginate && $num > 0 && is_object($pl)) {
-		$pl->displayPagingV2();
-	}
-	
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$rssUrl = $showRss ? $controller->getRssUrl($b) : '';
+$th = Loader::helper('text');
+//$ih = Loader::helper('image'); //<--uncomment this line if displaying image attributes (see below)
+//Note that $nh (navigation helper) is already loaded for us by the controller (for legacy reasons)
 ?>
+
+<div class="ccm-page-list">
+
+	<?php foreach ($pages as $page):
+
+		// Prepare data for each page being listed...
+		$title = $th->entities($page->getCollectionName());
+		$url = $nh->getLinkToCollection($page);
+		$target = ($page->getCollectionPointerExternalLink() != '' && $page->openCollectionPointerExternalLinkInNewWindow()) ? '_blank' : $page->getAttribute('nav_target');
+		$target = empty($target) ? '_self' : $target;
+		$description = $page->getCollectionDescription();
+		$description = $controller->truncateSummaries ? $th->shorten($description, $controller->truncateChars) : $description;
+		$description = $th->entities($description);	
+		
+		//Other useful page data...
+		//$date = date('F j, Y', strtotime($page->getCollectionDatePublic()));
+		//$last_edited_by = $page->getVersionObject()->getVersionAuthorUserName();
+		//$original_author = Page::getByID($page->getCollectionID(), 1)->getVersionObject()->getVersionAuthorUserName();
+		
+		/* CUSTOM ATTRIBUTE EXAMPLES:
+		 * $example_value = $page->getAttribute('example_attribute_handle');
+		 *
+		 * HOW TO USE IMAGE ATTRIBUTES:
+		 * 1) Uncomment the "$ih = Loader::helper('image');" line up top.
+		 * 2) Put in some code here like the following 2 lines:
+		 *      $img = $page->getAttribute('example_image_attribute_handle');
+		 *      $thumb = $ih->getThumbnail($img, 64, 9999, false);
+		 *    (Replace "64" with max width, "9999" with max height. The "9999" effectively means "no maximum size" for that particular dimension.)
+		 *    (Change the last argument from false to true if you want thumbnails cropped.)
+		 * 3) Output the image tag below like this:
+		 *		<img src="<?php echo $thumb->src ?>" width="<?php echo $thumb->width ?>" height="<?php echo $thumb->height ?>" alt="" />
+		 *
+		 * ~OR~ IF YOU DO NOT WANT IMAGES TO BE RESIZED:
+		 * 1) Put in some code here like the following 2 lines:
+		 * 	    $img_src = $img->getRelativePath();
+		 * 	    list($img_width, $img_height) = getimagesize($img->getPath());
+		 * 2) Output the image tag below like this:
+		 * 	    <img src="<?php echo $img_src ?>" width="<?php echo $img_width ?>" height="<?php echo $img_height ?>" alt="" />
+		 */
+		
+		/* End data preparation. */
+
+		/* The HTML from here through "endforeach" is repeated for every item in the list... */ ?>
+		<h3 class="ccm-page-list-title">
+			<a href="<?php echo $url ?>" target="<?php echo $target ?>"><?php echo $title ?></a>
+		</h3>
+		<div class="ccm-page-list-description">
+			<?php echo $description ?>
+		</div>
+		
+	<?php endforeach; ?>
+ 
+
+	<?php if ($showRss): ?>
+		<div class="ccm-page-list-rss-icon">
+			<a href="<?php echo $rssUrl ?>" target="_blank"><img src="<?php echo $rssIconSrc ?>" width="14" height="14" alt="<?php echo t('RSS Icon') ?>" title="<?php echo t('RSS Feed') ?>" /></a>
+		</div>
+		<link href="<?php echo BASE_URL.$rssUrl ?>" rel="alternate" type="application/rss+xml" title="<?php echo $rssTitle; ?>" />
+	<?php endif; ?>
+ 
+</div><!-- end .ccm-page-list -->
+
+
+<?php if ($showPagination): ?>
+	<div id="pagination">
+		<div class="ccm-spacer"></div>
+		<div class="ccm-pagination">
+			<span class="ccm-page-left"><?php echo $paginator->getPrevious('&laquo; ' . t('Previous')) ?></span>
+			<?php echo $paginator->getPages() ?>
+			<span class="ccm-page-right"><?php echo $paginator->getNext(t('Next') . ' &raquo;') ?></span>
+		</div>
+	</div>
+<?php endif; ?>
