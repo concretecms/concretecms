4c77a18b68a94e934cdf7efa69dd7fc9aed7ab80
Andrew Embler <andrew@concrete5.org>
2011-11-29 15:00:20 -0800

    adding transfer effect

--- web/concrete/js/ccm.app.js
+++ web/concrete/js/ccm.app.js
@@ -2999,8 +2999,22 @@ $.fn.metadata = function( opts ){
 }).call(this);
 
 ccm_closeDashboardPane = function(r) {
-	$(r).closest('div.ccm-pane').fadeOut(120, 'easeOutExpo');
+	var accepter = $('#ccm-recent-page-' + CCM_CID);
+	var l = $(r);
+	ccm_showQuickNav(function() { 
+		var title = l.parent().parent().parent().find('h3');
+		title.css('display','inline');
+		$(r).closest('div.ccm-pane').fadeOut(120, 'easeOutExpo');
+		title.effect("transfer", { to: accepter, 'easing': 'easeOutExpo'}, 600, function() {
+			accepter.hide().css('visibility','visible').fadeIn(240, 'easeInExpo');			
+			title.css('display','block');
+			ccm_quickNavTimer = setTimeout(function() {
+				ccm_hideQuickNav();
+			}, 1000);
+		});
+	});
 }
+
 
 ccm_getDashboardBackgroundImageData = function(image) {
 	$.getJSON(CCM_TOOLS_PATH + '/dashboard/get_image_data', {
