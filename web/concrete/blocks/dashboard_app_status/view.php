369d0bb5d3fd9aaf988ae1c678cb91661f39dc01
Andrew Embler <andrew@concrete5.org>
2011-12-05 20:47:05 -0800

    adding site activity and link to basic stats page
    Former-commit-id: 1e19b25bf905a3cdc9f15b22458616d1583e823b

--- web/concrete/blocks/dashboard_app_status/view.php
+++ web/concrete/blocks/dashboard_app_status/view.php
@@ -1,30 +1,24 @@
 <? defined('C5_EXECUTE') or die("Access Denied."); ?>
-<h1 id="ccm-dashboard-welcome-back"><?=t('Welcome Back')?>
-<span><?=t('You are currently running concrete5 version <strong>%s</strong>.', APP_VERSION)?></span>
-</h1>
+
+<h1><?=t('Welcome Back')?></h1>
+
+<p><?=t('You are currently running concrete5 version <strong>%s</strong>.', APP_VERSION)?></p>
 
 <? if (version_compare($latest_version, APP_VERSION, '>')) { ?>
-<div class="block-message alert-message info">
-<h4><?=t('A new version of concrete5 is available!')?></h4>
-<p><?=t('The latest version of concrete5 is <strong>%s</strong>. You are running %s.', $latest_version, APP_VERSION)?></p>
-<div class="alert-actions"><a class="small btn" href="<?=$this->url('/dashboard/system/maintenance/update', 'update')?>"><?=t('Update concrete5')?></a></div>
-</div>
+<p><span class="label notice"><?=t('concrete5')?></span> <?=t('The latest version of concrete5 is <strong>%s</strong>.', $latest_version)?> <a class="" href="<?=$this->url('/dashboard/system/maintenance/update', 'update')?>"><?=t('Learn more and update.')?></a></p>
+
 <? } else if (version_compare(APP_VERSION, Config::get('SITE_APP_VERSION'), '>')) { ?>
-<div class="block-message alert-message warning">
-<h4><?=t('A new version of concrete5 is available!')?></h4>
-<p><?=t('You have downloaded a new version of concrete5 but have not upgraded to it yet.');?></p>
-<div class="alert-actions"><a class="small btn" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/upgrade"><?=t('Update concrete5')?></a></div>
-</div>
+<p><span class="label warning"><?=t('concrete5')?></span>
+<?=t('You have downloaded a new version of concrete5 but have not upgraded to it yet.');?> <a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/upgrade"><?=t('Update concrete5.')?></a></p>
 <? } ?>
 
 <? if ($updates > 0) { ?>
-	<div class="block-message alert-message info">
-	<h4><?=t('Add-On updates are available!')?></h4>
+	<p><span class="label"><?=t('Add-On Updates')?></span> 
 	<? if ($updates == 1) { ?>
-		<p><?=t('There is currently <strong>1</strong> update available.')?></p>
+		<?=t('There is currently <strong>1</strong> update available.')?>
 	<? } else { ?>
-		<p><?=t('There are currently <strong>%s</strong> updates available.', $updates)?></p>
+		<?=t('There are currently <strong>%s</strong> updates available.', $updates)?>
 	<? } ?>
-	<div class="alert-actions"><a class="small btn" href="<?=$this->url('/dashboard/extend/update')?>"><?=t('Update Add-Ons')?></a></div>
-	</div>
+	<a class="" href="<?=$this->url('/dashboard/extend/update')?>"><?=t('Update add-ons.')?></a></p>
 <? } ?>
+
---
+++ web/concrete/blocks/dashboard_site_activity/controller.php
@@ -0,0 +1,44 @@
+<?
+	defined('C5_EXECUTE') or die("Access Denied.");
+	
+	class DashboardSiteActivityBlockController extends BlockController {
+
+		protected $btCacheBlockRecord = true;
+		protected $btIsInternal = true;		
+		
+		public function getBlockTypeDescription() {
+			return t("Displays a summary of website activity.");
+		}
+		
+		public function getBlockTypeName() {
+			return t("Dashboard Site Activity");
+		}
+		
+		public function view() {
+			$u = new User();
+			$ui = UserInfo::getByID($u->getUserID());
+			Loader::model("page_statistics");
+			Loader::model("user_statistics");
+			$timeStr = '';
+			if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') { 
+				$timeStr = '%x ' . t('at') . ' %I:%M %p';
+			} else {
+				$timeStr = '%x ' . t('at') . ' %l:%M %p';
+			}		
+			$this->set('uLastLogin', strftime($timeStr, $ui->getLastLogin('user')));
+			$this->set('uName', $ui->getUserName());
+			$this->set('totalVersions', PageStatistics::getTotalPageVersions());
+			$this->set('lastEditSite', strftime($timeStr, strtotime(PageStatistics::getSiteLastEdit('user'))));
+			$llu = UserStatistics::getLastLoggedInUser();
+			if ($llu->getUserID() == $u->getUserID()) {
+				$this->set('lastLoginSite', t('Your login is the most recent.'));		
+			} else { 
+				$this->set('lastLoginSite', strftime($timeStr, $llu->getLastLogin()));
+			}
+			$this->set('totalEditMode', PageStatistics::getTotalPagesCheckedOut());
+			Loader::block('form');
+			$this->set('totalFormSubmissions', FormBlockStatistics::getTotalSubmissions());
+			$this->set('totalFormSubmissionsToday', FormBlockStatistics::getTotalSubmissions(date('Y-m-d')));
+		}
+		
+	}
