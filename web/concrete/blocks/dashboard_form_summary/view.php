1e19b25bf905a3cdc9f15b22458616d1583e823b
Andrew Embler <andrew@concrete5.org>
2011-12-05 20:47:05 -0800

    adding site activity and link to basic stats page

--- web/concrete/blocks/dashboard_form_summary/view.php
+++
@@ -1,2 +0,0 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?>
-<p><?=t('Total form submissions: <strong>%s</strong> (<strong>%s</strong> total). <a href="%s">View Form Results</a>.', $totalFormSubmissions, $totalFormSubmissionsToday, $this->url('/dashboard/reports/forms'))?></p>
