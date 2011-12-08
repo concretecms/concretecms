a4684df7e11a12678763b3489b57ebbe389b82be
Andrew Embler <andrew@concrete5.org>
2011-12-07 17:04:12 -0800

    registration and verbiage changes
    Former-commit-id: f0610c5a3bf8a8acd0db7b31c4086d1e60899ca6

--- web/concrete/single_pages/dashboard/system/registration/public_registration.php
+++ web/concrete/single_pages/dashboard/system/registration/public_registration.php
@@ -1,38 +1,37 @@
 <? defined('C5_EXECUTE') or die("Access Denied.");?>
-<?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Public Registration'), t('Control the options available for Public Registration.'), false, false);?>
+<?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Public Registration'), t('Control the options available for Public Registration.'), 'span10 offset3', false);?>
 <?php
 $h = Loader::helper('concrete/interface');
 ?>	
-    <form method="post" id="registration-type-form" action="<?php echo $this->url('/dashboard/system/registration/public_registration', 'update_registration_type')?>">  
+    <form class="form-stacked" method="post" id="registration-type-form" action="<?php echo $this->url('/dashboard/system/registration/public_registration', 'update_registration_type')?>">  
     
     <div class="ccm-pane-body"> 
     	
     	<div class="clearfix">
-            <label id="optionsCheckboxes"><strong><?php echo t('Registration Options')?></strong></label>
+            <label id="optionsCheckboxes"><strong><?php echo t('Allow visitors to signup as site members?')?></strong></label>
             <div class="input">
-            <?php echo t('Allow visitors to signup as site members?');?>
 			  <ul class="inputs-list">
 			    <li>
 			      <label>
-			        <input type="radio" name="registration_type" value="disabled" style="vertical-align: middle" <?php echo ( $registration_type == "disabled" || !strlen($registration_type) )?'checked':''?> />
+			        <input type="radio" name="registration_type" value="disabled" style="" <?php echo ( $registration_type == "disabled" || !strlen($registration_type) )?'checked':''?> />
 			        <span><?php echo t('Off')?></span>
 			      </label>
 			    </li> 
 			    <li>
 			      <label>
-			        <input type="radio" name="registration_type" value="validate_email" style="vertical-align: middle" <?php echo ( $registration_type == "validate_email" )?'checked':''?> />
+			        <input type="radio" name="registration_type" value="validate_email" style="" <?php echo ( $registration_type == "validate_email" )?'checked':''?> />
 			        <span><?php echo t(' On - email validation')?></span>
 			      </label>
 			    </li>
 			    <li>
 			      <label>
-			        <input type="radio" name="registration_type" value="manual_approve" style="vertical-align: middle" <?php echo ( $registration_type == "manual_approve" )?'checked':''?> />
+			        <input type="radio" name="registration_type" value="manual_approve" style="" <?php echo ( $registration_type == "manual_approve" )?'checked':''?> />
 			        <span><?php echo t('On - approve manually')?></span>
 			      </label>
 			    </li>
 			    <li>
 			      <label>
-			        <input type="radio" name="registration_type" value="enabled" style="vertical-align: middle" <?php echo ( $registration_type == "enabled" )?'checked':''?> />
+			        <input type="radio" name="registration_type" value="enabled" style="" <?php echo ( $registration_type == "enabled" )?'checked':''?> />
 			        <span><?php echo t('On - signup and go')?></span>
 			      </label>
 			    </li>  
@@ -46,20 +45,20 @@ $h = Loader::helper('concrete/interface');
 			  <ul class="inputs-list">
 			    <li>
 			      <label>
-			        <input type="checkbox" name="enable_registration_captcha" value="1" style="vertical-align: middle" <?php echo ( $enable_registration_captcha )?'checked':''?> />
+			        <input type="checkbox" name="enable_registration_captcha" value="1" style="" <?php echo ( $enable_registration_captcha )?'checked':''?> />
 			        <span><?php echo t('CAPTCHA required')?></span>
 			      </label>
 			    </li>
 			    <li>
 			      <label>
-			        <input type="checkbox" name="enable_openID" value="1" style="vertical-align: middle" <?php echo ( $enable_openID )?'checked':''?> />
+			        <input type="checkbox" name="enable_openID" value="1" style="" <?php echo ( $enable_openID )?'checked':''?> />
 			        <span><?php echo t('Enable OpenID')?></span>
 			      </label>
 			    </li>
 			    <li>
 			      <label>
-			       <input type="checkbox" name="email_as_username" value="1" style="vertical-align: middle" <?php echo ( $email_as_username )?'checked':''?> />
-			        <span><?php echo t('Use eMails for login')?></span>
+			       <input type="checkbox" name="email_as_username" value="1" style="" <?php echo ( $email_as_username )?'checked':''?> />
+			        <span><?php echo t('Use emails for login')?></span>
 			      </label>
 			    </li>  
 			  </ul>
