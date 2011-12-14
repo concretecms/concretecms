f9cf6699cb114c97fce4100c21c7d088317bd9c9
Andrew Embler <andrew@concrete5.org>
2011-12-14 12:23:07 -0800

    Close #323

--- web/concrete/controllers/dashboard/system/backup_restore/controller.php
+++ web/concrete/controllers/dashboard/system/backup_restore/controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class DashboardSystemBackupRestoreController extends DashboardBaseController{
+class DashboardSystemBackupRestoreController extends DashboardBaseController {
 	/**
 	* Dashboard view - automatically redirects to a default
 	* page in the category
