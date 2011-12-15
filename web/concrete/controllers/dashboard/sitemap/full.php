0eaec5387a7725dc8d8d106dd40effeac664414c
Andrew Embler <andrew@concrete5.org>
2011-12-14 17:07:55 -0800

    Close #247
    Former-commit-id: c76f9a0ab6f3e1bec840e03709eda124450282e4

--- web/concrete/controllers/dashboard/sitemap/full.php
+++ web/concrete/controllers/dashboard/sitemap/full.php
@@ -3,8 +3,5 @@ defined('C5_EXECUTE') or die("Access Denied.");
 Loader::controller('/dashboard/base');
 class DashboardSitemapFullController extends DashboardBaseController {
 
-	public function getQuickNavigationLinkHTML() {
-		return '<a href="javascript:void(0)" onclick="jQuery.fn.dialog.open({modal:false, title: \'' . t('Navigate to Page') . '\', href: \'' . REL_DIR_FILES_TOOLS_REQUIRED . '/sitemap_search_selector?callback=ccm_goToSitemapNode&sitemap_select_mode=select_page\', width: \'90%\', height: \'70%\'})">' . t('Sitemap and Page Search') . '</a>';
-	}
 	
 }
