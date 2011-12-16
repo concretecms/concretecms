79170c62495a4454629071e7ffef75c1cf5c31bd
Andrew Embler <andrew@concrete5.org>
2011-12-16 12:34:51 -0800

    newsflow work

--- web/concrete/themes/dashboard/dashboard_header_four_col.php
+++ web/concrete/themes/dashboard/dashboard_header_four_col.php
@@ -11,10 +11,10 @@ $this->inc('elements/header.php', array('enableEditing' => true));
 	<td class="newsflow-em1" colspan="4"><? $a = new Area('Header'); $a->display($c); ?></td>
 </tr>
 <tr>
-	<td style="width: 25%"><? $a = new Area('Column 1'); $a->display($c); ?></td>
-	<td style="width: 25%"><? $a = new Area('Column 2'); $a->display($c); ?></td>
-	<td style="width: 25%"><? $a = new Area('Column 3'); $a->display($c); ?></td>
-	<td style="width: 25%"><? $a = new Area('Column 4'); $a->display($c); ?></td>
+	<td class="newsflow-column-quarter"><? $a = new Area('Column 1'); $a->display($c); ?></td>
+	<td class="newsflow-column-quarter"><? $a = new Area('Column 2'); $a->display($c); ?></td>
+	<td class="newsflow-column-quarter"><? $a = new Area('Column 3'); $a->display($c); ?></td>
+	<td class="newsflow-column-quarter"><? $a = new Area('Column 4'); $a->display($c); ?></td>
 </tr>
 </table>
 </div>
