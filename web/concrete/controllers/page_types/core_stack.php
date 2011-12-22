65de88e9479472b3e2fd3573e76caa107675bd7e
Andrew Embler <andrew@concrete5.org>
2011-12-22 14:22:33 -0800

    no longer displaying dashboard theme when rendering stacks 404 page
    Former-commit-id: 2f746de09a20bfc1be80cde620685b87ea348f12

--- web/concrete/controllers/page_types/core_stack.php
+++ web/concrete/controllers/page_types/core_stack.php
@@ -4,8 +4,6 @@ defined('C5_EXECUTE') or die("Access Denied.");
 Loader::controller('/dashboard/base');
 class CoreStackPageTypeController extends Controller {
 
-	public $theme = 'dashboard';
-	
 	public function on_start() {
 		$c = Page::getByPath('/dashboard/blocks/stacks');
 		$cp = new Permissions($c);
@@ -15,9 +13,18 @@ class CoreStackPageTypeController extends Controller {
 			if ((!$pcp->canReadVersions()) || ($_GET['vtask'] != 'view_versions' && $_GET['vtask'] != 'compare')) {
 				$cID = $c->getCollectionID();
 				$this->redirect('/dashboard/blocks/stacks','view_details', $cID);		
+			} else {
+				$this->theme = 'dashboard';
 			}
 		} else {
+			global $c; // ugh
 			$v = View::getInstance();
+			$c = new Page();
+			$c->loadError(COLLECTION_NOT_FOUND);
+			$v->setCollectionObject($c);
+			$this->c = $c;
+			$cont = Loader::controller("/page_not_found");
+			$v->setController($cont);				
 			$v->render('/page_not_found');
 		}
 	}		
