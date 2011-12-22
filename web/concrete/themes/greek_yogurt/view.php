7199e7af3bd386eca730c7d0fa2c6479013c7e3b
Andrew Embler <andrew@concrete5.org>
2011-12-22 10:39:15 -0800

    Adding a basic view.php to Greek Yogurt
    Former-commit-id: 21e86c98342a6d28f2b1fce3fe35b4610f2c7e90

---
+++ web/concrete/themes/greek_yogurt/view.php
@@ -0,0 +1,29 @@
+<?php 
+defined('C5_EXECUTE') or die("Access Denied.");
+$this->inc('elements/header.php'); ?>
+	
+	<div class="clear"></div>
+
+	<div id="main-content-container" class="grid_16">
+		<div id="main-content-inner">
+			<? print $innerContent; ?>
+		</div>
+	
+	</div>
+
+	<div id="right-sidebar-container" class="grid_8">
+
+		<div id="right-sidebar-inner">
+	
+			<?php 
+			$a = new Area('Sidebar');
+			$a->display($c);
+			?>
+			
+		</div>
+	
+	</div>
+	
+	<!-- end sidebar -->
+	
+<?php $this->inc('elements/footer.php'); ?>
