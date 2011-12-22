8900aa7a8fb1088aa4a87bbdbedb3a40b1aed322
Andrew Embler <andrew@concrete5.org>
2011-12-22 12:21:18 -0800

    ssl verify peer
    Former-commit-id: d2ba590bb22bee1fecb716a0faf3232b4671646d

--- web/concrete/helpers/file.php
+++ web/concrete/helpers/file.php
@@ -182,6 +182,7 @@ class FileHelper {
 				curl_setopt($curl_handle, CURLOPT_URL, $file);
 				curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, $timeout);
 				curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
 				$contents = curl_exec($curl_handle);
 				$http_code = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
 				if ($http_code == 404) {	
