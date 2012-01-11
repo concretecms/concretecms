2992435320ffaa3beeb87d2d7f835aa940159769
Andrew Embler <andrew@concrete5.org>
2012-01-11 18:37:20 -0500

    fixing error in sitemap saying object when updating meta details

--- web/concrete/js/ccm_app/sitemap.js
+++ web/concrete/js/ccm_app/sitemap.js
@@ -331,7 +331,7 @@ selectLabel = function(e, node) {
 }
 
 ccmSitemapHighlightPageLabel = function(cID, name) {
-	var sp = $("#tree-label" + cID + " span");
+	var sp = $("#tree-label" + cID + " > span");
 
 	if (sp.length == 0) {
 		var sp = $("tr.ccm-list-record[cID=" + cID + "]");
