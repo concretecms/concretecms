2d54fe7fc7ddf6d1fdc304d0d9ba20dc7561817d
klompie <rene@klomp.ws>
2012-01-11 16:46:13 +0100

    Add user to log entry
    Former-commit-id: e7faa83aaaf449495a2510846b807aac664081bd

--- web/concrete/libraries/log.php
+++ web/concrete/libraries/log.php
@@ -25,6 +25,7 @@ class LogEntry extends Object {
 	public function getType() {return $this->logType;}
 	public function getText() {return $this->logText;}
 	public function getID() {return $this->logID;}
+	public function getUser() {return $this->logUser;}
 	
 	public function getTimestamp($type = 'system') {
 		if(ENABLE_USER_TIMEZONES && $type == 'user') {
@@ -132,9 +133,11 @@ class Log {
 
 	
 	public function close() {
-		$v = array($this->log, htmlentities($this->sessionText, ENT_COMPAT, APP_CHARSET), $this->isInternal);
+		$u = new User();
+
+		$v = array($this->log, htmlentities($this->sessionText, ENT_COMPAT, APP_CHARSET), $this->isInternal, $u->getUserID());
 		$db = Loader::db();
-		$db->Execute("insert into Logs (logType, logText, logIsInternal) values (?, ?, ?)", $v);
+		$db->Execute("insert into Logs (logType, logText, logIsInternal, logUser) values (?, ?, ?, ?)", $v);
 		$this->sessionText = '';
 	}
 	
