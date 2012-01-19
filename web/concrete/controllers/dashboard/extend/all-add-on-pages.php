3b892cd9cd429dfef6685cbd2922635d8a477216
Andrew Embler <andrew@concrete5.org>
2012-01-19 15:12:48 -0800

    Adding all dashboard add-on pages

--- web/concrete/controllers/dashboard/extend/all-add-on-pages.php
+++ web/concrete/controllers/dashboard/extend/all-add-on-pages.php
@@ -3,6 +3,18 @@ defined('C5_EXECUTE') or die("Access Denied.");
 class DashboardExtendAllAddOnPagesController extends DashboardBaseController {
 
 	public function view() {
-
+		$pages = array();
+		$dashboard = Page::getByPath('/dashboard');
+		$children = $dashboard->getCollectionChildrenArray(true);
+		foreach($children as $cID) {
+			$nc = Page::getByID($cID, 'ACTIVE');
+			if ($nc->getPackageID() > 0) { 
+				$ncp = new Permissions($nc);
+				if ($ncp->canRead()) {
+					$pages[] = $nc;	
+				}
+			}
+		}
+		$this->set('pages', $pages);
 	}
 }
