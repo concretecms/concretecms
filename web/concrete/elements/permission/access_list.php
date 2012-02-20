f1d12047313cc638200ca36cf671e61a0f6a5b18
Andrew Embler <andrew@concrete5.org>
2012-02-20 15:23:24 -0800

    permissions css
    Former-commit-id: 6adc2addab86f1bcadec6efa16c9f6469482550f

--- web/concrete/elements/permission/access_list.php
+++ web/concrete/elements/permission/access_list.php
@@ -4,10 +4,15 @@ if (!$accessType) {
 	$accessType = PermissionKey::ACCESS_TYPE_INCLUDE;
 }
 ?>
-<table class="zebra-striped">
+<table class="zebra-striped ccm-permission-access-list">
 <tr>
-	<th colspan="3"><?=t('User/Group')?></th>
+	<th colspan="3">
+		<a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/permissions/access_entity?accessType=<?=$accessType?>" dialog-width="500" dialog-height="500" dialog-title="<?=t('Add Access Entity')?>" class="ccm-advanced-search-add-field dialog-launch"><span class="ccm-menu-icon ccm-icon-view"></span><?=t('Add')?></a>
+
+	<?=t('User/Group')?></th>
 </tr>
+<? if (count($list) > 0) { ?>
+
 <? foreach($list as $pa) {
 	$pae = $pa->getAccessEntityObject(); 
 	?>
@@ -15,7 +20,15 @@ if (!$accessType) {
 	<td width="100%"><?=$pae->getAccessEntityLabel()?></td>
 	<td><a href="javascript:void(0)" onclick=""><img src="<?=ASSETS_URL_IMAGES?>/icons/clock<? if (is_object($pa->getPermissionDurationObject())) { ?>_active<? } ?>.png" width="16" height="16" /><a/></td>
 	<td><a href="javascript:void(0)" onclick="ccm_deleteAccessEntityAssignment(<?=$pae->getAccessEntityID()?>)"><img src="<?=ASSETS_URL_IMAGES?>/icons/delete_small.png" width="16" height="16" /><a/></td>
+</tr>
+
 <? } ?>
+
+<? } else { ?>
+	<tr>
+	<td colspan="3"><?=t('None')?></td>
+	</tr>
+<? } ?>
+
 </table>
 
-<a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/permissions/access_entity?accessType=<?=$accessType?>" class="btn small dialog-launch" dialog-width="500" dialog-height="500" dialog-title="<?=t('Add Access Entity')?>"><?=t('Add')?></a>
