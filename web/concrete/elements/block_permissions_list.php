5ae037052d497452a72e1b95640e457a3ec090b8
Andrew Embler <andrew@concrete5.org>
2012-02-28 17:31:38 -0800

    UI work on permissions dialogs, ajax refresh
    Former-commit-id: acb19a35719a86013a4d3c047e71ffda6f0367e3

--- web/concrete/elements/block_permissions_list.php
+++ web/concrete/elements/block_permissions_list.php
@@ -26,72 +26,50 @@ if (!$b->overrideAreaPermissions()) { ?>
 	</div>
 
 <? } ?>
-<table>
+
+
+<?=Loader::element('permission/help');?>
+
+<table class="ccm-permission-grid">
+
 <?
 $permissions = PermissionKey::getList('block');
 foreach($permissions as $pk) { 
 	$pk->setPermissionObject($b);
 
 ?>
-	<tr><td style="white-space: nowrap"><strong><? if ($enablePermissions) { ?><a dialog-width="500" dialog-height="380" class="dialog-launch" dialog-title="<?=t('Permissions')?>" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/edit_block_popup?bID=<?=$b->getBlockID()?>&arHandle=<?=$b->getAreaHandle()?>&cvID=<?=$c->getVersionID()?>&bID=<?=$b->getBlockID()?>&cID=<?=$c->getCollectionID()?>&btask=set_advanced_permissions&pkID=<?=$pk->getPermissionKeyID()?>"><? } ?><?=$pk->getPermissionKeyName()?><? if ($enablePermissions) { ?></a><? } ?></strong></td>
-	<td width="100%">
-	<?
-	$included = $pk->getAssignmentList(BlockPermissionKey::ACCESS_TYPE_INCLUDE);
-	$excluded = $pk->getAssignmentList(BlockPermissionKey::ACCESS_TYPE_EXCLUDE);
-	
-	$includedStr = t('None');
-	$excludedStr = t('None');
-	if (count($included) > 0) {
-		$includedStr = '';
-		for ($i = 0; $i < count($included); $i++) { 
-			$as = $included[$i];
-			$entity = $as->getAccessEntityObject();
-			$includedStr .= $entity->getAccessEntityLabel();
-			if ($i + 1 < count($included)) {
-				$includedStr .= ', ';
-			}
-		}
-	}
-	if (count($excluded) > 0) {
-		$excludedStr = '';
-		for ($i = 0; $i < count($excluded); $i++) { 
-			$as = $excluded[$i];
-			$entity = $as->getAccessEntityObject();
-			$excludedStr .= $entity->getAccessEntityLabel();
-			if ($i + 1 < count($excluded)) {
-				$excludedStr .= ', ';
-			}
-		}
-	}
-	
-	?>
-	
-	
-	<?=t('Included: %s.', $includedStr)?> <?=t('Excluded: %s', $excludedStr)?>
-	</td>
+<tr>
+	<td class="ccm-permission-grid-name"><strong><? if ($enablePermissions) { ?><a dialog-width="500" dialog-height="380" dialog-on-destroy="ccm_refreshBlockPermissions()" class="dialog-launch" dialog-title="<?=t('Permissions')?>" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/edit_block_popup?bID=<?=$b->getBlockID()?>&arHandle=<?=$b->getAreaHandle()?>&cvID=<?=$c->getVersionID()?>&bID=<?=$b->getBlockID()?>&cID=<?=$c->getCollectionID()?>&btask=set_advanced_permissions&pkID=<?=$pk->getPermissionKeyID()?>"><? } ?><?=$pk->getPermissionKeyName()?><? if ($enablePermissions) { ?></a><? } ?></strong></td>
+	<td><?=Loader::element('permission/labels', array('pk' => $pk))?></td>
 </tr>
 <? } ?>
 </table>
+
+
+
+
 </div>
 
 <script type="text/javascript">
 ccm_revertToAreaPermissions = function() {
 	jQuery.fn.dialog.showLoader();
 	$.get('<?=$pk->getPermissionKeyToolsURL("revert_to_area_permissions")?>&bID=<?=$b->getBlockID()?>&cvID=<?=$c->getVersionID()?>&arHandle=<?=urlencode($b->getAreaHandle())?>&cID=<?=$c->getCollectionID()?>', function() { 
-		$.get('<?=REL_DIR_FILES_TOOLS_REQUIRED?>/edit_block_popup?btask=groups&bID=<?=$b->getBlockID()?>&cvID=<?=$c->getVersionID()?>&arHandle=<?=urlencode($b->getAreaHandle())?>&cID=<?=$c->getCollectionID()?>', function(r) { 
-			jQuery.fn.dialog.replaceTop(r);
-			jQuery.fn.dialog.hideLoader();
-		});
+		ccm_refreshBlockPermissions();
 	});
 }
 
 ccm_setBlockPermissionsToOverride = function() {
 	jQuery.fn.dialog.showLoader();
 	$.get('<?=$pk->getPermissionKeyToolsURL("override_area_permissions")?>&bID=<?=$b->getBlockID()?>&cvID=<?=$c->getVersionID()?>&arHandle=<?=urlencode($b->getAreaHandle())?>&cID=<?=$c->getCollectionID()?>', function() { 
-		$.get('<?=REL_DIR_FILES_TOOLS_REQUIRED?>/edit_block_popup?btask=groups&bID=<?=$b->getBlockID()?>&cvID=<?=$c->getVersionID()?>&arHandle=<?=urlencode($b->getAreaHandle())?>&cID=<?=$c->getCollectionID()?>', function(r) { 
-			jQuery.fn.dialog.replaceTop(r);
-			jQuery.fn.dialog.hideLoader();
-		});
+		ccm_refreshBlockPermissions();
+	});
+}
+
+ccm_refreshBlockPermissions = function() {
+	jQuery.fn.dialog.showLoader();
+	$.get('<?=REL_DIR_FILES_TOOLS_REQUIRED?>/edit_block_popup?btask=groups&bID=<?=$b->getBlockID()?>&cvID=<?=$c->getVersionID()?>&arHandle=<?=urlencode($b->getAreaHandle())?>&cID=<?=$c->getCollectionID()?>', function(r) { 
+		jQuery.fn.dialog.replaceTop(r);
+		jQuery.fn.dialog.hideLoader();
 	});
 }
 
