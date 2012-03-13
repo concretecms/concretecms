3853ceb29189a982fd57500fb2c3781e68a0fbf5
Greg Joyce <greg@concretecms.com>
2012-03-12 17:32:40 -0700

    updated single page edit to not look as bad in greek yogurt.
    Former-commit-id: a62e0c758dd745f8b97ae3c816a7c4984ab4ecac

--- web/concrete/single_pages/profile/edit.php
+++ web/concrete/single_pages/profile/edit.php
@@ -1,41 +1,41 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
 <div id="ccm-profile-wrapper">
-    <? Loader::element('profile/sidebar', array('profile'=> $ui)); ?>    
+    <?php  Loader::element('profile/sidebar', array('profile'=> $ui)); ?>    
     <div id="ccm-profile-body">	
 
-		<? if (isset($error) && $error->has()) {
+		<?php  if (isset($error) && $error->has()) {
             $error->output();
         } else if (isset($message)) { ?>
-            <div class="message"><?=$message?></div>
+            <div class="message"><?php echo $message?></div>
             <script type="text/javascript">
             $(function() {
                 $("div.message").show('highlight', {}, 500);
             });
             </script>
-        <? } ?>
+        <?php  } ?>
         
         
-        <h1><?=t('Edit Profile')?></h1>
+        <h1 class="profilin"><?php echo t('Edit Profile')?></h1>
         <div class="ccm-form">
-            <form method="post" action="<?=$this->action('save')?>" id="profile-edit-form" enctype="multipart/form-data">
-            <? $attribs = UserAttributeKey::getEditableInProfileList(); 
+            <form method="post" action="<?php echo $this->action('save')?>" id="profile-edit-form" enctype="multipart/form-data">
+            <?php  $attribs = UserAttributeKey::getEditableInProfileList(); 
             if(is_array($attribs) && count($attribs)) { 
             ?>
                 <fieldset>
                 <div class="ccm-profile-attribute">
-                    <?=$form->label('uEmail', t('Email'))?> <span class="ccm-required">*</span><br/>
-                    <?=$form->text('uEmail',$ui->getUserEmail())?>
+                    <?php echo $form->label('uEmail', t('Email'))?> <span class="ccm-required">*</span><br/>
+                    <?php echo $form->text('uEmail',$ui->getUserEmail())?>
                 </div>
-                <? if(ENABLE_USER_TIMEZONES) { ?>
+                <?php  if(ENABLE_USER_TIMEZONES) { ?>
                     <div class="ccm-profile-attribute">
-                        <?= $form->label('uTimezone', t('Time Zone'))?> <span class="ccm-required">*</span><br/>
-                        <?= $form->select('uTimezone', 
+                        <?php echo  $form->label('uTimezone', t('Time Zone'))?> <span class="ccm-required">*</span><br/>
+                        <?php echo  $form->select('uTimezone', 
 							$date->getTimezones(), 
 							($ui->getUserTimezone()?$ui->getUserTimezone():date_default_timezone_get())
 					); ?>
                     </div>
- 				<? } ?>               
-                <?
+ 				<?php  } ?>               
+                <?php 
                 $af = Loader::helper('form/attribute');
                 $af->setAttributeObject($ui);
                 foreach($attribs as $ak) {
@@ -44,21 +44,21 @@
                 	print '</div>';
                 } ?>
                 </fieldset>
-            <? } ?>
-            <h3><?=t('Change Password')?></h3>
-            <p><?=t("Leave blank if you'd like your password to remain the same")?></p>
+            <?php  } ?>
+            <h3><?php echo t('Change Password')?></h3>
+            <p><?php echo t("Leave this blank to keep your current password.")?></p>
             <fieldset>
                 <div>
-                    <?=$form->label('uPasswordNew', t('New Password'))?>
-                    <?=$form->password('uPasswordNew')?>
+                    <?php echo $form->label('uPasswordNew', t('New Password'))?>
+                    <?php echo $form->password('uPasswordNew')?>
                 </div>	
                 <div>
-                    <?=$form->label('uPasswordNewConfirm', t('Confirm New Password'))?>
-                    <?=$form->password('uPasswordNewConfirm')?>
+                    <?php echo $form->label('uPasswordNewConfirm', t('Confirm New Password'))?>
+                    <?php echo $form->password('uPasswordNewConfirm')?>
                 </div>   
             </fieldset>
             <div class="spacer" style="margin-top:20px">&nbsp;</div>
-            <?=$form->submit('save', t('Save'))?>
+            <?php echo $form->submit('save', t('Save'))?>
             </form>
             <div class="spacer">&nbsp;</div>
         </div>
