ad9389b92ba0c300a16b70255bb175e311f2450e
Jordan Lev <github@jordanlev.com>
2012-04-05 18:42:57 -0500

    refactored autonav templates
    Former-commit-id: 889cdedfa04db178779e10cd39ea94f08b4acfa6

--- web/concrete/blocks/autonav/templates/header_menu.php
+++ web/concrete/blocks/autonav/templates/header_menu.php
@@ -1,56 +1,27 @@
-<?
-	defined('C5_EXECUTE') or die("Access Denied.");
-	$aBlocks = $controller->generateNav();
-	$c = Page::getCurrentPage();
-	echo("<ul class=\"nav-header\">");
+<? defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
-	$nh = Loader::helper('navigation');
+<ul class="nav-header">
 
-	$isFirst = true;
-	foreach($aBlocks as $ni) {
-		$_c = $ni->getCollectionObject();
-		if (!$_c->getCollectionAttributeValue('exclude_nav')) {
-
-			$target = $ni->getTarget();
-			if ($target != '') {
-				$target = 'target="' . $target . '"';
-			}
-
-			if ($ni->isActive($c) || strpos($c->getCollectionPath(), $_c->getCollectionPath()) === 0) {
-				$navSelected='nav-selected';
-			} else {
-				$navSelected = '';
-			}
-			
-			$pageLink = false;
-			
-			if ($_c->getCollectionAttributeValue('replace_link_with_first_in_nav')) {
-				$subPage = $_c->getFirstChild();
-				if ($subPage instanceof Page) {
-					$pageLink = $nh->getLinkToCollection($subPage);
-				}
-			}
-			
-			if (!$pageLink) {
-				$pageLink = $ni->getURL();
-			}
-
-			if ($isFirst) $isFirstClass = 'first';
-			else $isFirstClass = '';
-			
-			echo '<li class="'.$navSelected.' '.$isFirstClass.'">';
-			
-			if ($c->getCollectionID() == $_c->getCollectionID()) { 
-				echo('<a class="nav-selected" href="' . $pageLink . '"  ' . $target . '>' . $ni->getName() . '</a>');
-			} else {
-				echo('<a href="' . $pageLink . '"  ' . $target . '>' . $ni->getName() . '</a>');
-			}	
-			
-			echo('</li>');
-			$isFirst = false;			
-		}
-	}
+<? foreach ($navItems as $ni) {
 	
-	echo('</ul>');
-	echo('<div class="ccm-spacer">&nbsp;</div>');
-?>
+	$classes = array();
+	if ($ni->isCurrent) {
+		$classes[] = 'nav-selected';
+	}
+	if ($ni->inPath) {
+		$classes[] = 'nav-path-selected';
+	}
+	if ($ni->isFirst) {
+		$classes[] = 'first';
+	}
+	$classes = implode(" ", $classes);
+	?>
+	
+	<li class="<?=$classes?>">
+		<a class="<?=$classes?>" href="<?=$ni->url?>" target="<?=$ni->target?>"><?=$ni->name?></a>
+	</li>
+<? } ?>
+
+</ul>
+
+<div class="ccm-spacer">&nbsp;</div>
--- web/concrete/blocks/autonav/view.php
+++ web/concrete/blocks/autonav/view.php
@@ -1,88 +1,130 @@
-<?
-	defined('C5_EXECUTE') or die("Access Denied.");
-	$aBlocks = $controller->generateNav();
-	$c = Page::getCurrentPage();
-	$containsPages = false;
-	
-	$nh = Loader::helper('navigation');
-	
-	//this will create an array of parent cIDs 
-	$inspectC=$c;
-	$selectedPathCIDs=array( $inspectC->getCollectionID() );
-	$parentCIDnotZero=true;	
-	while($parentCIDnotZero){
-		$cParentID=$inspectC->cParentID;
-		if(!intval($cParentID)){
-			$parentCIDnotZero=false;
-		}else{
-			$selectedPathCIDs[]=$cParentID;
-			$inspectC=Page::getById($cParentID);
-		}
-	} 	
-	
-	foreach($aBlocks as $ni) {
-		$_c = $ni->getCollectionObject();
-		if (!$_c->getCollectionAttributeValue('exclude_nav')) {
-			
-			
-			$target = $ni->getTarget();
-			if ($target != '') {
-				$target = 'target="' . $target . '"';
-			}
-			if (!$containsPages) {
-				// this is the first time we've entered the loop so we print out the UL tag
-				echo("<ul class=\"nav\">");
-			}
-			
-			$containsPages = true;
-			
-			$thisLevel = $ni->getLevel();
-			if ($thisLevel > $lastLevel) {
-				echo("<ul>");
-			} else if ($thisLevel < $lastLevel) {
-				for ($j = $thisLevel; $j < $lastLevel; $j++) {
-					if ($lastLevel - $j > 1) {
-						echo("</li></ul>");
-					} else {
-						echo("</li></ul></li>");
-					}
-				}
-			} else if ($i > 0) {
-				echo("</li>");
-			}
+<? defined('C5_EXECUTE') or die(_("Access Denied."));
+/**
+ * The $navItems is provided by the controller. It is an array of objects, each representing a nav menu item.
+ * The nav items array is a "flattened" one-dimensional list of all nav items -- it is not hierarchical,
+ * even though it can represent a hierarchical menu structure.
+ * You can look to various properties of each navItem object to determine its place in the menu hierarchy
+ * (see below, for example $navItem->level, $navItem->subDepth, $navItem->hasSubmenu, etc.)
+ * Items are ordered with the first top-level item first, followed by its sub-items, etc.
+ * 
+ * Each nav item object contains the following information:
+ *	$navItem->url        : URL to the page
+ *	$navItem->name       : page title (already escaped for html output)
+ *	$navItem->target     : link target (e.g. "_self" or "_blank")
+ *	$navItem->level      : number of levels deep the current menu item is from the top (so top-level nav items are 1, their sub-items are 2, etc.)
+ *	$navItem->subDepth   : number of levels deep the current menu item is *compared to the next item in the list* (useful for determining how many <ul>'s to close in a nested list)
+ *	$navItem->hasSubmenu : true/false -- if this item has one or more sub-items (sometimes useful for css styling)
+ *	$navItem->isFirst    : true/false -- if this is the first nav item *in its level* (for example, the first sub-item of a top-level item is TRUE)
+ *	$navItem->isLast     : true/false -- if this is the last nav item *in its level* (for example, the last sub-item of a top-level item is TRUE)
+ *	$navItem->isCurrent  : true/false -- if this nav item represents the page currently being viewed
+ *	$navItem->inPath     : true/false -- if this nav item represents a parent page of the page currently being viewed (also true for the page currently being viewed)
+ *	$navItem->attrClass  : Value of the 'nav_item_class' custom page attribute (if it exists and is set)
+ *	$navItem->isEnabled  : true/false -- if the nav item should link to its page (this is only false if the 'disable_nav_link' custom page attribute is checked)
+ *	$navItem->isHome     : true/false -- if this nav item represents the home page
+ *	$navItem->CID        : collection id of the page this nav item represents
+ *	$navItem->cObj       : collection object of the page this nav item represents (use this if you need to access page properties and attributes that aren't already available in the $navItem object)
+ */
 
-			$pageLink = false;
-			
-			if ($_c->getCollectionAttributeValue('replace_link_with_first_in_nav')) {
-				$subPage = $_c->getFirstChild();
-				if ($subPage instanceof Page) {
-					$pageLink = $nh->getLinkToCollection($subPage);
-				}
-			}
-			
-			if (!$pageLink) {
-				$pageLink = $ni->getURL();
-			}
 
-			if ($c->getCollectionID() == $_c->getCollectionID()) { 
-				echo('<li class="nav-selected nav-path-selected"><a class="nav-selected nav-path-selected" ' . $target . ' href="' . $pageLink . '">' . $ni->getName() . '</a>');
-			} elseif ( in_array($_c->getCollectionID(),$selectedPathCIDs) && ($_c->getCollectionID() != HOME_CID) ) {
-				echo('<li class="nav-path-selected"><a class="nav-path-selected" href="' . $pageLink . '" ' . $target . '>' . $ni->getName() . '</a>');
-			} else {
-				echo('<li><a href="' . $pageLink . '" ' . $target . ' >' . $ni->getName() . '</a>');
-			}	
-			$lastLevel = $thisLevel;
-			$i++;
-			
-			
-		}
-	}
-	
-	$thisLevel = 0;
-	if ($containsPages) {
-		for ($i = $thisLevel; $i <= $lastLevel; $i++) {
-			echo("</li></ul>");
-		}
+/** NOTE: For extra functionality, you can add the following page attributes to your site (via Dashboard "Page Attributes")
+ *
+ * 1) Handle: replace_link_with_first_in_nav
+ *    Type: Checkbox
+ *    Functionality: If a page has this checked, clicking on it in the nav menu will go to its first child (sub-page) instead.
+ *
+ * 2) Handle: exclude_subpages_from_nav
+ *    Type: Checkbox
+ *    Functionality: If a page has this checked, all of that pages children (sub-pages) will be excluded from the nav menu (but the page itself will be included).
+ *
+ * 3) Handle: nav_item_class
+ *    Type: Text
+ *    Functionality: Whatever is entered into this textbox will be outputted as an additional CSS class for that page's nav item.
+ *
+ * 4) Handle: disable_nav_link
+ *    Type: Checkbox
+ *    Functionality: If a page has this checked, it will appear in the nav menu but will not be "clickable" (will not link to any page).
+ */
+
+
+/*** STEP 1 of 2: Determine all CSS classes (only 2 are enabled by default, but you can un-comment other ones or add your own) ***/
+foreach ($navItems as $ni) {
+	$classes = array();
+
+	if ($ni->isCurrent) {
+		//class for the page currently being viewed
+		$classes[] = 'nav-selected';
 	}
 
-?>
+	if ($ni->inPath) {
+		//class for parent items of the page currently being viewed
+		$classes[] = 'nav-path-selected';
+	}
+
+	/*
+	if ($ni->isFirst) {
+		//class for the first item in each menu section (first top-level item, and first item of each dropdown sub-menu)
+		$classes[] = 'nav-first';
+	}
+	*/
+
+	/*
+	if ($ni->isLast) {
+		//class for the last item in each menu section (last top-level item, and last item of each dropdown sub-menu)
+		$classes[] = 'nav-last';
+	}
+	*/
+
+	/*
+	if ($ni->hasSubmenu) {
+		//class for items that have dropdown sub-menus
+		$classes[] = 'nav-dropdown';
+	}
+	*/
+
+	/*
+	if (!empty($ni->attrClass)) {
+		//class that can be set by end-user via the 'nav_item_class' custom page attribute
+		$classes[] = $ni->attrClass;
+	}
+	*/
+
+	/*
+	if ($ni->isHome) {
+		//home page
+		$classes[] = 'nav-home';
+	}
+	*/
+
+	/*
+	//unique class for every single menu item
+	$classes[] = 'nav-item-' . $ni->CID;
+	*/
+
+	//Put all classes together into one space-separated string
+	$ni->classes = implode(" ", $classes);
+}
+
+
+//*** Step 2 of 2: Output menu HTML ***/
+
+echo '<ul class="nav">'; //opens the top-level menu
+
+foreach ($navItems as $ni) {
+
+	echo '<li class="' . $ni->classes . '">'; //opens a nav item
+
+	if ($ni->isEnabled) {
+		echo '<a href="' . $ni->url . '" target="' . $ni->target . '" class="' . $ni->classes . '">' . $ni->name . '</a>';
+	} else {
+		echo '<span class="' . $ni->classes . '">' . $ni->name . '</span>';
+	}
+
+	if ($ni->hasSubmenu) {
+		echo '<ul>'; //opens a dropdown sub-menu
+	} else {
+		echo '</li>'; //closes a nav item
+		echo str_repeat('</ul></li>', $ni->subDepth); //closes dropdown sub-menu(s) and their top-level nav item(s)
+	}
+}
+
+echo '</ul>'; //closes the top-level menu
