012f590c56bea7e0aa4696b3cb1906bac08802e5
Andrew Embler <andrew@concrete5.org>
2012-05-22 10:28:33 -0700

    more bootstrap2 tomfoolery

--- web/concrete/blocks/autonav/controller.php
+++ web/concrete/blocks/autonav/controller.php
@@ -178,6 +178,7 @@
 		protected $btCacheBlockOutputOnPost = true;
 		protected $btCacheBlockOutputForRegisteredUsers = false;
 		protected $btCacheBlockOutputLifetime = 300;
+		protected $btWrapperClass = 'ccm-ui';
 		
 		protected $btExportPageColumns = array('displayPagesCID');
 		
