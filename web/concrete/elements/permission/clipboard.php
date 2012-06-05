1c99331b9ce149eeda6441879282dd7cff62252f
Andrew Embler <andrew@concrete5.org>
2012-06-04 18:09:23 -0700

    permission

---
+++ web/concrete/elements/permission/clipboard.php
@@ -0,0 +1,11 @@
+<? defined('C5_EXECUTE') or die("Access Denied."); ?>
+<div class="btn-group dropup">
+  <button class="btn btn-mini dropdown-toggle" data-toggle="dropdown">
+<?=t('Copy &amp; Apply Permissions')?>    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+	  <li><a href=""><i class="icon-plus"></i> <?=t('Copy Permissions')?></a></li>
+	  
+  </ul>
+</div>
+
