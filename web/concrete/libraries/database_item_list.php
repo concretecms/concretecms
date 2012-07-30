1446777bbdb98ab27c0787a70990d5a2406b2849
Andrew Embler <andrew@concrete5.org>
2012-07-30 16:05:02 -0700

    database item list column bug fix
    Former-commit-id: 38de0a7fcd3afa00b2bc8bbaad232750fa11cf0d

--- web/concrete/libraries/database_item_list.php
+++ web/concrete/libraries/database_item_list.php
@@ -6,5 +6,5 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 class DatabaseItemList extends Concrete5_Library_DatabaseItemList {}
 class DatabaseItemListColumn extends Concrete5_Library_DatabaseItemListColumn {}
-class DatabaseItemListAttributeKeyColumn extends DatabaseItemListColumn {}
+class DatabaseItemListAttributeKeyColumn extends Concrete5_Library_DatabaseItemListAttributeKeyColumn {}
 class DatabaseItemListColumnSet extends Concrete5_Library_DatabaseItemListColumnSet {}
