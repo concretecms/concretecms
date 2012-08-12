416a58fcf9e3303f97022d20546a8455fc242b4c
Andrew Embler <andrew@concrete5.org>
2012-08-12 11:52:38 -0700

    sitemap bug fixes

--- web/concrete/core/controllers/single_pages/upgrade.php
+++ web/concrete/core/controllers/single_pages/upgrade.php
@@ -34,14 +34,14 @@ class Concrete5_Controller_Upgrade extends Controller {
 			$sav = $this->site_version;
 	
 			if (!$sav) {
-				$message = t('Unable to determine your current version of Concrete. Upgrading cannot continue.');
+				$message = t('Unable to determine your current version of concrete5. Upgrading cannot continue.');
 			} else 	if (version_compare($sav, APP_VERSION, '>')) {
 				$message = t('Upgrading from <b>%s</b>', $sav) . '<br/>';
 				$message .= t('Upgrading to <b>%s</b>', APP_VERSION) . '<br/><br/>';
-				$message .= t('Your current website uses a version of Concrete5 greater than this one. You cannot upgrade.');
+				$message .= t('Your current website uses a version of concrete5 greater than this one. You cannot upgrade.');
 				$this->set('message', $message);
 			} else if (version_compare($sav, APP_VERSION, '=')) {
-				$this->set('message', t('Your site is already up to date! The current version of Concrete5 is <b>%s</b>. You should remove this file for security.', APP_VERSION));
+				$this->set('message', t('Your site is already up to date! The current version of concrete5 is <b>%s</b>. You should remove this file for security.', APP_VERSION));
 			} else {
 				if ($this->post('do_upgrade')) {
 					$this->do_upgrade();
