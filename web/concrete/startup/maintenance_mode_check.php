776adae51bc8964f793b5dfced90b0c729452253
Andrew Embler <andrew@concrete5.org>
2012-08-15 10:33:14 -0700

    maintenance mode tweaks

--- web/concrete/startup/maintenance_mode_check.php
+++ web/concrete/startup/maintenance_mode_check.php
@@ -3,7 +3,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 if ((!$c->isAdminArea()) && ($c->getCollectionPath() != '/login')) {
 
 	$smm = Config::get('SITE_MAINTENANCE_MODE');
-	if ($smm == 1) {
+	if ($smm == 1 && ($_SERVER['REQUEST_METHOD'] != 'POST' || Loader::helper('validation/token')->validate() == false)) {
 		$v = View::getInstance();
 		$v->render('/maintenance_mode/');
 		exit;
