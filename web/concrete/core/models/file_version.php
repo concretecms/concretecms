c62fed7b9a900db6ee636bb3df62844fcf89aa04
KorvinSzanto <korvin@concrete5.org>
2012-08-24 13:50:29 -0700

    Actually force download files
    Currently, downloads aren't forced unless they are in an inaccessible file system. This becomes apparent when Document Library doesn't force download any files that the browser can render / the server knows about.

--- web/concrete/core/models/file_version.php
+++ web/concrete/core/models/file_version.php
@@ -1,30 +1,30 @@
 <?
 
 class Concrete5_Model_FileVersion extends Object {
-	
-	private $numThumbnailLevels = 3; 
+
+	private $numThumbnailLevels = 3;
 	private $attributes = array();
-	
+
 	// Update type constants
 	const UT_REPLACE_FILE = 1;
 	const UT_TITLE = 2;
 	const UT_DESCRIPTION = 3;
 	const UT_TAGS = 4;
 	const UT_EXTENDED_ATTRIBUTE = 5;
-	
+
 	public function getFileID() {return $this->fID;}
 	public function getFileVersionID() {return $this->fvID;}
 	public function getPrefix() {return $this->fvPrefix;}
 	public function getFileName() {return $this->fvFilename;}
 	public function getTitle() {return $this->fvTitle;}
-	public function getTags() {return $this->fvTags;} 
+	public function getTags() {return $this->fvTags;}
 	public function getDescription() {return $this->fvDescription;}
 	public function isApproved() {return $this->fvIsApproved;}
-	
+
 	public function getGenericTypeText() {
 		$to = $this->getTypeObject();
 		return $to->getGenericTypeText( $to->getGenericType() );
-	}	
+	}
 
 	/**
 	 * returns the File object associated with this FileVersion object
@@ -34,19 +34,19 @@ class Concrete5_Model_FileVersion extends Object {
 		$fo = File::getByID($this->fID);
 		return $fo;
 	}
-	
+
 	//returns an array of tags, instead of a string
 	public function getTagsList(){
 		$tags=explode("\n",str_replace("\r","\n",trim($this->getTags())));
 		$clean_tags=array();
 		foreach($tags as $tag){
-			if( strlen(trim($tag)) ) 
+			if( strlen(trim($tag)) )
 				$clean_tags[]=trim($tag);
 		}
 		return $clean_tags;
-	} 
-	
-	/** 
+	}
+
+	/**
 	 * Gets an associative array of all attributes for a file version
 	 */
 	public function getAttributeList() {
@@ -56,12 +56,12 @@ class Concrete5_Model_FileVersion extends Object {
 		$attributes = FileAttributeKey::getAttributes($this->fID, $this->fvID);
 		return $attributes;
 	}
-	
-	/** 
+
+	/**
 	 * Gets an attribute for the file. If "nice mode" is set, we display it nicely
-	 * for use in the file attributes table 
+	 * for use in the file attributes table
 	 */
-	 
+
 	public function getAttribute($ak, $mode = false) {
 		$ak = (is_object($ak)) ? $ak->getAttributeKeyHandle() : $ak;
 		return $this->attributes->getAttribute($ak, $mode = false);
@@ -74,13 +74,13 @@ class Concrete5_Model_FileVersion extends Object {
 		$ext = $fh->getExtension($this->fvFilename);
 		return $h->mimeFromExtension($ext);
 	}
-	
+
 	public function populateAttributes() {
 		// load the attributes for a particular version object
-		Loader::model('attribute/categories/file');			
+		Loader::model('attribute/categories/file');
 		$this->attributes = FileAttributeKey::getAttributes($this->fID, $this->fvID);
 	}
-	
+
 	public function getSize() {
 		return round($this->fvSize / 1024) . t('KB');
 	}
@@ -90,16 +90,16 @@ class Concrete5_Model_FileVersion extends Object {
 	public function getAuthorName() {
 		return $this->fvAuthorName;
 	}
-	
+
 	public function getAuthorUserID() {
 		return $this->fvAuthorUID;
 	}
-	
+
 	/**
 	 * Gets the date a file version was added
 	 * if user is specified, returns in the current user's timezone
 	 * @param string $type (system || user)
-	 * @return string date formated like: 2009-01-01 00:00:00 
+	 * @return string date formated like: 2009-01-01 00:00:00
 	*/
 	function getDateAdded($type = 'system') {
 		if(ENABLE_USER_TIMEZONES && $type == 'user') {
@@ -109,11 +109,11 @@ class Concrete5_Model_FileVersion extends Object {
 			return $this->fvDateAdded;
 		}
 	}
-	
+
 	public function getExtension() {
 		return $this->fvExtension;
 	}
-	
+
 	public function logVersionUpdate($updateTypeID, $updateTypeAttributeID = 0) {
 		$db = Loader::db();
 		$db->Execute('insert into FileVersionLog (fID, fvID, fvUpdateTypeID, fvUpdateTypeAttributeID) values (?, ?, ?, ?)', array(
@@ -123,8 +123,8 @@ class Concrete5_Model_FileVersion extends Object {
 			$updateTypeAttributeID
 		));
 	}
-	
-	/** 
+
+	/**
 	 * Takes the current value of the file version and makes a new one with the same values
 	 */
 	public function duplicate() {
@@ -143,7 +143,7 @@ class Concrete5_Model_FileVersion extends Object {
 		$data['fvDateAdded'] = $date;
 		$u = new User();
 		$data['fvAuthorUID'] = $u->getUserID();
-		
+
 		// If This version is the approved version, we approve the new one.
 		if ($this->isApproved()) {
 			$data['fvIsApproved'] = 1;
@@ -156,12 +156,12 @@ class Concrete5_Model_FileVersion extends Object {
 		$i = 0;
 		$data2 = array();
 		foreach($data as $key => $value) {
-			if (!is_integer($key)) {	
+			if (!is_integer($key)) {
 				$data2[$key] = $value;
 			}
 		}
-		
-		foreach($data2 as $key => $value) {		
+
+		foreach($data2 as $key => $value) {
 			$fields .= $key;
 			$questions .= '?';
 			if (($i + 1) < count($data2)) {
@@ -170,18 +170,18 @@ class Concrete5_Model_FileVersion extends Object {
 			}
 			$i++;
 		}
-		
+
 		$db->Execute("insert into FileVersions (" . $fields . ") values (" . $questions . ")", $data2);
-		
-		
+
+
 		$this->deny();
-		
+
 		$r = $db->Execute('select fvID, akID, avID from FileAttributeValues where fID = ? and fvID = ?', array($this->getFileID(), $this->fvID));
 		while ($row = $r->fetchRow()) {
 			$db->Execute("insert into FileAttributeValues (fID, fvID, akID, avID) values (?, ?, ?, ?)", array(
-				$this->fID, 
+				$this->fID,
 				$fvID,
-				$row['akID'], 
+				$row['akID'],
 				$row['avID']
 			));
 		}
@@ -189,24 +189,24 @@ class Concrete5_Model_FileVersion extends Object {
 		Events::fire('on_file_version_duplicate', $fv2);
 		return $fv2;
 	}
-	
-	
+
+
 	public function getType() {
 		$ftl = $this->getTypeObject();
 		if (is_object($ftl)) {
 			return $ftl->getName();
 		}
 	}
-	
+
 	public function getTypeObject() {
 		$fh = Loader::helper('file');
 		$ext = $fh->getExtension($this->fvFilename);
-		
+
 		$ftl = FileTypeList::getType($ext);
 		return $ftl;
 	}
-	
-	/** 
+
+	/**
 	 * Returns an array containing human-readable descriptions of everything that happened in this version
 	 */
 	public function getVersionLogComments() {
@@ -243,7 +243,7 @@ class Concrete5_Model_FileVersion extends Object {
 		}
 		return $updates1;
 	}
-	
+
 	public function updateTitle($title) {
 		$db = Loader::db();
 		$db->Execute("update FileVersions set fvTitle = ? where fID = ? and fvID = ?", array($title, $this->getFileID(), $this->getFileVersionID()));
@@ -282,7 +282,7 @@ class Concrete5_Model_FileVersion extends Object {
 		$this->logVersionUpdate(FileVersion::UT_REPLACE_FILE);
 		$this->fvFilename = $filename;
 		$this->fvPrefix = $prefix;
-		
+
 		$fo = $this->getFile();
 		$fo->refreshCache();
 	}
@@ -319,25 +319,25 @@ class Concrete5_Model_FileVersion extends Object {
 		$fo->reindex();
 		unset($ak);
 	}
-	
-	
-	/** 
+
+
+	/**
 	 * Removes a version of a file. Note, does NOT remove the file because we don't know where the file might elsewhere be used/referenced.
 	 */
 	public function delete() {
 		if ($this->fvIsApproved == 1) {
 			return false; // can only delete non-live files
 		}
-		
+
 		$db = Loader::db();
 		// now from the DB
 		$db->Execute("delete from FileVersions where fID = ? and fvID = ?", array($this->fID, $this->fvID));
 		$db->Execute("delete from FileAttributeValues where fID = ? and fvID = ?", array($this->fID, $this->fvID));
 		$db->Execute("delete from FileVersionLog where fID = ? and fvID = ?", array($this->fID, $this->fvID));
 	}
-	
-	
-	/** 
+
+
+	/**
 	 * Returns a full filesystem path to the file on disk.
 	 */
 	public function getPath() {
@@ -351,34 +351,34 @@ class Concrete5_Model_FileVersion extends Object {
 		}
 		return $path;
 	}
-	
-	/** 
+
+	/**
 	 * Returns a full URL to the file on disk
 	 */
 	public function getURL() {
-		return BASE_URL . $this->getRelativePath();	
+		return BASE_URL . $this->getRelativePath();
 	}
-	
-	/** 
-	 * Returns a URL that can be used to download the file. This passes through the download_file single page.
+
+	/**
+	 * Returns a URL that can be used to download the file. This passes through the download_file single page, force_download method.
 	 */
 	public function getDownloadURL() {
 		$c = Page::getCurrentPage();
 		if($c instanceof Page) {
 			$cID = $c->getCollectionID();
-		} else { 
+		} else {
 			$cID = 0;
 		}
-		return BASE_URL . View::url('/download_file', $this->getFileID(),$cID);
+		return BASE_URL . View::url('/download_file','force_download', $this->getFileID(),$cID);
 	}
-	
+
 	public function getRelativePath($fullurl = false) {
 		$f = Loader::helper('concrete/file');
 		if ($this->fslID > 0) {
 			$c = Page::getCurrentPage();
 			if($c instanceof Page) {
 				$cID = $c->getCollectionID();
-			} else { 
+			} else {
 				$cID = 0;
 			}
 			$path = BASE_URL . View::url('/download_file', 'view_inline', $this->getFileID(),$cID);
@@ -388,16 +388,16 @@ class Concrete5_Model_FileVersion extends Object {
 			} else {
 				$path = $f->getFileRelativePath($this->fvPrefix, $this->fvFilename );
 			}
-		}		
+		}
 		return $path;
 	}
-	
+
 	public function getThumbnailPath($level) {
 		$f = Loader::helper('concrete/file');
 		$path = $f->getThumbnailSystemPath($this->fvPrefix, $this->fvFilename, $level);
 		return $path;
 	}
-	
+
 	public function getThumbnailSRC($level) {
 		if ($this->{"fvHasThumbnail{$level}"}) {
 			$f = Loader::helper('concrete/file');
@@ -405,11 +405,11 @@ class Concrete5_Model_FileVersion extends Object {
 			return $path;
 		}
 	}
-	
+
 	public function hasThumbnail($level) {
 		return $this->{"fvHasThumbnail{$level}"};
 	}
-	
+
 	public function getThumbnail($level, $fullImageTag = true) {
 		$html = Loader::helper('html');
 		if ($this->{"fvHasThumbnail{$level}"}) {
@@ -423,31 +423,31 @@ class Concrete5_Model_FileVersion extends Object {
 			return $ft->getThumbnail($level, $fullImageTag);
 		}
 	}
-	
-	// 
+
+	//
 	public function refreshThumbnails($refreshCache = true) {
 		$db = Loader::db();
 		$f = Loader::helper('concrete/file');
 		for ($i = 1; $i <= $this->numThumbnailLevels; $i++) {
-			$path = $f->getThumbnailSystemPath($this->fvPrefix, $this->fvFilename, $i);	
+			$path = $f->getThumbnailSystemPath($this->fvPrefix, $this->fvFilename, $i);
 			$hasThumbnail = 0;
 			if (file_exists($path)) {
 				$hasThumbnail = 1;
 			}
 			$db->Execute("update FileVersions set fvHasThumbnail" . $i . "= ? where fID = ? and fvID = ?", array($hasThumbnail, $this->fID, $this->fvID));
 		}
-		
+
 		if ($refreshCache) {
 			$fo = $this->getFile();
 			$fo->refreshCache();
 		}
 	}
-	
+
 	// update types
 	const UT_NEW = 0;
-	
-	
-	/** 
+
+
+	/**
 	 * Responsible for taking a particular version of a file and rescanning all its attributes
 	 * This will run any type-based import routines, and store those attributes, generate thumbnails,
 	 * etc...
@@ -457,30 +457,30 @@ class Concrete5_Model_FileVersion extends Object {
 		$ext = $fh->getExtension($this->fvFilename);
 		$ftl = FileTypeList::getType($ext);
 		$db = Loader::db();
-		
+
 		if (!file_exists($this->getPath())) {
 			return File::F_ERROR_FILE_NOT_FOUND;
 		}
-		
+
 		$size = filesize($this->getPath());
-		
+
 		$title = ($firstRun) ? $this->getFilename() : $this->getTitle();
-		
+
 		$db->Execute('update FileVersions set fvExtension = ?, fvType = ?, fvTitle = ?, fvSize = ? where fID = ? and fvID = ?',
 			array($ext, $ftl->getGenericType(), $title, $size, $this->getFileID(), $this->getFileVersionID())
 		);
 		if (is_object($ftl)) {
 			if ($ftl->getCustomImporter() != false) {
 				Loader::library('file/inspector');
-				
+
 				$db->Execute('update FileVersions set fvGenericType = ? where fID = ? and fvID = ?',
 					array($ftl->getGenericType(), $this->getFileID(), $this->getFileVersionID())
 				);
-				
+
 				// we have a custom library script that handles this stuff
 				$cl = $ftl->getCustomInspector();
 				$cl->inspect($this);
-				
+
 			}
 		}
 		$this->refreshThumbnails(false);
@@ -492,12 +492,12 @@ class Concrete5_Model_FileVersion extends Object {
 	public function createThumbnailDirectories(){
 		$f = Loader::helper('concrete/file');
 		for ($i = 1; $i <= $this->numThumbnailLevels; $i++) {
-			$path = $f->getThumbnailSystemPath($this->fvPrefix, $this->fvFilename, $i, true);	
+			$path = $f->getThumbnailSystemPath($this->fvPrefix, $this->fvFilename, $i, true);
 		}
 	}
-	
-	
-	/** 
+
+
+	/**
 	 * Checks current viewers for this type and returns true if there is a viewer for this type, false if not
 	 */
 	public function canView() {
@@ -539,32 +539,32 @@ class Concrete5_Model_FileVersion extends Object {
 				$av->setAttributeKey($ak);
 			}
 		}
-		
+
 		if ($createIfNotFound) {
 			$cnt = 0;
-		
+
 			// Is this avID in use ?
 			if (is_object($av)) {
 				$cnt = $db->GetOne("select count(avID) from FileAttributeValues where avID = ?", $av->getAttributeValueID());
 			}
-			
+
 			if ((!is_object($av)) || ($cnt > 1)) {
 				$av = $ak->addAttributeValue();
 			}
 		}
-		
+
 		return $av;
 	}
-	
+
 	//takes a string of comma or new line delimited tags, and puts them in the appropriate format
-	public static function cleanTags($tagsStr){ 
+	public static function cleanTags($tagsStr){
 		$tagsArray=explode("\n",str_replace(array("\r",","),"\n",$tagsStr));
 		$cleanTags=array();
 		foreach($tagsArray as $tag){
 			if( !strlen(trim($tag)) ) continue;
 			$cleanTags[]=trim($tag);
 		}
-		//the leading and trailing line break char is for searching: fvTag like %\ntag\n% 
+		//the leading and trailing line break char is for searching: fvTag like %\ntag\n%
 		return "\n".join("\n",$cleanTags)."\n";
-	}	
+	}
 }
