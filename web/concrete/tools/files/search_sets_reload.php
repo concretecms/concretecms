3f3edc8084fa7b89b2c135eb6042f1e29d345e61
Andrew Embler <andrew@concrete5.org>
2012-08-27 15:22:17 -0700

    fixing search set reloading

--- web/concrete/tools/files/search_sets_reload.php
+++ web/concrete/tools/files/search_sets_reload.php
@@ -16,13 +16,16 @@ $form = Loader::helper('form');
 
 $s1 = FileSet::getMySets();
 if (count($s1) > 0) { ?>
-<div class="span5" >
+		<div class="span4" style="width: 280px">
 	<?=$form->label('fsID', t('In Set(s)'))?>
 	<div class="input">
 		<select multiple name="fsID[]" class="chosen-select">
 			<? foreach($s1 as $s) { ?>
 				<option value="<?=$s->getFileSetID()?>"><?=$s->getFileSetName()?></option>
 			<? } ?>
+			<optgroup label="<?=t('Other')?>">
+				<option value="-1" <? if (is_array($req['fsID']) && in_array(-1, $req['fsID'])) { ?> selected="selected" <? } ?>><?=t('Files in no sets.')?></option>
+			</optgroup>
 		</select>
 	</div>
 </div>
