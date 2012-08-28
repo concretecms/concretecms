595fe97817dff9b445eb78a83da625acf54ebff1
Andrew Embler <andrew@concrete5.org>
2012-08-28 10:45:17 -0700

    tweaks to keywords

--- web/concrete/core/libraries/view.php
+++ web/concrete/core/libraries/view.php
@@ -606,10 +606,14 @@ defined('C5_EXECUTE') or die("Access Denied.");
 		public static function defaultExceptionHandler($e) {
 			// log if setup to do so
 			if (ENABLE_LOG_ERRORS) {
-				$l = new Log(LOG_TYPE_EXCEPTIONS, true, true);
-				$l->write(t('Exception Occurred: ') . sprintf("%s:%d %s (%d)\n", $e->getFile(), $e->getLine(), $e->getMessage(), $e->getCode()));
-				$l->write($e->getTraceAsString());
-				$l->close();
+				$db = Loader::db();
+				$tables = $db->MetaTables();
+				if (in_array('Logs', $tables)) {
+					$l = new Log(LOG_TYPE_EXCEPTIONS, true, true);
+					$l->write(t('Exception Occurred: ') . sprintf("%s:%d %s (%d)\n", $e->getFile(), $e->getLine(), $e->getMessage(), $e->getCode()));
+					$l->write($e->getTraceAsString());
+					$l->close();
+				}
 			}
 
 			if (Config::get('SITE_DEBUG_LEVEL') == DEBUG_DISPLAY_ERRORS) {
