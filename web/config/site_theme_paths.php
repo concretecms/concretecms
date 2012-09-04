e73b43cc79aeb5a4dbcf7f938b828bb10c166805
Andrew Embler <andrew@concrete5.org>
2012-09-04 11:08:12 -0700

    reverting theme paths
    Former-commit-id: 57080fbbe58822c0404bc7aca820614bbc704581

--- web/config/site_theme_paths.php
+++ web/config/site_theme_paths.php
@@ -20,6 +20,3 @@ $v->setThemeByPath('/page_forbidden', "yourtheme");
 $v->setThemeByPath('/register', "yourtheme");
 
 */
-
-$v = View::getInstance();
-$v->setThemeByPath('/page_not_found', "greek_yogurt");
