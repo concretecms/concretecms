726e46a4a62f09bf63c5fd2a82bf496379a6e826
Andrew Embler <andrew@concrete5.org>
2012-09-14 12:43:17 -0700

    separating bootstrap.js and ccm.app.js
    Former-commit-id: bb933a8972164eaa61394e610dbef2a8b4c9d8f4

--- web/concrete/elements/page_controls_header.php
+++ web/concrete/elements/page_controls_header.php
@@ -34,8 +34,8 @@ if (!$dh->inDashboard()) {
 	$this->addFooterItem($html->javascript('jquery.ui.js'));
 	$this->addFooterItem($html->javascript('jquery.form.js'));
 	$this->addFooterItem($html->javascript('jquery.rating.js'));
-	$this->addFooterItem($html->javascript('ccm.app.js'));
 	$this->addFooterItem($html->javascript('bootstrap.js'));
+	$this->addFooterItem($html->javascript('ccm.app.js'));
 	if (ENABLE_PROGRESSIVE_PAGE_REINDEX && Config::get('DO_PAGE_REINDEX_CHECK')) {
 		$this->addHeaderItem('<script type="text/javascript">$(function() { ccm_doPageReindexing(); });</script>');
 	}
