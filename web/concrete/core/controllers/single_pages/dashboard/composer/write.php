1df86b4741644352fc68273da0ea98cb63c8062d
Patrick Heck <patrick@patrickheck.de>
2012-10-02 12:19:54 +0300

    Use DATE_APP_GENERIC_T instead of literal

--- web/concrete/core/controllers/single_pages/dashboard/composer/write.php
+++ web/concrete/core/controllers/single_pages/dashboard/composer/write.php
@@ -90,7 +90,7 @@ class Concrete5_Controller_Dashboard_Composer_Write extends Controller {
 					$obj = new stdClass;
 					$dh = Loader::helper('date');
 					$obj->error = false;
-					$obj->time = $dh->getLocalDateTime('now','g:i a');
+					$obj->time = $dh->getLocalDateTime('now',DATE_APP_GENERIC_T);
 					$obj->timestamp =date('m/d/Y g:i a');
 					print $json->encode($obj);
 					exit;
