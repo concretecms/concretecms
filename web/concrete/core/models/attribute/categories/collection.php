1b02177560afb401b3a3e7bd8cdc72b8f38bc880
Andrew Embler <andrew@concrete5.org>
2012-10-12 12:50:47 -0700

    removing page attribute delete tweaks
    Former-commit-id: 84de74e3272153a25c3cca2a7e46cbf81e583f42

--- web/concrete/core/models/attribute/categories/collection.php
+++ web/concrete/core/models/attribute/categories/collection.php
@@ -181,10 +181,16 @@ class Concrete5_Model_CollectionAttributeValue extends AttributeValue {
 		));
 		
 		// Before we run delete() on the parent object, we make sure that attribute value isn't being referenced in the table anywhere else
+		// Note: we're going to keep these around and not delete them. We'll just clean this up with an optimize job later on - this'll speed up page deletion by a ton.
+		
+		/*
 		$num = $db->GetOne('select count(avID) from CollectionAttributeValues where avID = ?', array($this->getAttributeValueID()));
 		if ($num < 1) {
 			parent::delete();
 		}
+		*/
+
+
 		
 	}
 }
