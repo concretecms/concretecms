9fd2dca5de3fa17cac4046b902e28fc8e8e68323
Andrew Embler <andrew@concrete5.org>
2012-10-24 14:36:06 -0700

    starting on queuable jobs

---
+++ web/concrete/jobs/index_search_all.php
@@ -0,0 +1,11 @@
+<?
+/**
+*
+* Responsible for loading the indexed search class and initiating the reindex command.
+* @package Utilities
+*/
+
+defined('C5_EXECUTE') or die("Access Denied.");
+class IndexSearchAll extends Concrete5_Job_IndexSearchAll {
+
+}
