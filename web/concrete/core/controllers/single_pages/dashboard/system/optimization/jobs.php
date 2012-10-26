da4bb53a98cb3c45e92895504fe1a7e173529364
Andrew Embler <andrew@concrete5.org>
2012-10-25 18:07:42 -0700

    More job work

--- web/concrete/core/controllers/single_pages/dashboard/system/optimization/jobs.php
+++ web/concrete/core/controllers/single_pages/dashboard/system/optimization/jobs.php
@@ -2,10 +2,16 @@
 
 class Concrete5_Controller_Dashboard_System_Optimization_Jobs extends DashboardBaseController {
 
-	function view() {
+	function on_start() {
+		parent::on_start();
 		$this->set('availableJobs', Job::getAvailableList(0)); 
 		$this->set('installedJobs', Job::getList()); 
+		$this->set('jobSets', JobSet::getList());
 		$this->set('auth', Job::generateAuth());
+	}
+
+	public function view() {
+		$this->set('jobListSelected', true);
 	}
 	
 	function install($handle = null) {
@@ -51,5 +57,122 @@ class Concrete5_Controller_Dashboard_System_Optimization_Jobs extends DashboardB
 		$this->set('message', t('All running jobs have been reset.'));
 		$this->view();
 	}
-	
+
+	public function set_added() {
+		$this->set('success', t('Job set added.'));
+		$this->set('jobSetsSelected', true);
+	}
+
+	public function edit_set($jsID = false) {
+		$this->set('jobSetsSelected', true);
+		$js = JobSet::getByID($jsID);
+		if (is_object($js)) {
+			$this->set('set', $js);
+		} else {
+			$this->redirect('/dashboard/system/optimization/jobs');
+		}
+	}
+
+	public function update_set_jobs() {
+		if ($this->token->validate('update_set_jobs')) { 
+			$js = JobSet::getByID($this->post('jsID'));
+			if (!is_object($js)) {
+				$this->error->add(t('Invalid Job set.'));
+			}
+
+			if (!$this->error->has()) {
+				// go through and add all the attributes that aren't in another set
+				$js->clearJobs();
+				if (is_array($this->post('jID'))) {
+					foreach($_POST['jID'] as $jID) {
+						$j = Job::getByID($jID);
+						if(is_object($j)) {
+							$js->addJob($j);
+						}
+					}					
+				}
+				$this->redirect('/dashboard/system/optimization/jobs', 'set_updated');
+			}	
+			
+		} else {
+			$this->error->add($this->token->getErrorMessage());
+		}
+		$this->edit($this->post('asID'));
+	}
+	public function set_updated() {
+		$this->set('jobSetsSelected', true);
+		$this->set('success', t('Job Set updated successfully.'));
+	}
+
+	public function update_set() {
+		$this->set('jobSetsSelected', true);
+		if ($this->token->validate('update_set')) { 
+			$js = JobSet::getByID($this->post('jsID'));
+			if (!is_object($js)) {
+				$this->error->add(t('Invalid Job set.'));
+			} else {
+				if (!trim($this->post('jsName'))) { 
+					$this->error->add(t("Specify a name for your Job set."));
+				}
+			}
+			
+			if (!$this->error->has()) {
+				$js->updateJobSetName($this->post('jsName'));
+				$this->redirect('/dashboard/system/optimization/jobs', 'set_updated');
+			}
+			
+		} else {
+			$this->error->add($this->token->getErrorMessage());
+		}
+	}
+
+	public function set_deleted() {
+		$this->set('jobSetsSelected', true);
+		$this->set('success', t('Group set deleted successfully.'));
+	}
+
+	public function delete_set() {
+		$this->set('jobSetsSelected', true);
+		if ($this->token->validate('delete_set')) { 
+			$js = JobSet::getByID($this->post('jsID'));
+			if (!$js->canDelete()) {
+				$this->error->add(t('You cannot delete the default Job set.'));
+			}
+
+			if (!is_object($js)) {
+				$this->error->add(t('Invalid Job set.'));
+			}
+			if (!$this->error->has()) {
+				$js->delete();
+				$this->redirect('/dashboard/system/optimization/jobs', 'set_deleted');
+			}			
+			$this->edit_set($this->post('jsID'));
+		} else {
+			$this->error->add($this->token->getErrorMessage());
+		}
+	}
+
+	public function add_set() {
+		$this->set('jobSetsSelected', true);
+		if ($this->token->validate('add_set')) { 
+			if (!trim($this->post('jsName'))) { 
+				$this->error->add(t("Specify a name for your Job set."));
+			}
+			if (!$this->error->has()) {			
+				$js = JobSet::add($this->post('jsName'));
+				if (is_array($_POST['jID'])) {
+					foreach($_POST['jID'] as $jID) {
+						$j = Job::getByID($jID);
+						if(is_object($j)) {
+							$js->addJob($j);
+						}
+					}					
+				}
+				$this->redirect('/dashboard/system/optimization/jobs', 'set_added');
+			}
+			
+		} else {
+			$this->error->add($this->token->getErrorMessage());
+		}
+	}	
 }
