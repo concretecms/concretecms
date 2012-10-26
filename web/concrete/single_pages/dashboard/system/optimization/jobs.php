ffbd523a663a00b5aeb1d4c5fe859b9baaace53e
Andrew Embler <andrew@concrete5.org>
2012-10-26 09:50:00 -0700

    more job work

--- web/concrete/single_pages/dashboard/system/optimization/jobs.php
+++ web/concrete/single_pages/dashboard/system/optimization/jobs.php
@@ -112,6 +112,16 @@ $jh = Loader::helper('json');
 	</tbody>
 	</table>
 <? } ?>
+<? 
+$djs = JobSet::getDefault();
+if (is_object($djs)) { ?>
+<div class="well">
+<h4><?=t('Automation Instructions')?></h4>
+<p><?=t('To run all the jobs in the <a href="%s">%s</a> Job Set, schedule this URL using cron or a similar system:', $this->url('/dashboard/system/optimization/jobs', 'edit_set', $djs->getJobSetID()), $djs->getJobSetName())?></p>
+<div><input type="text" style="width: 700px" class="ccm-default-jobs-url" value="<?=BASE_URL . $this->url('/tools/required/jobs?auth=' . $auth)?>" /></div>
+</div>
+<? } ?>
+
 </div>
 
 <div id="ccm-tab-content-sets" <? if (!$jobSetsSelected) { ?>style="display: none" <? } ?>>
@@ -213,6 +223,12 @@ $jh = Loader::helper('json');
 		</form>
 		</div>
 	</div>
+
+		<div class="well">
+		<h4><?=t('Automation Instructions')?></h4>
+		<p><?=t('To run all the jobs in this Job Set, schedule this URL using cron or a similar system:', $set->getJobSetID())?></p>
+		<div><input type="text" style="width: 700px" class="ccm-default-jobs-url" value="<?=BASE_URL . $this->url('/tools/required/jobs?auth=' . $auth . '&jsID=' . $set->getJobSetID())?>" /></div>
+		</div>
 
 <? } else { ?>
 
@@ -317,6 +333,9 @@ jQuery.fn.processResponse = function(r) {
 
 $(function() {
 	$('tr.running').showLoading();
+	$('.ccm-default-jobs-url').on('click', function() {
+		$(this).get(0).select();
+	});
 	$('.icon-question-sign').tooltip();
 	$('.btn-run-job').on('click', $('#ccm-jobs-list'), function() {
 		var row = $(this).parent().parent();
@@ -354,202 +373,3 @@ $(function() {
 });
 </script>
 <?=$h->getDashboardPaneFooterWrapper();?>
-
-<?
-/*
-?>
-<style type="text/css">
-.run-all, .run-task { height: 16px; width: 16px; display: block; }
-.run-indicator {
-	height: 16px; width: 16px; display: none;
-	background: url("<?=ASSETS_URL_IMAGES?>/throbber_white_16.gif") no-repeat transparent;
-}
-
-.running .run-indicator {
-	display: block;
-}
-
-.running .run-all, 
-.running .run-task {
-	display: none;
-}
-
-.run-task {
-	background: url("<?=ASSETS_URL_IMAGES?>/ui-icons_222222_256x240.png") no-repeat 0 -160px transparent;
-}
-
-.run-all {
-	background: url("<?=ASSETS_URL_IMAGES?>/ui-icons_222222_256x240.png") no-repeat -32px -160px transparent;
-}
-</style>
-<script>
-jQuery(function($) {
-	$('.btn.remove').bind('click', function(e) {
-		if (!confirm(<?=$jh->encode(t("Are you sure you want to uninstall this job?"))?>)) {
-			e.preventDefault();
-		}
-	});
-
-	function runTaskForRow(row, jobId, jobTitle, supportsQueue, cb) {
-		row.addClass('running');
-		if (supportsQueue) {
-			ccm_triggerProgressiveOperation(
-				CCM_TOOLS_PATH + '/jobs', //?auth=<?=$auth?>&jID=' + jobId,
-				[
-					{'name': 'auth', 'value': '<?=$auth?>'},
-					{'name': 'jID', 'value': jobId}
-				],
-				jobTitle, function() {
-					$('.ui-dialog-content').dialog('close');
-					row.removeClass('running green red');
-					row.find('.jLastStatusText').html(json.message);
-					row.find('.jDateLastRun').html(json.jDateLastRun);
-					row.addClass(json.error == 0 ? 'green' : 'red');
-					if (cb && $.isFunction(cb)) {
-						cb(row, json, jobId);
-					}
-
-				}
-			);
-
-		} else { 
-			$.ajax({ 
-				url: CCM_TOOLS_PATH + '/jobs?auth=<?=$auth?>&jID=' + jobId,
-				dataType: 'json',
-				cache: false,
-				success: function(json) {
-					row.removeClass('running green red');
-					row.find('.jLastStatusText').html(json.message);
-					row.find('.jDateLastRun').html(json.jDateLastRun);
-					row.addClass(json.error == 0 ? 'green' : 'red');
-					if (cb && $.isFunction(cb)) {
-						cb(row, json, jobId);
-					}
-				}
-			});
-		}
-	}
-
-	$('.run-task[data-jobId]').bind('click', function(e) {
-		e.preventDefault();
-		var $this = $(this),
-			row = $this.closest('tr');
-		runTaskForRow(row, $this.attr('data-jobId'), $this.attr('data-job-title'), $this.attr('data-supports-queue'));
-	});
-
-	$('.run-all').bind('click', function(e) {
-		var $this = $(this),
-			table = $this.closest('table'),
-			links = table.find('tbody .run-task[data-jobId]'),
-			jobs = links.map(function() {
-					return {
-						jobId : $(this).attr('data-jobId'),
-						supportsQueue: $(this).attr('data-supports-queue'),
-						jobTitle: $(this).attr('data-job-title'),
-						row : $(this).closest('tr')
-					};
-			}).get(),
-			next = function() {
-				var job = jobs.shift();
-				if (job) {
-					runTaskForRow(job.row, job.jobId, job.jobTitle, job.supportsQueue, next);
-				} else {
-					table.removeClass('running');
-				}
-			};
-
-		if (!table.hasClass('running')) {
-			e.preventDefault();
-			table.addClass('running');
-			next();
-		}
-	});
-});
-</script>
-<?=$h->getDashboardPaneHeaderWrapper(t('Automated Jobs'), false, false, false);?>
-<div class="ccm-pane-body">
-<?if ($jobList->numRows() == 0):?>
-<?=t('You currently have no jobs installed.')?>
-<?else:?>
-<table class="table table-striped">
-<thead>
-<tr>
-	<th><a class="run-all" href="<?=BASE_URL.$this->url('/tools/required/jobs?auth='.$auth.'&debug=1')?>" title="<?=t('Run all')?>"></a><span class="run-indicator"></span></th>
-	<th><?=t('ID')?></th>
-	<th><?=t('Name')?></th>
-	<th><?=t('Description')?></th>
-	<th><?=t('Last Run')?></th>
-	<th><?=t('Results of Last Run')?></th>
-	<th></th>
-</tr>
-</thead>
-<tbody>
-<? $jobrunning = false; ?>
-<?foreach ($jobList as $job):
-	$j = Job::getByHandle($job['jHandle']);
-?>
-<tr <? if ($job['jStatus'] == 'RUNNING') {
-	
-	$jobrunning = true;?>class="running" <? } ?>>
-	<td><a class="run-task" title="<?=t('Run')?>" href="<?=BASE_URL.$this->url('/tools/required/jobs?auth='.$auth.'&jID='.$job['jID'])?>" data-job-title="<?=$job['jName']?>" data-supports-queue="<?=$j->supportsQueue()?>" data-jobId="<?=$job['jID']?>"></a><span class="run-indicator"></span></td>
-	<td><?=$job['jID']?></td>
-	<td><?=t($job['jName'])?></td>
-	<td><?=t($job['jDescription'])?></td>
-	<td class="jDateLastRun"><?
-	if ($job['jStatus'] == 'RUNNING') {
-		$runtime = date(DATE_APP_GENERIC_TS, strtotime($job['jDateLastRun']));
-		echo ("<strong>");
-		echo t("Currently Running (Since %s)", $runtime);					
-		echo ("</strong>");
-	} else if($job['jDateLastRun'] == '' || substr($job['jDateLastRun'], 0, 4) == '0000') {
-		echo t('Never');
-	} else {
-		$runtime = date(DATE_APP_GENERIC_MDY . t(' \a\t ') . DATE_APP_GENERIC_TS, strtotime($job['jDateLastRun']) );
-		echo $runtime;
-	}
-?></td>
-	<td class="jLastStatusText"><?=t($job['jLastStatusText'])?></td>
-	<td><?if(!$job['jNotUninstallable']):?>
-		<?=$ih->button(t('Remove'), $this->action('uninstall', $job['jID']), null, 'remove')?>
-	<?endif?></td>
-</tr>
-<?endforeach?>
-</tbody>
-</table>
-<?endif?>
-
-<?if($availableJobs):?>
-<h2><?=t('Jobs Available for Installation')?></h2>
-<table class="table table-striped">
-<thead>
-	<tr> 
-		<th><?=t('Name')?></th>
-		<th><?=t('Description')?></th> 
-		<th></th>
-	</tr>
-</thead>
-<tbody>
-	<?foreach($availableJobs as $availableJobName => $job):?>
-	<tr> 
-		<td><?=$job->getJobName() ?></td>
-		<td><?=$job->getJobDescription() ?></td> 
-		<td><?if(!$job->invalid):?>
-			<?=$ih->button(t('Install'), $this->action('install', $job->jHandle))?>
-		<?endif?></td>
-	</tr>	
-	<?endforeach?>
-</tbody>
-</table>
-<?endif?>
-<div><?=t('If you wish to run these jobs in the background, automate access to the following URL:')?></div>
-<div><a href="<?=BASE_URL.$this->url('/tools/required/jobs?auth='.$auth)?>"><?=BASE_URL . $this->url('/tools/required/jobs?auth=' . $auth)?></a></div>
-</div>
-<div class="ccm-pane-footer"><? if ($jobrunning == true) { ?>
-	<form method="post" style="display: inline" action="<?=$this->action('reset_running_jobs')?>">
-		<?=Loader::helper('validation/token')->output('reset_running_jobs')?>
-		<input type="submit" class="btn" value="<?=t('Reset all Running Jobs')?>" />
-	</form>
-<? } ?></div>
-<?=$h->getDashboardPaneFooterWrapper(false);?>
-
-*/ ?>
--- web/concrete/tools/jobs.php
+++ web/concrete/tools/jobs.php
@@ -5,136 +5,37 @@ defined('C5_EXECUTE') or die("Access Denied.");
 if (!ini_get('safe_mode')) {
 	@set_time_limit(0);
 }
-$jobObj = Loader::model("job");
+
 $json = Loader::helper('json');
-$outputDisabled=0;
+$r = new stdClass;
+$r->results = array();
 
-//JSON vars
-$jsonErrorCode=0;   // 0: Successful, 1: job not found, 2: Access Denoed
-$jsonMessage='';
-$jsonJHandle='';
-$jsonJID=0;
-$jsonJDateLastRun='';
+if (Job::authenticateRequest($_REQUEST['auth'])) {
 
-if (!Job::authenticateRequest($_REQUEST['auth'])) {
-	if(!$_REQUEST['debug']) {
-		die(t("Access Denied."));
-	} else {
-		$debug = array();
-		$debug['error'] = 2;
-		$debug['message'] = t("Access Denied.");
-		$debug['jHandle'] = '';
-		$debug['jID'] = '';
-		$debug['jDateLastRun'] = '';
-		die($json->encode($debug));
+	// Legacy 
+	if ($_REQUEST['jID']) {
+		$j = Job::getByID($_REQUEST['jID']);
+		$obj = $j->executeJob();
+		print $json->encode($obj);
+		exit;
 	}
-} 
 
-if( strlen($_REQUEST['jHandle'])>0 || intval($_REQUEST['jID'])>0 ){
-
-	if($_REQUEST['jHandle']) 
-		$job = Job::getByHandle($_REQUEST['jHandle']);
-	else 
-		$job = Job::getByID( intval($_REQUEST['jID']) );
-		
-	if(!$job){ 
-		$jsonErrorCode=1;
-		$jsonMessage=t('Error: Job not found');
+	if ($_REQUEST['jsID']) {
+		$js = JobSet::getByID($_REQUEST['jsID']);
 	} else {
+		// default set legacy support
+		$js = JobSet::getDefault();
+	}
 
-		if ($job->supportsQueue()) {
-
-			$q = Queue::get('job_' . $job->getJobHandle());
-			if ($_POST['process']) {
-				$obj = new stdClass;
-				$js = Loader::helper('json');
-				$messages = $q->receive($job->getJobQueueBatchSize());
-				foreach($messages as $key => $p) {
-					$job->processQueueItem($p);
-					$q->deleteMessage($p);
-				}
-				$obj->totalItems = $q->count();	
-				if ($q->count() == 0) {
-					$q->deleteQueue('job_' . $job->getJobHandle());
-				}
-				print $js->encode($obj);
-				exit;
-			} else if ($q->count() == 0) {
-				$job->addToQueue($q);
-			}
-
-			$totalItems = $q->count();
-			Loader::element('progress_bar', array('totalItems' => $totalItems, 'totalItemsSummary' => t2("%d items", "%d items", $totalItems)));
-
-			exit;
-
+	if (is_object($js)) {
+		$jobs = $js->getJobs();
+		foreach($jobs as $j) {
+			$obj = $j->executeJob();
+			$r->results[] = $obj;
 		}
 
-
+		print $json->encode($r);
+		exit;
 	}
-} 
-
-/*
-
-//Uncomment to turn debugging on
-//$_REQUEST['debug']=1;
-
-//all print/echo statements are suppressed, unless the debug variable is set
-if(!$_REQUEST['debug']) ob_start();
-
-
-//run by job handle (aka file name) or run by job id
-if( strlen($_REQUEST['jHandle'])>0 || intval($_REQUEST['jID'])>0 ){
-
-	if($_REQUEST['jHandle']) 
-		$jobObj=Job::getByHandle($_REQUEST['jHandle']);
-	else 
-		$jobObj=Job::getByID( intval($_REQUEST['jID']) );
-		
-	if(!$jobObj){ 
-		$jsonErrorCode=1;
-		$jsonMessage=t('Error: Job not found');
-	}else{ 
 	
-		//Change Job Status
-		if( $_REQUEST['jStatus'] ){
-			$jobObj->setJobStatus( $_REQUEST['jStatus'] );
-		//Run Job
-		}else{
-			$jsonMessage = $jobObj->executeJob(); 	
-		}
-		
-		//set json vars
-		$jsonJHandle = $jobObj->jHandle;	
-		$jsonJDateLastRun = $jobObj->jDateLastRun;		
-		$jsonJID=$jobObj->jID;			
-		if( $jobObj->isError() ) 
-			$jsonErrorCode = $jobObj->getError();		
-	} 
-	$runTime=date(DATE_APP_GENERIC_MDYT, strtotime($jsonJDateLastRun));
-//run all jobs - default
-}else{ 
-	Job::runAllJobs();
-	if(!$_REQUEST['debug']) 
-		$outputDisabled=1;
-	$jsonMessage=t('All Jobs Run Successfully');
-	$runTime=date(DATE_APP_GENERIC_MDYT);
-	$jsonJHandle =t('All Jobs');	
 }
-
-//all print/echo statements are suppressed, unless the debug variable is set
-if(!$_REQUEST['debug']){ 
-	ob_end_clean();
-}
-
-//$runTime=strtotime($jsonJDateLastRun);
-if(!$outputDisabled) {
-	$debug = array();
-	$debug['error'] = intval($jsonErrorCode);
-	$debug['message'] = $jsonMessage;
-	$debug['jHandle'] = $jsonJHandle;
-	$debug['jID'] = $jsonJID;
-	$debug['jDateLastRun'] = $runTime;
-	echo $json->encode($debug);
-}
-*/
