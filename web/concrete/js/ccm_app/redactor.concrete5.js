7f3358adf4723070235e95ef3d0123efda8d5d2d
Andrew Embler <andrew@concrete5.org>
2012-11-13 07:36:31 -0800

    tweaks to inline

--- web/concrete/js/ccm_app/redactor.concrete5.js
+++ web/concrete/js/ccm_app/redactor.concrete5.js
@@ -3,86 +3,14 @@ if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
 
 RedactorPlugins.concrete5inline = {
 
-	activeEditor: false,
-
-	selectSitemapNode: function(cID, cName) {
-		var url = CCM_BASE_URL + CCM_DISPATCHER_FILENAME + '?cID=' + cID;		
-		var editor = RedactorPlugins.concrete5inline.activeEditor;
-		var selectedText = editor.getSelected();
-
-		if (selectedText != '') {
-			editor.execCommand('inserthtml', '<a href="' + CCM_BASE_URL + CCM_DISPATCHER_FILENAME + '?cID=' + cID + '" title="' + cName + '">' + selectedText + '<\/a>');
-		} else {
-			var selectedText = '<a href="' + CCM_BASE_URL + CCM_DISPATCHER_FILENAME + '?cID=' + cID + '" title="' + cName + '">' + cName + '<\/a>';
-			editor.insertHtml(selectedText);
-		}
-
-	},
-
-
 	init: function() {
-		var editor = this.$editor;
-		var iobj = this;
-
-		this.addBtnBefore('html', 'concrete5', 'concrete5', false, {
-
-			insertLinkToPage:
-			{
-				title: ccmi18n_editor.insertLinkToPage,
-				callback: function() {
-
-			    $.fn.dialog.open({
-			        title: ccmi18n_sitemap.choosePage,
-			        href: CCM_TOOLS_PATH + '/sitemap_search_selector.php?sitemap_select_mode=select_page&callback=RedactorPlugins.concrete5inline.selectSitemapNode',
-			        width: '90%',
-			        modal: false,
-			        height: '70%'
-			    });
-
-				}
-			},
-			insertImage:
-			{
-				title: ccmi18n_editor.insertImage,
-				callback: function() {
-					var editor = RedactorPlugins.concrete5inline.activeEditor;
-					iobj.saveSelection();
-					ccm_launchFileManager();
-					ccm_chooseAsset = function(obj) {
-						iobj.restoreSelection();
-						editor.insertHtml('<img src="' + obj.filePathInline + '" alt="' + obj.title + '" width="' + obj.width + '" height="' + obj.height + '">');
-					}
-				}
-			},
-			insertLinkToFile:
-			{
-				title: ccmi18n_editor.insertLinkToFile,
-				callback: function() {
-					var editor = RedactorPlugins.concrete5inline.activeEditor;
-					iobj.saveSelection();
-					ccm_launchFileManager();
-					ccm_chooseAsset = function(obj) {
-						iobj.restoreSelection();
-						var selectedText = editor.getSelected();
-						if (selectedText != '') {
-							var html = '<a href="' + obj.filePathInline + '">' + selectedText + '<\/a>';
-						} else {
-							var html = '<a href="' + obj.filePathInline + '">' + obj.title + '<\/a>';
-						}
-						editor.insertHtml(html);
-					}
-				}
-			}
-		});
-		this.addBtnSeparatorAfter('concrete5');
-		this.opts.observeImages = false;
 
 		this.$toolbar.css({ position: 'fixed', width: '100%', zIndex: 999, top: '50px', left: '0px' });
 		this.$toolbar.append($('<li id="ccm-redactor-actions-buttons" class="ccm-ui"><button id="ccm-redactor-cancel-button" type="button" class="btn btn-mini">Cancel</button><button id="ccm-redactor-save-button" type="button" class="btn btn-primary btn-mini">Save</button></li>'));
 		this.$toolbar.appendTo($(document.body));
 		var toolbar = this.$toolbar;
+		var editor = this.$editor;
 
-		RedactorPlugins.concrete5inline.activeEditor = editor;
 
 		$('#ccm-redactor-cancel-button').on('click', function() {
 			$('li#ccm-redactor-actions-buttons').hide();
@@ -98,6 +26,82 @@ RedactorPlugins.concrete5inline = {
 			editor.destroyEditor();
 			$('#ccm-block-form').submit();
 		});
+
+	}
+
+}
+
+RedactorPlugins.concrete5 = {
+
+	init: function() {
+		var iobj = this;
+		this.addBtnBefore('html', 'concrete5', 'concrete5', false, {
+
+			insertLinkToPage:
+			{
+				title: ccmi18n_editor.insertLinkToPage,
+				callback: function(obj) {
+					var editor = obj.$editor;
+
+				    $.fn.dialog.open({
+				        title: ccmi18n_sitemap.choosePage,
+				        href: CCM_TOOLS_PATH + '/sitemap_search_selector.php?sitemap_select_mode=select_page',
+				        width: '90%',
+				        modal: false,
+				        height: '70%'
+				    });
+
+				    ccm_selectSitemapNode = function(cID, cName) {
+
+						var url = CCM_BASE_URL + CCM_DISPATCHER_FILENAME + '?cID=' + cID;		
+						var selectedText = editor.getSelected();
+
+						if (selectedText != '') {
+							editor.execCommand('inserthtml', '<a href="' + CCM_BASE_URL + CCM_DISPATCHER_FILENAME + '?cID=' + cID + '" title="' + cName + '">' + selectedText + '<\/a>');
+						} else {
+							var selectedText = '<a href="' + CCM_BASE_URL + CCM_DISPATCHER_FILENAME + '?cID=' + cID + '" title="' + cName + '">' + cName + '<\/a>';
+							editor.insertHtml(selectedText);
+						}
+				    }
+
+				}
+			},
+			insertImage:
+			{
+				title: ccmi18n_editor.insertImage,
+				callback: function(obj) {
+					var editor = obj.$editor;
+					iobj.saveSelection();
+					ccm_launchFileManager();
+					ccm_chooseAsset = function(res) {
+						iobj.restoreSelection();
+						editor.insertHtml('<img src="' + res.filePathInline + '" alt="' + res.title + '" width="' + res.width + '" height="' + res.height + '">');
+					}
+				}
+			},
+			insertLinkToFile:
+			{
+				title: ccmi18n_editor.insertLinkToFile,
+				callback: function(obj) {
+					var editor = obj.$editor;
+					iobj.saveSelection();
+					ccm_launchFileManager();
+					ccm_chooseAsset = function(res) {
+						iobj.restoreSelection();
+						var selectedText = editor.getSelected();
+						if (selectedText != '') {
+							var html = '<a href="' + res.filePathInline + '">' + selectedText + '<\/a>';
+						} else {
+							var html = '<a href="' + res.filePathInline + '">' + res.title + '<\/a>';
+						}
+						editor.insertHtml(html);
+					}
+				}
+			}
+		});
+		this.addBtnSeparatorAfter('concrete5');
+		this.opts.observeImages = false;
+
 
 	}
 
