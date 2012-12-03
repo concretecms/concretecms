76b4f6e9594d9a3a3ccb46025c7bc1203ae2314f
Andrew Embler <andrew@concrete5.org>
2012-12-03 09:49:20 -0800

    fixing overrides for states and countries

--- web/concrete/helpers/lists/countries.php
+++ web/concrete/helpers/lists/countries.php
@@ -17,33 +17,8 @@
  */
  
 defined('C5_EXECUTE') or die("Access Denied.");
-class ListsCountriesHelper {
+class ListsCountriesHelper extends Concrete5_Helper_Lists_Countries {
 
-	protected $countries = array();
-	
-	public function __construct() {
-		Loader::library('3rdparty/Zend/Locale');
-		$countries = Zend_Locale::getTranslationList('territory', Localization::activeLocale(), 2);
-		// unset invalid countries
-		unset($countries['SU'], $countries['ZZ'], $countries['IM'], $countries['JE'], $countries['VD']);
-		asort($countries, SORT_LOCALE_STRING);
-		$this->countries = $countries;
-	}
-	
-	/** 
-	 * Returns an array of Countries with their short name as the key and their full name as the value
-	 * @return array
-	 */
-	public function getCountries() {
-		return $this->countries;
-	}
-	
 
-	/** 
-	 * Gets a country full name given its index
-	 * @return string
-	 * @param string $index
-	 */
-	public function getCountryName($index) { return $this->countries[$index]; }
 
 }
