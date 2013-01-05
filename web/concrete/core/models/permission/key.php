95b61fce13866f2277220252a743f61e92d5b3b1
Andrew Embler <andrew@concrete5.org>
2013-01-05 12:04:45 -0800

    More bug fixin
    Former-commit-id: 52cd7be5d962940c8d8d7967c4b472b26da247fe

--- web/concrete/core/models/permission/key.php
+++ web/concrete/core/models/permission/key.php
@@ -291,8 +291,12 @@ abstract class Concrete5_Model_PermissionKey extends Object {
 	public function getPermissionAssignmentObject() {
 		if (is_object($this->permissionObject)) {
 			$class = Loader::helper('text')->camelcase(get_class($this->permissionObject) . 'PermissionAssignment');
-			if (!class_exists($class) && $this->permissionObject instanceof Page) {
-				$class = 'PagePermissionAssignment';
+			if (!class_exists($class)) {
+				if ($this->permissionObject instanceof Page) {
+					$class = 'PagePermissionAssignment';
+				} else if ($this->permissionObject instanceof Area) {
+					$class = 'AreaPermissionAssignment';
+				}
 			}
 			$targ = new $class();
 			$targ->setPermissionObject($this->permissionObject);
