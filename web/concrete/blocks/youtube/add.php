d720ed48e74ed63ad4038b36d2ff5ea48f526525
Jordan Mack <jordanmack@parhelic.com>
2013-01-08 13:31:39 -0800

    Fix PHP warning when adding YouTube block. Bug tracker: http://www.concrete5.org/developers/bugs/5-6-0-2/php-warning-when-adding-youtube-block/
    Former-commit-id: 3005a08579ae3d02890ffa8659327ddb7cfff82d

--- web/concrete/blocks/youtube/add.php
+++ web/concrete/blocks/youtube/add.php
@@ -3,4 +3,4 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $bObj=$controller;
 ?>
 
-<? $this->inc('form_setup_html.php'); ?> 
+<? $this->inc('form_setup_html.php', array('bObj' => $bObj)); ?> 
