ee84b32cf4be1bf399a896e487e7110b72a7a2ac
Andrew Embler <andrew@concrete5.org>
2013-01-17 23:11:11 -0800

    page theme grid work
    Former-commit-id: 838dc71086bc56b4e77b2f362c7effde50ea81e0

---
+++ web/concrete/models/page/theme/editable_style/model.php
@@ -0,0 +1,3 @@
+<?
+defined('C5_EXECUTE') or die("Access Denied.");
+class PageThemeEditableStyle extends Concrete5_Model_PageThemeEditableStyle {}
---
+++ web/concrete/models/page/theme/grid/model.php
@@ -0,0 +1,3 @@
+<?
+defined('C5_EXECUTE') or die("Access Denied.");
+class PageThemeGrid extends Concrete5_Model_PageThemeGrid {}
