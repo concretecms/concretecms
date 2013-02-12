aa8e9a6499d40f60ced4509706fca50b12fd24cf
Evan Cooper <evan@concretecms.com>
2013-02-12 14:59:55 -0800

    Apply Edit Bar Margin to HTML vs Body
    Applying margin-top: 49px !important to html element rather than
    body.

--- web/concrete/elements/page_controls_header.php
+++ web/concrete/elements/page_controls_header.php
@@ -10,7 +10,7 @@ if (isset($cp)) {
 
 ?>
 
-<style type="text/css">body {margin-top: 49px !important;} </style>
+<style type="text/css">html {margin-top: 49px !important;} </style>
 
 <script type="text/javascript">
 <?
