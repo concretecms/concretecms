e5370ffff2759e30a8ab4b28f8d751aae954bf09
Andrew Embler <andrew@concrete5.org>
2013-02-13 16:54:35 -0800

    tweak
    Former-commit-id: 8b8f4bbe203c8b88e5ffae5d5067cd1cacee9fad

--- web/concrete/elements/permission/duration.php
+++ web/concrete/elements/permission/duration.php
@@ -203,6 +203,10 @@ foreach($list['format']['wide'] as $key => $value) { ?>
 <script type="text/javascript">
 ccm_accessEntityCalculateRepeatOptions = function() {
 	// get the difference between start date and end date
+	if (!$("#pdStartDate_activate").is(':checked')) {
+		return false;
+	}
+       
 	var sdf = ($("#pdStartDate_dt").datepicker('option', 'dateFormat'));
 	var sdfr = $.datepicker.parseDate(sdf, $("#pdStartDate_dt").val());
 	var edf = ($("#pdEndDate_dt").datepicker('option', 'dateFormat'));
