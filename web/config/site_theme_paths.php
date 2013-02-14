513b2ffcc52fb5ebdb231ebebd1263eee068da27
Andrew Embler <andrew@concrete5.org>
2013-02-14 13:15:16 -0800

    fixing custom templates on forbidden pages
    Former-commit-id: f9b4bb3ad944d2502f6f0b3281da199428608b15

--- web/config/site_theme_paths.php
+++ web/config/site_theme_paths.php
@@ -12,11 +12,10 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 
 */
 
-/*
+
 $v = View::getInstance();
 
-$v->setThemeByPath('/login', "yourtheme");
-$v->setThemeByPath('/page_forbidden', "yourtheme");
-$v->setThemeByPath('/register', "yourtheme");
-
-*/
+$v->setThemeByPath('/login', "greek_yogurt");
+$v->setThemeByPath('/page_not_found', "greek_yogurt");
+$v->setThemeByPath('/page_forbidden', "greek_yogurt");
+$v->setThemeByPath('/register', "greek_yogurt");
