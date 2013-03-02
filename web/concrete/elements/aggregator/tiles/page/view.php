105b22d44e3922aae542724921c03a40e22f362c
Andrew Embler <andrew@concrete5.org>
2013-03-02 11:15:35 -0800

    working on features in the tiles

--- web/concrete/elements/aggregator/tiles/page/view.php
+++ web/concrete/elements/aggregator/tiles/page/view.php
@@ -4,3 +4,9 @@
 <div class="ccm-aggregator-tile-headline"><a href="<?=Loader::helper('navigation')->getLinkToCollection($page)?>"><?=$page->getCollectionName()?></a></div>
 <div><?=$item->getAggregatorItemPublicDateTime()?></div>
 <p><?=$page->getCollectionDescription()?></p>
+
+<?
+$assignments = $page->getFeatureAssignments();
+foreach($assignments as $ass) {
+	$fd = $ass->getFeatureDetailObject();
+}
