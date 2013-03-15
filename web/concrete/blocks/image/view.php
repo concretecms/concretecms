d0ad69b9d0571da154485201dc08388315984370
tylerryan <ryan@concrete5.org>
2013-03-15 14:26:50 -0700

    trimmed down image block a bit more, pulled inline css & js

--- web/concrete/blocks/image/view.php
+++ web/concrete/blocks/image/view.php
@@ -1,33 +1,14 @@
-<?
-defined('C5_EXECUTE') or die("Access Denied.");
-//echo($controller->getContentAndGenerate());
+<?php defined('C5_EXECUTE') or die("Access Denied.");
 if (strlen($relPathHover)) {
 	$mouseOvers = ' onmouseover="this.src=\''.$relPathHover.'\'" onmouseout="this.src=\''.$relPath.'\'"';
 }
-if (strlen($linkURL)) {
-	$linkClass = $hoverImage?'"ccm-image-hover"':'';
-?>
-	<a href="<?=$linkURL?>"<?=strlen($linkClass)?' class='.$linkClass:''?>>
+if (strlen($linkURL)) { ?>
+	<a href="<?=$linkURL?>"<?=strlen($hoverImage)?' class="ccm-image-hover"':''?>>
 <?}?>
-<style>
-	img.ccm-image-block.alternate {
-		display:none;
-	}
-</style>
-<script>
-$(function(){
-$( 'img.ccm-image-block.primary' ).hover(function(){
-		$(this).attr('oldsrc',$(this).attr('src'));
-		$(this).attr('src',$(this).next('img.ccm-image-block.alternate').attr('src'));
-	},
-	function(){
-		$(this).attr('src',$(this).attr('oldsrc'));
-	});
-});
-</script>
-
 <img class="ccm-image-block primary" alt="<?=$altText?>" src="<?=$relPath?>" <?=$sizeStr?>/>
-<img class="ccm-image-block alternate" alt="<?=$altText?>" src="<?=$relPathHover?>" <?=$sizeStr?>/>
+<? if($relPathHover) { ?>
+	<img class="ccm-image-block alternate" alt="<?=$altText?>" src="<?=$relPathHover?>" <?=$sizeStr?>/>
+<? }?>
 <?if (strlen($linkURL)) { ?>
 	</a>
 <?}?>
