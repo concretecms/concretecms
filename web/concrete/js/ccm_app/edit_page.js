889fe646c3e32356028d8625ea390139699bdac5
Andrew Embler <andrew@concrete5.org>
2013-03-18 16:37:32 -0700

    tweaks

--- web/concrete/js/ccm_app/edit_page.js
+++ web/concrete/js/ccm_app/edit_page.js
@@ -120,19 +120,15 @@ var CCMEditMode = function() {
 		$('div.ccm-area').sortable({
 			items: 'div.ccm-block-edit',
 			connectWith: 'div.ccm-area',
-			placeholder: "ccm-block-type-drop-holder",
 			opacity: 0.4,
 			over: function(e, ui) {
-				$(this).addClass('ccm-area-drag-over');
-				var w = $(this).width();
-				$(ui.helper).css('width', w + 'px');
-				return true;
+		
 			},
 			out: function() {
-				$(this).removeClass('ccm-area-drag-over');
+		
 			},
 			receive: function(e, ui) {
-				$dropelement = ui.item;
+		
 			}
 
 		});
@@ -140,26 +136,12 @@ var CCMEditMode = function() {
 			var $li = $(this);
 			var $sortables = $('div.ccm-area[data-accepts-block-types~=' + $li.attr('data-block-type-handle') + ']');
 			$li.draggable({
-				helper: function() {
-					var w = $(this).width();
-					var h = $(this).height();
-					var $d =  $('<div />', {'class': 'ccm-block-type-dragging'}).css('width', w).css('height', h);
-					return $d;
-				},
 				start: function(e, ui) {
-					$sortables.addClass('ccm-area-drag-active');
+
 				},
 				handle: '[data-inline-command=move-block]',
-				
 				stop: function(e, ui) {
-					if ($dropelement) {
-						$dropelement.remove();
-						$dropelement = false;
-					}
-					$sortables.removeClass('ccm-area-drag-active');
-		 			$("div.ccm-block-edit").removeClass('ccm-block-arrange-enabled');
-		 			$('div.ccm-block-edit').draggable().draggable('destroy');
-
+	
 				},
 				connectToSortable: $sortables
 			});
@@ -167,11 +149,9 @@ var CCMEditMode = function() {
 	}
 
 	return {
-		start: function() {
-			
+		start: function() {			
 			setupMenus();
-			//setupBlockMovement();
-
+			setupBlockMovement();
 		},
 
 		setupBlockForm: function(form, bID, task) {
