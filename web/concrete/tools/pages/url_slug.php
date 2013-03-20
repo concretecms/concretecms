9a4b1d1a540d1c9b8d95db6ce00725c757a7f284
Andrew Embler <andrew@concrete5.org>
2013-03-20 16:08:09 -0700

    url slug update

--- web/concrete/tools/pages/url_slug.php
+++ web/concrete/tools/pages/url_slug.php
@@ -1,9 +1,8 @@
 <?
 defined('C5_EXECUTE') or die("Access Denied.");
 if (Loader::helper('validation/token')->validate('get_url_slug', $_REQUEST['token'])) {
-	Loader::library('3rdparty/urlify');
-	$name = URLify::filter($_REQUEST['name']);
-
+	$text = Loader::helper('text');
+	$name = $text->urlify($_REQUEST['name']);
 	$ret = Events::fire('on_page_urlify', $_REQUEST['name']);
 	if ($ret) {
   		$name = $ret;
