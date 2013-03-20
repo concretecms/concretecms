e8ff59061b871bed2b56e6cfc19d4aeee91aec05
Korvin Szanto <korvinszanto@gmail.com>
2013-03-20 13:16:47 -0700

    Remove old banned words
    Former-commit-id: 12c50b223882d095f13988a548b6c9bd3e29dbed

--- web/concrete/core/helpers/validation/banned_words.php
+++ web/concrete/core/helpers/validation/banned_words.php
@@ -4,7 +4,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 /**
  * Helper functions for catching or stripping dirty words from content
  * @package Helpers
- * @category Concrete 
+ * @category Concrete
  * @subpackage Validation
  * @author  Ben / Tony Trupp <tony@concretecms.com>
  * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
@@ -26,40 +26,14 @@ define("STRING_UTILS_TRUNCATE_PARS",    '\n{2,}');
 class Concrete5_Helper_Validation_BannedWords {
 
     public $bannedWords;
-	
-    function getCSV_simple($file){
-		$lines = file($file);
-		$out = array();
-		foreach ($lines as $line) {
-			$aline = explode(",",str_rot13($line));
-			$oline = array();
-			foreach ($aline as $val) {
-			$oline[] = trim($val);
-			}
-			$out[] = $oline;
-		}
-		return $out;
-    }
-	
+
+    function getCSV_simple($file){}
+
     function loadBannedWords(){
-		if (isset($this->bannedWords)&&is_array($this->bannedWords)) return FALSE;
-		$file = DIR_BASE."/config/banned_words.txt";
-		if(!file_exists($file)) 
-			$file = DIR_BASE_CORE."/config/banned_words.txt";
-		if (!file_exists($file)) {
-			$this->errorMsg= t("Banned words list not found.");
-			return $this->errorMsg;
-		}
-		if (!is_readable($file)) {
-			$this->errorMsg = t("Banned words list has improper permissions.");
-			return $this->errorMsg;
-		}
-		$wb = ValidationBannedWordsHelper::getCSV_simple($file);
-		foreach ($wb as $aline) {
-			$this->bannedWords[$aline[0]] = $aline[1];
-		}
+		$bw = new BannedWords();
+		$this->bannedWords = $bw->get();
     }
-	
+
     function wordCase($word){
 		$lower = "abcdefghijklmnopqrstuvwxyz";
 		$UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -85,10 +59,10 @@ class Concrete5_Helper_Validation_BannedWords {
 		}
 		return $case;
     }
-	
+
     function forceCase($case, &$word){
 		$word = strtolower($word);
-		if ($case & STRING_UTILS_CASE_FIRST_UPPER) 
+		if ($case & STRING_UTILS_CASE_FIRST_UPPER)
 			$word = ucfirst($word);
 		$c = $case & STRING_UTILS_CASE_MIXED;
 		$i = 1;
@@ -101,80 +75,23 @@ class Concrete5_Helper_Validation_BannedWords {
 			$i++;
 		}
     }
-	
+
     function isBannedWord(&$word){
-		$case = ValidationBannedWordsHelper::wordCase($word);
-		$nword = strtolower($word);
-		if ($msg = $this->loadBannedWords()) {
-			//print "<span class=\"error\">$msg</span>";
-			return FALSE;
-		}
-		if (isset($this->bannedWords[$nword])) {
-			$word = $this->bannedWords[$nword];
-			ValidationBannedWordsHelper::forceCase($case, $word);
-			return TRUE;
-		}
-		return FALSE;
+		return in_array(strtolower($word), $this->bannedWords);
     }
-	
-    function hasBannedWords(&$string){ 
-		$alpha = "abcdefghijklmnopqrstuvwxyz";
-		$alpha .= strtoupper($alpha);
-		$start = $end = 0;
-		$ra    = 0;
-		$i     = 0;
-		$out   = 0;
-		while ($c = $string[$i]) {
-			if ($ra) {
-			if (strpos($alpha, $c)!==FALSE) {
-			} else {
-				$ra = 0;
-				$end = $i;
-				$word = substr($string, $start, $end-$start);
-				if ($this->isBannedWord($word)) {
-				$out++;
-				$string = substr($string, 0, $start).
-					  $word.
-					  substr($string,$end);
-				}
-			}
-			} else {
-			if (strpos($alpha, $c)!==FALSE) {
-				$ra    = 1;
-				$start = $i;
-			} else {
-			}
-			}
-			$i++;
-		}
-		if ($ra) {
-			$word = substr($string, $start);
-			if ($this->isBannedWord($word)) {
-			$out++;
-			$string = substr($string, 0, $start).
-				  $word;
-		
-			}
-		}
-		if(strlen($this->errorMsg) && !$this->errorMsgDisplayed) {
-			//echo "<div class=\"infoBox\">$this->errorMsg</div>";	
-			//$this->errorMsgDisplayed=1;
-		}	
-		return $out;
+
+    function hasBannedWords(&$string){
+    	return (strlen($string) == str_replace($this->bannedWords, array_pad(array(), count($this->bannedWords), ''), strtolower($string)));
     }
-	
+
     function hasBannedPart($string){
-		if ($msg = $this->loadBannedWords()) {
-			print "<span class=\"error\">$msg</span>";
-			return FALSE;
-		}
 		$string = strtolower($string);
 		foreach ($this->bannedWords as $bw => $replacement) {
 			if (strpos($string, $bw)!==FALSE) return TRUE;
 		}
 		return FALSE;
     }
-	
+
     function truncate($string, $num, $which=STRING_UTILS_TRUNCATE_CHARS, $ellipsis="&#8230;"){
 		$parts = preg_split("/($which)/", $string, -1, PREG_SPLIT_DELIM_CAPTURE);
 		$i = 0;
@@ -188,7 +105,7 @@ class Concrete5_Helper_Validation_BannedWords {
 
 
 	public function getBannedKeys($inputArray) {
-		$error_keys = array();	
+		$error_keys = array();
 		if(is_array($inputArray) && count($inputArray)) {
 			foreach(array_keys($inputArray) as $k) {
 				 if(is_string($inputArray[$k]) && $this->hasBannedWords( $inputArray[$k])) {
@@ -200,7 +117,7 @@ class Concrete5_Helper_Validation_BannedWords {
 							break;
 						}
 					}
-				 } 
+				 }
 			}
 		}
 		return $error_keys;
