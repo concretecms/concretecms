e2105165fc3b768f66a125186edd8d525b35652e
Korvin Szanto <korvinszanto@gmail.com>
2013-03-22 18:01:05 -0700

    Pass page and block id up to view_ajax

--- web/concrete/blocks/core_conversation/view.php
+++ web/concrete/blocks/core_conversation/view.php
@@ -3,27 +3,28 @@ $paginate = ($paginate) ? 'true' : 'false';
 $itemsPerPage = ($paginate) ? $itemsPerPage : -1;
 
 if (is_object($conversation)) {
-?>
-<div class="ccm-conversation-wrapper" data-conversation-id="<?=$conversation->getConversationID()?>">
-<?=t('Loading Conversation')?> <img src="<?=Loader::helper('concrete/urls')->getBlockTypeAssetsURL($b->getBlockTypeObject(), 'loading.gif')?>" />
-</div>
+	?>
+	<div class="ccm-conversation-wrapper" data-conversation-id="<?=$conversation->getConversationID()?>">
+	<?=t('Loading Conversation')?> <img src="<?=Loader::helper('concrete/urls')->getBlockTypeAssetsURL($b->getBlockTypeObject(), 'loading.gif')?>" />
+	</div>
 
-<script type="text/javascript">
-$(function() {
-	$('div[data-conversation-id=<?=$conversation->getConversationID()?>]').ccmconversation({
-		cnvID: <?=$conversation->getConversationID()?>,
-		blockID: <?=$bID?>,
-		posttoken: '<?=$posttoken?>',
-		displayMode: '<?=$displayMode?>',
-		paginate: <?=$paginate?>,
-		itemsPerPage: <?=$itemsPerPage?>,
-		orderBy: '<?=$orderBy?>',
-		enableOrdering: <?=$enableOrdering?>,
-		displayPostingForm: '<?=$displayPostingForm?>',
-		insertNewMessages: '<?=$insertNewMessages?>',
-		activeUsers: <?=Loader::helper('json')->encode($users)?>,
-		enableCommentRating: <?=$enableCommentRating?>
+	<script type="text/javascript">
+	$(function() {
+		$('div[data-conversation-id=<?=$conversation->getConversationID()?>]').ccmconversation({
+			cnvID: <?=$conversation->getConversationID()?>,
+			blockID: <?=$bID?>,
+			cID: <?=$cID?>,
+			posttoken: '<?=$posttoken?>',
+			displayMode: '<?=$displayMode?>',
+			paginate: <?=$paginate?>,
+			itemsPerPage: <?=$itemsPerPage?>,
+			orderBy: '<?=$orderBy?>',
+			enableOrdering: <?=$enableOrdering?>,
+			displayPostingForm: '<?=$displayPostingForm?>',
+			insertNewMessages: '<?=$insertNewMessages?>',
+			activeUsers: <?=Loader::helper('json')->encode($users)?>,
+			enableCommentRating: <?=$enableCommentRating?>
+		});
 	});
-});
-</script>
+	</script>
 <? } ?>
