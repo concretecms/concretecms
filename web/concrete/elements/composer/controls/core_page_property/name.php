44482f9410d2f56b35aed43064deb4a4f182d087
Andrew Embler <andrew@concrete5.org>
2013-03-28 17:33:55 -0700

    url slug composer
    Former-commit-id: 7a49137343656d2ee0a6317609c6dd4b01bed985

--- web/concrete/elements/composer/controls/core_page_property/name.php
+++ web/concrete/elements/composer/controls/core_page_property/name.php
@@ -4,7 +4,28 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 <div class="control-group">
 	<label class="control-label"><?=$label?></label>
-	<div class="controls">
+	<div class="controls" data-composer-field="name">
 		<?=$form->text($this->field('name'))?>
 	</div>
 </div>
+
+<script type="text/javascript">
+var ccm_composerAddPageTimer = false;
+$(function() {
+	$('div[data-composer-field=name] input').on('keyup', function() {
+		var val = $(this).val();
+		var frm = $(this);
+		clearTimeout(ccm_composerAddPageTimer);
+		ccm_composerAddPageTimer = setTimeout(function() {
+			$('#ccm-url-slug-loader').show();
+			$.post('<?=REL_DIR_FILES_TOOLS_REQUIRED?>/pages/url_slug', {
+				'token': '<?=Loader::helper('validation/token')->generate('get_url_slug')?>',
+				'name': val
+			}, function(r) {
+				$('#ccm-url-slug-loader').hide();
+				$('div[data-composer-field=url_slug] input').val(r);
+			});
+		}, 150);
+	});
+});
+</script>
--- web/concrete/elements/composer/controls/core_page_property/url_slug.php
+++ web/concrete/elements/composer/controls/core_page_property/url_slug.php
@@ -4,7 +4,9 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 <div class="control-group">
 	<label class="control-label"><?=$label?></label>
-	<div class="controls">
+	<div class="controls" data-composer-field="url_slug">
 		<?=$form->text($this->field('url_slug'))?>
+		<img src="<?=ASSETS_URL_IMAGES?>/loader_intelligent_search.gif" width="43" height="11" id="ccm-url-slug-loader" style="display: none" />
 	</div>
 </div>
+
