694b1e8db8c1cfdeaa3b7b720acd23a502f28c20
Andrew Embler <andrew@concrete5.org>
2013-04-04 12:40:57 -0700

    finishing composer js rebuild
    Former-commit-id: d9f32e6cbf59b11898e05a5c79ab3eeda70373a6

--- web/concrete/js/composer/composer.js
+++ web/concrete/js/composer/composer.js
@@ -6,27 +6,94 @@
 
     private:  {
 
-      saveDraft: function($this) {
-        console.log($this);
-        console.log($this.data('settings'));
-      },
+      saveDraft: function($f, onComplete) {        
+        formData = $f.serializeArray();
+        $.ajax({
+          dataType: 'json',
+          type: 'post',
+          data: formData,
+          url: $f.data('saveURL'),
+          success: function(r) {
+            $f.find('.ccm-composer-save-status').html('<div class="alert alert-info"><?=t("Page saved at ")?>' + r.time + '</div>');
+            if (r.saveurl) {
+              $f.data('saveURL', r.saveurl);
+            }
+            if (r.discardurl) {
+              $f.data('discardURL', r.discardurl);
+            }
+            if (r.publishurl) {
+              $f.data('publishURL', r.publishurl);
+            }
+            if (onComplete) {
+              onComplete();
+            }
+          }
+      });
+    }
 
-    },
+  },
 
     init: function(options) {
 
       var settings = $.extend({
-        autoSaveTimeout: 5000
+        autoSaveEnabled: true,
+        autoSaveTimeout: 5000,
+        onExit: function() {
+          window.location.href = CCM_DISPATCHER_FILENAME + '/dashboard/composer/drafts';
+        },
+        onAfterDiscard: function() {
+          window.location.href = CCM_DISPATCHER_FILENAME + '/dashboard/composer/drafts';
+        },
+        onAfterSaveAndExit: function() {
+          window.location.href = CCM_DISPATCHER_FILENAME + '/dashboard/composer/drafts';
+        }        
       }, options);
 
       return this.each(function() {
 
         var $this = $(this);
         $this.data('settings', settings);
+        $this.data('saveURL', settings.saveURL);
+        $this.data('discardURL', settings.discardURL);
+        $this.data('publishURL', settings.publishURL);
 
-        methods.saveinternal = setInterval(function() {
-          methods.private.saveDraft($this);
-        }, settings.autoSaveTimeout);       
+        if (settings.autoSaveEnabled) {
+          methods.saveinternal = setInterval(function() {
+            methods.private.saveDraft($this);
+          }, settings.autoSaveTimeout);
+        }
+
+        $this.find('button[data-composer-btn=exit]').on('click', function() {
+          settings.onExit();
+        });
+
+        $this.find('button[data-composer-btn=discard]').on('click', function() {
+          if (!$this.data('discardURL')) {
+            settings.onAfterDiscard();
+          } else {
+            $.ajax({
+              type: 'get',
+              url: $this.data('discardURL'),
+              success: function(r) {
+                settings.onAfterDiscard();
+              }
+            });
+          }
+        });
+
+        $this.find('button[data-composer-btn=save]').on('click', function() {
+          clearInterval(methods.saveinterval);
+          methods.private.saveDraft($this, function() {
+            settings.onAfterSaveAndExit();
+          });
+        });
+
+
+        $this.find('button[data-composer-btn=publish]').on('click', function() {
+          clearInterval(methods.saveinterval);
+          $this.attr('action', $this.data('publishURL'));
+          $this.submit();
+        });
 
       });
 
