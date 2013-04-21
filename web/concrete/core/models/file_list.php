c70379c1e797895e0d30f96c8399538508016d9e
VahanPWNS <vahanpwns@gmail.com>
2013-04-22 01:51:59 +0200

    Update file_list.php
    Additional check to make sure the ZipArchive extension is loaded and revert to the old behaviour if it isn't. Added a try-catch in case they both fail. Thanks Remo!
    
    Former-commit-id: 72c3357477e543a041bebe7431f1b54092ca37e3

--- web/concrete/core/models/file_list.php
+++ web/concrete/core/models/file_list.php
@@ -100,18 +100,25 @@ class Concrete5_Model_FileList extends DatabaseItemList {
 		$filenames = array();
 		$filename = $fh->getTemporaryDirectory() . '/' . $archive . '.zip';
 		if (count($files) > 0) {
-			$zip = new ZipArchive;
-			$res = $zip->open($filename, ZipArchive::CREATE);
-			if ($res === TRUE) {
-				foreach($files as $f)
-					if (!in_array(basename($f->getPath()), $filenames))
-						$zip->addFile(addslashes($f->getPath()), basename($f->getPath()));
-				$zip->close();
-			} else {
-				die('Failed to create zip file as '.$filename);
+			try {
+				if (class_exists('ZipArchive', false)) {
+					$zip = new ZipArchive;
+					$res = $zip->open($filename, ZipArchive::CREATE);
+					if ($res === TRUE) {
+						foreach($files as $f)
+							if (!in_array(basename($f->getPath()), $filenames))
+								$zip->addFile(addslashes($f->getPath()), basename($f->getPath()));
+						$zip->close();
+					} else {
+						die('Failed to create zip file as '.$filename);
+					}
+				} else {
+					exec(DIR_FILES_BIN_ZIP . ' -j \'' . addslashes($filename) . '\' ' . $filestring);
+				}
+			}catch(Exception $e) {
+				die('Failed to create zip file as "'.$filename.'": '.$e->getMessage());
 			}
 		}
-
 	}
 	
 	protected function setupFileSetFilters() {	
