1f0a0a832f22be1f33bf5be9bfacf006154536a3
Andrew Embler <andrew@concrete5.org>
2013-04-30 15:15:09 -0700

    ui work
    Former-commit-id: b701dce9703e67e0cc434d4b8568ca31e38c978e

--- web/concrete/themes/twitter_bootstrap/elements/header.php
+++ web/concrete/themes/twitter_bootstrap/elements/header.php
@@ -3,10 +3,11 @@
 <html lang="en">
 <head>
 
-<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
 
 <? Loader::element('header_required'); ?>
+
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 	
 </head>
 <body>
