4562445a71a42d6c5749bcf366bcb3c8fc830664
Andrew Embler <andrew@concrete5.org>
2013-05-10 15:58:45 -0700

    progress bar
    Former-commit-id: 0a4c0ca598df4f25fdd09385e13043902dd55d93

--- build/js.sh
+++ build/js.sh
@@ -51,6 +51,7 @@ cat ../web/concrete/js/ccm_app/jquery.colorpicker.js \
 ../web/concrete/js/ccm_app/in_context_menu.js \
 ../web/concrete/js/ccm_app/quicksilver.js \
 ../web/concrete/js/ccm_app/remote_marketplace.js \
+../web/concrete/js/ccm_app/progressive_operations.js \
 ../web/concrete/js/ccm_app/inline_edit.js \
 ../web/concrete/js/ccm_app/search.js \
 ../web/concrete/js/ccm_app/dynatree.js \
