b5245e2877fde1f403654d09024e2c86fc525c49
Jordan Lev <github@jordanlev.com>
2013-05-15 02:04:01 -0500

    fix next_prev block bug that showed excluded pages in some cases
    fix bug in next_prev block where last page in section gets returned even if it has the "exclude_nav" attribute.

--- web/concrete/core/controllers/blocks/next_previous.php
+++ web/concrete/core/controllers/blocks/next_previous.php
@@ -93,6 +93,8 @@ class Concrete5_Controller_Block_NextPrevious extends BlockController {
 				$cp = new Permissions($page);
 				if ($cp->canRead() && $page->getAttribute('exclude_nav') != 1) {
 					break;
+				} else {
+					$page = null; //avoid accidentally returning this $page if we're on last loop iteration
 				}
 			}
 		}
@@ -129,6 +131,8 @@ class Concrete5_Controller_Block_NextPrevious extends BlockController {
 				$cp = new Permissions($page);
 				if ($cp->canRead() && $page->getAttribute('exclude_nav') != 1) {
 					break;
+				} else {
+					$page = null; //avoid accidentally returning this $page if we're on last loop iteration
 				}
 			}
 		}
