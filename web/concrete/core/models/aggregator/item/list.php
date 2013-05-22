00c89e43cbdcef63732e56c001466102b70054f2
Andrew Embler <andrew@concrete5.org>
2013-05-22 13:36:56 -0700

    aggregator
    Former-commit-id: 7ff66e7ba80ab99b96592b8cd133a59f09ef2a2a

--- web/concrete/core/models/aggregator/item/list.php
+++ web/concrete/core/models/aggregator/item/list.php
@@ -2,7 +2,7 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 class Concrete5_Model_Aggregator_Item_List extends DatabaseItemList {
 
-	protected $itemsPerPage = 40;
+	protected $itemsPerPage = 24;
 
 	public function __construct(Aggregator $ag) {
 		$this->setQuery('select agiID from AggregatorItems');
