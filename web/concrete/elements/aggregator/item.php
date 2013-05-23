89df78c2c6476e0eef4c4f03d8accb6860fc03b2
Andrew Embler <andrew@concrete5.org>
2013-05-22 17:41:32 -0700

    aggregator

---
+++ web/concrete/elements/aggregator/item.php
@@ -0,0 +1,11 @@
+<? defined('C5_EXECUTE') or die("Access Denied."); ?>
+<div data-aggregator-item-batch-timestamp="<?=$item->getAggregatorItemBatchTimestamp()?>" data-aggregator-item-id="<?=$item->getAggregatorItemID()?>" class="ccm-aggregator-item h<?=$item->getAggregatorItemSlotHeight()?> w<?=$item->getAggregatorItemSlotWidth()?>">
+  <div class="ccm-aggregator-item-inner">
+  <? if ($showTileCommands) { ?>
+    <ul class="ccm-aggregator-item-inline-commands ccm-ui">
+      <li class="ccm-aggregator-item-inline-move"><a data-inline-command="move-tile" href="#"><i class="icon-move"></i></a></li>
+      <li class="ccm-aggregator-item-inline-options"><a data-inline-command="options-tile" href="#"><i class="icon-cog"></i></a></li>
+    </ul>
+  <? } ?>
+  <? $item->render(); ?></div>
+</div>
