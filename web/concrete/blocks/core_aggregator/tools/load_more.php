bb29cfcc8924318a9ebda2f646c4b997b1fdecdb
Andrew Embler <andrew@concrete5.org>
2013-05-22 17:41:32 -0700

    aggregator
    Former-commit-id: 89df78c2c6476e0eef4c4f03d8accb6860fc03b2

---
+++ web/concrete/blocks/core_aggregator/tools/load_more.php
@@ -0,0 +1,28 @@
+<? defined('C5_EXECUTE') or die("Access Denied.");
+
+$nh = Loader::helper('validation/numbers');
+if($_POST['bID'] && $_POST['cID'] && $nh->integer($_POST['bID']) && $nh->integer($_POST['cID'])) {
+  $c = Page::getByID($_POST['cID']);
+  if (is_object($c) && !$c->isError()) { 
+    $a = Area::get($c, $_POST['arHandle']);
+    $b = Block::getByID($_POST['bID'],$c, $a);
+    if ($b->getBlockTypeHandle() == BLOCK_HANDLE_SCRAPBOOK_PROXY) {
+      $cnt = $b->getController();
+      $b = Block::getByID($cnt->getOriginalBlockID());
+    }
+
+    $bp = new Permissions($b);
+    if ($bp->canViewBlock() && Loader::helper('validation/token')->validate('get_aggregator_items', $_POST['token'])) {
+      $controller = $b->getController();
+      if ($controller instanceof CoreAggregatorBlockController) {
+        $aggregator = $controller->getAggregatorObject();
+        $list = new AggregatorItemList($aggregator);
+        $list->sortByDateDescending();
+        $items = $list->getPage($_REQUEST['page']);
+        foreach($items as $item) {
+          Loader::element('aggregator/item', array("item" => $item));
+        }
+      }
+    }
+  }
+}
