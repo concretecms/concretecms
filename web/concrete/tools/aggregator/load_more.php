e7b2fe1e296c4ff5bb6f23e289d1b64c749f964a
Andrew Embler <andrew@concrete5.org>
2013-05-24 10:53:11 -0700

    edit aggregator
    Former-commit-id: 1473dfa3cff81d36545fdc2fd298bdc77e108448

---
+++ web/concrete/tools/aggregator/load_more.php
@@ -0,0 +1,15 @@
+<? defined('C5_EXECUTE') or die("Access Denied.");
+
+$nh = Loader::helper('validation/numbers');
+if ($_POST['agID'] && $nh->integer($_POST['agID'])) {
+  $aggregator = Aggregator::getByID($_POST['agID']);
+  if (is_object($aggregator) && Loader::helper('validation/token')->validate('get_aggregator_items', $_POST['loadToken'])) {
+    $showTileCommands = ($_POST['showTileCommands'] && Loader::helper('validation/token')->validate('update_aggregator_items', $_POST['editToken']));
+    $list = new AggregatorItemList($aggregator);
+    $list->sortByDateDescending();
+    $items = $list->getPage($_REQUEST['page']);
+    foreach($items as $item) {
+      Loader::element('aggregator/item', array("item" => $item, 'showTileCommands' => $showTileCommands));
+    }
+  }
+}
