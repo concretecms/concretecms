1ad917d745fc0f4cfaa390cf36b4c8b01f6d846c
Andrew Embler <andrew@concrete5.org>
2013-05-30 15:42:31 -0700

    doin the good work

--- web/concrete/blocks/core_aggregator/add.php
+++ web/concrete/blocks/core_aggregator/add.php
@@ -1,3 +1,21 @@
 <?
 defined('C5_EXECUTE') or die("Access Denied.");
-$this->inc('form.php');
+?>
+
+<?=Loader::helper('concrete/interface')->tabs(array(
+	array('sources', t('Data Sources'), true),
+	array('output', t('Output'))
+));?>
+
+
+<div class="ccm-tab-content" id="ccm-tab-content-sources">
+<?
+$this->inc('form/sources.php');
+?>
+</div>
+
+<div class="ccm-tab-content" id="ccm-tab-content-output">
+<?
+$this->inc('form/output.php');
+?>
+</div>
