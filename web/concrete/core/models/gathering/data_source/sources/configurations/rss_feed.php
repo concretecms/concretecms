bfeca357dc9991cd3f5ee2216b444bc252c84219
Greg Joyce <greg@concretecms.com>
2013-06-04 12:51:59 -0700

    data sources
    Former-commit-id: c6be25f05a99d0c78972cc25ee05a8548a051a2d

--- web/concrete/core/models/gathering/data_source/sources/configurations/rss_feed.php
+++ web/concrete/core/models/gathering/data_source/sources/configurations/rss_feed.php
@@ -1,6 +1,6 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-class Concrete5_Model_RssFeedAggregatorDataSourceConfiguration extends AggregatorDataSourceConfiguration  {
+class Concrete5_Model_RssFeedGatheringDataSourceConfiguration extends GatheringDataSourceConfiguration  {
 
 	public function setRssFeedURL($url) {
 		$this->url = $url;
--- web/concrete/core/models/gathering/data_source/sources/page.php
+++ web/concrete/core/models/gathering/data_source/sources/page.php
@@ -1,22 +1,22 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-class Concrete5_Model_PageAggregatorDataSource extends AggregatorDataSource {
+class Concrete5_Model_PageGatheringDataSource extends GatheringDataSource {
 
-	public function createConfigurationObject(Aggregator $ag, $post) {
-		$o = new PageAggregatorDataSourceConfiguration();
+	public function createConfigurationObject(Gathering $ga, $post) {
+		$o = new PageGatheringDataSourceConfiguration();
 		if ($post['ctID']) {
 			$o->setCollectionTypeID($post['ctID']);
 		}
 		return $o;
 	}
 	
-	public function createAggregatorItems(AggregatorDataSourceConfiguration $configuration) {
+	public function createGatheringItems(GatheringDataSourceConfiguration $configuration) {
 		$pl = new PageList();
 		$pl->ignoreAliases();
 		$pl->ignorePermissions();
-		$aggregator = $configuration->getAggregatorObject();
-		if ($aggregator->getAggregatorDateLastUpdated()) {
-			$pl->filterByPublicDate($aggregator->getAggregatorDateLastUpdated(), '>');
+		$gathering = $configuration->getGatheringObject();
+		if ($gathering->getGatheringDateLastUpdated()) {
+			$pl->filterByPublicDate($gathering->getGatheringDateLastUpdated(), '>');
 		}
 		$ctID = $configuration->getCollectionTypeID();
 		if ($ctID > 0) {
@@ -25,7 +25,7 @@ class Concrete5_Model_PageAggregatorDataSource extends AggregatorDataSource {
 		$pages = $pl->get();
 		$items = array();
 		foreach($pages as $c) {
-			$item = PageAggregatorItem::add($configuration, $c);
+			$item = PageGatheringItem::add($configuration, $c);
 			if (is_object($item)) {
 				$items[] = $item;
 			}
