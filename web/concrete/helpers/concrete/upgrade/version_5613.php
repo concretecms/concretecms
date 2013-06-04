29e124e7e0c8a47236d55fcd252f13a9f54aa387
Mnkras <mnkras@gmail.com>
2013-06-04 01:30:32 -0400

    Changed the max length of a custom template name to 255 chars from 32 chars
    Former-commit-id: 34913ba87b223c6d3efb4dd93b1299cafbfb4465

---
+++ web/concrete/helpers/concrete/upgrade/version_5613.php
@@ -0,0 +1,17 @@
+<?
+
+defined('C5_EXECUTE') or die("Access Denied.");
+class ConcreteUpgradeVersion5613Helper {
+
+	
+	public $dbRefreshTables = array(
+		'Blocks'
+	);
+
+	public function run() {
+		
+
+	}
+
+	
+}
