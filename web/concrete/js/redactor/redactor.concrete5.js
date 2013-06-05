0cf27d8fcd640130522af1f68045c3f4f5c1cd4c
Andrew Embler <andrew@concrete5.org>
2013-06-05 13:53:44 -0700

    more work on redactor

--- web/concrete/js/redactor/redactor.concrete5.js
+++ web/concrete/js/redactor/redactor.concrete5.js
@@ -8,7 +8,7 @@ RedactorPlugins.concrete5inline = {
 		//this.$toolbar.css({ position: 'fixed', width: '100%', zIndex: 999, top: '50px', left: '0px' });
 		//this.$toolbar.append($('<li id="ccm-redactor-actions-buttons" class="ccm-ui"><button id="ccm-redactor-cancel-button" type="button" class="btn btn-mini">Cancel</button><button id="ccm-redactor-save-button" type="button" class="btn btn-primary btn-mini">Save</button></li>'));
 		//this.$toolbar.appendTo($(document.body));
-		this.$toolbar.removeClass().addClass("ccm-inline-toolbar");
+		this.$toolbar.addClass("ccm-inline-toolbar");
 		var toolbar = this.$toolbar;
 		var editor = this.$editor;
 
