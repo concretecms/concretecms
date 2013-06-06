330ce9fc3c24bdd7ad43bfd23c3aeb46fd1cfd29
Korvin Szanto <korvinszanto@gmail.com>
2013-06-06 10:51:18 -0700

    Runtime hash
    Former-commit-id: 90fb60ec61bfdaad926dbb37096ab54235638f3d

--- web/concrete/dispatcher.php
+++ web/concrete/dispatcher.php
@@ -5,6 +5,10 @@
 		define('C5_EXECUTE', true);
 	}
 
+	if (!defined('RUNTIME')) {
+		define('RUNTIME', md5(uniqid()));
+	}
+
 	if(defined("E_DEPRECATED")) {
 		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED); // E_DEPRECATED required for php 5.3.0 because of depreciated function calls in 3rd party libs (adodb).
 	} else {
