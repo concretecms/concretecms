77db4adc1e001a8fb477c42477484a39a7583cdf
Andrew Embler <andrew@concrete5.org>
2013-06-11 11:08:02 -0700

    gathering stuff
    Former-commit-id: b1ce6bdd833b7165b3cdca6b368642059392a0a4

--- web/concrete/core/models/page_theme/twitter_bootstrap.php
+++ web/concrete/core/models/page_theme/twitter_bootstrap.php
@@ -4,4 +4,16 @@ class Concrete5_Model_PageTheme_TwitterBootstrap extends PageTheme {
 
 	protected $ptGridFrameworkHandle = 'bootstrap2';
 
+	public function getThemeGatheringGridItemMargin() {
+		return 20;
+	}
+
+	public function getThemeGatheringGridItemWidth() {
+		return 146;
+	}
+
+	public function getThemeGatheringGridItemHeight() {
+		return 146;
+	}
+
 }
