dbda6c280c7bb30b847b10bbb94e405d29920565
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2013-06-12 18:24:47 +0300

    Fixed httpS issues (unsafe content) in some places.

--- web/concrete/helpers/concrete/avatar.php
+++ web/concrete/helpers/concrete/avatar.php
@@ -219,7 +219,7 @@ class ConcreteAvatarHelper {
    * @source http://gravatar.com/site/implement/images/php/
    */
   function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
-	  $url = 'http://www.gravatar.com/avatar/';
+	  $url = '//www.gravatar.com/avatar/';
 	  $url .= md5( strtolower( trim( $email ) ) );
 	  $url .= "?s=$s&d=$d&r=$r";
 	  if ( $img ) {
