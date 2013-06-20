e61eabf5b818379ff36d320490f7552c2cbe423a
Andrew Embler <andrew@concrete5.org>
2013-06-20 16:20:31 -0700

    style tweaks
    Former-commit-id: 84b48233a6c8a5f6dfc19c512a44e772bb3a980a

--- web/concrete/single_pages/dashboard/sitemap/full.php
+++ web/concrete/single_pages/dashboard/sitemap/full.php
@@ -13,16 +13,9 @@ $sh = Loader::helper('concrete/dashboard/sitemap');
 <div class="ccm-pane-options">
 	<a href="javascript:void(0)" onclick="ccm_paneToggleOptions(this)" class="ccm-icon-option-<? if ($_SESSION['dsbSitemapShowSystem'] == 1) { ?>open<? } else { ?>closed<? } ?>"><?=t('Options')?></a>
 	<div class="ccm-pane-options-content" <? if ($_SESSION['dsbSitemapShowSystem'] == 1) { ?> style="display: block" <? } ?>>
-		<form>
-		<div id="ccm-show-all-pages" class="clearfix">
-			<label for="ccm-show-all-pages-cb"><?=t('Show System Pages')?></label>
-			<div class="input">
-			<ul class="inputs-list">
-				<li><input type="checkbox" id="ccm-show-all-pages-cb" <? if ($_SESSION['dsbSitemapShowSystem'] == 1) { ?> checked <? } ?> /></li>
-			</ul>		
-			</div>
-		</div>
-		</form>
+		<label for="ccm-show-all-pages-cb" class="checkbox"><?=t('Show System Pages')?>
+			<input type="checkbox" id="ccm-show-all-pages-cb" <? if ($_SESSION['dsbSitemapShowSystem'] == 1) { ?> checked <? } ?> />
+		</label>
 	</div>
 </div>
 <div class="ccm-pane-body ccm-pane-body-footer">
