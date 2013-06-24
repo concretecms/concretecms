18d32b15baa14a1a5449de2aa7eb964b1646066a
Andrew Embler <andrew@concrete5.org>
2013-06-24 10:21:19 -0700

    need to actually destroy dialog on close
    Former-commit-id: 538e4484e84330303955ef8d5d724846593a767e

--- web/concrete/js/ccm_app/dialog.js
+++ web/concrete/js/ccm_app/dialog.js
@@ -141,6 +141,7 @@ jQuery.fn.dialog.open = function(options) {
 			}
 		},
 		'close': function(ev, u) {
+			$(this).jqdialog('destroy').remove();
 			if (typeof options.onClose != "undefined") {
 				if ((typeof options.onClose) == 'function') {
 					options.onClose();
