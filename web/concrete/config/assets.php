34b6af47913689311505a15a68ba2d55b1eab221
Andrew Embler <andrew@concrete5.org>
2013-07-06 13:40:37 -0700

    making oassets not require on_page_view
    Former-commit-id: 2f2bd3dba5ed10dc7d0326e87d57fbea9c3f9037

--- web/concrete/config/assets.php
+++ web/concrete/config/assets.php
@@ -2,6 +2,8 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 
 $al = AssetList::getInstance();
+/*
 $al->register('gathering');
 $al->register('bootstrap/dropdown');
 $al->register('jqueryui/effects/bloom');
+*/
