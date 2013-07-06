5872462b77e4257623da6f14bb0269b25ab0f482
Andrew Embler <andrew@concrete5.org>
2013-07-06 14:15:27 -0700

    more work

--- web/concrete/core/libraries/asset/file/types/javascript.php
+++ web/concrete/core/libraries/asset/file/types/javascript.php
@@ -3,8 +3,16 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 class Concrete5_Library_JavaScriptAssetFile extends AssetFile {
 	
-	public function getAssetFilePosition() {
+	public function getAssetFileDefaultPosition() {
 		return AssetFile::ASSET_FILE_POSITION_HEADER;
+	}
+
+	public function getAssetFileDefaultMinify() {
+		return true;
+	}
+
+	public function getAssetFileDefaultCombine() {
+		return true;
 	}
 
 }
