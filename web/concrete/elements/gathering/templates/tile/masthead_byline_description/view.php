3c35e1c1cd976ddf1e0c72bb8a0c157e03a0f606
Evan Cooper <evan@concretecms.com>
2013-07-09 15:00:12 -0700

    stubbing out gathering tile templates for vimeo, twitter, adding temporary styles to bootstrap theme header
    Former-commit-id: 7196e013a7922c2fa31026d7f78daeb43d83ced1

--- web/concrete/elements/gathering/templates/tile/masthead_byline_description/view.php
+++ web/concrete/elements/gathering/templates/tile/masthead_byline_description/view.php
@@ -2,15 +2,16 @@
 $ownerID = $this->page->vObj->cvAuthorUID;
 $u = User::getByUserID($ownerID);
 $ownerName = $u->getUserName();
+$nh = Loader::helper('navigation');
 ?>
 <div class="ccm-gathering-masthead-byline-description">
-
 	<div class="ccm-gathering-tile-title-description">
 		<div class="ccm-gathering-tile-headline"><a href="<?=$link?>"><?=$title?></a></div>
-		<div class="ccm-gathering-tile-byline"><?php echo t('By '). '<span class="author-name">' .$ownerName. '</span>' ?></div>
+		<div class="ccm-gathering-tile-byline"><?php echo t('by '). '<span class="author-name">' .$ownerName. '</span>' ?></div>
 		<div class="ccm-gathering-tile-description">
 		<?=$description?>
 		</div>
+		<div class="ccm-gathering-tile-read-more"><a href="<?php echo $nh->getCollectionURL($this->page); ?>"><?php echo t('Read More') ?></a></div>
 	</div>
 	<div class="clearfix" style="clear: both;"></div>
 </div>
---
+++ web/concrete/elements/gathering/templates/tile/twitter/view.php
@@ -0,0 +1,9 @@
+<? defined('C5_EXECUTE') or die("Access Denied."); 
+?>
+<div class="ccm-gathering-twitter">
+	<img class="twitter-logo" src="https://abs.twimg.com/a/1373252541/images/resources/twitter-bird-light-bgs.png" />
+	<div class="tweet">
+		<p>This is awesome - Out on beta now! concrete5, 5.7 <a href="#">http://concrete.lk/R5IM @concrete5 #cms #awesome #webdesign</a></p>
+		<p><span class="elapsed">3 min ago</span><span class="who-from">CMS_Review</span></p>
+	</div>
+</div>
