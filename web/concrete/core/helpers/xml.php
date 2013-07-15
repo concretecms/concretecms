fd0f6d25d7f629882847e05372beaff6871db987
Andrew Embler <andrew@concrete5.org>
2013-07-15 16:30:07 -0700

    adding xml helper

---
+++ web/concrete/core/helpers/xml.php
@@ -0,0 +1,14 @@
+<?
+defined('C5_EXECUTE') or die("Access Denied.");
+class Concrete5_Helper_Xml { 
+
+	public function createCDataNode(SimpleXMLElement $x, $nodeName, $content) {
+		$node = $x->addChild($nodeName);
+		$node = dom_import_simplexml($node);
+		$no = $node->ownerDocument;
+		$node->appendChild($no->createCDataSection($content));
+		return $node;
+	}
+	
+	
+}
---
+++ web/concrete/helpers/xml.php
@@ -0,0 +1,6 @@
+<?
+defined('C5_EXECUTE') or die("Access Denied.");
+class XmlHelper extends Concrete5_Helper_Xml { 
+
+	
+}
