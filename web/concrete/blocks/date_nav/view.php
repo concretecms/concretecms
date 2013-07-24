b55e3a3257be2d3bbe70882b2b1378f1d6b76ff8
Mike Lay <mike@mkly.io>
2013-07-24 10:59:30 -0700

    Additional Fix and Cleanup Date Nav
    Additional Fix and Cleanup Date Nav
    
    
    Former-commit-id: 8fcd63af888fa01032a37fd78582f4b1e5010473

--- web/concrete/blocks/date_nav/view.php
+++ web/concrete/blocks/date_nav/view.php
@@ -4,38 +4,8 @@ defined('C5_EXECUTE') or die("Access Denied.");
 global $c;
 $textHelper = Loader::helper("text"); 
 $dateHelper = Loader::helper("date"); 
-
-/*
-// now that we're in the specialized content file for this block type, 
-// we'll include this block type's class, and pass the block to it, and get
-// the content
-	
-if (count($cArray) > 0) { ?>
-<div class="ccm-page-list">
-	
-	<?php  for ($i = 0; $i < count($cArray); $i++ ) {
-		$cobj = $cArray[$i];
-		$title = $cobj->getCollectionName(); ?>
-	
-	<h3 class="ccm-page-list-title"><a href="<?php echo $nh->getLinkToCollection($cobj)?>"><?php echo $title?></a></h3>
-	<div class="ccm-page-list-description">
-		<?php 
-		if(!$controller->truncateSummaries){
-			echo $cobj->getCollectionDescription();
-		}else{
-			echo $textHelper->shorten($cobj->getCollectionDescription(),$controller->truncateChars);
-		}
-		?>
-	</div>
-	
-	<?php   } ?>
-</div>
-<?php  } 
- 
-*/
 ?>
-	
-	 
+
 <div id="ccmDateNav<?php echo $bID?>" class="ccmDateNav">
 <?php   
 
