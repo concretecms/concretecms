33763bec6b56e7ea3db5d0c5b6843cbe9663380b
Remo Laubacher <remo.laubacher@gmail.com>
2013-08-08 09:58:55 +0200

    Revert "get area handle and name through method which is easier to override"
    This reverts commit 849e308a29617e770dbcc388dd042aaadec7e7b3 [formerly 3804e97eea47c29d944dfcd34c9f62c09a478e1b].
    
    Former-commit-id: 8ebaa6044fa6876ca979e606eaf330dbfcc1b92b

--- web/concrete/core/models/area.php
+++ web/concrete/core/models/area.php
@@ -156,12 +156,6 @@ class Concrete5_Model_Area extends Object {
 	public function getAreaHandle() {return $this->arHandle;}
 	
 	/**
-	 * returns the name for the current area
-	 * @return string
-	*/
-	public function getAreaName() {return $this->arHandle;}    
-	
-	/**
 	 * returns an array of custom templates
 	 * @return array
 	 */
