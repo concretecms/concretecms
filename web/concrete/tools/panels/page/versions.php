8fd63217a6959bc069afd6125f6d273cc11442c9
Andrew Embler <andrew@concrete5.org>
2013-08-20 14:03:35 -0700

    test

--- web/concrete/tools/panels/page/versions.php
+++ web/concrete/tools/panels/page/versions.php
@@ -3,8 +3,14 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $c = Page::getByID(Loader::helper('security')->sanitizeInt($_REQUEST['cID']));
 if (is_object($c) && !$c->isError()) {
 	$cp = new Permissions($c);
-	if ($cp->canViewToolbar()) { ?>
-derp
+	if ($cp->canViewPageVersions()) { ?>
+
+		<section>
+			<header>&lt; <?=t('Versions')?></header>
+			<br/><br/>
+		</section>
+
+
 	<? }
 }
 ?>
