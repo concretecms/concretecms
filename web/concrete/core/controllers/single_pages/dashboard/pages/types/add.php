e6d57486f4ada341f11bbf27bc3ad84bb22c2b8f
Mike Lay <mike@mkly.io>
2013-08-23 13:15:54 -0700

    Fix mysql Exception Shown on Page Type Add
    Fix mysql exception show on attempted duplicate page type handle
    addition.
    
    - Check for existing page type by handle and return error if found

--- web/concrete/core/controllers/single_pages/dashboard/pages/types/add.php
+++ web/concrete/core/controllers/single_pages/dashboard/pages/types/add.php
@@ -20,6 +20,10 @@ class Concrete5_Controller_Dashboard_Pages_Types_Add extends DashboardBaseContro
 		} else if (!$vs->handle($ctHandle)) {
 			$this->error->add(t('Handles must contain only letters, numbers or the underscore symbol.'));
 		}
+
+		if (CollectionType::getByHandle($ctHandle)) {
+			$this->error->add(t('Handle already exists.'));
+		}
 		
 		if (!$ctName) {
 			$this->error->add(t("Name required."));
