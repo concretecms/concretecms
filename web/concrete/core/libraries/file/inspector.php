ec3909060a57722f8fde746ee94d1d3f6e528c38
Mike Lay <mike@mkly.io>
2013-09-15 13:57:57 -0700

    FileTypeInspector defined twice
    FileTypeInspector defined twice
    
    Concrete5_Library_FileTypeInspector defined in two different files.
    
    - Add if class_defined check to only load one.
    
    
    Former-commit-id: ab96bcbf44bd2369e9004d53b69f5f2f7ae30bb9

--- web/concrete/core/libraries/file/inspector.php
+++ web/concrete/core/libraries/file/inspector.php
@@ -1,7 +1,12 @@
 <?
 defined('C5_EXECUTE') or die("Access Denied.");
+
+if (!class_exists('Concrete5_Library_FileTypeInspector')) {
+
 abstract class Concrete5_Library_FileTypeInspector {
 
 	abstract public function inspect($fv);
+
+}
 
 }
