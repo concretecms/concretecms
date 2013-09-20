376d82af8ebb00b6490926611eb8e06d88178c60
Mike Lay <mike@mkly.io>
2013-09-20 11:31:50 -0700

    Fix PH 5.4 error and Cleanup
    Fix PH 5.4 error and Cleanup
    
    - Pass `$controller` to form
    - Add ccm-ui wrapper
    - Remove closing `?>`

--- web/concrete/blocks/date_nav/add.php
+++ web/concrete/blocks/date_nav/add.php
@@ -1,6 +1,5 @@
 <?php  
 defined('C5_EXECUTE') or die("Access Denied.");
-Loader::model("collection_types");
 
 global $c;
 global $b;
@@ -8,7 +7,5 @@ global $b;
 $uh = Loader::helper('concrete/urls'); 
 	
 $c = $cp->getOriginalObject();
-//	echo $rssUrl;
 
-$bt->inc('form_setup_html.php', array( 'c'=>$c, 'b'=>$b, 'uh'=>$uh ) ); 
-?>
+$bt->inc('form_setup_html.php', array( 'c'=>$c, 'b'=>$b, 'uh'=>$uh, 'controller'=>$controller ) );
