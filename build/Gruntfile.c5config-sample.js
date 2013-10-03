9c2a316982e170cb713587a15537430fc370ab3c
Michele Locati <michele@locati.it>
2013-10-03 14:51:29 +0200

    Add the 'translations' task to Grunt
    Running 'grunt translations' does the following:
    - fetches the available languages from Transifex
    - skips the languages with a translation progress below 95%
    - downloads .po files
    - compile .mo files from .po files with gettext's msgfmt
      (that must be available in PATH)
    - deletes the downloaded .po files
    
    You have to create a Gruntfile.c5config.js file.
    See Gruntfile.c5config-sample.js for further instructions.

---
+++ build/Gruntfile.c5config-sample.js
@@ -0,0 +1,11 @@
+// This is an sample configuration file.
+// Set your values and save this file as Gruntfile.c5config.js
+
+module.exports = {
+	// Your Transifex login
+	txUsername: '',
+	// Your Transifex password
+	txPassword: '',
+	// The resource identifier of the Transifex resource, for instance 'core-5621'
+	txResource: ''
+};
