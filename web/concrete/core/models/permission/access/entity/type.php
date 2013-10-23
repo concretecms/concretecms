c745fc5e35626ee2112a05959464be50630c7f71
Michele Locati <michele@locati.it>
2013-10-23 17:14:30 +0200

    Introduce getAccessEntityTypeDisplayName
    Let's add the getAccessEntityTypeDisplayName method to the
    PermissionAccessEntityType class.
    It returns the localized name (so that you don't have to call tc()) and it
    optionally escapes the name in html format, javascript format (default is
    to escape in html format).
    
    
    Former-commit-id: 7d2bc52397f4275f6c07039db2f6adf57d902b4b

--- web/concrete/core/models/permission/access/entity/type.php
+++ web/concrete/core/models/permission/access/entity/type.php
@@ -9,6 +9,26 @@ class Concrete5_Model_PermissionAccessEntityType extends Object {
 		$class = Loader::helper('text')->camelcase($this->petHandle) . 'PermissionAccessEntity';
 		return $class;
 	}
+
+	/** Returns the display name for this attribute (localized and escaped accordingly to $format)
+	* @param string $format = 'html'
+	*	Escape the result in html format (if $format is 'html') or in JavaScript format (if $format is 'js').
+	*	If $format is 'text' or any other value, the display name won't be escaped.
+	* @return string
+	*/
+	public function getAccessEntityTypeDisplayName($format = 'html') {
+		$value = tc('AttributeKeyName', $this->getAccessEntityTypeName());
+		switch($format) {
+			case 'html':
+				return h($value);
+			case 'js':
+				return Loader::helper('json')->encode($value);
+			case 'text':
+			default:
+				return $value;
+		}
+	}
+
 	public static function getByID($petID) {
 		$db = Loader::db();
 		$row = $db->GetRow('select petID, pkgID, petHandle, petName from PermissionAccessEntityTypes where petID = ?', array($petID));
@@ -65,7 +85,7 @@ class Concrete5_Model_PermissionAccessEntityType extends Object {
 		foreach($ptypes as $pt) {
 			$ptype = $axml->addChild('permissionaccessentitytype');
 			$ptype->addAttribute('handle', $pt->getAccessEntityTypeHandle());
-			$ptype->addAttribute('name', tc('PermissionAccessEntityTypeName', $pt->getAccessEntityTypeName()));
+			$ptype->addAttribute('name', $pt->getAccessEntityTypeName());
 			$ptype->addAttribute('package', $pt->getPackageHandle());
 			$categories = $db->GetCol('select pkCategoryHandle from PermissionKeyCategories inner join PermissionAccessEntityTypeCategories where PermissionKeyCategories.pkCategoryID = PermissionAccessEntityTypeCategories.pkCategoryID and PermissionAccessEntityTypeCategories.petID = ?', array($pt->getAccessEntityTypeID()));
 			if (count($categories) > 0) {
