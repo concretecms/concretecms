2b4c2e2e87458f662de2d0955b674e51ba02e015
Andrew Embler <andrew@concrete5.org>
2013-10-26 11:30:06 -0700

    startin the dashboard
    Former-commit-id: 933c86264dfefd749d464fddb010566232a28580

--- web/concrete/views/system/panels/dashboard.php
+++ web/concrete/views/system/panels/dashboard.php
@@ -2,9 +2,16 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 ?>
 <section>
-	<header><?=t('Dashboard')?>
-		<aside><a href="<?=URL::to('/login', 'logout', Loader::helper('validation/token')->generate('logout'))?>"><?=t('Logout')?></a></aside>
-	</header>
+	<div data-panel-menu="accordion" class="ccm-panel-header-accordion">
+	<nav>
+	<span></span>
+	<ul class="ccm-panel-header-accordion-dropdown">
+		<li><a data-panel-accordion-tab="dashboard" <? if (!in_array($tab, array('favorites'))) { ?>data-panel-accordion-tab-selected="true" <? } ?>><?=t('Dashboard')?></a></li>
+		<li><a data-panel-accordion-tab="favorites" <? if ($tab == 'favorites') { ?>data-panel-accordion-tab-selected="true" <? } ?>><?=t('Favorites')?></a></>
+	</ul>
+	</nav>
+	</div>
+
 	<menu>
 		<? foreach($children as $cID) {
 			$cc = Page::getByID($cID, 'ACTIVE');
@@ -15,4 +22,9 @@ defined('C5_EXECUTE') or die("Access Denied.");
 		} ?>
 
 	</menu>
+
+	<div class="ccm-panel-dashboard-footer">
+		<p><?=t('Logged in as %s', $ui->getUserDisplayName());?>.</p>
+		<a href="<?=URL::to('/login', 'logout', Loader::helper('validation/token')->generate('logout'))?>"><?=t('Sign Out.')?></a>
+	</div>
 </section>
