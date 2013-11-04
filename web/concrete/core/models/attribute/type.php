9d50209fceb74d4687f1077443d6dfd544a70029
Michele Locati <michele@locati.it>
2013-11-04 19:18:51 +0100

    Remove the json encoding option from getAttributeTypeDisplayName
    Former-commit-id: 1e020b8fecab2e7e9b8584ad739804abe4b6cca9

--- web/concrete/core/models/attribute/type.php
+++ web/concrete/core/models/attribute/type.php
@@ -9,7 +9,7 @@ class Concrete5_Model_AttributeType extends Object {
 
 	/** Returns the display name for this attribute type (localized and escaped accordingly to $format)
 	* @param string $format = 'html'
-	*	Escape the result in html format (if $format is 'html') or in JavaScript format (if $format is 'js').
+	*	Escape the result in html format (if $format is 'html').
 	*	If $format is 'text' or any other value, the display name won't be escaped.
 	* @return string
 	*/
@@ -18,8 +18,6 @@ class Concrete5_Model_AttributeType extends Object {
 		switch($format) {
 			case 'html':
 				return h($value);
-			case 'js':
-				return Loader::helper('json')->encode($value);
 			case 'text':
 			default:
 				return $value;
