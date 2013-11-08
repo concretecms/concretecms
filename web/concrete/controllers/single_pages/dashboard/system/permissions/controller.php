d0e05484172cb0d615f98c5b5811783ff4651753
Andrew Embler <andrew@concrete5.org>
2013-11-08 11:56:37 -0800

    fixing some controllers
    Former-commit-id: 7936192e5052269e82b6d1fabca15404d910bd9e

--- web/concrete/controllers/single_pages/dashboard/system/permissions/controller.php
+++ web/concrete/controllers/single_pages/dashboard/system/permissions/controller.php
@@ -1,4 +1,4 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class DashboardSystemPermissionsController extends Concrete5_Controller_Dashboard_System_Permissions {}
+class DashboardSystemPermissionsPageController extends Concrete5_Controller_Page_Dashboard_System_Permissions {}
