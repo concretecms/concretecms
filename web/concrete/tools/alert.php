9cfe6da3f94c891317f6b7eab8f58550f2d202d4
Andrew Embler <andrew@concrete5.org>
2013-11-09 08:51:53 -0800

    making alert no longer call a separate file for no reason

--- web/concrete/tools/alert.php
+++
@@ -1,5 +0,0 @@
-<?  defined('C5_EXECUTE') or die("Access Denied.");?>
-
-<div id="ccm-popup-alert" class="ccm-ui">
-	<div id="ccm-popup-alert-message" class="alert-message block-message error"></div>
-</div>
