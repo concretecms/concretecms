6dca6514cf113df6527ccf297bd294010c38483b
Andrew Embler <andrew@concrete5.org>
2013-11-14 10:52:02 -0800

    access user search coming back
    Former-commit-id: 701adbf41e462f0640313c8dd6790b94dcb84d40

--- web/concrete/core/models/permission/access/list_items/custom/access_user_search.php
+++ web/concrete/core/models/permission/access/list_items/custom/access_user_search.php
@@ -3,21 +3,5 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 class Concrete5_Model_AccessUserSearchUserPermissionAccessListItem extends PermissionAccessListItem {
 	
-	protected $customGroupArray = array();
-	protected $groupsAllowedPermission = 'N';
-
-	public function setGroupsAllowedPermission($permission) {
-		$this->groupsAllowedPermission = $permission;
-	}
-	public function getGroupsAllowedPermission() {
-		return $this->groupsAllowedPermission;
-	}
-	public function setGroupsAllowedArray($gIDs) {
-		$this->customGroupArray = $gIDs;
-	}
-	public function getGroupsAllowedArray() {
-		return $this->customGroupArray;
-	}
-	
 	
 }
