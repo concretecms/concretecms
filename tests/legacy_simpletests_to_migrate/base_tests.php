1c1b84743d9b9ae63dfd2a6d9d3565074383c089
Andrew Embler <andrew@concrete5.org>
2013-11-18 13:18:28 -0800

    Moving unit tests into core directory
    Former-commit-id: 8b6bced5a447796f1f572df712a5fa5028f978c9

---
+++ tests/legacy_simpletests_to_migrate/base_tests.php
@@ -0,0 +1,11 @@
+<?
+
+class BaseTest extends UnitTestCase {
+    function BaseTest() {
+        $this->UnitTestCase('Base File and Environment Tests');
+    }
+ 
+    function testConfigFileExists() {
+		return $this->assertTrue(file_exists(DIR_BASE . '/config/site.php'));
+    }
+}
