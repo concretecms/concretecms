fbbaa2003d3d73d66a9ba63d8818d9bb28dae15e
Andrew Embler <andrew@concrete5.org>
2013-12-31 17:43:14 -0800

    user work
    Former-commit-id: 36efa027a8049a300c89fb2319cc141b48de8eb4

--- web/concrete/elements/attribute/editable_list.php
+++ web/concrete/elements/attribute/editable_list.php
@@ -2,7 +2,12 @@
 
 	<div class="row">
 		<div class="col-md-3"><p><?=tc('AttributeKeyName', $ak->getAttributeKeyName())?></p></div>
-		<div class="col-md-9"><p data-title="<?=$ak->getAttributeKeyName()?>" data-key-id="<?=$ak->getAttributeKeyID()?>" data-name="<?=$ak->getAttributeKeyID()?>" data-editable-field-type="xeditableAttribute" data-url="<?=$action?>" data-type="concreteattribute"><?=$object->getAttribute($ak->getAttributeKeyHandle(), 'displaySanitized', 'display')?></p></div>
+		<div class="col-md-9" data-editable-field-inline-commands="true">
+		<ul class="ccm-edit-mode-inline-commands">
+			<li><a href="#" data-key-id="<?=$ak->getAttributeKeyID()?>" data-url="<?=$clearAction?>" data-editable-field-command="clear_attribute"><i class="glyphicon glyphicon-trash"></i></a></li>
+		</ul>
+		<p data-title="<?=$ak->getAttributeKeyName()?>" data-key-id="<?=$ak->getAttributeKeyID()?>" data-name="<?=$ak->getAttributeKeyID()?>" data-editable-field-type="xeditableAttribute" data-url="<?=$saveAction?>" data-type="concreteattribute"><?=$object->getAttribute($ak->getAttributeKeyHandle(), 'displaySanitized', 'display')?></p>
+	</div>
 	</div>
 
 	<div style="display: none">
