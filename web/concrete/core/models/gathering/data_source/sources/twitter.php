a32a80615fbbb77e0b48f89e3573921189879129
Andrew Embler <andrew@concrete5.org>
2014-01-23 13:14:29 -0800

    twitter stuff
    Former-commit-id: f06a7a6c0930dca7aeb3df0590438ce8f3abaf46

---
+++ web/concrete/core/models/gathering/data_source/sources/twitter.php
@@ -0,0 +1,38 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+class Concrete5_Model_TwitterGatheringDataSource extends GatheringDataSource {
+
+	const TWITTER_SEARCH_URL = 'http://api.flickr.com/services/feeds/photos_public.gne';
+
+	public function createConfigurationObject(Gathering $ag, $post) {
+		$o = new TwitterGatheringDataSourceConfiguration();
+		$o->setTwitterUsername($post['twitterUsername']);
+		return $o;
+	}
+
+	public function createGatheringItems(GatheringDataSourceConfiguration $configuration) {
+		Loader::library('3rdparty/twitteroauth/twitteroauth');
+		$connection = new TwitterOAuth(TWITTER_APP_CONSUMER_KEY, TWITTER_APP_CONSUMER_SECRET, TWITTER_APP_ACCESS_TOKEN, TWITTER_APP_ACCESS_SECRET);
+		$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$configuration->getTwitterUsername()."&count=5");
+		if (!empty($tweets->errors[0])) {
+			throw new Exception($tweets->errors[0]->message);
+		}
+		
+		$gathering = $configuration->getGatheringObject();
+		$lastupdated = 0;
+		if ($gathering->getGatheringDateLastUpdated()) {
+			$lastupdated = strtotime($gathering->getGatheringDateLastUpdated());
+		}
+
+		$items = array();
+		foreach($tweets as $tweet) {
+			$item = TwitterGatheringItem::add($configuration, $tweet);
+
+			if (is_object($item)) {
+				$items[] = $item;
+			}
+		}
+		return $items;
+	}
+	
+}
---
+++ web/concrete/models/gathering/data_source/sources/configurations/twitter.php
@@ -0,0 +1,3 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+class TwitterGatheringDataSourceConfiguration extends Concrete5_Model_TwitterGatheringDataSourceConfiguration {}
