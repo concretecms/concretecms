a95fade56f51ce3a04de483fef19165c96ec1a59
Evan Cooper <evan@concretecms.com>
2014-01-24 00:01:20 -0800

    getting css on the twitter tiles, making hash tags, user mentions, urls and media urls show up as working links
    Former-commit-id: 657013f5befb57d922a20c820333da0bb5315dbc

--- web/concrete/core/models/gathering/item/types/twitter.php
+++ web/concrete/core/models/gathering/item/types/twitter.php
@@ -9,7 +9,7 @@ class Concrete5_Model_TwitterGatheringItem extends GatheringItem {
 		$ags = GatheringDataSource::getByHandle('twitter');
 		return GatheringItem::getListByKey($ags, $mixed->get_link());
 	}
-	
+
 	public static function add(GatheringDataSourceConfiguration $configuration, $tweet) {
 		$gathering = $configuration->getGatheringObject();
 		try {
@@ -25,6 +25,27 @@ class Concrete5_Model_TwitterGatheringItem extends GatheringItem {
 	}
 
 	public function assignFeatureAssignments($tweet) {
+		$userMentions = $tweet->entities->user_mentions;
+		if(count($userMentions) > 0) {  // link mentions
+			foreach($tweet->entities->user_mentions as $mention) {
+				$tweet->text = str_replace('@'.$mention->screen_name, '<a target="_blank" href="http://www.twitter.com/'.$mention->screen_name.'">@'.$mention->screen_name.'</a>', $tweet->text);
+			}
+		}
+		if(count($tweet->entities->hashtags) > 0) {   //link hashtags
+			foreach($tweet->entities->hashtags as $hash) {
+				$tweet->text = str_replace('#'.$hash->text, '<a target="_blank" href="http://www.twitter.com/search/%23'.$hash->text.'">#'.$hash->text.'</a>', $tweet->text);
+			}
+		}
+		if(count($tweet->entities->urls) > 0) {
+			foreach($tweet->entities->urls as $url) {
+				$tweet->text = str_replace($url->url, '<a target="_blank" href="'.$url->url.'">'.$url->url.'</a>', $tweet->text);
+			}
+		}
+		if(count($tweet->entities->media) > 0) {
+			foreach($tweet->entities->media as $medium) {
+				$tweet->text = str_replace($medium->url, '<a target="_blank" href="'.$medium->url.'">'.$medium->display_url.'</a>', $tweet->text);
+			}
+		}
 		$this->addFeatureAssignment('tweet', $tweet->text);
 		$this->addFeatureAssignment('date_time', $tweet->created_at);
 	}
