d1d61cc0c25c413c47d288dcde29c1c969e7c059
Evan Cooper <evan@concretecms.com>
2014-01-24 10:49:44 -0800

    checking in twitter stuff
    Former-commit-id: 8763721b06da99392762bd6629bdb76f995f4479

--- web/concrete/elements/gathering/templates/tile/tweet/view.php
+++ web/concrete/elements/gathering/templates/tile/tweet/view.php
@@ -3,7 +3,8 @@
 <div class="ccm-gathering-twitter">
 	<img class="twitter-logo" src="https://abs.twimg.com/a/1373252541/images/resources/twitter-bird-light-bgs.png" />
 	<div class="tweet">
-		<p><?=$tweet?></p>
-		<p><span class="elapsed"><?=date('m/d/y', strtotime($date_time))?></span><span class="who-from"></span></p>
+		<span class="tweet-body"><?=$tweet?></span>
+		<div style="clear: both;"></div>
+		<p class="tweet-info"><span class="elapsed"><?=date('m/d/y', strtotime($date_time))?></span><span class="who-from"><a href="http://www.twitter.com/<?php echo $description ?>"><?php echo $description ?></a></span></p>
 	</div>
 </div>
