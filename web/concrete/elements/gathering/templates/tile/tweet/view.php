657013f5befb57d922a20c820333da0bb5315dbc
Evan Cooper <evan@concretecms.com>
2014-01-24 00:01:20 -0800

    getting css on the twitter tiles, making hash tags, user mentions, urls and media urls show up as working links

--- web/concrete/elements/gathering/templates/tile/tweet/view.php
+++ web/concrete/elements/gathering/templates/tile/tweet/view.php
@@ -1,9 +1,9 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); 
+<? defined('C5_EXECUTE') or die("Access Denied.");
 ?>
 <div class="ccm-gathering-twitter">
 	<img class="twitter-logo" src="https://abs.twimg.com/a/1373252541/images/resources/twitter-bird-light-bgs.png" />
 	<div class="tweet">
 		<p><?=$tweet?></p>
-		<p><span class="elapsed"><?=$date_time?></span><span class="who-from">CMS_Review</span></p>
+		<p><span class="elapsed"><?=date('m/d/y', strtotime($date_time))?></span><span class="who-from"></span></p>
 	</div>
 </div>
