2f96bebad20322deda477524f7b0e73e9539ce17
Michele Locati <michele@locati.it>
2014-02-10 11:28:53 +0100

    Let formatSize return a specific size
    Extend NumberHelper->formatSize(): now we can specify a specific unit.
    For instance: formatSize(1024) returns '1.00 KB',
    but formatSize(1024, 'bytes') returns '1,024 bytes'

--- web/concrete/core/helpers/number.php
+++ web/concrete/core/helpers/number.php
@@ -92,32 +92,38 @@ class Concrete5_Helper_Number {
 
 	/** Formats a size (measured in bytes, KB, MB, ...).
 	* @param number $size The size to be formatted, in bytes.
-	* @return string|mixed If $size is not numeric, the function returns $ize (untouched), otherwise it returns the size with the correct usits (GB, MB, ...) and formatted following the locale rules.
+	* @param string $forceUnit = '' Set to 'bytes', 'KB', 'MB', 'GB' or 'TB' if you want to force the unit, leave empty to automatically determine the unit.
+	* @return string|mixed If $size is not numeric, the function returns $size (untouched), otherwise it returns the size with the correct usits (GB, MB, ...) and formatted following the locale rules.
 	* @example formatSize(0) returns '0 bytes'
 	* @example formatSize(1) returns '1 byte'
 	* @example formatSize(1000) returns '1,000 bytes'
 	* @example formatSize(1024) returns '1.00 KB'
+	* @example formatSize(1024, 'bytes') returns '1024 bytes'
+	* @example formatSize(1024, 'GB') returns '0.00 GB'
 	* @example formatSize(2000000) returns '1.91 MB'
 	* @example formatSize(-5000) returns '-4.88 KB'
 	* @example formatSize('hello') returns 'hello'
 	*/
-	public function formatSize($size) {
+	public function formatSize($size, $forceUnit = '') {
 		if(!is_numeric($size)) {
 			return $size;
 		}
-		if(abs($size) < 1024) {
+		if(strlen($forceUnit) && array_search($forceUnit, array('bytes', 'KB', 'MB', 'GB', 'TB')) === false) {
+			$forceUnit = '';
+		}
+		if($forceUnit === 'bytes' || (abs($size) < 1024 && (!strlen($forceUnit)))) {
 			return t2(/*i18n %s is a number */'%s byte', '%s bytes', $size, $this->format($size, 0));
 		}
 		$size /= 1024;
-		if(abs($size) < 1024) {
+		if($forceUnit === 'KB' || (abs($size) < 1024 && (!strlen($forceUnit)))) {
 			return t(/*i18n %s is a number, KB means Kilobyte */'%s KB', $this->format($size, 2));
 		}
 		$size /= 1024;
-		if($size < 1024) {
+		if($forceUnit === 'MB' || (abs($size) < 1024 && (!strlen($forceUnit)))) {
 			return t(/*i18n %s is a number, MB means Megabyte */'%s MB', $this->format($size, 2));
 		}
 		$size /= 1024;
-		if($size < 1024) {
+		if($forceUnit === 'GB' || (abs($size) < 1024 && (!strlen($forceUnit)))) {
 			return t(/*i18n %s is a number, GB means Gigabyte */'%s GB', $this->format($size, 2));
 		}
 		return t(/*i18n %s is a number, TB means Terabyte */'%s TB', $this->format($size, 2));
