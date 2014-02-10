6556787165d85758a199721a5bea4fd7d3875468
Michele Locati <michele@locati.it>
2014-02-10 18:15:17 +0100

    Fix escaping strings for JavaScript
    The correct way to pass strings from PHP to JavaScript is json_encode.
    
    Using addslashes or htmlspecialchars(..., ENT_QUOTES) may lead to
    unexpected issues.
    
    
    Former-commit-id: 6fc4086b0ef93e2fac0caab7e59a384911a8f23f

--- web/concrete/startup/security.php
+++ web/concrete/startup/security.php
@@ -3,7 +3,3 @@ defined('C5_EXECUTE') or die("Access Denied.");
 function h($input) {
     return Loader::helper('text')->specialchars($input);
 }
-
-function hq($input) {
-	return htmlspecialchars($input, ENT_QUOTES, APP_CHARSET, false);
-}
