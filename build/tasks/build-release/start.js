c435bf8d039197f364bc2c5ce77c61a3b1fc3a7f
Andrew Embler <andrew@concrete5.org>
2014-02-18 12:12:05 -0800

    Revert "Revert "build updates, merging TinyMCE""
    This reverts commit 9ddaba64328c3a664b3f05047233f23ea4c6fb1b.

--- build/tasks/build-release/start.js
+++ build/tasks/build-release/start.js
@@ -16,11 +16,11 @@ module.exports = function(grunt, config, parameters, done) {
 
 		// Download archive from git
 		process.stdout.write("Downloading Archive...\n");
-		download('https://github.com/concrete5/concrete5/archive/master.zip', path, {
-			extract: true,
-			close: function() {
-				done();
-			}
+		var stream = download('https://github.com/concrete5/concrete5/archive/master.zip', path, {
+			extract: true
+		});
+		stream.on('close', function() {
+			done();
 		});
 
 	}
