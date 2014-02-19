20184bcfa34cb14fcef09432fb44990c37d62677
Chris Ayoub <chris.ayoub@vuria.com>
2014-02-19 13:06:35 -0700

    Update Single Pages dashboard controller to pass error codes rather than error messages

--- web/concrete/core/controllers/single_pages/dashboard/pages/single.php
+++ web/concrete/core/controllers/single_pages/dashboard/pages/single.php
@@ -1,6 +1,30 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 class Concrete5_Controller_Dashboard_Pages_Single extends DashboardBaseController {
 
+	public function single_page_added() {
+		$this->view(t('Page Successfully Added.'));
+	}
+
+	public function single_page_invalid() {
+		$this->view(t('That specified path doesn\'t appear to be a valid static page.'), 1);
+	}
+
+	public function single_page_exists() {
+		$this->view(t("That page has already been added."), 1);
+	}
+
+	public function single_page_refreshed() {
+		$this->view(t('Page Successfully Refreshed.'));
+	}
+
+	public function single_page_no_refresh_permission() {
+		$this->view(t('You do not have permissions to refresh this page.'), 1);
+	}
+
+	public function single_page_not_refreshed() {
+		$this->view(t('Page Unsuccessfully Refreshed.'));
+	}
+
 	public function view($message = null, $error = null) {
 		Loader::model('single_page');
 		$this->set('generated', SinglePage::getList());
@@ -20,12 +44,12 @@ class Concrete5_Controller_Dashboard_Pages_Single extends DashboardBaseControlle
 					
 					if ($pc->getError() == COLLECTION_NOT_FOUND) {
 						SinglePage::add($this->post('pageURL'));
-						$this->redirect('/dashboard/pages/single', t('Page Successfully Added.'));
+						$this->redirect('/dashboard/pages/single', 'single_page_added');
 					} else {
-						$this->redirect('/dashboard/pages/single', t("That page has already been added."), 1);
+						$this->redirect('/dashboard/pages/single', 'single_page_exists');
 					}
 				} else {
-					$this->redirect('/dashboard/pages/single', t('That specified path doesn\'t appear to be a valid static page.'), 1);
+					$this->redirect('/dashboard/pages/single', 'single_page_invalid');
 				}
 			}
 			$this->redirect('/dashboard/pages/single', $this->token->getErrorMessage(), 1);
@@ -40,12 +64,12 @@ class Concrete5_Controller_Dashboard_Pages_Single extends DashboardBaseControlle
 				$cp = new Permissions($p);
 				if($cp->canAdmin()) {
 					$p->refresh();
-					$this->redirect('/dashboard/pages/single', t('Page Successfully Refreshed.'));
+					$this->redirect('/dashboard/pages/single', 'single_page_refreshed');
 				}
-				$this->redirect('/dashboard/pages/single', t('You do not have permissions to refresh this page.'), 1);
+				$this->redirect('/dashboard/pages/single', 'single_page_no_refresh');
 			}
 			$this->redirect('/dashboard/pages/single', $this->token->getErrorMessage(), 1);
 		}
-		$this->redirect('/dashboard/pages/single', t('Page Unsuccessfully Refreshed.'), 1);
+		$this->redirect('/dashboard/pages/single', 'single_page_not_refreshed');
 	}
 }
