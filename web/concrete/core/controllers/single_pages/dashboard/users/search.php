702f53a5f2e646b2c62a014fd1338871c4d11292
Andrew Embler <andrew@concrete5.org>
2014-02-19 10:19:51 -0800

    updating user error message
    Former-commit-id: 5c42d34b0281d0005a034ff74fea37e1835f4ecb

--- web/concrete/core/controllers/single_pages/dashboard/users/search.php
+++ web/concrete/core/controllers/single_pages/dashboard/users/search.php
@@ -104,9 +104,9 @@ class Concrete5_Controller_Dashboard_Users_Search extends Controller {
 				
 				if (strlen($username) >= USER_USERNAME_MINIMUM && !$valc->username($username)) {
 					if(USER_USERNAME_ALLOW_SPACES) {
-						$this->error->add(t('A username may only contain letters, numbers and spaces.'));
+						$this->error->add(t('A username may only contain letters, numbers, spaces, dots (not at the beginning/end), underscores (not at the beginning/end).'));
 					} else {
-						$this->error->add(t('A username may only contain letters or numbers.'));
+						$this->error->add(t('A username may only contain letters numbers, dots (not at the beginning/end), underscores (not at the beginning/end).'));
 					}
 				}
 				if (strcasecmp($uo->getUserName(), $username) && !$valc->isUniqueUsername($username)) {
