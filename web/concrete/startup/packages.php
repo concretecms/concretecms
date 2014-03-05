9b84f67bad73059e9b809bc2bb58e08c40183951
Andrew Embler <andrew@concrete5.org>
2014-03-05 10:58:16 -0800

    adding the ability to have automatic package updates

--- web/concrete/startup/packages.php
+++ web/concrete/startup/packages.php
@@ -7,6 +7,22 @@ foreach($pl as $p) {
 	if ($p->isPackageInstalled()) {
 		$pkg = Loader::package($p->getPackageHandle());
 		if (is_object($pkg)) {
+			// handle updates
+			if (ENABLE_AUTO_UPDATE_PACKAGES) {
+				$pkgInstalledVersion = $p->getPackageVersion();
+				$pkgFileVersion = $pkg->getPackageVersion();
+				if (version_compare($pkgFileVersion, $pkgInstalledVersion, '>')) {
+					$currentLocale = Localization::activeLocale();
+					if ($currentLocale != 'en_US') {
+						Localization::changeLocale('en_US');
+					}
+					$p->upgradeCoreData();
+					$p->upgrade();
+					if ($currentLocale != 'en_US') {
+						Localization::changeLocale($currentLocale);
+					}
+				}
+			}
 			$pkg->setupPackageLocalization();
 			if (method_exists($pkg, 'on_start')) {
 				$pkg->on_start();
