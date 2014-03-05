b445ddb566d61ffb129171f62535f3f43a565000
Michele Locati <michele@locati.it>
2014-03-05 16:11:36 +0100

    Fix potential problems with javascript/translations
    'Automation Instructions' may contains quotes in translations: let's be
    sure to correctly escape it before using it in JavaScript
    
    
    Former-commit-id: e059e0048f3705f631768ae02e12ca8eb2b9efdf

--- web/concrete/single_pages/dashboard/system/optimization/jobs.php
+++ web/concrete/single_pages/dashboard/system/optimization/jobs.php
@@ -448,7 +448,7 @@ $(function() {
 			height: 550,
 			width: 650,
 			modal: true,
-			title: '<?=t('Automation Instructions')?>'
+			title: <?=$jh->encode(t('Automation Instructions'))?>
 		});
 	});
 	$('.icon-question-sign').tooltip();
