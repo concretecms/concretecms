546d7c981d9de69249913db8b7f9cbd93d4b8063
Michele Locati <michele@locati.it>
2014-03-12 16:58:58 +0100

    Throw an exception if importing an unknown block
    When importing CIF data that contains an unknown block type, the execution
    halts with an error message that does not help understanding what happened
    ("getController() method called on a non object").
    This may happen for instance we don't have installed a package that
    implements a specific block type.
    
    It's better if we throw an exception that explains better what went wrong.
    
    
    Former-commit-id: 28c85ab01aac4770b67d04f06147578885963973

--- web/concrete/core/libraries/content/importer.php
+++ web/concrete/core/libraries/content/importer.php
@@ -217,6 +217,9 @@ class Concrete5_Library_Content_Importer {
 					if ($bx['type'] != '') {
 						// we check this because you might just get a block node with only an mc-block-id, if it's an alias
 						$bt = BlockType::getByHandle($bx['type']);
+						if(!is_object($bt)) {
+							throw new Exception(t('Invalid block type handle: %s' . strval($bx['type'])));
+						}
 						$btc = $bt->getController();
 						$btc->import($page, (string) $ax['name'], $bx);
 					} else if ($bx['mc-block-id'] != '') {
