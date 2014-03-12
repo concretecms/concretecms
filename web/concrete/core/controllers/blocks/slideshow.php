d6728a59fe1e5c89c16cc346e0e0a4664eb3c698
Michele Locati <michele@locati.it>
2014-03-12 09:39:38 +0100

    Fix importing CIF for slideshow
    Skip importing the slideshowImgId field of btSlideshowImg, since it's an
    auto-increment value.

--- web/concrete/core/controllers/blocks/slideshow.php
+++ web/concrete/core/controllers/blocks/slideshow.php
@@ -226,6 +226,29 @@ class Concrete5_Controller_Block_Slideshow extends BlockController {
 			$this->images=$sortedImgs;
 		}
 	}
-}
 
-?>
+	protected function importAdditionalData($b, $blockNode) {
+		if (isset($blockNode->data)) {
+			foreach($blockNode->data as $data) {
+				if (strtoupper($data['table']) != strtoupper($this->getBlockTypeDatabaseTable())) {
+					$table = (string) $data['table'];
+					if (isset($data->record)) {
+						foreach($data->record as $record) {
+							$aar = new ADODB_Active_Record($table);
+							$aar->bID = $b->getBlockID();
+							foreach($record->children() as $node) {
+								$nodeName = $node->getName();
+								if((strcasecmp($table, 'btSlideshowImg') === 0) && (strcasecmp($nodeName, 'slideshowImgId') === 0)) {
+									continue;
+								}
+								$aar->{$nodeName} = ContentImporter::getValue((string) $node);
+							}
+							$aar->Save();
+						}
+					}								
+				}
+			}
+		}
+	}
+
+}
