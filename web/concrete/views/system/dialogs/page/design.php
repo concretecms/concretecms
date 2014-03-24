a74691a3667e8abebbcae50f0a10aa426cb7ebbf
Andrew Embler <andrew@concrete5.org>
2014-03-24 14:49:11 -0700

    design ui
    Former-commit-id: 3369a237dfda3d07a0147b3edb7ba1a4b1984245

---
+++ web/concrete/views/system/dialogs/page/design.php
@@ -0,0 +1,35 @@
+<?
+defined('C5_EXECUTE') or die("Access Denied.");
+$form = Loader::Helper('form');
+$selectedThemeID = 0;
+$selectedTemplateID = 0;
+if (is_object($selectedTheme)) {
+	$selectedThemeID = $selectedTheme->getThemeID();
+}
+if (is_object($selectedTemplate)) {
+	$selectedTemplateID = $selectedTemplate->getPageTemplateID();
+}
+?>
+
+<div class="ccm-ui">
+<form method="post" data-dialog-form="design" action="<?=$controller->action('submit')?>">
+
+	<div class="form-group">
+		<?=$form->label('pTemplateID', t('Page Template'))?>
+		<?=$form->select('pTemplateID', $templatesSelect, $selectedTemplateID)?>
+	</div>
+
+	<div class="form-group">
+		<?=$form->label('pThemeID', t('Theme'))?>
+		<?=$form->select('pThemeID', $themesSelect, $selectedThemeID)?>
+	</div>
+
+
+
+	<div class="dialog-buttons">
+	<button class="btn pull-left" data-dialog-action="cancel"><?=t('Cancel')?></button>
+	<button type="button" data-dialog-action="submit" class="btn btn-primary pull-right"><?=t('Save')?></button>
+	</div>
+
+</form>
+</div>
