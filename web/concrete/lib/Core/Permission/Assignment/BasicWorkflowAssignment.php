f0355ac49b639041ff38f643ed128eb7823fa566
Andrew Embler <andrew@concrete5.org>
2014-04-04 08:18:19 -0700

    oopifying permissions a bit more
    Former-commit-id: a4307dce52966cca1988a1e60efff7d9f29b4f01

--- web/concrete/lib/Core/Permission/Assignment/BasicWorkflowAssignment.php
+++ web/concrete/lib/Core/Permission/Assignment/BasicWorkflowAssignment.php
@@ -1,6 +1,6 @@
 <?
-defined('C5_EXECUTE') or die("Access Denied.");
 namespace Concrete\Core\Permission\Assignment;
+use \Concrete\Core\Permission\Access\Access as PermissionAccess;
 use Loader;
 class BasicWorkflowAssignment extends Assignment {
 	
--- web/concrete/lib/Core/Permission/Assignment/StackAssignment.php
+++ web/concrete/lib/Core/Permission/Assignment/StackAssignment.php
@@ -1,5 +1,4 @@
 <?
-defined('C5_EXECUTE') or die("Access Denied.");
 namespace Concrete\Core\Permission\Assignment;
 class StackAssignment extends PageAssignment {
 }
