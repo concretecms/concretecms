1b31a05b0403573a941adfa0e4013d23f859a818
Andrew Embler <andrew@concrete5.org>
2014-04-22 11:29:23 -0700

    updating readme

--- web/concrete/bootstrap/autoload.php
+++ web/concrete/bootstrap/autoload.php
@@ -5,7 +5,9 @@
  * Load all composer autoload items.
  * ----------------------------------------------------------------------------
  */
-require DIR_BASE_CORE . '/' . DIRNAME_VENDOR . '/autoload.php';
+if (!@include(DIR_BASE_CORE . '/' . DIRNAME_VENDOR . '/autoload.php')) {
+	die('Third party libraries not installed. Make sure that composer has required libraries in the concrete/ directory.');
+}
 
 
 
