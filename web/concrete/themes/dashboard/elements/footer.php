e70aa73b184fb4b11527c27633708fb11398bc21
Andrew Embler <andrew@concrete5.org>
2014-04-22 21:53:21 -0700

    tweaks to dashboard
    Former-commit-id: 5a4140c6d6dd22e622de03a1c14f375dec346f4f

--- web/concrete/themes/dashboard/elements/footer.php
+++ web/concrete/themes/dashboard/elements/footer.php
@@ -8,9 +8,10 @@ if ($_GET['_ccm_dashboard_external']) {
 
 <? Loader::element('footer_required', array('disableTrackingCode' => true)); ?>
 <script type="text/javascript">
-	ConcretePanelManager.register({'identifier': 'dashboard', 'position': 'right', url: '<?=URL::to("/system/panels/dashboard")?>'});
-	ConcretePanelManager.register({'identifier': 'sitemap', 'position': 'right', url: '<?=URL::to("/system/panels/sitemap")?>'});
+	ConcretePanelManager.register({'identifier': 'dashboard', 'position': 'right', url: '<?=URL::to("/ccm/system/panels/dashboard")?>'});
+	ConcretePanelManager.register({'identifier': 'sitemap', 'position': 'right', url: '<?=URL::to("/ccm/system/panels/sitemap")?>'});
 	var panel = ConcretePanelManager.getByIdentifier('dashboard');
+	panel.isOpen = true;
 	panel.onPanelLoad()
 </script>
 </body>
