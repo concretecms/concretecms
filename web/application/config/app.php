0a78c1cc1d47936e7aad15cf8669433c8e4ba586
Korvin Szanto <korvinszanto@gmail.com>
2014-04-23 14:18:44 -0700

    Fix environment info page, add composer.phar to gitignore
    Former-commit-id: d427a6bcacad659b6b6cfbf9805e0e19e278341b

--- web/application/config/app.php
+++ web/application/config/app.php
@@ -2,15 +2,15 @@
 /**
  * ----------------------------------------------------------------------------
  * # Custom Application Handler
- * 
+ *
  * You can do a lot of things in this file.
  *
  * ## Set a theme by route:
- * 
+ *
  * $router = Router::getInstance();
  * $router->setThemeByRoute('/login', 'greek_yogurt');
  *
- * 
+ *
  * ## Register a class override.
  *
  * Core::bind('helper/feed', function() {
@@ -20,24 +20,24 @@
  * Core::bind('\Concrete\Attribute\Boolean\Controller', function($app, $params) {
  * 	return new \Application\Attribute\Boolean\Controller($params[0]);
  * });
- * 
+ *
  * ## Register Events.
- * 
+ *
  * Events::addListener('on_page_view', function($event) {
  * 	$page = $event->getPageObject();
  * });
  *
- * 
+ *
  * ## Register some custom MVC Routes
- * 
+ *
  * $router = Router::getInstance();
  * $router->register('/test', function() {
  * 	print 'This is a contrived example.';
  * });
- * 
+ *
  * $router->register('/custom/view', '\My\Custom\Controller::view');
  * $router->register('/custom/add', '\My\Custom\Controller::add');
- * 
+ *
  * ----------------------------------------------------------------------------
  */
 
