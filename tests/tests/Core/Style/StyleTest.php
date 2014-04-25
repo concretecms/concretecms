f7a0b460bec93b30aec04d5feb8f7b52b7913417
Andrew Embler <andrew@concrete5.org>
2014-04-25 09:56:11 -0700

    starting on style unit test
    Former-commit-id: 5f11d38493bd4e003d46eb3a7228c9e6c15e7e4c

---
+++ tests/tests/Core/Style/StyleTest.php
@@ -0,0 +1,24 @@
+<?
+
+class StyleTest extends \PHPUnit_Framework_TestCase {
+
+	/*
+	public static function tearDownAfterClass() {
+		@unlink(dirname(__FILE__) . '/fixtures/testing.css');
+		@unlink(dirname(__FILE__) . '/fixtures/cache/css/testing/styles.css');
+		@rmdir(dirname(__FILE__) . '/fixtures/cache/css/testing');
+		@rmdir(dirname(__FILE__) . '/fixtures/cache/css');
+		@rmdir(dirname(__FILE__) . '/fixtures/cache');
+
+		@unlink(DIR_PACKAGES . '/tester/themes/testerson/styles.css');
+		@rmdir(DIR_PACKAGES . '/tester/themes/testerson');
+		@rmdir(DIR_PACKAGES . '/tester/themes');
+		@rmdir(DIR_PACKAGES . '/tester');	
+	}
+	*/
+
+	public function testStyles() {
+
+	}
+	
+}
