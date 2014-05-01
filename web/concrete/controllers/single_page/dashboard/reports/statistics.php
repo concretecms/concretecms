0d5f82cd1742570fc640027d89ed0dc03becc330
Michael Krasnow <mnkras@gmail.com>
2014-05-01 12:54:49 -0400

    Move some js to the footer so it actually works
    Former-commit-id: cc194dfb7adc084a2ae5f3f64c483e609ea62857

--- web/concrete/controllers/single_page/dashboard/reports/statistics.php
+++ web/concrete/controllers/single_page/dashboard/reports/statistics.php
@@ -11,12 +11,12 @@ class Statistics extends DashboardPageController {
 
 	protected $labels = array();
 	protected $dates = array();
-	
+
 	protected function setLatestPageViews() {
-		
+
 		$viewsArray = array();
 		$u = new User();
-		foreach($this->dates as $i => $date) { 
+		foreach($this->dates as $i => $date) {
 			$total = PageStatistics::getTotalPageViewsForOthers($u, $date);
 			$viewsArray[$this->labels[$i]] = $total;
 		}
@@ -26,7 +26,7 @@ class Statistics extends DashboardPageController {
 	protected function setLatestPagesCreated() {
 		$viewsArray = array();
 		$u = new User();
-		foreach($this->dates as $i => $date) { 
+		foreach($this->dates as $i => $date) {
 			$total = PageStatistics::getTotalPagesCreated($date);
 			$newPages[$this->labels[$i]] = $total;
 		}
@@ -35,7 +35,7 @@ class Statistics extends DashboardPageController {
 
 	protected function setLatestRegistrations() {
 		$registrationsArray = array();
-		foreach($this->dates as $i => $date) { 
+		foreach($this->dates as $i => $date) {
 			$total = UserStatistics::getTotalRegistrationsForDay($date);
 			$registrationsArray[$this->labels[$i]] = $total;
 		}
@@ -48,27 +48,27 @@ class Statistics extends DashboardPageController {
 			$date = date('Y-m-d', strtotime($i . ' days'));
 			if ($i == 0) {
 				$label = t('Today');
-			} else { 
+			} else {
 				$label = $dh->date('D', strtotime($i . ' days'));
 			}
 			$this->labels[] = $label;
 			$this->dates[] = $date;
 		}
 	}
-	
+
 	protected function setDownloadStatistics() {
 		$downloads = File::getDownloadStatistics(5);
 		$this->set('downloads', $downloads);
 	}
-	
+
 	public function view() {
-		$this->addHeaderItem(Loader::helper('html')->javascript('jquery.visualize.js'));
-		$this->addHeaderItem(Loader::helper('html')->css('jquery.visualize.css'));
+		$this->addFooterItem(Loader::helper('html')->javascript('jquery.visualize.js'));
+		$this->addFooterItem(Loader::helper('html')->css('jquery.visualize.css'));
 		$this->setLatestPageViews();
 		$this->setLatestPagesCreated();
 		$this->setLatestRegistrations();
 		$this->setDownloadStatistics();
-		
+
 		$this->set('totalVersions', PageStatistics::getTotalPageVersions());
 		$this->set('totalEditMode', PageStatistics::getTotalPagesCheckedOut());
 
