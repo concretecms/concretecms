ab491d278a7c77df35683c745bc2b0f02d22f432
Korvin Szanto <korvinszanto@gmail.com>
2014-05-01 19:01:25 -0700

    Remove `Router` singleton in favor of a facade. Singletons make it difficult to test / extend code, facades remove these concerns while still providing a global instance that can be replaced.

--- web/concrete/config/aliases.php
+++ web/concrete/config/aliases.php
@@ -1,5 +1,4 @@
-<?
-defined('C5_EXECUTE') or die("Access Denied.");
+<?php defined('C5_EXECUTE') or die("Access Denied.");
 return array(
 	'Request' => '\Concrete\Core\Http\Request',
 	'Environment' => '\Concrete\Core\Foundation\Environment',
