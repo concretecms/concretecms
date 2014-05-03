1507decb438bd8fad8c21bd594e3a7b9f6e80f9a
Andrew Embler <andrew@concrete5.org>
2014-05-03 11:27:28 -0700

    Update newsflow.php
    Removing var_dump, reformatting.

--- web/concrete/tools/newsflow.php
+++ web/concrete/tools/newsflow.php
@@ -5,13 +5,12 @@ $newsflow = new Concrete\Core\Activity\Newsflow();
 
 if (Loader::helper('validation/numbers')->integer($_REQUEST['cID'])) {
     $ed = $newsflow->getEditionByID($_REQUEST['cID']);
-    var_dump($ed);
-	if ($ed !== false) {
-		print $ed->getContent();
-	}
+    if ($ed !== false) {
+        print $ed->getContent();
+    }
 } else if (isset($_REQUEST['cPath'])) {
-	$ed = $newsflow->getEditionByPath($_REQUEST['cPath']);
-	if ($ed !== false) {
-		print $ed->getContent();
-	}
+    $ed = $newsflow->getEditionByPath($_REQUEST['cPath']);
+    if ($ed !== false) {
+        print $ed->getContent();
+    }
 }
