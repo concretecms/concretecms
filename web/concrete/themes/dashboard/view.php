443d054cfde7939cc63760865b10215515bca3f2
Michele Locati <michele@locati.it>
2014-05-09 09:27:15 +0200

    Translate header of dashboard pages

--- web/concrete/themes/dashboard/view.php
+++ web/concrete/themes/dashboard/view.php
@@ -3,7 +3,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); 
 ?>
 
-<header><h1><?=$pageTitle?></h1></header>
+<header><h1><?=t($pageTitle)?></h1></header>
 
 <?
 if (isset($error)) {
