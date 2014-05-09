38af155c4b17647b8f7f0f574b6d9c0ac853953c
Korvin Szanto <korvinszanto@gmail.com>
2014-05-09 10:30:05 -0700

    Add logging to whoops

--- web/concrete/core/Error/Provider/WhoopsServiceProvider.php
+++ web/concrete/core/Error/Provider/WhoopsServiceProvider.php
@@ -1,16 +1,16 @@
 <?php
 namespace Concrete\Core\Error\Provider;
+
 use Concrete\Core\Error\Handler\ErrorHandler;
 use Concrete\Core\Foundation\Service\Provider;
-use Whoops\Example\Exception;
-use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 class WhoopsServiceProvider extends Provider
 {
 
     public function register()
     {
-        $run     = new \Whoops\Run;
+        $run     = new Run;
         $handler = new ErrorHandler();
 
         $run->pushHandler($handler);
