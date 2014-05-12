05646c2dc16292e71ed3db0b7408b57cde8294b3
Andrew Embler <andrew@concrete5.org>
2014-05-12 12:51:59 -0700

    logging work

--- web/concrete/core/Logging/Logger.php
+++ web/concrete/core/Logging/Logger.php
@@ -3,6 +3,7 @@ namespace Concrete\Core\Logging;
 
 use Concrete\Core\Logging\Handler\DatabaseHandler;
 use \Monolog\Logger as MonologLogger;
+use \Monolog\Formatter\LineFormatter;
 
 class Logger
 {
@@ -19,5 +20,40 @@ class Logger
     public function addDatabaseHandler()
     {
         $handler = new DatabaseHandler();
+        // set a more basic formatter.
+        $output = "%message%";
+        $formatter = new LineFormatter($output);
+        $handler->setFormatter($formatter);
+        $this->library->pushHandler($handler);
+    }
+
+    protected $levels = array(
+        MonologLogger::EMERGENCY => 'emergency',
+        MonologLogger::ALERT => 'alert',
+        MonologLogger::CRITICAL => 'critical',
+        MonologLogger::ERROR => 'error',
+        MonologLogger::WARNING => 'warning',
+        MonologLogger::NOTICE => 'notice',
+        MonologLogger::INFO => 'info',
+        MonologLogger::DEBUG => 'debug'
+    );
+
+    public function __call($nm, $args)
+    {
+        if (in_array($nm, $this->levels)) {
+            $writeMethod = 'add' . ucfirst($nm);
+            return call_user_func_array(array($this->library, $writeMethod), $args);
+        }
+    }
+
+    /**
+     * When given a PSR-3 standard log level, returns the
+     * internal code for that level.
+     */
+    public function getLevelCode($code)
+    {
+        if (in_array($code, $this->levels)) {
+            return array_search($code, $this->levels);
+        }
     }
 }
