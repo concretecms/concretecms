47d8f1a76a0674a880578fe6e9ebe14af7742696
Andrew Embler <andrew@concrete5.org>
2014-05-14 07:35:46 -0700

    monologgin'

--- web/concrete/core/Logging/Logger.php
+++ web/concrete/core/Logging/Logger.php
@@ -4,6 +4,7 @@ namespace Concrete\Core\Logging;
 use Concrete\Core\Logging\Handler\DatabaseHandler;
 use \Monolog\Logger as MonologLogger;
 use \Monolog\Formatter\LineFormatter;
+use Database;
 use Events;
 
 class Logger extends MonologLogger
@@ -54,4 +55,35 @@ class Logger extends MonologLogger
     {
         return $this->handlers;
     }
+
+    /**
+     * Returns a list of channels that have been used. Requires the database
+     *  handler
+     */
+    public function getChannels()
+    {
+        $db = Database::get();
+        $channels = (array) $db->FetchAssoc('select distinct channel from Logs order by channel asc');
+        return $channels;
+    }
+
+
+    /**
+     * Clears all log entries. Requires the database handler.
+     */
+    public static function clearAll()
+    {
+        $db = Database::get();
+        $db->Execute('delete from Logs');
+    }
+
+    /**
+     * Clears log entries by channel. Requires the database handler.
+     */
+    public static function clearByChannel($channel)
+    {
+        $db = Database::get();
+        $db->delete('Logs', array('channel' => $channel));
+    }
+
 }
