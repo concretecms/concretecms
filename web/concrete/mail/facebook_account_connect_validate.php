e610c7ed187b43c32046d41330197a5363fe3d01
Fumito Mizuno <ounziw@users.noreply.github.com>
2014-05-15 17:02:02 +0900

    Update facebook_account_connect_validate.php
    Correct the number of *
    
    Former-commit-id: c69a667120ac5287bc6aa4842d373a6eca7cdd35

--- web/concrete/mail/facebook_account_connect_validate.php
+++ web/concrete/mail/facebook_account_connect_validate.php
@@ -3,7 +3,7 @@
 $subject = t('A new facebook account has been connected to your %s account, verify within.',SITE);
 
 $b = array();
-$b[] = t('** DO NOT REPLY TO THE MESSAGE BELOW. ***');
+$b[] = t('*** DO NOT REPLY TO THE MESSAGE BELOW. ***');
 $b[] = t('Someone has tried to attach a facebook account to your %s account,',SITE)."\n".t('If this is you, please follow the link below:');
 $b[] = t('Please go to this like to validate: %s',$validateurl);
 
