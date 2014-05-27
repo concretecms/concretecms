b9c3b605fd7c2e8772552d5e15b3507b01cc4b64
Michele Locati <michele@locati.it>
2014-05-27 16:25:04 +0200

    Indentation fixes

--- web/concrete/core/StyleCustomizer/Preset.php
+++ web/concrete/core/StyleCustomizer/Preset.php
@@ -70,19 +70,19 @@ class Preset {
 
     /** Returns the display name for this preset (localized and escaped accordingly to $format)
      * @param string $format = 'html'
-     *	Escape the result in html format (if $format is 'html').
-     *	If $format is 'text' or any other value, the display name won't be escaped.
+     *   Escape the result in html format (if $format is 'html').
+     *   If $format is 'text' or any other value, the display name won't be escaped.
      * @return string
      */
     public function getPresetDisplayName($format = 'html') {
-    	$value = tc('PresetName', $this->getPresetName());
-    	switch($format) {
-    		case 'html':
-    			return h($value);
-    		case 'text':
-    		default:
-    			return $value;
-    	}
+        $value = tc('PresetName', $this->getPresetName());
+        switch($format) {
+            case 'html':
+                return h($value);
+            case 'text':
+            default:
+                return $value;
+        }
     }
 
     public function isDefaultPreset()
