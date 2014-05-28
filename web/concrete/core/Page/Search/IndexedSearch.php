4058e308d3ff964c812bebbf9668d888fd800bfb
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-05-28 00:34:28 -0400

    PSR-2 Formatting of files I touched

--- web/concrete/core/Page/Search/IndexedSearch.php
+++ web/concrete/core/Page/Search/IndexedSearch.php
@@ -1,5 +1,6 @@
-<?
+<?php
 namespace Concrete\Core\Page\Search;
+
 use Loader;
 use Config;
 use Cache;
@@ -10,151 +11,188 @@ use Concrete\Core\Area\SubArea;
 use Block;
 use stdClass;
 
-class IndexedSearch {
+class IndexedSearch
+{
 
-	public $searchBatchSize = PAGE_SEARCH_INDEX_BATCH_SIZE;
-	public $searchReindexTimeout = PAGE_SEARCH_INDEX_LIFETIME;
+    public $searchBatchSize = PAGE_SEARCH_INDEX_BATCH_SIZE;
+    public $searchReindexTimeout = PAGE_SEARCH_INDEX_LIFETIME;
 
-	private $cPathSections = array();
-	private $searchableAreaNames;
+    private $cPathSections = array();
+    private $searchableAreaNames;
 
-	public function getSearchableAreaAction() {
-		$action = Config::get('SEARCH_INDEX_AREA_METHOD');
-		if (!strlen($action)) {
-			$action = 'blacklist';
-		}
-		return $action;
-	}
+    public function getSearchableAreaAction()
+    {
+        $action = Config::get('SEARCH_INDEX_AREA_METHOD');
+        if (!strlen($action)) {
+            $action = 'blacklist';
+        }
+        return $action;
+    }
 
-	public function getSavedSearchableAreas() {
-		$areas = Config::get('SEARCH_INDEX_AREA_LIST');
-		$areas = unserialize($areas);
-		if (!is_array($areas)) {
-			$areas = array();
-		}
-		return $areas;
-	}
+    public function getSavedSearchableAreas()
+    {
+        $areas = Config::get('SEARCH_INDEX_AREA_LIST');
+        $areas = unserialize($areas);
+        if (!is_array($areas)) {
+            $areas = array();
+        }
+        return $areas;
+    }
 
-	public function clearSearchIndex() {
-		$db = Loader::db();
-		$db->Execute('truncate table PageSearchIndex');
-	}
+    public function clearSearchIndex()
+    {
+        $db = Loader::db();
+        $db->Execute('truncate table PageSearchIndex');
+    }
 
-	public function matchesArea($arHandle) {
-		if (!isset($this->arHandles)) {
-			$searchableAreaNamesInitial=$this->getSavedSearchableAreas();
-			if ($this->getSearchableAreaAction() == 'blacklist') {
-				$areas = Area::getHandleList();
-				foreach($areas as $arHandle) {
-					if (!in_array($arHandle, $searchableAreaNamesInitial)) {
-						$this->searchableAreaNames[] = $arHandle;
-					}
-				}
-			} else {
-				$this->searchableAreaNames = $searchableAreaNamesInitial;
-			}
-		}
+    public function matchesArea($arHandle)
+    {
+        if (!isset($this->arHandles)) {
+            $searchableAreaNamesInitial = $this->getSavedSearchableAreas();
+            if ($this->getSearchableAreaAction() == 'blacklist') {
+                $areas = Area::getHandleList();
+                foreach ($areas as $arHandle) {
+                    if (!in_array($arHandle, $searchableAreaNamesInitial)) {
+                        $this->searchableAreaNames[] = $arHandle;
+                    }
+                }
+            } else {
+                $this->searchableAreaNames = $searchableAreaNamesInitial;
+            }
+        }
 
-		foreach($this->searchableAreaNames as $sarHandle) {
-			if (preg_match('/^' . $sarHandle . SubArea::AREA_SUB_DELIMITER . '.+/i', $arHandle)) {
-				return true;
-			} else if (in_array($arHandle, $this->searchableAreaNames)) {
-				return true;
-			}
-		}
+        foreach ($this->searchableAreaNames as $sarHandle) {
+            if (preg_match('/^' . $sarHandle . SubArea::AREA_SUB_DELIMITER . '.+/i', $arHandle)) {
+                return true;
+            } else {
+                if (in_array($arHandle, $this->searchableAreaNames)) {
+                    return true;
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public function reindexPage($page) {
-		$db = Loader::db();
-		if (is_object($page) && ($page instanceof Collection) && ($page->getAttribute('exclude_search_index') != 1)) {
-			$datetime = Loader::helper('date')->getSystemDateTime();
-			$db->Replace('PageSearchIndex', array(
-				'cID' => $page->getCollectionID(),
-				'cName' => $page->getCollectionName(),
-				'cDescription' => $page->getCollectionDescription(),
-				'cPath' => $page->getCollectionPath(),
-				'cDatePublic' => $page->getCollectionDatePublic(),
-				'content' => $this->getBodyContentFromPage($page),
-				'cDateLastIndexed' => $datetime
-			), array('cID'), true);
-		} else {
-			$db->Execute('delete from PageSearchIndex where cID = ?', array($page->getCollectionID()));
-		}
-	}
+    public function reindexPage($page)
+    {
+        $db = Loader::db();
+        if (is_object($page) && ($page instanceof Collection) && ($page->getAttribute('exclude_search_index') != 1)) {
+            $datetime = Loader::helper('date')->getSystemDateTime();
+            $db->Replace(
+                'PageSearchIndex',
+                array(
+                    'cID' => $page->getCollectionID(),
+                    'cName' => $page->getCollectionName(),
+                    'cDescription' => $page->getCollectionDescription(),
+                    'cPath' => $page->getCollectionPath(),
+                    'cDatePublic' => $page->getCollectionDatePublic(),
+                    'content' => $this->getBodyContentFromPage($page),
+                    'cDateLastIndexed' => $datetime
+                ),
+                array('cID'),
+                true
+            );
+        } else {
+            $db->Execute('delete from PageSearchIndex where cID = ?', array($page->getCollectionID()));
+        }
+    }
 
-	public function getBodyContentFromPage($c) {
+    public function getBodyContentFromPage($c)
+    {
 
-		$text = '';
+        $text = '';
 
-		$tagsToSpaces=array('<br>','<br/>','<br />','<p>','</p>','</ p>','<div>','</div>','</ div>','&nbsp;');
-		$blarray=array();
-		$db = Loader::db();
-		$r = $db->Execute('select bID, arHandle from CollectionVersionBlocks where cID = ? and cvID = ?', array($c->getCollectionID(), $c->getVersionID()));
-		$th = Loader::helper('text');
-		while ($row = $r->FetchRow()) {
-			if ($this->matchesArea($row['arHandle'])) {
-				$b = Block::getByID($row['bID'], $c, $row['arHandle']);
-				if (!is_object($b)) {
-					continue;
-				}
-				$bi = $b->getInstance();
-				if(method_exists($bi,'getSearchableContent')){
-					$searchableContent = $bi->getSearchableContent();
-					if(strlen(trim($searchableContent)))
-						$text .= $th->decodeEntities(strip_tags(str_ireplace($tagsToSpaces,' ',$searchableContent)), ENT_QUOTES, APP_CHARSET).' ';
-				}
-				unset($b);
-				unset($bi);
-			}
-		}
+        $tagsToSpaces = array(
+            '<br>',
+            '<br/>',
+            '<br />',
+            '<p>',
+            '</p>',
+            '</ p>',
+            '<div>',
+            '</div>',
+            '</ div>',
+            '&nbsp;'
+        );
+        $blarray = array();
+        $db = Loader::db();
+        $r = $db->Execute(
+            'select bID, arHandle from CollectionVersionBlocks where cID = ? and cvID = ?',
+            array($c->getCollectionID(), $c->getVersionID())
+        );
+        $th = Loader::helper('text');
+        while ($row = $r->FetchRow()) {
+            if ($this->matchesArea($row['arHandle'])) {
+                $b = Block::getByID($row['bID'], $c, $row['arHandle']);
+                if (!is_object($b)) {
+                    continue;
+                }
+                $bi = $b->getInstance();
+                if (method_exists($bi, 'getSearchableContent')) {
+                    $searchableContent = $bi->getSearchableContent();
+                    if (strlen(trim($searchableContent))) {
+                        $text .= $th->decodeEntities(
+                                strip_tags(str_ireplace($tagsToSpaces, ' ', $searchableContent)),
+                                ENT_QUOTES,
+                                APP_CHARSET
+                            ) . ' ';
+                    }
+                }
+                unset($b);
+                unset($bi);
+            }
+        }
 
-		return $text;
-	}
+        return $text;
+    }
 
-	/**
-	 * Reindexes the search engine.
-	 */
-	public function reindexAll($fullReindex = false) {
-		Cache::disableLocalCache();
+    /**
+     * Reindexes the search engine.
+     */
+    public function reindexAll($fullReindex = false)
+    {
+        Cache::disableLocalCache();
 
-		$db = Loader::db();
+        $db = Loader::db();
 
 
-		if ($fullReindex) {
-			$db->Execute("truncate table PageSearchIndex");
-		}
+        if ($fullReindex) {
+            $db->Execute("truncate table PageSearchIndex");
+        }
 
-		$pl = new PageList();
-		$pl->ignoreAliases();
-		$pl->ignorePermissions();
-		$pl->sortByCollectionIDAscending();
-		$pl->filter(false, '(c.cDateModified > psi.cDateLastIndexed or UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(psi.cDateLastIndexed) > ' . $this->searchReindexTimeout . ' or psi.cID is null or psi.cDateLastIndexed is null)');
-		$pl->filter(false, '(ak_exclude_search_index is null or ak_exclude_search_index = 0)');
-		$pages = $pl->get($this->searchBatchSize);
+        $pl = new PageList();
+        $pl->ignoreAliases();
+        $pl->ignorePermissions();
+        $pl->sortByCollectionIDAscending();
+        $pl->filter(
+            false,
+            '(c.cDateModified > psi.cDateLastIndexed or UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(psi.cDateLastIndexed) > ' . $this->searchReindexTimeout . ' or psi.cID is null or psi.cDateLastIndexed is null)'
+        );
+        $pl->filter(false, '(ak_exclude_search_index is null or ak_exclude_search_index = 0)');
+        $pages = $pl->get($this->searchBatchSize);
 
-		$num = 0;
-		foreach($pages as $c) {
+        $num = 0;
+        foreach ($pages as $c) {
 
-			// make sure something is approved
-			$cv = $c->getVersionObject();
-			if(!$cv->cvIsApproved) {
-				continue;
-			}
+            // make sure something is approved
+            $cv = $c->getVersionObject();
+            if (!$cv->cvIsApproved) {
+                continue;
+            }
 
-			$c->reindex($this, true);
-			$num++;
-			unset($c);
-		}
+            $c->reindex($this, true);
+            $num++;
+            unset($c);
+        }
 
-		$pnum = Collection::reindexPendingPages();
-		$num = $num + $pnum;
+        $pnum = Collection::reindexPendingPages();
+        $num = $num + $pnum;
 
-		Cache::enableLocalCache();
-		$result = new stdClass;
-		$result->count = $num;
-		return $result;
-	}
+        Cache::enableLocalCache();
+        $result = new stdClass;
+        $result->count = $num;
+        return $result;
+    }
 
 }
--- web/concrete/jobs/index_search_all.php
+++ web/concrete/jobs/index_search_all.php
@@ -1,5 +1,6 @@
-<?
+<?php
 namespace Concrete\Job;
+
 use Loader;
 use QueueableJob;
 use \Concrete\Core\Page\Search\IndexedSearch;
@@ -9,50 +10,58 @@ use UserAttributeKey;
 use Page;
 use Database;
 
-class IndexSearchAll extends QueueableJob {
+class IndexSearchAll extends QueueableJob
+{
 
-	public $jNotUninstallable=1;
-	public $jSupportsQueue = true;
+    public $jNotUninstallable = 1;
+    public $jSupportsQueue = true;
 
-	public function getJobName() {
-		return t("Index Search Engine - All");
-	}
+    public function getJobName()
+    {
+        return t("Index Search Engine - All");
+    }
 
-	public function getJobDescription() {
-		return t("Empties the page search index and reindexes all pages.");
-	}
+    public function getJobDescription()
+    {
+        return t("Empties the page search index and reindexes all pages.");
+    }
 
-	public function start(\Zend_Queue $q) {
-		$this->is = new IndexedSearch();
+    public function start(\Zend_Queue $q)
+    {
+        $this->is = new IndexedSearch();
 
-		$attributes = CollectionAttributeKey::getList();
-		$attributes = array_merge($attributes, FileAttributeKey::getList());
-		$attributes = array_merge($attributes, UserAttributeKey::getList());
-		foreach($attributes as $ak) {
-			$ak->updateSearchIndex();
-		}
+        $attributes = CollectionAttributeKey::getList();
+        $attributes = array_merge($attributes, FileAttributeKey::getList());
+        $attributes = array_merge($attributes, UserAttributeKey::getList());
+        foreach ($attributes as $ak) {
+            $ak->updateSearchIndex();
+        }
 
-		$db = Loader::db();
-		$db->Execute('truncate table PageSearchIndex');
-		$r = $db->Execute('select Pages.cID from Pages left join CollectionSearchIndexAttributes csia on Pages.cID = csia.cID where (ak_exclude_search_index is null or ak_exclude_search_index = 0) and cIsActive = 1');
-		while ($row = $r->FetchRow()) {
-			$q->send($row['cID']);
-		}
-	}
+        $db = Loader::db();
+        $db->Execute('truncate table PageSearchIndex');
+        $r = $db->Execute(
+            'select Pages.cID from Pages left join CollectionSearchIndexAttributes csia on Pages.cID = csia.cID where (ak_exclude_search_index is null or ak_exclude_search_index = 0) and cIsActive = 1'
+        );
+        while ($row = $r->FetchRow()) {
+            $q->send($row['cID']);
+        }
+    }
 
-	public function finish(\Zend_Queue $q) {
-		$db = Loader::db();
-		$total = $db->GetOne('select count(*) from PageSearchIndex');
-		return t('Index updated. %s pages indexed.', $total);
-	}
+    public function finish(\Zend_Queue $q)
+    {
+        $db = Loader::db();
+        $total = $db->GetOne('select count(*) from PageSearchIndex');
+        return t('Index updated. %s pages indexed.', $total);
+    }
 
-	public function processQueueItem(\Zend_Queue_Message $msg) {
-		$c = Page::getByID($msg->body, 'ACTIVE');
-		$cv = $c->getVersionObject();
-		if (is_object($cv)) {
-			$c->reindex($this->is, true);
-		}
-	}
+    public function processQueueItem(\Zend_Queue_Message $msg)
+    {
+        $c = Page::getByID($msg->body, 'ACTIVE');
+        $cv = $c->getVersionObject();
+        if (is_object($cv)) {
+            $c->reindex($this->is, true);
+        }
+    }
 
 
 }
