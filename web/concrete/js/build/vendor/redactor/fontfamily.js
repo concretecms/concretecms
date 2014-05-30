534363f4ad61a398ac4f1891bc94a3ef346d68a5
Andrew Embler <andrew@concrete5.org>
2014-05-30 13:53:16 -0700

    more redactorin
    Former-commit-id: db89969f475ac42d2f653473ee55eb883406e2c0

--- web/concrete/js/build/vendor/redactor/fontfamily.js
+++ web/concrete/js/build/vendor/redactor/fontfamily.js
@@ -1,27 +1,27 @@
 if (!RedactorPlugins) var RedactorPlugins = {};
 
 RedactorPlugins.fontfamily = {
-	init: function ()
-	{
-		var fonts = [ 'Arial', 'Helvetica', 'Georgia', 'Times New Roman', 'Monospace' ];
-		var that = this;
-		var dropdown = {};
+    init: function ()
+    {
+        var fonts = [ 'Arial', 'Helvetica', 'Georgia', 'Times New Roman', 'Monospace' ];
+        var that = this;
+        var dropdown = {};
 
-		$.each(fonts, function(i, s)
-		{
-			dropdown['s' + i] = { title: s, callback: function() { that.setFontfamily(s); }};
-		});
+        $.each(fonts, function(i, s)
+        {
+            dropdown['s' + i] = { title: s, callback: function() { that.setFontfamily(s); }};
+        });
 
-		dropdown['remove'] = { title: 'Remove font', callback: function() { that.resetFontfamily(); }};
+        dropdown['remove'] = { title: 'Remove font', callback: function() { that.resetFontfamily(); }};
 
-		this.buttonAdd('fontfamily', 'Change font family', false, dropdown);
-	},
-	setFontfamily: function (value)
-	{
-		this.inlineSetStyle('font-family', value);
-	},
-	resetFontfamily: function()
-	{
-		this.inlineRemoveStyle('font-family');
-	}
+        this.buttonAddAfter('formatting', 'fontfamily', 'Change font family', false, dropdown);
+    },
+    setFontfamily: function (value)
+    {
+        this.inlineSetStyle('font-family', value);
+    },
+    resetFontfamily: function()
+    {
+        this.inlineRemoveStyle('font-family');
+    }
 };
