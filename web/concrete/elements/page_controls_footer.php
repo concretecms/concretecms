d54071311a85cdacbd4e4f41e1abb58df5621b23
Andrew Embler <andrew@concrete5.org>
2014-06-05 16:44:33 -0700

    merges and fixes

--- web/concrete/elements/page_controls_footer.php
+++ web/concrete/elements/page_controls_footer.php
@@ -149,7 +149,7 @@ if (isset($cp) && $canViewToolbar && (!$dh->inDashboard())) {
 				$wf = $wl->getWorkflowObject(); ?>
 				
 				<form method="post" action="<?=$wl->getWorkflowProgressFormAction()?>" id="ccm-notification-page-alert-form-<?=$i?>">
-					<i class="fa fa-info-sign"></i>
+					<i class="fa fa-info-circle"></i>
 					<div class="ccm-notification-inner">
 						<p><?=$wf->getWorkflowProgressCurrentDescription($wl)?></p>
 					<? $actions = $wl->getWorkflowProgressActions(); ?>
