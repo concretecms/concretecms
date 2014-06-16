604db9569dc123d57dd528ccf81a6954bcecb01f
Andrew Embler <andrew@concrete5.org>
2014-06-16 14:51:40 -0700

    Revert "Merge pull request #217 from Shotster/fix-permission-assignment"
    This reverts commit 0388b66211b24d7a050247cfa52f1d69919609e2, reversing
    changes made to 3b02af55269941ed9b192bebbc1b7b9205a781c5.

--- web/concrete/core/Permission/Key/Key.php
+++ web/concrete/core/Permission/Key/Key.php
@@ -348,7 +348,8 @@ abstract class Key extends Object {
 	public function getPermissionAssignmentObject() {
 		if (is_object($this->permissionObject)) {
 			$className = $this->permissionObject->getPermissionAssignmentClassName();
-			$targ = Core::make($className, array($this->permissionObject));
+			$targ = Core::make($className);
+			$targ->setPermissionObject($this->permissionObject);
 		} else {
 			$targ = new PermissionAssignment();
 		}
