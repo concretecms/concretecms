6f07ceb931946934aab863a8d100944c599b6962
jobbrown <job@JOB-MBP.local>
2014-06-21 22:02:01 +0200

    Removed ccm-pane-body. Removed use of interface helper. Removed BS3 references (hopefully all of them).

--- web/concrete/single_pages/dashboard/system/permissions/maintenance.php
+++ web/concrete/single_pages/dashboard/system/permissions/maintenance.php
@@ -1,29 +1,21 @@
 <? defined('C5_EXECUTE') or die("Access Denied.");
 $h = Loader::helper('concrete/dashboard');
-$ih = Loader::helper('concrete/ui');
 $form = Loader::helper('form');
 echo $h->getDashboardPaneHeaderWrapper(t('Maintenance Mode'), false, 'span6 offset3', false);?>
 
-<form id="maintenance-mode-form" action="<?php echo $view->action('')?>" method="post">
-	<div class="ccm-pane-body">
-		<?php echo $this->controller->token->output('update_maintenance')?>
-		<fieldset>
-			<legend style="margin-bottom: 0px"><?=t('Maintenance Mode')?></legend>
-			<div class="control-group">
-				<div class="controls">
-					<label class="radio"><?=$form->radio('site_maintenance_mode', '1', $site_maintenance_mode)?> <span><?=t('Enabled')?></span></label>
-					<label class="radio"><?=$form->radio('site_maintenance_mode', '0', $site_maintenance_mode)?> <span><?=t('Disabled')?></span></label>
-				</div>
-			</div>
-		</fieldset>	
-	</div>
+<form id="maintenance-mode-form" action="<?php echo $view->action('')?>" method="post" role="form">
+	<?php echo $this->controller->token->output('update_maintenance')?>
+	<fieldset>
+		<legend style="margin-bottom: 0px"><?=t('Maintenance Mode')?></legend>
+		<div class="form-group">
+			<label class="radio"><?=$form->radio('site_maintenance_mode', '1', $site_maintenance_mode)?> <span><?=t('Enabled')?></span></label>
+		    <label class="radio"><?=$form->radio('site_maintenance_mode', '0', $site_maintenance_mode)?> <span><?=t('Disabled')?></span></label>
+		</div>
+	</fieldset>	
 	
 	<div class="ccm-dashboard-form-actions-wrapper">
 		<div class="ccm-dashboard-form-actions">
-			<?php
-				$submit = $ih->submit( t('Save'), 'maintenance-mode-form', 'right', 'primary');
-				print $submit;
-			?>
+			<input type="submit" class="btn primary pull-right" name="maintenance-mode-form" value="<?=t('Save')?>">
 		</div>
 	</div>
 </form>
