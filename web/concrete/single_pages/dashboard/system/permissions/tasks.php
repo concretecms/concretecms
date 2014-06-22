6503b496363361bceb1bbba2556611b6e94f4ffa
jobbrown <job@JOB-MBP.local>
2014-06-22 03:08:32 +0200

    Reskin for 5.7
    Former-commit-id: da1e2fd8feb6836d6e3b8bdb1cba992b1b5dd376

--- web/concrete/single_pages/dashboard/system/permissions/tasks.php
+++ web/concrete/single_pages/dashboard/system/permissions/tasks.php
@@ -8,7 +8,7 @@
 	<?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Task Permissions'), $help, 'span8 offset2', false)?>
 	<form method="post" action="<?=$view->action('save')?>">
 	<?=Loader::helper('validation/token')->output('save_permissions')?>
-	<div class="ccm-pane-body">
+	
 	<?
 	$tp = new TaskPermission();
 	if ($tp->canAccessTaskPermissions()) { ?>	
@@ -16,10 +16,13 @@
 	<? } else { ?>
 		<p><?=t('You cannot access task permissions.')?></p>
 	<? } ?>
-	</div>
-	<div class="ccm-pane-footer">
-		<a href="<?=$view->url('/dashboard/system/permissions/tasks')?>" class="btn btn-default pull-left"><?=t('Cancel')?></a>
-		<button type="submit" value="<?=t('Save')?>" class="btn btn-primary pull-right"><?=t('Save')?> <i class="icon-ok-sign icon-white"></i></button>
-	</div>
+	
+	
+	<div class="ccm-dashboard-form-actions-wrapper">
+        <div class="ccm-dashboard-form-actions">
+            <a href="<?=$view->url('/dashboard/system/permissions/tasks')?>" class="btn btn-default pull-left"><?=t('Cancel')?></a>
+            <button class="pull-right btn btn-success" type="submit" ><?=t('Save')?> <i class="icon-ok-sign icon-white"></i></button>
+        </div>
+    </div>
 	</form>
 	<?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false)?>
