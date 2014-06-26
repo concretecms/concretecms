03af4fe799ec48a4f3ea6aa5390fcdcf3f2c4e6e
Evan Cooper <evan@concretecms.com>
2014-06-25 17:25:01 -0700

    SEO excluded URL Word List #279
    Former-commit-id: a487a4f79c6930d6e9eacc9675f7e514043578a9

--- web/concrete/single_pages/dashboard/system/seo/excluded.php
+++ web/concrete/single_pages/dashboard/system/seo/excluded.php
@@ -1,18 +1,16 @@
 <?php defined('C5_EXECUTE') or die('Access Denied');
-$form = Loader::helper('form');
-echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Excluded URL Word List'), t("Words listed here will be automatically removed from url slugs."), false, false); ?>
+$form = Loader::helper('form'); ?>
 <form method="post" id="url-form" action="<?php echo $view->action('save')?>">
-	<div class="ccm-pane-body">
 		<div class="control-group">
 			<textarea style='width:100%;height:100px' name='SEO_EXCLUDE_WORDS'><?=$SEO_EXCLUDE_WORDS?></textarea>
 		</div>
 		<div class="alert alert-info"><?=t('Separate reserved words with a comma. These words will be automatically removed from URL slugs. To remove no words from URLs, delete all the words above.')?></div>
-	</div>
-	<div class="ccm-pane-footer">
-	<? if (count($SEO_EXCLUDE_WORDS_ORIGINAL_ARRAY) != count($SEO_EXCLUDE_WORDS_ARRAY) || !$SEO_EXCLUDE_WORDS) { ?>
-		<a href="<?=$view->action('reset')?>" class="btn btn-default pull-left"><?=t('Reset To Default')?></a>
-	<? } ?>
-		<?php echo $interface->submit(t('Save'), null, 'right', 'primary');?>
-	</div>
+	    <div class="ccm-dashboard-form-actions-wrapper">
+            <div class="ccm-dashboard-form-actions">
+                <? if (count($SEO_EXCLUDE_WORDS_ORIGINAL_ARRAY) != count($SEO_EXCLUDE_WORDS_ARRAY) || !$SEO_EXCLUDE_WORDS) { ?>
+                    <a href="<?=$view->action('reset')?>" class="btn btn-default pull-left"><?=t('Reset To Default')?></a>
+                <? } ?>
+                    <?php echo $interface->submit(t('Save'), null, 'right', 'btn-primary');?>
+            </div>
+        </div>
 </form>
-<?php echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper();
