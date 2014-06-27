d72ec8bdc09c3237a09788e6274c231af43a3681
Evan Cooper <evan@concretecms.com>
2014-06-27 13:39:13 -0700

    proxy dashboard page cleanup #306

--- web/concrete/single_pages/dashboard/system/environment/proxy.php
+++ web/concrete/single_pages/dashboard/system/environment/proxy.php
@@ -1,51 +1,45 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$dh = Loader::helper('concrete/dashboard');
 
-echo $dh->getDashboardPaneHeaderWrapper(t('Proxy Server Settings'), t('Configures proxy server settings for your website'), 'span6 offset3', false);
 ?>
 
-<form method="post" class="form-stacked" id="proxy-form"
+<form method="post" id="proxy-form"
 	action="<?php echo $view->action('update_proxy'); ?>">
-	<div class="ccm-pane-body">
-		<?php echo $this->controller->token->output('update_proxy'); ?>
+    <?php echo $this->controller->token->output('update_proxy'); ?>
+    <div class="row">
+        <div class="col-md-4">
+            <fieldset>
+                <div class="form-group">
+                    <?=$form->label('http_proxy_host', t('Proxy Host'));?>
+                    <div class="input">
+                        <?=$form->text('http_proxy_host', $http_proxy_host)?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <?=$form->label('http_proxy_port', t('Proxy Port'));?>
+                    <div class="input">
+                        <?=$form->text('http_proxy_port', $http_proxy_port)?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <?=$form->label('http_proxy_user', t('Proxy User'));?>
+                    <div class="input">
+                        <?=$form->text('http_proxy_user', $http_proxy_user)?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <?=$form->label('http_proxy_pwd', t('Proxy Password'));?>
+                    <div class="input">
+                        <?=$form->text('http_proxy_pwd', $http_proxy_pwd)?>
+                    </div>
+                </div>
+            </fieldset>
 
-		<div class="clearfix">
-			<fieldset>
-				<legend>
-					<?=t('Proxy Server Settings')?>
-				</legend>
-				<div class="clearfix">
-					<?=$form->label('http_proxy_host', t('Proxy Host'));?>
-					<div class="input">
-						<?=$form->text('http_proxy_host', $http_proxy_host)?>
-					</div>
-				</div>
-				<div class="clearfix">
-					<?=$form->label('http_proxy_port', t('Proxy Port'));?>
-					<div class="input">
-						<?=$form->text('http_proxy_port', $http_proxy_port)?>
-					</div>
-				</div>
-				<div class="clearfix">
-					<?=$form->label('http_proxy_user', t('Proxy User'));?>
-					<div class="input">
-						<?=$form->text('http_proxy_user', $http_proxy_user)?>
-					</div>
-				</div>
-				<div class="clearfix">
-					<?=$form->label('http_proxy_pwd', t('Proxy Password'));?>
-					<div class="input">
-						<?=$form->text('http_proxy_pwd', $http_proxy_pwd)?>
-					</div>
-				</div>
-			</fieldset>
-		</div>
-	</div>
-
-	<div class="ccm-pane-footer">
-		<?php echo $interface->submit(t('Save'), 'proxy-form', 'right', 'primary'); ?>
-	</div>
+            <div class="ccm-dashboard-form-actions-wrapper">
+                <div class="ccm-dashboard-form-actions">
+                    <?php echo $interface->submit(t('Save'), 'proxy-form', 'right', 'btn-primary'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </form>
-
-<?php echo $dh->getDashboardPaneFooterWrapper(false); ?>
