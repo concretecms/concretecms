12ee05a3ab6f59cb8ce7c83febb44c0e9b088965
Andrew Embler <andrew@concrete5.org>
2014-07-03 13:24:45 -0700

    content work
    Former-commit-id: e5308f5016035df3da7c6592a2154b63c4a04373

--- web/application/themes/elemental/default.php
+++ web/application/themes/elemental/default.php
@@ -1,6 +1,14 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$this->inc('elements/header.php');
-?>
+$this->inc('elements/header.php'); ?>
+
+<main class="container">
+    <div class="col-md-12">
+        <?
+        $a = new Area('Main');
+        $a->display($c);
+        ?>
+    </div>
+</main>
 
 <?php  $this->inc('elements/footer.php'); ?>
