7418de825af31bcaee3e6a5fbee01ad067e7a2bb
Andrew Embler <andrew@concrete5.org>
2014-07-08 11:21:39 -0700

    Migrating elemental theme into the core, removing old greek yogurt starting point
    Former-commit-id: 91831cbe0f5c3a1fd85fb9c6e15a843313658956

--- web/concrete/themes/elemental/page_theme.php
+++ web/concrete/themes/elemental/page_theme.php
@@ -1,9 +1,9 @@
 <?
-namespace Application\Theme\Elemental;
+namespace Concrete\Theme\Elemental;
 class PageTheme extends \Concrete\Core\Page\Theme\Theme {
 
 	public function registerAssets() {
-		//$this->providesAsset('css', 'core/frontend/*');
+		$this->providesAsset('css', 'core/frontend/*');
 	}
 
     protected $pThemeGridFrameworkHandle = 'bootstrap3';
