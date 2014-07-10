23f6991602597bba378eb68c2edbe0a3db3f690d
Korvin Szanto <korvinszanto@gmail.com>
2014-07-10 09:57:31 -0700

    Reformat add_block controller

--- web/concrete/controllers/dialog/page/add_block.php
+++ web/concrete/controllers/dialog/page/add_block.php
@@ -1,119 +1,128 @@
 <?php
 namespace Concrete\Controller\Dialog\Page;
-use \Concrete\Controller\Backend\UserInterface\Page as BackendInterfacePageController;
-use BlockType;
+
 use Area;
+use Block;
+use BlockType;
+use Concrete\Controller\Backend\UserInterface\Page as BackendInterfacePageController;
+use Concrete\Core\Block\View\BlockView;
+use Exception;
+use Loader;
+use PageEditResponse;
+use Permissions;
 use Stack;
 use User;
-use Exception;
-use \Concrete\Core\Block\View\BlockView;
-use PageEditResponse;
-use Page;
-use Loader;
-use Permissions;
-use Block;
-class AddBlock extends BackendInterfacePageController {
 
-	protected $viewPath = '/dialogs/page/add_block';
+class AddBlock extends BackendInterfacePageController
+{
 
-	protected function canAccess() {
-		return $this->areaPermissions->canAddBlock($this->blockType);
-	}
+    protected $viewPath = '/dialogs/page/add_block';
 
-	public function __construct() {
-		parent::__construct();
-		$request = $this->request;
+    public function __construct()
+    {
+        parent::__construct();
+        $request = $this->request;
 
-		if (!Loader::helper('validation/numbers')->integer($_REQUEST['btID'])) {
-			throw new Exception(t('Access Denied'));
-		}
+        if (!Loader::helper('validation/numbers')->integer($_REQUEST['btID'])) {
+            throw new Exception(t('Access Denied'));
+        }
 
-		$this->blockType = BlockType::getByID($_REQUEST['btID']);
-		$this->area = Area::get($this->page, $_REQUEST['arHandle']);
-		$this->pageToModify = $this->page;
-		$this->areaToModify = $this->area;
-		if ($this->area->isGlobalArea()) {
-			$this->pageToModify = Stack::getByName($_REQUEST['arHandle']);
-			$this->areaToModify = Area::get($this->page, STACKS_AREA_NAME);
-		}
-		$this->areaPermissions = new Permissions($this->areaToModify);
-		$cnt = $this->blockType->getController();
-		if (!is_a($cnt, '\Concrete\Core\Block\BlockController')) {
-			throw new Exception(t('Unable to load the controller for this block type. Perhaps it has been moved or removed.'));
-		}
-		$this->blockTypeController = $cnt;
-	}
+        $this->blockType = BlockType::getByID($_REQUEST['btID']);
+        $this->area = Area::get($this->page, $_REQUEST['arHandle']);
+        $this->pageToModify = $this->page;
+        $this->areaToModify = $this->area;
+        if ($this->area->isGlobalArea()) {
+            $this->pageToModify = Stack::getByName($_REQUEST['arHandle']);
+            $this->areaToModify = Area::get($this->page, STACKS_AREA_NAME);
+        }
+        $this->areaPermissions = new Permissions($this->areaToModify);
+        $cnt = $this->blockType->getController();
+        if (!is_a($cnt, '\Concrete\Core\Block\BlockController')) {
+            throw new Exception(t(
+                                    'Unable to load the controller for this block type. Perhaps it has been moved or removed.'));
+        }
+        $this->blockTypeController = $cnt;
+    }
 
-	public function view() {
-		$bv = new BlockView($this->blockType);
-		$bv->setAreaObject($this->area);
-		// Handle special posted area parameters here
-		if (isset($_REQUEST['arGridMaximumColumns'])) {
-			$this->area->setAreaGridMaximumColumns(intval($_REQUEST['arGridMaximumColumns']));
-		}
+    public function view()
+    {
+        $bv = new BlockView($this->blockType);
+        $bv->setAreaObject($this->area);
+        // Handle special posted area parameters here
+        if (isset($_REQUEST['arGridMaximumColumns'])) {
+            $this->area->setAreaGridMaximumColumns(intval($_REQUEST['arGridMaximumColumns']));
+        }
         if (isset($_REQUEST['arEnableGridContainer']) && $_REQUEST['arEnableGridContainer'] == 1) {
             $this->area->enableGridContainer();
         }
-		$bv->addScopeItems(array('a' => $this->a, 'cp' => $this->permissions, 'ap' => $this->areaPermissions));
-		$this->set('blockView', $bv);
-		$this->set('blockType', $this->blockType);
-		$this->set('btHandle', $this->blockType->getBlockTypeHandle());
-		$this->set("blockTypeController", $this->blockTypeController);
-		$this->set('area', $this->area);
-	}
+        $bv->addScopeItems(array('a' => $this->a, 'cp' => $this->permissions, 'ap' => $this->areaPermissions));
+        $this->set('blockView', $bv);
+        $this->set('blockType', $this->blockType);
+        $this->set('btHandle', $this->blockType->getBlockTypeHandle());
+        $this->set("blockTypeController", $this->blockTypeController);
+        $this->set('area', $this->area);
+    }
 
-	public function submit() {
-		$pc = new PageEditResponse($this->error);
-		$pc->setPage($this->page);
-		if ($this->validateAction() || is_object($this->blockType)
+    public function submit()
+    {
+        $pc = new PageEditResponse($this->error);
+        $pc->setPage($this->page);
+        if ($this->validateAction() || is_object($this->blockType)
             && !$this->blockType->hasAddTemplate()
-            && Loader::helper('validation/token')->validate())
-        {
+            && Loader::helper('validation/token')->validate()
+        ) {
 
-			$data = $_POST;
-			$bt = $this->blockType;
-			$u = new User();
-			$data['uID'] = $u->getUserID();
+            $data = $_POST;
+            $bt = $this->blockType;
+            $u = new User();
+            $data['uID'] = $u->getUserID();
 
-			$e = $this->blockTypeController->validate($data);
-			if ((!is_object($e)) || (($e instanceof \Concrete\Core\Error\Error) && (!$e->has()))) {
+            $e = $this->blockTypeController->validate($data);
+            if ((!is_object($e)) || (($e instanceof \Concrete\Core\Error\Error) && (!$e->has()))) {
 
-				if (!$bt->includeAll()) {
-					$nvc = $this->pageToModify->getVersionToModify();
-					$nb = $nvc->addBlock($bt, $this->areaToModify, $data);
-				} else {
-					// if we apply to all, then we don't worry about a new version of the page
-					$nb = $cx->addBlock($bt, $this->areaToModify, $data);
-				}
+                if (!$bt->includeAll()) {
+                    $nvc = $this->pageToModify->getVersionToModify();
+                    $nb = $nvc->addBlock($bt, $this->areaToModify, $data);
+                } else {
+                    // if we apply to all, then we don't worry about a new version of the page
+                    $nb = $cx->addBlock($bt, $this->areaToModify, $data);
+                }
 
-				if ($this->area->isGlobalArea() && $nvc instanceof Collection) {
-					$xvc = $this->page->getVersionToModify(); // we need to create a new version of THIS page as well.
-					$xvc->relateVersionEdits($nvc);
-				}
+                if ($this->area->isGlobalArea() && $nvc instanceof Collection) {
+                    $xvc = $this->page->getVersionToModify(); // we need to create a new version of THIS page as well.
+                    $xvc->relateVersionEdits($nvc);
+                }
 
-				// now we check to see if there's a block in this area that we are adding it after.
-				if ($_REQUEST['dragAreaBlockID'] > 0 && Loader::helper('validation/numbers')->integer($_REQUEST['dragAreaBlockID'])) {
-					$db = Block::getByID($_REQUEST['dragAreaBlockID'], $this->pageToModify, $this->areaToModify);
-					if (is_object($db) && !$db->isError()) {
-						$nb->moveBlockToDisplayOrderPosition($db);
-					}
-				}
-				if (!is_object($db)) {
-					$nb->moveBlockToDisplayOrderPosition(false);
-				}
+                // now we check to see if there's a block in this area that we are adding it after.
+                if ($_REQUEST['dragAreaBlockID'] > 0 && Loader::helper('validation/numbers')->integer(
+                                                              $_REQUEST['dragAreaBlockID'])
+                ) {
+                    $db = Block::getByID($_REQUEST['dragAreaBlockID'], $this->pageToModify, $this->areaToModify);
+                    if (is_object($db) && !$db->isError()) {
+                        $nb->moveBlockToDisplayOrderPosition($db);
+                    }
+                }
+                if (!is_object($db)) {
+                    $nb->moveBlockToDisplayOrderPosition(false);
+                }
 
-				$pc->setAdditionalDataAttribute('btID', $nb->getBlockTypeID());
-				$pc->setAdditionalDataAttribute('bID', $nb->getBlockID());
-				$pc->setAdditionalDataAttribute('arHandle', $this->area->getAreaHandle());
+                $pc->setAdditionalDataAttribute('btID', $nb->getBlockTypeID());
+                $pc->setAdditionalDataAttribute('bID', $nb->getBlockID());
+                $pc->setAdditionalDataAttribute('arHandle', $this->area->getAreaHandle());
 
-				$pc->setAdditionalDataAttribute('aID', $this->area->getAreaID());
-			} else {
-				$pc->setError($e);
-			}
+                $pc->setAdditionalDataAttribute('aID', $this->area->getAreaID());
+            } else {
+                $pc->setError($e);
+            }
 
-		}
-		$pc->outputJSON();
-	}
+        }
+        $pc->outputJSON();
+    }
+
+    protected function canAccess()
+    {
+        return $this->areaPermissions->canAddBlock($this->blockType);
+    }
 
 }
 
