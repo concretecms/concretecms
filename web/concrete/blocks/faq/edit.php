6107d3933aff42171ea44d04556cdd521271ea99
Evan Cooper <evan@concretecms.com>
2014-07-11 16:37:14 -0700

    FAQ block adding getSearchableContent and Duplicate functions and removing cruft from edit and add files
    Former-commit-id: 6928f165f02d22c792d50f62b1d4de3bc8f2d75e

--- web/concrete/blocks/faq/edit.php
+++ web/concrete/blocks/faq/edit.php
@@ -1,6 +1,3 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$controllerObj=$controller;
-?>
-
-<?php $this->inc('form_setup_html.php', array('controllerObj' => $controller)); ?> 
+$this->inc('form_setup_html.php');
