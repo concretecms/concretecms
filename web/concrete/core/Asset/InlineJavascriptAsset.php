77874af2fc40ac98c5ed17bb4d49ec449799cc73
Michele Locati <michele@locati.it>
2014-07-11 09:40:52 +0200

    Added inline-javascript asset

---
+++ web/concrete/core/Asset/InlineJavascriptAsset.php
@@ -0,0 +1,33 @@
+<?php
+namespace Concrete\Core\Asset;
+
+class InlineJavascriptAsset extends Asset
+{
+    protected $assetSupportsMinification = false;
+    protected $assetSupportsCombination = false;
+
+    protected $code = '';
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function getAssetType()
+    {
+        return 'inline_javascript';
+    }
+
+    public function combine($assets) { }
+
+    public function minify($assets) { }
+
+    public function __toString()
+    {
+        return "<script type=\"text/javascript\">\n{$this->code}\n</script>";
+    }
+
+}
