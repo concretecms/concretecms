ffe14fc701fbc3058c3b0ddab8aa88acc247c414
Evan Cooper <evan@concretecms.com>
2014-07-14 11:04:57 -0700

    progress on page type edit form dashboard page, questions remain

--- web/concrete/elements/page_types/composer/form/layout_set/control.php
+++ web/concrete/elements/page_types/composer/form/layout_set/control.php
@@ -5,8 +5,8 @@ defined('C5_EXECUTE') or die("Access Denied.");
 <div class="ccm-page-type-composer-item-control-bar">
 	<ul class="ccm-page-type-composer-item-controls">
 		<li><a href="#" data-command="move-set-control" style="cursor: move"><i class="fa fa-arrows"></i></a></li>
-		<li><a data-command="edit-form-set-control" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/page_types/composer/form/edit_control?ptComposerFormLayoutSetControlID=<?=$control->getPageTypeComposerFormLayoutSetControlID()?>" class="dialog-launch" dialog-width="400" dialog-height="200" dialog-modal="true" dialog-title="<?=t('Edit Form Control')?>"><i class="fa fa-pencil"></i></a></li>
-		<li><a href="#" data-delete-set-control="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>"><i class="fa fa-trash"></i></a></li>
+		<li><a data-command="edit-form-set-control" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/page_types/composer/form/edit_control?ptComposerFormLayoutSetControlID=<?=$control->getPageTypeComposerFormLayoutSetControlID()?>" class="dialog-launch" dialog-width="400" dialog-height="auto" dialog-modal="true" dialog-title="<?=t('Edit Form Control')?>"><i class="fa fa-pencil"></i></a></li>
+		<li><a href="#" data-delete-set-control="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>"><i class="fa fa-trash-o"></i></a></li>
 	</ul>
 
 	<div style="display: none">
