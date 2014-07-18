a4a1196da04f1677752dcf5cf72d525d77c97530
Korvin Szanto <korvinszanto@gmail.com>
2014-07-18 16:06:21 -0700

    format survey block view for bootstrap
    Former-commit-id: 7ceba74ab6532876a4e92222b26df4a1a46a645b

--- web/concrete/blocks/survey/view.php
+++ web/concrete/blocks/survey/view.php
@@ -1,105 +1,182 @@
-<?
-	defined('C5_EXECUTE') or die("Access Denied.");
-	// basically a stub that includes some other files
-	$u = new User();
-	$uID=$u->getUserID();
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+// basically a stub that includes some other files
+$u = new User();
+$uID = $u->getUserID();
+$c = Page::getCurrentPage();
+
+//available chart colors are duplicated in content/surveys.php
+$availableChartColors = array(
+    '00CCdd',
+    'cc3333',
+    '330099',
+    'FF6600',
+    '9966FF',
+    'dd7700',
+    '66DD00',
+    '6699FF',
+    'FFFF33',
+    'FFCC33',
+    '00CCdd',
+    'cc3333',
+    '330099',
+    'FF6600',
+    '9966FF',
+    'dd7700',
+    '66DD00',
+    '6699FF',
+    'FFFF33',
+    'FFCC33');
+$options = $controller->getPollOptions();
+$optionNames = array();
+$optionResults = array();
+$graphColors = array();
+$i = 1;
+$totalVotes = 0;
+foreach ($options as $opt) {
+    $optionNamesAbbrev[] = $i;
+    $optionResults[] = $opt->getResults();
+    $i++;
+    $graphColors[] = array_pop($availableChartColors);
+    $totalVotes += intval($opt->getResults());
+}
+foreach ($optionResults as &$value) {
+    if ($totalVotes) {
+        $value = round($value / $totalVotes * 100, 0);
+    }
+}
+$show_graph = (count($optionNamesAbbrev) && !$_GET['dontGraphPoll'] && $totalVotes > 0);
 ?>
 
 <div class="poll">
-<?  
-if ($controller->hasVoted()) { ?>
-		<h3><?=t("You've voted on this survey.")?></h3>
-		<br/>	
-	<div style="">		
-		<? 
-		//available chart colors are duplicated in content/surveys.php
-		$availableChartColors=array('00CCdd','cc3333','330099','FF6600','9966FF','dd7700','66DD00','6699FF','FFFF33','FFCC33','00CCdd','cc3333','330099','FF6600','9966FF','dd7700','66DD00','6699FF','FFFF33','FFCC33');
-		$options = $controller->getPollOptions();
-		$optionNames=array();
-		$optionResults=array();
-		$graphColors=array();
-		$i = 1;
-		$totalVotes=0;
-		foreach($options as $opt) { 
-			$optionNamesAbbrev[] = $i;
-			$optionResults[]=$opt->getResults();
-			$i++;
-			$graphColors[]=array_pop($availableChartColors);
-			$totalVotes+=intval($opt->getResults());
-		}
-		foreach ($optionResults as &$value){
-			if($totalVotes) {
-				$value=round($value/$totalVotes*100,0);
-			}
-		}		
-		?>
+    <?php
+    if ($controller->hasVoted()) {
+        ?>
+        <h3><?= t("You've voted on this survey.") ?></h3>
 
-		<div id="surveyQuestion">
-			<strong><?=t("Question")?>: <?=$controller->getQuestion()?></strong>
-		</div>
+        <div class="row">
+            <div<?= $show_graph ? ' class="col-sm-9"' : '' ?>>
+                <div id="surveyQuestion">
+                    <strong><?= t("Question") ?>:</strong> <span><?= $controller->getQuestion() ?></span>
+                </div>
 
-		<div id="surveyResults" style=" <?=(!$_GET['dontGraphPoll'] && $totalVotes>0)?'float:left; width:45%':''?>">
-			<table style="width:98%">
-			<?	$i = 1; 
-				foreach($options as $opt) { ?>
-					<tr>
-						<td width="55px" class="note" style="white-space:nowrap">
-						<div class="surveySwatch" style="background:#<?=$graphColors[$i-1]?>"></div>&nbsp;<?=($totalVotes>0)?round($opt->getResults() / $totalVotes * 100):0 ?>% 
-						</td>
-						<td>
-						<strong><?=$opt->getOptionName() ?></strong>
-						</td>
-					</tr>
-					<? $i++; ?>
-			<? } ?>
-			</table>
-			<div class="note" style="margin-top:8px"><?=t2('%d Vote', '%d Votes', intval($totalVotes), intval($totalVotes))?></div>
-		</div>
-		<?
-		//&chl= join('|',$optionNamesAbbrev) 
-		if(count($optionNamesAbbrev) && !$_GET['dontGraphPoll'] && $totalVotes>0){ ?>
-		<div >
-		<img border="" src="//chart.apis.google.com/chart?cht=p&chd=t:<?=join(',',$optionResults)?>&chs=180x180&chco=<?=join(',',$graphColors)?>" alt="<?php echo t('survey results');?>" />
-		</div>
-		<? } ?>	
-		<div class="spacer">&nbsp;</div>	
-	</div>
-	
-	<? if($_GET['dontGraphPoll']){ ?>
-		<div class="small right" style="margin-top:8px"><a class="arrow" href="<?=DIR_REL?>/?cID=<?=$b->getBlockCollectionID() ?>"><?=t('View Full Results')?></a></div>
-	<? } ?>
-	
-	<div class="spacer">&nbsp;</div>
+                <div id="surveyResults">
+                    <table class="table">
+                        <?php
+                        $i = 1;
+                        foreach ($options as $opt) {
+                            ?>
+                            <tr>
 
-<? } else { ?>
-	
-	<div id="surveyQuestion">
-		<?=$controller->getQuestion()?><br/>
-	</div>
-	
-	<? if(!$controller->requiresRegistration() || intval($uID) > 0) { ?>
-	<form method="post" action="<?=$view->action('form_save_vote', '#survey-form-'.$controller->bID)?>">
-	<? $c = Page::getCurrentPage(); ?>
-	<input type="hidden" name="rcID" value="<?=$c->getCollectionID()?>" />
-	<? } ?>
-	
-	<?	
-	$options = $controller->getPollOptions();
-	foreach($options as $opt) { ?>
-		<input type="radio" name="optionID" style="vertical-align: middle" value="<?=$opt->getOptionID()?>" />
-		<?=$opt->getOptionName() ?><br/>
-	<? } ?>
-	
-	<? if(!$controller->requiresRegistration() || intval($uID) > 0) { ?>
-	<div class="buttons" style="text-align: left !important"><input type="submit" name="submit" value="<?=t('Vote')?>" /></div>
-	<? }else{ ?>
-		<div class="faint" style="margin-top:8px"><?=t('Please Login to Vote')?></div>
-	<? } ?>
+                                <td class="col-sm-2">
+                                    <div class="surveySwatch" style="background:#<?= $graphColors[$i - 1] ?>"></div>
+                                    &nbsp;<?= ($totalVotes > 0) ? round($opt->getResults() / $totalVotes * 100) : 0 ?>%
+                                </td>
+                                <td>
+                                    <strong>
+                                        <?= $opt->getOptionName() ?>
+                                    </strong>
+                                </td>
+                            </tr>
+                            <?php
+                            $i++;
+                            ?>
+                        <?php
+                        }
+                        ?>
+                    </table>
+                    <div class="help-block">
+                        <?= t2('%d Vote', '%d Votes', intval($totalVotes), intval($totalVotes)) ?>
+                    </div>
+                </div>
+            </div>
+            <?php
+            if ($show_graph) {
+                ?>
+                <div class="col-sm-3">
+                    <img
+                        border=""
+                        src="//chart.apis.google.com/chart?chf=bg,s,FFFFFF00&cht=p&chd=t:<?= join(
+                            ',',
+                            $optionResults) ?>&chs=180x180&chco=<?= join(
+                            ',',
+                            $graphColors) ?>"
+                        alt="<?php echo t('survey results');
+                        ?>"/>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+        <div class="spacer">&nbsp;</div>
 
-	<? if(!$controller->requiresRegistration() || intval($uID) > 0) { ?>
-	</form>
-	<? } ?>
+        <?php
+        if ($_GET['dontGraphPoll']) {
+            ?>
+            <div class="small right" style="margin-top:8px">
+                <a class="arrow" href="<?= DIR_REL ?>/?cID=<?= $b->getBlockCollectionID() ?>">
+                    <?= t('View Full Results') ?>
+                </a>
+            </div>
+        <?php
+        }
+        ?>
 
-<? } ?>
+        <div class="spacer">&nbsp;</div>
+
+    <?php
+    } else {
+        ?>
+
+        <div id="surveyQuestion" class="form-group">
+            <?= $controller->getQuestion() ?>
+        </div>
+
+        <?php
+        if (!$controller->requiresRegistration() || intval($uID) > 0) {
+            ?>
+            <form method="post" action="<?= $view->action('form_save_vote', '#survey-form-' . $controller->bID) ?>">
+                <input type="hidden" name="rcID" value="<?= $c->getCollectionID() ?>"/>
+        <?php
+        }
+        $options = $controller->getPollOptions();
+        foreach ($options as $opt) {
+            ?>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="optionID" value="<?= $opt->getOptionID() ?>"/>
+                    <?= $opt->getOptionName() ?>
+                </label>
+            </div>
+        <?php
+        }
+        if (!$controller->requiresRegistration() || intval($uID) > 0) {
+            ?>
+            <div class="form-group">
+                <button class="btn btn-primary">
+                    <?= t('Vote') ?>
+                </button>
+            </div>
+        <?php
+        } else {
+            ?>
+            <span class="help-block">
+                <?= t('Please Login to Vote') ?>
+            </span>
+        <?php
+        }
+        ?>
+
+        <?php
+        if (!$controller->requiresRegistration() || intval($uID) > 0) {
+            ?>
+            </form>
+        <?php
+        }
+        ?>
+
+    <?php
+    }
+    ?>
 
 </div>
