9bab1ebb0f901d95c2e73bfe65294c095da1187e
Andrew Embler <andrew@concrete5.org>
2014-07-18 10:19:26 -0700

    typo

--- web/concrete/blocks/topic_list/form.php
+++ web/concrete/blocks/topic_list/form.php
@@ -1,6 +1,6 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 <fieldset>
-    <div clxmlass="form-group">
+    <div class="form-group">
         <label class="control-label" for="topicTreeIDSelect"><?=t('Topic Tree')?></label>
         <select class="form-control" name="topicTreeID" id="topicTreeIDSelect">
             <? foreach($trees as $stree) { ?>
