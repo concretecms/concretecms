ea4e15bdf748a0c105e8aaeb325dd4e08284532b
Andrew Embler <andrew@concrete5.org>
2014-07-21 08:33:28 -0700

    Close #276

--- web/concrete/single_pages/dashboard/system/basics/images.php
+++ web/concrete/single_pages/dashboard/system/basics/images.php
@@ -1,27 +1,20 @@
 <? defined('C5_EXECUTE') or die("Access Denied.");?>
-<?=Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Interface Settings'), false, 'span10 offset1', false)?>
-<form method="post" class="form-horizontal" action="<?=$view->action('save_interface_settings')?>" enctype="multipart/form-data" >
-<div class="ccm-pane-body">
+<form method="post" class="form-stacked" action="<?=$view->action('save_interface_settings')?>" enctype="multipart/form-data" >
+
 <?=Loader::helper('validation/token')->output('save_interface_settings')?>
 
 <? if (!defined('WHITE_LABEL_DASHBOARD_BACKGROUND_FEED') && !defined('WHITE_LABEL_DASHBOARD_BACKGROUND_SRC')) { ?>
 
-<fieldset>
-	<legend style="margin-bottom: 0px"><?=t('Dashboard')?></legend>
-	<div class="control-group">
-		<label class="control-label"><?=t('Background Image')?></label>
-		<div class="controls">
-			<label class="radio"><?=$form->radio('DASHBOARD_BACKGROUND_IMAGE', '', $DASHBOARD_BACKGROUND_IMAGE)?> <span><?=t('Pull a picture of the day from concrete5.org (Default)')?></span></label>
-			<label class="radio"><?=$form->radio('DASHBOARD_BACKGROUND_IMAGE', 'none', $DASHBOARD_BACKGROUND_IMAGE)?> <span><?=t('None')?></span></label>
-			<label class="radio"><?=$form->radio('DASHBOARD_BACKGROUND_IMAGE', 'custom', $DASHBOARD_BACKGROUND_IMAGE)?> <span><?=t('Specify Custom Image')?></span>
-					<div id="custom-background-image" <? if ($DASHBOARD_BACKGROUND_IMAGE != 'custom') { ?>style="display: none" <? } ?>>
-						<br/>
-						<?=Loader::helper('concrete/asset_library')->image('DASHBOARD_BACKGROUND_IMAGE_CUSTOM_FILE_ID', DASHBOARD_BACKGROUND_IMAGE_CUSTOM_FILE_ID, t('Choose Image'), $imageObject)?>
-					</div>
-			</label>
-		</div>
-	</div>
-</fieldset>
+<div class="form-group">
+    <label class="control-label"><?=t('Background Image')?></label>
+    <div class="radio"><label><?=$form->radio('DASHBOARD_BACKGROUND_IMAGE', '', $DASHBOARD_BACKGROUND_IMAGE)?> <span><?=t('Pull a picture of the day from concrete5.org (Default)')?></span></label></div>
+    <div class="radio"><label><?=$form->radio('DASHBOARD_BACKGROUND_IMAGE', 'none', $DASHBOARD_BACKGROUND_IMAGE)?> <span><?=t('None')?></span></label></div>
+    <div class="radio"><label><?=$form->radio('DASHBOARD_BACKGROUND_IMAGE', 'custom', $DASHBOARD_BACKGROUND_IMAGE)?> <span><?=t('Specify Custom Image')?></span></div>
+    <div id="custom-background-image" <? if ($DASHBOARD_BACKGROUND_IMAGE != 'custom') { ?>style="display: none" <? } ?>>
+        <br/>
+        <?=Loader::helper('concrete/asset_library')->image('DASHBOARD_BACKGROUND_IMAGE_CUSTOM_FILE_ID', DASHBOARD_BACKGROUND_IMAGE_CUSTOM_FILE_ID, t('Choose Image'), $imageObject)?>
+    </div>
+</div>
 
 
 <script type="text/javascript">
@@ -40,10 +33,12 @@ $(function() {
     <?=t('Options disabled, interface settings are specified in config/site.php.')?>
 <?php } ?>
 
-</div>
-<div class="ccm-pane-footer">
-	<?=Loader::helper('concrete/ui')->submit(t('Save'), 'submit', 'right', 'primary')?>
-</div>
+    <div class="ccm-dashboard-form-actions-wrapper">
+        <div class="ccm-dashboard-form-actions">
+            <button type="submit" class="btn btn-primary pull-right"><?=t('Save')?></button>
+        </div>
+    </div>
+
 </form>
 
 <?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false);?>
