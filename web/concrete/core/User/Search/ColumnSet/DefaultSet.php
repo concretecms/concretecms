3b273f2869e08b21b10104bba53065ef81016f78
Remo Laubacher <remo.laubacher@gmail.com>
2014-07-28 13:26:35 +0200

    use search view - column name fixed
    Former-commit-id: 0d7a460736084d0b3c2fa5888468ec8b42b53f83

--- web/concrete/core/User/Search/ColumnSet/DefaultSet.php
+++ web/concrete/core/User/Search/ColumnSet/DefaultSet.php
@@ -20,7 +20,7 @@ class DefaultSet extends Set {
 	public function __construct() {
 		$this->addColumn(new Column('u.uName', t('Username'), array('Concrete\Core\User\Search\ColumnSet\DefaultSet', 'getUserName')));
 		$this->addColumn(new Column('u.uEmail', t('Email'), array('Concrete\Core\User\Search\ColumnSet\DefaultSet', 'getUserEmail')));
-		$this->addColumn(new Column('u.uDateAdded', t('Last Modified'), array('Concrete\Core\User\Search\ColumnSet\DefaultSet', 'getUserDateAdded')));
+		$this->addColumn(new Column('u.uDateAdded', t('Signup Date'), array('Concrete\Core\User\Search\ColumnSet\DefaultSet', 'getUserDateAdded')));
 		$this->addColumn(new Column('u.uNumLogins', t('# Logins'), 'getNumLogins'));
 		$date = $this->getColumnByKey('u.uDateAdded');
 		$this->setDefaultSortColumn($date, 'desc');
