ceb9863c8cbb5dbf4e496aa0fbd78cf503b1a86f
tylerryan <ryan@concrete5.org>
2014-07-31 13:53:06 -0700

    closes #228
    Former-commit-id: 675ec1469582ef7ff2925c85e1a10fedb92cc574

--- web/concrete/controllers/single_page/dashboard/system/basics/icons.php
+++ web/concrete/controllers/single_page/dashboard/system/basics/icons.php
@@ -1,75 +1,66 @@
 <?
 
 namespace Concrete\Controller\SinglePage\Dashboard\System\Basics;
+use Concrete\Core\Mail\Service;
 use \Concrete\Core\Page\Controller\DashboardPageController;
 use Config;
 use Loader;
 use FileImporter;
 use FileVersion;
+use View;
+use Core;
+
 
 class Icons extends DashboardPageController {
 
-    public $helpers = array('form', 'concrete/asset_library');
+    public $helpers = array('form', 'concrete/asset_library', 'json');
 
-	public function view() {
-		$this->set('site', SITE);
-	}
+    public function on_start() {
+        parent::on_start();
+        $view = View::getInstance();
+        $view->requireAsset('core/colorpicker');
+    }
 
 	public function favicon_saved() {
-		$this->set('message', t("Icons updated successfully."));	
-		$this->view();
+		$this->set('message', t("Icons updated successfully."));
 	}
 
 	public function favicon_removed() {
 		$this->set('message', t("Icon removed successfully."));	
-		$this->view();
 	}
 
 	public function iphone_icon_saved() {
 		$this->set('message', t("iPhone icon updated successfully."));	
-		$this->view();
 	}
 
 	public function iphone_icon_removed() {
 		$this->set('message', t("iPhone icon removed successfully."));	
-		$this->view();
 	}
 
 	public function modern_icon_saved() {
 		$this->set('message', t('Windows 8 icon updated successfully.'));
-		$this->view();
 	}
 
 	public function modern_icon_removed() {
 		$this->set('message', t('Windows 8 icon removed successfully.'));
-		$this->view();
 	}
 
 	function update_modern_thumbnail() {
 		if($this->token->validate('update_modern_thumbnail')) {
-			if(intval($this->post('remove_icon')) == 1) {
-				Config::save('MODERN_TILE_THUMBNAIL_FID', 0);
-				$this->redirect('/dashboard/system/basics/icons/', 'modern_icon_removed');
+            $S = Core::make('helper/security');
+            $modernThumbFID = $S->sanitizeInt($this->post('modernThumbFID'));
+            $modernThumbBG = $S->sanitizeString($this->post('modernThumbBG'));
+            $result = "modern_icon_saved";
+            if($modernThumbFID) {
+                Config::save('MODERN_TILE_THUMBNAIL_FID', $modernThumbFID);
+			} else {
+                Config::save('MODERN_TILE_THUMBNAIL_FID', 0);
+                $result = 'modern_icon_removed';
 			}
-			else {
-				$fi = new FileImporter();
-				$resp = $fi->import($_FILES['favicon_file']['tmp_name'], $_FILES['favicon_file']['name'], $fr);
-				if(!($resp instanceof FileVersion)) {
-					switch($resp) {
-						case FileImporter::E_FILE_INVALID_EXTENSION:
-							$this->error->add(t('Invalid file extension.'));
-							break;
-						case FileImporter::E_FILE_INVALID:
-							$this->error->add(t('Invalid file.'));
-							break;
-					}
-				}
-				else {
-					Config::save('MODERN_TILE_THUMBNAIL_FID', $resp->getFileID());
-					Config::save('MODERN_TILE_THUMBNAIL_BGCOLOR', Loader::helper('security')->sanitizeString($this->post('favicon_bgcolor')));
-					$this->redirect('/dashboard/system/basics/icons/', 'modern_icon_saved');
-				}
-			}
+
+            Config::save('MODERN_TILE_THUMBNAIL_BGCOLOR', $modernThumbBG);
+
+            $this->redirect('/dashboard/system/basics/icons/', $result);
 		}
 		else {
 			$this->set('error', array($this->token->getErrorMessage()));
@@ -77,65 +68,37 @@ class Icons extends DashboardPageController {
 	}
 
 	function update_iphone_thumbnail(){
-		if ($this->token->validate("update_iphone_thumbnail")) { 
-		
-			if(intval($this->post('remove_icon'))==1){
-				Config::save('IPHONE_HOME_SCREEN_THUMBNAIL_FID',0);
-					$this->redirect('/dashboard/system/basics/icons/', 'iphone_icon_removed');
-			} else {
-				$fi = new FileImporter();
-				$resp = $fi->import($_FILES['favicon_file']['tmp_name'], $_FILES['favicon_file']['name'], $fr);
-				if (!($resp instanceof FileVersion)) {
-					switch($resp) {
-						case FileImporter::E_FILE_INVALID_EXTENSION:
-							$this->error->add(t('Invalid file extension.'));
-							break;
-						case FileImporter::E_FILE_INVALID:
-							$this->error->add(t('Invalid file.'));
-							break;
-						
-					}
-				} else {
-				
-					Config::save('IPHONE_HOME_SCREEN_THUMBNAIL_FID', $resp->getFileID());
-					$this->redirect('/dashboard/system/basics/icons/', 'iphone_icon_saved');
+		if ($this->token->validate("update_iphone_thumbnail")) {
 
-				}
-			}		
-			
-		}else{
+            $S = Core::make('helper/security');
+            $faviconFID = $S->sanitizeInt($this->post('iosHomeFID'));
+
+			if($faviconFID){
+				Config::save('IPHONE_HOME_SCREEN_THUMBNAIL_FID', $faviconFID);
+                $this->redirect('/dashboard/system/basics/icons/', 'iphone_icon_saved');
+            } else {
+                Config::save('IPHONE_HOME_SCREEN_THUMBNAIL_FID', 0);
+                $this->redirect('/dashboard/system/basics/icons/', 'iphone_icon_removed');
+            }
+
+		} else {
 			$this->set('error', array($this->token->getErrorMessage()));
 		}
 	}
 	
 	function update_favicon(){
-		if ($this->token->validate("update_favicon")) { 
-		
-			if(intval($this->post('remove_favicon'))==1){
-				Config::save('FAVICON_FID',0);
-					$this->redirect('/dashboard/system/basics/icons/', 'favicon_removed');
-			} else {
-				$fi = new FileImporter();
-				$resp = $fi->import($_FILES['favicon_file']['tmp_name'], $_FILES['favicon_file']['name'], $fr);
-				if (!($resp instanceof FileVersion)) {
-					switch($resp) {
-						case FileImporter::E_FILE_INVALID_EXTENSION:
-							$this->error->add(t('Invalid file extension.'));
-							break;
-						case FileImporter::E_FILE_INVALID:
-							$this->error->add(t('Invalid file.'));
-							break;
-						
-					}
-				} else {
-				
-					Config::save('FAVICON_FID', $resp->getFileID());
-					$this->redirect('/dashboard/system/basics/icons/', 'favicon_saved');
+		if ($this->token->validate("update_favicon")) {
+            $S = Core::make('helper/security');
+            $faviconFID = $S->sanitizeInt($this->post('faviconFID'));
 
-				}
-			}		
-			
-		}else{
+            if($faviconFID) {
+                Config::save('FAVICON_FID', $faviconFID);
+                $this->redirect('/dashboard/system/basics/icons/', 'favicon_saved');
+            } else {
+                Config::save('FAVICON_FID',0);
+                $this->redirect('/dashboard/system/basics/icons/', 'favicon_removed');
+            }
+		} else {
 			$this->set('error', array($this->token->getErrorMessage()));
 		}
 	}
