57e85f6393824ff5c2c5f894ac89b7250ce0aab7
Korvin Szanto <korvinszanto@gmail.com>
2014-08-04 11:40:47 -0700

    Reformat page user_interface

--- web/concrete/controllers/backend/user_interface/page.php
+++ web/concrete/controllers/backend/user_interface/page.php
@@ -1,42 +1,49 @@
 <?php
 namespace Concrete\Controller\Backend\UserInterface;
+
+use Exception;
 use Page as ConcretePage;
 use Permissions;
-use Exception;
-abstract class Page extends \Concrete\Controller\Backend\UserInterface {
 
-	protected $page;
+abstract class Page extends \Concrete\Controller\Backend\UserInterface
+{
 
-	public function __construct() {
-		parent::__construct();
-		$request = $this->request;
-		$cID = $request->query->get('cID');
-		if ($cID) {
-			$page = ConcretePage::getByID($cID);
-			$this->setPageObject($page);
-			$request->setCurrentPage($this->page);
-		}
-	}
+    /** @var ConcretePage A page object */
+    protected $page;
 
-	public function setPageObject(ConcretePage $c) {
-		$this->page = $c;
-		$this->permissions = new Permissions($this->page);		
-		$this->set('c', $this->page);
-		$this->set('cp', $this->permissions);
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $request = $this->request;
+        $cID = $request->query->get('cID');
+        if ($cID) {
+            $page = ConcretePage::getByID($cID);
+            $this->setPageObject($page);
+            $request->setCurrentPage($this->page);
+        }
+    }
 
-	public function getViewObject() {
-		if ($this->permissions->canViewPage()) {
-			return parent::getViewObject();
-		}
-		throw new Exception(t('Access Denied'));
-	}
+    public function setPageObject(ConcretePage $c)
+    {
+        $this->page = $c;
+        $this->permissions = new Permissions($this->page);
+        $this->set('c', $this->page);
+        $this->set('cp', $this->permissions);
+    }
 
-	public function action() {
-		$url = call_user_func_array('parent::action', func_get_args());
-		$url .= '&cID=' . $this->page->getCollectionID();
-		return $url;
-	}
+    public function getViewObject()
+    {
+        if ($this->permissions->canViewPage()) {
+            return parent::getViewObject();
+        }
+        throw new Exception(t('Access Denied'));
+    }
+
+    public function action()
+    {
+        $url = call_user_func_array('parent::action', func_get_args());
+        $url .= '&cID=' . $this->page->getCollectionID();
+        return $url;
+    }
 
 }
-	
