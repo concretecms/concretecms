a39c814e75c29521c768b19a477764d18d6ed5dc
Michele Locati <michele@locati.it>
2014-08-04 17:58:10 +0200

    PRS-2 coding style

--- web/concrete/core/Tree/Tree.php
+++ web/concrete/core/Tree/Tree.php
@@ -1,40 +1,49 @@
-<?
+<?php
 namespace Concrete\Core\Tree;
 use \Concrete\Core\Foundation\Object;
 use Loader;
 use Core;
-abstract class Tree extends Object {
 
-	protected $treeNodeSelectedIDs = array();
+abstract class Tree extends Object
+{
+    protected $treeNodeSelectedIDs = array();
 
-	abstract protected function loadDetails();
-	abstract protected function deleteDetails();
-	abstract public function getTreeDisplayName();
+    abstract protected function loadDetails();
+
+    abstract protected function deleteDetails();
+
+    abstract public function getTreeDisplayName();
+
     abstract public function exportDetails(\SimpleXMLElement $sx);
+
     abstract public static function importDetails(\SimpleXMLElement $sx);
 
+    public function setSelectedTreeNodeIDs($nodeIDs)
+    {
+        $this->treeNodeSelectedIDs = $nodeIDs;
+    }
 
-	public function setSelectedTreeNodeIDs($nodeIDs) {
-		$this->treeNodeSelectedIDs = $nodeIDs;
-	}
+    public function getSelectedTreeNodeIDs()
+    {
+        return $this->treeNodeSelectedIDs;
+    }
 
-	public function getSelectedTreeNodeIDs() {
-		return $this->treeNodeSelectedIDs;
-	}
+    public function getTreeTypeID()
+    {
+        return $this->treeTypeID;
+    }
+    public function getTreeTypeObject()
+    {
+        return TreeType::getByID($this->treeTypeID);
+    }
 
-	public function getTreeTypeID() {
-		return $this->treeTypeID;
-	}
-	public function getTreeTypeObject() {
-		return TreeType::getByID($this->treeTypeID);
-	}
-
-	public function getTreeTypeHandle() {
-		$type = $this->getTreeTypeObject();
-		if (is_object($type)){
-			return $type->getTreeTypeHandle();
-		}
-	}
+    public function getTreeTypeHandle()
+    {
+        $type = $this->getTreeTypeObject();
+        if (is_object($type)) {
+            return $type->getTreeTypeHandle();
+        }
+    }
 
     public function export(\SimpleXMLElement $sx)
     {
@@ -66,8 +75,8 @@ abstract class Tree extends Object {
         $parent->importChildren($sx);
     }
 
-	public function getTreeID() {return $this->treeID;}
-	public function getRootTreeNodeObject() {return \Concrete\Core\Tree\Node\Node::getByID($this->rootTreeNodeID);}
+    public function getTreeID() {return $this->treeID;}
+    public function getRootTreeNodeObject() {return \Concrete\Core\Tree\Node\Node::getByID($this->rootTreeNodeID);}
     public function getRootTreeNodeID() {return $this->rootTreeNodeID;}
 
     /**
@@ -85,7 +94,7 @@ abstract class Tree extends Object {
 
         $computedPath = '';
         $tree = $this;
-        $walk = function($node, $computedPath) use (&$walk, &$tree, &$path) {
+        $walk = function ($node, $computedPath) use (&$walk, &$tree, &$path) {
             $node->populateDirectChildrenOnly();
 
             if ($node->getTreeNodeID() != $tree->getRootTreeNodeID()) {
@@ -97,84 +106,95 @@ abstract class Tree extends Object {
                 return $node;
             } else {
                 $children = $node->getChildNodes();
-                foreach($children as $child) {
+                foreach ($children as $child) {
                     $node = $walk($child, $computedPath);
                     if ($node !== null) {
                         return $node;
                     }
                 }
             }
+
             return null;
         };
         $node = $walk($root, $computedPath);
+
         return $node;
     }
 
-	public function setRequest($data) {
-		$this->requestData = $data;
-	}
+    public function setRequest($data)
+    {
+        $this->requestData = $data;
+    }
 
-	public function delete() {
-		$db = Loader::db();
-		// delete top level node
-		$node = $this->getRootTreeNodeObject();
-		$node->delete();
-		$this->deleteDetails();
-		$db->Execute('delete from Trees where treeID = ?', array($this->treeID));
-	}
+    public function delete()
+    {
+        $db = Loader::db();
+        // delete top level node
+        $node = $this->getRootTreeNodeObject();
+        $node->delete();
+        $this->deleteDetails();
+        $db->Execute('delete from Trees where treeID = ?', array($this->treeID));
+    }
 
-	public function duplicate() {
-		$root = $this->getRootTreeNodeObject();
-		$newRoot = $root->duplicate();
-		$type = $this->getTreeTypeObject();
-		$tree = $type->addTree($newRoot);
-		$db = Loader::db();
-		$nodes = $newRoot->getAllChildNodeIDs();
-		foreach($nodes as $nodeID) {
-			$db->Execute('update TreeNodes set treeID = ? where treeNodeID = ?', array($tree->getTreeID(), $nodeID));
-		}
-		return $tree;
-	}
+    public function duplicate()
+    {
+        $root = $this->getRootTreeNodeObject();
+        $newRoot = $root->duplicate();
+        $type = $this->getTreeTypeObject();
+        $tree = $type->addTree($newRoot);
+        $db = Loader::db();
+        $nodes = $newRoot->getAllChildNodeIDs();
+        foreach ($nodes as $nodeID) {
+            $db->Execute('update TreeNodes set treeID = ? where treeNodeID = ?', array($tree->getTreeID(), $nodeID));
+        }
 
-	public function getJSON() {
-		$root = $this->getRootTreeNodeObject();
-		$root->populateDirectChildrenOnly();
-		if(is_array($this->getSelectedTreeNodeIDs())) {
-			foreach($this->getSelectedTreeNodeIDs() as $magicNodes) {
-				$root->selectChildrenNodesByID($magicNodes);
-			}
-		}
-		if ($this->getSelectedTreeNodeIDs() > 0) {
-			$root->selectChildrenNodesByID($this->getSelectedTreeNodeIDs());
-		}
-		return $root->getTreeNodeJSON();
-	}
+        return $tree;
+    }
 
-	protected static function add(\Concrete\Core\Tree\Node\Node $rootNode) {
-		$db = Loader::db();
-		$date = Loader::helper('date')->getSystemDateTime();
-		$treeTypeHandle = Loader::helper('text')->uncamelcase(strrchr(get_called_class(), '\\'));
-		$type = TreeType::getByHandle($treeTypeHandle);
-		$db->Execute('insert into Trees (treeDateAdded, rootTreeNodeID, treeTypeID) values (?, ?, ?)', array(
-			$date, $rootNode->getTreeNodeID(), $type->getTreeTypeID()
-		));
-		$treeID = $db->Insert_ID();
-		$rootNode->setTreeNodeTreeID($treeID);
-		return $treeID;
-	}
+    public function getJSON()
+    {
+        $root = $this->getRootTreeNodeObject();
+        $root->populateDirectChildrenOnly();
+        if (is_array($this->getSelectedTreeNodeIDs())) {
+            foreach ($this->getSelectedTreeNodeIDs() as $magicNodes) {
+                $root->selectChildrenNodesByID($magicNodes);
+            }
+        }
+        if ($this->getSelectedTreeNodeIDs() > 0) {
+            $root->selectChildrenNodesByID($this->getSelectedTreeNodeIDs());
+        }
 
+        return $root->getTreeNodeJSON();
+    }
 
-	final public static function getByID($treeID) {
-		$db = Loader::db();
-		$row = $db->GetRow('select * from Trees where treeID = ?', array($treeID));
-		if (is_array($row) && $row['treeID']) {
-			$tt = TreeType::getByID($row['treeTypeID']);
-			$class = $tt->getTreeTypeClass();
-			$tree = Core::make($class);
-			$tree->setPropertiesFromArray($row);
-			$tree->loadDetails();
-			return $tree;
-		}
-	}
-	
+    protected static function add(\Concrete\Core\Tree\Node\Node $rootNode)
+    {
+        $db = Loader::db();
+        $date = Loader::helper('date')->getSystemDateTime();
+        $treeTypeHandle = Loader::helper('text')->uncamelcase(strrchr(get_called_class(), '\\'));
+        $type = TreeType::getByHandle($treeTypeHandle);
+        $db->Execute('insert into Trees (treeDateAdded, rootTreeNodeID, treeTypeID) values (?, ?, ?)', array(
+            $date, $rootNode->getTreeNodeID(), $type->getTreeTypeID()
+        ));
+        $treeID = $db->Insert_ID();
+        $rootNode->setTreeNodeTreeID($treeID);
+
+        return $treeID;
+    }
+
+    final public static function getByID($treeID)
+    {
+        $db = Loader::db();
+        $row = $db->GetRow('select * from Trees where treeID = ?', array($treeID));
+        if (is_array($row) && $row['treeID']) {
+            $tt = TreeType::getByID($row['treeTypeID']);
+            $class = $tt->getTreeTypeClass();
+            $tree = Core::make($class);
+            $tree->setPropertiesFromArray($row);
+            $tree->loadDetails();
+
+            return $tree;
+        }
+    }
+
 }
