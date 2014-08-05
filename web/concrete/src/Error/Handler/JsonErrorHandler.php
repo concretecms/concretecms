277b8e5a012652ba9f94f92c38dbd0755b0b87ba
Korvin Szanto <korvinszanto@gmail.com>
2014-08-05 11:21:21 -0700

    Fix the comments in Token class and update json response to be compatible with current error responses.
    Former-commit-id: 5143b3507f56a00c10519ad2054ae289f33b42cd

--- web/concrete/src/Error/Handler/JsonErrorHandler.php
+++ web/concrete/src/Error/Handler/JsonErrorHandler.php
@@ -1,13 +1,52 @@
 <?php
 namespace Concrete\Core\Error\Handler;
 
-use Whoops\Handler\JsonResponseHandler;
+use Config;
+use Whoops\Exception\Formatter;
+use Whoops\Handler\Handler;
 
-class JsonErrorHandler extends JsonResponseHandler {
+class JsonErrorHandler extends Handler
+{
 
-    public function onlyForAjaxRequests($onlyForAjaxRequests = null)
+    public function handle()
     {
-        return true;
+        $debug = intval(defined('SITE_DEBUG_LEVEL') ? SITE_DEBUG_LEVEL : Config::get('SITE_DEBUG_LEVEL'), 10);
+        if ($debug !== DEBUG_DISPLAY_ERRORS) {
+            return Handler::DONE;
+        }
+
+        if (!$this->isAjaxRequest()) {
+            return Handler::DONE;
+        }
+
+        $error = Formatter::formatExceptionAsDataArray(
+            $this->getInspector(),
+            true
+        );
+
+        $response = array(
+            'error'  => $error,
+            'errors' => array($error['message'])
+        );
+
+        if (\Whoops\Util\Misc::canSendHeaders()) {
+            header('Content-Type: application/json');
+        }
+
+        echo json_encode($response);
+        return Handler::QUIT;
+    }
+
+    /**
+     * Check, if possible, that this execution was triggered by an AJAX request.
+     *
+     * @return bool
+     */
+    private function isAjaxRequest()
+    {
+        return (
+            !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
     }
 
 }
