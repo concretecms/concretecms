8b0a6a0e8d51bfb512c0989c17f93d4dee61bdd0
Michele Locati <michele@locati.it>
2014-08-11 11:32:19 +0200

    PRS-2 coding style

--- web/concrete/controllers/single_page/account/messages/inbox.php
+++ web/concrete/controllers/single_page/account/messages/inbox.php
@@ -1,5 +1,6 @@
-<?
+<?php
 namespace Concrete\Controller\SinglePage\Account\Messages;
+
 use \Concrete\Core\Page\Controller\AccountPageController;
 use \Concrete\Core\User\PrivateMessage\Mailbox as UserPrivateMessageMailbox;
 use \Concrete\Core\User\PrivateMessage\PrivateMessage as UserPrivateMessage;
@@ -7,174 +8,188 @@ use UserInfo;
 use Loader;
 use User;
 
-class Inbox extends AccountPageController {
+class Inbox extends AccountPageController
+{
+    public function view()
+    {
+        $u = new User();
+        $ui = UserInfo::getByID($u->getUserID());
 
-	public function view() {
-		$u = new User();
-		$ui = UserInfo::getByID($u->getUserID());
+        $inbox = UserPrivateMessageMailbox::get($ui, $this->getMessageMailboxID('inbox'));
+        $sent = UserPrivateMessageMailbox::get($ui, $this->getMessageMailboxID('sent'));
 
-		$inbox = UserPrivateMessageMailbox::get($ui, $this->getMessageMailboxID('inbox'));
-		$sent = UserPrivateMessageMailbox::get($ui, $this->getMessageMailboxID('sent'));
+        $this->set('inbox', $inbox);
+        $this->set('sent', $sent);
+    }
 
-		$this->set('inbox', $inbox);
-		$this->set('sent', $sent);
-	}
+    protected function validateUser($uID)
+    {
+        if ($uID > 0) {
+            $ui = UserInfo::getByID($uID);
+            if ((is_object($ui)) && ($ui->getAttribute('profile_private_messages_enabled') == 1)) {
+                $this->set('recipient', $ui);
 
-	protected function validateUser($uID) {
-		if ($uID > 0) {
-			$ui = UserInfo::getByID($uID);
-			if ((is_object($ui)) && ($ui->getAttribute('profile_private_messages_enabled') == 1)) {
-				$this->set('recipient', $ui);
-				return true;
-			}
-		}
+                return true;
+            }
+        }
 
-		$this->redirect('/profile');
-	}
+        $this->redirect('/profile');
+    }
 
-	protected function getMessageMailboxID($box) {
-		$msgMailboxID = 0;
-		switch($box) {
-			case 'inbox':
-				$msgMailboxID = UserPrivateMessageMailbox::MBTYPE_INBOX;
-				break;
-			case 'sent':
-				$msgMailboxID = UserPrivateMessageMailbox::MBTYPE_SENT;
-				break;
-			default:
-				$msgMailboxID = $box;
-				break;
-		}
-		return $msgMailboxID;
-	}
+    protected function getMessageMailboxID($box)
+    {
+        $msgMailboxID = 0;
+        switch ($box) {
+            case 'inbox':
+                $msgMailboxID = UserPrivateMessageMailbox::MBTYPE_INBOX;
+                break;
+            case 'sent':
+                $msgMailboxID = UserPrivateMessageMailbox::MBTYPE_SENT;
+                break;
+            default:
+                $msgMailboxID = $box;
+                break;
+        }
 
-	public function view_mailbox($box) {
-		$msgMailboxID = $this->getMessageMailboxID($box);
+        return $msgMailboxID;
+    }
 
-		$u = new User();
-		$ui = UserInfo::getByID($u->getUserID());
+    public function view_mailbox($box)
+    {
+        $msgMailboxID = $this->getMessageMailboxID($box);
 
-		$mailbox = UserPrivateMessageMailbox::get($ui, $msgMailboxID);
-		if (is_object($mailbox)) {
-			$messageList = $mailbox->getMessageList();
-			$messages = $messageList->getPage();
-			$this->set('messages', $messages);
-			$this->set('messageList', $messageList);
-		}
+        $u = new User();
+        $ui = UserInfo::getByID($u->getUserID());
 
-		// also, we have to mark all messages in this mailbox as no longer "new"
+        $mailbox = UserPrivateMessageMailbox::get($ui, $msgMailboxID);
+        if (is_object($mailbox)) {
+            $messageList = $mailbox->getMessageList();
+            $messages = $messageList->getPage();
+            $this->set('messages', $messages);
+            $this->set('messageList', $messageList);
+        }
 
-		$mailbox->removeNewStatus();
-		$this->set('mailbox', $box);
-	}
+        // also, we have to mark all messages in this mailbox as no longer "new"
 
-	public function view_message($box, $msgID) {
-		$msgMailboxID = $this->getMessageMailboxID($box);
-		$u = new User();
-		$ui = UserInfo::getByID($u->getUserID());
-		$mailbox = UserPrivateMessageMailbox::get($ui, $msgMailboxID);
-		$msg = UserPrivateMessage::getByID($msgID, $mailbox);
-		if ($ui->canReadPrivateMessage($msg)) {
-			$msg->markAsRead();
-			$this->set('subject', $msg->getFormattedMessageSubject());
-			$this->set('msgContent', $msg->getMessageBody());
-			$this->set('dateAdded', $msg->getMessageDateAdded('user', DATE_APP_GENERIC_MDYT_FULL));
-			$this->set('author', $msg->getMessageAuthorObject());
-			$this->set('msg', $msg);
-			$this->set('box', $box);
-			$this->set('backURL', View::url('/account/messages/inbox', 'view_mailbox', $box));
-			$valt = Loader::helper('validation/token');
-			$token = $valt->generate('delete_message_' . $msgID);
-			$this->set('deleteURL', View::url('/account/messages/inbox', 'delete_message', $box, $msgID, $token));
-		} else {
-			$this->redirect('/account/messages/inbox');
-		}
-	}
+        $mailbox->removeNewStatus();
+        $this->set('mailbox', $box);
+    }
 
-	public function delete_message($box, $msgID, $token) {
-		$valt = Loader::helper('validation/token');
-		if (!$valt->validate('delete_message_' . $msgID, $token)) {
-			$this->error->add($valt->getErrorMessage());
-		}
+    public function view_message($box, $msgID)
+    {
+        $msgMailboxID = $this->getMessageMailboxID($box);
+        $u = new User();
+        $ui = UserInfo::getByID($u->getUserID());
+        $mailbox = UserPrivateMessageMailbox::get($ui, $msgMailboxID);
+        $msg = UserPrivateMessage::getByID($msgID, $mailbox);
+        if ($ui->canReadPrivateMessage($msg)) {
+            $msg->markAsRead();
+            $this->set('subject', $msg->getFormattedMessageSubject());
+            $this->set('msgContent', $msg->getMessageBody());
+            $this->set('dateAdded', $msg->getMessageDateAdded('user', DATE_APP_GENERIC_MDYT_FULL));
+            $this->set('author', $msg->getMessageAuthorObject());
+            $this->set('msg', $msg);
+            $this->set('box', $box);
+            $this->set('backURL', View::url('/account/messages/inbox', 'view_mailbox', $box));
+            $valt = Loader::helper('validation/token');
+            $token = $valt->generate('delete_message_' . $msgID);
+            $this->set('deleteURL', View::url('/account/messages/inbox', 'delete_message', $box, $msgID, $token));
+        } else {
+            $this->redirect('/account/messages/inbox');
+        }
+    }
 
-		$msgMailboxID = $this->getMessageMailboxID($box);
-		$u = new User();
-		$ui = UserInfo::getByID($u->getUserID());
-		$mailbox = UserPrivateMessageMailbox::get($ui, $msgMailboxID);
+    public function delete_message($box, $msgID, $token)
+    {
+        $valt = Loader::helper('validation/token');
+        if (!$valt->validate('delete_message_' . $msgID, $token)) {
+            $this->error->add($valt->getErrorMessage());
+        }
 
-		$msg = UserPrivateMessage::getByID($msgID, $mailbox);
-		if ($ui->canReadPrivateMessage($msg) && (!$this->error->has())) {
-			$msg->delete();
-			$this->redirect('/account/messages/inbox', 'view_mailbox', $box);
-		}
-		print $this->view();
-	}
+        $msgMailboxID = $this->getMessageMailboxID($box);
+        $u = new User();
+        $ui = UserInfo::getByID($u->getUserID());
+        $mailbox = UserPrivateMessageMailbox::get($ui, $msgMailboxID);
 
-	public function write($uID) {
-		$this->validateUser($uID);
-		$this->set('backURL', View::url('/account/profile/public_profile', 'view', $uID));
-	}
+        $msg = UserPrivateMessage::getByID($msgID, $mailbox);
+        if ($ui->canReadPrivateMessage($msg) && (!$this->error->has())) {
+            $msg->delete();
+            $this->redirect('/account/messages/inbox', 'view_mailbox', $box);
+        }
+        print $this->view();
+    }
 
-	public function reply($boxID, $msgID) {
-		$msg = UserPrivateMessage::getByID($msgID);
-		$uID = $msg->getMessageRelevantUserID();
-		$this->validateUser($uID);
-		$this->set('backURL', View::url('/account/messages/inbox', 'view_message', $boxID, $msgID));
-		$this->set('msgID', $msgID);
-		$this->set('box', $boxID);
-		$this->set('msg', $msg);
+    public function write($uID)
+    {
+        $this->validateUser($uID);
+        $this->set('backURL', View::url('/account/profile/public_profile', 'view', $uID));
+    }
 
-		$this->set('msgSubject', $msg->getFormattedMessageSubject());
+    public function reply($boxID, $msgID)
+    {
+        $msg = UserPrivateMessage::getByID($msgID);
+        $uID = $msg->getMessageRelevantUserID();
+        $this->validateUser($uID);
+        $this->set('backURL', View::url('/account/messages/inbox', 'view_message', $boxID, $msgID));
+        $this->set('msgID', $msgID);
+        $this->set('box', $boxID);
+        $this->set('msg', $msg);
 
-		$body = "\n\n\n" . $msg->getMessageDelimiter() . "\n";
-		$body .= t("From: %s\nDate Sent: %s\nSubject: %s", $msg->getMessageAuthorName(), $msg->getMessageDateAdded('user', DATE_APP_GENERIC_MDYT_FULL), $msg->getFormattedMessageSubject());
-		$body .= "\n\n" . $msg->getMessageBody();
-		$this->set('msgBody', $body);
-	}
+        $this->set('msgSubject', $msg->getFormattedMessageSubject());
 
-	public function send() {
-		$uID = $this->post('uID');
+        $body = "\n\n\n" . $msg->getMessageDelimiter() . "\n";
+        $body .= t("From: %s\nDate Sent: %s\nSubject: %s", $msg->getMessageAuthorName(), $msg->getMessageDateAdded('user', DATE_APP_GENERIC_MDYT_FULL), $msg->getFormattedMessageSubject());
+        $body .= "\n\n" . $msg->getMessageBody();
+        $this->set('msgBody', $body);
+    }
 
-		if ($this->post('msgID') > 0) {
-			$msgID = $this->post('msgID');
-			$box = $this->post('box');
-			$this->reply($box, $msgID);
-		} else {
-			$this->write($uID);
-		}
+    public function send()
+    {
+        $uID = $this->post('uID');
 
-		$vf = Loader::helper('validation/form');
-		$vf->setData($this->post());
-		$vf->addRequired('msgBody', t("You haven't written a message!"));
-		$vf->addRequiredToken("validate_send_message");
-		if ($vf->test()) {
-			$u = new User();
-			$sender = UserInfo::getByID($u->getUserID());
-			$r = $sender->sendPrivateMessage($this->get('recipient'), $this->post('msgSubject'), $this->post('msgBody'), $this->get('msg'));
-			if ($r instanceof \Concrete\Core\Helper\Validation\Error) {
-				$this->error = $r;
-			} else {
-				if ($this->post('msgID') > 0) {
-					$this->redirect('/account/messages/inbox', 'reply_complete', $box, $msgID);
-				} else {
-					$this->redirect('/account/messages/inbox', 'send_complete', $uID);
-				}
-			}
-		} else {
-			$this->error = $vf->getError();
-		}
-	}
+        if ($this->post('msgID') > 0) {
+            $msgID = $this->post('msgID');
+            $box = $this->post('box');
+            $this->reply($box, $msgID);
+        } else {
+            $this->write($uID);
+        }
 
-	public function send_complete($uID) {
-		$this->validateUser($uID);
-	}
+        $vf = Loader::helper('validation/form');
+        $vf->setData($this->post());
+        $vf->addRequired('msgBody', t("You haven't written a message!"));
+        $vf->addRequiredToken("validate_send_message");
+        if ($vf->test()) {
+            $u = new User();
+            $sender = UserInfo::getByID($u->getUserID());
+            $r = $sender->sendPrivateMessage($this->get('recipient'), $this->post('msgSubject'), $this->post('msgBody'), $this->get('msg'));
+            if ($r instanceof \Concrete\Core\Helper\Validation\Error) {
+                $this->error = $r;
+            } else {
+                if ($this->post('msgID') > 0) {
+                    $this->redirect('/account/messages/inbox', 'reply_complete', $box, $msgID);
+                } else {
+                    $this->redirect('/account/messages/inbox', 'send_complete', $uID);
+                }
+            }
+        } else {
+            $this->error = $vf->getError();
+        }
+    }
 
-	public function reply_complete($box, $msgID) {
-		$this->reply($box, $msgID);
-	}
+    public function send_complete($uID)
+    {
+        $this->validateUser($uID);
+    }
 
-	public function on_before_render() {
-		$this->set('error', $this->error);
-	}
+    public function reply_complete($box, $msgID)
+    {
+        $this->reply($box, $msgID);
+    }
+
+    public function on_before_render()
+    {
+        $this->set('error', $this->error);
+    }
 
 }
--- web/concrete/tools/files/importers/remote.php
+++ web/concrete/tools/files/importers/remote.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 defined('C5_EXECUTE') or die("Access Denied.");
 $u = new User();
@@ -7,14 +7,14 @@ use \Concrete\Core\File\EditResponse as FileEditResponse;
 $cf = Loader::helper('file');
 $fp = FilePermissions::getGlobal();
 if (!$fp->canAddFiles()) {
-	die(t("Unable to add files."));
+    die(t("Unable to add files."));
 }
 
 if (isset($_REQUEST['fID'])) {
-	// we are replacing a file
-	$fr = File::getByID($_REQUEST['fID']);
+    // we are replacing a file
+    $fr = File::getByID($_REQUEST['fID']);
 } else {
-	$fr = false;
+    $fr = false;
 }
 
 $r = new FileEditResponse();
@@ -29,121 +29,119 @@ $error = Loader::helper('validation/error');
 $incoming_urls = array();
 
 if (!function_exists('iconv_get_encoding')) {
-	$error->add(t('Remote URL import requires the iconv extension enabled on your server.'));
+    $error->add(t('Remote URL import requires the iconv extension enabled on your server.'));
 }
 
 if (!$error->has()) {
-	for ($i = 1; $i < 6; $i++) {
-		$this_url = trim($_REQUEST['url_upload_' .$i]); 
-	
-		// did we get anything?
-		if (!strlen($this_url))
-			continue; 
-		
-		// validate URL
+    for ($i = 1; $i < 6; $i++) {
+        $this_url = trim($_REQUEST['url_upload_' .$i]);
+
+        // did we get anything?
+        if (!strlen($this_url))
+            continue;
+
+        // validate URL
         try {
             $client = new \Guzzle\Http\Client($this_url);
             $request = $client->get();
             $response = $request->send();
             $incoming_urls[] = $this_url;
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             $error->add(t('%s is not a valid URL.', Loader::helper('text')->specialchars($this_url)));
         }
-	}
+    }
 
-	if (!$valt->validate('import_remote')) {
-		$$error->add($valt->getErrorMessage());
-	}
-				
-	
-	if (count($incoming_urls) < 1) {
-		$error->add(t('You must specify at least one valid URL.'));
-	}
+    if (!$valt->validate('import_remote')) {
+        $$error->add($valt->getErrorMessage());
+    }
+
+    if (count($incoming_urls) < 1) {
+        $error->add(t('You must specify at least one valid URL.'));
+    }
 
 }
-
 
 $import_responses = array();
 
 // if we haven't gotten any errors yet then try to process the form
 if (!$error->has()) {
-	// itterate over each incoming URL adding if relevant
-	foreach($incoming_urls as $this_url) {
-		// try to D/L the provided file
+    // itterate over each incoming URL adding if relevant
+    foreach ($incoming_urls as $this_url) {
+        // try to D/L the provided file
         $client = new \Guzzle\Http\Client($this_url);
         $request = $client->get();
         $response = $request->send();
-		
-		if ($response->getContentLength()) {
-			$fpath = $file->getTemporaryDirectory();
-	
-			// figure out a filename based on filename, mimetype, ???
-			if (preg_match('/^.+?[\\/]([-\w%]+\.[-\w%]+)$/', $request->getPath(), $matches)) {
-				// got a filename (with extension)... use it
-				$fname = $matches[1];
-			} else if (! is_null($response->getContentType())) {
-				// use mimetype from http response
-				$fextension = MimeHelper::mimeToExtension($response->getContentType());
-				if ($fextension === false)
-					$error->add(t('Unknown mime-type: %s', $response->getContentType()));
-				else {
-					// make sure we're coming up with a unique filename 
-					do {
-						// make up a filename based on the current date/time, a random int, and the extension from the mime-type
-						$fname = date(DATE_APP_FILENAME) . mt_rand(100, 999) . '.' . $fextension;
-					} while (file_exists($fpath.'/'.$fname));
-				}
-			} //else {
-				// if we can't get the filename from the file itself OR from the mime-type I'm not sure there's much else we can do 
-			//}
-			
-			if (strlen($fname)) {
-				// write the downloaded file to a temporary location on disk
-				$handle = fopen($fpath.'/'.$fname, "w");
-				fwrite($handle, $response->getBody());
-				fclose($handle);
-				
-				// import the file into concrete
-				if ($fp->canAddFileType($cf->getExtension($fname))) {
-					$fi = new FileImporter();
-					$resp = $fi->import($fpath.'/'.$fname, $fname, $fr);
-					$r->setMessage(t('File uploaded successfully.'));
-					if (is_object($fr)) {
-						$r->setMessage(t('File replaced successfully.'));
-					}
-				} else {
-					$resp = FileImporter::E_FILE_INVALID_EXTENSION;
-				}
-				if (!($resp instanceof \Concrete\Core\File\Version)) {
-					$error->add($fname . ': ' . FileImporter::getErrorMessage($resp));
-				} else {
-					$import_responses[] = $resp;
-					
-					if (!is_object($fr)) {
-						// we check $fr because we don't want to set it if we are replacing an existing file
-						$respf = $resp->getFile();
-						$respf->setOriginalPage($_POST['ocID']);
-					} else {
-						$respf = $fr;
-					}
-					
-				}
-				
-				// clean up the file
-				unlink($fpath.'/'.$fname);
-			} else {
-				// could not figure out a file name
-				$error->add(t(/*i18n: %s is an URL*/'Could not determine the name of the file at %s', $this_url));
-			}
-		} else {
-			// warn that we couldn't download the file
-			$error->add(t(/*i18n: %s is an URL*/'There was an error downloading %s', $this_url));
-		}
-	}
+
+        if ($response->getContentLength()) {
+            $fpath = $file->getTemporaryDirectory();
+
+            // figure out a filename based on filename, mimetype, ???
+            if (preg_match('/^.+?[\\/]([-\w%]+\.[-\w%]+)$/', $request->getPath(), $matches)) {
+                // got a filename (with extension)... use it
+                $fname = $matches[1];
+            } elseif (! is_null($response->getContentType())) {
+                // use mimetype from http response
+                $fextension = MimeHelper::mimeToExtension($response->getContentType());
+                if ($fextension === false)
+                    $error->add(t('Unknown mime-type: %s', $response->getContentType()));
+                else {
+                    // make sure we're coming up with a unique filename
+                    do {
+                        // make up a filename based on the current date/time, a random int, and the extension from the mime-type
+                        $fname = date(DATE_APP_FILENAME) . mt_rand(100, 999) . '.' . $fextension;
+                    } while (file_exists($fpath.'/'.$fname));
+                }
+            } //else {
+                // if we can't get the filename from the file itself OR from the mime-type I'm not sure there's much else we can do
+            //}
+
+            if (strlen($fname)) {
+                // write the downloaded file to a temporary location on disk
+                $handle = fopen($fpath.'/'.$fname, "w");
+                fwrite($handle, $response->getBody());
+                fclose($handle);
+
+                // import the file into concrete
+                if ($fp->canAddFileType($cf->getExtension($fname))) {
+                    $fi = new FileImporter();
+                    $resp = $fi->import($fpath.'/'.$fname, $fname, $fr);
+                    $r->setMessage(t('File uploaded successfully.'));
+                    if (is_object($fr)) {
+                        $r->setMessage(t('File replaced successfully.'));
+                    }
+                } else {
+                    $resp = FileImporter::E_FILE_INVALID_EXTENSION;
+                }
+                if (!($resp instanceof \Concrete\Core\File\Version)) {
+                    $error->add($fname . ': ' . FileImporter::getErrorMessage($resp));
+                } else {
+                    $import_responses[] = $resp;
+
+                    if (!is_object($fr)) {
+                        // we check $fr because we don't want to set it if we are replacing an existing file
+                        $respf = $resp->getFile();
+                        $respf->setOriginalPage($_POST['ocID']);
+                    } else {
+                        $respf = $fr;
+                    }
+
+                }
+
+                // clean up the file
+                unlink($fpath.'/'.$fname);
+            } else {
+                // could not figure out a file name
+                $error->add(t(/*i18n: %s is an URL*/'Could not determine the name of the file at %s', $this_url));
+            }
+        } else {
+            // warn that we couldn't download the file
+            $error->add(t(/*i18n: %s is an URL*/'There was an error downloading %s', $this_url));
+        }
+    }
 }
 
 $r->setError($error);
 if (is_object($respf)) {
-	$r->setFile($respf);
+    $r->setFile($respf);
 }
 $r->outputJSON();
