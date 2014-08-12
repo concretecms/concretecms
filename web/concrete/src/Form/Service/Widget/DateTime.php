679ff08ca8ed076b9d9d1b124e7f07028cff2351
Michele Locati <michele@locati.it>
2014-08-12 11:20:10 +0200

    Fix date/time widget
    - Take in account the correct timezone
    - Don't use javascript timestamps (working in JS with timestamp and timezone is quite hard)
    - Avoid posting <value> and <value_pub>: jist <value> please ;)
    - Allow specifying empty values for initial date/time widget

--- web/concrete/src/Form/Service/Widget/DateTime.php
+++ web/concrete/src/Form/Service/Widget/DateTime.php
@@ -1,46 +1,52 @@
 <?php
 namespace Concrete\Core\Form\Service\Widget;
 
-use Loader;
 use Core;
 
 class DateTime
 {
     /**
-	 * Takes a "field" and grabs all the corresponding disparate fields from $_POST and translates into a timestamp
-	 * @param string $field
-	 * @param array $arr
-	 * @return string $dateTime
-	 */
+     * Takes a "field" and grabs all the corresponding disparate fields from $_POST and translates into a timestamp
+     * @param string $field The name of the field to translate
+     * @param array $arr = null The array containing the value. If null (default) we'll use $_POST
+     * @return string|false $dateTime In case of success returns the timestamp (in the format 'Y-m-d H:i:s' or 'Y-m-d'), otherwise returns false ($field value is not in the array) or '' (if $field value is empty).
+     * If $field has both date and time, the resulting value will be converted fro user timezone to system timezone.
+     * If $field has only date and not time, no timezone conversion will occur.
+     */
     public function translate($field, $arr = null)
     {
         if ($arr == null) {
             $arr = $_POST;
         }
+        $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service\Date */
+        $date = null;
         if (isset($arr[$field . '_dt'])) {
-                    if ($arr[$field . '_dt'] == '') {
-                        return '';
+            $value = $arr[$field . '_dt'];
+            if (strlen(trim($value)) === 0) {
+                return '';
             }
-            // Timestamp is in ms - so "/ 1000" is needed
-            $dt = date('Y-m-d', floor( $arr[$field . '_dt'] / 1000) );
-            $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service\Date */
-            if ($dh->getTimeFormat() == 12) {
-                $str = $dt . ' ' . $arr[$field . '_h'] . ':' . $arr[$field . '_m'] . ' ' . $arr[$field . '_a'];
-            } else {
-                $str = $dt . ' ' . $arr[$field . '_h'] . ':' . $arr[$field . '_m'];
+            $h = @intval($arr[$field . '_h']);
+            $m = @intval($arr[$field . '_m']);
+            if (isset($arr[$field . '_a']) && ($arr[$field . '_a'] === 'PM')) {
+                $h += 12;
             }
-
-            return date('Y-m-d H:i:s', strtotime($str));
+            $value .= ' ' . substr("0$h", -2) . ':' . substr("0$m", -2);
+            try {
+                $date = new \DateTime($value, new \DateTimeZone($dh->getTimezone('user')));
+            } catch (Exception $foo) {
+            }
         } elseif (isset($arr[$field])) {
-                    if ($arr[$field] == '') {
-                        return '';
+            $value = $arr[$field];
+            if (strlen(trim($value)) === 0) {
+                return '';
             }
-            $dt = date('Y-m-d', floor( $arr[$field] / 1000) );
-
-            return $dt;
-        } else {
-            return false;
+            try {
+                $date = new \DateTime($value, new \DateTimeZone($dh->getTimezone('system')));
+            } catch (Exception $foo) {
+            }
         }
+
+        return $date ? $dh->formatCustom('Y-m-d H:i:s', $date, 'system') : null;
     }
 
     /**
@@ -56,7 +62,7 @@ class DateTime
     public function datetime($prefix, $value = null, $includeActivation = false, $calendarAutoStart = true)
     {
         if (substr($prefix, -1) == ']') {
-            $prefix = substr($prefix, 0, strlen($prefix) -1);
+            $prefix = substr($prefix, 0, strlen($prefix) - 1);
             $_activate = $prefix . '_activate]';
             $_dt = $prefix . '_dt]';
             $_h = $prefix . '_h]';
@@ -69,23 +75,22 @@ class DateTime
             $_m = $prefix . '_m';
             $_a = $prefix . '_a';
         }
-        
+
         $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service\Date */
         $timeFormat = $dh->getTimeFormat();
-
-        $dfh = ($timeFormat == 12) ? 'h' : 'H';
-        $dfhe = ($timeFormat == 12) ? '12' : '23';
-        $dfhs = ($timeFormat == 12) ? '1' : '0';
-        if ($value != null) {
-            $defaultDateJs = 'new Date(' . strtotime($value) * 1000 . ')';
-            $h = date($dfh, strtotime($value));
-            $m = date('i', strtotime($value));
-            $a = date('A', strtotime($value));
+        list($dateYear, $dateMonth, $dateDay, $timeHour, $timeMinute) = explode(',', $dh->formatCustom('Y,n,j,G,i', $value ? $value : 'now'));
+        $timeMinute = intval($timeMinute);
+        if($timeFormat == 12) {
+            $timeAMPM = ($timeHour < 12) ? 'AM' : 'PM';
+            $timeHour = ($timeHour % 12);
+            if ($timeHour == 0) {
+                $timeHour = 12;
+            }
+        }
+        if ($value === '') {
+            $defaultDateJs = '""';
         } else {
-            $defaultDateJs = "new Date()";
-            $h = date($dfh);
-            $m = date('i');
-            $a = date('A');
+            $defaultDateJs = "new Date($dateYear, $dateMonth - 1, $dateDay)";
         }
         $id = preg_replace("/[^0-9A-Za-z-]/", "_", $prefix);
         $html = '';
@@ -96,16 +101,17 @@ class DateTime
             } else {
                 $disabled = 'disabled';
             }
-
             $html .= '<input type="checkbox" id="' . $id . '_activate" class="ccm-activate-date-time" ccm-date-time-id="' . $id . '" name="' . $_activate . '" ' . $activated . ' />';
         }
 
-        $html .= '<div><span class="ccm-input-date-wrapper form-inline" id="' . $id . '_dw"><input id="' . $id . '_dt_pub" name="' . $_dt . '_pub" class="form-control ccm-input-date"  ' . $disabled . ' /><input id="' . $id . '_dt" name="' . $_dt . '" type="hidden" ' . $disabled . ' /></span>';
+        $html .= '<div><span class="ccm-input-date-wrapper form-inline" id="' . $id . '_dw"><input id="' . $id . '_dt_pub" class="form-control ccm-input-date"  ' . $disabled . ' /><input id="' . $id . '_dt" name="' . $_dt . '" type="hidden" ' . $disabled . ' /></span>';
         $html .= '<span class="ccm-input-time-wrapper form-inline" id="' . $id . '_tw">';
         $html .= '<select class="form-control" id="' . $id . '_h" name="' . $_h . '" ' . $disabled . '>';
 
-        for ($i = $dfhs; $i <= $dfhe; $i++) {
-            if ($h == $i) {
+        $hourStart = ($timeFormat == 12) ? 1 : 0;
+        $hourEnd = ($timeFormat == 12) ? 12 : 23;
+        for ($i = $hourStart; $i <= $hourEnd; $i++) {
+            if ($i == $timeHour) {
                 $selected = 'selected';
             } else {
                 $selected = '';
@@ -115,7 +121,7 @@ class DateTime
         $html .= '</select>:';
         $html .= '<select class="form-control"  id="' . $id . '_m" name="' . $_m . '" ' . $disabled . '>';
         for ($i = 0; $i <= 59; $i++) {
-            if ($m == sprintf('%02d', $i)) {
+            if ($i == $timeMinute) {
                 $selected = 'selected';
             } else {
                 $selected = '';
@@ -126,20 +132,20 @@ class DateTime
         if ($timeFormat == 12) {
             $html .= '<select class="form-control" id="' . $id . '_a" name="' . $_a . '" ' . $disabled . '>';
             $html .= '<option value="AM" ';
-            if ($a == 'AM') {
+            if ($timeAMPM == 'AM') {
                 $html .= 'selected';
             }
             $html .= '>';
             // This prints out the translation of "AM" in the current language
-            $html .= Loader::helper("date")->date("A",mktime(1));
+            $html .= $dh->date('A', mktime(1));
             $html .= '</option>';
             $html .= '<option value="PM" ';
-            if ($a == 'PM') {
+            if ($timeAMPM == 'PM') {
                 $html .= 'selected';
             }
             $html .= '>';
             // This prints out the translation of "PM" in the current language
-            $html .= Loader::helper("date")->date("A",mktime(13));
+            $html .= $dh->date('A', mktime(13));
             $html .= '</option>';
             $html .= '</select>';
         }
@@ -149,11 +155,11 @@ class DateTime
             $html .= '<script type="text/javascript">$(function () {
                 $("#' . $id . '_dt_pub").datepicker({
                     dateFormat: ' . $jh->encode($dh->getJQueryUIDatePickerFormat(t(/*i18n: Short date format: see http://www.php.net/manual/en/function.date.php */ 'n/j/Y'))) . ',
-                    altFormat: "@",
+                    altFormat: "yy-mm-dd",
                     altField: "#' . $id . '_dt",
                     changeYear: true,
                     showAnim: \'fadeIn\'
-                }).datepicker( "setDate" , ' . $defaultDateJs . ' ) })</script>';
+                }).datepicker("setDate" , ' . $defaultDateJs . '); })</script>';
         }
         // first we add a calendar input
 
@@ -197,26 +203,31 @@ EOS;
 	 */
     public function date($field, $value = null, $calendarAutoStart = true)
     {
+        $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service\Date */
         $id = preg_replace("/[^0-9A-Za-z-]/", "_", $field);
         if (isset($_REQUEST[$field])) {
-            $defaultDateJs = 'new Date(' .  preg_replace('/[^0-9]/', '', $_REQUEST[$field]) .')' ;
-        } elseif ($value != "") {
-            $defaultDateJs = 'new Date(' . (int) strtotime($value) * 1000 . ')';
+            $timestamp = empty($_REQUEST[$field]) ? false : @strtotime($_REQUEST[$field]);
+        } elseif ($value) {
+            $timestamp = @strtotime($value);
         } elseif ($value === '') {
-            $defaultDateJs = '""';
+            $timestamp = false;
         } else {
-            $defaultDateJs = 'new Date()';
+            // Today (in the user's timezone)
+            $timestamp = strtotime($dh->formatCustom('Y-m-d'));
         }
-        //$id = preg_replace("/[^0-9A-Za-z-]/", "_", $prefix);
+        if ($timestamp) {
+            $defaultDateJs = 'new Date(' . implode(', ', array(date('Y', $timestamp), date('n', $timestamp) - 1, date('j', $timestamp))) . ')';
+        } else {
+            $defaultDateJs = '""';
+        }
         $html = '';
-        $html .= '<div><span class="ccm-input-date-wrapper" id="' . $id . '_dw"><input id="' . $id . '_pub" name="' . $field . '_pub" class="ccm-input-date"  /><input id="' . $id . '" name="' . $field . '" type="hidden"  /></span></div>';
-        $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service\Date */
+        $html .= '<div><span class="ccm-input-date-wrapper" id="' . $id . '_dw"><input id="' . $id . '_pub" class="ccm-input-date"  /><input id="' . $id . '" name="' . $field . '" type="hidden"  /></span></div>';
         $jh = Core::make('helper/json'); /* @var $jh \Concrete\Core\Http\Service\Json */
         if ($calendarAutoStart) {
             $html .= '<script type="text/javascript">$(function () {
                 $("#' . $id . '_pub").datepicker({
                     dateFormat: ' . $jh->encode($dh->getJQueryUIDatePickerFormat(t(/*i18n: Short date format: see http://www.php.net/manual/en/function.date.php */ 'n/j/Y'))) . ',
-                    altFormat: "@",
+                    altFormat: "yy-mm-dd",
                     altField: "#' . $id . '",
                     changeYear: true,
                     showAnim: \'fadeIn\'
