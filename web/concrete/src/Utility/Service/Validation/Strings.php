c64148c225ed99ed31a42aebef11dcb9b5286f58
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-08-15 09:39:29 -0400

    psr2, one of these days I'll remember before my pr...
    Former-commit-id: 9b43ee1d78a44e75b0090c43d18267f27b21c689

--- web/concrete/src/Utility/Service/Validation/Strings.php
+++ web/concrete/src/Utility/Service/Validation/Strings.php
@@ -1,21 +1,14 @@
-<? 
+<?php
 
 namespace Concrete\Core\Utility\Service\Validation;
 
-
 use Egulias\EmailValidator\EmailValidator;
+
 /**
  * Functions useful for validating strings
- * @package Helpers
- * @category Concrete
- * @subpackage Validation
- * @author Andrew Embler <andrew@concrete5.org>
- * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
- * @license    http://www.concrete5.org/license/     MIT License
  */
-
-class Strings {
-
+class Strings
+{
 
     /**
      * Validates an email address
@@ -23,119 +16,136 @@ class Strings {
      * @param bool $testMXRecord
      * @return bool $isvalid
      */
-	public function email($em, $testMXRecord = false) {
+    public function email($em, $testMXRecord = false)
+    {
         $validator = new EmailValidator();
         return $validator->isValid($em, $testMXRecord, $testMXRecord);
-	}
-	
-	/**
-	 * Returns true on whether the passed field is completely alpha-numeric
-	 * @param string $field
-	 * @param bool $allow_spaces whether or not spaces are permitted in the field contents
-	 * @param bool $allow_dashes whether or not dashes (-) are permitted in the field contents
-	 * @return bool
-	 */
-	public function alphanum($field, $allow_spaces = false, $allow_dashes = false) {
-		if ($allow_spaces && $allow_dashes) {
-			return !preg_match("/[^A-Za-z0-9 \-]/", $field);
-		} else if ($allow_spaces) {
-			return !preg_match("/[^A-Za-z0-9 ]/", $field);
-		} else if ($allow_dashes) {
-			return !preg_match("/[^A-Za-z0-9\-]/", $field);
-		} else {
-			return !preg_match('/[^A-Za-z0-9]/', $field);
-		}
-	}
-	
-	/** 
-	 * Returns true if the passed field is a valid "handle" (e.g. only letters, numbers, or a _ symbol
-	 */
-	public function handle($handle) {
-		if (!$handle) {
-			return false;
-		}
-		return !preg_match("/[^A-Za-z0-9\_]/", $handle);
-	}
+    }
 
-	
-	/**
-	 * Returns false if the string is empty (including trim())
-	 * @param string $field
-	 * @return bool
-	 */
-	public function notempty($field) {
-		return ((is_array($field) && count($field) > 0) || (is_string($field) && trim($field) != ''));
-	}	
-	
-	/** 
-	 * Returns true on whether the passed string is larger or equal to the passed length
-	 * @param string $str
-	 * @param int $length
-	 * @return bool
-	 */
-	public function min($str, $length) {
-		return strlen(trim($str)) >= $length;
-	}
-	
-	/** 
-	 * Returns true on whether the passed is smaller or equal to the passed length
-	 * @param string $str
-	 * @param int $length
-	 * @return bool
-	 */
-	public function max($str, $length) {
-		return strlen(trim($str)) <= $length;
-	}
-	
-	/**
-	 * Returns 0 if there are no numbers in the string, or returns the number of numbers in the string
-	 * @param string $str
-	 * @return int
-	 */
-	public function containsNumber($str) {
-		return strlen(trim(preg_replace('/([^0-9]*)/', '', $str)));
-	}
-	
-	/**
-	 * Returns 0 if there are no upper case letters in the string, or returns the number of upper case letters in the string
-	 * @param string $str
-	 * @return int
-	 */
-	public function containsUpperCase($str) {
-		return strlen(trim(preg_replace('/([^A-Z]*)/', '', $str)));
-	}
+    /**
+     * Returns true on whether the passed field is completely alpha-numeric
+     * @param string $field
+     * @param bool $allow_spaces whether or not spaces are permitted in the field contents
+     * @param bool $allow_dashes whether or not dashes (-) are permitted in the field contents
+     * @return bool
+     */
+    public function alphanum($field, $allow_spaces = false, $allow_dashes = false)
+    {
+        if ($allow_spaces && $allow_dashes) {
+            return !preg_match("/[^A-Za-z0-9 \-]/", $field);
+        } else {
+            if ($allow_spaces) {
+                return !preg_match("/[^A-Za-z0-9 ]/", $field);
+            } else {
+                if ($allow_dashes) {
+                    return !preg_match("/[^A-Za-z0-9\-]/", $field);
+                } else {
+                    return !preg_match('/[^A-Za-z0-9]/', $field);
+                }
+            }
+        }
+    }
 
-	/**
-	 * Returns 0 if there are no lower case letters in the string, or returns the number of lower case letters in the string
-	 * @param string $str
-	 * @return int
-	 */
-	public function containsLowerCase($str) {
-		return strlen(trim(preg_replace('/([^a-z]*)/', '', $str)));
-	}
+    /**
+     * Returns true if the passed field is a valid "handle" (e.g. only letters, numbers, or a _ symbol
+     */
+    public function handle($handle)
+    {
+        if (!$handle) {
+            return false;
+        }
+        return !preg_match("/[^A-Za-z0-9\_]/", $handle);
+    }
 
-	/**
-	 * Returns 0 if there are no symbols in the string, or returns the number of symbols in the string
-	 * @param string $str
-	 * @return int
-	 */	
-	public function containsSymbol($str) {
-		return strlen(trim(preg_replace('/([a-zA-Z0-9]*)/', '', $str))); //we replace a-z and numbers and see if there is anything left.
-	}
-	
-	/**
-	 * Returns true if the string contains another string
-	 * @param string $str
-	 * @param array $cont
-	 * @return bool
-	 */
-	public function containsString($str, $cont = array()) {
-		$arr = (!is_array($cont)) ? array($cont) : $cont; // turn the string into an array
-		foreach ($arr as $item) {
-			if (strstr($str, $item) !== false) {
-				return true;
-			}
-		}
-		return false;
-	}
+
+    /**
+     * Returns false if the string is empty (including trim())
+     * @param string $field
+     * @return bool
+     */
+    public function notempty($field)
+    {
+        return ((is_array($field) && count($field) > 0) || (is_string($field) && trim($field) != ''));
+    }
+
+    /**
+     * Returns true on whether the passed string is larger or equal to the passed length
+     * @param string $str
+     * @param int $length
+     * @return bool
+     */
+    public function min($str, $length)
+    {
+        return strlen(trim($str)) >= $length;
+    }
+
+    /**
+     * Returns true on whether the passed is smaller or equal to the passed length
+     * @param string $str
+     * @param int $length
+     * @return bool
+     */
+    public function max($str, $length)
+    {
+        return strlen(trim($str)) <= $length;
+    }
+
+    /**
+     * Returns 0 if there are no numbers in the string, or returns the number of numbers in the string
+     * @param string $str
+     * @return int
+     */
+    public function containsNumber($str)
+    {
+        return strlen(trim(preg_replace('/([^0-9]*)/', '', $str)));
+    }
+
+    /**
+     * Returns 0 if there are no upper case letters in the string, or returns the number of upper case letters in the string
+     * @param string $str
+     * @return int
+     */
+    public function containsUpperCase($str)
+    {
+        return strlen(trim(preg_replace('/([^A-Z]*)/', '', $str)));
+    }
+
+    /**
+     * Returns 0 if there are no lower case letters in the string, or returns the number of lower case letters in the string
+     * @param string $str
+     * @return int
+     */
+    public function containsLowerCase($str)
+    {
+        return strlen(trim(preg_replace('/([^a-z]*)/', '', $str)));
+    }
+
+    /**
+     * Returns 0 if there are no symbols in the string, or returns the number of symbols in the string
+     * @param string $str
+     * @return int
+     */
+    public function containsSymbol($str)
+    {
+        return strlen(
+            trim(preg_replace('/([a-zA-Z0-9]*)/', '', $str))
+        ); //we replace a-z and numbers and see if there is anything left.
+    }
+
+    /**
+     * Returns true if the string contains another string
+     * @param string $str
+     * @param array $cont
+     * @return bool
+     */
+    public function containsString($str, $cont = array())
+    {
+        $arr = (!is_array($cont)) ? array($cont) : $cont; // turn the string into an array
+        foreach ($arr as $item) {
+            if (strstr($str, $item) !== false) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
