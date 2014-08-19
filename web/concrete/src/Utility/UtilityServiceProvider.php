d9012d203d0c8a4880b11379028b1b62acc11c76
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-08-19 18:44:14 -0400

    Moved the noFollowHref function to it's own Html utility class and included the class as part of the UtilityServiceProvider

--- web/concrete/src/Utility/UtilityServiceProvider.php
+++ web/concrete/src/Utility/UtilityServiceProvider.php
@@ -3,10 +3,10 @@ namespace Concrete\Core\Utility;
 use \Concrete\Core\Foundation\Service\Provider as ServiceProvider;
 class UtilityServiceProvider extends ServiceProvider {
 
-
 	public function register() {
 		$singletons = array(
-			'helper/text' => '\Concrete\Core\Utility\Service\Text',
+            'helper/text' => '\Concrete\Core\Utility\Service\Text',
+            'helper/html' => '\Concrete\Core\Utility\Service\Html',
 			'helper/arrays' => '\Concrete\Core\Utility\Service\Arrays',
 			'helper/number' => '\Concrete\Core\Utility\Service\Number',
 			'helper/xml' => '\Concrete\Core\Utility\Service\Xml',
