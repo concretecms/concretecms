ffa8d078be5bbb0c5311711a01a5895294a07ecf
Andrew Embler <andrew@concrete5.org>
2014-08-19 14:28:17 -0700

    Updating Zend Queue with custom DBAL adapter
    Former-commit-id: 33547f1b2a9953d1dad7d9a5d8c38e325f16f687

---
+++ web/concrete/src/Foundation/Queue/Queue.php
@@ -0,0 +1,43 @@
+<?php
+namespace Concrete\Core\Foundation\Queue;
+use Loader;
+use Database;
+use \ZendQueue\Queue as ZendQueue;
+
+class Queue {
+
+	public static function get($name, $additionalConfig = array()) {
+
+		$config = array(
+            'name' => $name,
+		);
+
+        $db = Database::get();
+
+        $adapterOptions = array(
+            'connection' => $db
+        );
+
+        $adapter = new DatabaseQueueAdapter($adapterOptions);
+		$config = array_merge($config, $additionalConfig);
+		return new ZendQueue($adapter, $config);
+	}
+
+	public static function exists($name) {
+		// probably should use the Zend Queue for this but it's just such overhead for a quick
+		// DB call.
+		$db = Loader::db();
+		$r = $db->GetOne('select queue_id from Queues where queue_name = ?', array($name));
+		return $r > 0;
+
+		/*
+		$q = Queue::get($name);
+		if ($q->count() > 0) {
+			return true;
+		} else {
+			$q->deleteQueue($name);
+			return false;
+		}
+		*/
+	}
+}
