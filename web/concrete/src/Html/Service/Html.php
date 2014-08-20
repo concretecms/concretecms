c427288f76439e28b9479926925da15aacf38eba
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-08-20 12:14:20 -0400

    Html service already existed... woops....
    Former-commit-id: 09eff87c8e4a6def1363c931d71ba5365089bdc6

--- web/concrete/src/Html/Service/Html.php
+++ web/concrete/src/Html/Service/Html.php
@@ -148,4 +148,26 @@ class Html {
 		}
 		return $str;
 	}
+
+    /**
+     * Takes in a string, and adds rel="nofollow" to any a tags that contain an href attribute
+     * @param string $input
+     * @return string
+     */
+    public function noFollowHref($input)
+    {
+        return preg_replace_callback(
+            '/(?:<a(.*?href.*?)>)/i',
+            function ($matches) {
+                if (strpos($matches[1], 'rel="nofollow"') === false) {
+                    //if there is no nofollow add it
+                    return '<a' . $matches[1] . ' rel="nofollow">';
+                } else {
+                    //if there is already a nofollow take no action
+                    return $matches[0];
+                }
+            },
+            $input
+        );
+    }
 }
--- web/concrete/src/Utility/UtilityServiceProvider.php
+++ web/concrete/src/Utility/UtilityServiceProvider.php
@@ -6,7 +6,6 @@ class UtilityServiceProvider extends ServiceProvider {
 	public function register() {
 		$singletons = array(
             'helper/text' => '\Concrete\Core\Utility\Service\Text',
-            'helper/html' => '\Concrete\Core\Utility\Service\Html',
 			'helper/arrays' => '\Concrete\Core\Utility\Service\Arrays',
 			'helper/number' => '\Concrete\Core\Utility\Service\Number',
 			'helper/xml' => '\Concrete\Core\Utility\Service\Xml',
