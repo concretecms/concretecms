1af21de2f8325bed8b3a0c0c03504f911a3ba0af
Korvin Szanto <korvinszanto@gmail.com>
2014-08-22 15:10:47 -0700

    merge configs into app config, reintroduce config loader.
    Former-commit-id: f94cf805e64d78273549d721e3499100ed044bb1

--- web/concrete/config/app.php
+++ web/concrete/config/app.php
@@ -131,16 +131,6 @@ return array(
     'routes'              => array(
 
         /**
-         * Install
-         */
-        '/install'                                                                      => array('\Concrete\Controller\Install::view'),
-        '/install/select_language'                                                      => array('\Concrete\Controller\Install::select_language'),
-        '/install/setup'                                                                => array('\Concrete\Controller\Install::setup'),
-        '/install/test_url/{num1}/{num2}'                                               => array('\Concrete\Controller\Install::test_url'),
-        '/install/configure'                                                            => array('\Concrete\Controller\Install::configure'),
-        '/install/run_routine/{pkgHandle}/{routine}'                                    => array('\Concrete\Controller\Install::run_routine'),
-
-        /**
          * Tools - legacy
          */
         '/tools/blocks/{btHandle}/{tool}'                                               => array(
