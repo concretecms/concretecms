9e9b40695e46385801384531415464364d55cfb7
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-08-27 11:56:41 -0400

    Created a new alias for the PermissionCache
    Changed places that overrided the Response::testForErrors() function, so that if they pass all other validation, they now call the parent function which has been modified to return false. This also cleaned up the return values so that a boolean or error INT constant is always returned.
    
    Switched out the Loader helper function for the Core::make function
    
    Cleaned up `use` statements that didn't appear to actually be used.
    
    Removed seemingly unused protected variables from the Response class.
    
    PHPDocs to help with intellisense / autocomplete and inline documentation

--- web/concrete/src/Permission/Response/PageResponse.php
+++ web/concrete/src/Permission/Response/PageResponse.php
@@ -114,6 +114,7 @@ class PageResponse extends Response {
 				return COLLECTION_FORBIDDEN;
 			}
 		}
+        return parent::testForErrors();
 	}
 
 
--- web/concrete/src/Permission/Response/Response.php
+++ web/concrete/src/Permission/Response/Response.php
@@ -1,63 +1,91 @@
 <?
 namespace Concrete\Core\Permission\Response;
 use \Exception;
-use Loader;
-use Page;
 use User;
-use Group;
-use PermissionKey;
 use PermissionKeyCategory;
-use Permissions;
 use Core;
-use \Concrete\Core\Permission\Cache as PermissionCache;
+use PermissionCache;
 class Response {
 
+    /** @var \Concrete\Core\Permission\ObjectInterface */
 	protected $object;
-	protected $allowedPermissions = array();
-	protected $customClassObjects = array();
+    /** @var PermissionKeyCategory */
 	protected $category;
 	static $cache = array();
 
-	public function setPermissionObject($object) {
+    /**
+     * Sets the current permission object to the object provided, this object should implement the Permission ObjectInterface
+     * @param \Concrete\Core\Permission\ObjectInterface $object
+     */
+    public function setPermissionObject($object) {
 		$this->object = $object;
 	}
-	public function getPermissionObject() {
+
+    /**
+     * Retrieves the current permission object
+     */
+    public function getPermissionObject() {
 		return $this->object;
 	}
-	public function setPermissionCategoryObject($category) {
+
+    /**
+     * Sets the current Permission Category object to an appropriate PermissionKeyCategory
+     * @param PermissionKeyCategory $category
+     */
+    public function setPermissionCategoryObject($category) {
 		$this->category = $category;
 	}
 
-	public function testForErrors() { }
+    /**
+     * Returns an error constant if an error is present, false if there are no errors.
+     * @return bool|int
+     */
+    public function testForErrors()
+    {
+        return false;
+    }
 
-	public static function getResponse($object) {
+    /**
+     * Passing in any object that implements the ObjectInterface, retrieve the Permission Response object.
+     * @param \Concrete\Core\Permission\ObjectInterface $object
+     * @return \Concrete\Core\Permission\Response\Response
+     */
+    public static function getResponse($object) {
 		$r = PermissionCache::getResponse($object);
 		if (is_object($r)) {
 			return $r;
 		}
 
 		$className = $object->getPermissionResponseClassName();
+        /** @var \Concrete\Core\Permission\Response\Response $pr */
+        $pr = Core::make($className);
 		if ($object->getPermissionObjectKeyCategoryHandle()) {
 			$category = PermissionKeyCategory::getByHandle($object->getPermissionObjectKeyCategoryHandle());
+            $pr->setPermissionCategoryObject($category);
 		}
-		$pr = Core::make($className);
-		$pr->setPermissionCategoryObject($category);
 		$pr->setPermissionObject($object);
 		PermissionCache::addResponse($object, $pr);
 		return $pr;
 	}
 
-	public function validate($permission, $args = array()) {
+    /**
+     * This function returns true if the user has permission to the object, or false if they do not have access.
+     * @param string $permissionHandle A Permission Key Handle
+     * @param array $args Arguments to pass to the PermissionKey object's validate function
+     * @return bool
+     * @throws Exception
+     */
+    public function validate($permissionHandle, $args = array()) {
 		$u = new User();
 		if ($u->isSuperUser()) {
 			return true;
 		}
 		if (!is_object($this->category)) {
-			throw new \Exception(t('Unable to get category for permission %s', $permission));
+			throw new Exception(t('Unable to get category for permission %s', $permissionHandle));
 		}
-		$pk = $this->category->getPermissionKeyByHandle($permission);
+		$pk = $this->category->getPermissionKeyByHandle($permissionHandle);
 		if (!$pk) {
-			throw new Exception(t('Unable to get permission key for %s', $permission));
+			throw new Exception(t('Unable to get permission key for %s', $permissionHandle));
 		}
 		$pk->setPermissionObject($this->object);
 		return call_user_func_array(array($pk, 'validate'), $args);
@@ -65,7 +93,9 @@ class Response {
 
 	public function __call($f, $a) {
 		$permission = substr($f, 3);
-		$permission = Loader::helper('text')->uncamelcase($permission);
+        /** @var \Concrete\Core\Utility\Service\Text $textHelper */
+        $textHelper = Core::make('helper/text');
+		$permission = $textHelper->uncamelcase($permission);
 		return $this->validate($permission, $a);
 	}
 
