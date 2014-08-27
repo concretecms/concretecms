6da1ec01c482ab4fbdccb61d3a80d6be5f9f32ab
Evan Cooper <evan@concretecms.com>
2014-08-27 14:51:00 -0700

    bootstrapping youtube block edit interface

--- web/concrete/blocks/youtube/form_setup_html.php
+++ web/concrete/blocks/youtube/form_setup_html.php
@@ -1,10 +1,4 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?> 
-<style type="text/css">
-table#videoBlockSetup th {font-weight: bold; text-style: normal; padding-right: 8px; white-space: nowrap; vertical-align:top ; padding-bottom:8px}
-table#videoBlockSetup td{ font-size:12px; vertical-align:top; padding-bottom:8px;}
-table#videoBlockSetup .note{ font-size:10px; color:#999999; font-weight:normal }
-</style> 
-
+<? defined('C5_EXECUTE') or die("Access Denied."); ?>
 <?
 if (!$bObj->vWidth) {
 	$bObj->vWidth=425;
@@ -12,37 +6,36 @@ if (!$bObj->vWidth) {
 if (!$bObj->vHeight) {
 	$bObj->vHeight=344;
 }
-
 ?>
-
-<table id="videoBlockSetup" style="width:100%" class="table table-bordered"> 
-	<tr>
-		<th><?=t('Title')?></th>
-		<td><input type="text" style="width: 230px" name="title" value="<?=$bObj->title?>"/></td>
-	</tr>	
-	<tr>
-		<th><?=t('YouTube URL')?></th>
-		<td>
-			<input type="text" style="width: 230px" id="YouTubeVideoURL" name="videoURL" value="<?=$bObj->videoURL?>" />
-		</td>
-	</tr>
-	<tr>
-		<th><?=t('Width')?></th>
-		<td>
-			<input type="text" style="width: 40px" id="YouTubeVideoWidth" name="vWidth" value="<?=$bObj->vWidth?>" />
-		</td>
-	</tr>
-	<tr>
-		<th><?=t('Height')?></th>
-		<td>
-			<input type="text" style="width: 40px" id="YouTubeVideoHeight" name="vHeight" value="<?=$bObj->vHeight?>" />
-		</td>
-	</tr>
-	<tr>
-		<th><?=t('Video Player')?></th>
-		<td>
-			<input type="radio" name="vPlayer" value="1" <?=($bObj->vPlayer)?'checked':''?> /> <?=t('iFrame - Works in more devices')?> <br/>
-			<input type="radio" name="vPlayer" value="0" <?=(!$bObj->vPlayer)?'checked':''?> /> <?=t('Flash Embed - Legacy method')?>
-		</td>
-	</tr>	
-</table>
+<style>
+    .ccm-ui .form-control.yt-vid-dims {
+        width: 100px;
+    }
+</style>
+<div class="form-group">
+    <label><?=t('Title')?></label>
+    <input type="text" class="form-control" name="title" value="<?=$bObj->title?>"/>
+</div>
+<div class="form-group">
+    <label><?=t('YouTube URL')?></label>
+    <input type="text" class="form-control" id="YouTubeVideoURL" name="videoURL" value="<?=$bObj->videoURL?>" />
+</div>
+<div class="form-group">
+    <label><?=t('Width')?></label>
+    <input type="text" class="form-control yt-vid-dims" id="YouTubeVideoWidth" name="vWidth" value="<?=$bObj->vWidth?>" />
+</div>
+<div class="form-group">
+    <label><?=t('Height')?></label>
+    <input type="text" class="form-control yt-vid-dims" id="YouTubeVideoHeight" name="vHeight" value="<?=$bObj->vHeight?>" />
+</div>
+<div class="form-group">
+    <label><?=t('Video Player')?></label>
+        <div class="radio">
+            <label><input type="radio" name="vPlayer" value="1" <?=($bObj->vPlayer)?'checked':''?> /> <?=t('iFrame - Works in more devices')?></label>
+        </div>
+    <div class="radio">
+        <label>
+            <input type="radio" name="vPlayer" value="0" <?=(!$bObj->vPlayer)?'checked':''?> /> <?=t('Flash Embed - Legacy method')?>
+        </label>
+    </div>
+</div>
