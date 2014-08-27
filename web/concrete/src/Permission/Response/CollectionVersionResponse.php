e99dfc6602f648b54f29323bc7a0b525b1576865
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2014-08-27 12:19:37 -0400

    PSR2, couple more PHPDocs

--- web/concrete/src/Permission/Response/CollectionVersionResponse.php
+++ web/concrete/src/Permission/Response/CollectionVersionResponse.php
@@ -1,24 +1,32 @@
-<?
+<?php
 namespace Concrete\Core\Permission\Response;
 
 use Page;
 use Permissions;
 
-class CollectionVersionResponse extends Response {
+/**
+ * Class CollectionVersionResponse
+ * @package Concrete\Core\Permission\Response
+ */
+class CollectionVersionResponse extends Response
+{
+    /** @var \Concrete\Core\Page\Collection\Version\Version $object */
+    protected $object;
 
-	public function testForErrors() { 
-		if (!$this->object->getVersionID()) {
-			$c = Page::getByID($this->object->getCollectionID());
-			$cp = new Permissions($c);
-			if ($cp->canViewPageVersions()) { 
-				return COLLECTION_FORBIDDEN;
-			} else { 
-				return COLLECTION_NOT_FOUND;
-			}
-		} else if (!$this->object->isMostRecent()) {
-			return VERSION_NOT_RECENT;
-		}
+    public function testForErrors()
+    {
+        if (!$this->object->getVersionID()) {
+            $c = Page::getByID($this->object->getCollectionID());
+            $cp = new Permissions($c);
+            if ($cp->canViewPageVersions()) {
+                return COLLECTION_FORBIDDEN;
+            } else {
+                return COLLECTION_NOT_FOUND;
+            }
+        } elseif (!$this->object->isMostRecent()) {
+            return VERSION_NOT_RECENT;
+        }
         return parent::testForErrors();
-	}
+    }
 
 }
--- web/concrete/src/Permission/Response/Response.php
+++ web/concrete/src/Permission/Response/Response.php
@@ -1,40 +1,46 @@
-<?
+<?php
 namespace Concrete\Core\Permission\Response;
+
 use \Exception;
 use User;
 use PermissionKeyCategory;
 use Core;
 use PermissionCache;
-class Response {
+
+class Response
+{
 
     /** @var \Concrete\Core\Permission\ObjectInterface */
-	protected $object;
+    protected $object;
     /** @var PermissionKeyCategory */
-	protected $category;
-	static $cache = array();
+    protected $category;
+    static $cache = array();
 
     /**
      * Sets the current permission object to the object provided, this object should implement the Permission ObjectInterface
      * @param \Concrete\Core\Permission\ObjectInterface $object
      */
-    public function setPermissionObject($object) {
-		$this->object = $object;
-	}
+    public function setPermissionObject($object)
+    {
+        $this->object = $object;
+    }
 
     /**
      * Retrieves the current permission object
      */
-    public function getPermissionObject() {
-		return $this->object;
-	}
+    public function getPermissionObject()
+    {
+        return $this->object;
+    }
 
     /**
      * Sets the current Permission Category object to an appropriate PermissionKeyCategory
      * @param PermissionKeyCategory $category
      */
-    public function setPermissionCategoryObject($category) {
-		$this->category = $category;
-	}
+    public function setPermissionCategoryObject($category)
+    {
+        $this->category = $category;
+    }
 
     /**
      * Returns an error constant if an error is present, false if there are no errors.
@@ -50,23 +56,24 @@ class Response {
      * @param \Concrete\Core\Permission\ObjectInterface $object
      * @return \Concrete\Core\Permission\Response\Response
      */
-    public static function getResponse($object) {
-		$r = PermissionCache::getResponse($object);
-		if (is_object($r)) {
-			return $r;
-		}
+    public static function getResponse($object)
+    {
+        $r = PermissionCache::getResponse($object);
+        if (is_object($r)) {
+            return $r;
+        }
 
-		$className = $object->getPermissionResponseClassName();
+        $className = $object->getPermissionResponseClassName();
         /** @var \Concrete\Core\Permission\Response\Response $pr */
         $pr = Core::make($className);
-		if ($object->getPermissionObjectKeyCategoryHandle()) {
-			$category = PermissionKeyCategory::getByHandle($object->getPermissionObjectKeyCategoryHandle());
+        if ($object->getPermissionObjectKeyCategoryHandle()) {
+            $category = PermissionKeyCategory::getByHandle($object->getPermissionObjectKeyCategoryHandle());
             $pr->setPermissionCategoryObject($category);
-		}
-		$pr->setPermissionObject($object);
-		PermissionCache::addResponse($object, $pr);
-		return $pr;
-	}
+        }
+        $pr->setPermissionObject($object);
+        PermissionCache::addResponse($object, $pr);
+        return $pr;
+    }
 
     /**
      * This function returns true if the user has permission to the object, or false if they do not have access.
@@ -75,29 +82,31 @@ class Response {
      * @return bool
      * @throws Exception
      */
-    public function validate($permissionHandle, $args = array()) {
-		$u = new User();
-		if ($u->isSuperUser()) {
-			return true;
-		}
-		if (!is_object($this->category)) {
-			throw new Exception(t('Unable to get category for permission %s', $permissionHandle));
-		}
-		$pk = $this->category->getPermissionKeyByHandle($permissionHandle);
-		if (!$pk) {
-			throw new Exception(t('Unable to get permission key for %s', $permissionHandle));
-		}
-		$pk->setPermissionObject($this->object);
-		return call_user_func_array(array($pk, 'validate'), $args);
-	}
+    public function validate($permissionHandle, $args = array())
+    {
+        $u = new User();
+        if ($u->isSuperUser()) {
+            return true;
+        }
+        if (!is_object($this->category)) {
+            throw new Exception(t('Unable to get category for permission %s', $permissionHandle));
+        }
+        $pk = $this->category->getPermissionKeyByHandle($permissionHandle);
+        if (!$pk) {
+            throw new Exception(t('Unable to get permission key for %s', $permissionHandle));
+        }
+        $pk->setPermissionObject($this->object);
+        return call_user_func_array(array($pk, 'validate'), $args);
+    }
 
-	public function __call($f, $a) {
-		$permission = substr($f, 3);
+    public function __call($f, $a)
+    {
+        $permission = substr($f, 3);
         /** @var \Concrete\Core\Utility\Service\Text $textHelper */
         $textHelper = Core::make('helper/text');
-		$permission = $textHelper->uncamelcase($permission);
-		return $this->validate($permission, $a);
-	}
+        $permission = $textHelper->uncamelcase($permission);
+        return $this->validate($permission, $a);
+    }
 
 
 }
