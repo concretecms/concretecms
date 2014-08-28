fe9fafc52e1b923f3eab0e1a2556aa474671b58e
Evan Cooper <evan@concretecms.com>
2014-08-28 10:43:15 -0700

    adding magic quotes check into new included preprocess file, included by the dispatcher
    Former-commit-id: 55cd9e37a6cc9e7b5821b2003b6242bfc8f38df4

---
+++ web/concrete/bootstrap/preprocess.php
@@ -0,0 +1,24 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+
+/**
+ * ----------------------------------------------------------------------------------
+ * Checks to see if magic_quotes_gpc is enabled, if so we process all the input. Blah
+ * Thanks very much to the guide posted by eZ Systems:
+ * http://talks.php.net/show/php-best-practices/26
+ * ----------------------------------------------------------------------------------
+ */
+
+if (get_magic_quotes_gpc()) {
+    $in = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($k,$v) = each($in)) {
+        foreach ($v as $key => $val) {
+            if (!is_array($val)) {
+                $in[$k][$key] = stripslashes($val);
+                continue;
+            }
+            $in[] =& $in[$k][$key];
+        }
+    }
+    unset($in);
+}
