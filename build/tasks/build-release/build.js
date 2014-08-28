3a6a67db82e3bacfeead0316d5df3de9513cfa68
Remo Laubacher <remo.laubacher@gmail.com>
2014-08-28 22:04:20 +0200

    use optimized autoloader by default

--- build/tasks/build-release/build.js
+++ build/tasks/build-release/build.js
@@ -34,7 +34,7 @@ module.exports = function(grunt, config, parameters, done) {
 						process.stdout.write('done.\n');
 						process.stdout.write('Installng PHP dependencies with Composer... ');
 						exec(
-							'composer install --prefer-dist',
+							'composer install --prefer-dist --optimize-autoloader',
 							{
 								cwd: path.join(workFolder, 'web/concrete')
 							},
