4560d0df2998ee98c93029f06e9a372d606f7814
Evan Cooper <evan@concretecms.com>
2014-08-29 00:32:24 -0700

    formatting tweaks

--- web/concrete/blocks/video/edit.php
+++ web/concrete/blocks/video/edit.php
@@ -26,25 +26,25 @@
         margin-bottom: 20px;
     }
 </style>
-<div class="form-group">
-<label><?=t('Video Files')?></label>
-    <span class="help-text"><?php echo t('Include WebM, Ogg, and MP4 file types for best cross-browser results') ?></span>
-    <div class="input">
-        <div class="input">
+<fieldset>
+    <div class="form-group">
+    <label><?=t('Video Files')?></label>
+        <span class="help-text"><?php echo t('Include WebM, Ogg, and MP4 file types for best cross-browser results') ?></span>
+        <div class="form-group">
             <label><?php echo t('Video Placeholder Image'); ?></label>
             <?=$al->image('ccm-b-poster-file', 'posterfID', t('Choose Video Placeholder Image (Optional)'), $poster);?>
         </div>
-        <label>WebM</label>
-        <?=$al->video('ccm-b-webm-file', 'webmfID', t('Choose WebM Video File'), $webm);?>
-    </div>
-    <div class="input">
-        <label>Ogg</label>
-        <?=$al->video('ccm-b-ogg-file', 'oggfID', t('Choose Video File'), $ogg);?>
-    </div>
-    <div class="input">
-        <label>MP4</label>
-        <?=$al->video('ccm-b-mp4-file', 'mp4fID', t('Choose Video File'), $mp4);?>
-    </div>
-</div>
-
+        <div class="form-group">
+            <label>WebM</label>
+            <?=$al->video('ccm-b-webm-file', 'webmfID', t('Choose WebM Video File'), $webm);?>
+        </div>
+        <div class="form-group">
+            <label>Ogg</label>
+            <?=$al->video('ccm-b-ogg-file', 'oggfID', t('Choose Video File'), $ogg);?>
+        </div>
+        <div class="form-group">
+            <label>MP4</label>
+            <?=$al->video('ccm-b-mp4-file', 'mp4fID', t('Choose Video File'), $mp4);?>
+        </div>
+</fieldset>
 <? $this->inc('form_setup_html.php'); ?> 
