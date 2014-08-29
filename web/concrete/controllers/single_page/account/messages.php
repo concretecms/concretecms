e7f4a0040b52d0f5bb87f401608e1ce516dce838
Evan Cooper <evan@concretecms.com>
2014-08-29 13:59:04 -0700

    fixing blank page for account messages page, making it redirect proper, deleting cruft messages page under profile

--- web/concrete/controllers/single_page/account/messages.php
+++ web/concrete/controllers/single_page/account/messages.php
@@ -1,8 +1,8 @@
-<?
+<?php
 namespace Concrete\Controller\SinglePage\Account;
 use \Concrete\Core\Page\Controller\AccountPageController;
 
-class Message extends AccountPageController {
+class Messages extends AccountPageController {
 	
 	public function view() {
 		$this->redirect('/account/messages/inbox');
