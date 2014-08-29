4986dc268abb44cf132178457c85e902cd4e3c39
Evan Cooper <evan@concretecms.com>
2014-08-29 00:22:52 -0700

    Video block overhaul. Kept the old flash stuff since we need the fallback anyway. Still need to check it on IE8 on windows. Fingers crossedgit add ../web/concrete/blocks/video/edit.phpgit add ../web/concrete/blocks/video/edit.php

--- web/concrete/blocks/video/add.php
+++ web/concrete/blocks/video/add.php
@@ -6,10 +6,24 @@ $includeAssetLibrary = true;
 $al = Loader::helper('concrete/asset_library');
 ?>
 <div class="form-group">
-<label><?=t('Video File')?></label>
-<div class="input">
-	<?=$al->video('ccm-b-flv-file', 'fID', t('Choose Video File') );?>
-</div>
+    <label><?=t('Video Files')?></label>
+    <span style="color: #ddd"><?php echo t('Include WebM, Ogg, and MP4 file types for best cross-browser results') ?></span>
+    <div class="input">
+        <label><?php echo t('Video Placeholder Image'); ?></label>
+        <?=$al->image('ccm-b-poster-file', 'posterfID', t('Choose Video Placeholder Image (Optional)'));?>
+    </div>
+    <div class="input">
+        <label>WebM</label>
+        <?=$al->video('ccm-b-webm-file', 'webmfID', t('Choose Video File'));?>
+    </div>
+    <div class="input">
+        <label>Ogg</label>
+        <?=$al->video('ccm-b-ogg-file', 'oggfID', t('Choose Video File'));?>
+    </div>
+    <div class="input">
+        <label>MP4</label>
+        <?=$al->video('ccm-b-mp4-file', 'mp4fID', t('Choose Video File'));?>
+    </div>
 </div>
 
 <? $this->inc('form_setup_html.php'); ?> 
