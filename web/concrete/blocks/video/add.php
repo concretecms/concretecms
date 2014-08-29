37b9174ca4fdd1bdb9cda8cc042373d162755f8c
Evan Cooper <evan@concretecms.com>
2014-08-29 00:32:24 -0700

    formatting tweaks
    Former-commit-id: 4560d0df2998ee98c93029f06e9a372d606f7814

--- web/concrete/blocks/video/add.php
+++ web/concrete/blocks/video/add.php
@@ -5,25 +5,33 @@ $bObj=$controller;
 $includeAssetLibrary = true;
 $al = Loader::helper('concrete/asset_library');
 ?>
-<div class="form-group">
+<style>
+    .help-text {
+        color: #ddd;
+        display: block;
+        line-height: 1.3em;
+        font-size: 11px;
+        margin-bottom: 20px;
+    }
+</style>
+<fieldset>
     <label><?=t('Video Files')?></label>
-    <span style="color: #ddd"><?php echo t('Include WebM, Ogg, and MP4 file types for best cross-browser results') ?></span>
-    <div class="input">
+    <span class="help-text"><?php echo t('Include WebM, Ogg, and MP4 file types for best cross-browser results') ?></span>
+    <div class="form-group">
         <label><?php echo t('Video Placeholder Image'); ?></label>
         <?=$al->image('ccm-b-poster-file', 'posterfID', t('Choose Video Placeholder Image (Optional)'));?>
     </div>
-    <div class="input">
+    <div class="form-group">
         <label>WebM</label>
         <?=$al->video('ccm-b-webm-file', 'webmfID', t('Choose Video File'));?>
     </div>
-    <div class="input">
+    <div class="form-group">
         <label>Ogg</label>
         <?=$al->video('ccm-b-ogg-file', 'oggfID', t('Choose Video File'));?>
     </div>
-    <div class="input">
+    <div class="form-group">
         <label>MP4</label>
         <?=$al->video('ccm-b-mp4-file', 'mp4fID', t('Choose Video File'));?>
     </div>
-</div>
-
+</fieldset>
 <? $this->inc('form_setup_html.php'); ?> 
