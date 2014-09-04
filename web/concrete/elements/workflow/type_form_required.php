7dd562fa666a64484deae7d70037f17360854724
Evan Cooper <evan@concretecms.com>
2014-09-03 17:01:56 -0700

    end of line, indentation

--- web/concrete/elements/workflow/type_form_required.php
+++ web/concrete/elements/workflow/type_form_required.php
@@ -47,8 +47,8 @@ if ($type->getPackageID() > 0) {
 ?>
 
 <div class="ccm-dashboard-form-actions-wrapper">
-<div class="ccm-dashboard-form-actions">
-	<a href="<?=URL::to('/dashboard/workflow/workflows')?>" class="btn btn-default pull-left"><?=t('Back to List')?></a>
+    <div class="ccm-dashboard-form-actions">
+        <a href="<?=URL::to('/dashboard/workflow/workflows')?>" class="btn btn-default pull-left"><?=t('Back to List')?></a>
 
-</div>
+    </div>
 </div>
