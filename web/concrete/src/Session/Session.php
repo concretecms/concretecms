cef862a40febaa474c101f3fe79e799aba493e1b
Andrew Embler <andrew@concrete5.org>
2014-09-03 17:11:19 -0700

    reformat session
    Former-commit-id: 0183616dbb311a79de820098a250c3666c853a33

--- web/concrete/src/Session/Session.php
+++ web/concrete/src/Session/Session.php
@@ -1,18 +1,21 @@
-<?php 
+<?php
 namespace Concrete\Core\Session;
+
 use \Symfony\Component\HttpFoundation\Session\Session as SymfonySession;
 use \Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use \Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
 use Core;
 
-class Session {
+class Session
+{
 
-	public static function start() {
-		$app = Core::make('app');
-		if ($app->isRunThroughCommandLineInterface()) {
-			$storage = new MockArraySessionStorage();
-		} else {
+    public static function start()
+    {
+        $app = Core::make('app');
+        if ($app->isRunThroughCommandLineInterface()) {
+            $storage = new MockArraySessionStorage();
+        } else {
             if (SESSION_HANDLER == 'database') {
                 $db = \Database::get();
                 $storage = new NativeSessionStorage(array(),
@@ -28,49 +31,50 @@ class Session {
                 $storage = new NativeSessionStorage();
             }
             $options = array(
-				'cookie_path' => DIR_REL . '/',
-				'cookie_lifetime' => 0,
-				'cookie_secure' => false,
-				'cookie_httponly' => true,
-				'gc_maxlifetime' => SESSION_MAX_LIFETIME
-			);
-			if (defined('SESSION_COOKIE_PARAM_PATH') && SESSION_COOKIE_PARAM_PATH) {
-				$options['cookie_path'] = SESSION_COOKIE_PARAM_PATH;
-			}
-			if (defined('SESSION_COOKIE_PARAM_LIFETIME') && SESSION_COOKIE_PARAM_LIFETIME) {
-				$options['cookie_lifetime'] = SESSION_COOKIE_PARAM_LIFETIME;
-			}
-			if (defined('SESSION_COOKIE_PARAM_DOMAIN') && SESSION_COOKIE_PARAM_DOMAIN) {
-				$options['cookie_domain'] = SESSION_COOKIE_PARAM_DOMAIN;
-			}
-			if (defined('SESSION_COOKIE_PARAM_SECURE') && SESSION_COOKIE_PARAM_SECURE) {
-				$options['cookie_secure'] = SESSION_COOKIE_PARAM_SECURE;
-			}
-			if (defined('SESSION_COOKIE_PARAM_HTTPONLY') && SESSION_COOKIE_PARAM_HTTPONLY) {
-				$options['cookie_httponly'] = SESSION_COOKIE_PARAM_HTTPONLY;
-			}
-			$storage->setOptions($options);
-		}
+                'cookie_path' => DIR_REL . '/',
+                'cookie_lifetime' => 0,
+                'cookie_secure' => false,
+                'cookie_httponly' => true,
+                'gc_maxlifetime' => SESSION_MAX_LIFETIME
+            );
+            if (defined('SESSION_COOKIE_PARAM_PATH') && SESSION_COOKIE_PARAM_PATH) {
+                $options['cookie_path'] = SESSION_COOKIE_PARAM_PATH;
+            }
+            if (defined('SESSION_COOKIE_PARAM_LIFETIME') && SESSION_COOKIE_PARAM_LIFETIME) {
+                $options['cookie_lifetime'] = SESSION_COOKIE_PARAM_LIFETIME;
+            }
+            if (defined('SESSION_COOKIE_PARAM_DOMAIN') && SESSION_COOKIE_PARAM_DOMAIN) {
+                $options['cookie_domain'] = SESSION_COOKIE_PARAM_DOMAIN;
+            }
+            if (defined('SESSION_COOKIE_PARAM_SECURE') && SESSION_COOKIE_PARAM_SECURE) {
+                $options['cookie_secure'] = SESSION_COOKIE_PARAM_SECURE;
+            }
+            if (defined('SESSION_COOKIE_PARAM_HTTPONLY') && SESSION_COOKIE_PARAM_HTTPONLY) {
+                $options['cookie_httponly'] = SESSION_COOKIE_PARAM_HTTPONLY;
+            }
+            $storage->setOptions($options);
+        }
 
-		$session = new SymfonySession($storage);
-		$session->setName(SESSION);
+        $session = new SymfonySession($storage);
+        $session->setName(SESSION);
 
-		static::testSessionFixation($session);
-		return $session;
-	}
+        static::testSessionFixation($session);
+        return $session;
+    }
 
-	protected static function testSessionFixation(SymfonySession $session) {
-		$ip = $session->get('CLIENT_REMOTE_ADDR');
-		$agent = $session->get('CLIENT_HTTP_USER_AGENT');
-		if ($ip && $ip != $_SERVER['REMOTE_ADDR'] || $agent && $agent != $_SERVER['HTTP_USER_AGENT']) {
-			$session->invalidate();
-		}
+    protected static function testSessionFixation(SymfonySession $session)
+    {
+        $ip = $session->get('CLIENT_REMOTE_ADDR');
+        $agent = $session->get('CLIENT_HTTP_USER_AGENT');
+        if ($ip && $ip != $_SERVER['REMOTE_ADDR'] || $agent && $agent != $_SERVER['HTTP_USER_AGENT']) {
+            $session->invalidate();
+        }
 
-		if (!$ip && isset($_SERVER['REMOTE_ADDR'])) {
-			$session->set('CLIENT_REMOTE_ADDR', $_SERVER['REMOTE_ADDR']);
-		}
-		if (!$agent && isset($_SERVER['HTTP_USER_AGENT'])) {
-			$session->set('CLIENT_HTTP_USER_AGENT', $_SERVER['HTTP_USER_AGENT']);
-		}
-	}
+        if (!$ip && isset($_SERVER['REMOTE_ADDR'])) {
+            $session->set('CLIENT_REMOTE_ADDR', $_SERVER['REMOTE_ADDR']);
+        }
+        if (!$agent && isset($_SERVER['HTTP_USER_AGENT'])) {
+            $session->set('CLIENT_HTTP_USER_AGENT', $_SERVER['HTTP_USER_AGENT']);
+        }
+    }
 }
