e665731757caea33f88ba6c7aabf90e0edc0e6ad
Korvin Szanto <korvinszanto@gmail.com>
2014-09-04 13:49:14 -0700

    Move globalarea handling under permission check
    Former-commit-id: 8020df7f6ba382b94b06ed4f7c59ceecc4aa6cbc

--- web/concrete/controllers/backend/page/arrange_blocks.php
+++ web/concrete/controllers/backend/page/arrange_blocks.php
@@ -33,6 +33,38 @@ class ArrangeBlocks extends Page
             $affectedAreaIDs[] = $destinationAreaID;
         }
 
+        if (PERMISSIONS_MODEL == 'advanced') {
+            // first, we check to see if we have permissions to edit the area contents for the source area.
+            $arHandle = Area::getAreaHandleFromID($sourceAreaID);
+            $ar = Area::getOrCreate($nvc, $arHandle);
+            $ap = new Permissions($ar);
+            if (!$ap->canEditAreaContents()) {
+                $e->add(t('You may not arrange the contents of area %s.', $arHandle));
+            } else {
+                // now we get further in. We check to see if we're dealing with both a source AND a destination area.
+                // if so, we check the area permissions for the destination area.
+                if ($sourceAreaID != $destinationAreaID) {
+                    $destAreaHandle = Area::getAreaHandleFromID($destinationAreaID);
+                    $destArea = Area::getOrCreate($nvc, $destAreaHandle);
+                    $destAP = new Permissions($destArea);
+                    if (!$destAP->canEditAreaContents()) {
+                        $e->add(t('You may not arrange the contents of area %s.', $destAreaHandle));
+                    } else {
+                        // we're not done yet. Now we have to check to see whether this user has permission to add
+                        // a block of this type to the destination area.
+                        $b = Block::getByID($_REQUEST['block'], $nvc, $arHandle);
+                        $bt = $b->getBlockTypeObject();
+                        if (!$destAP->canAddBlock($bt)) {
+                            $e->add(t('You may not add %s to area %s.', t($bt->getBlockTypeName()), $destAreaHandle));
+                        }
+                    }
+                }
+            }
+
+            // now, if we get down here we perform the arrangement
+            // it will be set to true if we're in simple permissions mode, or if we've passed all the checks
+        }
+
         $source_area = Area::get($nvc, Area::getAreaHandleFromID($sourceAreaID));
         $destination_area = Area::get($this->page, Area::getAreaHandleFromID($destinationAreaID));
 
@@ -70,38 +102,6 @@ class ArrangeBlocks extends Page
                 }
                 $block->move($stack->getVersionToModify(), Area::get($stack, STACKS_AREA_NAME));
             }
-        }
-
-        if (PERMISSIONS_MODEL == 'advanced') {
-            // first, we check to see if we have permissions to edit the area contents for the source area.
-            $arHandle = Area::getAreaHandleFromID($sourceAreaID);
-            $ar = Area::getOrCreate($nvc, $arHandle);
-            $ap = new Permissions($ar);
-            if (!$ap->canEditAreaContents()) {
-                $e->add(t('You may not arrange the contents of area %s.', $arHandle));
-            } else {
-                // now we get further in. We check to see if we're dealing with both a source AND a destination area.
-                // if so, we check the area permissions for the destination area.
-                if ($sourceAreaID != $destinationAreaID) {
-                    $destAreaHandle = Area::getAreaHandleFromID($destinationAreaID);
-                    $destArea = Area::getOrCreate($nvc, $destAreaHandle);
-                    $destAP = new Permissions($destArea);
-                    if (!$destAP->canEditAreaContents()) {
-                        $e->add(t('You may not arrange the contents of area %s.', $destAreaHandle));
-                    } else {
-                        // we're not done yet. Now we have to check to see whether this user has permission to add
-                        // a block of this type to the destination area.
-                        $b = Block::getByID($_REQUEST['block'], $nvc, $arHandle);
-                        $bt = $b->getBlockTypeObject();
-                        if (!$destAP->canAddBlock($bt)) {
-                            $e->add(t('You may not add %s to area %s.', t($bt->getBlockTypeName()), $destAreaHandle));
-                        }
-                    }
-                }
-            }
-
-            // now, if we get down here we perform the arrangement
-            // it will be set to true if we're in simple permissions mode, or if we've passed all the checks
         }
 
         if (!$e->has()) {
