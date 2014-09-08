0f684f03f2e9b00765724247546f70df2ec70563
Andrew Embler <andrew@concrete5.org>
2014-09-08 16:29:39 -0700

    event

---
+++ web/concrete/src/Page/Type/Event.php
@@ -0,0 +1,16 @@
+<?php
+namespace Concrete\Core\Page\Type;
+use Concrete\Core\Page\Event as PageEvent;
+
+class Event extends PageEvent {
+
+	protected $type;
+
+	public function setPageType(Type $type) {
+		$this->type = $type;
+	}
+
+	public function getPageTypeObject() {
+		return $this->type;
+	}
+}
