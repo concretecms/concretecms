ee9b0251144513d580198045bfab8d8ee0c14f39
Korvin Szanto <korvinszanto@gmail.com>
2014-09-08 14:14:47 -0700

    Update symbol generator to minimize phpstorm errors detected.

--- build/libraries/generate-symbols.php
+++ build/libraries/generate-symbols.php
@@ -13,16 +13,12 @@ else {
     define('DIR_BASE', dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'web');
 }
 $corePath = DIR_BASE . '/concrete';
+$cms = require_once $corePath . '/dispatcher.php';
 
-require $corePath . '/bootstrap/configure.php';
-require $corePath . '/bootstrap/autoload.php';
-$cms = require $corePath . '/bootstrap/start.php';
-
-error_reporting('E_ALL');
 $generator = new \Concrete\Core\Support\Symbol\SymbolGenerator();
 
 $symbols = $generator->render();
 file_put_contents(DIR_BASE . '/concrete/src/Support/__IDE_SYMBOLS__.php', $symbols);
-echo "Generation Complete.\n";
-die(0);
+
+die("Generation Complete.\n");
 
