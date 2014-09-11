492df2ff51a761ddc46813c6d8c1219a03eaadbc
Andrew Embler <andrew@concrete5.org>
2014-09-11 07:21:30 -0700

    Fix #951

--- web/concrete/src/File/Search/ColumnSet/Available.php
+++ web/concrete/src/File/Search/ColumnSet/Available.php
@@ -6,6 +6,6 @@ class Available extends DefaultSet {
 	protected $attributeClass = 'FileAttributeKey';
 	public function __construct() {
 		parent::__construct();
-		$this->addColumn(new Column('fvAuthorName', t('Author'), 'getAuthorName'));
+		$this->addColumn(new Column('fvAuthorName', t('Author'), 'getAuthorName', false));
 	}
 }
