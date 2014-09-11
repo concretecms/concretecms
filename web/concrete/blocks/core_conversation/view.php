a5f37c8a6ff2a44609c6a71be0bd1f9a3f78bc33
Christopher Hougard <chris@exchangecore.com>
2014-09-11 02:01:32 -0500

    Tabs to spaces
    Former-commit-id: b5712b717e542c06d7ff68e3ac13438231c521b4

--- web/concrete/blocks/core_conversation/view.php
+++ web/concrete/blocks/core_conversation/view.php
@@ -6,49 +6,49 @@ $iph = Loader::helper('validation/ip');
 $commentRatingIP = ip2long($iph->getRequestIP());
 $u = new User();
 if ($u->isLoggedIn()) {
-	$uID = $u->getUserID();
-	$maxFileSize = $maxFileSizeRegistered;
-	$maxFiles = $maxFilesRegistered;
+    $uID = $u->getUserID();
+    $maxFileSize = $maxFileSizeRegistered;
+    $maxFiles = $maxFilesRegistered;
 }else{
-	$maxFileSize = $maxFileSizeGuest;
-	$maxFiles = $maxFilesGuest;
-	$uID = 0;
+    $maxFileSize = $maxFileSizeGuest;
+    $maxFiles = $maxFilesGuest;
+    $uID = 0;
 }
 
 if (is_object($conversation)) {
-	?>
-	<div class="ccm-conversation-wrapper" data-conversation-id="<?=$conversation->getConversationID()?>">
-	<?=t('Loading Conversation')?> <i class="fa fa-spin fa-circle-o-notch"></i>
-	</div>
+    ?>
+    <div class="ccm-conversation-wrapper" data-conversation-id="<?=$conversation->getConversationID()?>">
+    <?=t('Loading Conversation')?> <i class="fa fa-spin fa-circle-o-notch"></i>
+    </div>
 
-	<script type="text/javascript">
-	$(function() {
-		$('div[data-conversation-id=<?=$conversation->getConversationID()?>]').concreteConversation({
-			cnvID: <?=$conversation->getConversationID()?>,
-			blockID: <?=$bID?>,
-			cID: <?=$cID?>,
-			posttoken: '<?=$posttoken?>',
-			displayMode: '<?=$displayMode?>',
-			addMessageLabel: '<?=$addMessageLabel?>',
-			paginate: <?=$paginate?>,
-			itemsPerPage: <?=$itemsPerPage?>,
-			orderBy: '<?=$orderBy?>',
-			enableOrdering: <?=$enableOrdering?>,
-			displayPostingForm: '<?=$displayPostingForm?>',
-			insertNewMessages: '<?=$insertNewMessages?>',
-			activeUsers: <?=Loader::helper('json')->encode($users)?>,
-			enableCommentRating: <?=$enableCommentRating?>,
-			commentRatingUserID: <?=$uID?>,
-			commentRatingIP: '<?=$commentRatingIP?>',
-			dateFormat: '<?=$dateFormat?>',
+    <script type="text/javascript">
+    $(function() {
+        $('div[data-conversation-id=<?=$conversation->getConversationID()?>]').concreteConversation({
+            cnvID: <?=$conversation->getConversationID()?>,
+            blockID: <?=$bID?>,
+            cID: <?=$cID?>,
+            posttoken: '<?=$posttoken?>',
+            displayMode: '<?=$displayMode?>',
+            addMessageLabel: '<?=$addMessageLabel?>',
+            paginate: <?=$paginate?>,
+            itemsPerPage: <?=$itemsPerPage?>,
+            orderBy: '<?=$orderBy?>',
+            enableOrdering: <?=$enableOrdering?>,
+            displayPostingForm: '<?=$displayPostingForm?>',
+            insertNewMessages: '<?=$insertNewMessages?>',
+            activeUsers: <?=Loader::helper('json')->encode($users)?>,
+            enableCommentRating: <?=$enableCommentRating?>,
+            commentRatingUserID: <?=$uID?>,
+            commentRatingIP: '<?=$commentRatingIP?>',
+            dateFormat: '<?=$dateFormat?>',
             customDateFormat: '<?=$customDateFormat?>',
-			blockAreaHandle: '<?=$blockAreaHandle ?>',
-			fileExtensions: '<?=$fileExtensions?>',
-			maxFileSize: '<?=$maxFileSize?>',
-			maxFiles: '<?=$maxFiles?>',
+            blockAreaHandle: '<?=$blockAreaHandle ?>',
+            fileExtensions: '<?=$fileExtensions?>',
+            maxFileSize: '<?=$maxFileSize?>',
+            maxFiles: '<?=$maxFiles?>',
             attachmentsEnabled: '<?=$attachmentsEnabled?>',
             attachmentOverridesEnabled: '<?=attachmentOverridesEnabled?>'
-		});
-	});
-	</script>
+        });
+    });
+    </script>
 <? } ?>
