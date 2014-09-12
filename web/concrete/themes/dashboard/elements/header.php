2c42e76e2b578387d833695f9cb6b7c83a5a3dee
Andrew Embler <andrew@concrete5.org>
2014-09-11 22:49:21 -0700

    config tweaks

--- web/concrete/themes/dashboard/elements/header.php
+++ web/concrete/themes/dashboard/elements/header.php
@@ -12,7 +12,7 @@ $v = View::getInstance();
 $v->requireAsset('dashboard');
 $v->addFooterItem('<script type="text/javascript" src="' . REL_DIR_FILES_TOOLS_REQUIRED . '/i18n_js"></script>');
 $v->addFooterItem('<script type="text/javascript">$(function() { ConcreteToolbar.start(); });</script>');
-if (Config::get('concrete.misc.enabled_progressive_page_reindex') && Config::get('concrete.misc.do_page_reindex_check')) {
+if (Config::get('concrete.misc.enable_progressive_page_reindex') && Config::get('concrete.misc.do_page_reindex_check')) {
     $v->addFooterItem('<script type="text/javascript">$(function() { ccm_doPageReindexing(); });</script>');
 }
 if (Localization::activeLanguage() != 'en') {
