b0d3e7ec177cdeac1337ace6c5691232ba9286cd
Michele Locati <michele@locati.it>
2014-09-13 19:08:40 +0200

    PSR-2
    Former-commit-id: b926cde4d6b0c0a7f384e4250cf34d1c82eb33ba

--- web/concrete/src/Page/Type/Composer/Control/CorePageProperty/UrlSlugCorePageProperty.php
+++ web/concrete/src/Page/Type/Composer/Control/CorePageProperty/UrlSlugCorePageProperty.php
@@ -1,36 +1,42 @@
 <?php
 namespace Concrete\Core\Page\Type\Composer\Control\CorePageProperty;
+
 use Loader;
 use Page;
-class UrlSlugCorePageProperty extends CorePageProperty {
-	
-	public function __construct() {
-		$this->setCorePagePropertyHandle('url_slug');
-		$this->setPageTypeComposerControlName(t('URL Slug'));
-		$this->setPageTypeComposerControlIconSRC(ASSETS_URL . '/attributes/text/icon.png');
-	}
 
-	public function publishToPage(Page $c, $data, $controls) {
-		$this->addPageTypeComposerControlRequestValue('cHandle', $data['url_slug']);
-		parent::publishToPage($c, $data, $controls);
-	}
+class UrlSlugCorePageProperty extends CorePageProperty
+{
+    public function __construct()
+    {
+        $this->setCorePagePropertyHandle('url_slug');
+        $this->setPageTypeComposerControlName(t('URL Slug'));
+        $this->setPageTypeComposerControlIconSRC(ASSETS_URL . '/attributes/text/icon.png');
+    }
 
-	public function validate() {
-		$e = Loader::helper('validation/error');
-		$handle = $this->getPageTypeComposerControlDraftValue();
-		if (!$handle) {
-			$e->add(t('You must specify a URL slug.'));
-			return $e;
-		}
-	}
+    public function publishToPage(Page $c, $data, $controls)
+    {
+        $this->addPageTypeComposerControlRequestValue('cHandle', $data['url_slug']);
+        parent::publishToPage($c, $data, $controls);
+    }
 
-	public function getPageTypeComposerControlDraftValue() {
-		if (is_object($this->page)) {
-			$c = $this->page;
-			return $c->getCollectionHandle();
-		}
-	}
-	
+    public function validate()
+    {
+        $e = Loader::helper('validation/error');
+        $handle = $this->getPageTypeComposerControlDraftValue();
+        if (!$handle) {
+            $e->add(t('You must specify a URL slug.'));
 
+            return $e;
+        }
+    }
+
+    public function getPageTypeComposerControlDraftValue()
+    {
+        if (is_object($this->page)) {
+            $c = $this->page;
+
+            return $c->getCollectionHandle();
+        }
+    }
 
 }
--- web/concrete/src/Page/Type/Composer/Control/CorePageProperty/UserCorePageProperty.php
+++ web/concrete/src/Page/Type/Composer/Control/CorePageProperty/UserCorePageProperty.php
@@ -1,40 +1,45 @@
 <?php
 namespace Concrete\Core\Page\Type\Composer\Control\CorePageProperty;
+
 use Loader;
 use UserInfo;
 
-class UserCorePageProperty extends CorePageProperty {
-	
-	public function __construct() {
-		$this->setCorePagePropertyHandle('user');
-		$this->setPageTypeComposerControlName(t('User'));
-		$this->setPageTypeComposerControlIconSRC(ASSETS_URL . '/attributes/text/icon.png');
-	}
+class UserCorePageProperty extends CorePageProperty
+{
+    public function __construct()
+    {
+        $this->setCorePagePropertyHandle('user');
+        $this->setPageTypeComposerControlName(t('User'));
+        $this->setPageTypeComposerControlIconSRC(ASSETS_URL . '/attributes/text/icon.png');
+    }
 
-	public function publishToPage(Page $c, $data, $controls) {
-		if (Loader::helper('validation/numbers')->integer($data['user'])) {
-			$this->addPageTypeComposerControlRequestValue('uID', $data['user']);
-		}
-		parent::publishToPage($c, $data, $controls);
-	}
+    public function publishToPage(Page $c, $data, $controls)
+    {
+        if (Loader::helper('validation/numbers')->integer($data['user'])) {
+            $this->addPageTypeComposerControlRequestValue('uID', $data['user']);
+        }
+        parent::publishToPage($c, $data, $controls);
+    }
 
-	public function validate() {
-		$uID = $this->getPageTypeComposerControlDraftValue();
-		$ux = UserInfo::getByID($uID);
-		$e = Loader::helper('validation/error');
-		if (!is_object($ux)) {
-			$e->add(t('You must specify a valid user.'));
-			return $e;
-		}
-	}
+    public function validate()
+    {
+        $uID = $this->getPageTypeComposerControlDraftValue();
+        $ux = UserInfo::getByID($uID);
+        $e = Loader::helper('validation/error');
+        if (!is_object($ux)) {
+            $e->add(t('You must specify a valid user.'));
 
-	public function getPageTypeComposerControlDraftValue() {
-		if (is_object($this->page)) {
-			$c = $this->page;
-			return $c->getCollectionUserID();
-		}
-	}
-	
+            return $e;
+        }
+    }
 
+    public function getPageTypeComposerControlDraftValue()
+    {
+        if (is_object($this->page)) {
+            $c = $this->page;
+
+            return $c->getCollectionUserID();
+        }
+    }
 
 }
