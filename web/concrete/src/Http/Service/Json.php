377d33b3c6ed0271293b4e07396e4fda196979a5
Michele Locati <michele@locati.it>
2014-09-14 19:48:52 +0200

    Fix json_encode/json_decode parameters

--- web/concrete/src/Http/Service/Json.php
+++ web/concrete/src/Http/Service/Json.php
@@ -23,26 +23,34 @@ class Json
 	 * @param string $string
 	 * @param bool $assoc [default: false] When true, returned objects will be converted into associative arrays, when false they'll be converted into stdClass instances.
 	 * @param int $depth [default: 512] User specified recursion depth.
-	 * @param int $options [default: 0] Bitmask of JSON decode options.
+	 * @param int $options [default: 0] Bitmask of JSON decode options (used from PHP 5.4).
 	 * @return mixed
 	 * @link http://php.net/manual/function.json-decode.php
 	 */
     public function decode($string, $assoc = false, $depth = 512, $options = 0)
     {
-        return json_decode($string, $assoc, $depth, $options);
+        if ($options && (version_compare(PHP_VERSION, '5.4') >= 0)) {
+            return json_decode($string, $assoc, $depth, $options);
+        } else {
+            return json_decode($string, $assoc, $depth);
+        }
     }
 
     /**
 	 * Encodes a data structure into a JSON string
 	 * @param mixed $mixed
 	 * @param int $options [default: 0] Bitmask of JSON encode options.
-	 * @param int $depth [default: 512] User specified recursion depth.
+	 * @param int $depth [default: 512] User specified recursion depth (used from PHP 5.5).
 	 * @return string
 	 * @link http://php.net/manual/function.json-encode.php
 	 */
     public function encode($mixed, $options = 0, $depth = 512)
     {
-        return json_encode($mixed, $options, $depth);
+        if (($depth != 512) && (version_compare(PHP_VERSION, '5.5') >= 0)) {
+            return json_encode($mixed, $options, $depth);
+        } else {
+            return json_encode($mixed, $options);
+        }
     }
 
 }
