1a2e1886f33e05a49ac337d79cd6748a87c63587
Michele Locati <michele@locati.it>
2014-09-14 20:02:14 +0200

    PSR-2
    Former-commit-id: 6d8b9f77d144c37192bb125cf2c73e6121b13490

--- web/concrete/src/Http/Service/Json.php
+++ web/concrete/src/Http/Service/Json.php
@@ -19,14 +19,14 @@ namespace Concrete\Core\Http\Service;
 class Json
 {
     /**
-	 * Decodes a JSON string into a php variable
-	 * @param string $string
-	 * @param bool $assoc [default: false] When true, returned objects will be converted into associative arrays, when false they'll be converted into stdClass instances.
-	 * @param int $depth [default: 512] User specified recursion depth.
-	 * @param int $options [default: 0] Bitmask of JSON decode options (used from PHP 5.4).
-	 * @return mixed
-	 * @link http://php.net/manual/function.json-decode.php
-	 */
+     * Decodes a JSON string into a php variable
+     * @param string $string
+     * @param bool $assoc [default: false] When true, returned objects will be converted into associative arrays, when false they'll be converted into stdClass instances.
+     * @param int $depth [default: 512] User specified recursion depth.
+     * @param int $options [default: 0] Bitmask of JSON decode options (used from PHP 5.4).
+     * @return mixed
+     * @link http://php.net/manual/function.json-decode.php
+     */
     public function decode($string, $assoc = false, $depth = 512, $options = 0)
     {
         if ($options && (version_compare(PHP_VERSION, '5.4') >= 0)) {
@@ -37,13 +37,13 @@ class Json
     }
 
     /**
-	 * Encodes a data structure into a JSON string
-	 * @param mixed $mixed
-	 * @param int $options [default: 0] Bitmask of JSON encode options.
-	 * @param int $depth [default: 512] User specified recursion depth (used from PHP 5.5).
-	 * @return string
-	 * @link http://php.net/manual/function.json-encode.php
-	 */
+     * Encodes a data structure into a JSON string
+     * @param mixed $mixed
+     * @param int $options [default: 0] Bitmask of JSON encode options.
+     * @param int $depth [default: 512] User specified recursion depth (used from PHP 5.5).
+     * @return string
+     * @link http://php.net/manual/function.json-encode.php
+     */
     public function encode($mixed, $options = 0, $depth = 512)
     {
         if (($depth != 512) && (version_compare(PHP_VERSION, '5.5') >= 0)) {
