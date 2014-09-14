c9c666330b12575cd8b11833c1c73060a44f2d4d
Remo Laubacher <remo.laubacher@gmail.com>
2014-09-14 09:36:00 +0200

    add some json encode/decode options
    Former-commit-id: 8a5087b37c2274c84378cff04cd4817c7091b5da

--- web/concrete/src/Http/Service/Json.php
+++ web/concrete/src/Http/Service/Json.php
@@ -22,20 +22,24 @@ class Json {
 	 * Decodes a JSON string into a php variable
 	 * @param string $string
 	 * @param bool $assoc [default: false] When true, returned objects will be converted into associative arrays, when false they'll be converted into stdClass instances.
+	 * @param int $depth [default: 512] User specified recursion depth.
+	 * @param int $options [default: 0] Bitmask of JSON decode options.
 	 * @return mixed
 	 */
-	public function decode($string, $assoc = false) {
-		return json_decode($string, $assoc);
+	public function decode($string, $assoc = false, $depth = 512, $options = 0) {
+		return json_decode($string, $assoc, $depth, $options);
 	}
 
 
 	/**
 	 * Encodes a data structure into a JSON string
 	 * @param mixed $mixed
+	 * @param int $options [default: 0] Bitmask of JSON encode options.
+	 * @param int $depth [default: 512] User specified recursion depth.
 	 * @return string
 	 */
-	public function encode($mixed) {
-		return json_encode($mixed);
+	public function encode($mixed, $options = 0, $depth = 512) {
+		return json_encode($mixed, $options, $depth);
 	}
 
 
