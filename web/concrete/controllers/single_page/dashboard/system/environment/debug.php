40c48b07d4dc4e40a167fc4465953d0378cd58fe
Korvin Szanto <korvinszanto@gmail.com>
2014-09-18 11:06:31 -0700

    Reformat debug DashboardPageController

--- web/concrete/controllers/single_page/dashboard/system/environment/debug.php
+++ web/concrete/controllers/single_page/dashboard/system/environment/debug.php
@@ -1,32 +1,36 @@
-<?
+<?php
 namespace Concrete\Controller\SinglePage\Dashboard\System\Environment;
-use \Concrete\Core\Page\Controller\DashboardPageController;
+
+use Concrete\Core\Page\Controller\DashboardPageController;
 use Config;
-use Loader;
 
-class Debug extends DashboardPageController {
+class Debug extends DashboardPageController
+{
 
-	public function view() {
+    public function view()
+    {
 
-		$debug_level = Config::get('concrete.debug.level');
-		$this->set('debug_level', $debug_level);
-	}
+        $debug_level = Config::get('concrete.debug.level');
+        $this->set('debug_level', $debug_level);
+    }
 
-	public function update_debug() {
-		if ($this->token->validate("update_debug")) {
-			if ($this->isPost()) {
-				Config::save('concrete.debug.level', $this->post('debug_level'));
-				$this->redirect('/dashboard/system/environment/debug', 'debug_saved');
+    public function update_debug()
+    {
+        if ($this->token->validate("update_debug")) {
+            if ($this->isPost()) {
+                Config::save('concrete.debug.level', $this->post('debug_level'));
+                $this->redirect('/dashboard/system/environment/debug', 'debug_saved');
 
-			}
-		} else {
-			$this->set('error', array($this->token->getErrorMessage()));
-		}
-	}
+            }
+        } else {
+            $this->set('error', array($this->token->getErrorMessage()));
+        }
+    }
 
-	public function debug_saved(){
-		$this->set('message', t('Debug configuration saved.'));
-		$this->view();
-	}
+    public function debug_saved()
+    {
+        $this->set('message', t('Debug configuration saved.'));
+        $this->view();
+    }
 
 }
