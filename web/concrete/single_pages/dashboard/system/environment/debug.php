aa2f2da2039db12bd691221afe828a7f27596642
Korvin Szanto <korvinszanto@gmail.com>
2014-09-18 11:11:38 -0700

    Reformat debug single page
    Former-commit-id: d377df86f5a427121bd746514ba80dc8495bec44

--- web/concrete/single_pages/dashboard/system/environment/debug.php
+++ web/concrete/single_pages/dashboard/system/environment/debug.php
@@ -1,28 +1,32 @@
-<? defined('C5_EXECUTE') or die("Access Denied.");?>
+<?php defined('C5_EXECUTE') or die("Access Denied.") ?>
 
-<form method="post" id="debug-form" action="<?php echo $view->url('/dashboard/system/environment/debug', 'update_debug')?>">
-<?php echo $this->controller->token->output('update_debug')?>
-	
+<form method="post" id="debug-form"
+      action="<?php echo $view->url('/dashboard/system/environment/debug', 'update_debug') ?>">
+    <?php echo $this->controller->token->output('update_debug') ?>
 
     <div class="form-group">
         <div class="radio">
             <label>
-                <input type="radio" name="debug_level" value="<?php echo DEBUG_DISPLAY_PRODUCTION?>" <?php  if ($debug_level == DEBUG_DISPLAY_PRODUCTION) { ?> checked <?php  } ?> /> <span><?php echo t('Hide errors from site visitors.')?> </span>
+                <input type="radio" name="debug_level"
+                       value="<?php echo DEBUG_DISPLAY_PRODUCTION ?>" <?php if ($debug_level == DEBUG_DISPLAY_PRODUCTION) { ?> checked <?php } ?> />
+                <span><?php echo t('Hide errors from site visitors.') ?> </span>
             </label>
         </div>
         <div class="radio">
             <label>
-                <input type="radio" name="debug_level" value="<?php echo DEBUG_DISPLAY_ERRORS?>" <?php  if ($debug_level == DEBUG_DISPLAY_ERRORS) { ?> checked <?php  } ?> /> <span><?php echo t('Show errors in page.')?></span>
+                <input type="radio" name="debug_level"
+                       value="<?php echo DEBUG_DISPLAY_ERRORS ?>" <?php if ($debug_level == DEBUG_DISPLAY_ERRORS) { ?> checked <?php } ?> />
+                <span><?php echo t('Show errors in page.') ?></span>
             </label>
         </div>
     </div>
-	
 
-<div class="ccm-dashboard-form-actions-wrapper">
-    <div class="ccm-dashboard-form-actions">
-        <?
-        print $interface->submit(t('Save'), 'debug-form', 'right','btn-primary');
-        ?>
+    <div class="ccm-dashboard-form-actions-wrapper">
+        <div class="ccm-dashboard-form-actions">
+            <?
+            print $interface->submit(t('Save'), 'debug-form', 'right', 'btn-primary');
+            ?>
+        </div>
     </div>
-</div>
+
 </form>
