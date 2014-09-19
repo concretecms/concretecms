5c9108385451cd959b00f70d505613dbb2c4bf45
Andrew Embler <andrew@concrete5.org>
2014-09-19 11:31:41 -0700

    Fix #1147
    Former-commit-id: 6d7455dc56288b48954093567a8e812dfd86411a

---
+++ tests/tests/Asset/AssetTest.php
@@ -0,0 +1,35 @@
+<?php
+use Concrete\Core\Asset\Asset;
+
+class AssetTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testPackageAssetURLs()
+    {
+
+        $al = AssetList::getInstance();
+        $al->register(
+            'css', 'test-css', 'css/awesome.css'
+        );
+
+        $al->register('javascript', 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', array('local' => false));
+
+        $pkg = new Package();
+        $pkg->pkgHandle = 'testing_package';
+        $al->register(
+            'javascript', 'testing/tab', 'blocks/testing_block/js/tab.js',
+            array('version' => '3.2.0', 'position' => Asset::ASSET_POSITION_HEADER, 'minify' => false, 'combine' => false), $pkg
+        );
+
+        $asset1 = $al->getAsset('css', 'test-css');
+        $asset2 = $al->getAsset('javascript', 'testing/tab');
+        $asset3 = $al->getAsset('javascript', 'jquery');
+        $this->assertEquals('/concrete/css/awesome.css', $asset1->getAssetURL());
+        $this->assertEquals('/packages/testing_package/blocks/testing_block/js/tab.js', $asset2->getAssetURL());
+        $this->assertTrue($asset2->isAssetLocal());
+        $this->assertFalse($asset3->isAssetLocal());
+        $this->assertEquals('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', $asset3->getAssetURL());
+
+    }
+
+}
