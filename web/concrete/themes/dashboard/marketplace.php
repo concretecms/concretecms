f068aaa5ce43732d3935e7013734279aadc2f82f
Andrew Embler <andrew@concrete5.org>
2014-09-26 16:02:41 -0700

    more work on marketplace

--- web/concrete/themes/dashboard/marketplace.php
+++ web/concrete/themes/dashboard/marketplace.php
@@ -10,7 +10,12 @@ $this->inc('elements/header.php'); ?>
         </nav>
     </div>
     <div class="ccm-marketplace-search">
-
+    	<?=$form->select('marketplaceRemoteItemSetID', $sets, $selectedSet, array('style' => 'width: 150px'))?>
+        <div class="ccm-marketplace-search-input">
+            <i class="fa fa-search"></i>
+            <input type="search" name="keywords" value="<?=$keywords?>" />
+        </div>
+        <a href="" class="ccm-marketplace-search-advanced"><?=t('Advanced Search')?></a>
     </div>
 </header>
 
