7fba62cda9cbe7d7dafabcc3d336aa8e43a7824b
Andrew Embler <andrew@concrete5.org>
2014-10-02 14:12:17 -0700

    more work on theme detail page, screenshots, slideshow
    Former-commit-id: 8d45bc1779d9413b2f9d40880880901423d68d82

--- web/concrete/controllers/single_page/dashboard/extend/themes.php
+++ web/concrete/controllers/single_page/dashboard/extend/themes.php
@@ -10,6 +10,15 @@ class Themes extends MarketplaceDashboardPageController {
         return 'themes';
     }
 
+    public function view_detail($mpID = null)
+    {
+        parent::view_detail($mpID);
+        $html = \Core::make('helper/html');
+        $this->addFooterItem($html->javascript(ASSETS_URL . '/' . DIRNAME_BLOCKS . '/image_slider/view.js'));
+        $this->addHeaderItem($html->css(ASSETS_URL . '/' . DIRNAME_BLOCKS . '/image_slider/view.css'));
+    }
+
+
     public function getMarketplaceDefaultHeading()
     {
         return t('Themes');
--- web/concrete/single_pages/dashboard/extend/themes.php
+++ web/concrete/single_pages/dashboard/extend/themes.php
@@ -3,6 +3,80 @@ defined('C5_EXECUTE') or die("Access Denied.");
 if ($controller->getTask() == 'view_detail') { ?>
 
 
+    <div class="ccm-marketplace-detail-theme-slideshow-wrapper">
+    <div class="ccm-marketplace-detail-theme-slideshow">
+        <?
+        $screenshots = $item->getSlideshow();
+        $detailShots = $item->getScreenshots();
+        ?>
+        <ul data-slideshow="marketplace-theme">
+            <?php foreach($screenshots as $i => $image) {
+                $detail = $detailShots[$i]; ?>
+                <li><a href="<?=$detail->src?>"><img src="<?=$image->src?>" /></a></li>
+            <? } ?>
+        </ul>
+    </div>
+        <div class="ccm-marketplace-detail-theme-slideshow-nav">
+            <nav>
+                <li><a href="#" data-navigation="marketplace-slideshow-previous"><i class="fa fa-chevron-left"></i></a></li>
+                <li><a href="#" data-navigation="marketplace-slideshow-next"><i class="fa fa-chevron-right"></i></a></li>
+                <li><a href="#" data-launch="marketplace-slideshow-gallery"><i class="fa fa-image"></i></a></li>
+            </nav>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+    $(function () {
+        $("ul[data-slideshow=marketplace-theme]").responsiveSlides({
+            prevText: "",   // String: Text for the "previous" button
+            nextText: "",
+            nav: true
+        });
+
+        $('a[data-navigation=marketplace-slideshow-previous]').on('click', function(e) {
+            e.preventDefault();
+            $('.rslides_nav.prev').trigger('click');
+        });
+
+        $('a[data-navigation=marketplace-slideshow-next]').on('click', function(e) {
+            e.preventDefault();
+            $('.rslides_nav.next').trigger('click');
+        });
+
+        $('a[data-launch=marketplace-slideshow-gallery]').on('click', function(e) {
+            e.preventDefault();
+            $('ul[data-slideshow=marketplace-theme] li:first-child a').trigger('click');
+        });
+
+        $('ul[data-slideshow=marketplace-theme]').magnificPopup({
+          delegate: 'a',
+          type: 'image',
+          closeOnContentClick: false,
+          closeBtnInside: false,
+          mainClass: 'mfp-zoom-in mfp-img-mobile',
+          image: {
+            verticalFit: true
+          },
+          gallery: {
+            enabled: true
+          },
+          callbacks: {
+              open: function() {
+                $('.mfp-content').addClass('ccm-ui');
+              }
+          },
+          zoom: {
+            enabled: true,
+            duration: 300,
+            opener: function(element) {
+              return element.find('img');
+            }
+          }
+
+        });
+    });
+    </script>
+
 <? } else if (count($items)) { ?>
 
     <? foreach($items as $mi) { ?>
