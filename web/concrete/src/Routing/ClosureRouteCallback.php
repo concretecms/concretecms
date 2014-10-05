3130338c54a8956299a333ce34c0e077fe2b7690
Mike Lay <mike@mkly.io>
2014-10-04 18:04:23 -0700

    Add Parameters to Route Closures
    Use 'HttpKernel\Controller\ControllerResolver::getArguments' to get the
    parameter arguments mapped properly to the closure function arguments
    
    Function arguments must have the same name as the route parameters but
    can be in any order.

--- web/concrete/src/Routing/ClosureRouteCallback.php
+++ web/concrete/src/Routing/ClosureRouteCallback.php
@@ -1,12 +1,16 @@
 <?php
 namespace Concrete\Core\Routing;
+use Symfony\Component\HttpKernel;
 use Response;
 use Request;
+
 class ClosureRouteCallback extends RouteCallback {
 
 	public function execute(Request $request, \Concrete\Core\Routing\Route $route, $parameters) {
+		$resolver = new HttpKernel\Controller\ControllerResolver();
+		$arguments = $resolver->getArguments($request, $this->callback);
 		$r = new Response();
-		$r->setContent($this->callback->__invoke());
+		$r->setContent(call_user_func_array($this->callback, $arguments));
 		return $r;
 	}
 
