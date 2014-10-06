f56ce55b229d19a508b7fb7d36b975d632e46d06
Michael Krasnow <mnkras@gmail.com>
2014-10-05 21:09:49 -0400

    Add in Sign into twitter image.
    @TODO make this look nicer
    
    
    Former-commit-id: 3a20daf29334039b5aaee28d9684c10711758415

--- web/concrete/authentication/twitter/hook.php
+++ web/concrete/authentication/twitter/hook.php
@@ -1,4 +1,4 @@
 <?php
 defined('C5_EXECUTE') or die('Access Denied');
 ?>
-<a href="<?= \URL::to('/system/authentication/twitter/attempt_attach'); ?>"><?php echo t('Login With Twitter')?></a>
+<a href="<?= \URL::to('/system/authentication/twitter/attempt_attach'); ?>"><img src="<?php echo ASSETS_URL_IMAGES.'/authentication/twitter/sign-in-with-twitter-link.png';?>?>" alt="<?php echo t('Login With Twitter')?>"/></a>
