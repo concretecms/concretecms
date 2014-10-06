17d99c14513443847e2b7d366c4289fa3775458d
Michael Krasnow <mnkras@gmail.com>
2014-10-05 20:27:45 -0400

    C5_execute

--- web/concrete/authentication/twitter/form.php
+++ web/concrete/authentication/twitter/form.php
@@ -1,4 +1,5 @@
 <?php
+defined('C5_EXECUTE') or die('Access Denied');
 if (isset($error)) {
     ?>
     <div class="alert alert-danger"><?= $error ?></div>
--- web/concrete/authentication/twitter/hook.php
+++ web/concrete/authentication/twitter/hook.php
@@ -1,1 +1,4 @@
-<a href="<?= \URL::to('/system/authentication/twitter/attempt_attach'); ?>">Login With Twitter</a>
+<?php
+defined('C5_EXECUTE') or die('Access Denied');
+?>
+<a href="<?= \URL::to('/system/authentication/twitter/attempt_attach'); ?>"><?php echo t('Login With Twitter')?></a>
