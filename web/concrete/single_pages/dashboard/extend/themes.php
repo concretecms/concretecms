1a8fb8c64c2a49aa8f704e9195d57e0eaa6de89a
Andrew Embler <andrew@concrete5.org>
2014-10-06 16:15:53 -0700

    more work on offsite marketplace'

--- web/concrete/single_pages/dashboard/extend/themes.php
+++ web/concrete/single_pages/dashboard/extend/themes.php
@@ -30,8 +30,8 @@ if ($controller->getTask() == 'view_detail') { ?>
 
             <div class="ccm-marketplace-detail-theme-buy">
                 <div class="btn-group">
-                    <button class="btn btn-price" style="background-color: #1888d3"><?=$item->getDisplayPrice()?></button>
-                    <button class="btn btn-description"><? if ($item->purchaseRequired()) { ?><?=t('Purchase')?><? } else { ?><?=t('Download')?><? } ?></button>
+                    <button onclick="ConcreteMarketplace.purchaseOrDownload({mpID: <?=$item->getMarketplaceItemID()?>})" class="btn btn-price" style="background-color: #1888d3"><?=$item->getDisplayPrice()?></button>
+                    <button onclick="ConcreteMarketplace.purchaseOrDownload({mpID: <?=$item->getMarketplaceItemID()?>})" class="btn btn-description"><? if ($item->purchaseRequired()) { ?><?=t('Purchase')?><? } else { ?><?=t('Download')?><? } ?></button>
                 </div>
             </div>
         </div>
