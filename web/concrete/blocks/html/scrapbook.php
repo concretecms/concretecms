0bab152167935fd20974abcc65a9835a5a3c366a
Michele Locati <michele@locati.it>
2014-10-07 18:21:41 +0200

    PSR-2

--- web/concrete/blocks/html/scrapbook.php
+++ web/concrete/blocks/html/scrapbook.php
@@ -1,5 +1,5 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <div id="HTMLBlock<?=intval($bID)?>" class="HTMLBlock" style="max-height:300px; overflow:auto">
-<?=Concrete\Block\Html\Controller::xml_highlight(($content)) ?>
+<?php echo Concrete\Block\Html\Controller::xml_highlight(($content)); ?>
 </div>
