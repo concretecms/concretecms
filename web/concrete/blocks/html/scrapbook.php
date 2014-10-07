b940c6d9d387374a81407538b8cdda7114bbdcde
Michele Locati <michele@locati.it>
2014-10-07 18:16:31 +0200

    HtmlBlockController is now Concrete\Block\Html\Controller
    Former-commit-id: e328a70488fba03b04535ea1234fb90c98226d54

--- web/concrete/blocks/html/scrapbook.php
+++ web/concrete/blocks/html/scrapbook.php
@@ -1,5 +1,5 @@
 <? defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <div id="HTMLBlock<?=intval($bID)?>" class="HTMLBlock" style="max-height:300px; overflow:auto">
-<?=HtmlBlockController::xml_highlight(($content)) ?>
+<?=Concrete\Block\Html\Controller::xml_highlight(($content)) ?>
 </div>
