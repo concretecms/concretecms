f934aea707a7c838dbaa5ae20bcc066db0d1b90e
Andrew Embler <andrew@concrete5.org>
2014-10-07 07:51:18 -0700

    cache

--- web/concrete/views/dialogs/block/cache.php
+++ web/concrete/views/dialogs/block/cache.php
@@ -19,6 +19,23 @@ $bp = new Permissions($b);
     </fieldset>
     <? } ?>
 
+    <? if ($bp->canEditBlockCacheSettings()) { ?>
+    <fieldset>
+        <legend><?=t('Caching')?></legend>
+        <? if (!Config::get('concrete.cache.blocks')) { ?>
+            <div class="alert alert-warning"><?=t('Block caching is currently disabled globally. These settings won\'t take affect until block caching is turned on. You can turn these settings on from the <a href="%s">Cache and Speed Settings</a> page in the Dashboard.', URL::to('/dashboard/system/optimization/cache'))?></div>
+        <? } ?>
+
+        <div class="form-group">
+            <label class="control-label" for="bName">
+                <?=t('Block Name')?>
+                <i class="fa fa-question-circle launch-tooltip" title="<?=t('This can be useful when working with a block programmatically. This is rarely used or needed.')?>"></i>
+            </label>
+            <input type="text" class="form-control" name="bName" id="bName" value="<?=$bName?>">
+        </div>
+    </fieldset>
+    <? } ?>
+
     <div class="dialog-buttons">
         <button class="btn btn-default pull-left" data-dialog-action="cancel"><?=t('Cancel')?></button>
         <button type="button" data-dialog-action="submit" class="btn btn-primary pull-right"><?=t('Save')?></button>
