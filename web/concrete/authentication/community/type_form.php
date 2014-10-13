f51e26322522b01cd1d8a7c9107942356d4db983
Korvin Szanto <korvinszanto@gmail.com>
2014-10-13 14:27:52 -0700

    Make community configuration more verbose, fix login js.

--- web/concrete/authentication/community/type_form.php
+++ web/concrete/authentication/community/type_form.php
@@ -1,4 +1,9 @@
 <?php defined('C5_EXECUTE') or die('Access denied.'); ?>
+<div class="alert alert-info">
+    <h4><?php echo t('Community Authentication Configuration'); ?></h4>
+    <p><?php echo t('<a href="%s" target="_blank">Click here</a> to obtain your access keys.', \Config::get('concrete.urls.concrete5_secure') . '/profile/apps/'); ?></p>
+    <p><?php echo t('Set the "Callback URL" to: %s.', '<code>' . BASE_URL . DIR_REL . \URL::to('/system/authentication/community/callback') . '</code>'); ?></p>
+</div>
 
 <div class='form-group'>
     <?=$form->label('apikey', t('App ID'))?>
