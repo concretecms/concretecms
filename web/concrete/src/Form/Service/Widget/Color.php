270ff5d181073f4140d0bfc74e535a470497fae4
Shahroq <shahroq@yahoo.com>
2014-10-13 17:51:52 +0330

    Allow to send option (preferredFormat, rgb or hex) to the method.

--- web/concrete/src/Form/Service/Widget/Color.php
+++ web/concrete/src/Form/Service/Widget/Color.php
@@ -31,7 +31,7 @@ class Color
         $options['showInput'] = true;
         $options['cancelText'] = t('Cancel');
         $options['chooseText'] = t('Choose');
-        $options['preferredFormat'] = 'rgb';
+        $options['preferredFormat'] = isset($options['preferredFormat'])?$options['preferredFormat']:'rgb';
         $options['clearText'] = t('Clear Color Selection');
         $strOptions = json_encode($options);
 
