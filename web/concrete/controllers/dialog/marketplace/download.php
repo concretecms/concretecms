62fc6da83a4f2c4439b77fe71e5571f4dc3edd3d
Andrew Embler <andrew@concrete5.org>
2014-10-13 16:08:20 -0700

    remote marketplace
    Former-commit-id: 87cea5c48f1854b99b18d305785658b177b4386e

---
+++ web/concrete/controllers/dialog/marketplace/download.php
@@ -0,0 +1,14 @@
+<?php
+namespace Concrete\Controller\Dialog\Marketplace;
+
+use Concrete\Controller\Backend\UserInterface\MarketplaceItem;
+
+class Download extends MarketplaceItem
+{
+
+    public function view()
+    {
+
+    }
+
+}
