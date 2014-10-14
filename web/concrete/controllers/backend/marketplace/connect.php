a19cba175ae0c8e2b5f34dd006ac992bcfe15cc4
Andrew Embler <andrew@concrete5.org>
2014-10-14 15:52:07 -0700

    more new marketplace work

--- web/concrete/controllers/backend/marketplace/connect.php
+++ web/concrete/controllers/backend/marketplace/connect.php
@@ -3,23 +3,34 @@ namespace Concrete\Controller\Backend\Marketplace;
 
 use Concrete\Controller\Backend\UserInterface\MarketplaceItem;
 use Concrete\Core\Application\EditResponse;
+use Concrete\Core\Legacy\TaskPermission;
 
 class Connect extends MarketplaceItem
 {
 
     public function view()
     {
-        // we also perform the "does the user need to buy it?" query here to save some requests
-        $r = new EditResponse();
-        $r->setAdditionalDataAttribute('isConnected', $this->marketplace->isConnected());
-        $r->setAdditionalDataAttribute('connectionError', $this->marketplace->getConnectionError());
-        $r->setAdditionalDataAttribute('purchaseRequired', $this->item->purchaseRequired());
-        $r->setAdditionalDataAttribute('remoteURL', $this->item->getRemoteURL());
-        $r->setAdditionalDataAttribute('localURL', $this->item->getLocalURL());
-        if (!$this->item->purchaseRequired()) {
-            $this->item->enableFreeLicense();
+        if ($this->canAccess()) {
+            // we also perform the "does the user need to buy it?" query here to save some requests
+            $r = new EditResponse();
+            $r->setAdditionalDataAttribute('isConnected', $this->marketplace->isConnected());
+            $r->setAdditionalDataAttribute('connectionError', $this->marketplace->getConnectionError());
+            if (is_object($this->item)) {
+                $r->setAdditionalDataAttribute('purchaseRequired', $this->item->purchaseRequired());
+                $r->setAdditionalDataAttribute('remoteURL', $this->item->getRemoteURL());
+                $r->setAdditionalDataAttribute('localURL', $this->item->getLocalURL());
+                if (!$this->item->purchaseRequired()) {
+                    $this->item->enableFreeLicense();
+                }
+            }
+            $r->outputJSON();
         }
-        $r->outputJSON();
+    }
+
+    protected function canAccess()
+    {
+        $tp = new TaskPermission();
+        return $tp->canInstallPackages();
     }
 
 }
