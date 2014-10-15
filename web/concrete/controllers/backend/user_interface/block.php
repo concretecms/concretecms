5ca1f850400e875a4307afeaf4bcf3130d82e12e
Andrew Embler <andrew@concrete5.org>
2014-10-15 13:22:36 -0700

    Fix #1119

--- web/concrete/controllers/backend/user_interface/block.php
+++ web/concrete/controllers/backend/user_interface/block.php
@@ -66,11 +66,42 @@ abstract class Block extends Page
 
         $b->loadNewCollection($nvc);
 
-        //if this block is being changed, make sure it's a new version of the block.
-        if ($b->isAlias()) {
-            $nb = $b->duplicate($nvc);
+        if ($b->getBlockTypeHandle() == BLOCK_HANDLE_SCRAPBOOK_PROXY) {
+            // if we're editing a scrapbook display block, we add a new block in this position for the real block type
+            // set the block to the display order
+            // delete the scrapbook display block, and save the data
+            /*
+            $originalDisplayOrder = $b->getBlockDisplayOrder();
+            $btx = BlockType::getByHandle($_b->getBlockTypeHandle());
+            $nb = $nvc->addBlock($btx, $ax, array());
+            $nb->setAbsoluteBlockDisplayOrder($originalDisplayOrder);
             $b->deleteBlock();
-            $b = $nb;
+            $b = &$nb;
+            */
+
+            $originalDisplayOrder = $b->getBlockDisplayOrder();
+            $cnt = $b->getController();
+            $ob = \Block::getByID($cnt->getOriginalBlockID());
+            $ob->loadNewCollection($nvc);
+            if (!is_object($ax)) {
+                $ax = Area::getOrCreate($cx, $ax);
+            }
+            $ob->setBlockAreaObject($ax);
+            $nb = $ob->duplicate($nvc);
+            $nb->setAbsoluteBlockDisplayOrder($originalDisplayOrder);
+            $b->deleteBlock();
+            $b = & $nb;
+
+        } else {
+            if ($b->isAlias()) {
+
+                // then this means that the block we're updating is an alias. If you update an alias, you're actually going
+                // to duplicate the original block, and update the newly created block. If you update an original, your changes
+                // propagate to the aliases
+                $nb = $b->duplicate($nvc);
+                $b->deleteBlock();
+                $b = $nb;
+            }
         }
 
         return $b;
