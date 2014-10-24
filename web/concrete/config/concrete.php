9c92e9bc119daad23bd08db2626557c9c72e3c67
Andrew Embler <andrew@concrete5.org>
2014-10-24 12:11:08 -0700

    5.7.0 updates
    Former-commit-id: b01e2658509b671bc6cec987d408e80277b06e86

--- web/concrete/config/concrete.php
+++ web/concrete/config/concrete.php
@@ -8,8 +8,8 @@ return array(
      * @var string
      */
     'version'           => '5.7.2',
-    'version_installed' => '5.7.2',
-    'version_db' => '20141024000000', // the key of the latest database migration - corresponds to 5.7.2
+    'version_installed' => '5.7.1.1',
+    'version_db' => '20141023000000', // the key of the latest database migration - corresponds to 5.7.1
 
     /**
      * Installation status
