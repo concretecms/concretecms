d4d90d8e87bae478361d3a025f0667347f833bb8
Korvin Szanto <korvinszanto@gmail.com>
2014-11-04 12:27:52 -0800

    Give ConcreteAlert some handy functionality
    * Click to close
    * Error styles
    * info/error helper methods
    * Optional callback
    * Configurable lifetime
    
    
    Former-commit-id: 387601487676c6a44290ddb5a6bd3dd082972683

--- web/concrete/js/build/core/app/alert.js
+++ web/concrete/js/build/core/app/alert.js
@@ -16,13 +16,33 @@
             });
         },
 
+        info: function(defaults) {
+            var options = $.extend({
+                type: 'info',
+                icon: 'question'
+            }, defaults);
+
+            return this.notify(options);
+        },
+
+        error: function(defaults) {
+            var options = $.extend({
+                type: 'danger',
+                icon: 'times'
+            }, defaults);
+
+            return this.notify(options);
+        },
+
         notify: function(defaults) {
             var options = $.extend({
-                'type': 'success',
-                'icon': 'check',
-                'title': false,
-                'message': false,
-                'appendTo': false,
+                type: 'success',
+                icon: 'check',
+                title: false,
+                message: false,
+                appendTo: false,
+                delay: 2000,
+                callback: function() {}
             }, defaults);
 
             var messageText = '',
@@ -34,24 +54,34 @@
                 messageText = '<h3>' + options.message + '</h3>';
             }
 
-
-            $('<div id="ccm-notification-hud" class="ccm-ui ccm-notification ccm-notification-' + options.type + '"><i class="ccm-notification-icon fa fa-' + options.icon + '"></i><div class="ccm-notification-inner">' + messageText + '</div></div>').
+            var elem = $('<div id="ccm-notification-hud" class="ccm-ui ccm-notification ccm-notification-' + options.type + '"><i class="ccm-notification-icon fa fa-' + options.icon + '"></i><div class="ccm-notification-inner">' + messageText + '</div></div>').
             appendTo($appendTo).delay(5).queue(function() {
                 $(this).addClass('animated fadeIn');
                 $(this).dequeue();
-            }).delay(2000).queue(function() {
-                $(this).css('opacity', 1);
-                $(this).dequeue();
-            }).delay(1).queue(function() {
-                $(this).addClass('animated bounceOutRight');
-                $(this).dequeue();
-            }).delay(1000).queue(function() {
-                $(this).remove();
-                $(this).dequeue();
+            });
+
+            var removeElem = _.once(function() {
+                    elem.queue(function () {
+                        $(this).css('opacity', 1);
+                        $(this).dequeue();
+                    }).delay(1).queue(function () {
+                        $(this).addClass('animated bounceOutRight');
+                        $(this).dequeue();
+                    }).delay(1000).queue(function () {
+                        $(this).remove();
+                        $(this).dequeue();
+                        options.callback();
+                    });
+                }),
+                timeout = setTimeout(removeElem, options.delay);
+
+            elem.click(function() {
+                removeElem();
+                clearTimeout(timeout);
             });
         }
 
-    }
+    };
 
     global.ConcreteAlert = ConcreteAlert;
 
