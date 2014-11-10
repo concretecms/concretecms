8552351b5b99bf06779af5f66b450f2d57f50c87
Andrew Embler <andrew@concrete5.org>
2014-11-10 11:10:10 -0800

    Revert "change redirect path & namespace"
    This reverts commit 1d6b8fb8e1b4751395713932220da61dab5d78e3 [formerly 62efc296f6019320cf744c6e88d8fe4ca0004230].
    
    
    Former-commit-id: 94c745ec065835f00ad9f37cfc891abf0c156452

--- web/concrete/controllers/single_page/members.php
+++ web/concrete/controllers/single_page/members.php
@@ -4,7 +4,7 @@ use \Concrete\Core\Page\Controller\PageController;
 class Members extends PageController {
 	
 	public function view() {
-        $this->redirect('/members/directory');
+        $this->redirect('/');
 	}
 
 }
