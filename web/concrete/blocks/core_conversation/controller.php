6bcc02ac87850ab7d64cab84eb362e62f7bddd13
Chris Hougard <chris@exchangecore.com>
2014-11-23 23:57:31 -0600

    Tabs to spaces

--- web/concrete/blocks/core_conversation/controller.php
+++ web/concrete/blocks/core_conversation/controller.php
@@ -19,48 +19,48 @@ use Page;
  * @license    http://www.concrete5.org/license/     MIT License
  *
  */
-	class Controller extends BlockController implements ConversationFeatureInterface {
+    class Controller extends BlockController implements ConversationFeatureInterface {
 
-		protected $btCacheBlockRecord = true;
-		protected $btTable = 'btCoreConversation';
-		protected $conversation;
-		protected $btWrapperClass = 'ccm-ui';
-		protected $btCopyWhenPropagate = true;
-		protected $btFeatures = array(
-			'conversation'
-		);
+        protected $btCacheBlockRecord = true;
+        protected $btTable = 'btCoreConversation';
+        protected $conversation;
+        protected $btWrapperClass = 'ccm-ui';
+        protected $btCopyWhenPropagate = true;
+        protected $btFeatures = array(
+            'conversation'
+        );
 
-		public function getBlockTypeDescription() {
-			return t("Displays conversations on a page.");
-		}
+        public function getBlockTypeDescription() {
+            return t("Displays conversations on a page.");
+        }
 
-		public function getBlockTypeName() {
-			return t("Conversation");
-		}
+        public function getBlockTypeName() {
+            return t("Conversation");
+        }
 
-		public function getConversationFeatureDetailConversationObject() {
-			return $this->getConversationObject();
-		}
+        public function getConversationFeatureDetailConversationObject() {
+            return $this->getConversationObject();
+        }
 
-		public function getConversationObject() {
-			if (!isset($this->conversation)) {
-				// i don't know why this->cnvid isn't sticky in some cases, leading us to query
-				// every damn time
-				$db = Loader::db();
-				$cnvID = $db->GetOne('select cnvID from btCoreConversation where bID = ?', array($this->bID));
-				$this->conversation = Conversation::getByID($cnvID);
-			}
-			return $this->conversation;
-		}
+        public function getConversationObject() {
+            if (!isset($this->conversation)) {
+                // i don't know why this->cnvid isn't sticky in some cases, leading us to query
+                // every damn time
+                $db = Loader::db();
+                $cnvID = $db->GetOne('select cnvID from btCoreConversation where bID = ?', array($this->bID));
+                $this->conversation = Conversation::getByID($cnvID);
+            }
+            return $this->conversation;
+        }
 
-		public function duplicate_master($newBID, $newPage) {
-			parent::duplicate($newBID);
-			$db = Loader::db();
-			$conv = Conversation::add();
-			$conv->setConversationPageObject($newPage);
-			$this->conversation = $conv;
-			$db->Execute('update btCoreConversation set cnvID = ? where bID = ?', array($conv->getConversationID(), $newBID));
-		}
+        public function duplicate_master($newBID, $newPage) {
+            parent::duplicate($newBID);
+            $db = Loader::db();
+            $conv = Conversation::add();
+            $conv->setConversationPageObject($newPage);
+            $this->conversation = $conv;
+            $db->Execute('update btCoreConversation set cnvID = ? where bID = ?', array($conv->getConversationID(), $newBID));
+        }
 
         public function edit(){
             $fileSettings = $this->getFileSettings();
@@ -78,70 +78,70 @@ use Page;
             $this->requireAsset('core/conversation');
             $this->requireAsset('core/lightbox');
 
-			$fp = FilePermissions::getGlobal();
-			if ($fp->canAccessFileManager()) {
-				$this->requireAsset('core/file-manager');
-			}
-		}
-		public function view() {
-			$fileSettings = $this->getFileSettings();
-			$conversation = $this->getConversationObject();
-			if (is_object($conversation)) {
-				$this->set('conversation', $conversation);
-				if ($this->enablePosting) {
-					$token = Loader::helper('validation/token')->generate('add_conversation_message');
-				} else {
-					$token = '';
-				}
-				$this->set('posttoken', $token);
-				$this->set('cID',Page::getCurrentPage()->getCollectionID());
-				$this->set('users', $this->getActiveUsers(true));
-				$this->set('maxFilesGuest', $fileSettings['maxFilesGuest']);
-				$this->set('maxFilesRegistered', $fileSettings['maxFilesRegistered']);
-				$this->set('maxFileSizeGuest', $fileSettings['maxFileSizeGuest']);
-				$this->set('maxFileSizeRegistered', $fileSettings['maxFileSizeRegistered']);
-				$this->set('fileExtensions', $fileSettings['fileExtensions']);
+            $fp = FilePermissions::getGlobal();
+            if ($fp->canAccessFileManager()) {
+                $this->requireAsset('core/file-manager');
+            }
+        }
+        public function view() {
+            $fileSettings = $this->getFileSettings();
+            $conversation = $this->getConversationObject();
+            if (is_object($conversation)) {
+                $this->set('conversation', $conversation);
+                if ($this->enablePosting) {
+                    $token = Loader::helper('validation/token')->generate('add_conversation_message');
+                } else {
+                    $token = '';
+                }
+                $this->set('posttoken', $token);
+                $this->set('cID',Page::getCurrentPage()->getCollectionID());
+                $this->set('users', $this->getActiveUsers(true));
+                $this->set('maxFilesGuest', $fileSettings['maxFilesGuest']);
+                $this->set('maxFilesRegistered', $fileSettings['maxFilesRegistered']);
+                $this->set('maxFileSizeGuest', $fileSettings['maxFileSizeGuest']);
+                $this->set('maxFileSizeRegistered', $fileSettings['maxFileSizeRegistered']);
+                $this->set('fileExtensions', $fileSettings['fileExtensions']);
                 $this->set('attachmentsEnabled', $fileSettings['attachmentsEnabled']);
                 $this->set('attachmentOverridesEnabled', $fileSettings['attachmentOverridesEnabled']);
-			}
-		}
+            }
+        }
 
-		public function getFileSettings(){
+        public function getFileSettings(){
             $conversation = $this->getConversationObject();
-			$helperFile = Loader::helper('concrete/file');
-			if($conversation->getConversationMaxFilesGuest() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
-				$maxFilesGuest = $conversation->getConversationMaxFilesGuest();
-			} else {
-				$maxFilesGuest = Config::get('conversations.files.guest.max', 3);
-			}
+            $helperFile = Loader::helper('concrete/file');
+            if($conversation->getConversationMaxFilesGuest() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
+                $maxFilesGuest = $conversation->getConversationMaxFilesGuest();
+            } else {
+                $maxFilesGuest = Config::get('conversations.files.guest.max', 3);
+            }
             if($conversation->getConversationAttachmentOverridesEnabled() > 0) {
 
                 $attachmentOverridesEnabled = $conversation->getConversationAttachmentOverridesEnabled();
             }
 
-			if($conversation->getConversationMaxFilesRegistered() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
-				$maxFilesRegistered = $conversation->getConversationMaxFilesRegistered();
-			} else {
-				$maxFilesRegistered = Config::get('conversations.files.registered.max', 6);
-			}
+            if($conversation->getConversationMaxFilesRegistered() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
+                $maxFilesRegistered = $conversation->getConversationMaxFilesRegistered();
+            } else {
+                $maxFilesRegistered = Config::get('conversations.files.registered.max', 6);
+            }
 
-			if($conversation->getConversationMaxFileSizeGuest() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
-				$maxFileSizeGuest = $conversation->getConversationMaxFileSizeGuest();
-			} else {
-				$maxFileSizeGuest = Config::get('conversations.files.guest.max_size', 3);
-			}
+            if($conversation->getConversationMaxFileSizeGuest() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
+                $maxFileSizeGuest = $conversation->getConversationMaxFileSizeGuest();
+            } else {
+                $maxFileSizeGuest = Config::get('conversations.files.guest.max_size', 3);
+            }
 
-			if($conversation->getConversationMaxFileSizeRegistered() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
-				$maxFileSizeRegistered = $conversation->getConversationMaxFileSizeRegistered();
-			} else {
-				$maxFileSizeRegistered = Config::get('conversations.files.registered.max_size', 10);
-			}
+            if($conversation->getConversationMaxFileSizeRegistered() > 0 && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
+                $maxFileSizeRegistered = $conversation->getConversationMaxFileSizeRegistered();
+            } else {
+                $maxFileSizeRegistered = Config::get('conversations.files.registered.max_size', 10);
+            }
 
-			if($conversation->getConversationFileExtensions() && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
-				$fileExtensions = $conversation->getConversationFileExtensions();
-			} else {
-				$fileExtensions = Config::get('conversations.files.allowed_types', '*.jpg;*.png;*.gif;*.doc');
-			}
+            if($conversation->getConversationFileExtensions() && $conversation->getConversationAttachmentOverridesEnabled()  > 0) {
+                $fileExtensions = $conversation->getConversationFileExtensions();
+            } else {
+                $fileExtensions = Config::get('conversations.files.allowed_types', '*.jpg;*.png;*.gif;*.doc');
+            }
 
             if($conversation->getConversationAttachmentOverridesEnabled() > 0) {
                 $attachmentsEnabled = $conversation->getConversationAttachmentsEnabled();
@@ -149,47 +149,47 @@ use Page;
                 $attachmentsEnabled = is_numeric(Config::get('concrete.conversations.attachments_enabled')) ? Config::get('concrete.conversations.attachments_enabled') : 1;
             }
 
-			$fileExtensions = implode(',', $helperFile->unserializeUploadFileExtensions($fileExtensions)); //unserialize and implode extensions into comma separated string
+            $fileExtensions = implode(',', $helperFile->unserializeUploadFileExtensions($fileExtensions)); //unserialize and implode extensions into comma separated string
 
-			$fileSettings = array();
-			$fileSettings['maxFileSizeRegistered'] = $maxFileSizeRegistered;
-			$fileSettings['maxFileSizeGuest'] = $maxFileSizeGuest;
-			$fileSettings['maxFilesGuest'] = $maxFilesGuest;
-			$fileSettings['maxFilesRegistered'] = $maxFilesRegistered;
-			$fileSettings['fileExtensions'] = $fileExtensions;
+            $fileSettings = array();
+            $fileSettings['maxFileSizeRegistered'] = $maxFileSizeRegistered;
+            $fileSettings['maxFileSizeGuest'] = $maxFileSizeGuest;
+            $fileSettings['maxFilesGuest'] = $maxFilesGuest;
+            $fileSettings['maxFilesRegistered'] = $maxFilesRegistered;
+            $fileSettings['fileExtensions'] = $fileExtensions;
             $fileSettings['attachmentsEnabled'] = $attachmentsEnabled;
             $fileSettings['attachmentOverridesEnabled'] = $attachmentOverridesEnabled;
 
-			return $fileSettings;
-		}
+            return $fileSettings;
+        }
 
-		public function getActiveUsers($lower=false) {
-			$cnv = $this->getConversationObject();
-			$uobs = $cnv->getConversationMessageUsers();
-			$users = array();
-			foreach ($uobs as $user) {
-				if ($lower) {
-					$users[] = strtolower($user->getUserName());
-				} else {
-					$users[] = $user->getUserName();
-				}
-			}
-			return $users;
-		}
+        public function getActiveUsers($lower=false) {
+            $cnv = $this->getConversationObject();
+            $uobs = $cnv->getConversationMessageUsers();
+            $users = array();
+            foreach ($uobs as $user) {
+                if ($lower) {
+                    $users[] = strtolower($user->getUserName());
+                } else {
+                    $users[] = $user->getUserName();
+                }
+            }
+            return $users;
+        }
 
-		public function save($post) {
-			$helperFile = Loader::helper('concrete/file');
-			$db = Loader::db();
-			$cnvID = $db->GetOne('select cnvID from btCoreConversation where bID = ?', array($this->bID));
-			if (!$cnvID) {
-				$conversation = Conversation::add();
-				$b = $this->getBlockObject();
-				$xc = $b->getBlockCollectionObject();
-				$conversation->setConversationPageObject($xc);
-			} else {
-				$conversation = Conversation::getByID($cnvID);
-			}
-			$values = $post;
+        public function save($post) {
+            $helperFile = Loader::helper('concrete/file');
+            $db = Loader::db();
+            $cnvID = $db->GetOne('select cnvID from btCoreConversation where bID = ?', array($this->bID));
+            if (!$cnvID) {
+                $conversation = Conversation::add();
+                $b = $this->getBlockObject();
+                $xc = $b->getBlockCollectionObject();
+                $conversation->setConversationPageObject($xc);
+            } else {
+                $conversation = Conversation::getByID($cnvID);
+            }
+            $values = $post;
             if ($values['attachmentOverridesEnabled']) {
                 $conversation->setConversationAttachmentOverridesEnabled(intval($values['attachmentOverridesEnabled']));
             } else {
@@ -198,38 +198,38 @@ use Page;
             if ($values['attachmentsEnabled']) {
                 $conversation->setConversationAttachmentsEnabled(intval($values['attachmentsEnabled']));
             }
-			if (!$values['itemsPerPage']) {
-				$values['itemsPerPage'] = 0;
-			}
-			if ($values['maxFilesGuest']) {
-				$conversation->setConversationMaxFilesGuest(intval($values['maxFilesGuest']));
-			}
-			if ($values['maxFilesRegistered']) {
+            if (!$values['itemsPerPage']) {
+                $values['itemsPerPage'] = 0;
+            }
+            if ($values['maxFilesGuest']) {
+                $conversation->setConversationMaxFilesGuest(intval($values['maxFilesGuest']));
+            }
+            if ($values['maxFilesRegistered']) {
                 $conversation->setConversationMaxFilesRegistered(intval($values['maxFilesRegistered']));
-			}
-			if ($values['maxFileSizeGuest']) {
+            }
+            if ($values['maxFileSizeGuest']) {
                 $conversation->setConversationMaxFileSizeGuest(intval($values['maxFileSizeGuest']));
-			}
-			if ($values['maxFileSizeRegistered']) {
+            }
+            if ($values['maxFileSizeRegistered']) {
                 $conversation->setConversationMaxFilesRegistered(intval($values['maxFileSizeRegistered']));
-			}
-			if (!$values['enableOrdering']) {
-				$values['enableOrdering'] = 0;
-			}
+            }
+            if (!$values['enableOrdering']) {
+                $values['enableOrdering'] = 0;
+            }
             if (!$values['attachmentsEnabled']) {
                 $conversation->setConversationAttachmentsEnabled(intval($values['attachmentsEnabled']));
             }
-			if (!$values['enableCommentRating']) {
-				$values['enableCommentRating'] = 0;
-			}
+            if (!$values['enableCommentRating']) {
+                $values['enableCommentRating'] = 0;
+            }
 
-			if ($values['fileExtensions']) {
-				$receivedExtensions = preg_split('{,}',strtolower($values['fileExtensions']),null,PREG_SPLIT_NO_EMPTY);
-				$conversation->setConversationFileExtensions($helperFile->serializeUploadFileExtensions($receivedExtensions));
-			}
+            if ($values['fileExtensions']) {
+                $receivedExtensions = preg_split('{,}',strtolower($values['fileExtensions']),null,PREG_SPLIT_NO_EMPTY);
+                $conversation->setConversationFileExtensions($helperFile->serializeUploadFileExtensions($receivedExtensions));
+            }
 
-			$values['cnvID'] = $conversation->getConversationID();
-			parent::save($values);
-		}
+            $values['cnvID'] = $conversation->getConversationID();
+            parent::save($values);
+        }
 
-	}
+    }
