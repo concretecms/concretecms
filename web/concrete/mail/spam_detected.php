dd07f31fa10d7552ed14d0d91325061080372b0a
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2014-11-30 13:43:15 +0900

    Delete deprecated SITE constant

--- web/concrete/mail/spam_detected.php
+++ web/concrete/mail/spam_detected.php
@@ -2,7 +2,7 @@
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
-$subject = SITE . " " . t("Notification - Spam Detected");
+$subject = $siteName . " " . t("Notification - Spam Detected");
 $body = t("
 
 Someone has attempted to send you spam through your website. Details below:
