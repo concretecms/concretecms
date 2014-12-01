3a3c23fcc6d70fad77b7b248952ac518343fdcd8
Korvin Szanto <korvinszanto@gmail.com>
2014-12-01 14:16:52 -0800

    Fix invalid array error
    Former-commit-id: 64c073525e4c8132a11164338ebf30a569d56f72

--- web/concrete/src/Authentication/Type/OAuth/OAuth2/GenericOauth2TypeController.php
+++ web/concrete/src/Authentication/Type/OAuth/OAuth2/GenericOauth2TypeController.php
@@ -53,7 +53,7 @@ abstract class GenericOauth2TypeController extends GenericOauthTypeController
             } catch (Exception $e) {
                 $this->showError($e->getMessage());
             } catch (\Exception $e) {
-                \Log::addError($e->getMessage(), 'Oauth Error');
+                \Log::addError($e->getMessage(), array('Oauth Error'));
                 $this->showError('An unexpected error occurred.');
             }
         } else {
