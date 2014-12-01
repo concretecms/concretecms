53e098b70e1bac9aa733499bec13e52632b09e20
Korvin Szanto <korvinszanto@gmail.com>
2014-12-01 13:41:05 -0800

    Style google to match google branding guidelines

--- web/concrete/authentication/google/hook.php
+++ web/concrete/authentication/google/hook.php
@@ -2,23 +2,30 @@
 
 <div class="form-group">
         <span>
-            <?= t('Attach a %s account', t('google')) ?>
+            <?= t('Attach a %s account', t('Google')) ?>
         </span>
     <hr>
 </div>
 <div class="form-group">
     <a href="<?= \URL::to('/system/authentication/google/attempt_attach'); ?>" class="btn btn-primary btn-google">
         <i class="fa fa-google"></i>
-        <?= t('Attach a %s account', t('google')) ?>
+        <?= t('Attach a %s account', t('Google')) ?>
     </a>
 </div>
 
 <style>
-    .ccm-ui .btn-facebook {
+    .ccm-ui .btn-google {
         border-width: 0px;
-        background: #3b5998;
+        background: #dd4b39;
     }
-    .btn-facebook .fa-facebook {
+    .ccm-ui .btn-google:hover {
+        background: #f04f3d;
+    }
+    .ccm-ui .btn-google:active {
+        background: #c74433;
+    }
+
+    .btn-google .fa-google {
         margin: 0 6px 0 3px;
     }
 </style>
