e791777539182ab0fa88a31ee929c7023432803c
Korvin Szanto <korvinszanto@gmail.com>
2014-12-11 15:52:47 -0800

    Set cookies on redirectresponse, return cookie from set
    Former-commit-id: 20d7984ee234abe37124666d858f125b33d18d09

--- web/concrete/src/Routing/RedirectResponse.php
+++ web/concrete/src/Routing/RedirectResponse.php
@@ -1,15 +1,27 @@
-<?php 
+<?php
 namespace Concrete\Core\Routing;
-use Loader;
+
 use Request;
-class RedirectResponse extends \Symfony\Component\HttpFoundation\RedirectResponse {
 
-	protected $request;
+class RedirectResponse extends \Symfony\Component\HttpFoundation\RedirectResponse
+{
 
-	public function setRequest(Request $r) {
-		$this->request = $r;
-	}
+    protected $request;
 
+    public function setRequest(Request $r)
+    {
+        $this->request = $r;
+    }
 
+    public function send()
+    {
+        // First, we see if we have any cookies to send along
+        $cl = \Cookie::getInstance();
+        $cookies = $cl->getCookies();
+        foreach ($cookies as $cookie) {
+            $this->headers->setCookie($cookie);
+        }
+        parent::send();
+    }
 
 }
