5c292ec682d16c588bfbe5cc4b13a1f5a1556ba9
Dan Klassen <dan@triplei.ca>
2014-12-15 17:10:56 -0800

    allow for Application/src directory autoloading
    This would allows for the following:
    
    $test = new Application\Core\Folder\File();
    
    which would autoload the File class from
    
    application/src/Folder/File.php
    
    Anything outside of the Application\Core namespace would be left alone.

--- web/application/bootstrap/autoload.php
+++ web/application/bootstrap/autoload.php
@@ -8,3 +8,25 @@
 if (!@include(DIR_BASE_CORE . '/' . DIRNAME_VENDOR . '/autoload.php')) {
     die('Third party libraries not installed. Make sure that composer has required libraries in the concrete/ directory.');
 }
+
+/**
+* autoload Application\Core namespaced classes from the application/src/ directory
+*/
+spl_autoload_register(function($class){
+    $prefix = "Application\\Core";
+    // does the class use the Application\Core namespace prefix?
+    $len = strlen($prefix);
+
+    if (strncmp($prefix, $class, $len) !== 0) { // check if this is in our namespace or not
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $base_dir = DIR_APPLICATION . "/src";
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
