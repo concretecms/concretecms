731c99dda676307d3e48885bd33f0691096f38e8
Chris Hougard <chris@exchangecore.com>
2014-12-16 01:29:25 -0600

    Removed extra loop

--- web/concrete/src/Support/ServiceProvider/MetadataGenerator.php
+++ web/concrete/src/Support/ServiceProvider/MetadataGenerator.php
@@ -8,26 +8,23 @@ class MetadataGenerator
 {
     public function render()
     {
-        $methodTypes = array();
+        $file = '<?php namespace PHPSTORM_META { $STATIC_METHOD_TYPES = array(\\Core::make(\'\') => array(';
 
         $bindings = Core::getBindings();
         foreach ($bindings as $name => $binding) {
             /** @var \Closure $binding */
             $reflection = new \ReflectionFunction($binding['concrete']);
             $static = $reflection->getStaticVariables();
+            $className = null;
             if (!isset($static['concrete'])) {
                 try {
                     $class = Core::make($name);
-                    $methodTypes[$name] = get_class($class);
+                    $className = get_class($class);
                 } catch (\Exception $e) {}
             } else {
-                $methodTypes[$name] = $static['concrete'];
+                $className = $static['concrete'];
             }
-        }
-
-        $file = '<?php namespace PHPSTORM_META { $STATIC_METHOD_TYPES = array(\\Core::make(\'\') => array(';
-        foreach ($methodTypes as $name => $class) {
-            $file .= '\'' . $name . '\' instanceof ' . $class . ',';
+            $file .= '\'' . $name . '\' instanceof ' . $className . ',';
         }
         $file .= '));}';
 
