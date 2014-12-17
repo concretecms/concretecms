6375cd89135a8b14c90fd275d6f84a0a68f08911
Andrew Embler <andrew@concrete5.org>
2014-12-16 16:07:35 -0800

    more work on po editor and extractor

--- web/concrete/src/Multilingual/Service/Extractor.php
+++ web/concrete/src/Multilingual/Service/Extractor.php
@@ -1,11 +1,28 @@
 <?php
 
 namespace Concrete\Core\Multilingual\Service;
+use Concrete\Core\Multilingual\Page\Section;
+use Gettext\Extractors\Po;
+use Gettext\Translations;
+use Gettext\Extractors\PhpCode;
+use Gettext\Generators\Po as PoGenerator;
+use Gettext\Generators\Mo as MoGenerator;
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
 class Extractor
 {
+    public function __construct()
+    {
+        // register concrete5 translator functions
+        PhpCode::$functions['t'] = '__';
+        PhpCode::$functions['t2'] = 'n__';
+        PhpCode::$functions['tc'] = 'p__';
+    }
+
+    /**
+     * return \GetText\Translations $translations;
+     */
     public function extractTranslatableSiteStrings()
     {
         // first, we figure out which files we're going to operate on.
@@ -30,7 +47,67 @@ class Extractor
             }
         }
 
-        
+        $translations = new Translations();
+        foreach($files as $file) {
+            $fileTranslations = Translations::fromPhpCodeFile($file);
+            $translations->mergeWith($fileTranslations);
+        }
 
+        return $translations;
     }
+
+    public function mergeTranslationsWithSectionFile(Section $section, Translations $translations)
+    {
+        $file = DIR_LANGUAGES_SITE_INTERFACE . '/' . $section->getLocale() . '.po';
+        if (file_exists($file)) {
+            $sectionTranslations = Po::fromFile($file);
+            $translations->mergeWith($sectionTranslations, Translations::MERGE_HEADERS | Translations::MERGE_COMMENTS);
+        }
+        return $translations;
+    }
+
+    public function saveSectionTranslationsToFile(Section $section, Translations $translations)
+    {
+        $po = DIR_LANGUAGES_SITE_INTERFACE . '/' . $section->getLocale() . '.po';
+        $mo = DIR_LANGUAGES_SITE_INTERFACE . '/' . $section->getLocale() . '.mo';
+
+        PoGenerator::toFile($translations, $po);
+        MoGenerator::toFile($translations, $mo);
+    }
+
+    public function saveSectionTranslationsToDatabase(Section $section, Translations $translations)
+    {
+        $db = \Database::get();
+        $db->delete('MultilingualTranslations', array('mtSectionID' => $section->getCollectionID()));
+        foreach($translations as $translation) {
+            $comments = implode(':', $translation->getComments());
+            $references = $translation->getReferences();
+            $refs = '';
+            foreach($references as $reference) {
+                $refs = implode(':', $reference) . "\n";
+            }
+            $db->insert('MultilingualTranslations', array(
+               'mtSectionID' => $section->getCollectionID(),
+                'msgid' => $translation->getOriginal(),
+                'msgstr' => $translation->getTranslation(),
+                'context' => $translation->getContext(),
+                'comments' => $comments,
+                'reference' => $refs
+            ));
+        }
+    }
+
+    public function getSectionSiteInterfaceCompletionData(Section $section)
+    {
+        $db = \Database::get();
+        $data = array();
+        $data['messageCount'] = $db->GetOne('select count(mtID) from MultilingualTranslations where mtSectionID = ?', array($section->getCollectionID()));
+        $data['translatedCount'] = $db->GetOne('select count(mtID) from MultilingualTranslations where mtSectionID = ? and msgstr != ""', array($section->getCollectionID()));
+        $data['completionPercentage'] = 0;
+        if ($data['messageCount'] > 0) {
+            $data['completionPercentage'] = round(($data['translatedCount'] / $data['messageCount']) * 100);
+        }
+        return $data;
+    }
+
 }
