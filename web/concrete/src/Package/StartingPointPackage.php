18c4dfcc73d9e1f2e2bde5c243ed4ac767111dd1
Michael Krasnow <mnkras@gmail.com>
2014-12-22 22:51:45 -0500

    show nicer error instead of ugly mysql constraint error

--- web/concrete/src/Package/StartingPointPackage.php
+++ web/concrete/src/Package/StartingPointPackage.php
@@ -185,6 +185,12 @@ class StartingPointPackage extends BasePackage {
 
 	public function install_database() {
 		$db = Loader::db();
+        $num = $db->GetCol("show tables");
+        if (count($num) > 0) {
+            throw new \Exception(t(
+                    'There are already %s tables in this database. concrete5 must be installed in an empty database.',
+                    count($num)));
+        }
 		$installDirectory = DIR_BASE_CORE. '/config';
 		try {
 			Package::installDB($installDirectory . '/db.xml');
