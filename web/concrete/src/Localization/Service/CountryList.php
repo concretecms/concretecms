4c5b7bb53a7d811eea69a2ed0ef6f565c691908b
Michele Locati <michele@locati.it>
2014-12-23 09:21:45 +0100

    PSR-2

--- web/concrete/src/Localization/Service/CountryList.php
+++ web/concrete/src/Localization/Service/CountryList.php
@@ -1,44 +1,45 @@
 <?php
 namespace Concrete\Core\Localization\Service;
 
-use Localization;
 use Events;
 
-class CountryList {
+class CountryList
+{
+    protected $countries = array();
 
-	protected $countries = array();
+    public function __construct()
+    {
+        $countries = \Punic\Territory::getCountries();
+        unset(
+            // Fake countries
+            $countries['IM'], // Isle of Man (it's a British Crown Dependency)
+            $countries['JE'] // Jersey (it's a British Crown Dependency)
+        );
 
-	public function __construct() {
+        $event = new \Symfony\Component\EventDispatcher\GenericEvent();
+        $event->setArgument('countries', $countries);
+        $event = Events::dispatch('on_get_countries_list', $event);
+        $countries = $event->getArgument('countries');
 
-		$countries = \Punic\Territory::getCountries();
-		unset(
-			// Fake countries
-			$countries['IM'], // Isle of Man (it's a British Crown Dependency)
-			$countries['JE'] // Jersey (it's a British Crown Dependency)
-		);
+        $this->countries = $countries;
+    }
 
-		$event = new \Symfony\Component\EventDispatcher\GenericEvent();
-		$event->setArgument('countries', $countries);
-		$event = Events::dispatch('on_get_countries_list', $event);
-		$countries = $event->getArgument('countries');
+    /** Returns an array of countries with their short name as the key and their full name as the value
+    * @return array Keys are the country codes, values are the county names
+    */
+    public function getCountries()
+    {
+        return $this->countries;
+    }
 
-		$this->countries = $countries;
-	}
+    /** Gets a country full name given its code
+    * @param string $code The country code
+    * @return string
+    */
+    public function getCountryName($code)
+    {
+        $countries = $this->getCountries(true);
 
-	/** Returns an array of countries with their short name as the key and their full name as the value
-	* @return array Keys are the country codes, values are the county names
-	*/
-	public function getCountries() {
-		return $this->countries;
-	}
-
-	/** Gets a country full name given its code
-	* @param string $code The country code
-	* @return string
-	*/
-	public function getCountryName($code) {
-		$countries = $this->getCountries(true);
-		return $countries[$code];
-	}
-
+        return $countries[$code];
+    }
 }
