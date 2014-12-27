2e648a545d73d79ff03e5f9bd976cf244cecce16
Takayuki Kaneko <kaneko@tagworks.jp>
2014-12-27 16:46:53 +0900

    Add Japanese States and not sort JP
    Former-commit-id: 04fe8b3dd974958992f4991d091581f7b32f55e6

--- web/concrete/src/Localization/Service/StatesProvincesList.php
+++ web/concrete/src/Localization/Service/StatesProvincesList.php
@@ -487,14 +487,68 @@ class StatesProvincesList {
 		'VV' => 'Vibo Valentia',
 		'VI' => 'Vicenza',
 		'VT' => 'Viterbo'
+	),
+        
+	'JP' => array(
+		'01' => '北海道',
+		'02' => '青森県',
+		'03' => '岩手県',
+		'04' => '宮城県',
+		'05' => '秋田県',
+		'06' => '山形県',
+		'07' => '福島県',
+		'08' => '茨城県',
+		'09' => '栃木県',
+		'10' => '群馬県',
+		'11' => '埼玉県',
+		'12' => '千葉県',
+		'13' => '東京都',
+		'14' => '神奈川県',
+		'15' => '新潟県',
+		'16' => '富山県',
+		'17' => '石川県',
+		'18' => '福井県',
+		'19' => '山梨県',
+		'20' => '長野県',
+		'21' => '岐阜県',
+		'22' => '静岡県',
+		'23' => '愛知県',
+		'24' => '三重県',
+		'25' => '滋賀県',
+		'26' => '京都府',
+		'27' => '大阪府',
+		'28' => '兵庫県',
+		'29' => '奈良県',
+		'30' => '和歌山県',
+		'31' => '鳥取県',
+		'32' => '島根県',
+		'33' => '岡山県',
+		'34' => '広島県',
+		'35' => '山口県',
+		'36' => '徳島県',
+		'37' => '香川県',
+		'38' => '愛媛県',
+		'39' => '高知県',
+		'40' => '福岡県',
+		'41' => '佐賀県',
+		'42' => '長崎県',
+		'43' => '熊本県',
+		'44' => '大分県',
+		'45' => '宮崎県',
+		'46' => '鹿児島県',
+		'47' => '沖縄県'
 	)
 
 	);
 
+	protected $sortedCountries = array('JP');
+
 	public function __construct() {
 		$this->stateProvinces['GB'] = $this->stateProvinces['UK'];
 		foreach(array_keys($this->stateProvinces) as $country) {
-			asort($this->stateProvinces[$country]);
+			if (!in_array($country, $this->sortedCountries)) {
+				asort($this->stateProvinces[$country]);
+			}
 		}
 
 		$event = new \Symfony\Component\EventDispatcher\GenericEvent();
