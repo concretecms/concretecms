f2a5e6de2f339d06ebe76e1741e896b55ee3be89
Korvin Szanto <korvinszanto@gmail.com>
2014-12-29 15:10:08 -0800

    Add event list

---
+++ web/concrete/src/Calendar/Event/EventList.php
@@ -0,0 +1,48 @@
+<?php
+namespace Concrete\Core\Calendar\Event;
+
+use Concrete\Core\Search\Pagination\Pagination;
+use Pagerfanta\Adapter\DoctrineDbalAdapter;
+
+class EventList extends \Concrete\Core\Search\ItemList\Database\AttributedItemList
+{
+
+    public function getResult($row)
+    {
+        return Event::getByID(array_get($row, 'eventID'));
+    }
+
+    /**
+     * Returns the total results in this item list.
+     *
+     * @return int
+     */
+    public function getTotalResults()
+    {
+        $query = $this->deliverQueryObject();
+        return $query->select('count(distinct e.eventID)')->setMaxResults(1)->execute()->fetchColumn();
+
+    }
+
+    public function createQuery()
+    {
+        $this->query->select('e.eventID')->from('CalendarEvents', 'e');
+    }
+
+    protected function getAttributeKeyClassName()
+    {
+        return '\\Concrete\\Core\\Attribute\\Key\\EventKey';
+    }
+
+    /**
+     * @return \Concrete\Core\Search\Pagination\Pagination
+     */
+    protected function createPaginationObject()
+    {
+        $adapter = new DoctrineDbalAdapter(
+            $this->deliverQueryObject(), function ($query) {
+            $query->select('count(distinct e.eventID)')->setMaxResults(1);
+        });
+        return new Pagination($this, $adapter);
+    }
+}
