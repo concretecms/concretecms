28ac590fd53a9bd0f7fffa52d68ddb92397bc7b9
Florian Delizy <fdy@voltalis.com>
2015-01-01 04:40:55 +0100

    refactored block type list as well to be coherent with the rest

--- web/concrete/elements/dashboard/block_type_icon.php
+++ web/concrete/elements/dashboard/block_type_icon.php
@@ -9,6 +9,6 @@ $ci = \Core::make( 'helper/concrete/urls' );
 <div class="ccm-block-type-display <?=$class?>">
 	<img src="<?php echo $ci->getBlockTypeIconURL($bt) ?>" alt="<?php echo t('block type icon')?>"/>
 	<span>
-		<a href="<?php echo $view->url('/dashboard/blocks/types', 'inspect', $bt->getBlockTypeID()); ?>"><?php echo t($bt->getBlockTypeName()); ?></a>
+    <?php if (!$nolink) { ?><a href="<?php echo $view->url('/dashboard/blocks/types', 'inspect', $bt->getBlockTypeID()); ?>"><?php } ?><?php echo t($bt->getBlockTypeName()); ?><?php if (!$nolink){?></a><?php } ?>
 	</span>
 </div>
--- web/concrete/single_pages/dashboard/blocks/types/view.php
+++ web/concrete/single_pages/dashboard/blocks/types/view.php
@@ -1,13 +1,13 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-<? if ($this->controller->getTask() == 'inspect' || $this->controller->getTask() == 'refresh') { 
+<?php if ($this->controller->getTask() == 'inspect' || $this->controller->getTask() == 'refresh') { 
  Loader::element( 'dashboard/inspect_block_type', array( 'bt' => $bt ) );
 ?>
 
 <hr/>
 
 <a href="<?=$view->url('/dashboard/blocks/types')?>" class="btn btn-default pull-left"><?=t('Back to Block Types')?></a>
-    <?
+    <?php
     $u = new User();
     if ($u->isSuperUser()) { ?>
 
@@ -24,65 +24,60 @@
         }
     </script>
 
-    <? } else { ?>
+    <?php } else { ?>
         <a href="<?=URL::to('/dashboard/blocks/types', 'refresh', $bt->getBlockTypeID())?>" class="btn btn-default"><?=t('Refresh')?></a>
-    <? } ?>
+    <?php } ?>
 </div>
 
-<? } else { ?>
+<?php } else { ?>
 
 	<h3><?=t('Awaiting Installation')?></h3>
-	<? if (count($availableBlockTypes) > 0) { ?>
+	<?php if (count($availableBlockTypes) > 0) { ?>
 
         <ul class="item-select-list">
-            <?	foreach ($availableBlockTypes as $bt) {
-                $btIcon = $ci->getBlockTypeIconURL($bt);
-                ?>
-                <li><span><img src="<?=$btIcon?>" /> <?=t($bt->getBlockTypeName())?>
+            <?php	foreach ($availableBlockTypes as $bt) { ?>
+                <li><span><?php Loader::element('dashboard/block_type_icon', array('bt'=>$bt, 'class'=>'mini', 'nolink'=>true))?> <?=t($bt->getBlockTypeName())?>
                     <a href="<?=URL::to('/dashboard/blocks/types','install', $bt->getBlockTypeHandle())?>" class="btn pull-right btn-sm btn-default"><?=t('Install')?></a>
                     </span>
                 </li>
-            <? } ?>
+            <?php } ?>
         </ul>
 
-	<? } else { ?>
+	<?php } else { ?>
 		<p><?=t('No custom block types are awaiting installation.')?></p>
-	<? } ?>
+	<?php } ?>
 
-    <? if (Config::get('concrete.marketplace.enabled') == true) { ?>
+    <?php if (Config::get('concrete.marketplace.enabled') == true) { ?>
     <div class="alert alert-info">
         <a class="btn btn-success btn-xs pull-right" href="<?=$view->url('/dashboard/extend/add-ons')?>"><?=t("More Add-ons")?></a>
         <p><?=t('Browse our marketplace of add-ons to extend your site!')?></p>
     </div>
-    <? } ?>
+    <?php } ?>
 
     <hr/>
 
 	<h3><?=t('Installed Block Types')?></h3>
 	<ul id="ccm-block-type-list-installed" class="item-select-list ccm-block-type-sortable-list">
-		<? foreach($normalBlockTypes as $bt) {
-			$btIcon = $ci->getBlockTypeIconURL($bt);
+		<?php foreach($normalBlockTypes as $bt) {
 			$btID = $bt->getBlockTypeID();
 			?>
 			<li id="btID_<?=$btID?>" data-btid="<?=$btID?>">
-                <a href="<?=$view->action('inspect', $bt->getBlockTypeID())?>"><img src="<?=$btIcon?>" /> <?=t($bt->getBlockTypeName())?></a>
+                <a href="<?=$view->action('inspect', $bt->getBlockTypeID())?>"><?php Loader::element('dashboard/block_type_icon', array('bt'=>$bt, 'class'=>'mini', 'nolink'=>true))?><?=t($bt->getBlockTypeName())?></a>
 			</li>
-		<? } ?>
+		<?php } ?>
 	</ul>
 
 	<h3><?=t('Internal Block Types')?></h3>
     <ul class="item-select-list">
-		<? foreach($internalBlockTypes as $bt) {
-			$btIcon = $ci->getBlockTypeIconURL($bt);
-			?>
+		<?php foreach($internalBlockTypes as $bt) { ?>
 			<li>
-                <a href="<?=$view->action('inspect', $bt->getBlockTypeID())?>"><img src="<?=$btIcon?>" /> <?=t($bt->getBlockTypeName())?></a>
+                <a href="<?=$view->action('inspect', $bt->getBlockTypeID())?>"><?php Loader::element('dashboard/block_type_icon', array('bt'=>$bt, 'class'=>'mini', 'nolink'=>true))?><?=t($bt->getBlockTypeName())?></a>
 			</li>
-		<? } ?>
+		<?php } ?>
 	</ul>
 
 
 
 </div>
 
-<? } ?>
+<?php } ?>
