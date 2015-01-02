bf78c983ffe79b0c264ac794a2adc4d903c74eff
Florian Delizy <fdy@voltalis.com>
2015-01-02 15:08:02 +0100

    pretty print

--- build/libraries/generate-constants.php
+++ build/libraries/generate-constants.php
@@ -16,7 +16,7 @@ $cms = require_once $corePath . '/dispatcher.php';
 $generator = new \Concrete\Core\Support\JSConstantGenerator( $constants );
 $constants = $generator->render();
 
-$js =  "// GENERATED FILE DO NOT MODIFY (see grunt generate-constants target), DO NOT EDIT THIS FILE, MODIFICATIONS WILL BE LOST!!\n$(function(){ Concrete.const = $constants; })\n";
+$js =  "// GENERATED FILE DO NOT MODIFY (see grunt generate-constants target), DO NOT EDIT THIS FILE, MODIFICATIONS WILL BE LOST!!\n$(function(){\n\nConcrete.const = $constants;\n\n})\n";
 
 file_put_contents(DIR_BASE . '/concrete/js/build/core/app/concrete5-const.js', $js);
 
