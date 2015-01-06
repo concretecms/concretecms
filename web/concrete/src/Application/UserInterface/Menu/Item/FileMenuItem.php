08d45c3183ff1ef5fcd7ba4961f8353c5427d925
Florian Delizy <fdy@voltalis.com>
2015-01-06 23:24:08 +0100

    psr-2 coding standard

--- web/concrete/src/Application/UserInterface/Menu/Item/FileMenuItem.php
+++ web/concrete/src/Application/UserInterface/Menu/Item/FileMenuItem.php
@@ -3,39 +3,72 @@ namespace Concrete\Core\Application\UserInterface\Menu\Item;
 
 class FileMenuItem extends Item
 {
-	const ACTION_AJAX_REQUEST = 'ajax';	  	//!< @javascript-exported
-	const ACTION_OPEN_DIALOG  = 'dialog'; 	//!< @javascript-exported
-	const ACTION_DOWNLOAD     = 'download';	//!< @javascript-exported
-	const ACTION_OPEN		  = 'open';		//!< @javascript-exported
-	const ACTION_SEPARATOR	  = 'separator';//!< @javascript-exported
+    const ACTION_AJAX_REQUEST = 'ajax';     //!< @javascript-exported
+    const ACTION_OPEN_DIALOG  = 'dialog';   //!< @javascript-exported
+    const ACTION_DOWNLOAD     = 'download'; //!< @javascript-exported
+    const ACTION_OPEN         = 'open';     //!< @javascript-exported
+    const ACTION_SEPARATOR    = 'separator';//!< @javascript-exported
 
 
-	protected $actionType = self::ACTION_OPEN_DIALOG;
+    protected $actionType = self::ACTION_OPEN_DIALOG;
 
-	public function getActionType() { return $this->actionType; }
-	public function setActionType($type) { 
-		$this->setLinkAttribute( 'data-filemenu-type', $type );
-		$this->actionType = $type; 
-	}
+    public function getActionType()
+    { 
+        return $this->actionType; 
+    }
+
+    public function setActionType($type) 
+    { 
+        $this->setLinkAttribute('data-filemenu-type', $type);
+        $this->actionType = $type; 
+    }
 
     const CAN_UNIQUE        = 1;
     const CAN_MULTIPLE      = 2;
     const CAN_ALL           = 3;
 
-	protected $abilities    = self::CAN_ALL;
+    protected $abilities    = self::CAN_ALL;
 
-    public function handleMultiple() { return $this->abilities & self::CAN_MULTIPLE; }
-    public function handleUnique()   { return $this->abilities & self::CAN_UNIQUE; }
-    public function setAbilities( $abilities ) { $this->abilities = $abilities; }
-    public function hasAbility($ability) { return ($this->abilities & $ability) == $ability; }
+    public function handleMultiple()
+    { 
+        return $this->abilities & self::CAN_MULTIPLE; 
+    }
 
-	public function isSeparator() { return self::ACTION_SEPARATOR == $this->actionType; }
+    public function handleUnique()
+    { 
+        return $this->abilities & self::CAN_UNIQUE; 
+    }
 
-	protected $dangerous	  = false;
-	public function isDangerous() { return $this->dangerous; }
-	public function setDangerous($dangerous = true) { $this->dangerous = $dangerous; }
+    public function setAbilities($abilities)
+    { 
+        $this->abilities = $abilities;
+    }
+    
+    public function hasAbility($ability)
+    {
+        return ($this->abilities & $ability) == $ability;
+    }
 
-    public function getLinkAttributes() {
+    public function isSeparator()
+    {
+        return self::ACTION_SEPARATOR == $this->actionType;
+    }
+
+
+    protected $dangerous      = false;
+
+    public function isDangerous()
+    {
+        return $this->dangerous;
+    }
+
+    public function setDangerous($dangerous = true)
+    {
+        $this->dangerous = $dangerous;
+    }
+
+    public function getLinkAttributes()
+    {
         $attrs = parent::getLinkAttributes();
         $attrs['data-filemenu-type'] = $this->getActionType();
         return $attrs;
@@ -50,8 +83,18 @@ class FileMenuItem extends Item
     const CAN_DELETE   = 'canDeleteFile';
     const CAN_PERMS    = 'canEditFilePermissions';
 
-    public function getRestrictions() { return $this->restrictions; }
-    public function addRestriction( $restrict ) { $this->restrictions[] = $restrict; }
-    public function setRestrictions( array $restrict ) { $this->restrictions = $restrict; }
+    public function getRestrictions()
+    {
+        return $this->restrictions;
+    }
 
+    public function addRestriction($restrict)
+    { 
+        $this->restrictions[] = $restrict;
+    }
+
+    public function setRestrictions(array $restrict)
+    {
+        $this->restrictions = $restrict;
+    }
 }
