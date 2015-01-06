1be9f8a10ce83093d171b0ec157ada8ffe9bb7c0
Florian Delizy <fdy@voltalis.com>
2015-01-06 23:47:47 +0100

    psr-2 coding standard

--- web/concrete/controllers/backend/file.php
+++ web/concrete/controllers/backend/file.php
@@ -1,5 +1,6 @@
 <?
 namespace Concrete\Controller\Backend;
+
 use Controller;
 use FileSet;
 use File as ConcreteFile;
@@ -11,9 +12,11 @@ use Permissions as ConcretePermissions;
 use FilePermissions;
 use FileVersion;
 
-class File extends Controller {
+class File extends Controller
+{
 
-    public function star() {
+    public function star()
+    {
         $fs = FileSet::createAndGetSet('Starred Files', FileSet::TYPE_STARRED);
         $files = $this->getRequestFiles();
         $r = new FileEditResponse();
@@ -30,7 +33,8 @@ class File extends Controller {
         $r->outputJSON();
     }
 
-    public function rescan() {
+    public function rescan()
+    {
         $files = $this->getRequestFiles('canEditFileContents');
         $r = new FileEditResponse();
         $r->setFiles($files);
@@ -66,7 +70,8 @@ class File extends Controller {
         $r->outputJSON();
     }
 
-    public function approveVersion() {
+    public function approveVersion()
+    {
         $files = $this->getRequestFiles('canEditFileContents');
         $r = new FileEditResponse();
         $r->setFiles($files);
@@ -79,7 +84,8 @@ class File extends Controller {
         $r->outputJSON();
     }
 
-    public function deleteVersion() {
+    public function deleteVersion()
+    {
         $files = $this->getRequestFiles('canEditFileContents');
         $r = new FileEditResponse();
         $r->setFiles($files);
@@ -92,7 +98,8 @@ class File extends Controller {
         $r->outputJSON();
     }
 
-    protected function getRequestFiles($permission = 'canViewFileInFileManager') {
+    protected function getRequestFiles($permission = 'canViewFileInFileManager')
+    {
         $files = array();
         if (is_array($_REQUEST['fID'])) {
             $fileIDs = $_REQUEST['fID'];
@@ -114,7 +121,8 @@ class File extends Controller {
         return $files;
     }
 
-    public function upload() {
+    public function upload()
+    {
         $fp = FilePermissions::getGlobal();
         $cf = \Core::make('helper/file');
         if (!$fp->canAddFiles()) {
@@ -151,7 +159,8 @@ class File extends Controller {
         \Core::make('helper/ajax')->sendResult($files);
     }
 
-    public function duplicate() {
+    public function duplicate()
+    {
         $files = $this->getRequestFiles('canCopyFile');
         $r = new FileEditResponse();
         $newFiles = array();
@@ -163,14 +172,16 @@ class File extends Controller {
         $r->outputJSON();
     }
 
-    public function getJSON() {
+    public function getJSON()
+    {
         $files = $this->getRequestFiles();
         $r = new FileEditResponse();
         $r->setFiles($files);
         $r->outputJSON();
     }
 
-    public function stream( $fID, $fvID ) {
+    public function stream( $fID, $fvID )
+    {
 
         $fp = FilePermissions::getGlobal();
         $f = ConcreteFile::getByID($fID);
@@ -192,7 +203,8 @@ class File extends Controller {
         }
     }
 
-    public function zipAndStream( array $fIDs, $fvID = null ) {
+    public function zipAndStream( array $fIDs, $fvID = null )
+    {
 
         $vh = \Core::make('helper/validation/identifier');
         $fh = \Core::make('helper/file');
@@ -215,9 +227,9 @@ class File extends Controller {
             }
         }
 
-        if(empty($files)) die(t("None of the requested files could be found."));
+        if (empty($files)) die(t("None of the requested files could be found."));
 
-        if(class_exists('ZipArchive', false)) {
+        if (class_exists('ZipArchive', false)) {
 
             $zip = new \ZipArchive;
             $res = $zip->open( $filename, \ZipArchive::CREATE );
@@ -236,7 +248,8 @@ class File extends Controller {
 
     }
 
-    public function download() {
+    public function download()
+    {
 
         $fp = FilePermissions::getGlobal();
 
@@ -244,15 +257,15 @@ class File extends Controller {
 
         $target = $_REQUEST['fID'];
 
-        if ( !is_array($target) && isset( $_REQUEST['zipit'] ) ) $target = array( $target );
+        if (!is_array($target) && isset($_REQUEST['zipit'])) $target = array($target);
                 
-        if ( is_array($target) ) {
+        if (is_array($target)) {
             // sanitize:
             foreach ( $target as $key => $val ) $target[$key] = intval($val);
             $this->zipAndStream($target);
             return;
         } else {
-            $this->stream( intval($target), isset( $_REQUEST['fvID'] )? intval($_REQUEST['fvID']) : null );
+            $this->stream(intval($target), isset($_REQUEST['fvID']) ? intval($_REQUEST['fvID']) : null);
         }
     }
 
