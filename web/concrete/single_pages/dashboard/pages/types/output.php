40efd00cf70d5ab6dbed7f5a599dfd74bd29003e
Andrew Embler <andrew@concrete5.org>
2015-01-06 16:07:01 -0800

    Fixing 404 on edit page type outputs if clicking on icon
    Former-commit-id: 9ad003b029f67cffb9da26a23f742db423183450

--- web/concrete/single_pages/dashboard/pages/types/output.php
+++ web/concrete/single_pages/dashboard/pages/types/output.php
@@ -7,7 +7,7 @@
 	
 
     <tr>
-        <td><a href="<?=$view->action('edit', $pt->getPageTemplateID())?>"><?=$pt->getPageTemplateIconImage()?></a></td>
+        <td><a href="<?=$view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID())?>" target="_blank"><?=$pt->getPageTemplateIconImage()?></a></td>
         <td style="width: 100%; vertical-align: middle"><p class="lead" style="margin-bottom: 0px"><?=$pt->getPageTemplateDisplayName()?></p></td>
         <td style="vertical-align: middle"><a href="<?=$view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID())?>" target="_blank" class="btn btn-default"><?=t('Edit Defaults')?></a></td>
     </tr>
