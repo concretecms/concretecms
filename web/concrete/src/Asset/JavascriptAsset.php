7128bf82feac2a1a451b12ca0bae2cf30130e0e3
Florian Delizy <fdy@voltalis.com>
2015-01-08 14:24:47 +0100

    psr-2 coding standard: whitespaces

--- web/concrete/src/Asset/JavascriptAsset.php
+++ web/concrete/src/Asset/JavascriptAsset.php
@@ -7,76 +7,76 @@ use Config;
 class JavascriptAsset extends Asset 
 {
 
-	protected $assetSupportsMinification = true;
-	protected $assetSupportsCombination = true;
+    protected $assetSupportsMinification = true;
+    protected $assetSupportsCombination = true;
 
     public function getAssetDefaultPosition() 
     {
-		return Asset::ASSET_POSITION_FOOTER;
-	}
+        return Asset::ASSET_POSITION_FOOTER;
+    }
 
     public function getRelativeOutputDirectory() 
     {
-		return REL_DIR_FILES_CACHE . '/' . DIRNAME_JAVASCRIPT;
-	}
+        return REL_DIR_FILES_CACHE . '/' . DIRNAME_JAVASCRIPT;
+    }
 
     protected static function getOutputDirectory() 
     {
-		if (!file_exists(Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT)) {
-			$proceed = @mkdir(Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT);
-		} else {
-			$proceed = true;
-		}
-		if ($proceed) {
-			return Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT;
-		} else {
-			return false;
-		}
-	}
+        if (!file_exists(Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT)) {
+            $proceed = @mkdir(Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT);
+        } else {
+            $proceed = true;
+        }
+        if ($proceed) {
+            return Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT;
+        } else {
+            return false;
+        }
+    }
 
     protected static function process($assets, $processFunction) 
     {
-		if ($directory = self::getOutputDirectory()) {
-			$filename = '';
+        if ($directory = self::getOutputDirectory()) {
+            $filename = '';
             $sourceFiles = array();
-			for ($i = 0; $i < count($assets); $i++) {
-				$asset = $assets[$i];
-				$filename .= $asset->getAssetURL();
+            for ($i = 0; $i < count($assets); $i++) {
+                $asset = $assets[$i];
+                $filename .= $asset->getAssetURL();
                 $sourceFiles[] = $asset->getAssetURL();
-			}
-			$filename = sha1($filename);
-			$cacheFile = $directory . '/' . $filename . '.js';
-			if (!file_exists($cacheFile)) {
-				$js = '';
-				foreach($assets as $asset) {
-					$js .= file_get_contents($asset->getAssetPath()) . "\n\n";
-					$js = $processFunction($js, $asset->getAssetURLPath(), self::getRelativeOutputDirectory());
-				}
-				@file_put_contents($cacheFile, $js);
-			}
+            }
+            $filename = sha1($filename);
+            $cacheFile = $directory . '/' . $filename . '.js';
+            if (!file_exists($cacheFile)) {
+                $js = '';
+                foreach($assets as $asset) {
+                    $js .= file_get_contents($asset->getAssetPath()) . "\n\n";
+                    $js = $processFunction($js, $asset->getAssetURLPath(), self::getRelativeOutputDirectory());
+                }
+                @file_put_contents($cacheFile, $js);
+            }
 
-			$asset = new JavascriptAsset();
-			$asset->setAssetURL(self::getRelativeOutputDirectory() . '/' . $filename . '.js');
-			$asset->setAssetPath($directory . '/' . $filename . '.js');
+            $asset = new JavascriptAsset();
+            $asset->setAssetURL(self::getRelativeOutputDirectory() . '/' . $filename . '.js');
+            $asset->setAssetPath($directory . '/' . $filename . '.js');
             $asset->setCombinedAssetSourceFiles($sourceFiles);
-			return array($asset);
-		}
-		return $assets;
+            return array($asset);
+        }
+        return $assets;
     }
 
     public function combine($assets) 
     {
-		return self::process($assets, function($js, $assetPath, $targetPath) {
-			return $js;
-		});
-	}
+        return self::process($assets, function($js, $assetPath, $targetPath) {
+            return $js;
+        });
+    }
 
     public function minify($assets) 
     {
-		return self::process($assets, function($js, $assetPath, $targetPath) {
-			return \JShrink\Minifier::minify($js);
-		});
-	}
+        return self::process($assets, function($js, $assetPath, $targetPath) {
+            return \JShrink\Minifier::minify($js);
+        });
+    }
 
     public function getAssetType() 
     {
@@ -96,6 +96,6 @@ class JavascriptAsset extends Asset
             $e->setAttribute('data-source', $source);
         }
         return (string) $e;
-	}
+    }
 
 }
