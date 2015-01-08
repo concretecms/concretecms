99d3ef9edc4fbcb742c66775521bdbca48ce44c0
Florian Delizy <fdy@voltalis.com>
2015-01-08 14:24:13 +0100

    psr-2 coding standard

--- web/concrete/src/Asset/JavascriptAsset.php
+++ web/concrete/src/Asset/JavascriptAsset.php
@@ -1,22 +1,27 @@
 <?php
 namespace Concrete\Core\Asset;
+
 use HtmlObject\Element;
 use Config;
 
-class JavascriptAsset extends Asset {
+class JavascriptAsset extends Asset 
+{
 
 	protected $assetSupportsMinification = true;
 	protected $assetSupportsCombination = true;
 
-	public function getAssetDefaultPosition() {
+    public function getAssetDefaultPosition() 
+    {
 		return Asset::ASSET_POSITION_FOOTER;
 	}
 
-	public function getRelativeOutputDirectory() {
+    public function getRelativeOutputDirectory() 
+    {
 		return REL_DIR_FILES_CACHE . '/' . DIRNAME_JAVASCRIPT;
 	}
 
-	protected static function getOutputDirectory() {
+    protected static function getOutputDirectory() 
+    {
 		if (!file_exists(Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT)) {
 			$proceed = @mkdir(Config::get('concrete.cache.directory') . '/' . DIRNAME_JAVASCRIPT);
 		} else {
@@ -29,7 +34,8 @@ class JavascriptAsset extends Asset {
 		}
 	}
 
-    protected static function process($assets, $processFunction) {
+    protected static function process($assets, $processFunction) 
+    {
 		if ($directory = self::getOutputDirectory()) {
 			$filename = '';
             $sourceFiles = array();
@@ -58,21 +64,27 @@ class JavascriptAsset extends Asset {
 		return $assets;
     }
 
-	public function combine($assets) {
+    public function combine($assets) 
+    {
 		return self::process($assets, function($js, $assetPath, $targetPath) {
 			return $js;
 		});
 	}
 
-	public function minify($assets) {
+    public function minify($assets) 
+    {
 		return self::process($assets, function($js, $assetPath, $targetPath) {
 			return \JShrink\Minifier::minify($js);
 		});
 	}
 
-	public function getAssetType() {return 'javascript';}
+    public function getAssetType() 
+    {
+        return 'javascript';
+    }
 
-	public function __toString() {
+    public function __toString() 
+    {
         $e = new Element('script');
         $e->type('text/javascript')->src($this->getAssetURL());
         if (count($this->combinedAssetSourceFiles)) {
