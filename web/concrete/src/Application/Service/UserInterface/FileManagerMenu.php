e9febbec775f89a1ecfad83074e1bd0a926937de
Florian Delizy <fdy@voltalis.com>
2015-01-09 09:23:44 +0100

    remove icons from menu

--- web/concrete/src/Application/Service/UserInterface/FileManagerMenu.php
+++ web/concrete/src/Application/Service/UserInterface/FileManagerMenu.php
@@ -16,46 +16,46 @@ class FileManagerMenu
 
         $default = array (
 
-            "ccm_view"       => array( "label"=> t('View'), 'icon' => 'eye', "url"=> $uh->getToolsURL('files/view'), // TODO refactor tool to a route
+            "ccm_view"       => array( "label"=> t('View'), "url"=> $uh->getToolsURL('files/view'), // TODO refactor tool to a route
                                        "type" => FileMenuItem::ACTION_OPEN_DIALOG, 'perms' => array( FileMenuItem::CAN_VIEW ), 'ability' => FileMenuItem::CAN_UNIQUE,
                                        "options" => array( 'modal' => 'false', 'buttons' => 'true', 'height' =>  '75%', 'width' => '90%') ),
 
-            "ccm_download"   => array( "label"=> t('Download'), 'icon' => 'download', "url"=>\URL::to('/ccm/system/file/download'),
+            "ccm_download"   => array( "label"=> t('Download'), "url"=>\URL::to('/ccm/system/file/download'),
                                        "type" => FileMenuItem::ACTION_DOWNLOAD, 'perms' => array(FileMenuItem::CAN_VIEW) ),
 
-            "ccm_edit"       => array( "label"=> t('Edit'), 'icon' => 'edit', "url"=> $uh->getToolsURL('files/edit'), // TODO refactor tool to a route
+            "ccm_edit"       => array( "label"=> t('Edit'), "url"=> $uh->getToolsURL('files/edit'), // TODO refactor tool to a route
                                        "type" => FileMenuItem::ACTION_OPEN_DIALOG, 'perms' => array( FileMenuItem::CAN_EDIT), 'ability' => FileMenuItem::CAN_UNIQUE,
                                        "options" => array( 'modal' => 'true', 'height' =>  '70%', 'width' => '90%') ),
 
-            "ccm_bulk_properties" => array( "label" => t('Properties'), 'icon' => 'sliders', "url" => \URL::to('/ccm/system/dialogs/file/bulk/properties'), 
+            "ccm_bulk_properties" => array( "label" => t('Properties'), "url" => \URL::to('/ccm/system/dialogs/file/bulk/properties'), 
                                         "type" => FileMenuItem::ACTION_OPEN_DIALOG,  'ability' => FileMenuItem::CAN_MULTIPLE,
                                         'options' =>  array( "width"=> "680", "height" => 450 )),
 
-            "ccm_properties" => array( "label" => t('Properties'), 'icon' => 'sliders', "url" => \URL::to('/ccm/system/dialogs/file/properties'), 
+            "ccm_properties" => array( "label" => t('Properties'), "url" => \URL::to('/ccm/system/dialogs/file/properties'), 
                                         "type" => FileMenuItem::ACTION_OPEN_DIALOG,  'ability' => FileMenuItem::CAN_UNIQUE,
                                         'options' =>  array( "width"=> "680", "height" => 450 )),
 
-            "ccm_replace"    => array( "label"=> t('Replace'), 'icon' => 'angle-double-down', "url"=> $uh->getToolsURL('files/replace'), // TODO refactor tool to a route
+            "ccm_replace"    => array( "label"=> t('Replace'), "url"=> $uh->getToolsURL('files/replace'), // TODO refactor tool to a route
                                        "type" => FileMenuItem::ACTION_OPEN_DIALOG, 'perms' => array( FileMenuItem::CAN_REPLACE), 'ability' => FileMenuItem::CAN_UNIQUE,
                                        "options" => array( 'modal' => 'true', 'height' =>  '500', 'width' => '200')),
 
-            "ccm_duplicate"  => array( "label" => t('Duplicate'), 'icon' => 'copy', "url" => \URL::to('/ccm/system/file/duplicate'),// FIXME: For now duplicate is broken on Doctrine side
+            "ccm_duplicate"  => array( "label" => t('Duplicate'), "url" => \URL::to('/ccm/system/file/duplicate'),// FIXME: For now duplicate is broken on Doctrine side
                                        "type" => FileMenuItem::ACTION_AJAX_REQUEST, 'perms' => array( FileMenuItem::CAN_COPY)),
 
-            "ccm_sets"       => array( "label" => t('Sets'), 'icon' => 'reorder', "url" => $uh->getToolsURL('files/add_to'),      // TODO refactor and remove tools once and for all
+            "ccm_sets"       => array( "label" => t('Sets'), "url" => $uh->getToolsURL('files/add_to'),      // TODO refactor and remove tools once and for all
                                         "type" => FileMenuItem::ACTION_OPEN_DIALOG, 
                                         "options" => array( "width"=>"500", "height"=>"400", )),
 
-            "ccm_rescan"     => array( "label" => t('Rescan'), 'icon' => 'refresh', "url"=>\URL::to('/ccm/system/file/rescan'),
+            "ccm_rescan"     => array( "label" => t('Rescan'), "url"=>\URL::to('/ccm/system/file/rescan'),
                                         "type" => FileMenuItem::ACTION_AJAX_REQUEST ),
 
             "ccm_dangerous"  => array( 'type' => FileMenuItem::ACTION_SEPARATOR ),
 
-            "ccm_perms"      => array( "label" => t('Permissions'), 'icon' => 'key', "url" => $uh->getToolsURL('files/permissions'),      // TODO refactor and remove tools once and for all
+            "ccm_perms"      => array( "label" => t('Permissions'), "url" => $uh->getToolsURL('files/permissions'),      // TODO refactor and remove tools once and for all
                                         "type" => FileMenuItem::ACTION_OPEN_DIALOG, 'perms' => array( FileMenuItem::CAN_PERMS ), 'ability' => FileMenuItem::CAN_UNIQUE, // TODO: This tool would be nice as CAN_ALL
                                         "options" => array( "width"=>"500", "height"=>"400", )),
 
-            "ccm_delete"     => array( "label" => t('Delete'),  'icon' => 'trash', "url" => \URL::to('/ccm/system/dialogs/file/bulk/delete'),
+            "ccm_delete"     => array( "label" => t('Delete'),  "url" => \URL::to('/ccm/system/dialogs/file/bulk/delete'),
                                         "type" => FileMenuItem::ACTION_OPEN_DIALOG, 'dangerous' => true, 'perms' => array( FileMenuItem::CAN_DELETE ),
                                         "options" => array ( "width"=> "500", "height" => 400, )),
         );
