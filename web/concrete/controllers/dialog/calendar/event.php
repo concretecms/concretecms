22f7dcb9e46113d428b210f9b6b09f2fe62e6f95
Andrew Embler <andrew@concrete5.org>
2015-01-09 16:06:30 -0800

    more work on occurrences

--- web/concrete/controllers/dialog/calendar/event.php
+++ web/concrete/controllers/dialog/calendar/event.php
@@ -2,7 +2,10 @@
 namespace Concrete\Controller\Dialog\Calendar;
 
 use \Concrete\Controller\Backend\UserInterface as BackendInterfaceController;
+use Concrete\Core\Application\EditResponse;
 use Concrete\Core\Calendar\Calendar;
+use Concrete\Core\Calendar\Event\EventRepetition;
+use Concrete\Core\Calendar\Event\Event as CalendarEvent;
 
 class Event extends BackendInterfaceController
 {
@@ -26,7 +29,22 @@ class Event extends BackendInterfaceController
 
     public function submit()
     {
+        $repetition = new EventRepetition();
+        $repetition->setStartDate(date('Y-m-d'));
+        $repetition->setStartDateAllDay(true);
+        $repetition->setEndDate(date('Y-m-d'));
+        $repetition->setEndDateAllDay(true);
+        $repetition->save();
+        $ev = new CalendarEvent(
+            $this->request->request->get('name'),
+            $this->request->request->get('description'),
+            $repetition
+        );
+        $ev->save();
 
+        $r = new EditResponse();
+        $r->setMessage(t('Event added successfully.'));
+        $r->outputJSON();
     }
 
 }
