15a07821d74f814a8d050ae29927008b6943a8ed
Florian Delizy <fdy@voltalis.com>
2015-01-10 11:57:26 +0100

    psr-2 coding standard

--- web/concrete/elements/dashboard/inspect_block_type.php
+++ web/concrete/elements/dashboard/inspect_block_type.php
@@ -1,16 +1,20 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.');
 
-if ( !is_object($bt) ) $bt = BlockType::getByHandle($bt);
-if ( !is_object($bt) ) return;
+if (!is_object($bt)) $bt = BlockType::getByHandle($bt);
+if (!is_object($bt)) return;
 
-$ci = \Core::make( 'helper/concrete/urls' );
+$ci = \Core::make('helper/concrete/urls');
 $btID = $bt->getBlockTypeID();
 
 ?>
 
 <li id="btID_<?=$btID?>"  data-btid="<?=$btID?>">
-        <?php foreach ( $bt->getBlockTypeSets() as $set ) echo '<span class="badge pull-right">' . $set->getBlockTypeSetName() . '</span>'; ?>
-        <div class="pull-right" style="margin-left: 10px; margin-right: 10px" ><?php echo t('Usage Count: %s (%s%s on active pages %s)', $bt->getCount(), "<strong>", $bt->getCount(true), "</strong>" ) ?></div>
+        <?php
+        foreach ($bt->getBlockTypeSets() as $set) {
+            echo '<span class="badge pull-right">' . $set->getBlockTypeSetName() . '</span>';
+        }
+        ?>
+        <div class="pull-right" style="margin-left: 10px; margin-right: 10px" ><?=t('Usage Count: %s (%s%s on active pages %s)', $bt->getCount(), "<strong>", $bt->getCount(true), "</strong>") ?></div>
         <a href="<?= $view->url('/dashboard/blocks/types', 'inspect', $bt->getBlockTypeID()); ?>"><img src="<?=$ci->getBlockTypeIconURL($bt)?>" /> <?=t($bt->getBlockTypeName()); ?></a>
 </li>
 
@@ -23,5 +27,5 @@ $btID = $bt->getBlockTypeID();
 				<span class="label label-danger"><?=t( "internal block type")?></span>
 			<? } ?>
 		</div>
-		<div class="ccm-block-type-description-content"> <?php echo t($bt->getBlockTypeDescription()); ?> </div>
+		<div class="ccm-block-type-description-content"> <?=t($bt->getBlockTypeDescription()); ?> </div>
 	</div>
