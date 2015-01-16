7bf11c9260cfd797fa8a3884134383f3b0a7d1a4
Florian Delizy <fdy@voltalis.com>
2015-01-16 05:27:18 +0100

    adding missing file_context_menu files (whoops)

---
+++ web/concrete/controllers/backend/file_context_menu.php
@@ -0,0 +1,15 @@
+<?php
+namespace Concrete\Controller\Backend;
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+use Controller;
+
+class FileContextMenu extends Controller
+{
+	protected $viewPath = '/backend/file_context_menu';
+
+	public function view()
+	{
+	}
+}
---
+++ web/concrete/views/backend/file_context_menu.php
@@ -0,0 +1,3 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+
+Loader::element('files/file_context_menu');
