fa0f08f6e59aa923840d9c6873f906dc92eb26c4
Florian Delizy <fdy@voltalis.com>
2015-01-16 05:55:28 +0100

    made the view dialog responsive (see #1774)

--- web/concrete/views/backend/file_view.php
+++ web/concrete/views/backend/file_view.php
@@ -17,6 +17,32 @@ if ($to->getPackageHandle() != '') {
 
 <div class="dialog-buttons">
 <form method="post" action="<?=\URL::to('/ccm/system/file/download')?>?fID=<?=$f->getFileID()?>&fvID=<?=$f->getFileVersionID()?>" style="margin: 0px">
+<label class="ccm-resize-related"><input id="ccm-resize-control" type="checkbox" checked/><?=t('Resize to dialog size')?></label>
 <?=$form->submit('submit', t('Download'), array('class' => 'btn btn-primary pull-right'))?>
 </form>
 </div>
+<style type="text/css">
+	.ccm-resize-target.responsive {
+		max-width:100%;
+		width: auto;
+		height:auto;
+	}
+</style>
+<script type="text/javascript">
+$(function(){
+	if ($('.ccm-resize-target').length < 1) {
+		$('ccm-resize-related').hide();
+	} else { 
+		function updateResizeTarget() {
+			var mustResize = $('#ccm-resize-control').is(':checked');
+			if (mustResize) {
+				$('.ccm-resize-target').addClass('responsive');
+			} else {
+				$('.ccm-resize-target').removeClass('responsive');
+			}
+		}
+		$('#ccm-resize-control').change(updateResizeTarget);
+		updateResizeTarget();
+	}
+});
+</script>
