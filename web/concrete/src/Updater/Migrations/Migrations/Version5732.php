ec066df468cbb60a46cbb3783d5e0ad735ee9cb7
Michele Locati <michele@locati.it>
2015-01-23 13:32:35 +0100

    Add info about plural rules
    Former-commit-id: 8224848994947e00fc24afdfe7570d5882c2927a

--- web/concrete/src/Updater/Migrations/Migrations/Version5732.php
+++ web/concrete/src/Updater/Migrations/Migrations/Version5732.php
@@ -15,8 +15,38 @@ class Version5732 extends AbstractMigration
     {
         $db = \Database::get();
         $db->Execute('DROP TABLE IF EXISTS PageStatistics');
+        $ms = $schema->getTable('MultilingualSections');
+        $msUpdated = false;
+        if (!$ms->hasColumn('msPlurals')) {
+            $ms->addColumn('msNumPlurals', 'integer', array('notnull' => true, 'unsigned' => true, 'default' => 2));
+            $msUpdated = true;
+        }
+        if (!$ms->hasColumn('msPluralRule')) {
+            $ms->addColumn('msPluralRule', 'string', array('notnull' => true, 'length' => 255, 'default' => '(n != 1)'));
+            $msUpdated = true;
+        }
+        if ($msUpdated) {
+            $rs = $db->Execute('select cID, msLanguage, msCountry from MultilingualSections');
+            while ($row = $rs->FetchRow()) {
+                $locale = $row['msLanguage'];
+                if ($row['msCountry']) {
+                    $locale .= '_' . $row['msCountry'];
+                }
+                $localeInfo = \Gettext\Utils\Locales::getLocaleInfo($locale);
+                if ($localeInfo) {
+                    $db->update(
+                        'MultilingualSections',
+                        array(
+                            'msNumPlurals' => $localeInfo['plurals'],
+                            'msPluralRule' => $localeInfo['pluralRule'],
+                        ),
+                        array('cID' => $row['cID'])
+                    );
+                }
+            }
+        }
         $mt = $schema->getTable('MultilingualTranslations');
-        $mtsUpdated = false;
+        $mtUpdated = false;
         if (!$mt->hasColumn('msgidPlural')) {
             $mt->addColumn('msgidPlural', 'text', array('notnull' => false));
             $mtUpdated = true;
