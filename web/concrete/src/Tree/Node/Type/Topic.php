056d7dae853b6b323a68c7c8507e0c14b77421ce
Andrew Embler <andrew@concrete5.org>
2015-02-02 10:00:10 -0800

    Tweaks
    Former-commit-id: 84a1aea643d5e1a78fab32f88f136f29b5736b5a

--- web/concrete/src/Tree/Node/Type/Topic.php
+++ web/concrete/src/Tree/Node/Type/Topic.php
@@ -5,23 +5,28 @@ use Concrete\Core\Tree\Node\Node as TreeNode;
 use Loader;
 use Core;
 
-class Topic extends TreeNode {
+class Topic extends TreeNode
+{
 
-	public function getPermissionResponseClassName() {
-		return '\\Concrete\\Core\\Permission\\Response\\TopicTreeNodeResponse';
-	}
+    public function getPermissionResponseClassName()
+    {
+        return '\\Concrete\\Core\\Permission\\Response\\TopicTreeNodeResponse';
+    }
 
-	public function getPermissionAssignmentClassName() {
-		return '\\Concrete\\Core\\Permission\\Assignment\\TopicTreeNodeAssignment';
-	}
-	public function getPermissionObjectKeyCategoryHandle() {
-		return 'topic_tree_node';
-	}
+    public function getPermissionAssignmentClassName()
+    {
+        return '\\Concrete\\Core\\Permission\\Assignment\\TopicTreeNodeAssignment';
+    }
 
-	public function getTreeNodeName()
-	{
-		return $this->treeNodeTopicName;
-	}
+    public function getPermissionObjectKeyCategoryHandle()
+    {
+        return 'topic_tree_node';
+    }
+
+    public function getTreeNodeName()
+    {
+        return $this->treeNodeTopicName;
+    }
 
     public function getTreeNodeDisplayName($format = 'html')
     {
@@ -36,47 +41,66 @@ class Topic extends TreeNode {
         }
     }
 
-	public function loadDetails() {
-		$db = Loader::db();
-		$row = $db->GetRow('select * from TreeTopicNodes where treeNodeID = ?', array($this->treeNodeID));
-		$this->setPropertiesFromArray($row);
-	}
+    public function loadDetails()
+    {
+        $db = Loader::db();
+        $row = $db->GetRow('select * from TreeTopicNodes where treeNodeID = ?', array($this->treeNodeID));
+        $this->setPropertiesFromArray($row);
+    }
 
-	public function deleteDetails() {
-		$db = Loader::db();
-		$db->Execute('delete from TreeTopicNodes where treeNodeID = ?', array($this->treeNodeID));
-	}
+    public function deleteDetails()
+    {
+        $db = Loader::db();
+        $db->Execute('delete from TreeTopicNodes where treeNodeID = ?', array($this->treeNodeID));
+    }
 
-	public function getTreeNodeJSON() {
-		$obj = parent::getTreeNodeJSON();
-		if (is_object($obj)) {
+    public function getTreeNodeJSON()
+    {
+        $obj = parent::getTreeNodeJSON();
+        if (is_object($obj)) {
             $obj->iconClass = 'fa fa-comment-o';
-			return $obj;
-		}
-	}
+            return $obj;
+        }
+    }
 
-    public function duplicate($parent = false) {
-		$node = $this::add($this->treeNodeTopicName, $parent);
-		$this->duplicateChildren($node);
-		return $node;
-	}
+    public function duplicate($parent = false)
+    {
+        $node = $this::add($this->treeNodeTopicName, $parent);
+        $this->duplicateChildren($node);
+        return $node;
+    }
 
-	public function setTreeNodeTopicName($treeNodeTopicName) {
-		$db = Loader::db();
-		$db->Replace('TreeTopicNodes', array('treeNodeID' => $this->getTreeNodeID(), 'treeNodeTopicName' => $treeNodeTopicName), array('treeNodeID'), true);
-		$this->treeNodeTopicName = $treeNodeTopicName;
-	}
+    public function setTreeNodeTopicName($treeNodeTopicName)
+    {
+        $db = Loader::db();
+        $db->Replace('TreeTopicNodes',
+            array('treeNodeID' => $this->getTreeNodeID(), 'treeNodeTopicName' => $treeNodeTopicName),
+            array('treeNodeID'), true);
+        $this->treeNodeTopicName = $treeNodeTopicName;
+    }
 
     public function importNode(\SimpleXMLElement $sx, $parent = false)
     {
-        return static::add((string) $sx['name'], $parent);
+        return static::add((string)$sx['name'], $parent);
     }
 
-    public static function add($treeNodeTopicName, $parent = false) {
-		$db = Loader::db();
-		$node = parent::add($parent);
-		$node->setTreeNodeTopicName($treeNodeTopicName);
-		return $node;
-	}
+    public static function add($treeNodeTopicName, $parent = false)
+    {
+        $db = Loader::db();
+        $node = parent::add($parent);
+        $node->setTreeNodeTopicName($treeNodeTopicName);
+        return $node;
+    }
 
+    /**
+     * return @Concrete\Core\Tree\Node\Type\Topic | null
+     */
+    public static function getNodeByName($name)
+    {
+        $db = Loader::db();
+        $treeNodeID = $db->GetOne('select treeNodeID from TreeTopicNodes where treeNodeTopicName = ?', array($name));
+        if ($treeNodeID) {
+            return static::getByID($treeNodeID);
+        }
+    }
 }
