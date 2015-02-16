ab6022abad4bc469a9d70c3fb5d8d6f015c0a684
Mike Lay <mike@mkly.io>
2015-02-16 16:10:47 -0600

    Fix to PSR-2
    Former-commit-id: 50f8a9c04f03ed4f29da15145601a1cab1a92299

--- web/concrete/src/Workflow/Progress/Category.php
+++ web/concrete/src/Workflow/Progress/Category.php
@@ -3,9 +3,10 @@ namespace Concrete\Core\Workflow\Progress;
 use \Concrete\Core\Foundation\Object;
 use Loader;
 use \Concrete\Core\Package\PackageList;
-class Category extends Object {
-
-	public static function getByID($wpCategoryID) {
+class Category extends Object
+{
+	public static function getByID($wpCategoryID)
+	{
 		$db = Loader::db();
 		$row = $db->GetRow('select wpCategoryID, wpCategoryHandle, pkgID from WorkflowProgressCategories where wpCategoryID = ?', array($wpCategoryID));
 		if (isset($row['wpCategoryID'])) {
@@ -15,7 +16,8 @@ class Category extends Object {
 		}
 	}
 
-	public static function getByHandle($wpCategoryHandle) {
+	public static function getByHandle($wpCategoryHandle)
+	{
 		$db = Loader::db();
 		$row = $db->GetRow('select wpCategoryID, wpCategoryHandle, pkgID from WorkflowProgressCategories where wpCategoryHandle = ?', array($wpCategoryHandle));
 		if (isset($row['wpCategoryID'])) {
@@ -25,7 +27,8 @@ class Category extends Object {
 		}
 	}
 
-	public static function exportList($xml) {
+	public static function exportList($xml)
+	{
 		$attribs = self::getList();
 		$axml = $xml->addChild('workflowprogresscategories');
 		foreach($attribs as $pkc) {
@@ -35,7 +38,8 @@ class Category extends Object {
 		}
 	}
 
-	public static function getListByPackage($pkg) {
+	public static function getListByPackage($pkg)
+	{
 		$db = Loader::db();
 		$list = array();
 		$r = $db->Execute('select wpCategoryID from WorkflowProgressCategories where pkgID = ? order by wpCategoryID asc', array($pkg->getPackageID()));
@@ -51,19 +55,22 @@ class Category extends Object {
 	public function getPackageID() {return $this->pkgID;}
 	public function getPackageHandle() {return PackageList::getHandle($this->pkgID);}
 
-	public function __call($method, $arguments) {
+	public function __call($method, $arguments)
+	{
 		$className = '\\Core\\Workflow\\Progress\\' . Loader::helper('text')->camelcase($this->wpCategoryHandle) . 'Progress';
 		$pkHandle = $this->getPackageHandle();
 		$class = core_class($className, $pkHandle);
 		return call_user_func_array(array($class, $method), $arguments);
 	}
 
-	public function delete() {
+	public function delete()
+	{
 		$db = Loader::db();
 		$db->Execute('delete from WorkflowProgressCategories where wpCategoryID = ?', array($this->wpCategoryID));
 	}
 
-	public static function getList() {
+	public static function getList()
+	{
 		$db = Loader::db();
 		$cats = array();
 		$r = $db->Execute('select wpCategoryID from WorkflowProgressCategories order by wpCategoryID asc');
@@ -73,7 +80,8 @@ class Category extends Object {
 		return $cats;
 	}
 
-	public static function add($wpCategoryHandle, $pkg = false) {
+	public static function add($wpCategoryHandle, $pkg = false)
+	{
 		$db = Loader::db();
 		if (is_object($pkg)) {
 			$pkgID = $pkg->getPackageID();
