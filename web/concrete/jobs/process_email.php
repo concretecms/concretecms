9681d74a74acff790d7d01d836429cf9947da3d7
EC-Joe <joemeyer@exchangecore.com>
2015-02-18 10:03:00 -0500

    Automated jobs psr2 / code cleanup & phpdocs
    Former-commit-id: efeddf5a8a0e456f7d8f32f7c2fbae67e10d41f5

--- web/concrete/jobs/process_email.php
+++ web/concrete/jobs/process_email.php
@@ -1,39 +1,44 @@
-<?
+<?php
 namespace Concrete\Job;
+
 use \Job as AbstractJob;
 use \Concrete\Core\Mail\Importer\MailImporter;
-class ProcessEmail extends AbstractJob {
+use Loader;
 
-	public function getJobName() {
-		return t("Process Email Posts");
-	}
+class ProcessEmail extends AbstractJob
+{
 
-	public function getJobDescription() {
-		return t("Polls an email account and grabs private messages/postings that are sent there..");
-	}
+    public function getJobName()
+    {
+        return t("Process Email Posts");
+    }
 
-	public function run() {
+    public function getJobDescription()
+    {
+        return t("Polls an email account and grabs private messages/postings that are sent there..");
+    }
 
-		$list = MailImporter::getEnabledList();
-		foreach($list as $mi) {
-			// for each one, we connect and retrieve any mail messages we haven't seen
-			$messages = $mi->getPendingMessages();
-			foreach($messages as $me) {
-				if ($me->validate()) {
-					$mi->process($me);
-				} elseif(!$me->isSendError()) {
-					$mh = Loader::helper('mail');
-					$mh->to($me->getOriginalSender());
-					$mh->from($mi->getMailImporterEmail());
-					$mh->addParameter('originalSubject', $me->getSubject());
-					$mh->addParameter('error', $mi->getValidationErrorMessage());
-					$mh->load('mail_importer_error');
-					$mh->sendMail();
-				}
-				$mi->cleanup($me);
-			}
-		}
-	}
+    public function run()
+    {
+        $mailImporter = new MailImporter();
+        $list = $mailImporter->getEnabledList();
+        foreach ($list as $mi) {
+            // for each one, we connect and retrieve any mail messages we haven't seen
+            $messages = $mi->getPendingMessages();
+            foreach ($messages as $me) {
+                if ($me->validate()) {
+                    $mi->process($me);
+                } elseif (!$me->isSendError()) {
+                    $mh = Loader::helper('mail');
+                    $mh->to($me->getOriginalSender());
+                    $mh->from($mi->getMailImporterEmail());
+                    $mh->addParameter('originalSubject', $me->getSubject());
+                    $mh->addParameter('error', $mi->getValidationErrorMessage());
+                    $mh->load('mail_importer_error');
+                    $mh->sendMail();
+                }
+                $mi->cleanup($me);
+            }
+        }
+    }
 }
-
-?>
