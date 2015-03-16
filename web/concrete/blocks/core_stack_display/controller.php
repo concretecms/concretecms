4229d0e9c20a1c5324f9ed2d3aa7574422a87e16
Andrew Embler <andrew@concrete5.org>
2015-03-16 13:34:37 -0700

    Fix #1854
    Former-commit-id: 7a161719d1dfb254cb5c0c93eda1df0bd51f2d7d

--- web/concrete/blocks/core_stack_display/controller.php
+++ web/concrete/blocks/core_stack_display/controller.php
@@ -1,5 +1,6 @@
 <?
 namespace Concrete\Block\CoreStackDisplay;
+
 use Stack;
 use Permissions;
 use Loader;
@@ -14,70 +15,114 @@ use Loader;
  *
  */
 use \Concrete\Core\Block\BlockController;
-class Controller extends BlockController {
 
-	protected $btCacheBlockRecord = true;
-	protected $btTable = 'btCoreStackDisplay';
-	protected $btIsInternal = true;		
-	public function getBlockTypeDescription() {
-		return t("Proxy block for stacks added through the UI.");
-	}
-	
-	public function getBlockTypeName() {
-		return t("Stack Display");
-	}
-	
-	public function getOriginalBlockID() {
-		return $this->bOriginalID;
-	}
+class Controller extends BlockController
+{
 
-	public function getImportData($blockNode, $page) {
-		$args = array();
-		$content = (string) $blockNode->stack;
-		$stack = Stack::getByName($content);
-		$args['stID'] = 0;
-		if (is_object($stack)) {
-			$args['stID'] = $stack->getCollectionID();
-		}
-		return $args;
-	}
-	
-	public function export(\SimpleXMLElement $blockNode) {			
-		$stack = Stack::getByID($this->stID);
-		if (is_object($stack)) {
-			$cnode = $blockNode->addChild('stack');
-			$node = dom_import_simplexml($cnode);
-			$no = $node->ownerDocument;
-			$node->appendChild($no->createCDataSection($stack->getCollectionName()));
-		}
-	}
-	
-	public function on_page_view($page) {
-		$stack = Stack::getByID($this->stID);
-		if (!is_object($stack)) {
-			return false;
-		}
-		$p = new Permissions($stack);
-		if ($p->canViewPage()) {
-			$blocks = $stack->getBlocks();
-			foreach($blocks as $b) {
-				$bp = new Permissions($b);
-				if ($bp->canViewBlock()) {
-					$btc = $b->getInstance();
-					if('Controller' != get_class($btc)){
-						$btc->outputAutoHeaderItems();
-					}
-					$csr = $b->getCustomStyle();
-					if (is_object($csr)) {
+    protected $btCacheBlockRecord = true;
+    protected $btTable = 'btCoreStackDisplay';
+    protected $btIsInternal = true;
+
+    public function getBlockTypeDescription()
+    {
+        return t("Proxy block for stacks added through the UI.");
+    }
+
+    public function getBlockTypeName()
+    {
+        return t("Stack Display");
+    }
+
+    public function getOriginalBlockID()
+    {
+        return $this->bOriginalID;
+    }
+
+    public function getImportData($blockNode, $page)
+    {
+        $args = array();
+        $content = (string)$blockNode->stack;
+        $stack = Stack::getByName($content);
+        $args['stID'] = 0;
+        if (is_object($stack)) {
+            $args['stID'] = $stack->getCollectionID();
+        }
+        return $args;
+    }
+
+    public function isValidControllerTask($method, $parameters = array())
+    {
+        $b = $this->findBlockForAction($method, $parameters);
+        return !empty($b);
+    }
+
+    public function runAction($action, $parameters = array())
+    {
+
+        $b = $this->findBlockForAction($action, $parameters);
+        if (empty($b)) {
+            return;
+        }
+
+        $controller = $b->getController();
+
+        return $controller->runAction($action, $parameters);
+    }
+
+    function findBlockForAction($method, $parameters)
+    {
+        $stack = Stack::getByID($this->stID);
+        if (!is_object($stack)) {
+            return null;
+        }
+        $blocks = $stack->getBlocks();
+        foreach ($blocks as $b) {
+            $controller = $b->getController();
+            if ($controller->isValidControllerTask($method, $parameters)) {
+                return $b;
+            }
+        }
+        return null;
+    }
+
+
+    public function export(\SimpleXMLElement $blockNode)
+    {
+        $stack = Stack::getByID($this->stID);
+        if (is_object($stack)) {
+            $cnode = $blockNode->addChild('stack');
+            $node = dom_import_simplexml($cnode);
+            $no = $node->ownerDocument;
+            $node->appendChild($no->createCDataSection($stack->getCollectionName()));
+        }
+    }
+
+    public function on_page_view($page)
+    {
+        $stack = Stack::getByID($this->stID);
+        if (!is_object($stack)) {
+            return false;
+        }
+        $p = new Permissions($stack);
+        if ($p->canViewPage()) {
+            $blocks = $stack->getBlocks();
+            foreach ($blocks as $b) {
+                $bp = new Permissions($b);
+                if ($bp->canViewBlock()) {
+                    $btc = $b->getInstance();
+                    if ('Controller' != get_class($btc)) {
+                        $btc->outputAutoHeaderItems();
+                    }
+                    $csr = $b->getCustomStyle();
+                    if (is_object($csr)) {
                         $styleHeader = '<style type="text/css" data-style-set="' . $csr->getStyleSet()->getID() . '">' . $csr->getCSS() . '</style>';
-						$btc->addHeaderItem($styleHeader);
-					}
-					$btc->runTask('on_page_view', array($page));
-				}
-			}			
-		}
-	}
+                        $btc->addHeaderItem($styleHeader);
+                    }
+                    $btc->runTask('on_page_view', array($page));
+                }
+            }
+        }
+    }
 
-	
-	
+
 }
