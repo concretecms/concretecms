789b87c3eb556d9024976f19b9c8001043c4ea0e
Michael Krasnow <mnkras@gmail.com>
2015-03-18 00:03:07 -0400

    PSR
    Former-commit-id: 4736e3476dec7e506c9739a04e48632cea9757af

--- web/concrete/controllers/single_page/dashboard/system/seo/urls.php
+++ web/concrete/controllers/single_page/dashboard/system/seo/urls.php
@@ -1,16 +1,18 @@
-<?
-
+<?php
 namespace Concrete\Controller\SinglePage\Dashboard\System\Seo;
+
 use \Concrete\Core\Page\Controller\DashboardPageController;
 use Loader;
 use Config;
-class Urls extends DashboardPageController {
+class Urls extends DashboardPageController
+{
     /**
     * Returns the mod_rewrite rules
     *
     * @return string
     */
-    public function getRewriteRules(){
+    public function getRewriteRules()
+    {
         $strRules = '
         <IfModule mod_rewrite.c>
         RewriteEngine On
@@ -29,7 +31,8 @@ class Urls extends DashboardPageController {
     *
     * @return string
     */
-    public function getHtaccessText(){
+    public function getHtaccessText()
+    {
         $strHt = '
         # -- concrete5 urls start --'
         . $this->getRewriteRules() . '
@@ -41,30 +44,33 @@ class Urls extends DashboardPageController {
 
 
     /**
-    * Dasboard page view
+    * Dashboard page view
     *
-    * @param string $strStatus - Result of attempting to update rewrite rules
+    * @param string|bool $strStatus - Result of attempting to update rewrite rules
     * @param boolean $blnHtu - Flag denoting if the .htaccess file was writable or not
     * @return void
     */
-    public function view($strStatus = false, $blnHtu = false){
+    public function view($strStatus = false, $blnHtu = false)
+    {
         $strStatus = (string) $strStatus;
         $blnHtu = (bool) $blnHtu;
         $intRewriting = Config::get('concrete.seo.url_rewriting') == 1 ? 1 : 0;
 
-        $this->set('fh', Loader::helper('form'));
+        $this->set('fh', \Core::make('helper/form'));
         $this->set('strRules', $this->getRewriteRules());
         $this->set('intRewriting', $intRewriting);
         $this->set('host', Config::get('concrete.seo.canonical_host'));
         $this->set('port', Config::get('concrete.seo.canonical_port'));
 
-        if($strStatus == 'saved'){
-            if(Config::get('concrete.seo.url_rewriting') && !$blnHtu){
-                $this->set('message', t('Settings Saved. URL rewriting enabled. You need to update .htaccess by hand.'));
-            }elseif(Config::get('concrete.seo.url_rewriting') && $blnHtu){
-                $this->set('message', t('Settings Saved. URL rewriting enabled. We were able to automatically update .htaccess file.'));
-            }else{
-                $this->set('message', t('Settings Saved.'));
+        if ($strStatus == 'saved') {
+            $message = t('Settings Saved.');
+            $urlmsg = t('URL rewriting enabled.');
+            if (Config::get('concrete.seo.url_rewriting') && !$blnHtu) {
+                $this->set('message', $message . ' ' . $urlmsg . ' ' . t('You need to update .htaccess by hand.'));
+            } elseif (Config::get('concrete.seo.url_rewriting') && $blnHtu) {
+                $this->set('message', $message . ' ' . $urlmsg . ' ' .t('We were able to automatically update .htaccess file.'));
+            } else {
+                $this->set('message', $message);
             }
         }
     }
@@ -75,9 +81,10 @@ class Urls extends DashboardPageController {
     *
     * @return void
     */
-    public function save_urls(){
+    public function save_urls()
+    {
 
-        if(!$this->token->validate('save_urls')){
+        if (!$this->token->validate('save_urls')) {
             $this->error->add($this->token->getErrorMessage());
         }
 
@@ -85,8 +92,7 @@ class Urls extends DashboardPageController {
             $strHtText = (string) $this->getHtaccessText();
             $blnHtu = 0;
 
-            if($this->isPost()){
-
+            if ($this->isPost()) {
                 Config::save('concrete.seo.canonical_host', $this->post('canonical_host'));
                 Config::save('concrete.seo.canonical_port', $this->post('canonical_port'));
 
@@ -94,28 +100,28 @@ class Urls extends DashboardPageController {
                 $intPosted = $this->post('URL_REWRITING') == 1 ? 1 : 0;
 
                 // If there was no change we don't attempt to edit/create the .htaccess file
-                if($intCurrent == $intPosted){
+                if ($intCurrent == $intPosted) {
                     $this->redirect('/dashboard/system/seo/urls');
                 }
 
                 Config::save('concrete.seo.url_rewriting', $intPosted);
 
-                if($this->post('URL_REWRITING') == 1){
-                    if(file_exists(DIR_BASE . '/.htaccess') && is_writable(DIR_BASE . '/.htaccess')){
-                        if(file_put_contents(DIR_BASE . '/.htaccess', $strHtText, FILE_APPEND)){
+                if ($this->post('URL_REWRITING') == 1) {
+                    if (file_exists(DIR_BASE . '/.htaccess') && is_writable(DIR_BASE . '/.htaccess')) {
+                        if (file_put_contents(DIR_BASE . '/.htaccess', $strHtText, FILE_APPEND)) {
                             $blnHtu = 1;
                         }
-                    }elseif(!file_exists(DIR_BASE . '/.htaccess') && is_writable(DIR_BASE)){
-                        if(file_put_contents(DIR_BASE . '/.htaccess', $strHtText)){
+                    } elseif (!file_exists(DIR_BASE . '/.htaccess') && is_writable(DIR_BASE)) {
+                        if (file_put_contents(DIR_BASE . '/.htaccess', $strHtText)) {
                             $blnHtu = 1;
                         }
                     }
-                }else{
-                    if(file_exists(DIR_BASE . '/.htaccess') && is_writable(DIR_BASE . '/.htaccess')){
+                } else {
+                    if (file_exists(DIR_BASE . '/.htaccess') && is_writable(DIR_BASE . '/.htaccess')) {
                         $fh = Loader::helper('file');
                         $contents = $fh->getContents(DIR_BASE . '/.htaccess');
 
-                        if(file_put_contents(DIR_BASE . '/.htaccess', str_replace($strHtText, '', $contents))){
+                        if (file_put_contents(DIR_BASE . '/.htaccess', str_replace($strHtText, '', $contents))) {
                             $blnHtu = 1;
                         }
                     }
