d8cd4d30848d12311e76dc3e463b726847e222ec
Andrew Embler <andrew@concrete5.org>
2015-03-18 14:06:17 -0700

    integrating redactor
    Former-commit-id: 8f93e49feaff783c28e1c1dc7aea603ddbe44fc8

--- web/concrete/blocks/content/add.php
+++ web/concrete/blocks/content/add.php
@@ -21,7 +21,7 @@ $(function() {
             lightbox: true
         },
 		'plugins': [
-            'fontcolor', 'concrete5inline', 'concrete5', 'underline'
+            'concrete5inline', 'concrete5'
 		]
 	});
 });
