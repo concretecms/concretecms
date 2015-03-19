83ae71c127e1fd5fa2d9c1d2051c000c3bc18cb9
Andrew Embler <andrew@concrete5.org>
2015-03-19 07:36:51 -0700

    more robust notification

--- web/concrete/blocks/core_conversation/form.php
+++ web/concrete/blocks/core_conversation/form.php
@@ -27,8 +27,7 @@ if ($controller->getTask() == 'add') {
     $maxFilesRegistered = Config::get('conversations.files.registered.max');
     $fileExtensions = implode(',', $fileAccessFileTypes);
     $attachmentsEnabled = intval(Config::get('conversations.attachments_enabled'));
-	$notification = (bool) Config::get('conversations.notification');
-	$notificationEmail = Config::get('conversations.notification_email');
+	$notificationUsers = Conversation::getDefaultSubscribedUsers();
 }
 
 if(!$dateFormat) {
@@ -218,15 +217,10 @@ if(!$dateFormat) {
 		</div>
 	</div>
 	<div class="form-group notification-overrides">
-		<div class="checkbox">
-			<label class="control-label">
-				<?=$form->checkbox('notification', 1, $notification)?> <?=t('Send an email when a message is posted.')?>
-			</label>
+		<div class="form-group">
+			<label class="control-label"><?=t('Users To Receive Conversation Notifications')?></label>
+			<?=Core::make("helper/form/user_selector")->selectMultipleUsers('notificationUsers', $notificationUsers)?>
 		</div>
-	</div>
-	<div class="form-group notification-overrides">
-		<label class="control-label"><?=t('Email Address')?></label>
-		<?=$form->text('notificationEmail', $notification > 0 ? $notificationEmail : '')?>
 	</div>
 </fieldset>
 
@@ -253,11 +247,9 @@ $(function() {
 	$('input[name=notificationOverridesEnabled]').on('change', function() {
 		var ao = $('input[name=notificationOverridesEnabled]:checked');
 		if (ao.val() == 1) {
-			$('.notification-overrides input').prop('disabled', false);
-			$('.notification-overrides label').removeClass('text-muted');
+			$('.notification-overrides').show();
 		} else {
-			$('.notification-overrides input').prop('disabled', true);
-			$('.notification-overrides label').addClass('text-muted');
+			$('.notification-overrides').hide();
 		}
 	}).trigger('change');
 });
