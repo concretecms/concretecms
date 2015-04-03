d6d3a5a7f4c0a694b85cba26c2e86a23b435e4aa
Michele Locati <michele@locati.it>
2015-04-03 11:37:14 +0200

    Fix phpDoc

--- web/concrete/src/Url/Resolver/UrlResolverInterface.php
+++ web/concrete/src/Url/Resolver/UrlResolverInterface.php
@@ -7,12 +7,12 @@ interface UrlResolverInterface
     /**
      * Resolve url's from any type of input
      *
-     * This method MUST either return a `\League\URL\URL` when a url is resolved
+     * This method MUST either return a `\League\Url\Url` when a url is resolved
      * or null when a url cannot be resolved.
      *
      * @param array                    $arguments A list of the arguments
-     * @param \League\URL\URLInterface $resolved
-     * @return \League\URL\URLInterface
+     * @param \League\Url\UrlInterface $resolved
+     * @return \League\Url\UrlInterface
      */
     public function resolve(array $arguments, $resolved = null);
 
