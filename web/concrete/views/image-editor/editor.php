2135ff159c48e094f7d1774eb94ceb316a0e1d17
Andrew Embler <andrew@concrete5.org>
2015-04-10 13:19:02 -0700

    tweaks to urls
    Former-commit-id: 9d30898896d91b77fa9db2ca030b3e3ce9f1e3c0
    Former-commit-id: d11e179ce4af13958117c4c6423859c398efcd27

--- web/concrete/views/image-editor/editor.php
+++ web/concrete/views/image-editor/editor.php
@@ -93,7 +93,7 @@ foreach ($filters as $filter) {
         $(function () {
             _.defer(function () {
                 var defaults = {
-                        saveUrl: CCM_APPLICATION_URL + '/index.php/tools/required/files/importers/imageeditor',
+                        saveUrl: CCM_DISPATCHER_FILENAME + '/tools/required/files/importers/imageeditor',
                         src: '<?=$fv->getURL()?>',
                         fID: <?= $fv->getFileID() ?>,
                         controlsets: {},
