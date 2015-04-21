c75e51e4f5b18ca151e9f2ed48921d2a3b7fe366
Michele Locati <michele@locati.it>
2015-04-21 15:57:43 +0200

    Correct handling of canonical URL in CLI enviro
    Former-commit-id: 43ff44d2aff51cd2a57a0e917ff489a841f22e94
    Former-commit-id: 8bef62eff71d7cf18f22c521073ffe87e2b582b8

--- web/concrete/src/Url/Resolver/CanonicalUrlResolver.php
+++ web/concrete/src/Url/Resolver/CanonicalUrlResolver.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Concrete\Core\Url\Resolver;
 
 use Concrete\Core\Url\Url;
@@ -7,44 +8,48 @@ use Concrete\Core\Http\Request;
 
 class CanonicalUrlResolver implements UrlResolverInterface
 {
+   /**
+    * Resolve url's from any type of input.
+    *
+    * This method MUST either return a `\League\URL\URL` when a url is resolved
+    * or null when a url cannot be resolved.
+    *
+    * @param array                    $arguments A list of the arguments
+    * @param \League\URL\URLInterface $resolved
+    *
+    * @return \League\URL\URLInterface
+    */
+   public function resolve(array $arguments, $resolved = null)
+   {
+       $url = Url::createFromUrl('', (bool) \Config::get('concrete.seo.trailing_slash'));
 
-    /**
-     * Resolve url's from any type of input
-     *
-     * This method MUST either return a `\League\URL\URL` when a url is resolved
-     * or null when a url cannot be resolved.
-     *
-     * @param array                    $arguments A list of the arguments
-     * @param \League\URL\URLInterface $resolved
-     * @return \League\URL\URLInterface
-     */
-    public function resolve(array $arguments, $resolved = null)
-    {
-        $url = Url::createFromUrl('', (bool) \Config::get('concrete.seo.trailing_slash'));
+       $url->setHost(null);
+       $url->setScheme(null);
 
-        $url->setHost(null);
-        $url->setScheme(null);
-
-        if (\Config::get('concrete.seo.canonical_url')) {
-            $canonical = UrlImmutable::createFromUrl(\Config::get('concrete.seo.canonical_url'),
+       if (\Config::get('concrete.seo.canonical_url')) {
+           $canonical = UrlImmutable::createFromUrl(\Config::get('concrete.seo.canonical_url'),
                 (bool) \Config::get('concrete.seo.trailing_slash')
             );
-            $url->getHost()->set($canonical->getHost());
-            $url->getScheme()->set($canonical->getScheme());
-            if (intval($canonical->getPort()->get()) > 0) {
-                $url->getPort()->set($canonical->getPort());
-            }
-        } else {
-            $url->getHost()->set(Request::getInstance()->getHost());
-            $url->setScheme(Request::getInstance()->getScheme());
-            $url->setPortIfNecessary(Request::getInstance()->getPort());
-        }
+           $url->getHost()->set($canonical->getHost());
+           $url->getScheme()->set($canonical->getScheme());
+           if (intval($canonical->getPort()->get()) > 0) {
+               $url->getPort()->set($canonical->getPort());
+           }
+       } else {
+           $scheme = \Request::getInstance()->getScheme();
+           $host = \Request::getInstance()->getHost();
+           if ($scheme && $host) {
+               $url
+                   ->setScheme($scheme)
+                   ->setHost($host)
+                   ->setPortIfNecessary(Request::getInstance()->getPort());
+           }
+       }
 
-        if ($relative_path = \Core::getApplicationRelativePath()) {
-            $url = $url->setPath($relative_path);
-        }
+       if ($relative_path = \Core::getApplicationRelativePath()) {
+           $url = $url->setPath($relative_path);
+       }
 
-        return UrlImmutable::createFromUrl($url);
-    }
-
+       return UrlImmutable::createFromUrl($url);
+   }
 }
