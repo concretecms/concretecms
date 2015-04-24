6ff45c81c4dcc67eac23826e97ec0b7cf41494de
Andrew Embler <andrew@concrete5.org>
2015-04-24 12:23:44 -0700

    fix redirection on accessiblity settings page

--- web/concrete/controllers/single_page/dashboard/system/basics/accessibility.php
+++ web/concrete/controllers/single_page/dashboard/system/basics/accessibility.php
@@ -5,19 +5,26 @@ use Concrete\Core\Page\Controller\DashboardPageController;
 use Request;
 use Config;
 
-class Accessibility extends DashboardPageController {
+class Accessibility extends DashboardPageController
+{
 
-    public function view() {
+    public function view()
+    {
         $this->set('show_titles', Config::get('concrete.accessibility.toolbar_titles'));
         $this->set('increase_font_size', Config::get('concrete.accessibility.toolbar_large_font'));
     }
 
-    public function save() {
-        Config::save('concrete.accessibility.toolbar_titles', !!Request::post('show_titles', false));
-        Config::save('concrete.accessibility.toolbar_large_font', !!Request::post('increase_font_size', false));
-
+    public function saved()
+    {
         $this->set('message', t('Successfully saved accessibility settings.'));
         $this->view();
+    }
+
+    public function save()
+    {
+        Config::save('concrete.accessibility.toolbar_titles', !!Request::post('show_titles', false));
+        Config::save('concrete.accessibility.toolbar_large_font', !!Request::post('increase_font_size', false));
+        $this->redirect('/dashboard/system/basics/accessibility', 'saved');
     }
 
 }
