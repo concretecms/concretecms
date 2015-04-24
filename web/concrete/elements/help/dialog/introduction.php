02abaf25922d39e5bb3d2e84dc60657d7962b255
Andrew Embler <andrew@concrete5.org>
2015-04-24 08:36:30 -0700

    miscellaneous fixes and tweaks

--- web/concrete/elements/help/dialog/introduction.php
+++ web/concrete/elements/help/dialog/introduction.php
@@ -6,53 +6,53 @@ $ag->requireAsset('core/lightbox');
 <div id="ccm-dialog-help" class="ccm-ui">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-xs-8">
                 <h2><?=t('Learn the basics.')?></h2>
                 <div class="spacer-row-4"></div>
                 <div class="container-fluid">
                     <div class="row">
-                    <div class="col-md-offset-1 col-md-11">
+                    <div class="col-xs-offset-1 col-xs-11">
                         <div class="ccm-dialog-help-item">
                             <h4><?=t('Use the toolbar')?></h4>
                             <ol class="breadcrumb">
                                 <li><a data-lightbox="iframe" href="https://www.youtube.com/watch?v=VB-R71zk06U"><?=t('Watch Video')?></a></li>
-                                <li><a href="#" data-launch-guide="toolbar"><?=t('Run Guide')?></a></li>
+                                <li class="hidden-xs"><a href="#" data-launch-guide="toolbar"><?=t('Run Guide')?></a></li>
                             </ol>
                         </div>
                         <div class="ccm-dialog-help-item">
                             <h4><?=t('Change content')?></h4>
                             <ol class="breadcrumb">
                                 <li><a href="https://www.youtube.com/watch?v=Y1VmBVffLM0" data-lightbox="iframe"><?=t('Watch Video')?></a></li>
-                                <li><a href="#" data-launch-guide="change-content"><?=t('Run Guide')?></a></li>
+                                <li class="hidden-xs"><a href="#" data-launch-guide="change-content"><?=t('Run Guide')?></a></li>
                             </ol>
                         </div>
                         <div class="ccm-dialog-help-item">
                             <h4><?=t('Add a page')?></h4>
                             <ol class="breadcrumb">
                                 <li><a href="https://www.youtube.com/watch?v=mWTNga4_O_Q" data-lightbox="iframe"><?=t('Watch Video')?></a></li>
-                                <li><a href="#" data-launch-guide="add-page"><?=t('Run Guide')?></a></li>
+                                <li class="hidden-xs"><a href="#" data-launch-guide="add-page"><?=t('Run Guide')?></a></li>
                             </ol>
                         </div>
                         <div class="ccm-dialog-help-item">
                             <h4><?=t('Personalize your site')?></h4>
                             <ol class="breadcrumb">
                                 <li><a href="https://www.youtube.com/watch?v=xI8dUNAc6fU" data-lightbox="iframe"><?=t('Watch Video')?></a></li>
-                                <li><a href="#" data-launch-guide="personalize"><?=t('Run Guide')?></a></li>
+                                <li class="hidden-xs"><a href="#" data-launch-guide="personalize"><?=t('Run Guide')?></a></li>
                             </ol>
                         </div>
                         <div class="ccm-dialog-help-item">
                             <h4><?=t('Cleanup and organize your site')?></h4>
                             <ol class="breadcrumb">
                                 <li><a href="https://www.youtube.com/watch?v=_NhlWLU_L6E" data-lightbox="iframe"><?=t('Watch Video')?></a></li>
-                                <li><a href="#" data-launch-guide="dashboard"><?=t('Run Guide')?></a></li>
+                                <li class="hidden-xs"><a href="#" data-launch-guide="dashboard"><?=t('Run Guide')?></a></li>
                             </ol>
                         </div>
                     </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-accented">
-                <h2><?=t('Find more help.')?></h2>
+            <div class="col-xs-4 col-accented">
+                <h2><?=t('More help.')?></h2>
                 <div class="spacer-row-4"></div>
 
                 <div class="ccm-dialog-help-item">
