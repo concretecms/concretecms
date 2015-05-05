451c5a7c4e1c2ce02c150c25157fe2af51f16d24
Michael Krasnow <mnkras@gmail.com>
2015-05-05 12:08:44 -0400

    Some more escaping
    Former-commit-id: a144fa42d8c1436b289939953bbb70099b84b949
    Former-commit-id: b5a2c9359930b42a143b947c0f781d70759e01ad

--- web/concrete/elements/group/search.php
+++ web/concrete/elements/group/search.php
@@ -17,7 +17,7 @@ $registeredGroupNode = GroupTreeNode::getTreeNodeByGroupID(REGISTERED_GROUP_ID);
 <div data-search="groups">
 <script type="text/template" data-template="search-form">
 <form role="form" data-search-form="groups" action="<?=URL::to('/ccm/system/search/groups/submit')?>" class="form-inline ccm-search-fields ccm-search-fields-none">
-	<input type="hidden" name="filter" value="<?=$searchRequest['filter']?>" />
+	<input type="hidden" name="filter" value="<?=h($searchRequest['filter'])?>" />
 
 	<div class="ccm-search-fields-row">
 	<div class="form-group">
