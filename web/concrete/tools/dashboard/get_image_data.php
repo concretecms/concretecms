3c2d5a46e2ef28f16f34bd017d39c067c1913098
Michele Locati <michele@locati.it>
2015-05-06 09:30:09 +0200

    PSR

--- web/concrete/tools/dashboard/get_image_data.php
+++ web/concrete/tools/dashboard/get_image_data.php
@@ -1,23 +1,23 @@
-<?
+<?php
+
 defined('C5_EXECUTE') or die("Access Denied.");
 session_write_close();
 
 // first, we check to see if the dashboard image data has been set in the cache
 if (Config::get('concrete.urls.background_info')) {
-	if ($_REQUEST['image'] && preg_match('/([0-9]+)\.jpg/i', $_REQUEST['image'])) {
+    if ($_REQUEST['image'] && preg_match('/([0-9]+)\.jpg/i', $_REQUEST['image'])) {
         /** @var \Stash\Interfaces\ItemInterface $imageDataCache */
         $imageDataCache = Core::make('cache')->getItem('dashboard_image_data/' . $_REQUEST['image']);
-		if ($imageDataCache->isMiss()) {
-			// call out to the server to grab the data
+        if ($imageDataCache->isMiss()) {
+            // call out to the server to grab the data
             $imageDataCache->lock();
-			$cfToken = Marketplace::getSiteToken();
+            $cfToken = Marketplace::getSiteToken();
             $imageData = Loader::helper('file')->getContents(Config::get('concrete.urls.background_info') . '?image=' . $_REQUEST['image'] . '&cfToken=' . $cfToken);
             $imageDataCache->set($imageData);
-		} else {
+        } else {
             $imageData = $imageDataCache->get();
         }
-	}
+    }
 
-	print $imageData;
-
+    print $imageData;
 }
