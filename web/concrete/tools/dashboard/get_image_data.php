a97e589620f14119fb27a62af30383055c58d5ca
Michele Locati <michele@locati.it>
2015-05-06 09:32:38 +0200

    Avoid warnings and check if image URL already contains a "?"
    Former-commit-id: 57121750a38bf962b88296845bfa6d2135715aa8
    Former-commit-id: b3fa401f85a58d33e73e675e9de118531b8b01f2

--- web/concrete/tools/dashboard/get_image_data.php
+++ web/concrete/tools/dashboard/get_image_data.php
@@ -5,19 +5,23 @@ session_write_close();
 
 // first, we check to see if the dashboard image data has been set in the cache
 if (Config::get('concrete.urls.background_info')) {
-    if ($_REQUEST['image'] && preg_match('/([0-9]+)\.jpg/i', $_REQUEST['image'])) {
+    if (isset($_REQUEST['image']) && is_string($_REQUEST['image']) && preg_match('/([0-9]+)\.jpg/i', $_REQUEST['image'])) {
         /** @var \Stash\Interfaces\ItemInterface $imageDataCache */
         $imageDataCache = Core::make('cache')->getItem('dashboard_image_data/' . $_REQUEST['image']);
         if ($imageDataCache->isMiss()) {
             // call out to the server to grab the data
             $imageDataCache->lock();
             $cfToken = Marketplace::getSiteToken();
-            $imageData = Loader::helper('file')->getContents(Config::get('concrete.urls.background_info') . '?image=' . $_REQUEST['image'] . '&cfToken=' . $cfToken);
+            $imageDataUrl = Config::get('concrete.urls.background_info');
+            $imageDataUrl .= (strpos($imageDataUrl, '?') === false) ? '?' : '&';
+            $imageDataUrl .= 'image=' . $_REQUEST['image'] . '&cfToken=' . $cfToken;
+            $imageData = Core::make('helper/file')->getContents($imageDataUrl);
             $imageDataCache->set($imageData);
         } else {
             $imageData = $imageDataCache->get();
         }
+        if ($imageData !== false) {
+            print $imageData;
+        }
     }
-
-    print $imageData;
 }
