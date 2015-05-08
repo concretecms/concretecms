b23d4a17fc5aef9882a611df6036ea3f6adc65e7
Michele Locati <michele@locati.it>
2015-05-08 10:10:50 +0200

    PSR

--- web/concrete/src/Permission/Access/Access.php
+++ web/concrete/src/Permission/Access/Access.php
@@ -1,19 +1,19 @@
 <?php
+
 namespace Concrete\Core\Permission\Access;
 
-use \Concrete\Core\Foundation\Object;
+use Concrete\Core\Foundation\Object;
 use Loader;
 use CacheLocal;
 use PermissionKey;
 use User;
 use Core;
-use \Concrete\Core\Permission\Access\Entity\Entity as PermissionAccessEntity;
-use \Concrete\Core\Permission\Duration as PermissionDuration;
-use \Concrete\Core\Workflow\Workflow;
+use Concrete\Core\Permission\Access\Entity\Entity as PermissionAccessEntity;
+use Concrete\Core\Permission\Duration as PermissionDuration;
+use Concrete\Core\Workflow\Workflow;
 
 class Access extends Object
 {
-
     protected $paID;
     protected $paIDList = array();
 
@@ -78,6 +78,7 @@ class Access extends Object
             }
             $list[] = $obj;
         }
+
         return $list;
     }
 
@@ -89,6 +90,7 @@ class Access extends Object
                 $entities[] = $ae;
             }
         }
+
         return $entities;
     }
 
@@ -106,6 +108,7 @@ class Access extends Object
                 $valid = false;
             }
         }
+
         return $valid;
     }
 
@@ -116,6 +119,7 @@ class Access extends Object
             return true;
         }
         $accessEntities = $u->getUserAccessEntityObjects();
+
         return $this->validateAccessEntities($accessEntities);
     }
 
@@ -128,6 +132,7 @@ class Access extends Object
         }
         $p->pk = $permissions[0]->pk;
         $p->paID = -1;
+
         return $p;
     }
 
@@ -138,6 +143,7 @@ class Access extends Object
                     ',',
                     $this->paIDList
                 ) . ')';
+
             return $this->deliverAccessListItems($q, $accessType, $filterEntities);
         } else {
             $filter = $accessType . ':';
@@ -160,6 +166,7 @@ class Access extends Object
                 $this->getPermissionAccessID() . $filter . strtolower(get_class($this->pk)),
                 $items
             );
+
             return $items;
         }
     }
@@ -179,6 +186,7 @@ class Access extends Object
         } else {
             $accessType = ' and accessType = ' . $accessType;
         }
+
         return $peIDs . ' ' . $accessType . ' order by accessType desc'; // we order desc so that excludes come last (-1)
     }
 
@@ -213,6 +221,7 @@ class Access extends Object
                 $workflows[] = $wf;
             }
         }
+
         return $workflows;
     }
 
@@ -231,6 +240,7 @@ class Access extends Object
             $newPA->attachWorkflow($wf);
         }
         $newPA->setPermissionKey($this->pk);
+
         return $newPA;
     }
 
@@ -239,7 +249,6 @@ class Access extends Object
         $db = Loader::db();
         $db->Execute('update PermissionAccess set paIsInUse = 1 where paID = ?', array($this->paID));
     }
-
 
     public function addListItem(
         PermissionAccessEntity $pae,
@@ -258,7 +267,7 @@ class Access extends Object
                 'paID' => $this->getPermissionAccessID(),
                 'peID' => $pae->getAccessEntityID(),
                 'pdID' => $pdID,
-                'accessType' => $accessType
+                'accessType' => $accessType,
             ),
             array('paID', 'peID'),
             false
@@ -282,13 +291,14 @@ class Access extends Object
     {
         $db = Loader::db();
         $db->Execute('insert into PermissionAccess (paIsInUse) values (0)');
+
         return static::getByID($db->Insert_ID(), $pk);
     }
 
     public static function getByID($paID, PermissionKey $pk, $checkPA = true)
     {
         $cache = Core::make('cache/request');
-    	$identifier = sprintf('permission/access/%s/%s', $pk->getPermissionKeyID(), $paID);
+        $identifier = sprintf('permission/access/%s/%s', $pk->getPermissionKeyID(), $paID);
         $item = $cache->getItem($identifier);
         if (!$item->isMiss()) {
             return $item->get();
@@ -320,7 +330,7 @@ class Access extends Object
         }
 
         $item->set($obj);
+
         return $obj;
     }
-
 }
