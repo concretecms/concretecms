48b0554cd3fc8e3076c3c2f554838ef21a2428fd
Michele Locati <michele@locati.it>
2015-05-13 08:40:02 +0200

    Avoid same method and class names
    Former-commit-id: d95d2ddf263221df67c73c61981a2b7bf254d822
    Former-commit-id: 9f6d14546bc8aa13af38481bf48ada089725eb75

---
+++ web/concrete/src/Http/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Concrete\Core\Http;
+
+/**
+ * An object that represents a particular request to the Concrete-powered website. The request object then determines what is being requested, based on the path, and presents itself to the rest of the dispatcher (which loads the page, etc...).
+ *
+ * @package    Core
+ * @author     Andrew Embler <andrew@concrete5.org>
+ * @category   Concrete
+ * @copyright  Copyright (c) 2003-2008 Concrete5. (http://www.concrete5.org)
+ * @license    http://www.concrete5.org/license/     MIT License
+ */
+class Request extends RequestBase
+{
+}
