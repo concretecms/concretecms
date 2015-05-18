79f09ce49f53dba3eff69364422da9ddfe122e25
Andrew Embler <andrew@concrete5.org>
2015-05-18 14:33:02 -0700

    Migrations merge

--- web/concrete/src/Updater/Migrations/Migrations/Version20150515000000.php
+++ web/concrete/src/Updater/Migrations/Migrations/Version20150515000000.php
@@ -23,9 +23,38 @@ class Version20150515000000 extends AbstractMigration
         // I can't seem to get the doctrine cache to clear any other way.
         $cms = \Core::make('app');
         $cms->clearCaches();
+
+        $this->purgeOrphanedScrapbooksBlocks();
     }
 
     public function down(Schema $schema)
     {
     }
+
+    protected function purgeOrphanedScrapbooksBlocks()
+    {
+        $db = \Database::connection();
+        $orphanedCollectionVersionBlocks = $db->fetchAll(
+            '
+            select cID, cvID, cvb.bID, arHandle
+            from CollectionVersionBlocks cvb
+                inner join btCoreScrapbookDisplay btCSD on cvb.bID = btCSD.bID
+                inner join Blocks b on b.bID = btCSD.bOriginalID
+                left join BlockTypes bt on b.btID = bt.btID
+            where bt.btID IS NULL',
+            array()
+        );
+        foreach($orphanedCollectionVersionBlocks AS $row) {
+            $nc = \Page::getByID($row['cID'], $row['cvID']);
+            if (!is_object($nc) || $nc->isError()) {
+                continue;
+            }
+            $b = \Block::getByID($row['bID'], $nc, $row['arHandle']);
+            if (is_object($b)) {
+                $b->deleteBlock();
+            }
+        }
+    }
+
+
 }
