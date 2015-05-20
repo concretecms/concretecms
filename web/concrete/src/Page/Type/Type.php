cd8db4c4f9be7f1bc8b23fa4211682dc421985eb
Andrew Embler <andrew@concrete5.org>
2015-05-20 06:24:05 -0700

    Add warning for page type delete based on count of pages

--- web/concrete/src/Page/Type/Type.php
+++ web/concrete/src/Page/Type/Type.php
@@ -581,6 +581,13 @@ class Type extends Object implements \Concrete\Core\Permission\ObjectInterface
         }
     }
 
+    public function getPageTypeUsageCount()
+    {
+        $db = Loader::db();
+        $count = $db->GetOne('select count(cID) from Pages where cIsTemplate = 0 and ptID = ? and cIsActive = 1', array($this->ptID));
+        return $count;
+    }
+
     public function duplicate($ptHandle, $ptName)
     {
         $data = array(
