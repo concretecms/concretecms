7fa201c05f041699dd9fb9a47583f0f893be2e9f
Ryan Hewitt <ryan@mesuva.com.au>
2015-06-02 21:27:51 +0930

    Addition of option to force download
    This change:
    - Adds the option to force a linked file to download
    - Uses the newer  ->getDownloadURL() call
    - Increases the height of the form block interface to fit the new
    checkbox

--- web/concrete/blocks/file/edit.php
+++ web/concrete/blocks/file/edit.php
@@ -7,10 +7,14 @@
     }
 ?>
 <div class="form-group">
-<?=$form->label('fID', t('File'))?>
-<?=$al->file('ccm-b-file', 'fID', t('Choose File'), $bf);?>
+	<?=$form->label('fID', t('File'))?>
+	<?=$al->file('ccm-b-file', 'fID', t('Choose File'), $bf);?>
 </div>
 <div class="form-group">
-<?=$form->label('fileLinkText', t('Text for Linked File'))?>
-<?=$form->text('fileLinkText', $controller->getLinkText())?>
+	<?=$form->label('fileLinkText', t('Text for Linked File'))?>
+	<?=$form->text('fileLinkText', $controller->getLinkText())?>
+</div>
+<div class="form-group">
+	<?=$form->checkbox('forceDownload', '1', $forceDownload); ?>
+	<?=$form->label('forceDownload', t('Force file to download')); ?>
 </div>
