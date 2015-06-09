66b9917330bf4f9f6843ea18a84c095da82ebde8
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-06-09 13:10:53 +0900

    Check proper permissions for buttons on composer form

--- web/concrete/elements/page_types/composer/form/output/buttons.php
+++ web/concrete/elements/page_types/composer/form/output/buttons.php
@@ -1,9 +1,25 @@
 <?
 defined('C5_EXECUTE') or die("Access Denied.");
 $cmpp = new Permissions($pagetype);
+$cp = new Permissions($page)
 ?>
 
-<button type="button" data-page-type-composer-form-btn="publish" class="btn btn-primary pull-right"><?=t('Publish')?></button>
+<? if ($cp->canApprovePageVersions()) {
+	if ($page->isPageDraft()) {
+		$publishTitle = t('Publish');
+	} else {
+		$publishTitle = t('Publish');
+		$pk = PermissionKey::getByHandle('approve_page_versions');
+		$pk->setPermissionObject($page);
+		$pa = $pk->getPermissionAccessObject();
+		if (is_object($pa) && count($pa->getWorkflows()) > 0) {
+			$publishTitle = t('Submit');
+		}
+	}
+	?>
+	<button type="button" data-page-type-composer-form-btn="publish" class="btn btn-primary pull-right"><?=$publishTitle?></button>
+	<?
+} ?>
 
 <? if (!is_object($page) || $page->isPageDraft()) { ?>
 <button type="button" data-page-type-composer-form-btn="preview" class="btn btn-success pull-right"><?=t('Edit Mode')?></button>
@@ -11,8 +27,10 @@ $cmpp = new Permissions($pagetype);
 <button type="button" data-page-type-composer-form-btn="preview" class="btn btn-success pull-right"><?=t('Save')?></button>
 <? } ?>
 
-<? if (is_object($page) && $page->isPageDraft()) { ?>
+<? if (is_object($page) && $page->isPageDraft()) {
+	if ($cp->canDeletePage()) { ?>
 	<button type="button" data-page-type-composer-form-btn="discard" class="btn btn-danger pull-left"><?=t('Discard Draft')?></button>
+	<? } ?>
     <button type="button" data-page-type-composer-form-btn="exit" class="btn btn-default pull-left"><?=t('Save and Exit')?></button>
 <? } ?>
 
