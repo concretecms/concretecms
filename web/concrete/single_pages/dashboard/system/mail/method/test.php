2905b50bdb1ee461f93cf763808e78ced4cfe182
Michele Locati <michele@locati.it>
2015-06-17 19:35:40 +0200

    Remove old constant ENABLE_EMAILS

--- web/concrete/single_pages/dashboard/system/mail/method/test.php
+++ web/concrete/single_pages/dashboard/system/mail/method/test.php
@@ -9,7 +9,7 @@ $fh = Loader::helper('form');
         <div class="col-md-6">
             <?php
             if(!Config::get('concrete.email.enabled')) {
-                ?><div class="alert alert-info"><?php echo t(/*i18n: %1$s is a configuration name, %2$s is a configuration value*/'It\'s not possible to test the settings since the mail system is disabled (the setting %1$s is set to %2$s in the configuration).', '<b>ENABLE_EMAILS</b>', '<b>false</b>'); ?></div><?php
+                ?><div class="alert alert-info"><?php echo t(/*i18n: %1$s is a configuration name, %2$s is a configuration value*/'It\'s not possible to test the settings since the mail system is disabled (the setting %1$s is set to %2$s in the configuration).', '<b>concrete.email.enabled</b>', '<b>false</b>'); ?></div><?php
             }
             else {
                 echo Loader::helper('validation/token')->output('test'); ?>
