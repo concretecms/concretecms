0ac5cc881af7f06422a90babbca244ab81bf341a
Chris Hougard <chris@exchangecore.com>
2015-06-19 22:00:42 -0500

    Code formatting
    Former-commit-id: 511e2d87d6af6026dbd5f96303871857a498e22b
    Former-commit-id: 816f4c4bcdabaeb2f1326fbe1d4175c3bb586607

--- web/concrete/src/Console/Command/JobCommand.php
+++ web/concrete/src/Console/Command/JobCommand.php
@@ -2,18 +2,17 @@
 
 namespace Concrete\Core\Console\Command;
 
+use Job;
+use JobSet;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use JobSet;
-use Job;
 
 class JobCommand extends Command
 {
-
     protected function configure()
     {
         $this
@@ -104,5 +103,4 @@ class JobCommand extends Command
             }
         }
     }
-
 }
