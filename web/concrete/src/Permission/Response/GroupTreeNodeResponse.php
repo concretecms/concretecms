65c6065028b4d56108e350baf7c6d034b7ff0a96
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-06-23 02:30:20 +0900

    and PSR CS Fix
    Former-commit-id: 1fca5b6ccc8e3f302c2dfbe85d93dad0b8ecab00
    Former-commit-id: 0b515b82c76d3f0d195c6c189e80180b478d615d

--- web/concrete/src/Permission/Response/GroupTreeNodeResponse.php
+++ web/concrete/src/Permission/Response/GroupTreeNodeResponse.php
@@ -1,34 +1,41 @@
 <?php
 namespace Concrete\Core\Permission\Response;
+
 use User;
 use Group;
 use PermissionKey;
 use TaskPermission;
-class GroupTreeNodeResponse extends TreeNodeResponse {
 
-	public function canEditTreeNodePermissions() {
-		return $this->validate('edit_group_permissions');
-	}
+class GroupTreeNodeResponse extends TreeNodeResponse
+{
+    public function canEditTreeNodePermissions()
+    {
+        return $this->validate('edit_group_permissions');
+    }
 
-	public function canViewTreeNode() {
-		$tp = new TaskPermission();
-		return $tp->canAccessGroupSearch();
-	}
+    public function canViewTreeNode()
+    {
+        $tp = new TaskPermission();
+        return $tp->canAccessGroupSearch();
+    }
 
-	public function canDuplicateTreeNode() {
-		return false;
-	}
+    public function canDuplicateTreeNode()
+    {
+        return false;
+    }
 
-	public function canEditTreeNode() {
-		return $this->validate('edit_group');
-	}
+    public function canEditTreeNode()
+    {
+        return $this->validate('edit_group');
+    }
 
-	public function canAddTreeSubNode() {
-		return $this->validate('add_sub_group');
-	}
+    public function canAddTreeSubNode()
+    {
+        return $this->validate('add_sub_group');
+    }
 
-	public function canDeleteTreeNode() {
-		return false;
-	}
-
+    public function canDeleteTreeNode()
+    {
+        return false;
+    }
 }
