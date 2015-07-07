b5119690a77915aaa50ecdd6cb550aa3ae4950bc
Michele Locati <michele@locati.it>
2015-07-07 12:11:25 +0200

    Make ConsoleRunner::run compatible with parent class method

--- web/concrete/src/Tools/Console/Doctrine/ConsoleRunner.php
+++ web/concrete/src/Tools/Console/Doctrine/ConsoleRunner.php
@@ -3,10 +3,10 @@
 namespace Concrete\Core\Tools\Console\Doctrine;
 
 use \Doctrine\ORM\Tools\Console\ConsoleRunner as DoctrineConsoleRunner;
+use Symfony\Component\Console\Helper\HelperSet as HelperSet;
 
 class ConsoleRunner extends DoctrineConsoleRunner
 {
-
     static public function run(HelperSet $helperSet, $commands = array())
     {
 die;    }
