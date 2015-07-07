a73a6e429aa21cb4f2fec7415121ebe0cb4540f8
Michele Locati <michele@locati.it>
2015-07-07 12:13:46 +0200

    PSR

--- web/concrete/src/Tools/Console/Doctrine/ConsoleRunner.php
+++ web/concrete/src/Tools/Console/Doctrine/ConsoleRunner.php
@@ -2,13 +2,13 @@
 
 namespace Concrete\Core\Tools\Console\Doctrine;
 
-use \Doctrine\ORM\Tools\Console\ConsoleRunner as DoctrineConsoleRunner;
+use Doctrine\ORM\Tools\Console\ConsoleRunner as DoctrineConsoleRunner;
 use Symfony\Component\Console\Helper\HelperSet as HelperSet;
 
 class ConsoleRunner extends DoctrineConsoleRunner
 {
-    static public function run(HelperSet $helperSet, $commands = array())
+    public static function run(HelperSet $helperSet, $commands = array())
     {
-die;    }
-
+        die;
+    }
 }
