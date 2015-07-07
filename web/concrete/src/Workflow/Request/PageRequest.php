b3caad8c413f222a299ea1163466f59e1fc3bfb3
Michele Locati <michele@locati.it>
2015-07-07 11:43:04 +0200

    PSR
    Former-commit-id: 90f25f34cfd7cd8cc0f664b42b61ec2fef0880ea
    Former-commit-id: 3784bcbb1e229a0940c70e619863cb7138a94a0f

--- web/concrete/src/Workflow/Request/PageRequest.php
+++ web/concrete/src/Workflow/Request/PageRequest.php
@@ -1,63 +1,64 @@
 <?php
+
 namespace Concrete\Core\Workflow\Request;
-use Loader;
+
 use Page;
-use \Concrete\Core\Workflow\Description as WorkflowDescription;
-use Permissions;
 use PermissionKey;
-use \Concrete\Core\Workflow\Progress\Progress as WorkflowProgress;
-use CollectionVersion;
-use Events;
-use \Concrete\Core\Workflow\Progress\Action\Action as WorkflowProgressAction;
-use \Concrete\Core\Workflow\Progress\Response as WorkflowProgressResponse;
-use \Concrete\Core\Workflow\Progress\PageProgress as PageWorkflowProgress;
+use Concrete\Core\Workflow\Progress\Progress as WorkflowProgress;
+use Concrete\Core\Workflow\Progress\Response as WorkflowProgressResponse;
+use Concrete\Core\Workflow\Progress\PageProgress as PageWorkflowProgress;
 
-abstract class PageRequest extends Request {
+abstract class PageRequest extends Request
+{
+    public function setRequestedPage($c)
+    {
+        $this->cID = $c->getCollectionID();
+    }
 
-	public function setRequestedPage($c) {
-		$this->cID = $c->getCollectionID();
-	}
+    public function getRequestedPageID()
+    {
+        return $this->cID;
+    }
 
-	public function getRequestedPageID() {
-		return $this->cID;
-	}
+    public function getRequestedPageVersionID()
+    {
+        if (isset($this->cvID)) {
+            return $cvID;
+        }
+        $c = Page::getByID($this->cID, 'RECENT');
 
-	public function getRequestedPageVersionID() {
-		if (isset($this->cvID)) {
-			return $cvID;
-		}
-		$c = Page::getByID($this->cID, 'RECENT');
-		return $c->getVersionID();
-	}
+        return $c->getVersionID();
+    }
 
-	public function setRequestedPageVersionID($cvID) {
-		$this->cvID = $cvID;
-	}
+    public function setRequestedPageVersionID($cvID)
+    {
+        $this->cvID = $cvID;
+    }
 
-	public function addWorkflowProgress(\Concrete\Core\Workflow\Workflow $wf) {
+    public function addWorkflowProgress(\Concrete\Core\Workflow\Workflow $wf)
+    {
+        $pwp = PageWorkflowProgress::add($wf, $this);
+        $r = $pwp->start();
+        $pwp->setWorkflowProgressResponseObject($r);
 
-		$pwp = PageWorkflowProgress::add($wf, $this);
-		$r = $pwp->start();
-		$pwp->setWorkflowProgressResponseObject($r);
-		return $pwp;
-	}
+        return $pwp;
+    }
 
-	public function trigger() {
-		$page = Page::getByID($this->cID);
-		$pk = PermissionKey::getByID($this->pkID);
-		$pk->setPermissionObject($page);
-		return parent::triggerRequest($pk);
-	}
+    public function trigger()
+    {
+        $page = Page::getByID($this->cID);
+        $pk = PermissionKey::getByID($this->pkID);
+        $pk->setPermissionObject($page);
 
-	public function cancel(WorkflowProgress $wp) {
-		$c = Page::getByID($this->getRequestedPageID());
-		$wpr = new WorkflowProgressResponse();
-		$wpr->setWorkflowProgressResponseURL(\URL::to($c));
-		return $wpr;
-	}
+        return parent::triggerRequest($pk);
+    }
 
+    public function cancel(WorkflowProgress $wp)
+    {
+        $c = Page::getByID($this->getRequestedPageID());
+        $wpr = new WorkflowProgressResponse();
+        $wpr->setWorkflowProgressResponseURL(\URL::to($c));
+
+        return $wpr;
+    }
 }
-
-
-
-
