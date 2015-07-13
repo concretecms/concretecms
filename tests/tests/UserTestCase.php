dfc0d07cd700ab6c37a1a789b5b0dca270a778ac
Mike Lay <mike@mkly.io>
2015-07-13 17:48:12 -0500

    Fix failing UserPoint test
    - Set `gPath` on group object after rescan
    - Add addtional test for rescan group path
    - Add test for `Group::updateGroup()`
    
    - Run `parent::setUp()` as DBUnit has a setUp method that does not get
      run if this is not included
    - Clear `CacheLocal` on tear down for clean state between tests

--- tests/tests/UserTestCase.php
+++ tests/tests/UserTestCase.php
@@ -3,13 +3,19 @@ use Concrete\Core\Attribute\Key\Category;
 abstract class UserTestCase extends ConcreteDatabaseTestCase {
 
     protected $fixtures = array();
-    protected $tables = array('Users', 'UserGroups', 'Groups', 'AttributeKeyCategories',
-        'TreeTypes', 'Trees',  'TreeGroupNodes', 'UserAttributeValues'); // so brutal
+    protected $tables = array(
+        'Users', 'UserGroups', 'Groups', 'AttributeKeyCategories',
+        'TreeTypes', 'TreeNodes', 'TreeNodePermissionAssignments',
+        'Packages', 'AttributeKeys', 'AttributeSetKeys', 'AttributeSets',
+        'PermissionKeyCategories', 'PermissionKeys', 'TreeNodeTypes', 'Trees',
+        'TreeGroupNodes', 'UserAttributeValues'
+    ); // so brutal
 
-    public function setUp() {
+    protected function setUp() {
         parent::setUp();
         Category::add('user');
     }
+
     protected static function createUser($uName, $uEmail)
     {
         $user = \Concrete\Core\User\UserInfo::add(
