77b760a6535767f194fb88b7b4ba41adda7cb1e4
Andrew Embler <andrew@concrete5.org>
2015-07-13 09:15:12 -0700

    Add update to fix #2533
    Former-commit-id: 3820b690c2185306116840965c464c0de5f5bf90
    Former-commit-id: 1c19412ec8403ca0448e8430a33c56464cbc1da2

---
+++ web/concrete/src/Updater/Migrations/Migrations/Version20150713000000.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Concrete\Core\Updater\Migrations\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20150713000000 extends AbstractMigration
+{
+
+    public function up(Schema $schema)
+    {
+        $db = \Database::connection();
+        $db->Execute("update UserPointActions set upaHasCustomClass = 1 where upaHandle = 'won_badge'");
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+
+
+}
