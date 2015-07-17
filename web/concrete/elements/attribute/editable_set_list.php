152744e5fcad977a37442e62c6399ef47829c8ea
EC-Joe <joemeyer@exchangecore.com>
2015-07-17 16:44:32 -0400

    move the editable attribute row to it's own element for reusability
    Former-commit-id: 8edf69401f892f703da438e6236c8fb13863ba6d
    Former-commit-id: cada427364d5bea8e412c5b5dd356c2f693fd155

--- web/concrete/elements/attribute/editable_set_list.php
+++ web/concrete/elements/attribute/editable_set_list.php
@@ -1,113 +1,47 @@
 <?php
 /**
  * @var \Concrete\Core\Attribute\Key\Category $category
+ * @var $objects
+ * @var $object
+ * @var callback $permissionsCallback
+ * @var array $permissionsArguments
+ * @var string $clearAction
+ * @var string $saveAction
  */
 $sets = $category->getAttributeSets();
-foreach($sets AS $set) {
+foreach ($sets AS $set) {
     echo '<h3>' . $set->getAttributeSetDisplayName() . '</h3><hr/>';
     foreach ($set->getAttributeKeys() as $key => $ak) {
-        if (isset($objects)) {
-            foreach ($objects as $object) {
-                $value = $object->getAttributeValueObject($ak);
-                if (!is_object($value)) {
-                    $display = '';
-                } else {
-                    $display = $value->getValue('displaySanitized', 'display');
-                }
-                if (isset($lastDisplay) && $display != $lastDisplay) {
-                    $display = t('Multiple Values');
-                }
-                $lastDisplay = $display;
-            }
-        } else {
-            $value = $object->getAttributeValueObject($ak);
-            if (is_object($value)) {
-                $display = $value->getValue('displaySanitized', 'display');
-            } else {
-                $display = '';
-            }
-        }
-
-        $canEdit = $permissionsCallback($ak, $permissionsArguments); ?>
-
-        <div class="row">
-            <div class="col-md-3"><p><?=$ak->getAttributeKeyDisplayName()?></p></div>
-            <div class="col-md-9" <?php if ($canEdit) { ?>data-editable-field-inline-commands="true"<?php } ?>>
-                <?php if ($canEdit) { ?>
-                    <ul class="ccm-edit-mode-inline-commands">
-                        <li><a href="#" data-key-id="<?=$ak->getAttributeKeyID()?>" data-url="<?=$clearAction?>" data-editable-field-command="clear_attribute"><i class="fa fa-trash-o"></i></a></li>
-                    </ul>
-                <?php } ?><p><span <?php if ($canEdit) { ?>data-title="<?=$ak->getAttributeKeyDisplayName()?>" data-key-id="<?=$ak->getAttributeKeyID()?>" data-name="<?=$ak->getAttributeKeyID()?>" data-editable-field-type="xeditableAttribute" data-url="<?=$saveAction?>" data-type="concreteattribute"<?php } ?>><?=$display?></span></p>
-            </div>
-        </div>
-
-        <?php if ($canEdit) { ?>
-
-            <div style="display: none">
-                <div data-editable-attribute-key-id="<?=$ak->getAttributeKeyID()?>">
-                    <?php
-                    $value = $object->getAttributeValueObject($ak);
-                    $ak->render('form', $value);
-                    ?>
-                </div>
-            </div>
-
-        <?php }
-        unset($lastDisplay);
+        Loader::element(
+            'attribute/editable_attribute',
+            array(
+                'ak' => $ak,
+                'object' => isset($object) ? $object : null,
+                'objects' => isset($objects) ? $objects : null,
+                'saveAction' => $saveAction,
+                'clearAction' => $clearAction,
+                'permissionsCallback' => $permissionsCallback,
+                'permissionArguments' => $permissionsArguments
+            )
+        );
     }
 }
 
 $attributeKeys = $category->getUnassignedAttributeKeys();
-if(count($attributeKeys) > 0) {
+if (count($attributeKeys) > 0) {
     echo '<h3>' . t('Other') . '</h3><hr/>';
     foreach ($attributeKeys as $key => $ak) {
-        if (isset($objects)) {
-            foreach ($objects as $object) {
-                $value = $object->getAttributeValueObject($ak);
-                if (!is_object($value)) {
-                    $display = '';
-                } else {
-                    $display = $value->getValue('displaySanitized', 'display');
-                }
-                if (isset($lastDisplay) && $display != $lastDisplay) {
-                    $display = t('Multiple Values');
-                }
-                $lastDisplay = $display;
-            }
-        } else {
-            $value = $object->getAttributeValueObject($ak);
-            if (is_object($value)) {
-                $display = $value->getValue('displaySanitized', 'display');
-            } else {
-                $display = '';
-            }
-        }
-
-        $canEdit = $permissionsCallback($ak, $permissionsArguments); ?>
-
-        <div class="row">
-            <div class="col-md-3"><p><?=$ak->getAttributeKeyDisplayName()?></p></div>
-            <div class="col-md-9" <?php if ($canEdit) { ?>data-editable-field-inline-commands="true"<?php } ?>>
-                <?php if ($canEdit) { ?>
-                    <ul class="ccm-edit-mode-inline-commands">
-                        <li><a href="#" data-key-id="<?=$ak->getAttributeKeyID()?>" data-url="<?=$clearAction?>" data-editable-field-command="clear_attribute"><i class="fa fa-trash-o"></i></a></li>
-                    </ul>
-                <?php } ?><p><span <?php if ($canEdit) { ?>data-title="<?=$ak->getAttributeKeyDisplayName()?>" data-key-id="<?=$ak->getAttributeKeyID()?>" data-name="<?=$ak->getAttributeKeyID()?>" data-editable-field-type="xeditableAttribute" data-url="<?=$saveAction?>" data-type="concreteattribute"<?php } ?>><?=$display?></span></p>
-            </div>
-        </div>
-
-        <?php if ($canEdit) { ?>
-
-            <div style="display: none">
-                <div data-editable-attribute-key-id="<?=$ak->getAttributeKeyID()?>">
-                    <?php
-                    $value = $object->getAttributeValueObject($ak);
-                    $ak->render('form', $value);
-                    ?>
-                </div>
-            </div>
-
-        <?php }
-        unset($lastDisplay);
+        Loader::element(
+            'attribute/editable_attribute',
+            array(
+                'ak' => $ak,
+                'object' => isset($object) ? $object : null,
+                'objects' => isset($objects) ? $objects : null,
+                'saveAction' => $saveAction,
+                'clearAction' => $clearAction,
+                'permissionsCallback' => $permissionsCallback,
+                'permissionArguments' => $permissionsArguments
+            )
+        );
     }
 }
