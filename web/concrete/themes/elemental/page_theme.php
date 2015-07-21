c5410bd9268b6af79ddc101666557397be53c31d
Andrew Embler <andrew@concrete5.org>
2015-07-21 15:29:58 -0700

    Close #2649
    Former-commit-id: b323abdb0c22cb1e4b1c8405f7dde4d155794ea4
    Former-commit-id: bab39ddc4726a4e14b8799c3f632f973135c8e41

--- web/concrete/themes/elemental/page_theme.php
+++ web/concrete/themes/elemental/page_theme.php
@@ -95,13 +95,13 @@ class PageTheme extends \Concrete\Core\Page\Theme\Theme implements ThemeProvider
     public function getThemeEditorClasses()
     {
         return array(
-            array('title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin'),
-            array('title' => t('Title Caps Bold'), 'menuClass' => 'title-caps-bold', 'spanClass' => 'title-caps-bold'),
-            array('title' => t('Title Caps'), 'menuClass' => 'title-caps', 'spanClass' => 'title-caps'),
-            array('title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption'),
-            array('title' => t('Standard Button'), 'menuClass' => '', 'spanClass' => 'btn btn-default'),
-            array('title' => t('Success Button'), 'menuClass' => '', 'spanClass' => 'btn btn-success'),
-            array('title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary'),
+            array('title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin', 'forceBlock' => 1),
+            array('title' => t('Title Caps Bold'), 'menuClass' => 'title-caps-bold', 'spanClass' => 'title-caps-bold', 'forceBlock' => 1),
+            array('title' => t('Title Caps'), 'menuClass' => 'title-caps', 'spanClass' => 'title-caps', 'forceBlock' => 1),
+            array('title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption', 'forceBlock' => '-1'),
+            array('title' => t('Standard Button'), 'menuClass' => '', 'spanClass' => 'btn btn-default', 'forceBlock' => '-1'),
+            array('title' => t('Success Button'), 'menuClass' => '', 'spanClass' => 'btn btn-success', 'forceBlock' => '-1'),
+            array('title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary', 'forceBlock' => '-1'),
         );
     }
 
