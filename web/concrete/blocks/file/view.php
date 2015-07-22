aac901f70e509599bacbb230557daf7524b57563
Tim Dix <timwdix@gmail.com>
2015-07-22 11:56:09 -0400

    Adds support for the File Block in Composer Adds feature request #1489

--- web/concrete/blocks/file/view.php
+++ web/concrete/blocks/file/view.php
@@ -1,11 +1,12 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied.");
 $f = $controller->getFileObject();
 $fp = new Permissions($f);
-if ($fp->canViewFile()) {
+if ($f && $fp->canViewFile()) {
 	$c = Page::getCurrentPage();
 	if($c instanceof Page) {
 		$cID = $c->getCollectionID();
 	}
+
 	?>
 	<div class="ccm-block-file">
 		<a href="<?php echo ($forceDownload ? $f->getForceDownloadURL() : $f->getDownloadURL()); ?>"><?php echo stripslashes($controller->getLinkText()) ?></a>
