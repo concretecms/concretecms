a1a4d472d1e36e86093e1c6fc63678bfa12b4f4e
Andrew Embler <andrew@concrete5.org>
2015-07-22 15:49:31 -0700

    adding updatecontents
    Former-commit-id: 8c4f9eaafd73a228d6b25c418b11de5fd8e7bccc
    Former-commit-id: 2b9c2a5478efa88552f69629f9e0ad4aadc061fb

--- web/concrete/src/File/Version.php
+++ web/concrete/src/File/Version.php
@@ -34,6 +34,8 @@ class Version
     const UT_DESCRIPTION = 3;
     const UT_TAGS = 4;
     const UT_EXTENDED_ATTRIBUTE = 5;
+    const UT_CONTENTS = 6;
+
     /**
      * /* @Id
      * @ManyToOne(targetEntity="File", inversedBy="versions")
@@ -485,6 +487,9 @@ class Version
                 case self::UT_TAGS:
                     $updates[] = t('Tags');
                     break;
+                case self::UT_CONTENTS:
+                    $updates[] = t('File Content');
+                    break;
                 case self::UT_EXTENDED_ATTRIBUTE:
                     $val = $db->GetOne(
                         "SELECT akName FROM AttributeKeys WHERE akID = ?",
@@ -545,6 +550,19 @@ class Version
         $this->logVersionUpdate(self::UT_DESCRIPTION);
         $fe = new \Concrete\Core\File\Event\FileVersion($this);
         Events::dispatch('on_file_version_update_description', $fe);
+    }
+
+    public function updateContents($contents)
+    {
+        $cf = Core::make('helper/concrete/file');
+        $storage = $this->getFile()->getFileStorageLocationObject();
+        if (is_object($storage)) {
+            $storage->getFileSystemObject()->write($cf->prefix($this->fvPrefix, $this->fvFilename), $contents);
+            $this->logVersionUpdate(self::UT_CONTENTS);
+            $fe = new \Concrete\Core\File\Event\FileVersion($this);
+            Events::dispatch('on_file_version_update_contents', $fe);
+            $this->refreshAttributes();
+        }
     }
 
     public function updateFile($filename, $prefix)
