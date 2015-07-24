f33d07012b4e062ff60416148a9856e2e99e9794
Mitch Ray <contact@mitchray.com.au>
2015-07-24 18:38:41 +1000

    Converted composer sets to a table layout, using Bootstrap.

--- web/concrete/elements/page_types/composer/form/layout_set/control.php
+++ web/concrete/elements/page_types/composer/form/layout_set/control.php
@@ -1,44 +1,46 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 /** @var $control Concrete\Core\Page\Type\Composer\FormLayoutSetControl */
+
+$der = Concrete\Core\Page\Type\Composer\Control\Type\Type::getByID($control->getPageTypeComposerControlTypeID());
+$pto = $control->getPageTypeComposerControlObject();
+$name = '';
+if (strlen($control->getPageTypeComposerFormLayoutSetControlCustomLabel())) {
+	$name = $pto->getPageTypeComposerControlName() . ' ';
+}
+
 ?>
-<div class="ccm-page-type-composer-form-layout-control-set-control" data-page-type-composer-form-layout-control-set-control-id="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>">
-    <div class="ccm-page-type-composer-item-control-bar">
-        <ul class="ccm-page-type-composer-item-controls">
-            <li><a href="#" data-command="move-set-control" style="cursor: move"><i class="fa fa-arrows"></i></a></li>
-            <li><a data-command="edit-form-set-control" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/page_types/composer/form/edit_control?ptComposerFormLayoutSetControlID=<?=$control->getPageTypeComposerFormLayoutSetControlID()?>" class="dialog-launch" dialog-width="400" dialog-height="auto" dialog-modal="true" dialog-title="<?=t('Edit Form Control')?>"><i class="fa fa-pencil"></i></a></li>
-            <li><a href="#" data-delete-set-control="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>"><i class="fa fa-trash-o"></i></a></li>
-        </ul>
+<tr class="ccm-page-type-composer-form-layout-control-set-control" data-page-type-composer-form-layout-control-set-control-id="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>">
+	<td style="white-space: nowrap;">
+		<?= $control->getPageTypeComposerControlDisplayLabel(); ?>
+	</td>
 
-        <div style="display: none">
-            <div data-delete-set-control-dialog="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>">
-                <?=t("Delete this control? This cannot be undone.")?>
-                <?=Core::make('helper/validation/token')->output('delete_set_control')?>
+	<td style="white-space: nowrap;">
+		<?= $der->getPageTypeComposerControlTypeDisplayName(); ?>
+	</td>
 
-                <div class="dialog-buttons">
-                    <button class="btn btn-default pull-left" onclick="jQuery.fn.dialog.closeTop()"><?=t('Cancel')?></button>
-                    <button class="btn btn-danger pull-right" onclick="Composer.deleteFromLayoutSetControl(<?=$control->getPageTypeComposerFormLayoutSetControlID()?>)"><?=t('Update Set')?></button>
-                </div>
+	<td style="width: 100%;">
+		<?= $name; ?>
+	</td>
 
-            </div>
-        </div>
+	<td style="text-align: right; width: 200px; white-space: nowrap;">
+		<ul class="ccm-page-type-composer-item-controls">
+			<li><a href="#" data-command="move-set-control" style="cursor: move"><i class="fa fa-arrows"></i></a></li>
+			<li><a data-command="edit-form-set-control" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/page_types/composer/form/edit_control?ptComposerFormLayoutSetControlID=<?=$control->getPageTypeComposerFormLayoutSetControlID()?>" class="dialog-launch" dialog-width="400" dialog-height="auto" dialog-modal="true" dialog-title="<?=t('Edit Form Control')?>"><i class="fa fa-pencil"></i></a></li>
+			<li><a href="#" data-delete-set-control="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>"><i class="fa fa-trash-o"></i></a></li>
+		</ul>
 
-        <div class="ccm-page-type-composer-form-layout-control-set-control-inner">
-            <?php
-            $der = Concrete\Core\Page\Type\Composer\Control\Type\Type::getByID($control->getPageTypeComposerControlTypeID());
-            $pto = $control->getPageTypeComposerControlObject();
-            //var_dump($control);
-            $name = '';
-            if (strlen($control->getPageTypeComposerFormLayoutSetControlCustomLabel())) {
-                $name = $pto->getPageTypeComposerControlName() . ' ';
-            }
-            echo sprintf(
-                '%s - <small>%s(%s)</small>',
-                $control->getPageTypeComposerControlDisplayLabel(),
-                $name,
-                $der->getPageTypeComposerControlTypeDisplayName()
-            );
-            ?>
-        </div>
-    </div>
-</div>
+		<div style="display: none">
+			<div data-delete-set-control-dialog="<?=$control->getPageTypeComposerFormLayoutSetControlID()?>">
+				<?=t("Delete this control? This cannot be undone.")?>
+				<?=Core::make('helper/validation/token')->output('delete_set_control')?>
+
+				<div class="dialog-buttons">
+					<button class="btn btn-default pull-left" onclick="jQuery.fn.dialog.closeTop()"><?=t('Cancel')?></button>
+					<button class="btn btn-danger pull-right" onclick="Composer.deleteFromLayoutSetControl(<?=$control->getPageTypeComposerFormLayoutSetControlID()?>)"><?=t('Update Set')?></button>
+				</div>
+
+			</div>
+		</div>
+	</td>
+</tr>
