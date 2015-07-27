088a19fade524ef2aa78cfa2830d7d66c06be361
Michele Locati <michele@locati.it>
2015-07-27 10:24:56 +0200

    Switch to 5.7 code

--- web/concrete/tools/pages/url_slug.php
+++ web/concrete/tools/pages/url_slug.php
@@ -1,8 +1,9 @@
 <?
+use Concrete\Core\Multilingual\Page\Section\Section as MultilingualSection;
 defined('C5_EXECUTE') or die("Access Denied.");
 if (Loader::helper('validation/token')->validate('get_url_slug', $_REQUEST['token'])) {
 	$lang = Localization::activeLanguage();
-	if (isset($_REQUEST['parentID']) && $multilingual = Package::getByHandle('multilingual') ) {
+	if (isset($_REQUEST['parentID']) && Core::make('multilingual/detector')->isEnabled()) {
 		$ms = MultilingualSection::getBySectionOfSite(Page::getByID($_REQUEST['parentID']));
 		if (is_object($ms)) {
 			$lang = $ms->getLanguage();
