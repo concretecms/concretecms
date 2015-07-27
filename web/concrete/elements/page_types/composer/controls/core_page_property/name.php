af25d9abff1dfa85bed4381cf88d33318c2ccc71
Michele Locati <michele@locati.it>
2015-07-27 10:14:43 +0200

    Pass parent collection ID to the URL slug generator
    Former-commit-id: 1dc7f5f329379f39577142e81f9c3551253a31ad
    Former-commit-id: 3fa6a136173a351fcab429ab241b4ea6581af151

--- web/concrete/elements/page_types/composer/controls/core_page_property/name.php
+++ web/concrete/elements/page_types/composer/controls/core_page_property/name.php
@@ -18,18 +18,26 @@ $(function() {
 	var $urlSlugField = $('div[data-composer-field=url_slug] input');
 	if ($urlSlugField.length) {
 		$('div[data-composer-field=name] input').on('keyup', function() {
-			var val = $(this).val();
-			var frm = $(this);
+			var input = $(this);
+			var send = {
+				token: '<?=Loader::helper('validation/token')->generate('get_url_slug')?>',
+				name: input.val()
+			};
+			var parentID = input.closest('form').find('input[name=cParentID]').val();
+			if (parentID) {
+				send.parentID = parentID;
+			}
 			clearTimeout(concreteComposerAddPageTimer);
 			concreteComposerAddPageTimer = setTimeout(function() {
 				$('.ccm-composer-url-slug-loading').show();
-				$.post('<?=REL_DIR_FILES_TOOLS_REQUIRED?>/pages/url_slug', {
-					'token': '<?=Loader::helper('validation/token')->generate('get_url_slug')?>',
-					'name': val
-				}, function(r) {
-					$('.ccm-composer-url-slug-loading').hide();
-					$urlSlugField.val(r);
-				});
+				$.post(
+					'<?=REL_DIR_FILES_TOOLS_REQUIRED?>/pages/url_slug',
+					send,
+					function(r) {
+						$('.ccm-composer-url-slug-loading').hide();
+						$urlSlugField.val(r);
+					}
+				);
 			}, 150);
 		});
 	}
