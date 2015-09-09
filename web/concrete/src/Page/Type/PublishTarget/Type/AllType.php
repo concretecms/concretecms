8936e558a52a40ca3303499ce21f317b63a2fb27
Michele Locati <michele@locati.it>
2015-09-09 14:24:24 +0200

    PSR
    Former-commit-id: d759717b1a35cf61008060a9ecc895a5c034c8f6
    Former-commit-id: 7bcb8377eb9cfb46c32c3942aa8bbc80c6d41d6e

--- web/concrete/src/Page/Type/PublishTarget/Type/AllType.php
+++ web/concrete/src/Page/Type/PublishTarget/Type/AllType.php
@@ -1,17 +1,17 @@
 <?php
+
 namespace Concrete\Core\Page\Type\PublishTarget\Type;
 
-use Loader;
 use Concrete\Core\Page\Type\Type as PageType;
-use \Concrete\Core\Page\Type\PublishTarget\Configuration\AllConfiguration;
+use Concrete\Core\Page\Type\PublishTarget\Configuration\AllConfiguration;
 
 class AllType extends Type
 {
-
     public function configurePageTypePublishTarget(PageType $pt, $post)
     {
         $configuredTarget = new AllConfiguration($this);
         $configuredTarget->setSelectorFormFactor(isset($post['selectorFormFactorAll']) ? $post['selectorFormFactorAll'] : null);
+
         return $configuredTarget;
     }
 
@@ -22,10 +22,7 @@ class AllType extends Type
         if ($formFactor) {
             $configuration->setSelectorFormFactor($formFactor);
         }
+
         return $configuration;
     }
-
-
-
-
 }
