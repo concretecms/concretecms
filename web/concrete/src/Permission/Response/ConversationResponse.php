26250f07fad68591eca9777bcf7b2e0d3322ebf8
Andrew Embler <andrew@concrete5.org>
2015-09-11 12:57:45 -0700

    Fix #2977

--- web/concrete/src/Permission/Response/ConversationResponse.php
+++ web/concrete/src/Permission/Response/ConversationResponse.php
@@ -3,4 +3,18 @@ namespace Concrete\Core\Permission\Response;
 class ConversationResponse extends Response
 {
 
+    /**
+     * @todo Make this dependent on conversation-specific permissions.
+     */
+    public function canViewConversation()
+    {
+        $conversation = $this->getPermissionObject();
+        if (is_object($conversation)) {
+            $c = $conversation->getConversationPageObject();
+            if (is_object($c) && !$c->isError()) {
+                $cp = new \Permissions($c);
+                return $cp->canViewPage();
+            }
+        }
+    }
 }
