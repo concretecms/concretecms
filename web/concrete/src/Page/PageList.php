e1e37010efd84c76d57a7e53d2550f7cb313205d
Andrew Embler <andrew@concrete5.org>
2015-09-15 11:21:24 -0700

    select attribute option API improvements; edge case page list sorting fix
    Former-commit-id: bc5d1d6214013f3c130373d4c35e57f56987d4c8
    Former-commit-id: 7d6810f9d8070d049179fda6fe5fd4c0c286ec3a

--- web/concrete/src/Page/PageList.php
+++ web/concrete/src/Page/PageList.php
@@ -152,8 +152,10 @@ class PageList extends DatabaseItemList implements PermissionableListItemInterfa
         $u = new \User();
         if ($this->permissionsChecker == -1) {
             $query = $this->deliverQueryObject();
-
-            return $query->select('count(distinct p.cID)')->setMaxResults(1)->execute()->fetchColumn();
+            // We add a custom order by here because otherwise, if we've added
+            // items to the select parts, and we're ordering by them, we get a SQL error
+            // when we get total results, because we're resetting the select
+            return $query->select('count(distinct p.cID)')->orderBy('p.cID', 'asc')->setMaxResults(1)->execute()->fetchColumn();
         } else {
             return -1; // unknown
         }
@@ -164,7 +166,10 @@ class PageList extends DatabaseItemList implements PermissionableListItemInterfa
         $u = new \User();
         if ($this->permissionsChecker == -1) {
             $adapter = new DoctrineDbalAdapter($this->deliverQueryObject(), function ($query) {
-                $query->select('count(distinct p.cID)')->setMaxResults(1);
+                // We add a custom order by here because otherwise, if we've added
+                // items to the select parts, and we're ordering by them, we get a SQL error
+                // when we get total results, because we're resetting the select
+                $query->select('count(distinct p.cID)')->orderBy('p.cID', 'asc')->setMaxResults(1);
             });
             $pagination = new Pagination($this, $adapter);
         } else {
