416833b6a43108fead2e6b2007047e49e92b2540
Andrew Embler <andrew@concrete5.org>
2015-10-10 11:32:45 -0700

    adding some flexibility to the entity manager

---
+++ web/concrete/src/Database/EntityManagerFactoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Concrete\Core\Database;
+
+use Concrete\Core\Database\Connection\Connection;
+
+interface EntityManagerFactoryInterface
+{
+
+    public function create(Connection $connection);
+
+}
