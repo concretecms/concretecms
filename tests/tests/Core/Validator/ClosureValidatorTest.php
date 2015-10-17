ed3c49a88199f9ea15a3c72e67e733ea86e1d67d
Korvin Szanto <korvinszanto@gmail.com>
2015-10-17 15:53:22 -0700

    Add validator tests
    Former-commit-id: 7206e4eff79fb5fce59e456f84292571a9eddbe0
    Former-commit-id: 1187b0c982c142ccb2461f48002226de21f6720d

--- tests/tests/Core/Validator/ClosureValidatorTest.php
+++ tests/tests/Core/Validator/ClosureValidatorTest.php
@@ -1,6 +1,47 @@
 <?php
+namespace Concrete\Core\Tests\Validator;
 
-class ClosureValidatorTest
+class ClosureValidatorTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function testIsValid()
+    {
+        $error_mock = $this->getMock('Concrete\Core\Error\Error');
+        $test_value = 'test';
+        $ran = false;
+
+        $closure_validator = null;
+        $closure_validator = new \Concrete\Core\Validator\ClosureValidator(function(){}, function(){});
+
+        $closure_validator->setValidatorClosure(function($validator, $mixed, $error) use (&$closure_validator, $test_value, $error_mock, &$ran) {
+            $this->assertEquals($error_mock, $error);
+            $this->assertEquals($test_value, $mixed);
+            $this->assertEquals($validator, $closure_validator);
+
+            $ran = true;
+            return true;
+        });
+
+        $this->assertTrue($closure_validator->isValid($test_value, $error_mock));
+        $this->assertTrue($ran, 'Closure didn\'t fire.');
+    }
+
+    public function testRequirements()
+    {
+        $ran = false;
+
+        $closure_validator = null;
+        $closure_validator = new \Concrete\Core\Validator\ClosureValidator(function(){}, function(){});
+
+        $closure_validator->setRequirementsClosure(function($validator) use (&$closure_validator, &$ran) {
+            $this->assertEquals($validator, $closure_validator);
+
+            $ran = true;
+            return array('test');
+        });
+
+        $this->assertEquals(array('test'), $closure_validator->getRequirementStrings());
+        $this->assertTrue($ran, 'Requirements Closure didn\'t fire.');
+    }
 
 }
--- tests/tests/Core/Validator/String/MaximumLengthValidatorTest.php
+++ tests/tests/Core/Validator/String/MaximumLengthValidatorTest.php
@@ -1,8 +1,37 @@
 <?php
+namespace Concrete\Core\Tests\Validator\String;
 
-namespace tests\Core\Validator\String;
-
-class MaximumLengthValidatorTest
+class MaximumLengthValidatorTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function testIsValid()
+    {
+        $validator = new \Concrete\Core\Validator\String\MaximumLengthValidator(1000);
+        $validator->setMaximumLength(5);
+
+        $this->assertNotEmpty($validator->getRequirementStrings());
+
+        $this->assertFalse($validator->isValid('123456'));
+        $this->assertTrue($validator->isValid('12345'));
+        $this->assertFalse($validator->isValid('12345678'));
+        $this->assertTrue($validator->isValid('12'));
+    }
+
+    public function testErrorAdded()
+    {
+        $validator = new \Concrete\Core\Validator\String\MaximumLengthValidator(5);
+        $error = $this->getMock('Concrete\Core\Error\Error');
+        $error->expects($this->once())->method('add');
+
+        $this->assertFalse($validator->isValid('123456', $error));
+    }
+
+    public function testInvalidInput()
+    {
+        $validator = new \Concrete\Core\Validator\String\MaximumLengthValidator(5);
+
+        $this->setExpectedException('InvalidArgumentException');
+        $validator->isValid($validator);
+    }
 
 }
