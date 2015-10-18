7b11b07ebfbc478769f5453440fdbef0b3afb422
Korvin Szanto <korvinszanto@gmail.com>
2015-10-17 20:06:52 -0700

    Make core error object extend arrayaccess

--- web/concrete/src/Error/Error.php
+++ web/concrete/src/Error/Error.php
@@ -6,11 +6,77 @@ use Exception;
 use Loader;
 use stdClass;
 
-class Error
+class Error implements \ArrayAccess
 {
 
     protected $error = array();
     public $helperAlwaysCreateNewInstance = true;
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return $this->error[$offset];
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return array_get($this->error, $offset);
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->add($value);
+        } else {
+            $this->error[$offset] = $value;
+        }
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->error[$offset]);
+    }
 
     /**
      * Adds an error object or exception to the internal error array
