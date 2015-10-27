37a439c4257e1d52bfeec1d21133c5fb132d491e
Korvin Szanto <korvinszanto@gmail.com>
2015-10-27 11:57:03 -0700

    Feature (DB): Fully use the laravel container so that reflection works properly
    This adds bindings for DatabaseManager, DatabaseManagerORM, and EntityManager classes so that we can use the application make method to its fullest.
    
    Before this commit, if we have a class Foo
    
    ```
    class Foo
    {
    
        public function __construct(\Doctrine\ORM\EntityManagerInterface $manager)
        {
    
        }
    
    }
    ```
    
    and call `\Core::make('Foo')`, we will get an exception since there's nothing bound to `\Doctrine\ORM\EntityManagerInterface`.
    
    After this commit, `\Core::make('Foo')` will build a `Foo` object and pass the default concrete5 EntityManager instance.
    
    
    Former-commit-id: 4daeba6d8972a62517dc5ee7da9fedfaf2eaf420
    Former-commit-id: 79eb385e46e8fad6cc6ac37ad7bf02e380dcbc66

--- web/concrete/src/Database/DatabaseServiceProvider.php
+++ web/concrete/src/Database/DatabaseServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 namespace Concrete\Core\Database;
 
-use Concrete\Core\Database\Connection\ConnectionFactory;
+use Concrete\Core\Application\Application;
 use Concrete\Core\Database\Driver\DriverManager;
 use Concrete\Core\Foundation\Service\Provider as ServiceProvider;
 
@@ -10,24 +10,59 @@ class DatabaseServiceProvider extends ServiceProvider
 
     public function register()
     {
+        // Bind the driver manager config constructor
         $this->app->bind(
+            'Concrete\Core\Database\Driver\DriverManager',
+            function(Application $app) {
+                $manager = new DriverManager($app);
+                $manager->configExtensions($app['config']->get('database.drivers'));
+
+                return $manager;
+            });
+
+        // Set DatabaseManager as a singleton, we want to be able to add managers to be available globally
+        $this->app->singleton('Concrete\Core\Database\DatabaseManager');
+        $this->app->bind('database', 'Concrete\Core\Database\DatabaseManager');
+
+        // Do the same with ORM
+        $this->app->singleton('Concrete\Core\Database\DatabaseManagerORM');
+        $this->app->bind('database/orm', 'Concrete\Core\Database\DatabaseManagerORM');
+
+        // Bind the structure manager
+        $this->app->bind('database/structure', 'Concrete\Core\Database\DatabaseStructureManager');
+
+        // Set the EntityManagerInterface to the default entitymanger
+        $this->app->bind('Doctrine\ORM\EntityManagerInterface', function(Application $app) {
+            return $app->make('Concrete\Core\Database\DatabaseManagerORM')->entityManager();
+        });
+        $this->app->bind('Doctrine\ORM\EntityManager', 'Doctrine\ORM\EntityManagerInterface');
+
+        // Set the concrete Connection classname to the default connection
+        $this->app->bind('Concrete\Core\Database\Connection\Connection', function(Application $app) {
+            return $app->make('Concrete\Core\Database\DatabaseManager')->connection();
+        });
+        $this->app->bind('Doctrine\DBAL\Connection', 'Concrete\Core\Database\Connection\Connection');
+    }
+
+    /**
+     * A list of things that this service provider provides
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array(
             'database',
-            function ($cms) {
-                $driver_manager = new DriverManager($cms);
-                $driver_manager->configExtensions(\Config::get('database.drivers'));
-                $factory = new ConnectionFactory($cms, $driver_manager);
-                return new DatabaseManager($cms, $factory);
-            });
-        $this->app->bind(
             'database/orm',
-            function ($cms) {
-                return new DatabaseManagerORM($cms);
-            });
-        $this->app->bind(
             'database/structure',
-            function ($cms, $em) {
-                return new DatabaseStructureManager($em);
-            });
+            'Concrete\Core\Database\Driver\DriverManager',
+            'Concrete\Core\Database\DatabaseManager',
+            'Concrete\Core\Database\DatabaseManagerORM',
+            'Doctrine\ORM\EntityManager',
+            'Doctrine\ORM\EntityManagerInterface',
+            'Concrete\Core\Database\Connection\Connection',
+            'Doctrine\DBAL\Connection'
+        );
     }
 
 }
