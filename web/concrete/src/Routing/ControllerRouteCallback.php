80f5068ed61e686697197e03896c4ab04376b3e9
Michele Locati <michele@locati.it>
2015-11-10 21:13:48 +0100

    PSR
    Former-commit-id: 3057a7c9678018be4b4cb9142a0434e61d6c132a
    Former-commit-id: 3854d21b83509d714e1fc503c4b5bb1148815c7b

--- web/concrete/src/Routing/ControllerRouteCallback.php
+++ web/concrete/src/Routing/ControllerRouteCallback.php
@@ -1,33 +1,34 @@
 <?php
+
 namespace Concrete\Core\Routing;
+
 use Symfony\Component\HttpKernel;
 use Response;
-use View;
-use Concrete\Controller;
 use Request;
 
-class ControllerRouteCallback extends RouteCallback {
+class ControllerRouteCallback extends RouteCallback
+{
+    /**
+     * @param Request $request
+     * @param Route $route
+     * @param array $parameters
+     *
+     * @return Response
+     */
+    public function execute(Request $request, Route $route, $parameters)
+    {
+        $resolver = new HttpKernel\Controller\ControllerResolver();
+        $callback = $resolver->getController($request);
+        $arguments = $resolver->getArguments($request, $callback);
+        $controller = $callback[0];
+        $method = $callback[1];
+        $controller->on_start();
+        $response = $controller->runAction($method, $arguments);
 
-
-	/**
-	 * @param Request $request
-	 * @param Route $route
-	 * @param array $parameters
-	 * @return Response
-	 */
-	public function execute(Request $request, Route $route, $parameters) {
-		$resolver = new HttpKernel\Controller\ControllerResolver();
-		$callback = $resolver->getController($request);
-		$arguments = $resolver->getArguments($request, $callback);
-		$controller = $callback[0];
-		$method = $callback[1];
-		$controller->on_start();
-		$response = $controller->runAction($method, $arguments);
-
-		if ($response instanceof \Symfony\Component\HttpFoundation\Response) {
-			// note, our RedirectResponse doesn't extend Response, it extends symfony2 response
-			return $response;
-		} elseif ($response instanceof \Concrete\Core\View\AbstractView) {
+        if ($response instanceof \Symfony\Component\HttpFoundation\Response) {
+            // note, our RedirectResponse doesn't extend Response, it extends symfony2 response
+            return $response;
+        } elseif ($response instanceof \Concrete\Core\View\AbstractView) {
             $content = $response->render();
         } else {
             $view = $controller->getViewObject();
@@ -38,20 +39,22 @@ class ControllerRouteCallback extends RouteCallback {
                 }
             }
         }
-		$response = new Response();
-		$response->setContent($content);
-		return $response;
-	}
+        $response = new Response();
+        $response->setContent($content);
 
-	/**
-	 * @return array
-	 */
-	public static function getRouteAttributes($callback) {
-		$attributes = array();
-		$attributes['_controller'] = $callback;
-		$callback = new static($callback);
-		$attributes['callback'] = $callback;
-		return $attributes;
-	}
+        return $response;
+    }
 
+    /**
+     * @return array
+     */
+    public static function getRouteAttributes($callback)
+    {
+        $attributes = array();
+        $attributes['_controller'] = $callback;
+        $callback = new static($callback);
+        $attributes['callback'] = $callback;
+
+        return $attributes;
+    }
 }
