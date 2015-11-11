8d16026831bec73088965c4eff0af7a7aabb4ab7
Michele Locati <michele@locati.it>
2015-11-11 10:33:06 +0100

    Introduce AttributeKey::getDefaultIndexedSearchTable, to be called statically instead of getIndexedSearchTable
    Conflicts:
    	web/concrete/blocks/page_list/controller.php

--- web/concrete/blocks/page_list/controller.php
+++ web/concrete/blocks/page_list/controller.php
@@ -157,7 +157,7 @@ class Controller extends BlockController
         }
 
         $db = Database::connection();
-        $columns = $db->MetaColumnNames(CollectionAttributeKey::getIndexedSearchTable());
+        $columns = $db->MetaColumnNames(CollectionAttributeKey::getDefaultIndexedSearchTable());
         if (in_array('ak_exclude_page_list', $columns)) {
             $this->list->filter(false, '(ak_exclude_page_list = 0 or ak_exclude_page_list is null)');
         }
