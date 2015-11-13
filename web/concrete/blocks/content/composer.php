b7e0d88ea4427bfa854c3636d45380fba19365ec
Andrew Embler <andrew@concrete5.org>
2015-11-13 05:48:51 -0800

    style tweaks
    Former-commit-id: 67ac39f04cbac15ec452a1060aa1feb8fe97ddbe
    Former-commit-id: a95ec9857310211394940f445a5893f3047704b6

--- web/concrete/blocks/content/composer.php
+++ web/concrete/blocks/content/composer.php
@@ -2,19 +2,17 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 ?>
 
-<div class="control-group">
-	<label class="control-label"><?=$label?></label>
+<div class="form-group">
+	<label><?=$label?></label>
 	<? if($description): ?>
 	<i class="fa fa-question-circle launch-tooltip" title="" data-original-title="<?=$description?>"></i>
 	<? endif; ?>
-	<div class="controls">
-		<?
-		$content = $controller->getContentEditMode();
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$data = $view->getRequestValue();
-			$content = $data['content'];
-		}
-		print Core::make('editor')->outputPageComposerEditor($view->field('content'), $content);
-		?>
-	</div>
+	<?
+	$content = $controller->getContentEditMode();
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$data = $view->getRequestValue();
+		$content = $data['content'];
+	}
+	print Core::make('editor')->outputPageComposerEditor($view->field('content'), $content);
+	?>
 </div>
