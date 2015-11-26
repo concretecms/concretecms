2650c3874df33d93f479f9cdfbc00ed921ec9035
Michele Locati <michele@locati.it>
2015-11-26 15:28:30 +0100

    PSR

--- web/concrete/src/Validation/CSRF/Token.php
+++ web/concrete/src/Validation/CSRF/Token.php
@@ -6,11 +6,10 @@ use User;
 
 class Token
 {
-
     const VALID_HASH_TIME_THRESHOLD = 86400; // 24 hours (in seconds)
 
     /**
-     * For localization we can't just store this as a constant, unfortunately
+     * For localization we can't just store this as a constant, unfortunately.
      */
     public function getErrorMessage()
     {
@@ -18,10 +17,11 @@ class Token
     }
 
     /**
-     * prints out a generated token as a hidden form field
+     * Prints out a generated token as a hidden form field.
      *
      * @param string $action
      * @param bool   $return
+     *
      * @return string
      */
     public function output($action = '', $return = false)
@@ -37,10 +37,11 @@ class Token
 
     /**
      * Generates a unique token for a given action. This is a token in the form of
-     * time:hash, where hash is md5(time:userID:action:pepper)
+     * time:hash, where hash is md5(time:userID:action:pepper).
      *
      * @param string $action
      * @param int    $time
+     *
      * @return string
      */
     public function generate($action = '', $time = null)
@@ -55,18 +56,21 @@ class Token
         }
         $config = \Core::make('config/database');
         $hash = $time . ':' . md5($time . ':' . $uID . ':' . $action . ':' . $config->get('concrete.security.token.validation'));
+
         return $hash;
     }
 
     /**
-     * returns a generated token as a query string variable
+     * Returns a generated token as a query string variable.
      *
      * @param string $action
+     *
      * @return string
      */
     public function getParameter($action = '')
     {
         $hash = $this->generate($action);
+
         return 'ccm_token=' . $hash;
     }
 
@@ -77,6 +81,7 @@ class Token
      *
      * @param string $action
      * @param string $token
+     *
      * @return bool
      */
     public function validate($action = '', $token = null)
@@ -92,13 +97,12 @@ class Token
             $now = time();
 
             if (substr($compHash, strpos($compHash, ':') + 1) == $hash) {
-
                 $diff = $now - $time;
                 //hash is only valid if $diff is less than VALID_HASH_TIME_RECORD
                 return $diff <= static::VALID_HASH_TIME_THRESHOLD;
             }
         }
+
         return false;
     }
-
 }
