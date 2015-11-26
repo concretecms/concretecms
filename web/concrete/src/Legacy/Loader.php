bc3900d1255005b11448ab8a98b4a72c2af3e52e
Michele Locati <michele@locati.it>
2015-11-26 14:28:25 +0100

    PSR

--- web/concrete/src/Legacy/Loader.php
+++ web/concrete/src/Legacy/Loader.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Concrete\Core\Legacy;
+
 use Database;
 use Core;
 use View;
@@ -7,38 +9,44 @@ use View;
 /**
  * @deprecated
  */
-class Loader {
-
+class Loader
+{
     /**
      * @return \Concrete\Core\Database\Connection\Connection
      */
-    public static function db() {
-		return Database::getActiveConnection();
-	}
+    public static function db()
+    {
+        return Database::getActiveConnection();
+    }
 
-	public static function helper($service, $pkgHandle = false) {
-		if ($pkgHandle !== false) {
-			return Core::make('/packages/' . $pkgHandle . '/helper/' . $service);
-		} else {
-			return Core::make('helper/' . $service);
-		}
-	}
+    public static function helper($service, $pkgHandle = false)
+    {
+        if ($pkgHandle !== false) {
+            return Core::make('/packages/' . $pkgHandle . '/helper/' . $service);
+        } else {
+            return Core::make('helper/' . $service);
+        }
+    }
 
-	public static function packageElement($file, $pkgHandle, $args = null) {
-		self::element($file, $args, $pkgHandle);
-	}
+    public static function packageElement($file, $pkgHandle, $args = null)
+    {
+        self::element($file, $args, $pkgHandle);
+    }
 
-	public static function element($_file, $args = null, $_pkgHandle= null) {
-		return View::element($_file, $args, $_pkgHandle);
-	}
+    public static function element($_file, $args = null, $_pkgHandle = null)
+    {
+        return View::element($_file, $args, $_pkgHandle);
+    }
 
-	public static function model($model, $pkgHandle = false) {
-		return false;
-	}
+    public static function model($model, $pkgHandle = false)
+    {
+        return false;
+    }
 
-	public static function library($library, $pkgHandle = false) {
-		return false;
-	}
+    public static function library($library, $pkgHandle = false)
+    {
+        return false;
+    }
 
     public static function controller($item)
     {
@@ -47,7 +55,7 @@ class Loader {
         }
 
         $controller = '\\Concrete\\Controller\\' . camelcase($item);
+
         return new $controller();
     }
-
 }
