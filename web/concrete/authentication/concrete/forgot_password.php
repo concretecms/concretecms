38c6943fd7222a5122cdf6e410423930dfe74710
Michele Locati <michele@locati.it>
2015-11-26 15:45:18 +0100

    Avoid accessing an undefined variable in forgot_password

--- web/concrete/authentication/concrete/forgot_password.php
+++ web/concrete/authentication/concrete/forgot_password.php
@@ -6,7 +6,7 @@ $form = Core::make('helper/form');
 	<form method="post" action="<?= URL::to('/login', 'callback', $authType->getAuthenticationTypeHandle(), 'forgot_password') ?>">
 		<div class="form-group">
 			<h4><?= t('Forgot Your Password?') ?></h4>
-			<div class="ccm-message"><?= $intro_msg ?></div>
+			<div class="ccm-message"><?= isset($intro_msg) ? $intro_msg : '' ?></div>
 				<div class='help-block'>
 					<?= t('Enter your email address below. We will send you instructions to reset your password.') ?>
 				</div>
