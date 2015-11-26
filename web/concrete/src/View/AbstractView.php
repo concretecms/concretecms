f0fa070b7db3007b23e28de7937358ad625e22e6
Michele Locati <michele@locati.it>
2015-11-26 13:56:44 +0100

    PSR
    Former-commit-id: fb0763a70b3e048ba3112796dc930eb2f43fe73b
    Former-commit-id: c2846dffbf9f722fd9020f8b73080678d90084d6

--- web/concrete/src/View/AbstractView.php
+++ web/concrete/src/View/AbstractView.php
@@ -1,191 +1,224 @@
 <?php
+
 namespace Concrete\Core\View;
+
 use Concrete\Core\Http\ResponseAssetGroup;
 use Request;
 use URL;
 use Loader;
 
-abstract class AbstractView {
+abstract class AbstractView
+{
+    protected static $requestInstance;
+    protected static $requestInstances = array();
+    protected $scopeItems = array();
+    public $controller;
+    protected $template;
+    protected $outputAssets = array();
 
-	protected static $requestInstance;
-	protected static $requestInstances = array();
-	protected $scopeItems = array();
+    public function addScopeItems($items)
+    {
+        foreach ($items as $key => $value) {
+            $this->scopeItems[$key] = $value;
+        }
+    }
 
-	public function addScopeItems($items) {
-		foreach($items as $key => $value) {
-			$this->scopeItems[$key] = $value;
-		}
-	}
+    public static function getRequestInstance()
+    {
+        if (null === self::$requestInstance) {
+            View::setRequestInstance(new View());
+        }
 
-	public static function getRequestInstance() {
-		if (null === self::$requestInstance) {
-			View::setRequestInstance(new View());
-		}
-		return self::$requestInstance;
-	}
+        return self::$requestInstance;
+    }
 
-	public function __construct($mixed = false) {
-		$this->constructView($mixed);
-	}
+    public function __construct($mixed = false)
+    {
+        $this->constructView($mixed);
+    }
 
-	protected static function setRequestInstance(View $v) {
-		View::$requestInstances[] = $v;
-		self::$requestInstance = $v;
-	}
+    protected static function setRequestInstance(View $v)
+    {
+        View::$requestInstances[] = $v;
+        self::$requestInstance = $v;
+    }
 
-	protected static function revertRequestInstance() {
-		array_pop(View::$requestInstances);
-		self::$requestInstance = last(View::$requestInstances);
-	}
+    protected static function revertRequestInstance()
+    {
+        array_pop(View::$requestInstances);
+        self::$requestInstance = last(View::$requestInstances);
+    }
 
-	public $controller;
+    abstract public function start($state);
 
-	protected $template;
-	protected $outputAssets = array();
+    public function startRender()
+    {
+        if (is_object($this->controller)) {
+            $this->controller->on_before_render();
+        }
+    }
+    abstract protected function constructView($mixed);
 
-	abstract public function start($state);
-	public function startRender() {
-		if (is_object($this->controller)) {
-			$this->controller->on_before_render();
-		}
-	}
-	abstract protected function constructView($mixed);
-	abstract public function setupRender();
-	abstract public function finishRender($contents);
-	abstract public function action($action);
+    abstract public function setupRender();
 
+    abstract public function finishRender($contents);
 
-	public function addHeaderAsset($asset) {
-		$r = ResponseAssetGroup::get();
-		$r->addHeaderAsset($asset);
-	}
+    abstract public function action($action);
 
-	public function addFooterAsset($asset) {
-		$r = ResponseAssetGroup::get();
-		$r->addFooterAsset($asset);
-	}
+    public function addHeaderAsset($asset)
+    {
+        $r = ResponseAssetGroup::get();
+        $r->addHeaderAsset($asset);
+    }
 
-	public function addOutputAsset($asset) {
-		$r = ResponseAssetGroup::get();
-		$r->addOutputAsset($asset);
-	}
+    public function addFooterAsset($asset)
+    {
+        $r = ResponseAssetGroup::get();
+        $r->addFooterAsset($asset);
+    }
 
-	public function requireAsset($assetType, $assetHandle = false) {
-		$r = ResponseAssetGroup::get();
-		$r->requireAsset($assetType, $assetHandle);
-	}
+    public function addOutputAsset($asset)
+    {
+        $r = ResponseAssetGroup::get();
+        $r->addOutputAsset($asset);
+    }
 
-	public function setController($controller) {
-		$this->controller = $controller;
-	}
+    public function requireAsset($assetType, $assetHandle = false)
+    {
+        $r = ResponseAssetGroup::get();
+        $r->requireAsset($assetType, $assetHandle);
+    }
 
-	public function setViewTemplate($template) {
-		$this->template = $template;
-	}
+    public function setController($controller)
+    {
+        $this->controller = $controller;
+    }
 
-	/**
-	 * Returns the value of the item in the POST array.
-	 * @access public
-	 * @param $key
-	 * @return void
-	*/
-	public function post($key, $defaultValue = null) {
-		$r = Request::getInstance();
-		return $r->post($key, $defaultValue);
-	}
+    public function setViewTemplate($template)
+    {
+        $this->template = $template;
+    }
 
-	abstract protected function onBeforeGetContents();
+    /**
+     * Returns the value of the item in the POST array.
+     *
+     * @param $key
+     */
+    public function post($key, $defaultValue = null)
+    {
+        $r = Request::getInstance();
 
-	protected function postProcessViewContents($contents) {return $contents;}
-	protected function onAfterGetContents() {}
+        return $r->post($key, $defaultValue);
+    }
 
-	public function getScopeItems() {
-		if (is_object($this->controller)) {
-			$sets = $this->controller->getSets();
-			$helpers = $this->controller->getHelperObjects();
-			$return = array_merge($this->scopeItems, $sets, $helpers);
-		} else {
-			$return = $this->scopeItems;
-		}
-		$return['view'] = $this;
-		$return['controller'] = $this->controller;
-		return $return;
-	}
+    abstract protected function onBeforeGetContents();
 
-	public function render($state = false) {
-		if ($this instanceof View) {
-			$this->setRequestInstance($this);
-		}
-		$this->start($state);
-		$this->setupRender();
-		$this->startRender();
-		$scopeItems = $this->getScopeItems();
-		$contents = $this->renderViewContents($scopeItems);
-		$contents = $this->postProcessViewContents($contents);
-		$response = $this->finishRender($contents);
-		if ($this instanceof View) {
-			$this->revertRequestInstance();
-		}
-		return $response;
-	}
+    protected function postProcessViewContents($contents)
+    {
+        return $contents;
+    }
+    protected function onAfterGetContents()
+    {
+    }
 
-	public function renderViewContents($scopeItems) {
-		if (file_exists($this->template)) {
-			extract($scopeItems);
-			ob_start();
-			$this->onBeforeGetContents();
-			include($this->template);
-			$this->onAfterGetContents();
-			$contents = ob_get_contents();
-			ob_end_clean();
-			return $contents;
-		}
-	}
+    public function getScopeItems()
+    {
+        if (is_object($this->controller)) {
+            $sets = $this->controller->getSets();
+            $helpers = $this->controller->getHelperObjects();
+            $return = array_merge($this->scopeItems, $sets, $helpers);
+        } else {
+            $return = $this->scopeItems;
+        }
+        $return['view'] = $this;
+        $return['controller'] = $this->controller;
 
-	/**
-	 * url is a utility function that is used inside a view to setup urls w/tasks and parameters
-	 * @deprecated
-	 * @param string $action
-	 * @param string $task
-	 * @return string $url
-	*/
-	public function url($action, $task = null) {
-		$args = func_get_args();
-		return (string) call_user_func_array(array('URL', 'to'), $args);
-	}
+        return $return;
+    }
 
-	// Legacy Items. Deprecated
+    public function render($state = false)
+    {
+        if ($this instanceof View) {
+            $this->setRequestInstance($this);
+        }
+        $this->start($state);
+        $this->setupRender();
+        $this->startRender();
+        $scopeItems = $this->getScopeItems();
+        $contents = $this->renderViewContents($scopeItems);
+        $contents = $this->postProcessViewContents($contents);
+        $response = $this->finishRender($contents);
+        if ($this instanceof View) {
+            $this->revertRequestInstance();
+        }
 
-	public function setThemeByPath($path, $theme = NULL, $wrapper = FILENAME_THEMES_VIEW) {
-		$l = Router::get();
-		$l->setThemeByRoute($path, $theme, $wrapper);
-	}
+        return $response;
+    }
 
+    public function renderViewContents($scopeItems)
+    {
+        if (file_exists($this->template)) {
+            extract($scopeItems);
+            ob_start();
+            $this->onBeforeGetContents();
+            include $this->template;
+            $this->onAfterGetContents();
+            $contents = ob_get_contents();
+            ob_end_clean();
 
-	public function renderError($title, $error, $errorObj = null) {
-		Loader::helper('concrete/ui')->renderError($title, $error);
-	}
+            return $contents;
+        }
+    }
 
-	/**
-	 * @access private
-	 */
-	public function addHeaderItem($item) {
-		$this->addHeaderAsset($item);
-	}
+    /**
+     * url is a utility function that is used inside a view to setup urls w/tasks and parameters.
+     *
+     * @deprecated
+     *
+     * @param string $action
+     * @param string $task
+     *
+     * @return string $url
+     */
+    public function url($action, $task = null)
+    {
+        $args = func_get_args();
 
-	/**
-	 * @access private
-	 */
-	public function addFooterItem($item) {
-		$this->addFooterAsset($item);
-	}
+        return (string) call_user_func_array(array('URL', 'to'), $args);
+    }
+
+    // Legacy Items. Deprecated
+
+    public function setThemeByPath($path, $theme = null, $wrapper = FILENAME_THEMES_VIEW)
+    {
+        $l = Router::get();
+        $l->setThemeByRoute($path, $theme, $wrapper);
+    }
+
+    public function renderError($title, $error, $errorObj = null)
+    {
+        Loader::helper('concrete/ui')->renderError($title, $error);
+    }
+
+    /**
+     */
+    public function addHeaderItem($item)
+    {
+        $this->addHeaderAsset($item);
+    }
+
+    /**
+     */
+    public function addFooterItem($item)
+    {
+        $this->addFooterAsset($item);
+    }
 
     /**
      * @return View
      */
-    public static function getInstance() {
-		return View::getRequestInstance();
-	}
-
-
+    public static function getInstance()
+    {
+        return View::getRequestInstance();
+    }
 }
