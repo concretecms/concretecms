f9942e9caffdd043c6d55a6b6370311cd5751a4b
Remo Laubacher <remo.laubacher@gmail.com>
2015-11-26 23:01:24 +0100

    fix column name

--- web/concrete/src/User/UserInfo.php
+++ web/concrete/src/User/UserInfo.php
@@ -125,7 +125,7 @@ class UserInfo extends Object implements \Concrete\Core\Permission\ObjectInterfa
             }
         }
 
-        $r = $db->query("DELETE FROM OauthUserMap WHERE uID = ?", array(intval($this->uID)));
+        $r = $db->query("DELETE FROM OauthUserMap WHERE user_id = ?", array(intval($this->uID)));
         
         $r = $db->query("DELETE FROM UserSearchIndexAttributes WHERE uID = ?", array(intval($this->uID)));
 
