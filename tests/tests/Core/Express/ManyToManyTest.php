2a0c3fd9d49bc598eede3772c950250019399b60
Andrew Embler <andrew@concrete5.org>
2015-11-29 06:07:33 -0800

    starting a new test

---
+++ tests/tests/Core/Express/ManyToManyTest.php
@@ -0,0 +1,54 @@
+<?php
+use Concrete\Core\Express\ObjectBuilder;
+
+require_once __DIR__ . "/ObjectBuilderTestTrait.php";
+
+class ManyToManyTest extends PHPUnit_Framework_TestCase
+{
+
+    protected function getEntityManager()
+    {
+        $user = new \Concrete\Core\Entity\Express\Entity();
+        $user->setTableName('Users');
+        $user->setName('User');
+
+        $group = new \Concrete\Core\Entity\Express\Entity();
+        $group->setTableName('Groups');
+        $group->setName('Group');
+
+        $builder->addOneToMany($teacher, $student);
+        $builder->addManyToOne($student, $teacher);
+
+        return $this->deliverTeachersAndStudents($student, $teacher);
+
+    }
+    public function testCreateDataObject()
+    {
+        $builder = $this->getObjectBuilder();
+
+        /**
+         * @var $object \Concrete\Core\Entity\Express\Entity
+         */
+        $object = $builder->buildObject();
+        $this->assertInstanceOf('\Concrete\Core\Entity\Express\Entity', $object);
+        $this->assertEquals(4, count($object->getAttributes()));
+        $attributes = $object->getAttributes();
+        $this->assertInstanceOf('\Concrete\Core\Entity\AttributeKey\TextAttributeKey', $attributes[0]);
+        $this->assertInstanceOf('\Concrete\Core\Entity\AttributeKey\TextAttributeKey', $attributes[1]);
+        $this->assertInstanceOf('\Concrete\Core\Entity\AttributeKey\TextAreaAttributeKey', $attributes[2]);
+
+        /** @var $first \Concrete\Core\Entity\AttributeKey\TextAttributeKey */
+        $first = $attributes[0];
+        $this->assertEquals('First Name', $first->getName());
+        $this->assertEquals(false, $first->getIsIndexed());
+        $this->assertEquals(true, $first->getIsSearchable());
+
+        /** @var $bio \Concrete\Core\Entity\AttributeKey\TextAreaAttributeKey */
+        $bio = $attributes[2];
+        $this->assertEquals('rich_text', $bio->getMode());
+        $this->assertEquals(true, $bio->getIsIndexed());
+        $this->assertEquals(true, $bio->getIsSearchable());
+    }
+
+
+}
