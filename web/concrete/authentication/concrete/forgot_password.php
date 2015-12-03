50c6f002471429668cf48a557b2f9572c77539c7
Michele Locati <michele@locati.it>
2015-12-03 15:13:44 +0100

    Remove misplaced div
    Former-commit-id: 034291e512bc7c49441183f79ffe9c6596cd3674
    Former-commit-id: 47173eda7aaaaeeda98b6997d1274d081841c7bd

--- web/concrete/authentication/concrete/forgot_password.php
+++ web/concrete/authentication/concrete/forgot_password.php
@@ -2,16 +2,15 @@
 
 <div class="forgotPassword">
 	<form method="post" action="<?= URL::to('/login', 'callback', $authType->getAuthenticationTypeHandle(), 'forgot_password') ?>">
+		<h4><?= t('Forgot Your Password?') ?></h4>
+		<div class="ccm-message"><?= isset($intro_msg) ? $intro_msg : '' ?></div>
+			<div class='help-block'>
+				<?= t('Enter your email address below. We will send you instructions to reset your password.') ?>
+			</div>
+		</div>
 		<div class="form-group">
-			<h4><?= t('Forgot Your Password?') ?></h4>
-			<div class="ccm-message"><?= isset($intro_msg) ? $intro_msg : '' ?></div>
-				<div class='help-block'>
-					<?= t('Enter your email address below. We will send you instructions to reset your password.') ?>
-				</div>
-			</div>
-			<div class="form-group">
-				<input name="uEmail" type="email" placeholder="<?= t('Email Address') ?>" class="form-control" />
-			</div>
-			<button name="resetPassword" class="btn btn-primary btn-block"><?= t('Reset and Email Password') ?></button>
+			<input name="uEmail" type="email" placeholder="<?= t('Email Address') ?>" class="form-control" />
+		</div>
+		<button name="resetPassword" class="btn btn-primary btn-block"><?= t('Reset and Email Password') ?></button>
 	</form>
 </div>
