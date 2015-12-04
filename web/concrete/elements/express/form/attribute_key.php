74f01bd587f672fe9ada7809643b272fed2cb146
Andrew Embler <andrew@concrete5.org>
2015-12-04 14:35:39 -0800

    more

---
+++ web/concrete/elements/express/form/attribute_key.php
@@ -0,0 +1,8 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+?>
+
+<div class="form-group">
+    <label class="control-label"><?=$label?></label>
+    <?=$key->render('composer')?>
+</div>
