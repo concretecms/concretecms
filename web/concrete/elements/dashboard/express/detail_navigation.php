d9e8d1923ebee5f0b485e14ae0b6a30045136629
Andrew Embler <andrew@concrete5.org>
2015-12-07 19:48:01 -0800

    data types work
    Former-commit-id: d3b29d1e259598726ee11bbcc59d2b16deec4ae3
    Former-commit-id: 33cf89893ce40b6a99e8687c74f81844471864b7

---
+++ web/concrete/elements/dashboard/express/detail_navigation.php
@@ -0,0 +1,11 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$c = Page::getCurrentPage();
+?>
+<div class="col-md-4">
+	<div class="list-group">
+		<a class="list-group-item <? if ($view->controller->getTask() == 'view_entity') { ?>active<? } ?>" href="<?=URL::to('/dashboard/express/entities', 'view_entity', $entity->getId())?>"><?=t('Overview')?></a>
+		<a class="list-group-item <? if ($view->controller->getTask() == 'attributes') { ?>active<? } ?>" href="<?=URL::to('/dashboard/express/entities', 'attributes', $entity->getId())?>"><?=t('Attributes')?></a>
+		<a class="list-group-item <? if ($view->controller->getTask() == 'associations') { ?>active<? } ?>" href="<?=URL::to('/dashboard/express/entities', 'associations', $entity->getId())?>"><?=t('Associations')?></a>
+	</div>
+</div>
