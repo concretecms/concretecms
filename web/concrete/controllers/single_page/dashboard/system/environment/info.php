ff816ddbcb5f56795a7d9f086dbf80ddb5e2759e
Michele Locati <michele@locati.it>
2015-12-10 15:34:20 +0100

    Avoid "Only variables should be passed by reference" warnings in environment info

--- web/concrete/controllers/single_page/dashboard/system/environment/info.php
+++ web/concrete/controllers/single_page/dashboard/system/environment/info.php
@@ -85,15 +85,17 @@ class Info extends DashboardPageController
 
         ob_start();
         phpinfo();
-        $phpinfo = array('phpinfo' => array());
+        $section = 'phpinfo';
+        $phpinfo = array($section => array());
         if (preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER)) {
             foreach ($matches as $match) {
                 if (strlen($match[1])) {
-                    $phpinfo[$match[1]] = array();
+                    $section = $match[1];
+                    $phpinfo[$section] = array();
                 } elseif (isset($match[3])) {
-                    $phpinfo[end(array_keys($phpinfo))][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
+                    $phpinfo[$section][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
                 } else {
-                    $phpinfo[end(array_keys($phpinfo))][] = $match[2];
+                    $phpinfo[$section][] = $match[2];
                 }
             }
         }
