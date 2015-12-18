9dea9591bf36665df39e3277222df3f1f8af24f0
Michele Locati <michele@locati.it>
2015-12-18 09:34:57 +0100

    Fix CLI run with symlinked core directory under Windows

--- web/concrete/bin/concrete5.php
+++ web/concrete/bin/concrete5.php
@@ -1,5 +1,33 @@
 <?php
 $DIR_BASE_CORE = dirname(__DIR__);
+
+if (stripos(PHP_OS, 'WIN') === 0) {
+    foreach (array('PHP_SELF', 'SCRIPT_NAME', 'SCRIPT_FILENAME', 'PATH_TRANSLATED') as $key) {
+        // Check if the key is valid
+        if (!isset($_SERVER[$key])) {
+            continue;
+        }
+        $value = $_SERVER[$key];
+        if (!is_file($value)) {
+            continue;
+        }
+        // Just to simplify the regular expressions
+        $value = str_replace('\\', '/', $value);
+        // Check if the key is an absolute path
+        if (preg_match('%^([A-Z]:/|//\w)%i', $value) !== 1) {
+            continue;
+        }
+        if (preg_match('%/\.{1,2}/%', $value) !== 0) {
+            continue;
+        }
+        // Ok!
+        $DIR_BASE_CORE = dirname(dirname($value));
+        break;
+    }
+    unset($key);
+    unset($value);
+}
+
 define('DIR_BASE', dirname($DIR_BASE_CORE));
 
 require $DIR_BASE_CORE . '/bootstrap/configure.php';
