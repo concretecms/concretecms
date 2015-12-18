c5990b848712a3d3028576eb338370f7e4172fcb
Michele Locati <michele@locati.it>
2015-12-18 10:07:27 +0100

    Fix symlinked core for *nix too
    Former-commit-id: a3a3278792468a613bf74cf494a0bafe9138b3b3
    Former-commit-id: 87d210c30bc09d8ac2bcb2521e568478a924199a

--- web/concrete/bin/concrete5.php
+++ web/concrete/bin/concrete5.php
@@ -1,32 +1,36 @@
 <?php
 $DIR_BASE_CORE = dirname(__DIR__);
 
-if (stripos(PHP_OS, 'WIN') === 0) {
-    foreach (array('PHP_SELF', 'SCRIPT_NAME', 'SCRIPT_FILENAME', 'PATH_TRANSLATED') as $key) {
-        // Check if the key is valid
-        if (!isset($_SERVER[$key])) {
-            continue;
-        }
-        $value = $_SERVER[$key];
-        if (!is_file($value)) {
-            continue;
-        }
+foreach (array('PHP_SELF', 'SCRIPT_NAME', 'SCRIPT_FILENAME', 'PATH_TRANSLATED') as $key) {
+    // Check if the key is valid
+    if (!isset($_SERVER[$key])) {
+        continue;
+    }
+    $value = $_SERVER[$key];
+    if (!is_file($value)) {
+        continue;
+    }
+    if (stripos(PHP_OS, 'WIN') === 0) {
         // Just to simplify the regular expressions
         $value = str_replace('\\', '/', $value);
         // Check if the key is an absolute path
         if (preg_match('%^([A-Z]:/|//\w)%i', $value) !== 1) {
             continue;
         }
-        if (preg_match('%/\.{1,2}/%', $value) !== 0) {
+    } else {
+        if (preg_match('%^/%i', $value) !== 1) {
             continue;
         }
-        // Ok!
-        $DIR_BASE_CORE = dirname(dirname($value));
-        break;
     }
-    unset($key);
-    unset($value);
+    if (preg_match('%/\.{1,2}/%', $value) !== 0) {
+        continue;
+    }
+    // Ok!
+    $DIR_BASE_CORE = dirname(dirname($value));
+    break;
 }
+unset($key);
+unset($value);
 
 define('DIR_BASE', dirname($DIR_BASE_CORE));
 
