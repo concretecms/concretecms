f64db40005980cc7af43c1cca23c4a2fd2781da7
Andrew Embler <andrew@concrete5.org>
2015-12-21 09:05:36 -0800

    Adding upgrade

--- web/concrete/src/Updater/Migrations/Migrations/Version20151221000000.php
+++ web/concrete/src/Updater/Migrations/Migrations/Version20151221000000.php
@@ -12,11 +12,18 @@ class Version20151221000000 extends AbstractMigration
 
     public function up(Schema $schema)
     {
+        // image resizing options
         $sp = Page::getByPath('/dashboard/system/files/image_uploading');
         if (!is_object($sp) || $sp->isError()) {
             $sp = \Concrete\Core\Page\Single::add('/dashboard/system/files/image_uploading');
             $sp->update(array('cName' => 'Image Uploading'));
         }
+
+        // background size/position
+        \Concrete\Core\Database\Schema\Schema::refreshCoreXMLSchema(array(
+            'StyleCustomizerInlineStyleSets',
+        ));
+
     }
 
     public function down(Schema $schema)
