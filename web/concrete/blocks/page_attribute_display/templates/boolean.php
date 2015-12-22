dcee36c9d74a3f21843d36ef9ebb63a1c7a4b3a9
Michele Locati <michele@locati.it>
2015-12-22 15:56:30 +0100

    PSR
    Former-commit-id: e5f215c44679782005259df3c5b20016f5529cdf
    Former-commit-id: 9378b715eb7d9829d157cc874ec29c64a7d857af

--- web/concrete/blocks/page_attribute_display/templates/boolean.php
+++ web/concrete/blocks/page_attribute_display/templates/boolean.php
@@ -2,5 +2,5 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 echo $this->controller->getOpenTag();
 echo $this->controller->getTitle();
-echo ($this->controller->getContent()?t('Yes'):t('No'));
+echo($this->controller->getContent() ? t('Yes') : t('No'));
 echo $this->controller->getCloseTag();
--- web/concrete/blocks/page_attribute_display/templates/date_time.php
+++ web/concrete/blocks/page_attribute_display/templates/date_time.php
@@ -4,9 +4,9 @@ $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service
 echo $this->controller->getOpenTag();
 echo $this->controller->getTitle();
 try {
-    $format = (strlen($this->controller->dateFormat)?$this->controller->dateFormat:"m/d/y");
+    $format = (strlen($this->controller->dateFormat) ? $this->controller->dateFormat : "m/d/y");
     echo $dh->formatCustom($format, $this->controller->getContent());
-} catch(\Exception $e) {
+} catch (\Exception $e) {
     print $this->controller->getContent();
 }
 echo $this->controller->getCloseTag();
