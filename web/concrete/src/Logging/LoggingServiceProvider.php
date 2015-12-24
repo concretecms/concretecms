6af5a00be742611f01bfec662ed8cc24f2d9e199
Korvin Szanto <korvinszanto@gmail.com>
2015-12-24 13:35:40 -0800

    Bind the logger against concrete types and the PSR-3 interface
    Former-commit-id: b60b777f9ee746d3dae86b99ba6b2b03f3bf6940
    Former-commit-id: e9bfa2e213b5fdb949a57e8059e60029549312bf

--- web/concrete/src/Logging/LoggingServiceProvider.php
+++ web/concrete/src/Logging/LoggingServiceProvider.php
@@ -1,13 +1,18 @@
-<?php 
+<?php
 namespace Concrete\Core\Logging;
 use \Concrete\Core\Foundation\Service\Provider as ServiceProvider;
 
 class LoggingServiceProvider extends ServiceProvider {
 
 	public function register() {
-        $logger = new Logger();
-        $this->app->instance('log', $logger);
-	}
+        // Bind the logger singleton
+        $this->app->singleton('Concrete\Core\Logging\Logger', function($app) {
+            $app->build('Concrete\Core\Logging\Logger', array(null, null));
+        });
 
+        // Bind the PSR-3 logger interface against the singleton
+        $this->app->bind('Psr\Log\LoggerInterface', 'Concrete\Core\Logging\Logger');
+        $this->app->bind('log', 'Concrete\Core\Logging\Logger');
+	}
 
 }
