5eb371665b3bf0e580483dd64e13ea48e130520e
Korvin Szanto <korvinszanto@gmail.com>
2015-12-24 13:36:13 -0800

    Reformat LoggingServiceProvider
    Former-commit-id: cc9b4b3e0dd3fc3c8da11d69f334b55c5f66b136
    Former-commit-id: 5a8703f97f08c95c74ae12a0c2a09833f05da8aa

--- web/concrete/src/Logging/LoggingServiceProvider.php
+++ web/concrete/src/Logging/LoggingServiceProvider.php
@@ -1,18 +1,21 @@
 <?php
 namespace Concrete\Core\Logging;
-use \Concrete\Core\Foundation\Service\Provider as ServiceProvider;
 
-class LoggingServiceProvider extends ServiceProvider {
+use Concrete\Core\Foundation\Service\Provider as ServiceProvider;
 
-	public function register() {
+class LoggingServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
         // Bind the logger singleton
-        $this->app->singleton('Concrete\Core\Logging\Logger', function($app) {
+        $this->app->singleton('Concrete\Core\Logging\Logger', function ($app) {
             $app->build('Concrete\Core\Logging\Logger', array(null, null));
         });
 
         // Bind the PSR-3 logger interface against the singleton
         $this->app->bind('Psr\Log\LoggerInterface', 'Concrete\Core\Logging\Logger');
         $this->app->bind('log', 'Concrete\Core\Logging\Logger');
-	}
+    }
 
 }
