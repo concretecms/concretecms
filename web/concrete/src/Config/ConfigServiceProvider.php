45eded66b6231152112e55fb54ce3760d688601a
Korvin Szanto <korvinszanto@gmail.com>
2015-12-26 13:11:54 -0800

    Move config bindings to a service provider

---
+++ web/concrete/src/Config/ConfigServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+namespace Concrete\Core\Config;
+
+use Concrete\Core\Foundation\Service\Provider;
+
+class ConfigServiceProvider extends Provider
+{
+    /**
+     * Configuration repositories
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerFileConfig();
+        $this->registerDatabaseConfig();
+    }
+
+    public function registerFileConfig()
+    {
+        // Bind abstract type
+        $this->app->singleton('config', function($app) {
+            $loader = $app->make('Concrete\Core\Config\FileLoader');
+            $saver = $app->make('Concrete\Core\Config\FileSaver');
+
+            return $app->build('Concrete\Core\Config\Repository\Repository', array($loader, $saver, $app->environment()));
+        });
+
+        // Bind the concrete type
+        $this->app->bind('Concrete\Core\Config\Repository\Repository', 'config');
+        $this->app->bind('Illuminate\Config\Repository', 'Concrete\Core\Config\Repository\Repository');
+    }
+
+    public function registerDatabaseConfig()
+    {
+        $this->app->bindShared('config/database', function($app) {
+            $loader = $app->make('Concrete\Core\Config\DatabaseLoader');
+            $saver = $app->make('Concrete\Core\Config\DatabaseSaver');
+
+            return $app->build('Concrete\Core\Config\Repository\Repository', array($loader, $saver, $app->environment()));
+        });
+    }
+
+}
