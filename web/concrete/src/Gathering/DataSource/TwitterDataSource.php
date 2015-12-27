95ee5ae351313cfb4dbd00e37984bb750abfa930
Korvin Szanto <korvinszanto@gmail.com>
2015-12-26 22:40:44 -0800

    Rewrite old twitter gathering datasource to work with the other oauth dependency

--- web/concrete/src/Gathering/DataSource/TwitterDataSource.php
+++ web/concrete/src/Gathering/DataSource/TwitterDataSource.php
@@ -2,6 +2,11 @@
 namespace Concrete\Core\Gathering\DataSource;
 
 use Concrete\Core\Gathering\DataSource\Configuration\Configuration as GatheringDataSourceConfiguration;
+use Concrete\Core\Url\Url;
+use OAuth\Common\Consumer\Credentials;
+use OAuth\Common\Storage\Memory;
+use OAuth\Common\Storage\SymfonySession;
+use OAuth\OAuth1\Token\StdOAuth1Token;
 
 class TwitterDataSource extends DataSource
 {
@@ -15,11 +20,40 @@ class TwitterDataSource extends DataSource
         return $o;
     }
 
+    private function getTwitterService()
+    {
+        $key = TWITTER_APP_CONSUMER_KEY;
+        $secret = TWITTER_APP_CONSUMER_SECRET;
+        $access_token = TWITTER_APP_ACCESS_TOKEN;
+        $access_secret = TWITTER_APP_ACCESS_SECRET;
+
+        /** @var \OAuth\ServiceFactory $factory */
+        $factory = \Core::make('oauth/factory/service');
+
+        // Initialize the token
+        $token = new StdOAuth1Token($access_token);
+        $token->setAccessTokenSecret($access_secret);
+
+        // Store the token in memory
+        $storage = new Memory();
+        $storage->storeAccessToken('Twitter', $token);
+
+        // Create the twitter service
+        return $factory->createService('twitter', new Credentials($key, $secret, ''), $storage);
+    }
+
     public function createGatheringItems(GatheringDataSourceConfiguration $configuration)
     {
-        $connection = new TwitterOAuth(TWITTER_APP_CONSUMER_KEY, TWITTER_APP_CONSUMER_SECRET, TWITTER_APP_ACCESS_TOKEN,
-            TWITTER_APP_ACCESS_SECRET);
-        $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" . $configuration->getTwitterUsername() . "&count=50");
+        $twitter = $this->getTwitterService();
+
+        $url = Url::createFromUrl('');
+        $url->setPath('/statuses/user_timeline.json');
+        $url->setQuery(array(
+            'screen_name' => $configuration->getTwitterUsername(),
+            'count' => 50
+        ));
+
+        $tweets = json_decode($twitter->request($url));
         if (!empty($tweets->errors[0])) {
             throw new Exception($tweets->errors[0]->message);
         }
