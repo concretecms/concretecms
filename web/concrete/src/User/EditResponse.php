9b25f98eabebeb4f5160f30282d50745dd4f783d
Korvin Szanto <korvinszanto@gmail.com>
2015-12-27 20:11:34 -0800

    PSR-2 reformat
    Former-commit-id: e070526fcd3a658fdafbaf694299d4e837b60388
    Former-commit-id: 89da0b7ac4e0fc9184a2d3c7a758753bbda4e4c4

--- web/concrete/src/User/EditResponse.php
+++ web/concrete/src/User/EditResponse.php
@@ -1,29 +1,33 @@
 <?php
 namespace Concrete\Core\User;
+
 use stdclass;
 
-class EditResponse extends \Concrete\Core\Application\EditResponse {
+class EditResponse extends \Concrete\Core\Application\EditResponse
+{
 
-	protected $users = array();
+    protected $users = array();
 
-	public function setUser(UserInfo $user) {
-		$this->users[] = $user;
-	}
+    public function setUser(UserInfo $user)
+    {
+        $this->users[] = $user;
+    }
 
-	public function setUsers($users) {
-		$this->users = $users;
-	}
+    public function setUsers($users)
+    {
+        $this->users = $users;
+    }
 
-	public function getJSONObject() {
-		$o = parent::getBaseJSONObject();
-		foreach($this->users as $user) {
-			$uo = new stdClass;
-			$uo->uID = $user->getUserID();
-			$uo->displayName = $user->getUserDisplayName();
-			$o->users[] = $uo;
-		}
-		return $o;
-	}
-
+    public function getJSONObject()
+    {
+        $o = parent::getBaseJSONObject();
+        foreach ($this->users as $user) {
+            $uo = new stdClass;
+            $uo->uID = $user->getUserID();
+            $uo->displayName = $user->getUserDisplayName();
+            $o->users[] = $uo;
+        }
+        return $o;
+    }
 
 }
