ea9b18922a1b6dd2cf6daea10c1ad503604ad79e
Korvin Szanto <korvinszanto@gmail.com>
2016-01-05 19:57:38 -0800

    Update CanonicalUrlResolver to cache the resolved canonical url to improve performance
    This is pretty much a micro-optimization, but it saves us a few thousand function calls in a single page load so lets do it.
    
    
    Former-commit-id: b1a151940c39921844a81585fe5b97db19fbecd1
    Former-commit-id: 2e58738d6b5b8c1601ea1180108485ecc73f8c9e

--- web/concrete/src/Url/Resolver/CanonicalUrlResolver.php
+++ web/concrete/src/Url/Resolver/CanonicalUrlResolver.php
@@ -17,60 +17,77 @@ class CanonicalUrlResolver implements UrlResolverInterface
     /** @var Application */
     protected $app;
 
+    /** @var Url */
+    protected $cached;
+
     public function __construct(Application $app, Request $request)
     {
         $this->app = $app;
         $this->request = $request;
     }
 
-   /**
-    * Resolve url's from any type of input.
-    *
-    * This method MUST either return a `\League\URL\URL` when a url is resolved
-    * or null when a url cannot be resolved.
-    *
-    * @param array                    $arguments A list of the arguments
-    * @param \League\URL\URLInterface $resolved
-    *
-    * @return \League\URL\URLInterface
-    */
-   public function resolve(array $arguments, $resolved = null)
-   {
-       $url = Url::createFromUrl('');
+    /**
+     * Resolve url's from any type of input.
+     *
+     * This method MUST either return a `\League\URL\URL` when a url is resolved
+     * or null when a url cannot be resolved.
+     *
+     * @param array $arguments A list of the arguments
+     * @param \League\URL\URLInterface $resolved
+     *
+     * @return \League\URL\URLInterface
+     */
+    public function resolve(array $arguments, $resolved = null)
+    {
+        if ($this->cached) {
+            return $this->cached;
+        }
 
-       $url->setHost(null);
-       $url->setScheme(null);
+        $url = Url::createFromUrl('');
 
+        $url->setHost(null);
+        $url->setScheme(null);
 
-       if (\Config::get('concrete.seo.canonical_url')) {
-           $canonical = UrlImmutable::createFromUrl(Config::get('concrete.seo.canonical_url'));
+        if (\Config::get('concrete.seo.canonical_url')) {
+            $canonical = UrlImmutable::createFromUrl(Config::get('concrete.seo.canonical_url'));
 
-           // If the request is over https and the canonical url is http, lets just say https for the canonical url.
-           if (strtolower($canonical->getScheme()) == 'http' && strtolower($this->request->getScheme()) == 'https') {
-               $url->setScheme('https');
-           } else {
-               $url->setScheme($canonical->getScheme());
-           }
+            // If the request is over https and the canonical url is http, lets just say https for the canonical url.
+            if (strtolower($canonical->getScheme()) == 'http' && strtolower($this->request->getScheme()) == 'https') {
+                $url->setScheme('https');
+            } else {
+                $url->setScheme($canonical->getScheme());
+            }
 
-           $url->setHost($canonical->getHost());
+            $url->setHost($canonical->getHost());
 
-           if (intval($canonical->getPort()->get()) > 0) {
-               $url->setPort($canonical->getPort());
-           }
-       } else {
-           $host = $this->request->getHost();
-           $scheme = $this->request->getScheme();
-           if ($scheme && $host) {
-               $url->setScheme($scheme)
-                   ->setHost($host)
-                   ->setPortIfNecessary(Request::getInstance()->getPort());
-           }
-       }
+            if (intval($canonical->getPort()->get()) > 0) {
+                $url->setPort($canonical->getPort());
+            }
+        } else {
+            $host = $this->request->getHost();
+            $scheme = $this->request->getScheme();
+            if ($scheme && $host) {
+                $url->setScheme($scheme)
+                    ->setHost($host)
+                    ->setPortIfNecessary(Request::getInstance()->getPort());
+            }
+        }
 
-       if ($relative_path = \Core::getApplicationRelativePath()) {
-           $url = $url->setPath($relative_path);
-       }
+        if ($relative_path = \Core::getApplicationRelativePath()) {
+            $url = $url->setPath($relative_path);
+        }
 
-       return UrlImmutable::createFromUrl($url);
-   }
+        $this->cached = UrlImmutable::createFromUrl($url);
+
+        return $this->cached;
+    }
+
+    /**
+     * Clear the cached canonical URL
+     */
+    public function clearCached()
+    {
+        $this->cached = null;
+    }
+
 }
