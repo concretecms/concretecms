d9f68207ad36a23e82960704f4bbcd49ea21e796
Korvin Szanto <korvinszanto@gmail.com>
2016-01-07 16:42:55 -0800

    Feature: Attach mode for CLI installer
    When the `--attach` flag is supplied with a `concrete5 c5:install` call, if the supplied database already has rows we will attach to it rather than failing.
    
    This change adds an interface that lets you mark your own `StartingPointInstallRoutine` steps as required to run in attach mode.
    
    This should resolve #3298
    
    
    Former-commit-id: 07f678900d526322de6639f3a57a38a479b2758b
    Former-commit-id: c9a26b153374ca52f622b68f22272f40e20cd476

---
+++ web/concrete/src/Package/Routine/AttachModeInstallRoutine.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Concrete\Core\Package\Routine;
+
+use Concrete\Core\Package\StartingPointInstallRoutine;
+
+class AttachModeInstallRoutine extends StartingPointInstallRoutine implements AttachModeCompatibleRoutineInterface
+{
+
+}
