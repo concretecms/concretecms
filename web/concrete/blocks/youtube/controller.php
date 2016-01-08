879af192f14e2fca89c17070e805aa443258669a
Ryan Hewitt <ryan@mesuva.com.au>
2016-01-09 00:47:31 +1030

    YouTube block upgrade/tidy

--- web/concrete/blocks/youtube/controller.php
+++ web/concrete/blocks/youtube/controller.php
@@ -8,7 +8,7 @@ class Controller extends BlockController
 {
     protected $btTable = 'btYouTube';
     protected $btInterfaceWidth = "400";
-    protected $btInterfaceHeight = "430";
+    protected $btInterfaceHeight = "490";
     protected $btCacheBlockRecord = true;
     protected $btCacheBlockOutput = true;
     protected $btCacheBlockOutputOnPost = true;
@@ -16,7 +16,7 @@ class Controller extends BlockController
 
     public $title = '';
     public $videoURL = "";
-    public $vHeight = "344";
+    public $vHeight = "370";
     public $vWidth = "425";
     public $vPlayer = '1';
     public $mode = "youtube";
@@ -47,15 +47,37 @@ class Controller extends BlockController
         $this->requireAsset('swfobject');
     }
 
+    public function edit()
+    {
+        if ($this->vWidth || $this->vWidth) {
+            $this->set('sizing', 'fixed');
+        }
+    }
+
     public function view()
     {
-        $this->set('bID', $this->bID);
-        $this->set('title', $this->title);
-        $this->set('videoURL', $this->videoURL);
-        $this->set('vHeight', $this->vHeight);
-        $this->set('vWidth', $this->vWidth);
-        $this->set('vPlayer', $this->vPlayer);
-        $this->set('mode', $this->mode);
+        $url       = parse_url($this->videoURL);
+        $pathParts = explode('/', rtrim($url['path'], '/'));
+        $videoID   = end($pathParts);
+        $playListID = '';
+
+        if (isset($url['query'])) {
+            parse_str($url['query'], $query);
+
+            if (isset($query['list'])) {
+                $playListID = $query['list'];
+                $videoID = '';
+            } else {
+                $videoID = (isset($query['v'])) ? $query['v'] : $videoID;
+            }
+        }
+
+        if (strpos($videoID, ',') !== false) {
+            $this->set('playlist', $videoID);
+        }
+
+        $this->set('videoID', $videoID);
+        $this->set('playListID', $playListID);
     }
 
     public function save($data)
@@ -64,7 +86,23 @@ class Controller extends BlockController
         $args['videoURL'] = isset($data['videoURL']) ? trim($data['videoURL']) : '';
         $args['vHeight'] = isset($data['vHeight']) ? trim($data['vHeight']) : '';
         $args['vWidth'] = isset($data['vWidth']) ? trim($data['vWidth']) : '';
-        $args['vPlayer'] = ($data['vPlayer'] == 1) ? 1 : 0;
+
+        $args['rel'] = $data['rel'] ? 1 : 0;
+        $args['showinfo'] = $data['showinfo'] ? 1 : 0;
+        $args['autoplay'] = $data['autoplay'] ? 1 : 0;
+        $args['loopEnd'] = $data['loopEnd'] ? 1 : 0;
+        $args['modestbranding'] = $data['modestbranding'] ? 1 : 0;
+        $args['iv_load_policy'] = $data['iv_load_policy'] ? 1 : 3;
+        $args['color'] = $data['color'];
+        $args['sizing'] = $data['sizing'];
+        $args['iv_load_policy'] = $data['iv_load_policy'];
+        $args['controls'] = $data['controls'];
+
+        if ($data['sizing'] !='fixed') {
+            $args['vHeight'] = '';
+            $args['vWidth'] = '';
+        }
+
         parent::save($args);
     }
 }
--- web/concrete/blocks/youtube/form_setup_html.php
+++ web/concrete/blocks/youtube/form_setup_html.php
@@ -1,41 +1,170 @@
-<? defined('C5_EXECUTE') or die("Access Denied."); ?>
-<?
-if (!$bObj->vWidth) {
-	$bObj->vWidth=425;
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php
+if (!$vWidth) {
+    $vWidth=640;
 }
-if (!$bObj->vHeight) {
-	$bObj->vHeight=344;
+if (!$vHeight) {
+    $vHeight=385;
+}
+
+if (!$sizing) {
+    $sizing = '16:9';
 }
 ?>
-<style>
-    .ccm-ui .form-control.yt-vid-dims {
-        width: 100px;
-    }
-</style>
-<div class="form-group">
-    <label><?=t('Title')?></label>
-    <input type="text" class="form-control" name="title" value="<?=$bObj->title?>"/>
-</div>
-<div class="form-group">
-    <label><?=t('YouTube URL')?></label>
-    <input type="text" class="form-control" id="YouTubeVideoURL" name="videoURL" value="<?=$bObj->videoURL?>" />
-</div>
-<div class="form-group">
-    <label><?=t('Width')?></label>
-    <input type="text" class="form-control yt-vid-dims" id="YouTubeVideoWidth" name="vWidth" value="<?=$bObj->vWidth?>" />
-</div>
-<div class="form-group">
-    <label><?=t('Height')?></label>
-    <input type="text" class="form-control yt-vid-dims" id="YouTubeVideoHeight" name="vHeight" value="<?=$bObj->vHeight?>" />
-</div>
-<div class="form-group">
-    <label><?=t('Video Player')?></label>
-        <div class="radio">
-            <label><input type="radio" name="vPlayer" value="1" <?=($bObj->vPlayer)?'checked':''?> /> <?=t('iFrame - Works in more devices')?></label>
+
+<?php
+echo Core::make('helper/concrete/ui')->tabs(array(
+    array('video', t('Video'), true),
+    array('settings', t('Settings'))
+));
+?>
+
+<div class="ccm-tab-content" id="ccm-tab-content-video">
+    <fieldset>
+        <legend><?php echo t('YouTube URL'); ?></legend>
+        <div class="form-group">
+            <?php echo $form->text('videoURL', $videoURL, array('required'=>'required'));?>
         </div>
-    <div class="radio">
-        <label>
-            <input type="radio" name="vPlayer" value="0" <?=(!$bObj->vPlayer)?'checked':''?> /> <?=t('Flash Embed - Legacy method')?>
-        </label>
-    </div>
+    </fieldset>
+
+    <fieldset>
+        <legend><?php echo t('Size'); ?></legend>
+        <div class="radio">
+            <label>
+                <?php echo $form->radio('sizing', '16:9', $sizing); ?>
+                <?php echo t("16:9 (widescreen)")?>
+            </label>
+        </div>
+        <div class="radio">
+            <label>
+                <?php echo $form->radio('sizing', '4:3', $sizing); ?>
+                <?php echo t("4:3")?>
+            </label>
+        </div>
+        <div class="radio">
+            <label>
+                <?php echo $form->radio('sizing', 'fixed', $sizing); ?>
+                <?php echo t("Fixed Size (non-responsive)")?>
+            </label>
+        </div>
+        <div id="fixedsizes" class="row <?php echo ($sizing == 'fixed' ? '' : 'hidden');?>">
+            <div class="form-group col-xs-6">
+                <label><?php echo t('Width')?></label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="YouTubeVideoWidth" name="vWidth" value="<?php echo $vWidth?>" />
+                    <div class="input-group-addon"><?php echo t('px');?></div>
+                </div>
+            </div>
+            <div class="form-group col-xs-6">
+                <label><?php echo t('Height')?></label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="YouTubeVideoHeight" name="vHeight" value="<?php echo $vHeight?>" />
+                    <div class="input-group-addon"><?php echo t('px');?></div>
+                </div>
+            </div>
+        </div>
+    </fieldset>
 </div>
+<div class="ccm-tab-content" id="ccm-tab-content-settings">
+    <fieldset>
+        <legend><?php echo t('Appearance'); ?></legend>
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="form-group">
+                    <label>
+                        <?php echo $form->checkbox('showinfo', 1, (isset($showinfo) ? $showinfo : true)); ?>
+                        <?php echo t("Show video information")?>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <?php  echo $form->label('color', t('Color Scheme'))?>
+                    <?php  echo $form->select('color', array('red'=>t('Red'), 'white'=>t('White')), $color)?>
+                </div>
+            </div>
+
+            <div class="col-xs-6">
+                <div class="form-group">
+                    <label>
+                        <?php echo $form->checkbox('controls', 1, (isset($controls) ? $controls : true)); ?>
+                        <?php echo t("Show controls")?>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <?php
+                        $disabledattr = array();
+                        if ($color == 'white') {
+                            $disabledattr['disabled'] = 'disabled';
+                        }
+                        echo $form->checkbox('modestbranding', 1,  (isset($modestbranding) ? $modestbranding : true),$disabledattr); ?>
+                        <?php echo t("Hide YouTube Logo")?>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend><?php echo t('Playback Options'); ?></legend>
+        <div class="row">
+            <div class="col-xs-6">
+                    <div class="form-group">
+                        <label>
+                            <?php echo $form->checkbox('autoplay', 1, $autoplay); ?>
+                            <?php echo t("Automatically play")?>
+                        </label>
+                    </div>
+             </div>
+            <div class="col-xs-6">
+                <div class="form-group">
+                    <label>
+                        <?php echo $form->checkbox('loopEnd', 1, $loopEnd); ?>
+                        <?php echo t("Loop video")?>
+                    </label>
+                </div>
+            </div>
+
+            <div class="form-group col-xs-12">
+                <div class="form-group">
+                    <label>
+                        <?php echo $form->checkbox('rel', 1, $rel); ?>
+                        <?php echo t("Show related videos when playback ends")?>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <?php echo $form->checkbox('iv_load_policy', 1, $iv_load_policy); ?>
+                        <?php echo t("Show annotations by default")?>
+                    </label>
+                </div>
+             </div>
+        </div>
+    </fieldset>
+</div>
+
+
+<script>
+    $(document).ready(function() {
+        $('#sizing1, #sizing2, #sizing3').change(function(){
+            if ($('#sizing3').is(':checked')) {
+                $('#fixedsizes').removeClass('hidden');
+            } else {
+                $('#fixedsizes').addClass('hidden');
+            }
+        });
+
+        $('#color').change(function(){
+            if ($(this).val() == 'white') {
+                $('#modestbranding').prop('disabled','disabled').prop('checked',false);
+            } else {
+                $('#modestbranding').removeProp('disabled');
+            }
+        });
+
+        $('#ccm-form-submit-button').click(function(){
+            if (!$('#videoURL').val()) {
+                $('[data-tab="video"').click();
+            }
+        });
+    });
+</script>
