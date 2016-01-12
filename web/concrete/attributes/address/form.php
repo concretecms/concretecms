87d2690724fe63d8efd7b1fee4371634639e6046
Andrew Embler <andrew@concrete5.org>
2016-01-12 09:50:04 -0800

    composer address
    Former-commit-id: 951d39aa5d4e30ba27283428393f7f2d80154792
    Former-commit-id: e35053a420c9119cdadb4c4121cad3eb341d5015

--- web/concrete/attributes/address/form.php
+++ web/concrete/attributes/address/form.php
@@ -2,22 +2,25 @@
 <?php $f = Loader::helper('form'); ?>
 <?php $co = Loader::helper('lists/countries'); ?>
 
-<fieldset class="ccm-attribute ccm-attribute-address ccm-attribute-address-<?=$key->getAttributeKeyID()?>">
-<div class="ccm-attribute-address-line form-group">
-    <?=$f->label($this->field('address1'), t('Address 1'))?>
-    <?=$f->text($this->field('address1'), $address1)?>
-</div>
-<div class="ccm-attribute-address-line form-group">
-    <?=$f->label($this->field('address2'), t('Address 2'))?>
-    <?=$f->text($this->field('address2'), $address2)?>
-</div>
-<div class="ccm-attribute-address-line form-group">
-    <?=$f->label($this->field('city'), t('City'))?>
-    <?=$f->text($this->field('city'), $city)?>
-</div>
+<div class="ccm-attribute-address-composer-wrapper ccm-attribute-address-<?=$key->getAttributeKeyID()?>">
 
-<div class="ccm-attribute-address-line ccm-attribute-address-state-province form-group">
-    <?=$f->label($this->field('state_province'), t('State/Province'))?>
+    <div class="form-group">
+        <?=$f->label($this->field('address1'), t('Address 1'))?>
+        <?=$f->text($this->field('address1'), $address1)?>
+    </div>
+
+    <div class="form-group">
+        <?=$f->label($this->field('address2'), t('Address 2'))?>
+        <?=$f->text($this->field('address2'), $address2)?>
+    </div>
+
+    <div class="form-group">
+        <?=$f->label($this->field('city'), t('City'))?>
+        <?=$f->text($this->field('city'), $city)?>
+    </div>
+
+    <div class="form-group ccm-attribute-address-state-province">
+        <?=$f->label($this->field('state_province'), t('State/Province'))?>
     <?php
     $spreq = $f->getRequestValue($this->field('state_province'));
     if ($spreq != false) {
@@ -27,42 +30,42 @@
     if ($creq != false) {
         $country = $creq;
     }
-
     ?>
-    <?=$f->select($this->field('state_province_select'), array('' => t('Choose State/Province')), $state_province, array('classes' => 'form-control', 'ccm-attribute-address-field-name' => $this->field('state_province')))?>
-    <?=$f->text($this->field('state_province_text'), $state_province, array('style' => 'display: none', 'ccm-attribute-address-field-name' => $this->field('state_province')))?>
-</div>
-<?php
+        <?=$f->select($this->field('state_province_select'), array('' => t('Choose State/Province')), $state_province, array('classes' => 'form-control', 'ccm-attribute-address-field-name' => $this->field('state_province')))?>
+        <?=$f->text($this->field('state_province_text'), $state_province, array('style' => 'display: none', 'ccm-attribute-address-field-name' => $this->field('state_province')))?>
+    </div>
 
-if (!$country && !$search) {
-    if ($akDefaultCountry != '') {
-        $country = $akDefaultCountry;
-    } else {
-        $country = 'US';
+    <?php
+
+    if (!$country && !$search) {
+        if ($akDefaultCountry != '') {
+            $country = $akDefaultCountry;
+        } else {
+            $country = 'US';
+        }
     }
-}
 
-$countriesTmp = $co->getCountries();
-$countries = array();
-foreach ($countriesTmp as $_key => $_value) {
-    if ((!$akHasCustomCountries) || ($akHasCustomCountries && in_array($_key, $akCustomCountries))) {
-        $countries[$_key] = $_value;
+    $countriesTmp = $co->getCountries();
+    $countries = array();
+    foreach ($countriesTmp as $_key => $_value) {
+        if ((!$akHasCustomCountries) || ($akHasCustomCountries && in_array($_key, $akCustomCountries))) {
+            $countries[$_key] = $_value;
+        }
     }
-}
-$countries = array_merge(array('' => t('Choose Country')), $countries);
-?>
+    $countries = array_merge(array('' => t('Choose Country')), $countries);
+    ?>
 
-<div class="ccm-attribute-address-line ccm-attribute-address-country form-group">
-    <?=$f->label($this->field('country'), t('Country'))?>
-    <?=$f->select($this->field('country'), $countries, $country, array('classes' => 'form-control')); ?>
+    <div class="form-group ccm-attribute-address-country">
+        <?=$f->label($this->field('country'), t('Country'))?>
+        <?=$f->select($this->field('country'), $countries, $country); ?>
+    </div>
+
+    <div class="form-group">
+        <?=$f->label($this->field('postal_code'), t('Postal Code'))?>
+        <?=$f->text($this->field('postal_code'), $postal_code)?>
+    </div>
+
 </div>
-
-<div class="ccm-attribute-address-line form-group">
-    <?=$f->label($this->field('postal_code'), t('Postal Code'))?>
-    <?=$f->text($this->field('postal_code'), $postal_code)?>
-</div>
-
-</fieldset>
 
 <script type="text/javascript">
 //<![CDATA[
