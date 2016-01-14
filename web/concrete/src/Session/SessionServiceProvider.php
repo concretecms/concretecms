136fd0b2ce0af7494fc920953f5edcde2b836e35
Korvin Szanto <korvinszanto@gmail.com>
2016-01-14 12:23:42 -0800

    Pass the application object to the session static class rather than using the facade
    Former-commit-id: 10db162df822d6195b05b37c9e410567a2954a0c
    Former-commit-id: 165d30b7acf4a8b22bb2e4a5662ee6352d8443c0

--- web/concrete/src/Session/SessionServiceProvider.php
+++ web/concrete/src/Session/SessionServiceProvider.php
@@ -7,6 +7,9 @@ class SessionServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // Supply the deprecated static session accessor with a real application object
+        Session::setApplicationObject($this->app);
+
         $this->app->bind('Concrete\Core\Session\SessionValidatorInterface', 'Concrete\Core\Session\SessionValidator');
         $this->app->bind('Concrete\Core\Session\SessionFactoryInterface', 'Concrete\Core\Session\SessionFactory');
 
