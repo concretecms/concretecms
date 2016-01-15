e6e481fa178a72887edd184ecd8b0d5ad5a987c2
Andrew Embler <andrew@concrete5.org>
2016-01-15 10:51:42 -0800

    Fix #3274

--- web/concrete/src/Permission/Key/AddFileFileSetKey.php
+++ web/concrete/src/Permission/Key/AddFileFileSetKey.php
@@ -1,54 +1,65 @@
 <?php
 namespace Concrete\Core\Permission\Key;
+
+use Concrete\Core\File\File;
 use Loader;
 use User;
 use \Concrete\Core\Permission\Duration as PermissionDuration;
-class AddFileFileSetKey extends FileSetKey  {
 
-	public function getAllowedFileExtensions() {
-		$u = new User();
+class AddFileFileSetKey extends FileSetKey
+{
 
-		$extensions = array();
-		if ($u->isSuperUser()) {
-			$extensions = Loader::helper('concrete/file')->getAllowedFileExtensions();
-			return $extensions;
-		}
+    public function getAllowedFileExtensions()
+    {
+        $u = new User();
 
-		$pae = $this->getPermissionAccessObject();
-		if (!is_object($pae)) {
-			return array();
-		}
-	
-		$accessEntities = $u->getUserAccessEntityObjects();
-		$accessEntities = $pae->validateAndFilterAccessEntities($accessEntities);
-		$list = $this->getAccessListItems(FileSetKey::ACCESS_TYPE_ALL, $accessEntities);
-		$list = PermissionDuration::filterByActive($list);
+        $extensions = array();
+        if ($u->isSuperUser()) {
+            $extensions = Loader::helper('concrete/file')->getAllowedFileExtensions();
+            return $extensions;
+        }
 
-		foreach($list as $l) {
-			if ($l->getFileTypesAllowedPermission() == 'N') {
-				$extensions = array();
-			}
-			if ($l->getFileTypesAllowedPermission() == 'C') {
-				$extensions = array_unique(array_merge($extensions, $l->getFileTypesAllowedArray()));
-			}
-			if ($l->getFileTypesAllowedPermission() == 'A') {
-				$extensions = Loader::helper('concrete/file')->getAllowedFileExtensions();
-			}
-		}
+        $pae = $this->getPermissionAccessObject();
+        if (!is_object($pae)) {
+            return array();
+        }
 
-		$extensions = array_map('strtolower', $extensions);
-		return $extensions;
-	}
-	
-	public function validate($extension = false) {
-		$extensions = $this->getAllowedFileExtensions();
-		$extension = strtolower($extension);
-		if ($extension != false) {
-			return in_array($extension, $extensions);
-		} else {
-			return count($extensions) > 0;
-		}
-	}
-	
+        $accessEntities = $u->getUserAccessEntityObjects();
+        $accessEntities = $pae->validateAndFilterAccessEntities($accessEntities);
+        $list = $this->getAccessListItems(FileSetKey::ACCESS_TYPE_ALL, $accessEntities);
+        $list = PermissionDuration::filterByActive($list);
+
+        foreach ($list as $l) {
+            if ($l->getFileTypesAllowedPermission() == 'N') {
+                $extensions = array();
+            }
+            if ($l->getFileTypesAllowedPermission() == 'C') {
+                $extensions = array_unique(array_merge($extensions, $l->getFileTypesAllowedArray()));
+            }
+            if ($l->getFileTypesAllowedPermission() == 'A') {
+                $extensions = Loader::helper('concrete/file')->getAllowedFileExtensions();
+            }
+        }
+
+        $extensions = array_map('strtolower', $extensions);
+        return $extensions;
+    }
+
+    public function validate($file = false)
+    {
+        $extensions = $this->getAllowedFileExtensions();
+        if ($file instanceof File) {
+            $extension = $file->getExtension();
+        } else {
+            $extension = $file;
+        }
+        $extension = strtolower($extension);
+        if ($extension != false) {
+            return in_array($extension, $extensions);
+        } else {
+            return count($extensions) > 0;
+        }
+    }
+
 
 }
