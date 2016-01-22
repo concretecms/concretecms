6a7968e6144eac8e6a8beb24126b470de60fffc0
Andrew Embler <andrew@concrete5.org>
2016-01-22 13:22:42 -0800

    Fix http://www.concrete5.org/developers/bugs/5-7-5-4/layout-column-widths-are-no-longer-editable-after-being-saved-th/

--- web/concrete/blocks/core_area_layout/edit_grid.php
+++ web/concrete/blocks/core_area_layout/edit_grid.php
@@ -5,6 +5,7 @@
 ?>
 
 <input type="hidden" name="arLayoutID" value="<?=$controller->arLayout->getAreaLayoutID()?>" />
+<input type="hidden" name="arLayoutEdit" value="1" />
 
 <div id="ccm-layouts-edit-mode" class="ccm-layouts-edit-mode-edit">
 
