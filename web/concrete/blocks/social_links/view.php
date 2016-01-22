226e3cbe6c2eaf386a622bc14d8c7ed376e5a107
MrKarlDilkington <mrkarldilkington@outlook.com>
2016-01-21 20:18:59 -0500

    open links in new tab

--- web/concrete/blocks/social_links/view.php
+++ web/concrete/blocks/social_links/view.php
@@ -1,13 +1,11 @@
-<?php
-defined('C5_EXECUTE') or die("Access Denied.");
-?>
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-<div id="ccm-block-social-links<?=$bID?>" class="ccm-block-social-links">
+<div id="ccm-block-social-links<?php echo $bID?>" class="ccm-block-social-links">
     <ul class="list-inline">
-    <? foreach($links as $link) {
+    <?php foreach($links as $link) {
         $service = $link->getServiceObject();
         ?>
-        <li><a href="<?= h($link->getURL()) ?>"><?=$service->getServiceIconHTML()?></a></li>
-    <? } ?>
+        <li><a target="_blank" href="<?php echo h($link->getURL()); ?>"><?php echo $service->getServiceIconHTML(); ?></a></li>
+    <?php } ?>
     </ul>
 </div>
