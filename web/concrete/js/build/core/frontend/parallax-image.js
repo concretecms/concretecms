eb2e205f23349f91c621da5d61c25724f0e93c66
Korvin Szanto <korvinszanto@gmail.com>
2016-01-26 09:34:57 -0800

    Revitalize parallax js

--- web/concrete/js/build/core/frontend/parallax-image.js
+++ web/concrete/js/build/core/frontend/parallax-image.js
@@ -1,182 +1,252 @@
-$.fn.parallaxize = (function (global, $) {
-    'use strict';
+(function (window, $) {
 
-    function Parallax() {
-        this.init.apply(this, Array.prototype.slice.call(arguments));
-    }
+    /**
+     * Parallax class
+     *
+     *     var parallax = new Parallax(container_div, { speed: 0.25 });
+     *     parallax.render();
+     *
+     * Settings takes:
+     * * speed: float between 0 and 1, 0 has no effect, 1 appears stationary. Sweet spot is between 0.2 and 0.7
+     * * window: The nearest scrolling parent element to calculate the parallax against, defaults to the window
+     *
+     * @param HTMLElement element The container
+     * @param object settings The settings
+     * @constructor
+     */
+    var Parallax = function Parallax(element, settings) {
+        this.$element = $(element);
+        this.$image = null;
+        this.image = null;
+
+        // Apply the default settings
+        this.settings = $.fn.extend({
+            variation: 0,
+            speed: 0.25,
+            window: window
+        }, settings);
+
+        /**
+         *
+         */
+        if (this.settings.variation) {
+            // Always override speed if the variation is set.
+            this.settings.speed = this.settings.variation / this.$element.height() / 5
+        }
+
+        this.$window = $(this.settings.window);
+    };
 
     Parallax.prototype = {
-        init: function (parent, settings) {
-            this.initializeConfig(settings);
-            this.config('element', parent);
-            this.config('width', 1);
-            this.config('height', 1);
 
-            this.initializeDOM();
-            this.initializeBindings();
-        },
+        /**
+         * Initialize the parallax, this method must be called for the parallax to begin loading.
+         */
+        init: function () {
 
-        initializeConfig: function (settings) {
-            var config = {};
-            settings = $.fn.extend({variation: 20}, settings);
+            var my = this,
+                outer = $('<div/>').addClass('parallax-image-container'),
+                image = new Image();
 
-            var getGetter = function (object) {
-                return function (key, value) {
-                    if (typeof key === 'undefined') {
-                        return object;
-                    }
-                    if (typeof value === 'undefined') {
-                        return object[key];
-                    }
-                    return object[key] = value;
-                }
-            };
-
-            this.config = getGetter(config);
-            this.setting = getGetter(settings);
-        },
-
-        initializeDOM: function () {
-            var elem = this.config('element'),
-                image_container = $('<div/>'),
-                image = new Image(),
-                image_elem = $(image).appendTo(image_container),
-                me = this;
-            elem.prepend(image_container);
-            image_elem.hide();
-
-            var synchronous = true;
             image.onload = function () {
-                me.config('height', image_elem.height());
-                me.config('width', image_elem.width());
-                me.translate(false);
+                my.image = image;
+                my.$image = $(image).addClass('parallaxic-image');
 
-                if (synchronous) {
-                    image_elem.show();
-                } else {
-                    image_elem.fadeIn(250);
-                }
+                my.handleResize();
+                outer.append(my.$image);
+
+                my.bindListeners();
             };
-            image.src = elem.data('background-image');
-            setTimeout(function () {
-                synchronous = false;
-            }, 150);
-            this.config('image', image_elem);
 
-            elem.addClass('parallaxic-container');
-            image_elem.addClass('parallaxic-image');
-            image_container.addClass('parallax-image-container');
+            image.src = this.$element.data('background-image');
 
+            this.$element.addClass('parallaxic-container').prepend(outer);
         },
 
-        initializeBindings: function () {
-            var me = this;
-            $(global).scroll(function () {
-                me.translate();
-            });
-
-            $(global).resize(function () {
-                me.translate(false);
+        /**
+         * Bind event listeners
+         */
+        bindListeners: function () {
+            var obj = this;
+            this.$window.on('resize', function (e) {
+                obj.handleResize();
+            }).on('scroll', function (e) {
+                obj.handleScroll();
             });
         },
 
-        translate: function (cache) {
-            var transform = [
-                this.resizeTranslation(cache),
-                this.positionTranslation()
-            ].join(' ');
+        /**
+         * Handle browser resize
+         */
+        handleResize: function () {
+            var frame = this.getFrame();
+            frame.determineScale(this);
+            frame.determineOffsetLeft(this);
 
-            this.config('image').css({
-                transform: transform,
-                '-webkit-transform': transform,
-                '-moz-transform': transform,
-                '-o-transform': transform
-            });
+            this.render();
         },
 
-        resizeTranslation: function (cached) {
-            var scale;
-            if (cached !== false) {
-                scale = this.config('scale');
-                if (scale) {
-                    return 'scale(' + scale + ')';
-                }
+        /**
+         * Handle the browser scroll
+         */
+        handleScroll: function () {
+            var window_top = this.$window.scrollTop(),
+                window_height = this.$window.height(),
+                container_top = this.$element.offset().top,
+                container_height = this.$element.height();
+
+            // Only render if this container is within the viewable window
+            if (window_top + window_height > container_top &&
+                window_top < container_top + container_height) {
+                this.render();
+            }
+        },
+
+        /**
+         * Render the parallax
+         */
+        render: function () {
+            var frame = this.getFrame();
+
+            frame.determineOffsetTop(this);
+            frame.determineParallax(this);
+            frame.render(this);
+        },
+
+        /**
+         * Get the ParallaxFrame object
+         * @returns ParallaxFrame
+         */
+        getFrame: function () {
+            if (!this.frame) {
+                this.frame = new ParallaxFrame();
             }
 
-            var variation = this.setting('variation'),
-                elem = this.config('element'),
-                image = this.config('image'),
-                height = elem.height(),
-                image_height = this.config('height'), image_width = this.config('width'),
-                potential_height = height + variation * 2,
-                new_width = elem.width(), new_height;
-
-            new_height = (image_height / image_width) * new_width;
-
-            if (new_height < potential_height) {
-                new_height = potential_height;
-                new_width = (image_width / image_height) * new_height;
-            }
-
-            scale = Math.ceil(new_height / image_height * 1000) / 1000;
-
-            //if (Modernizr && !Modernizr.csstransforms) {
-            //    image.css({
-            //        width: new_width,
-            //        height: new_height
-            //    });
-            //    return '';
-            //}
-
-            this.config('scale', scale);
-            return 'scale(' + scale + ')';
-        },
-
-        positionTranslation: function () {
-
-            var variation = this.setting('variation'),
-                scale = this.config('scale'),
-                elem = this.config('element'),
-                image = this.config('image'),
-                height = elem.height(), width = elem.width(),
-                image_height = this.config('height'), image_width = this.config('width'),
-                top = (height - image_height) / 2,
-                left = (width - image_width) / 2,
-                scroll_percentage = this.getScrollPercentage();
-
-            top += (variation * 2 * scroll_percentage) - variation;
-
-            // First assume translate
-            //var method_start = 'translate(', method_end = ')';
-            //
-            //if (Modernizr) {
-            //    if (Modernizr.csstransforms3d) {
-            //        method_start = 'translate3d(';
-            //        method_end = ', 0px)';
-            //    } else if (!Modernizr.csstransforms) {
-            //        return '';
-            //    }
-            //}
-            //return method_start + Math.round(left) + 'px, ' + Math.round(top) + 'px' + method_end;
-
-            return ['translate3d(', Math.round(left / scale), 'px, ', Math.round(top), 'px, ', '0px)'].join('');
-        },
-
-        getScrollPercentage: function () {
-            var elem = this.config('element'),
-                image = this.config('image'),
-                main = $(global),
-                min = elem.offset().top - main.height(),
-                max = elem.offset().top + elem.height() - min,
-                scroll = main.scrollTop() - min;
-
-            return Math.max(0, Math.min(1, scroll / max));
+            return this.frame;
         }
 
     };
 
-    return function (settings) {
-        this.data('parallax', new Parallax(this, settings))
+    /**
+     * The class used for managing parallax state change
+     * @constructor
+     */
+    var ParallaxFrame = function ParallaxFrame() {
+        this.scale = 1;
+        this.offset = {x: 0, y: 0};
     };
 
-}(window, $));
+    ParallaxFrame.prototype = {
+
+        /**
+         * Determine the images scale to properly satisfy the requirements
+         * @param parallax
+         */
+        determineScale: function (parallax) {
+            var image_height = parallax.image.height,
+                image_width = parallax.image.width,
+                container_height = parallax.$element.height(),
+                container_width = parallax.$element.width(),
+                speed = parallax.settings.speed,
+                required_padding = speed * image_height,
+                padded_container_height = container_height + required_padding,
+                ratio;
+
+            // If we aren't wide enough, or we are too tall
+            if (image_width < container_width || image_height > padded_container_height) {
+                // Get the ratio of the container width to the base image width
+                ratio = container_width / image_width;
+
+                // If we scale by the ratio, will we be tall enough?
+                if (image_height * ratio >= padded_container_height) {
+                    this.scale = ratio;
+                } else {
+                    // Since we weren't tall enough, scale by the height ratio
+                    this.scale = padded_container_height / image_height / 2;
+                }
+            } else {
+                // We're too wide and not tall enough, lets just scale by the height ratio
+                this.scale = padded_container_height / image_height;
+            }
+
+            this.scale = Math.ceil(this.scale * 1000) / 1000;
+        },
+
+        /**
+         * Figure out how far the image needs to be from the left
+         * We need to do this because the image scales away from the center, not the top left
+         * @param parallax
+         */
+        determineOffsetLeft: function (parallax) {
+            var image_width = parallax.image.width,
+                scaled_width = image_width * this.scale,
+                width_difference = scaled_width - image_width;
+
+            this.offset.x = width_difference / 2;
+        },
+
+        /**
+         * Figure out how far the image needs to be from the top
+         * We need to do this because the image scales away from the center, not the top left
+         * @param parallax
+         */
+        determineOffsetTop: function (parallax) {
+            var image_height = parallax.image.height,
+                scaled_height = image_height * this.scale,
+                height_difference = scaled_height - image_height;
+
+            this.offset.y = height_difference / 2;
+        },
+
+        /**
+         * Modify the offset to simulate parallax effect
+         * @param parallax
+         */
+        determineParallax: function (parallax) {
+            var speed = parallax.settings.speed,
+                $container = parallax.$element,
+                container_offset = $container.offset(),
+                window_scroll = parallax.$window.scrollTop(),
+                distance_to_top;
+
+            distance_to_top = window_scroll - container_offset.top;
+            this.offset.y += distance_to_top * speed;
+        },
+
+        /**
+         * Set the actual CSS to change the positioning of this image
+         * @param parallax
+         */
+        render: function (parallax) {
+            var transformation = "";
+
+            // Apply translation
+            transformation += "translate3d(" + this.offset.x + "px," + this.offset.y + "px, 0) ";
+
+            // Apply scale
+            transformation += "scale3d(" + this.scale + "," + this.scale + "," + this.scale + ")";
+
+            parallax.$image.get(0).style.transform = transformation;
+        }
+    };
+
+    /**
+     * Jquery parallaxize extension, easily create or retrieve a parallax instance with jQuery.
+     *
+     * @param object settings
+     * @return Parallax
+     */
+    $.fn.parallaxize = function Parallaxize(settings) {
+        var me = $(this), parallax;
+        if (!me.data('parallax')) {
+            parallax = new Parallax(this, settings);
+            $(this).data('parallax', parallax);
+            parallax.init();
+        } else {
+            parallax = me.data('parallax');
+        }
+
+        return parallax;
+    };
+
+}(this, jQuery));
