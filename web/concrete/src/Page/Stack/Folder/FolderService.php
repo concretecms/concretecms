ffbf15ebb5f3a136cf7fab7c39a8e04347e48206
Michele Locati <michele@locati.it>
2016-01-28 13:36:54 +0100

    Bugfix
    Former-commit-id: af33884770f7a77ab8639e74feadecf5a288fca1
    Former-commit-id: 368fcf230158db05d774b43e81b11781cb06249d

--- web/concrete/src/Page/Stack/Folder/FolderService.php
+++ web/concrete/src/Page/Stack/Folder/FolderService.php
@@ -36,7 +36,7 @@ class FolderService
     public function add($name, Folder $folder = null)
     {
         $type = Type::getByHandle(STACK_CATEGORY_PAGE_TYPE);
-        $parent = $folder ? $folder->getPageObject() : \Page::getByPath(STACKS_PAGE_PATH);
+        $parent = $folder ? $folder->getPage() : \Page::getByPath(STACKS_PAGE_PATH);
         $data = array();
         $data['name'] = $name;
         $page = $parent->add($type, $data);
