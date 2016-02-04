1044650c3739ecd07f71f7ad238e93334f6712af
Michele Locati <michele@locati.it>
2016-02-04 09:36:30 +0100

    Avoid deprecated stuff
    Former-commit-id: bbbe46eb7e8a6dc6cedaa0a2cd676a6c759a2211
    Former-commit-id: 358fead001d7ac1e3d4a2cfe5dab32fa7f522d71

--- web/concrete/controllers/single_page/dashboard/system/seo/bulk.php
+++ web/concrete/controllers/single_page/dashboard/system/seo/bulk.php
@@ -2,11 +2,10 @@
 namespace Concrete\Controller\SinglePage\Dashboard\System\Seo;
 
 use Concrete\Core\Page\Controller\DashboardPageController;
-use Config;
-use Loader;
 use Page;
 use Concrete\Core\Page\PageList;
 use Concrete\Core\Multilingual\Page\Section\Section;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class Bulk extends DashboardPageController
 {
@@ -71,7 +70,7 @@ class Bulk extends DashboardPageController
 
     public function view()
     {
-        $html = Loader::helper('html');
+        $html = $this->app->make('helper/html');
         $this->requireAsset('javascript', 'jquery/textcounter');
         $pageList = $this->getRequestedSearchResults();
         if (is_object($pageList)) {
@@ -89,12 +88,16 @@ class Bulk extends DashboardPageController
 
     public function saveRecord()
     {
-        $text = Loader::helper('text');
+        $text = $this->app->make('helper/text');
         $success = t('success');
         $cID = $this->post('cID');
         $c = Page::getByID($cID);
+        if (!$c || $c->isError()) {
+            throw new \RuntimeException(t('Unable to find the specified page'));
+        }
+        $titleFormat = $this->app->make('config')->get('concrete.seo.title_format');
         $siteName = $this->getSiteNameForPage($c);
-        if (trim(sprintf(Config::get('concrete.seo.title_format'), $siteName, $c->getCollectionName())) != trim($this->post('meta_title')) && $this->post('meta_title')) {
+        if (trim(sprintf($titleFormat, $siteName, $c->getCollectionName())) != trim($this->post('meta_title')) && $this->post('meta_title')) {
             $c->setAttribute('meta_title', trim($this->post('meta_title')));
         }
 
@@ -107,7 +110,8 @@ class Bulk extends DashboardPageController
         $newPath = Page::getCollectionPathFromID($cID);
         $newHandle = $text->urlify($cHandle);
         $result = array('success' => $success, 'cID' => $cID, 'cHandle' => $newHandle, 'newPath' => $newHandle, 'newLink' => $newPath);
-        echo Loader::helper('json')->encode($result);
+
+        JsonResponse::create($result)->send();
         exit;
     }
 
@@ -116,57 +120,62 @@ class Bulk extends DashboardPageController
      */
     public function getRequestedSearchResults()
     {
-        $dh = Loader::helper('concrete/dashboard/sitemap');
+        $dh = $this->app->make('helper/concrete/dashboard/sitemap');
         if (!$dh->canRead()) {
             return false;
         }
 
         $pageList = new PageList();
 
-        if ($_REQUEST['submit_search']) {
+        if ($this->request('submit_search')) {
             $pageList->resetSearchRequest();
         }
 
-        $req = $_REQUEST;
         $pageList->displayUnapprovedPages();
 
         $pageList->sortBy('cDateModified', 'desc');
 
-        $cvName = htmlentities($req['cvName'], ENT_QUOTES, APP_CHARSET);
-
-        if ($cvName != '') {
+        $cvName = $this->request('cvName');
+        if ($cvName) {
             $pageList->filterByName($cvName);
         }
 
-        if ($req['cParentIDSearchField'] > 0) {
-            if ($req['cParentAll'] == 1) {
-                $pc = Page::getByID($req['cParentIDSearchField']);
-                $cPath = $pc->getCollectionPath();
-                $pageList->filterByPath($cPath);
+        $cParentIDSearchField = $this->request('cParentIDSearchField');
+        if ($cParentIDSearchField > 0) {
+            if ($this->request('cParentAll') == 1) {
+                $pc = Page::getByID($cParentIDSearchField);
+                if ($pc && !$pc->isError()) {
+                    $cPath = $pc->getCollectionPath();
+                    $pageList->filterByPath($cPath);
+                }
             } else {
-                $pageList->filterByParentID($req['cParentIDSearchField']);
+                $pageList->filterByParentID($cParentIDSearchField);
             }
             $parentDialogOpen = 1;
         }
 
-        $keywords = htmlentities($req['keywords'], ENT_QUOTES, APP_CHARSET);
+        $keywords = $this->request('keywords');
         $pageList->filterByKeywords($keywords, true);
 
-        if ($req['numResults']) {
-            $pageList->setItemsPerPage($req['numResults']);
+        $numResults = $this->request('numResults');
+        if ($numResults) {
+            $pageList->setItemsPerPage($numResults);
         }
 
-        if ($req['ptID']) {
-            $pageList->filterByPageTypeID($req['ptID']);
+        $ptID = $this->request('ptID');
+        if ($ptID) {
+            $pageList->filterByPageTypeID($ptID);
         }
 
-        if ($_REQUEST['noDescription'] == 1) {
+        if ($this->request('noDescription') == 1) {
             $pageList->filter('CollectionSearchIndexAttributes.ak_meta_description', null, '=');
             $this->set('descCheck', true);
             $parentDialogOpen = 1;
+        } else {
+            $parentDialogOpen = null;
         }
 
-        $this->set('searchRequest', $req);
+        $this->set('searchRequest', $_REQUEST);
         $this->set('parentDialogOpen', $parentDialogOpen);
 
         return $pageList;
