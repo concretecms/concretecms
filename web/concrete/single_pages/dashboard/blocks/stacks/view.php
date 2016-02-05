788ba8a1849534c88fa29c966ba2f00929b35c90
Michele Locati <michele@locati.it>
2016-02-05 09:41:00 +0100

    Breadcrumb to the bottom of the page

--- web/concrete/single_pages/dashboard/blocks/stacks/view.php
+++ web/concrete/single_pages/dashboard/blocks/stacks/view.php
@@ -19,42 +19,8 @@ use Concrete\Core\Page\Stack\Stack;
 if ($controller->getTask() == 'view_details' && isset($neutralStack) && $neutralStack) {
     /* @var Stack $neutralStack */
     /* @var Stack|null $stackToEdit */
-    /* @var array $breadcrumb */
     /* @var bool $isGlobalArea */
     ?>
-    <div class="ccm-search-results-breadcrumb">
-        <ol class="breadcrumb">
-            <?php
-            foreach ($breadcrumb as $value) {
-                ?><li class="<?=$value['active'] ? 'active' : ''?>"><?php
-                if (isset($value['children'])) {
-                    ?><span class="dropdown">
-                        <button type="button" class="btn btn-default btn-xs" data-toggle="dropdown">
-                            <?=$value['name']?>
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <?php
-                            foreach ($value['children'] as $child) {
-                                ?><li><a href="<?=h($child['url'])?>"><?=$child['name']?></a></li><?php
-                            }
-                            ?>
-                        </ul>
-                    </span><?php
-                } else {
-                    if (!$value['active']) {
-                        ?><a href="<?=h($value['url'])?>"><?php
-                    }
-                    echo $value['name'];
-                    if (!$value['active']) {
-                        ?></a><?php
-                    }
-                }
-                ?></li><?php
-            }
-            ?>
-        </ol>
-    </div>
     <div class="ccm-dashboard-header-buttons">
         <?php if ($isGlobalArea) { ?>
             <a href="<?=URL::to('/dashboard/blocks/stacks', 'view_global_areas')?>" class="btn btn-default"><i class="fa fa-angle-double-left"></i> <?=t("Back to Global Areas")?></a>
@@ -290,27 +256,6 @@ $(function() {
     </form>
     <?php
 } else {
-    if (isset($breadcrumb)) {
-        ?>
-        <div class="ccm-search-results-breadcrumb">
-            <ol class="breadcrumb">
-                <?php
-                foreach ($breadcrumb as $value) {
-                    ?><li class="<?=$value['active'] ? 'ccm-undroppable-search-item active' : 'ccm-droppable-search-item'?>" data-collection-id="<?=$value['id']?>"><?php
-                    if (!$value['active']) {
-                        ?><a href="<?=$value['url']?>"><?php
-                    }
-                    echo $value['name'];
-                    if (!$value['active']) {
-                        ?></a><?php
-                    }
-                    ?></li><?php
-                }
-                ?>
-            </ol>
-        </div>
-        <?php
-    }
     if (!isset($showGlobalAreasFolder)) {
         $showGlobalAreasFolder = false;
     }
@@ -576,5 +521,42 @@ $(function() {
     ?>
 });
     </script>
+    <?php
+}
+if (isset($breadcrumb) && (!empty($breadcrumb))) {
+    ?>
+    <div class="ccm-search-results-breadcrumb">
+        <ol class="breadcrumb">
+            <?php
+            foreach ($breadcrumb as $value) {
+                ?><li class="<?=$value['active'] ? 'ccm-undroppable-search-item active' : 'ccm-droppable-search-item'?>" data-collection-id="<?=$value['id']?>"><?php
+                if (isset($value['children'])) {
+                    ?><span class="dropdown">
+                        <button type="button" class="btn btn-default btn-xs" data-toggle="dropdown">
+                            <?=$value['name']?>
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <?php
+                            foreach ($value['children'] as $child) {
+                                ?><li><a href="<?=h($child['url'])?>"><?=$child['name']?></a></li><?php
+                            }
+                            ?>
+                        </ul>
+                    </span><?php
+                } else {
+                    if (!$value['active']) {
+                        ?><a href="<?=h($value['url'])?>"><?php
+                    }
+                    echo $value['name'];
+                    if (!$value['active']) {
+                        ?></a><?php
+                    }
+                }
+                ?></li><?php
+            }
+            ?>
+        </ol>
+    </div>
     <?php
 }
