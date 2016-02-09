cdc90d6bf7a7b5e2d6b687a491050f1c5917473e
Andrew Embler <andrew@concrete5.org>
2016-02-09 06:28:47 -0800

    Removing files not necessary after data type scale-back

--- web/concrete/src/Express/ExpressServiceProvider.php
+++ web/concrete/src/Express/ExpressServiceProvider.php
@@ -14,17 +14,6 @@ class ExpressServiceProvider extends ServiceProvider
         $this->app->bindShared('express.builder.association', function () use ($app) {
             return $app->make('Concrete\Core\Express\ObjectAssociationBuilder');
         });
-        $this->app->bindShared('express.publisher', function () use ($app) {
-            return $app->make('Concrete\Core\Express\ObjectPublisher');
-        });
-
-        $this->app->bindShared('express.writer', function () use ($app) {
-            $writer = $app->make('Concrete\Core\Express\EntityWriter');
-            $writer->setNamespace($app['config']->get('express.entity_classes.namespace'));
-            $writer->setOutputPath($app['config']->get('express.entity_classes.output_path'));
-
-            return $writer;
-        });
         $this->app->bindShared('express', function () use ($app) {
             return $app->make('Concrete\Core\Express\ObjectManager');
         });
