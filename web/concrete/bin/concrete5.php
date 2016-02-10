6bff97af5a3e6b988327eeafb1897cd7faf0658f
Michele Locati <michele@locati.it>
2016-02-10 11:24:52 +0100

    Detect CLI environment from current working directory
    Former-commit-id: eb2e9012e989225bb5635dd26bf6db697f2bb848
    Former-commit-id: bff60da2cddf48516997e799038a146b9a226ec8

--- web/concrete/bin/concrete5.php
+++ web/concrete/bin/concrete5.php
@@ -1,36 +1,63 @@
 <?php
-$DIR_BASE_CORE = dirname(__DIR__);
 
-foreach (array('PHP_SELF', 'SCRIPT_NAME', 'SCRIPT_FILENAME', 'PATH_TRANSLATED') as $key) {
-    // Check if the key is valid
-    if (!isset($_SERVER[$key])) {
-        continue;
-    }
-    $value = $_SERVER[$key];
-    if (!is_file($value)) {
-        continue;
-    }
-    if (stripos(PHP_OS, 'WIN') === 0) {
-        // Just to simplify the regular expressions
-        $value = str_replace('\\', '/', $value);
-        // Check if the key is an absolute path
-        if (preg_match('%^([A-Z]:/|//\w)%i', $value) !== 1) {
-            continue;
+if (!isset($DIR_BASE_CORE)) {
+    // Try to detect the concrete directory starting from the current working directory
+    // (useful with CLI scripts started from a common launcher)
+    $dir = rtrim(str_replace(DIRECTORY_SEPARATOR, '/', getcwd()), '/');
+    while (@is_dir($dir)) {
+        if (is_file($dir.'/index.php') && is_file($dir.'/concrete/bootstrap/configure.php')) {
+            $DIR_BASE_CORE = $dir.'/concrete';
+            break;
         }
-    } else {
-        if (preg_match('%^/%i', $value) !== 1) {
-            continue;
+        $newDir = @dirname($dir);
+        if ($newDir === $dir) {
+            break;
         }
+        $dir = $newDir;
     }
-    if (preg_match('%/\.{1,2}/%', $value) !== 0) {
-        continue;
-    }
-    // Ok!
-    $DIR_BASE_CORE = dirname(dirname($value));
-    break;
+    unset($dir);
+    unset($newDir);
 }
-unset($key);
-unset($value);
+
+if (!isset($DIR_BASE_CORE)) {
+    // Try to detect the concrete directory starting from the filename of the currently executing script
+    // (useful with symlinked concrete directories)
+    foreach (array('PHP_SELF', 'SCRIPT_NAME', 'SCRIPT_FILENAME', 'PATH_TRANSLATED') as $key) {
+        // Check if the key is valid
+        if (!isset($_SERVER[$key])) {
+            continue;
+        }
+        $value = $_SERVER[$key];
+        if (!is_file($value)) {
+            continue;
+        }
+        if (stripos(PHP_OS, 'WIN') === 0) {
+            // Just to simplify the regular expressions
+            $value = str_replace('\\', '/', $value);
+            // Check if the key is an absolute path
+            if (preg_match('%^([A-Z]:/|//\w)%i', $value) !== 1) {
+                continue;
+            }
+        } else {
+            if (preg_match('%^/%i', $value) !== 1) {
+                continue;
+            }
+        }
+        if (preg_match('%/\.{1,2}/%', $value) !== 0) {
+            continue;
+        }
+        // Ok!
+        $DIR_BASE_CORE = dirname(dirname($value));
+        break;
+    }
+    unset($key);
+    unset($value);
+}
+
+if (!isset($DIR_BASE_CORE)) {
+    // Fall back to the real directory containing this script
+    $DIR_BASE_CORE = dirname(__DIR__);
+}
 
 define('DIR_BASE', dirname($DIR_BASE_CORE));
 
