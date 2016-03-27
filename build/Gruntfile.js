84381b76565cc336c759669ec256301e28ee39e7
Allyson Beckers <abeckers@spydertrap.com>
2016-03-26 22:20:44 -0500

    Attain proper namespace of Bootstrap by compiling it separately and importing it as a CSS file
    Former-commit-id: daa3e401b1b770ad27f9fd9f124440c5bc6a5350
    Former-commit-id: 7d8206644460f188f6132559d739a4beb3eb60d7

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -368,6 +368,7 @@ module.exports = function(grunt) {
 
     // List of the CSS files to be generated
     var css = {
+        '<%= DIR_BASE %>/concrete/css/build/vendor/bootstrap/bootstrap.css': '<%= DIR_BASE %>/concrete/css/build/vendor/bootstrap/bootstrap.less',
         '<%= DIR_BASE %>/concrete/css/app.css': '<%= DIR_BASE %>/concrete/css/build/core/app/app.less',
         '<%= DIR_BASE %>/concrete/css/editable-fields.css': '<%= DIR_BASE %>/concrete/css/build/core/editable-fields.less',
         '<%= DIR_BASE %>/concrete/css/select2.css': '<%= DIR_BASE %>/concrete/css/build/core/select2.less',
