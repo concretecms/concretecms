bc4481f8ae417c50c372982bfd01b33aa2859ce9
Andrew Embler <andrew@concrete5.org>
2016-04-26 10:50:29 -0700

    work on file folders

--- web/concrete/controllers/search/file_folder.php
+++ web/concrete/controllers/search/file_folder.php
@@ -7,25 +7,42 @@ use Concrete\Core\File\Filesystem;
 use Concrete\Core\File\FolderItemList;
 use Concrete\Core\File\Search\ColumnSet\FolderSet;
 use Concrete\Core\File\Search\Result\Result;
+use Concrete\Core\Search\StickyRequest;
+use Concrete\Core\Tree\Node\Node;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class FileFolder extends AbstractController
 {
     protected $list;
     protected $result;
+    protected $searchRequest;
     protected $filesystem;
 
     public function __construct()
     {
         parent::__construct();
         $this->filesystem = new Filesystem();
-        $folder = $this->filesystem->getRootFolder();
-        $this->list = new FolderItemList($folder);
+        $this->searchRequest = new StickyRequest('file_manager');
+        $this->list = new FolderItemList($this->searchRequest);
     }
 
     public function search()
     {
         $this->list->sortByNodeName();
+        $searchRequest = $this->searchRequest->getSearchRequest();
+        if (isset($searchRequest['folder'])) {
+            $node = Node::getByID($searchRequest['folder']);
+            if (is_object($node) && $node instanceof \Concrete\Core\Tree\Node\Type\FileFolder) {
+                $folder = $node;
+            }
+        }
+
+        if (!isset($folder)) {
+            $folder = $this->filesystem->getRootFolder();
+        }
+
+        $this->list->filterByParentFolder($folder);
+
         $columns = new FolderSet();
         $ilr = new Result($columns, $this->list, \URL::to('/ccm/system/file/folder/contents'));
         $this->result = $ilr;
--- web/concrete/js/build/core/file-manager/search.js
+++ web/concrete/js/build/core/file-manager/search.js
@@ -10,9 +10,34 @@
 
         ConcreteAjaxSearch.call(my, $element, options);
 
+        my.setupFolders();
     }
 
     ConcreteFileManager.prototype = Object.create(ConcreteAjaxSearch.prototype);
+
+    ConcreteFileManager.prototype.setupFolders = function() {
+        var my = this;
+        var $total = my.$element.find('tbody tr');
+        my.$element.find('tbody tr').on('dblclick', function() {
+            var index = $total.index($(this));
+            if (index > -1) {
+                var result = my.getResult().items[index];
+                if (result) {
+                    if (result.isFolder) {
+                        my.openFolder(result);
+                    }
+                }
+            }
+        });
+    }
+
+    ConcreteFileManager.prototype.openFolder = function(folderNode) {
+        var my = this;
+        var data = my.getSearchData();
+        data.push({'name': 'folder', 'value': folderNode.treeNodeID});
+        console.log(my.options.result);
+        my.ajaxUpdate(my.options.result.baseUrl, data);
+    }
 
     $.fn.concreteFileManager = function(options) {
         return $.each($(this), function(i, obj) {
