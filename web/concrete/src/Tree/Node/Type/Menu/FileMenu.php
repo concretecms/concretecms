9d7209f90023342247678e3343f6fda6e07daf19
Andrew Embler <andrew@concrete5.org>
2016-04-27 15:53:16 -0700

    bringing back the file manager menu
    Former-commit-id: c2edfac56aef181917a253ce0f72260d7f777e4a
    Former-commit-id: d03f4d43c62b390b632aa66b515a216a4d12860b

---
+++ web/concrete/src/Tree/Node/Type/Menu/FileMenu.php
@@ -0,0 +1,84 @@
+<?php
+namespace Concrete\Core\Tree\Node\Type\Menu;
+
+
+use Concrete\Core\Application\UserInterface\ContextMenu\Item\DialogLinkItem;
+use Concrete\Core\Application\UserInterface\ContextMenu\Item\DividerItem;
+use Concrete\Core\Application\UserInterface\ContextMenu\Item\LinkItem;
+use Concrete\Core\Application\UserInterface\ContextMenu\Menu;
+use Concrete\Core\Tree\Node\Type\File;
+
+class FileMenu extends Menu
+{
+
+    public function __construct(File $node)
+    {
+        parent::__construct();
+        /*$p = new \Permissions($group);
+        if ($p->canEditTreeNode()) {
+            $url = \URL::to('/dashboard/users/groups', 'edit', $group->getTreeNodeGroupID());
+            $this->addItem(new LinkItem($url, t('Edit Group')));
+        }
+        if ($p->canEditTreeNodePermissions()) {
+            $this->addItem(new EditPermissionsItem($group));
+        }
+        if ($p->canDeleteTreeNode()) {
+            $this->addItem(new DeleteItem($topic));
+        }
+        */
+
+        $file = $node->getTreeNodeFileObject();
+        $this->addItem(new LinkItem('#', t('Clear'), ['data-file-manager-action' => 'clear']));
+        $this->addItem(new DividerItem());
+
+        $this->addItem(new DialogLinkItem(
+                REL_DIR_FILES_TOOLS_REQUIRED . '/files/view?fID=' . $node->getTreeNodeFileID(),
+            t('View'), t('View'), '90%', '75%')
+        );
+        $this->addItem(new LinkItem('#', t('Download'), [
+            'data-file-manager-action' => 'download',
+            'data-file-id' => $node->getTreeNodeFileID()
+        ]));
+
+        if ($file->canEdit()) {
+            $this->addItem(new DialogLinkItem(
+                    REL_DIR_FILES_TOOLS_REQUIRED . '/files/edit?fID=' . $node->getTreeNodeFileID(),
+                    t('Edit'), t('Edit'), '90%', '75%')
+            );
+        }
+        $this->addItem(new DialogLinkItem(
+                \URL::to('/ccm/system/dialogs/file/thumbnails?fID=' . $node->getTreeNodeFileID()),
+                t('Thumbnails'), t('Thumbnails'), '90%', '75%')
+        );
+        $this->addItem(new DialogLinkItem(
+                \URL::to('/ccm/system/dialogs/file/properties?fID=' . $node->getTreeNodeFileID()),
+                t('Properties'), t('Properties'), '850', '450')
+        );
+        $this->addItem(new DialogLinkItem(
+                REL_DIR_FILES_TOOLS_REQUIRED . '/files/replace?fID=' . $node->getTreeNodeFileID(),
+                t('Replace'), t('Replace'), '500', '200')
+        );
+
+        $this->addItem(new LinkItem('#', t('Duplicate'), [
+            'data-file-manager-action' => 'duplicate',
+            'data-file-id' => $node->getTreeNodeFileID()
+        ]));
+
+        $this->addItem(new DividerItem());
+
+        $this->addItem(new DialogLinkItem(
+                REL_DIR_FILES_TOOLS_REQUIRED . '/files/permissions?fID=' . $node->getTreeNodeFileID(),
+                t('Permissions'), t('Permissions & Access'), '520', '450')
+        );
+
+        $this->addItem(new DialogLinkItem(
+                REL_DIR_FILES_TOOLS_REQUIRED . '/files/delete?fID=' . $node->getTreeNodeFileID(),
+                t('Delete'), t('Delete File'), '500', '350')
+        );
+
+
+
+
+    }
+
+}
