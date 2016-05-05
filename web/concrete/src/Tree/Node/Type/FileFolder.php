e45f6f31f522f7828408b4d3bcf22497b5aa1980
Andrew Embler <andrew@concrete5.org>
2016-05-05 15:07:54 -0700

    file manager cleanup

--- web/concrete/src/Tree/Node/Type/FileFolder.php
+++ web/concrete/src/Tree/Node/Type/FileFolder.php
@@ -1,11 +1,14 @@
 <?php
 namespace Concrete\Core\Tree\Node\Type;
 
+use Concrete\Core\File\FolderItemList;
 use Concrete\Core\Tree\Node\Node as TreeNode;
 use Concrete\Core\Tree\Node\Type\Formatter\CategoryListFormatter;
 use Concrete\Core\Tree\Node\Type\Menu\CategoryMenu;
 use Concrete\Core\Tree\Node\Type\Menu\FileFolderMenu;
+use Concrete\Core\User\User;
 use Loader;
+use Symfony\Component\HttpFoundation\Request;
 
 class FileFolder extends Category
 {
@@ -38,4 +41,28 @@ class FileFolder extends Category
         return parent::getTreeNodeName();
     }
 
+    public function getFolderItemList(User $u = null, Request $request)
+    {
+        $sort = false;
+        $list = new FolderItemList();
+        $list->filterByParentFolder($this);
+        if (is_object($u)) {
+            if (($column = $request->get($list->getQuerySortColumnParameter())) && ($direction = $request->get($list->getQuerySortDirectionParameter()))) {
+                $sort = array($column, $direction);
+                $u->saveConfig(sprintf('file_manager.sort.%s', $this->getTreeNodeID()), json_encode($sort));
+            } else {
+                $sort = $u->config(sprintf('file_manager.sort.%s', $this->getTreeNodeID()));
+                if ($sort) {
+                    $sort = json_decode($sort);
+                }
+            }
+            if (is_array($sort)) {
+                $list->sortBy($sort[0], $sort[1]);
+            }
+        }
+        if (!is_array($sort)) {
+            $list->sortByNodeType();
+        }
+        return $list;
+    }
 }
