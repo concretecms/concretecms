d2e6dc32fe39e2dc2f9d020fcf2fe66cf67af3f8
Michael Krasnow <mnkras@gmail.com>
2016-05-10 13:25:32 -0400

    Updates for v8

--- tests/tests/Core/File/Service/Fixtures/TestStorageLocation.php
+++ tests/tests/Core/File/Service/Fixtures/TestStorageLocation.php
@@ -2,19 +2,19 @@
 
 namespace Concrete\Tests\Core\File\Service\Fixtures;
 use Concrete\Core\File\StorageLocation\StorageLocation;
-use Concrete\Flysystem\Cache\Noop;
+use League\Flysystem\Cache\Noop;
 
 class TestStorageLocation extends StorageLocation
 {
     /**
      * Returns the proper file system object for the current storage location, by mapping
      * it through Flysystem
-     * @return \Concrete\Flysystem\Filesystem
+     * @return \League\Flysystem\Filesystem
      */
     public function getFileSystemObject()
     {
         $adapter = $this->getConfigurationObject()->getAdapter();
-        $filesystem = new \Concrete\Flysystem\Filesystem($adapter, new Noop());
+        $filesystem = new \League\Flysystem\Filesystem($adapter, new Noop());
         return $filesystem;
     }
 }
