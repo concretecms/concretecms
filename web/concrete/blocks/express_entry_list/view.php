2148c9289c2791875be55669315f94b10dc33378
Andrew Embler <andrew@concrete5.org>
2016-05-11 15:57:19 -0700

    express form
    Former-commit-id: ff962a217d8164709e57e99e821fa05424d2876b
    Former-commit-id: 02e95e9853509371ea192cf1d853de9476d7496e

--- web/concrete/blocks/express_entry_list/view.php
+++ web/concrete/blocks/express_entry_list/view.php
@@ -1,3 +1,48 @@
-<?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
+<?php defined('C5_EXECUTE') or die(_("Access Denied."));
 
-Express Entry List
+if ($tableName) { ?>
+
+    <h2><?=$tableName?></h2>
+
+<?php }
+
+if ($entity) {
+
+    $results = $result->getItemListObject()->getResults();
+
+    if (count($results)) { ?>
+
+
+        <table class="table">
+            <thead>
+            <tr>
+            <?php foreach($result->getColumns() as $column) {
+                ?>
+                <th class="<?=$column->getColumnStyleClass()?>"><a href="<?=$column->getColumnSortURL()?>"><?=$column->getColumnTitle()?></a></th>
+            <?php } ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($result->getItems() as $item) { ?>
+                <tr>
+                <?php foreach($item->getColumns() as $column) { ?>
+                    <td><?=$column->getColumnValue($item);?></td>
+                <?php } ?>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+
+        <?php if ($pagination) { ?>
+            <?=$pagination ?>
+        <?php } ?>
+
+
+    <?php } else { ?>
+
+        <p><?=t('No "%s" entries can be found', $entity->getName())?>
+
+    <?php } ?>
+
+
+<?php } ?>
