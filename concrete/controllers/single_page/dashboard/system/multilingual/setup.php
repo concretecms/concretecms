049499a08722beefcf404a16928d93658d073db5
Yihka <masteryihka@hotmail.com>
2016-05-19 08:27:48 +0200

    Allow the ability to change the redirect_home_status_code by users for the Multilangual. SEO experts don't like a 302 and would most likely want it to be set to 301, even though it is wrong.

--- concrete/controllers/single_page/dashboard/system/multilingual/setup.php
+++ concrete/controllers/single_page/dashboard/system/multilingual/setup.php
@@ -42,6 +42,7 @@ class Setup extends DashboardPageController
         $this->set('defaultSourceCountry', $defaultSourceCountry);
         $this->set('redirectHomeToDefaultLocale', Config::get('concrete.multilingual.redirect_home_to_default_locale'));
         $this->set('useBrowserDetectedLocale', Config::get('concrete.multilingual.use_browser_detected_locale'));
+        $this->set('redirectHomeStatusCode', Config::get('concrete.multilingual.redirect_home_status_code'));
     }
 
     protected function populateCopyArray($startingPage)
@@ -129,6 +130,7 @@ class Setup extends DashboardPageController
                 Config::save('concrete.multilingual.default_locale', $this->post('defaultLocale'));
                 Config::save('concrete.multilingual.redirect_home_to_default_locale', $this->post('redirectHomeToDefaultLocale'));
                 Config::save('concrete.multilingual.use_browser_detected_locale', $this->post('useBrowserDetectedLocale'));
+                Config::save('concrete.multilingual.redirect_home_status_code', $this->post('redirectHomeStatusCode'));
                 $defaultSourceLocale = '';
                 $s = $this->post('defaultSourceLanguage');
                 if (is_string($s) && array_key_exists($s, $languages)) {
