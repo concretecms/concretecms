07c8297efd5b8c9d57a07951a6d31b6b9c9d1267
a3020 <a3020@users.noreply.github.com>
2016-05-21 17:20:01 +0200

    Cleanup html block (#3844)
    * Cleanup everything but controller.php
    
    - bID is always an integer, no need for intval
    - Use h helper instead of htmlspecialchars
    - Use form helper for labels
    - Remove unnecessary vars like $controllerObj
    - PSR fixer
    
    I've tested the block with composer and the layout remains the same.
    
    * tabs to spaces
    
    * Keep the original htmlspecialchars
    
    
    Former-commit-id: 3a28e3b9ab49facdc7abfe932c9041afee21dd38
    Former-commit-id: 9e6393334372e848e7fd55ed4a444bbb9863ad89

--- concrete/blocks/html/view.php
+++ concrete/blocks/html/view.php
@@ -1,5 +1,5 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-<div id="HTMLBlock<?=intval($bID)?>" class="HTMLBlock">
-<?=$content; ?>
+<div id="HTMLBlock<?php echo $bID ?>" class="HTMLBlock">
+<?php echo $content; ?>
 </div>
