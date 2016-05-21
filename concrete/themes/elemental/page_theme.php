22ac1161c4a71892279098fec262887df1361df1
a3020 <a3020@users.noreply.github.com>
2016-05-21 21:41:48 +0200

    PageTheme improvements
    - Docblocks (but methods needs more explanation)
    - Shorthand array
    - Imports

--- concrete/themes/elemental/page_theme.php
+++ concrete/themes/elemental/page_theme.php
@@ -2,8 +2,9 @@
 namespace Concrete\Theme\Elemental;
 
 use Concrete\Core\Area\Layout\Preset\Provider\ThemeProviderInterface;
+use Concrete\Core\Page\Theme\Theme;
 
-class PageTheme extends \Concrete\Core\Page\Theme\Theme implements ThemeProviderInterface
+class PageTheme extends Theme implements ThemeProviderInterface
 {
     public function registerAssets()
     {
@@ -42,90 +43,108 @@ class PageTheme extends \Concrete\Core\Page\Theme\Theme implements ThemeProvider
         return t('Elegant, spacious theme with support for blogs, portfolios, layouts and more.');
     }
 
+    /**
+     * @return array
+     */
     public function getThemeBlockClasses()
     {
-        return array(
-            'feature' => array('feature-home-page'),
-            'page_list' => array(
+        return [
+            'feature' => ['feature-home-page'],
+            'page_list' => [
                 'recent-blog-entry',
                 'blog-entry-list',
                 'page-list-with-buttons',
                 'block-sidebar-wrapped',
-            ),
-            'next_previous' => array('block-sidebar-wrapped'),
-            'share_this_page' => array('block-sidebar-wrapped'),
-            'content' => array(
+            ],
+            'next_previous' => ['block-sidebar-wrapped'],
+            'share_this_page' => ['block-sidebar-wrapped'],
+            'content' => [
                 'block-sidebar-wrapped',
                 'block-sidebar-padded',
-            ),
-            'date_navigation' => array('block-sidebar-padded'),
-            'topic_list' => array('block-sidebar-wrapped'),
-            'testimonial' => array('testimonial-bio'),
-            'image' => array(
+            ],
+            'date_navigation' => ['block-sidebar-padded'],
+            'topic_list' => ['block-sidebar-wrapped'],
+            'testimonial' => ['testimonial-bio'],
+            'image' => [
                 'image-right-tilt',
                 'image-circle',
-            ),
-        );
+            ],
+        ];
     }
 
+    /**
+     * @return array
+     */
     public function getThemeAreaClasses()
     {
-        return array(
-            'Page Footer' => array('area-content-accent'),
-        );
+        return [
+            'Page Footer' => ['area-content-accent'],
+        ];
     }
 
+    /**
+     * @return array
+     */
     public function getThemeDefaultBlockTemplates()
     {
-        return array(
+        return [
             'calendar' => 'bootstrap_calendar.php',
-        );
+        ];
     }
 
+    /**
+     * @return array
+     */
     public function getThemeResponsiveImageMap()
     {
-        return array(
+        return [
             'large' => '900px',
             'medium' => '768px',
             'small' => '0',
-        );
+        ];
     }
 
+    /**
+     * @return array
+     */
     public function getThemeEditorClasses()
     {
-        return array(
-            array('title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin', 'forceBlock' => 1),
-            array('title' => t('Title Caps Bold'), 'menuClass' => 'title-caps-bold', 'spanClass' => 'title-caps-bold', 'forceBlock' => 1),
-            array('title' => t('Title Caps'), 'menuClass' => 'title-caps', 'spanClass' => 'title-caps', 'forceBlock' => 1),
-            array('title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption', 'forceBlock' => '-1'),
-            array('title' => t('Standard Button'), 'menuClass' => '', 'spanClass' => 'btn btn-default', 'forceBlock' => '-1'),
-            array('title' => t('Success Button'), 'menuClass' => '', 'spanClass' => 'btn btn-success', 'forceBlock' => '-1'),
-            array('title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary', 'forceBlock' => '-1'),
-        );
+        return [
+            ['title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin', 'forceBlock' => 1],
+            ['title' => t('Title Caps Bold'), 'menuClass' => 'title-caps-bold', 'spanClass' => 'title-caps-bold', 'forceBlock' => 1],
+            ['title' => t('Title Caps'), 'menuClass' => 'title-caps', 'spanClass' => 'title-caps', 'forceBlock' => 1],
+            ['title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption', 'forceBlock' => '-1'],
+            ['title' => t('Standard Button'), 'menuClass' => '', 'spanClass' => 'btn btn-default', 'forceBlock' => '-1'],
+            ['title' => t('Success Button'), 'menuClass' => '', 'spanClass' => 'btn btn-success', 'forceBlock' => '-1'],
+            ['title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary', 'forceBlock' => '-1'],
+        ];
     }
 
+    /**
+     * @return array
+     */
     public function getThemeAreaLayoutPresets()
     {
-        $presets = array(
-            array(
+        $presets = [
+            [
                 'handle' => 'left_sidebar',
                 'name' => 'Left Sidebar',
                 'container' => '<div class="row"></div>',
-                'columns' => array(
+                'columns' => [
                     '<div class="col-sm-4"></div>',
                     '<div class="col-sm-8"></div>',
-                ),
-            ),
-            array(
+                ],
+            ],
+            [
                 'handle' => 'right_sidebar',
                 'name' => 'Right Sidebar',
                 'container' => '<div class="row"></div>',
-                'columns' => array(
+                'columns' => [
                     '<div class="col-sm-8"></div>',
                     '<div class="col-sm-4"></div>',
-                ),
-            ),
-        );
+                ],
+            ],
+        ];
 
         return $presets;
     }
