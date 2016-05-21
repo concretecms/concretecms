2885b79a6e03f2f788251678b1d190fa5633d98c
a3020 <a3020@users.noreply.github.com>
2016-05-21 17:19:14 +0200

    Cleanup desktop_app_status block (#3845)
    * Cleanup controller
    
    - Remove unused $updates var
    - Use full namespace and imports
    - Use Permissions instead of deprecated TaskPermissions
    - Use shorthand array
    
    * PSR controller
    
    * Cleanup view
    
    - Reformatting
    - t2 should use %d instead of %s
    
    * Add clearfix and remove closing p tag.
    
    - The p tag doesn't start anywhere, so let's remove it.
    - Add a clearfix to the alert box for the floating button.
    
    
    Former-commit-id: dd98d7449d6ed3f148060b2793b096d2c70ce472
    Former-commit-id: 40cfc3fe89e9296588844b2865d440aaca6d0d65

--- concrete/blocks/desktop_app_status/view.php
+++ concrete/blocks/desktop_app_status/view.php
@@ -1,27 +1,26 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-    <?php if (version_compare($latest_version, APP_VERSION, '>')) {
-        ?>
+<?php
+if (version_compare($latest_version, APP_VERSION, '>')) {
+    ?>
     <div class="alert alert-info">
-        <?=t('The latest version of concrete5 is <strong>%s</strong>. You are currently running concrete5 version <strong>%s</strong>.', $latest_version, APP_VERSION)?>
-        <a class="pull-right btn btn-info btn-xs" href="<?=$view->url('/dashboard/system/backup/update')?>"><?=t('Update')?></a>
-        </div>
-
+        <?php echo t('The latest version of concrete5 is <strong>%s</strong>. You are currently running concrete5 version <strong>%s</strong>.', $latest_version, APP_VERSION) ?>
+        <a class="pull-right btn btn-info btn-xs" href="<?php echo $view->url('/dashboard/system/backup/update')?>"><?php echo t('Update')?></a>
+    </div>
     <?php
-    } elseif (version_compare(APP_VERSION, Config::get('concrete.version'), '>')) {
-        ?>
+} elseif (version_compare(APP_VERSION, Config::get('concrete.version'), '>')) {
+    ?>
     <div class="alert alert-warning">
-
-    <?=t('You have downloaded a new version of concrete5 but have not upgraded to it yet.');
-        ?> <a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/upgrade" class="pull-right btn btn-warning btn-xs"><?=t('Update')?></a></div>
+        <?php echo t('You have downloaded a new version of concrete5 but have not upgraded to it yet.'); ?>
+        <a href="<?php echo REL_DIR_FILES_TOOLS_REQUIRED ?>/upgrade" class="pull-right btn btn-warning btn-xs"><?php echo t('Update')?></a></div>
     <?php
-    } ?>
+}
 
-    <?php if ($updates > 0) { ?>
-
-        <div class="alert alert-info">
-            <?=t2('There is currently %s add-on update available.', 'There are currently %s add-on updates available.', $updates)?>
-            <a class="btn btn-info btn-xs pull-right" href="<?=$view->url('/dashboard/extend/update')?>"><?=t('Update')?></a></p>
-        </div>
-
-    <?php } ?>
+if ($updates > 0) {
+    ?>
+    <div class="alert alert-info clearfix">
+        <?php echo t2('There is currently %d add-on update available.', 'There are currently %d add-on updates available.', $updates) ?>
+        <a class="btn btn-info btn-xs pull-right" href="<?php echo $view->url('/dashboard/extend/update') ?>"><?php echo t('Update') ?></a>
+    </div>
+    <?php
+}
