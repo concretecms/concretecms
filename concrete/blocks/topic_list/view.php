1038008ad2f27ff76ece9b78838a0449754899d9
Siton-Design <siton22@hotmail.com>
2016-06-13 04:28:24 +0300

    Fix wrong markup  of nested list item(mode s)
    Topic list like this:
    A
    B
    C (parent li)
    ---C1 (inside C)
    ---C2 (inside C)
    D
    
    Output very wrong markup (make a test)
    
    The correct markup needs to be:
    <ul class="ccm-block-topic-list-list">
        <li>A</li>
        <li>B</li>
        <li class="uk-parent">C
            <ul class="ccm-block-topic-sub-list">
                <li>C1</li>
                <li>C2</li>
            </ul>
        </li>
    </ul>
    </ul>
    Example: http://www.w3schools.com/html/tryit.asp?filename=tryhtml_lists_nested
    
    Benefits:
    1) Correct/valid nested list markup
    2) Add "parent list" selector (For CSS design of the "parent li item")
    3) Add "parent list" selector (For CSS design of the "sub menu items")

--- concrete/blocks/topic_list/view.php
+++ concrete/blocks/topic_list/view.php
@@ -1,44 +1,61 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <div class="ccm-block-topic-list-wrapper">
-
+	
     <div class="ccm-block-topic-list-header">
         <h5><?php echo h($title); ?></h5>
-    </div>
-
+	</div>
+	
     <?php
-    if ($mode == 'S' && is_object($tree)) {
-        $node = $tree->getRootTreeNodeObject();
-        $node->populateChildren();
-        if (is_object($node)) {
-            if (!isset($selectedTopicID)) {
-                $selectedTopicID = null;
-            }
-            $walk = function ($node) use (&$walk, &$view, $selectedTopicID) {
-                ?><ul class="ccm-block-topic-list-list"><?php
-                foreach ($node->getChildNodes() as $topic) {
-                    if ($topic instanceof \Concrete\Core\Tree\Node\Type\Category) {
-                        ?><li><?php echo $topic->getTreeNodeDisplayName(); ?></li>
-                        <?php
-                    } else {
-                        ?><a href="<?php echo $view->controller->getTopicLink($topic); ?>" <?php
-                        if (isset($selectedTopicID) && $selectedTopicID == $topic->getTreeNodeID()) {
-                            ?> class="ccm-block-topic-list-topic-selected"<?php
-                        }
-                        ?>><?php echo $topic->getTreeNodeDisplayName(); ?></a></li><?php
-                    }
-                    if (count($topic->getChildNodes())) {
-                        $walk($topic);
-                    } ?>
-                    </li>
-                    <?php
-                }
-                ?></ul><?php
-            };
-            $walk($node);
-        }
-    }
-
+		if ($mode == 'S' && is_object($tree)) {
+			$node = $tree->getRootTreeNodeObject();
+			$node->populateChildren();
+			if (is_object($node)) {
+				if (!isset($selectedTopicID)) {
+					$selectedTopicID = null;
+				}?>		
+				<ul class="ccm-block-topic-list-list">
+					<?php
+						$walk = function ($node) use (&$walk, &$view, $selectedTopicID) {
+							foreach ($node->getChildNodes() as $topic) {
+								//Case1: Print Node without child elements!
+								if (count($topic->getChildNodes())== 0) {?> 
+								<li><a href="<?php echo $view->controller->getTopicLink($topic); ?>" 	
+									<?php
+										if (isset($selectedTopicID) && $selectedTopicID == $topic->getTreeNodeID()) {
+											?> class="ccm-block-topic-list-topic-selected"<?php
+										}
+										?>><?php echo $topic->getTreeNodeDisplayName(); ?>
+									</a>
+								</li>
+								<?php } 
+								//Case2: Node with child elements (nested ul list inside li item!)
+								if (count($topic->getChildNodes())) {
+								?>
+								<li class="ccm-block-topic-list-topic-parent">
+									
+									<a href="<?php echo $view->controller->getTopicLink($topic); ?>" 	
+									<?php
+										if (isset($selectedTopicID) && $selectedTopicID == $topic->getTreeNodeID()) {
+											?> class="ccm-block-topic-list-topic-selected"<?php
+										}
+										?>><?php echo $topic->getTreeNodeDisplayName(); ?>
+									</a>
+									<!-- Open Nested list-->
+									<ul class="ccm-block-topic-nested-list-list">
+										<?php $walk($topic); ?>
+									</ul>
+								</li>
+								<?php } 
+							}
+							
+						};
+						$walk($node);
+						//Close outer ul tag
+						echo "</ul>";
+					}
+				}
+				
     if ($mode == 'P') {
         if (count($topics)) {
             ?><ul class="ccm-block-topic-list-page-topics"><?php
