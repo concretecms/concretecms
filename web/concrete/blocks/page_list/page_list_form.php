1d48cdfada87c763d3bee10d9a6c0b35962016a0
Andrew Embler <andrew@concrete5.org>
2016-06-15 16:50:57 -0700

    adding option to ignore permissions to page list block
    Former-commit-id: 979e9ac11b9561376baae95c8fbae0d8a0e7679c
    Former-commit-id: bdc573c0070893e5ac9aaecebfbc5c1693b5ef00

--- web/concrete/blocks/page_list/page_list_form.php
+++ web/concrete/blocks/page_list/page_list_form.php
@@ -171,6 +171,14 @@ $form = Loader::helper('form/page_selector');
 
         <div class="checkbox">
             <label>
+                <input type="checkbox" name="ignorePermissions"
+                       value="1" <? if ($ignorePermissions == 1) { ?> checked <? } ?> />
+                <?= t('Ignore page permissions.') ?>
+            </label>
+        </div>
+
+            <div class="checkbox">
+            <label>
                 <input type="checkbox" name="enableExternalFiltering" value="1" <? if ($enableExternalFiltering) { ?>checked<? } ?> />
                 <?= t('Enable Other Blocks to Filter This Page List.') ?>
             </label>
