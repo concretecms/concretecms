3a4891effd2d38e47e8a3350badbdf48a20bdb84
uimatters <buymysoap@gmail.com>
2016-06-20 09:28:56 -0500

    Add ARIA label to social share icons

--- web/concrete/blocks/share_this_page/view.php
+++ web/concrete/blocks/share_this_page/view.php
@@ -1,9 +1,9 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+ aria-label="<?php echo h($service->getDisplayName()) ?>"<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <div class="ccm-block-share-this-page">
     <ul class="list-inline">
     <? foreach($selected as $service) { ?>
-        <li><a href="<?= h($service->getServiceLink()) ?>"><?=$service->getServiceIconHTML()?></a></li>
+        <li><a href="<?= h($service->getServiceLink()) ?>" aria-label="<?php echo h($service->getDisplayName()) ?>"><?=$service->getServiceIconHTML()?></a></li>
     <? } ?>
     </ul>
 </div>
