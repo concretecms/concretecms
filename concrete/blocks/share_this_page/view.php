76a4497cc29ef27b4bb5f7658fdf4fae2e6a1296
uimatters <buymysoap@gmail.com>
2016-06-22 14:30:44 -0500

    WCAG fix for Share This Page block

--- concrete/blocks/share_this_page/view.php
+++ concrete/blocks/share_this_page/view.php
@@ -1,10 +1,10 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <div class="ccm-block-share-this-page">
     <ul class="list-inline">
     <?php foreach ($selected as $service) {
     ?>
-        <li><a href="<?= h($service->getServiceLink()) ?>"><?=$service->getServiceIconHTML()?></a></li>
+        <li><a href="<?= h($service->getServiceLink()) ?>" aria-label="<?= h($service->getDisplayName()) ?>"><?=$service->getServiceIconHTML()?></a></li>
     <?php 
 } ?>
     </ul>
