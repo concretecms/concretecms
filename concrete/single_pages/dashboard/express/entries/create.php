3d54a1366f401be9ad924689a6d7ab65b529cc98
tylerryan <ryan@concrete5.org>
2016-07-08 15:54:37 -0700

    running php-cs-fixer

--- concrete/single_pages/dashboard/express/entries/create.php
+++ concrete/single_pages/dashboard/express/entries/create.php
@@ -9,14 +9,15 @@ if (is_object($expressForm)) {
 
 
     <?php
-        echo $renderer->render($expressForm);
-    ?>
+        echo $renderer->render($expressForm); ?>
 
         <div class="ccm-dashboard-form-actions-wrapper">
             <div class="ccm-dashboard-form-actions">
-                <?php if ($backURL) { ?>
+                <?php if ($backURL) {
+    ?>
                     <a class="pull-left btn btn-default" href="<?=$backURL?>"><?=t('Back')?></a>
-                <?php } ?>
+                <?php 
+} ?>
                 <button class="pull-right btn btn-primary" type="submit"><?=t('Add %s', $entity->getName())?></button>
             </div>
         </div>
