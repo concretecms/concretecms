7d00f63b28970dc0389cdba5224b2b0aba6d161b
Korvin Szanto <korvinszanto@gmail.com>
2016-07-12 12:27:28 -0700

    PSR2

--- web/concrete/controllers/single_page/dashboard/system/seo/excluded.php
+++ web/concrete/controllers/single_page/dashboard/system/seo/excluded.php
@@ -1,66 +1,69 @@
 <?php
 
 namespace Concrete\Controller\SinglePage\Dashboard\System\Seo;
+
 use Concrete\Core\Page\Controller\DashboardPageController;
 use Config;
 use Loader;
 use URLify;
 
-class Excluded extends DashboardPageController {
+class Excluded extends DashboardPageController
+{
 
-	public function save() {
+    public function save()
+    {
 
-		$token = $this->app['token'];
-		$config = $this->app['config'];
-		$security = $this->app['helper/security'];
+        $token = $this->app['token'];
+        $config = $this->app['config'];
+        $security = $this->app['helper/security'];
 
-		if ($token->validate('excluded_words_save')) {
-			$words = $security->sanitizeString($this->post('SEO_EXCLUDE_WORDS'));
-			$config->save('concrete.seo.exclude_words', $words);
+        if ($token->validate('excluded_words_save')) {
+            $words = $security->sanitizeString($this->post('SEO_EXCLUDE_WORDS'));
+            $config->save('concrete.seo.exclude_words', $words);
 
-			$this->redirect('/dashboard/system/seo/excluded', 'saved');
-		} else {
-			$this->error->add('Invalid token.');
-			$this->view();
-		}
-	}
+            $this->redirect('/dashboard/system/seo/excluded', 'saved');
+        } else {
+            $this->error->add('Invalid token.');
+            $this->view();
+        }
+    }
 
-	public function reset() {
-		$this->app['config']->save("concrete.seo.exclude_words", implode(', ', URLify::$remove_list));
-		$this->redirect('/dashboard/system/seo/excluded', 'reset_complete');
-	}
+    public function reset()
+    {
+        $this->app['config']->save("concrete.seo.exclude_words", implode(', ', URLify::$remove_list));
+        $this->redirect('/dashboard/system/seo/excluded', 'reset_complete');
+    }
 
-	public function view($message = false) {
-		$this->set('app', $this->app);
+    public function view($message = false)
+    {
+        $this->set('app', $this->app);
         $config = $this->app['config'];
 
-		if ($message) {
-			switch($message) {
-				case 'reset_complete':
-					$this->set('message', t('Reserved words reset.'));
-					break;
-				case 'saved':
-					$this->set('success', t('Reserved words updated.'));
-					break;
-			}
-		}
-		$this->set('SEO_EXCLUDE_WORDS_ORIGINAL_ARRAY', Urlify::$remove_list);
-		$excludeSeoWords = $config->get('concrete.seo.exclude_words');
-		if(is_string($excludeSeoWords)) {
-			if(strlen($excludeSeoWords)) {
-				$remove_list = explode(',', $excludeSeoWords);
-				$remove_list = array_map('trim', $remove_list);
-				$remove_list = array_filter($remove_list, 'strlen');
-			}
-			else {
-				$remove_list = array();
-			}
-		}
-		else {
-			$remove_list = Urlify::$remove_list;
-		}
-		$this->set('SEO_EXCLUDE_WORDS_ARRAY', $remove_list);
-		$this->set('SEO_EXCLUDE_WORDS',Config::get('concrete.seo.exclude_words'));
-	}
+        if ($message) {
+            switch ($message) {
+                case 'reset_complete':
+                    $this->set('message', t('Reserved words reset.'));
+                    break;
+                case 'saved':
+                    $this->set('success', t('Reserved words updated.'));
+                    break;
+            }
+        }
+        $this->set('SEO_EXCLUDE_WORDS_ORIGINAL_ARRAY', Urlify::$remove_list);
+        $excludeSeoWords = $config->get('concrete.seo.exclude_words');
+        if (is_string($excludeSeoWords)) {
+            if (strlen($excludeSeoWords)) {
+                $remove_list = explode(',', $excludeSeoWords);
+                $remove_list = array_map('trim', $remove_list);
+                $remove_list = array_filter($remove_list, 'strlen');
+            } else {
+                $remove_list = array();
+            }
+        } else {
+            $remove_list = Urlify::$remove_list;
+        }
+        $this->set('SEO_EXCLUDE_WORDS_ARRAY', $remove_list);
+        $this->set('SEO_EXCLUDE_WORDS', Config::get('concrete.seo.exclude_words'));
+    }
 
 }
