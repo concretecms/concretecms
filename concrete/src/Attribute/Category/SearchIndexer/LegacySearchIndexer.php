b232ad8551a95f3fe71abe72bf2e96af49f45ddf
Andrew Embler <andrew@concrete5.org>
2016-08-03 12:30:06 -0700

    Bug fix
    Former-commit-id: d84e9eae6e8103f5c629b51847abfe55becbb2b0
    Former-commit-id: bb2a40fd0fa596c9216cc15a400e3c4367af0a82

--- concrete/src/Attribute/Category/SearchIndexer/LegacySearchIndexer.php
+++ concrete/src/Attribute/Category/SearchIndexer/LegacySearchIndexer.php
@@ -4,7 +4,6 @@ namespace Concrete\Core\Attribute\Category\SearchIndexer;
 use Concrete\Core\Attribute\AttributeKeyInterface;
 use Concrete\Core\Attribute\AttributeValueInterface;
 use Concrete\Core\Attribute\Category\CategoryInterface;
-use Concrete\Core\Attribute\Key\SearchIndexer\StandardSearchIndexer;
 use Concrete\Core\Database\Connection\Connection;
 use Concrete\Core\Entity\Attribute\Value\Value;
 use Doctrine\DBAL\Schema\Schema;
