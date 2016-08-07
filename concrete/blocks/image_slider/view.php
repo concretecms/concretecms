432f766fe143f7cf9dc4c871003e1764a1e0a47d
Siton-Design <siton22@hotmail.com>
2016-08-07 04:23:50 +0300

    Add font-aswome icons to slider "edit mode"
    Tiny UI issue - but Like this its more "clear" thats this is a slider (like wireframe design style) + Also you know how much "sliders" you have in your block (Dots idea - 3 sliders = 3 dots. the first one is active only for more "real" slider wireframe).

--- concrete/blocks/image_slider/view.php
+++ concrete/blocks/image_slider/view.php
@@ -4,7 +4,18 @@ $c = Page::getCurrentPage();
 if ($c->isEditMode()) {
     ?>
     <div class="ccm-edit-mode-disabled-item" style="<?php echo isset($width) ? "width: $width;" : '' ?><?php echo isset($height) ? "height: $height;" : '' ?>">
-        <div style="padding: 40px 0px 40px 0px"><?php echo t('Image Slider disabled in edit mode.')?></div>
+        <i style="font-size:40px; margin-bottom:20px; display:block;" class="fa fa-picture-o" aria-hidden="true"></i>
+        <div style="padding: 40px 0px 40px 0px"><?php echo t('Image Slider disabled in edit mode.')?>
+			<div style="margin-top: 15px; font-size:9px;">
+				<i class="fa fa-circle" aria-hidden="true"></i>
+				<?php if (count($rows) > 0) { ?>
+					<?php foreach (array_slice($rows, 1) as $row) { ?>
+						<i class="fa fa-circle-thin" aria-hidden="true"></i>
+						<?php } 
+					} 
+				?>
+			</div>
+        </div>
     </div>
 <?php
 } else {
