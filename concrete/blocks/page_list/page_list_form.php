c22814c21d652d8be5de53dddf74d5317c61f719
Michele Locati <michele@locati.it>
2016-08-09 15:30:55 +0200

    Remove PHP short open tags from page_list_form

--- concrete/blocks/page_list/page_list_form.php
+++ concrete/blocks/page_list/page_list_form.php
@@ -206,14 +206,14 @@ $form = Loader::helper('form/page_selector');
         <div class="checkbox">
             <label>
                 <input type="checkbox" name="ignorePermissions"
-                       value="1" <? if ($ignorePermissions == 1) { ?> checked <? } ?> />
+                       value="1" <?php if ($ignorePermissions == 1) { ?> checked <?php } ?> />
                 <?= t('Ignore page permissions.') ?>
             </label>
         </div>
 
             <div class="checkbox">
             <label>
-                <input type="checkbox" name="enableExternalFiltering" value="1" <? if ($enableExternalFiltering) { ?>checked<? } ?> />
+                <input type="checkbox" name="enableExternalFiltering" value="1" <?php if ($enableExternalFiltering) { ?>checked<?php } ?> />
                 <?= t('Enable Other Blocks to Filter This Page List.') ?>
             </label>
         </div>
