14d568eee6ed0a6ecbc0da3565b3d61a1b20fb45
Michele Locati <michele@locati.it>
2016-08-12 08:02:47 +0200

    Better translatable string

--- concrete/single_pages/dashboard/system/backup/backup.php
+++ concrete/single_pages/dashboard/system/backup/backup.php
@@ -1,8 +1,6 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <div class="alert alert-warning">
-    <p><?php echo t('Backing up your site is important. Typically your web host can help you make a complete backup of your environment
-    from your hosting control panel. Alternatively, there are additional backup solutions available in the concrete5.org marketplace.'); ?></p>
-    <a href="https://www.concrete5.org/marketplace/addons/-/view/?submit_search=1&search_keywords=backup">
-    <?php echo t('https://www.concrete5.org/marketplace/addons/-/view/?submit_search=1&search_keywords=backup'); ?></a>
+    <p><?php echo t('Backing up your site is important. Typically your web host can help you make a complete backup of your environment from your hosting control panel. Alternatively, there are additional backup solutions available in the concrete5.org marketplace.'); ?></p>
+    <a href="https://www.concrete5.org/marketplace/addons/-/view/?submit_search=1&search_keywords=backup"><?php echo t('https://www.concrete5.org/marketplace/addons/-/view/?submit_search=1&search_keywords=backup'); ?></a>
 </div>
