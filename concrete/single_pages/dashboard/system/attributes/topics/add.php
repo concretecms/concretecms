206339e106b8f44d8a3cec54aa997217e20238ed
MrKarlDilkington <mrkarldilkington@outlook.com>
2016-08-15 21:02:17 -0400

    tidy up

--- concrete/single_pages/dashboard/system/attributes/topics/add.php
+++ concrete/single_pages/dashboard/system/attributes/topics/add.php
@@ -1,27 +1,22 @@
-<?php
-defined('C5_EXECUTE') or die("Access Denied.");
+<?php defined('C5_EXECUTE') or die("Access Denied.");
 $form = Core::make('helper/form');
 ?>
 
-<form method="post" action="<?=$view->action('submit')?>" class="form-horizontal">
-<?php if (PermissionKey::getByHandle('add_topic_tree')->validate()) {
-    ?>
-	<?=Loader::helper('validation/token')->output('submit')?>
+<form method="post" action="<?php echo $view->action('submit'); ?>" class="form-horizontal">
+<?php if (PermissionKey::getByHandle('add_topic_tree')->validate()) { ?>
+	<?php echo Loader::helper('validation/token')->output('submit'); ?>
 	<div class="form-group">
-		<?=$form->label('topicTreeName', t('Tree Name'))?>
-		<?=$form->text('topicTreeName', $topicTreeName, array('class' => 'span4'))?>
+		<?php echo $form->label('topicTreeName', t('Tree Name')); ?>
+		<?php echo $form->text('topicTreeName', $topicTreeName, array('class' => 'span4')); ?>
 	</div>
-<?php
-} else {
-    ?>
-	<p><?=t('You may not add topic trees.')?></p>
-<?php
-} ?>
+<?php } else { ?>
+	<p><?php echo t('You may not add topic trees.'); ?></p>
+<?php } ?>
 
-<div class="ccm-dashboard-form-actions-wrapper">
-    <div class="ccm-dashboard-form-actions ">
-        <a href="<?=$view->url('/dashboard/system/attributes/topics')?>" class="btn btn-default pull-left"><?=t("Cancel")?></a>
-        <button type="submit" class="btn btn-primary pull-right"><?=t('Add Topic Tree')?></button>
+    <div class="ccm-dashboard-form-actions-wrapper">
+        <div class="ccm-dashboard-form-actions ">
+            <a href="<?php echo $view->url('/dashboard/system/attributes/topics'); ?>" class="btn btn-default pull-left"><?php echo t("Cancel"); ?></a>
+            <button type="submit" class="btn btn-primary pull-right"><?php echo t('Add Topic Tree'); ?></button>
+        </div>
     </div>
-</div>
 </form>
