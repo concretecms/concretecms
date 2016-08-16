8b5030ee4c59e58069e100a412dc173c93724bea
MrKarlDilkington <mrkarldilkington@outlook.com>
2016-08-15 22:38:35 -0400

    removed Bootstrap grid elements breaking fieldless styling

--- concrete/single_pages/dashboard/system/mail/method/test.php
+++ concrete/single_pages/dashboard/system/mail/method/test.php
@@ -3,40 +3,37 @@
 $cih = Loader::helper('concrete/ui');
 /* @var $fh FormHelper */
 $fh = Loader::helper('form');
+?>
 
-?><form method="post" action="<?=$controller->action('do_test')?>" id="mail-settings-test-form">
-    <div class="row">
-        <div class="col-md-6">
+<form method="post" action="<?=$controller->action('do_test')?>" id="mail-settings-test-form">
+<?php
+if (!Config::get('concrete.email.enabled')) {
+    ?><div class="alert alert-info"><?php echo t(/*i18n: %1$s is a configuration name, %2$s is a configuration value*/'It\'s not possible to test the settings since the mail system is disabled (the setting %1$s is set to %2$s in the configuration).', '<b>concrete.email.enabled</b>', '<b>false</b>');
+    ?></div><?php
+
+} else {
+    echo Loader::helper('validation/token')->output('test');
+    ?>
+    <div class="form-group">
+        <?php echo $fh->label('mailRecipient', t('Recipient email address'));
+    ?>
             <?php
-            if (!Config::get('concrete.email.enabled')) {
-                ?><div class="alert alert-info"><?php echo t(/*i18n: %1$s is a configuration name, %2$s is a configuration value*/'It\'s not possible to test the settings since the mail system is disabled (the setting %1$s is set to %2$s in the configuration).', '<b>concrete.email.enabled</b>', '<b>false</b>');
-                ?></div><?php
-
-            } else {
-                echo Loader::helper('validation/token')->output('test');
-                ?>
-                <div class="form-group">
-                    <?php echo $fh->label('mailRecipient', t('Recipient email address'));
-                ?>
-                        <?php
-                        if (!isset($mailRecipient)) {
-                            $mailRecipient = '';
-                            if (User::isLoggedIn()) {
-                                $me = new User();
-                                $myInfo = UserInfo::getByID($me->getUserID());
-                                $mailRecipient = $myInfo->getUserEmail();
-                            }
-                        }
-                echo $fh->email('mailRecipient', $mailRecipient, array('required' => 'required'));
-                ?>
-                </div>
-                <?php
-
+            if (!isset($mailRecipient)) {
+                $mailRecipient = '';
+                if (User::isLoggedIn()) {
+                    $me = new User();
+                    $myInfo = UserInfo::getByID($me->getUserID());
+                    $mailRecipient = $myInfo->getUserEmail();
+                }
             }
-            ?>
-        </div>
+    echo $fh->email('mailRecipient', $mailRecipient, array('required' => 'required'));
+    ?>
     </div>
-	<div class="ccm-dashboard-form-actions-wrapper">
+    <?php
+
+}
+?>
+    <div class="ccm-dashboard-form-actions-wrapper">
         <div class="ccm-dashboard-form-actions">
             <a href="<?=$view->url('/dashboard/system/mail/method')?>" class="btn btn-default pull-left"><?=t('Change Settings')?></a>
             <?php
@@ -45,5 +42,5 @@ $fh = Loader::helper('form');
             }
             ?>
         </div>
-	</div>
+    </div>
 </form>
