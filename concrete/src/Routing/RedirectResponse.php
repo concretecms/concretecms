46c253b88bc9edb0715a606a22727ce96b5c1c0f
Korvin Szanto <korvinszanto@gmail.com>
2016-08-19 20:55:06 -0700

    Shim missing redirectresponse method

--- concrete/src/Routing/RedirectResponse.php
+++ concrete/src/Routing/RedirectResponse.php
@@ -1,14 +1,24 @@
 <?php
 namespace Concrete\Core\Routing;
 
+use Concrete\Core\Http\Request;
+
 class RedirectResponse extends \Symfony\Component\HttpFoundation\RedirectResponse
 {
     protected $request;
 
     public function __construct($url, $status = 302, $headers = array())
     {
-        $url = (string) $url; // sometimes we get an object.
+        $url = (string)$url; // sometimes we get an object.
         parent::__construct($url, $status, $headers);
     }
 
+    /**
+     * @deprecated This method doesn't actually do anything.
+     * @param \Concrete\Core\Http\Request $r
+     */
+    public function setRequest(Request $r)
+    {
+    }
 }
+
