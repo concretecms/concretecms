3ee14f620cd3b5f7cab3fb840b4299e41a4f649a
a3020 <a3020@users.noreply.github.com>
2016-08-22 19:58:50 +0200

    Elemental fixes (#3860)
    * Cleanup theme templates
    
    - Indentation & readability
    - Loader::element -> View::element
    - Shorthand arrays
    
    * PageTheme improvements
    
    - Docblocks (but methods needs more explanation)
    - Shorthand array
    - Imports
    
    * Add newlines at the end of the files
    
    
    Former-commit-id: 94a3174dd67ae46884fb2e599c849e49f4b82bc0
    Former-commit-id: 88662fc1e64838dab2159f0c3f5c37eac0caffc5

--- concrete/themes/elemental/view.php
+++ concrete/themes/elemental/view.php
@@ -1,18 +1,27 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$this->inc('elements/header.php'); ?>
+
+$this->inc('elements/header.php');
+?>
 
 <main>
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
+                <?php
+                View::element('system_errors', [
+                    'format' => 'block',
+                    'error' => isset($error) ? $error : null,
+                    'success' => isset($success) ? $success : null,
+                    'message' => isset($message) ? $message : null,
+                ]);
 
-                <?php View::element('system_errors', array('format' => 'block', 'error' => isset($error) ? $error : null, 'success' => isset($success) ? $success : null, 'message' => isset($message) ? $message : null)); ?>
-
-                <?php echo $innerContent; ?>
+                echo $innerContent;
+                ?>
             </div>
         </div>
     </div>
 </main>
 
-<?php  $this->inc('elements/footer.php'); ?>
+<?php
+$this->inc('elements/footer.php');
