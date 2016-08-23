d4e39517f6b9462c921c429c9976ecb8b70b5de3
Andrew Embler <andrew@concrete5.org>
2016-08-22 18:54:27 -0700

    Fix #4245

--- concrete/src/Express/Search/ColumnSet/Available.php
+++ concrete/src/Express/Search/ColumnSet/Available.php
@@ -1,7 +1,17 @@
 <?php
 namespace Concrete\Core\Express\Search\ColumnSet;
 
-class Available extends DefaultSet
+use Concrete\Core\Attribute\Category\ExpressCategory;
+use Concrete\Core\Search\Column\Column;
+
+class Available extends ColumnSet
 {
+
+    public function __construct(ExpressCategory $category)
+    {
+        parent::__construct($category);
+        $this->addColumn(new Column('e.exEntryDateCreated', t('Date Added'), array('\Concrete\Core\Express\Search\ColumnSet\DefaultSet', 'getDateAdded')));
+
+    }
 
 }
