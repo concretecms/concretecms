a70840d899be21eeb61ef387cad272f46fbcdcc3
Karl Dilkington <MrKarlDilkington@users.noreply.github.com>
2016-08-24 11:02:41 -0400

    increase modal width (#4254)
    Former-commit-id: e5326109143da07d10776ccb882fc00437437540
    Former-commit-id: 31f1c587609a9676da9b45aff32dc0af7a043591

--- concrete/elements/permission/access/list.php
+++ concrete/elements/permission/access/list.php
@@ -3,7 +3,7 @@
 <?php foreach ($accessTypes as $accessType => $title) {
     $list = $permissionAccess->getAccessListItems($accessType);
     ?>
-	<a style="float: right" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/permissions/access_entity?accessType=<?=$accessType?>&pkCategoryHandle=<?=$pkCategoryHandle?>" dialog-width="500" dialog-height="500" dialog-title="<?=t('Add Access Entity')?>" class="dialog-launch btn btn-xs btn-default"><?=t('Add')?></a>
+	<a style="float: right" href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/permissions/access_entity?accessType=<?=$accessType?>&pkCategoryHandle=<?=$pkCategoryHandle?>" dialog-width="510" dialog-height="500" dialog-title="<?=t('Add Access Entity')?>" class="dialog-launch btn btn-xs btn-default"><?=t('Add')?></a>
 
 	<h4><?=$title?></h4>
 
@@ -22,33 +22,33 @@
 <tr>
     <td>
     <a href="javascript:void(0)" class="icon-link pull-right" style="margin-left: 10px" onclick="ccm_deleteAccessEntityAssignment(<?=$pae->getAccessEntityID()?>)"><i class="fa fa-trash-o"></i></a>
-	<a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/permissions/access_entity?peID=<?=$pae->getAccessEntityID()?>&pdID=<?=$pdID?>&accessType=<?=$accessType?>" dialog-width="500" dialog-height="500" dialog-title="<?=t('Add Access Entity')?>" class="<?php if (!is_object($pa->getPermissionDurationObject())) {
-    ?>icon-link<?php 
+	<a href="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/permissions/access_entity?peID=<?=$pae->getAccessEntityID()?>&pdID=<?=$pdID?>&accessType=<?=$accessType?>" dialog-width="510" dialog-height="500" dialog-title="<?=t('Add Access Entity')?>" class="<?php if (!is_object($pa->getPermissionDurationObject())) {
+    ?>icon-link<?php
 }
     ?> pull-right dialog-launch"><i class="fa fa-clock-o <?php if (is_object($pa->getPermissionDurationObject())) {
-    ?>text-info<?php 
+    ?>text-info<?php
 }
     ?>"></i></a>
     <?=$pae->getAccessEntityLabel()?>
     </td>
 </tr>
 
-<?php 
+<?php
 }
     ?>
 
-<?php 
+<?php
 } else {
     ?>
 	<tr>
 	<td><?=t('None')?></td>
 	</tr>
-<?php 
+<?php
 }
     ?>
 
 </table>
 
 
-<?php 
+<?php
 } ?>
