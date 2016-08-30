fab753e45e8dc20a6521d8ba18706bec09501bd7
Andrew Embler <andrew@concrete5.org>
2016-08-29 20:33:58 -0700

    Fix #4274

--- concrete/src/Search/Field/AbstractField.php
+++ concrete/src/Search/Field/AbstractField.php
@@ -1,6 +1,7 @@
 <?php
 namespace Concrete\Core\Search\Field;
 
+use Concrete\Core\Http\ResponseAssetGroup;
 use Doctrine\Common\Collections\ArrayCollection;
 
 abstract class AbstractField implements FieldInterface
@@ -16,11 +17,28 @@ abstract class AbstractField implements FieldInterface
 
     public function jsonSerialize()
     {
+
+        ob_start();
+        print $this->renderSearchField();
+        $field = ob_get_contents();
+        ob_end_clean();
+
+        $ag = ResponseAssetGroup::get();
+        $assetsResponse = array();
+        foreach ($ag->getAssetsToOutput() as $position => $assets) {
+            foreach ($assets as $asset) {
+                if (is_object($asset)) {
+                    $assetsResponse[$asset->getAssetType()][] = $asset->getAssetURL();
+                }
+            }
+        }
+
         return [
             'key' => $this->getKey(),
             'label' => $this->getDisplayName(),
-            'element' => $this->renderSearchField(),
-            'data' => $this->data
+            'element' => $field,
+            'data' => $this->data,
+            'assets' => $assetsResponse
         ];
     }
 
