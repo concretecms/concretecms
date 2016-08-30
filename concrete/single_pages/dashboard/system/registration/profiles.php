13326470c17e35433767eebe0d95c57d025df60d
Karl Dilkington <MrKarlDilkington@users.noreply.github.com>
2016-08-29 23:18:20 -0400

    make Public Profiles single page fieldless  (#4266)
    * Bootstrap grid elements are breaking fieldless inputs and missing form-group class
    
    * tabs to spaces

--- concrete/single_pages/dashboard/system/registration/profiles.php
+++ concrete/single_pages/dashboard/system/registration/profiles.php
@@ -1,69 +1,63 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 $token = \Core::make('token');
 ?>
-    <form method="post" id="public-profiles-form" action="<?php echo $view->url('/dashboard/system/registration/profiles', 'update_profiles')?>">
-		<?php
-        $token->output('update_profile');
-        ?>
 
-	<div class="container">
-    	<div class="row">
-    		<div class="col-sm-12">
-		    	<div class="form-group">
-		            <label id="optionsCheckboxes" for="public_profiles" class="control-label"><?php echo t('Profile Options')?></label>
-					      <div class="checkbox">
-					      	<label>
-					        <input type="checkbox" id="public_profiles" name="public_profiles" value="1" <?php  if ($public_profiles) {
-     ?> checked <?php 
- } ?> />
-					        <span><?php echo t('Enable public profiles.')?></span>
-						    </label>
-						 </div>
-			 	</div>
-			 	<div class="form-group">
-			 		<?php echo $form->label('gravatar_fallback', t('Fall Back To Gravatar')); ?>
-			 		<div class="checkbox">
-			 			<label>
-			 			<?php echo $form->checkbox('gravatar_fallback', 1, $gravatar_fallback); ?> <span><?php echo t('Use image from <a href="http://gravatar.com" target="_blank">gravatar.com</a> if the user has not uploaded one')?></span>
-				 		</label>
-			 		</div>
-			 	</div>
-			 </div>
-		</div>
-	 	<div class="row">
-	 	<div id="gravatar-options" class="col-sm-3">
-	 		<div class="form-group">
-		 		<?php echo $form->label('gravatar_max_level', t('Maximum Gravatar Rating')); ?>
-	 			<?php echo $form->select('gravatar_max_level', $gravatar_level_options, $gravatar_max_level); ?>
-			</div>
-			<div class="control-group">
-		 		<?php echo $form->label('gravatar_image_set', t('Gravatar Image Set')); ?>
-	 			<?php echo $form->select('gravatar_image_set', $gravatar_set_options, $gravatar_image_set); ?>
-		 	</div>
-	 	</div>
-	 </div>
-	</div>
+<form method="post" id="public-profiles-form" action="<?php echo $view->url('/dashboard/system/registration/profiles', 'update_profiles')?>">
+    <?php $token->output('update_profile'); ?>
 
-	<div class="ccm-dashboard-form-actions-wrapper">
-	<div class="ccm-dashboard-form-actions">
-		<button class="pull-right btn btn-primary" type="submit" ><?=t('Save')?></button>
-	</div>
-	</div>
+    <div class="form-group">
+        <label id="optionsCheckboxes" for="public_profiles" class="control-label"><?php echo t('Profile Options')?></label>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" id="public_profiles" name="public_profiles" value="1" <?php if ($public_profiles) { ?> checked <?php } ?>>
+                <span><?php echo t('Enable public profiles.')?></span>
+            </label>
+        </div>
+    </div>
 
+    <div class="form-group">
+        <?php echo $form->label('gravatar_fallback', t('Fall Back To Gravatar')); ?>
+        <div class="checkbox">
+            <label>
+                <?php echo $form->checkbox('gravatar_fallback', 1, $gravatar_fallback); ?>
+                <span><?php echo t('Use image from <a href="http://gravatar.com" target="_blank">gravatar.com</a> if the user has not uploaded one')?></span>
+            </label>
+        </div>
+    </div>
+
+    <div id="gravatar-options">
+        <div class="form-group">
+            <?php echo $form->label('gravatar_max_level', t('Maximum Gravatar Rating')); ?>
+            <?php echo $form->select('gravatar_max_level', $gravatar_level_options, $gravatar_max_level); ?>
+        </div>
+
+        <div class="form-group">
+            <?php echo $form->label('gravatar_image_set', t('Gravatar Image Set')); ?>
+            <?php echo $form->select('gravatar_image_set', $gravatar_set_options, $gravatar_image_set); ?>
+        </div>
+    </div>
+
+    <div class="ccm-dashboard-form-actions-wrapper">
+        <div class="ccm-dashboard-form-actions">
+            <button class="pull-right btn btn-primary" type="submit" ><?=t('Save')?></button>
+        </div>
+    </div>
 </form>
-<script type="text/javascript">
+
+<script>
 $(document).ready(function(){
-	$('#gravatar_fallback').change(function(){
-		if($(this).prop('checked') == true) {
-			$('#gravatar-options').css('display', 'block');
-		} else {
-			$('#gravatar-options').css('display', 'none');
-		}
-	})
-})
+    $('#gravatar_fallback').change(function(){
+        if($(this).prop('checked') == true) {
+            $('#gravatar-options').css('display', 'block');
+        } else {
+            $('#gravatar-options').css('display', 'none');
+        }
+    });
+});
 </script>
-<style type="text/css">
+
+<style>
 #gravatar-options {
-	display: <?php echo $gravatar_fallback ? 'block' : 'none'; ?>;
+    display: <?php echo $gravatar_fallback ? 'block' : 'none'; ?>;
 }
 </style>
