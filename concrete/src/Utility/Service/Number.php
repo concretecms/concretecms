64a4e7037127d4513c374d0b0c56c7c1551bce19
Michele Locati <michele@locati.it>
2016-09-02 18:24:13 +0200

    Simplify flexround and avoid potential warnings

--- concrete/src/Utility/Service/Number.php
+++ concrete/src/Utility/Service/Number.php
@@ -12,15 +12,7 @@ class Number
      */
     public function flexround($value)
     {
-        $v = explode('.', $value);
-        $p = 0;
-        for ($i = 0; $i < strlen($v[1]); ++$i) {
-            if (substr($v[1], $i, 1) > 0) {
-                $p = $i + 1;
-            }
-        }
-
-        return round($value, $p);
+        return (float) $value;
     }
 
     /**
