58d0a8040385a626881d402620e2f123917876f1
Michele Locati <michele@locati.it>
2016-09-06 09:51:06 +0200

    php-cs-fixer

--- concrete/src/User/Group/GroupList.php
+++ concrete/src/User/Group/GroupList.php
@@ -12,7 +12,7 @@ class GroupList extends DatabaseItemList
 {
     protected $includeAllGroups = false;
 
-    protected $autoSortColumns = array('gName', 'gID');
+    protected $autoSortColumns = ['gName', 'gID'];
 
     public function includeAllGroups()
     {
@@ -26,13 +26,13 @@ class GroupList extends DatabaseItemList
      */
     public function filterByKeywords($keywords)
     {
-        $expressions = array(
+        $expressions = [
             $this->query->expr()->like('g.gName', ':keywords'),
             $this->query->expr()->like('g.gDescription', ':keywords'),
-        );
+        ];
 
         $expr = $this->query->expr();
-        $this->query->andWhere(call_user_func_array(array($expr, 'orX'), $expressions));
+        $this->query->andWhere(call_user_func_array([$expr, 'orX'], $expressions));
         $this->query->setParameter('keywords', '%' . $keywords . '%');
     }
 
@@ -49,9 +49,9 @@ class GroupList extends DatabaseItemList
     {
         if (Config::get('concrete.permissions.model') != 'simple') {
             // there's gotta be a more reasonable way than this but right now i'm not sure what that is.
-            $excludeGroupIDs = array(GUEST_GROUP_ID, REGISTERED_GROUP_ID);
+            $excludeGroupIDs = [GUEST_GROUP_ID, REGISTERED_GROUP_ID];
             $db = Loader::db();
-            $r = $db->Execute('select gID from Groups where gID > ?', array(REGISTERED_GROUP_ID));
+            $r = $db->Execute('select gID from Groups where gID > ?', [REGISTERED_GROUP_ID]);
             while ($row = $r->FetchRow()) {
                 $g = Group::getByID($row['gID']);
                 $gp = new Permissions($g);
@@ -60,7 +60,7 @@ class GroupList extends DatabaseItemList
                 }
             }
             $this->query->andWhere(
-                $this->query->expr()->notIn('g.gId', array_map(array($db, 'quote'), $excludeGroupIDs))
+                $this->query->expr()->notIn('g.gId', array_map([$db, 'quote'], $excludeGroupIDs))
             );
         }
     }
