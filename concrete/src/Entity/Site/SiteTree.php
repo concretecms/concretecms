69434cab57270d37815d645390e90920f75142e3
Andrew Embler <andrew@concrete5.org>
2016-09-14 06:20:00 -0700

    Making site tree type extendable, some bug fixes

---
+++ concrete/src/Entity/Site/SiteTree.php
@@ -0,0 +1,47 @@
+<?php
+namespace Concrete\Core\Entity\Site;
+
+use Concrete\Core\Site\Tree\TreeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="treeType", type="string")
+ * @ORM\Table(
+ *     name="SiteTreeTrees"
+ * )
+ */
+class SiteTree extends Tree
+{
+
+    /**
+     * @var Tree
+     * @ORM\OneToOne(targetEntity="Site", inversedBy="tree")
+     * @ORM\JoinColumn(name="siteID", referencedColumnName="siteID")
+     **/
+    protected $site;
+
+    /**
+     * @return mixed
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
+     * @param mixed $site
+     */
+    public function setSite($site)
+    {
+        $this->site = $site;
+    }
+
+    public function getSiteType()
+    {
+        return $this->getSite()->getType();
+    }
+
+
+}
