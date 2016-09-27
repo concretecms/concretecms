ec684f6a82c3d55caf2523ac83a8b0d01b631f25
Michele Locati <michele@locati.it>
2016-09-27 15:21:18 +0200

    Avoid accessing undefined variable in Legacy\PageList::setBaseQuery

--- concrete/src/Legacy/PageList.php
+++ concrete/src/Legacy/PageList.php
@@ -445,6 +445,8 @@ class PageList extends DatabaseItemList {
 		if ($this->isIndexedSearch()) {
 			$db = Loader::db();
 			$ik = ', match(psi.cName, psi.cDescription, psi.content) against (' . $db->quote($this->indexedKeywords) . ') as cIndexScore ';
+		} else {
+		    $ik = '';
 		}
 
 		if (!$this->includeAliases) {
