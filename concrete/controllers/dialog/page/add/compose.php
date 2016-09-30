be8144bc7a97a6ce755d6ae5d0fccc064a24c53e
Michele Locati <michele@locati.it>
2016-09-30 09:39:50 +0200

    Avoid deprecated methods

--- concrete/controllers/dialog/page/add/compose.php
+++ concrete/controllers/dialog/page/add/compose.php
@@ -9,7 +9,6 @@ use Concrete\Core\Page\Type\Type;
 use Concrete\Core\View\DialogView;
 use Page;
 use Permissions;
-use Core;
 
 class Compose extends Controller
 {
@@ -18,7 +17,7 @@ class Compose extends Controller
     public function view($ptID, $cParentID)
     {
         $pagetype = Type::getByID($ptID);
-        $e = Core::make('error');
+        $e = $this->app->make('error');
         if (is_object($pagetype)) {
             $ptp = new Permissions($pagetype);
             if (!$ptp->canAddPageType()) {
@@ -50,7 +49,7 @@ class Compose extends Controller
 
     public function submit()
     {
-        $e = Core::make('error');
+        $e = $this->app->make('error');
         $pagetype = Type::getByID($this->request->request->get('ptID'));
         if (is_object($pagetype)) {
             $configuredTarget = $pagetype->getPageTypePublishTargetObject();
