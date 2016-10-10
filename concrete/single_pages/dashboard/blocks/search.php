d077724986bef3cec11605fdc00f8921043bb383
Andrew Embler <andrew@concrete5.org>
2016-10-10 12:32:19 -0700

    starting on block search

--- concrete/single_pages/dashboard/blocks/search.php
+++ concrete/single_pages/dashboard/blocks/search.php
@@ -1,23 +1,5 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-<?php
-$c = Page::getCurrentPage();
-$ocID = $c->getCollectionID();
-$fp = FilePermissions::getGlobal();
-$imageresize = Config::get('concrete.file_manager.restrict_uploaded_image_sizes');
-
-if ($fp->canAddFile() || $fp->canSearchFiles()) { ?>
-
-
-
 <div class="ccm-dashboard-content-full">
-<?php Loader::element('files/search', array('result' => $result))?>
+    <?php// View::element('blocks/search', array('result' => $result))?>
 </div>
-
-    <?php
-} else {
-    ?>
-	<p><?=t("You do not have access to the file manager.");
-    ?></p>
-<?php
-} ?>
