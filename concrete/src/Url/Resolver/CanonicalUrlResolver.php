040034a12a3e3b64ccbcb2e9bc3239b555de6589
Korvin Szanto <korvinszanto@gmail.com>
2016-10-11 18:02:34 -0700

    Use relative urls instead of the request url:

--- concrete/src/Url/Resolver/CanonicalUrlResolver.php
+++ concrete/src/Url/Resolver/CanonicalUrlResolver.php
@@ -91,14 +91,6 @@ class CanonicalUrlResolver implements UrlResolverInterface
                     }
                 }
             }
-        } else {
-            $host = $this->request->getHost();
-            $scheme = $this->request->getScheme();
-            if ($scheme && $host) {
-                $url = $url->setScheme($scheme)
-                    ->setHost($host)
-                    ->setPort($this->request->getPort());
-            }
         }
 
         if ($relative_path = $this->app['app_relative_path']) {
