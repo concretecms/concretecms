099266a3fbb25498f6ccad46b32d549b28c3f10f
Michele Locati <michele@locati.it>
2016-10-18 10:59:20 +0200

    Fix coding standard error
    It seems that phpcs misbehaves...

--- concrete/single_pages/dashboard/blocks/search.php
+++ concrete/single_pages/dashboard/blocks/search.php
@@ -1,5 +1,7 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <div class="ccm-dashboard-content-full">
-    <?php// View::element('blocks/search', array('result' => $result))?>
+    <?php
+        // View::element('blocks/search', array('result' => $result))
+    ?>
 </div>
