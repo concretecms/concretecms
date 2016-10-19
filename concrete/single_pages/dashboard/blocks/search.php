14cecd5e05d39e341954dfbe067c09ed8b3973e6
Andrew Embler <andrew@concrete5.org>
2016-10-19 15:06:37 -0700

    Removing unneeded dashboard files

--- concrete/single_pages/dashboard/blocks/search.php
+++
@@ -1,5 +0,0 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
-
-<div class="ccm-dashboard-content-full">
-    <?php// View::element('blocks/search', array('result' => $result))?>
-</div>
