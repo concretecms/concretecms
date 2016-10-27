e06a2b03619e6bfa572cf32dc826777a43da23b2
Markus Liechti <kaapiii@hotmail.com>
2016-10-27 23:59:53 +0200

    Move all test cases from ApplicationXXXMetadataDriverTests to ApplicationDriverTests, add mew tests amd code coverage, remove all ApplicationXXXMetadataDriverTests

--- tests/tests/Core/Database/EntityManager/Driver/ApplicationDriverTest.php
+++ tests/tests/Core/Database/EntityManager/Driver/ApplicationDriverTest.php
@@ -14,6 +14,7 @@ use Illuminate\Filesystem\Filesystem;
  */
 class ApplicationDriverTest extends \PHPUnit_Framework_TestCase
 {
+
     /**
      * @var \Concrete\Core\Application\Application
      */
@@ -25,6 +26,11 @@ class ApplicationDriverTest extends \PHPUnit_Framework_TestCase
     protected $configRepository;
 
     /**
+     * @var Illuminate\Filesystem\Filesystem; 
+     */
+    protected $filesystem;
+
+    /**
      * Setup
      */
     public function setUp()
@@ -33,6 +39,7 @@ class ApplicationDriverTest extends \PHPUnit_Framework_TestCase
         $this->app = Application::getFacadeApplication();
         $entityManagerConfigFactory = $this->app->make('Concrete\Core\Database\EntityManagerConfigFactory');
         $this->configRepository = $entityManagerConfigFactory->getConfigRepository();
+        $this->filesystem = new Filesystem();
     }
 
     /**
@@ -43,19 +50,17 @@ class ApplicationDriverTest extends \PHPUnit_Framework_TestCase
      * @covers ApplicationDriver::getDriver
      * @throws \Exception
      */
-    public function testGetDefaultDriver(){
-
-        $filesystem = new Filesystem();
-        if (!$filesystem->isWritable(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES)) {
+    public function testGetDefaultDriver()
+    {
+        // prepare
+        if (!$this->filesystem->isWritable(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES)) {
             throw new \Exception("Cannot write to the application/src directory for the testing purposes. Please check permissions!");
         }
-        if(!$filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES)){
-            $filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES);
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES);
         }
-        if(!$filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)){
-            $filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR .DIRNAME_ENTITIES);
-        }
-
+        
+        // test
         $reader = $this->app->make('orm/cachedAnnotationReader');
         $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
         $driver = $applicationDriver->getDriver();
@@ -63,15 +68,7 @@ class ApplicationDriverTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Doctrine\ORM\Mapping\Driver\AnnotationDriver', $driver);
         // check if it contains the correct reader
         // Doctrine doesn't provied a way of accessing the original reader in the cached reader
-        $this->assertEquals($reader, $driver->getReader());
-
-        if($filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES.DIRECTORY_SEPARATOR.DIRNAME_ENTITIES)){
-            $filesystem->deleteDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES.DIRECTORY_SEPARATOR.DIRNAME_ENTITIES);
-        }
-        if($filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML)){
-            $filesystem->deleteDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML);
-        }
-
+        $this->assertEquals($reader, $driver->getReader(), 'The AnnotationReader is not wrapped with a CachedReader.');
     }
 
     /**
@@ -79,68 +76,259 @@ class ApplicationDriverTest extends \PHPUnit_Framework_TestCase
      *
      * @covers ApplicationDriver::getDriver
      */
-    public function testFailingGetDefaultDriverWithNoEntityFolder(){
+    public function testFailingGetDefaultDriverWithNoEntityDirectory()
+    {
         $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
         $driver = $applicationDriver->getDriver();
         // this case doesn't return a driver
         $this->assertNull($driver);
     }
 
+    /**
+     * Test application legacy driver
+     * - empty CONFIG_ORM_METADATA_APPLICATION config setting
+     * - a present application/src folder
+     * - and config 'app.enable_legacy_src_namespace' = true
+     *
+     * @covers ApplicationDriver::getDriver
+     * @throws \Exception
+     */
+    public function testGetLegacyDriver()
+    {
+        // prepare
+        if (!$this->filesystem->isWritable(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES)) {
+            throw new \Exception("Cannot write to the application/src directory for the testing purposes. Please check permissions!");
+        }
+        $this->configRepository->set('app.enable_legacy_src_namespace', true);
 
-
-    public function testGetLegacyDriver(){
-
-
-    }
-    
-    public function testGetXMLDriver(){
+        // test
+        $reader = $this->app->make('orm/cachedSimpleAnnotationReader');
+        $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
+        $driver = $applicationDriver->getDriver();
+        // check if its the correct driver
+        $this->assertInstanceOf('Doctrine\ORM\Mapping\Driver\AnnotationDriver', $driver);
+        // check if it contains the correct reader
+        // Doctrine doesn't provied a way of accessing the original reader in the cached reader
+        $this->assertEquals($reader, $driver->getReader(), 'The SimpleAnnotationReader is not wrapped with a CachedReader.');
         
     }
-
-    public function testGetYMLDriver(){
-
+    
+    /**
+     * Test application with xml driver
+     * - CONFIG_ORM_METADATA_APPLICATION = xml
+     * - a existing application/src/Entity folder
+     * - a existing application/config/xml 
+     * 
+     * @covers ApplicationDriver::getDriver
+     * @throws \Exception
+     */
+    public function testGetXMLDriver()
+    {
+        // prepare
+        if (!$this->filesystem->isWritable(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CONFIG)) {
+            throw new \Exception("Cannot write to the application/config directory for the testing purposes. Please check permissions!");
+        }
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_XML)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_XML);
+        }
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES);
+        }
+        $this->configRepository->set(CONFIG_ORM_METADATA_APPLICATION, 'xml');
+        
+        // test
+        $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
+        $driver = $applicationDriver->getDriver();
+        $this->assertEquals('xml', $this->configRepository->get(CONFIG_ORM_METADATA_APPLICATION));
+        $this->assertTrue($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES,
+                'application/src/Entities doesn\'t exist.'));
+        $this->assertInstanceOf('\Doctrine\ORM\Mapping\Driver\XmlDriver', $driver);
+        // Test if the driver contains the default lookup path
+        $driverPaths = $driver->getLocator()->getPaths();
+        $this->assertEquals(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_XML, $driverPaths[0]);
     }
-
-
-
-    public function dataProviderGetDriver(){
-
+    
+    /**
+     * Failing test for XMLDriver with missing application/config/xml directory
+     * 
+     * @covers ApplicationDriver::getDriver
+     */
+    public function testFailingGetXMLDriverWithNoConfigXMLDirectory(){
+        $this->configRepository->set(CONFIG_ORM_METADATA_APPLICATION, 'xml');
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES);
+        }
+        
+        $this->assertFalse($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_XML));
+        $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
+        $driver = $applicationDriver->getDriver();
+        // this case doesn't return a driver
+        $this->assertInstanceOf('Doctrine\ORM\Mapping\Driver\AnnotationDriver', $driver);
+    }
+    
+    /**
+     * Test application with xml driver
+     * - CONFIG_ORM_METADATA_APPLICATION = yml || yaml
+     * - a existing application/src/Entity folder
+     * - a existing application/config/yaml 
+     * 
+     * @dataProvider dataProviderTestGetYMLDriver
+     * @covers ApplicationDriver::getDriver
+     * 
+     * @param string $setting
+     * 
+     * @throws \Exception
+     */
+    public function testGetYMLDriver($setting)
+    {
+                // prepare
+        if (!$this->filesystem->isWritable(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CONFIG)) {
+            throw new \Exception("Cannot write to the application/config directory for the testing purposes. Please check permissions!");
+        }
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML);
+        }
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES);
+        }
+        $this->configRepository->set(CONFIG_ORM_METADATA_APPLICATION, $setting);
+        
+        // test
+        $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
+        $driver = $applicationDriver->getDriver();
+        $this->assertEquals($setting, $this->configRepository->get(CONFIG_ORM_METADATA_APPLICATION));
+        $this->assertTrue($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES,
+                'application/src/Entities doesn\'t exist.'));
+        $this->assertInstanceOf('\Doctrine\ORM\Mapping\Driver\YamlDriver', $driver);
+        // Test if the driver contains the default lookup path
+        $driverPaths = $driver->getLocator()->getPaths();
+        $this->assertEquals(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML, $driverPaths[0]);
+    }
+    
+    /**
+     * Failing test for XMLDriver with missing application/config/yaml directory
+     * 
+     * @dataProvider dataProviderTestGetYMLDriver
+     * @covers ApplicationDriver::getDriver
+     * 
+     * @param string $setting
+     */
+    public function testFailingGetYMLDriverWithNoConfigYamlDirectory($setting){
+        $this->configRepository->set(CONFIG_ORM_METADATA_APPLICATION, $setting);
+        if (!$this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)) {
+            $this->filesystem->makeDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES);
+        }
+        
+        $this->assertFalse($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML));
+        $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
+        $driver = $applicationDriver->getDriver();
+        // this case doesn't return a driver
+        $this->assertInstanceOf('Doctrine\ORM\Mapping\Driver\AnnotationDriver', $driver);
+    }
+    
+    /**
+     * Data provider
+     * 
+     * @return array
+     */
+    public function dataProviderTestGetYMLDriver()
+    {
+        return array(
+            array('yml'),
+            array('yaml'),
+        );
     }
 
     /**
      * Test namespace
      *
      * @dataProvider dataProviderGetNamespace
-     *
-     * @param array $settings
+     * @covers ApplicationDriver::getNamespace
+     * 
+     * @param boolean $isLegacy
+     * @param string $namespace
      */
-    public function testGetNamespace($isLegacy, $namespace){
-        $config = $this->app->make('config');
-        if($isLegacy){
-            $config->save('app.enable_legacy_src_namespace', true);
+    public function testGetNamespace($isLegacy, $namespace)
+    {
+        if ($isLegacy) {
+            $this->configRepository->save('app.enable_legacy_src_namespace', true);
         }
-        
+
         $applicationDriver = new ApplicationDriver($this->configRepository, $this->app);
         $this->assertEquals($namespace, $applicationDriver->getNamespace());
 
-        if($isLegacy){
-            $config->save('app.enable_legacy_src_namespace', false);
+        if ($isLegacy) {
+            $this->configRepository->save('app.enable_legacy_src_namespace', false);
         }
-
     }
 
     /**
-     * Test default and legacy namespaces of application entites
+     * Test the default and the legacy namespaces of application entites
      *
      * @return array
      */
     public function dataProviderGetNamespace()
     {
         return array(
-            array('isLegacy'=> true, 'namespace' => 'Application\Src'),
-            array('isLegacy'=> false, 'namespace' => 'Application\Entity'),
+            array('isLegacy' => true, 'namespace' => 'Application\Src'),
+            array('isLegacy' => false, 'namespace' => 'Application\Entity'),
         );
     }
-
-
+    
+    /**
+     * Clean up after each tests
+     */
+    public function tearDown(){
+        $this->cleanupFolderSystem();
+        $this->cleanupConfig();
+    }
+    
+    /**
+     * Clean up if a Exception is thrown
+     */
+    protected function onNotSuccessfulTest($e)
+    {
+        $this->cleanupFolderSystem();
+        $this->cleanupConfig();
+    }
+    
+    
+    /**
+     * Clean up altern folder system
+     */
+    public function cleanupFolderSystem(){
+        if ($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES)) {
+            $this->filesystem->deleteDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . DIRNAME_CLASSES . DIRECTORY_SEPARATOR . DIRNAME_ENTITIES);
+        }
+        if ($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_XML)) {
+            $this->filesystem->deleteDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_XML);
+        }
+        if ($this->filesystem->isDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML)) {
+            $this->filesystem->deleteDirectory(DIR_APPLICATION . DIRECTORY_SEPARATOR . REL_DIR_METADATA_YAML);
+        }
+    }
+    
+    /**
+     * Clean up altered config values
+     */
+    public function cleanupConfig(){
+        $this->configRepository->save('app.enable_legacy_src_namespace', false);
+        $this->configRepository->set(CONFIG_ORM_METADATA_APPLICATION, '');
+    }
+    
+    /**
+     * Example of mocking the concrete config repository
+     */
+    /*protected function getMockConcreteConfigRepository($setting){
+        $configRepoStub = $this->getMockBuilder('Concrete\Core\Config\Repository\Repository')
+                                ->disableOriginalConstructor()
+                                ->getMock();
+        $configRepoStub->method('get')
+            ->will($this->onConsecutiveCalls(
+                false,
+                array(),
+                false,
+                $setting
+                ));
+    }*/
 }
