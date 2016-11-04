d69ae41ed3d784278f169b9551a381ab0f814d3d
Andrew Embler <andrew@concrete5.org>
2016-11-04 13:02:35 -0700

    fixing legacy attributes
    Former-commit-id: 679b00f202c1c65200c2b18d3c82bccbff348294
    Former-commit-id: b4e22261b8a04e3bea670b23b1d5671ef7ff8ef7

--- concrete/src/Entity/Attribute/Value/LegacyValue.php
+++ concrete/src/Entity/Attribute/Value/LegacyValue.php
@@ -3,12 +3,6 @@ namespace Concrete\Core\Entity\Attribute\Value;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\Table(
- *     name="LegacyAttributeValues"
- * )
- */
 class LegacyValue extends AbstractValue
 {
 
