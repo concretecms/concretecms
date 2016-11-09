bb3de5c7490d598cf610e49af16ce57d0769a991
Andrew Embler <andrew@concrete5.org>
2016-11-09 07:12:26 -0800

    tweaks for interim upgrades

--- concrete/src/Entity/Attribute/Key/Settings/TextareaSettings.php
+++ concrete/src/Entity/Attribute/Key/Settings/TextareaSettings.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 class TextareaSettings extends Settings
 {
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     protected $akTextareaDisplayMode = '';
 
