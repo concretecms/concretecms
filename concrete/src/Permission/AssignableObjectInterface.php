4c1055244c833f0fe3ac587db7db4aabe1d633db
Andrew Embler <andrew@concrete5.org>
2016-11-09 14:29:37 -0800

    making workflows packageable
    Former-commit-id: a102cad5c76a3f095c39571e93ccba8daaba1047
    Former-commit-id: 1dc0f23660ebaf7b8e0484c45cd38feaa55ceb92

--- concrete/src/Permission/AssignableObjectInterface.php
+++ concrete/src/Permission/AssignableObjectInterface.php
@@ -6,4 +6,5 @@ use Concrete\Core\Permission\Key\Key;
 interface AssignableObjectInterface
 {
     function assignPermissions($userOrGroup, $permissions, $accessType = Key::ACCESS_TYPE_INCLUDE);
+
 }
