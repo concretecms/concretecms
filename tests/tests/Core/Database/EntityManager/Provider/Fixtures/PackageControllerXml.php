f2dab87a03309bf104d151066d8aee0ce9126b68
Markus Liechti <kaapiii@hotmail.com>
2016-11-14 21:46:13 +0100

    fix comments & psr2 format
    Former-commit-id: 7b29e01eddbf183e920ed0ee65357c4c880bcd7c
    Former-commit-id: 2fca49dba1ba519ee5383fc076192cae35b6fc09

--- tests/tests/Core/Database/EntityManager/Provider/Fixtures/PackageControllerXml.php
+++ tests/tests/Core/Database/EntityManager/Provider/Fixtures/PackageControllerXml.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Concrete\Tests\Core\Database\EntityManager\Provider\Fixtures;
 
 use Concrete\Core\Database\EntityManager\Provider\XmlProvider;
@@ -8,33 +9,37 @@ use Concrete\Core\Database\EntityManager\Provider\ProviderInterface;
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 /**
- * Controller test addon - testing metadatadriver with legacy annotation driver
+ * Controller test addon - testing xml metadatadriver
  *
  * @author @author Markus Liechti <markus@liechti.io>
  */
-class PackageControllerXml extends Package implements ProviderInterface{
+class PackageControllerXml extends Package implements ProviderInterface
+{
 
     protected $pkgHandle = 'test_metadatadriver_xml';
     protected $appVersionRequired = '5.8.0';
     protected $pkgVersion = '0.0.1';
 
-    public function getPackageDescription() {
+    public function getPackageDescription()
+    {
         return t("Test addon registers entities via the xml driver");
     }
 
-    public function getPackageName(){
+    public function getPackageName()
+    {
         return t("Test addon - uses xml driver");
     }
-    
+
     /**
      * Return customized metadata driver wrapped in a XMLProvider for doctrine orm
      * Path: {package}/config/xml
      * 
      * @return XmlProvider
      */
-    public function getDrivers(){
-        
+    public function getDrivers()
+    {
         $xmlProvider = new XmlProvider($this);
         return $xmlProvider->getDrivers();
     }
+
 }
