c0dc168ff7cdaf129ca517197f202f2f3f6f6327
Korvin Szanto <korvinszanto@gmail.com>
2016-11-13 22:03:24 -0800

    Avoid StorageLocation naming collision
    Former-commit-id: 39b3cf044edc66886ff582761c3ed90b3b3c292b
    Former-commit-id: fb94318967fe55fbf1276dd0bf1bedef12483009

--- concrete/src/File/StorageLocation/StorageLocationFactory.php
+++ concrete/src/File/StorageLocation/StorageLocationFactory.php
@@ -3,7 +3,7 @@
 namespace Concrete\Core\File\StorageLocation;
 
 use Concrete\Core\Database\DatabaseManagerORM;
-use Concrete\Core\Entity\File\StorageLocation\StorageLocation;
+use Concrete\Core\Entity\File\StorageLocation\StorageLocation as StorageLocationEntity;
 use Concrete\Core\File\StorageLocation\Configuration\ConfigurationInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -27,11 +27,11 @@ class StorageLocationFactory
      * Create a new StorageLocation, pass this to StorageLocationFactory->persist() to store this location
      * @param \Concrete\Core\File\StorageLocation\Configuration\ConfigurationInterface $configuration
      * @param $name
-     * @return StorageLocation
+     * @return StorageLocationEntity
      */
     public function create(ConfigurationInterface $configuration, $name)
     {
-        $location = new StorageLocation();
+        $location = new StorageLocationEntity();
         $location->setConfigurationObject($configuration);
         $location->setName($name);
 
@@ -40,13 +40,13 @@ class StorageLocationFactory
 
     /**
      * Store a created storage location to the database
-     * @param StorageLocation $storageLocation
-     * @return StorageLocation The persisted location, may not be the same as the passed in object
+     * @param StorageLocationEntity $storageLocation
+     * @return StorageLocationEntity The persisted location, may not be the same as the passed in object
      */
-    public function persist(StorageLocation $storageLocation)
+    public function persist(StorageLocationEntity $storageLocation)
     {
         return $this->entityManager->transactional(function (EntityManagerInterface $em) use ($storageLocation) {
-            $em->createQueryBuilder()->update(StorageLocation::class, 'l')->set('fslIsDefault', false);
+            $em->createQueryBuilder()->update(StorageLocationEntity::class, 'l')->set('fslIsDefault', false);
             $em->persist($storageLocation);
 
             return $storageLocation;
@@ -56,46 +56,46 @@ class StorageLocationFactory
     /**
      * Fetch a storage location by its ID
      * @param int $id
-     * @return null|StorageLocation
+     * @return null|StorageLocationEntity
      * @throws \Doctrine\ORM\ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      * @throws \Doctrine\ORM\TransactionRequiredException
      */
     public function fetchByID($id)
     {
-        return $this->entityManager->find(StorageLocation::class, (int)$id);
+        return $this->entityManager->find(StorageLocationEntity::class, (int)$id);
     }
 
     /**
      * Fetch a storage location by its name
      * @param string $name
-     * @return null|StorageLocation
+     * @return null|StorageLocationEntity
      * @throws \Doctrine\ORM\ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      * @throws \Doctrine\ORM\TransactionRequiredException
      */
     public function fetchByName($name)
     {
-        return $this->entityManager->getRepository(StorageLocation::class, 'l')->findBy(['fslName' => $name]);
+        return $this->entityManager->getRepository(StorageLocationEntity::class, 'l')->findBy(['fslName' => $name]);
     }
 
     /**
      * Fetch a list of storage locations
-     * @return StorageLocation[]
+     * @return StorageLocationEntity[]
      */
     public function fetchList()
     {
-        $repository = $this->entityManager->getRepository(StorageLocation::class);
+        $repository = $this->entityManager->getRepository(StorageLocationEntity::class);
         return $repository->findBy([], ['fslID' => 'asc']);
     }
 
     /**
      * Fetch the default storage location
-     * @return StorageLocation
+     * @return StorageLocationEntity
      */
     public function fetchDefault()
     {
-        $repository = $this->entityManager->getRepository(StorageLocation::class);
+        $repository = $this->entityManager->getRepository(StorageLocationEntity::class);
         return $repository->findOneBy([
             'fslIsDefault' => true
         ]);
