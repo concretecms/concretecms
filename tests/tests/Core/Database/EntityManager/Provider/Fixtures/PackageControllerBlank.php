304fca9cbe090c6a6e59ab847e60dadc6cf315a4
Markus Liechti <kaapiii@hotmail.com>
2016-11-14 22:38:55 +0100

    add more test for ...Core\Database\EntityManager.... - DriverTest - PackageProviderFactoryTest - DefaultPackageProviderTest
    Former-commit-id: 15846b855d2c238d60a45390e2683bbfc859fb0c
    Former-commit-id: 01d8535b05292892c56e6bad7cd09e5b9b461c7a

---
+++ tests/tests/Core/Database/EntityManager/Provider/Fixtures/PackageControllerBlank.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Concrete\Tests\Core\Database\EntityManager\Provider\Fixtures;
+
+use Concrete\Core\Package\Package;
+use Concrete\Core\Application\Application;
+
+/**
+ * Controller test addon - testing metadatadriver with default annotation driver
+ *
+ * @author Markus Liechti <markus@liechti.io>
+ */
+class PackageControllerBlank extends Package
+{
+
+    protected $pkgHandle = 'test_metadatadriver_default';
+    protected $appVersionRequired = '5.8.0';
+    protected $pkgVersion = '0.0.1';
+
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+    }
+
+    public function getPackageDescription()
+    {
+        return t("Test addon registers entities via standard annotation driver");
+    }
+
+    public function getPackageName()
+    {
+        return t("Test addon - uses standard annotation driver");
+    }
+
+}
