7ec769904ea25590f24cfbb3d1c616b97b371e1b
Andrew Embler <andrew@concrete5.org>
2016-11-14 09:21:38 -0800

    Fix #4621
    Former-commit-id: 621d42753a4ee42add81dba274c7b92a2e4a196c
    Former-commit-id: 77b0ccc99a42dcf31f65da7efe0dca77a02bc981

--- concrete/src/Legacy/FilePermissions.php
+++ concrete/src/Legacy/FilePermissions.php
@@ -2,6 +2,7 @@
 namespace Concrete\Core\Legacy;
 
 use Concrete\Core\File\Filesystem;
+use Concrete\Core\Tree\Node\Type\FileFolder;
 use FileSet;
 use Permissions;
 
@@ -13,7 +14,14 @@ final class FilePermissions
     public static function getGlobal()
     {
         $filesystem = new Filesystem();
-        $fsp = new Permissions($filesystem->getRootFolder());
+        $folder = $filesystem->getRootFolder();
+        if (!is_object($folder)) {
+            // We don't have the root folder yet for some reason (maybe this is
+            // the beginning of an upgrade?)
+            $folder = new FileFolder();
+            // Just pass in a stub one, it shouldn't matter
+        }
+        $fsp = new Permissions($folder);
         return $fsp;
     }
 }
