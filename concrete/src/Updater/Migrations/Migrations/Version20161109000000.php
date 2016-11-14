4fc665f348e4d452be14070eed942080129bef59
Andrew Embler <andrew@concrete5.org>
2016-11-14 13:11:41 -0800

    Merging migrations
    Former-commit-id: c93c80ef85697f8338aea6eb3fe9b0d8bf292c26
    Former-commit-id: 15fa59cd348c627fd514bfd43d63402d22ba8a0a

--- concrete/src/Updater/Migrations/Migrations/Version20161109000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20161109000000.php
@@ -8,6 +8,13 @@ class Version20161109000000 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
+        $this->version->getConfiguration()->getOutputWriter()->write(t('Updating tables found in doctrine xml...'));
+        // Update tables that still exist in db.xml
+        \Concrete\Core\Database\Schema\Schema::refreshCoreXMLSchema(array(
+            'FileImageThumbnailPaths'
+        ));
+
+        $this->version->getConfiguration()->getOutputWriter()->write(t('Adding fields to workflow and select tables...'));
         if (!$schema->getTable('Workflows')->hasColumn('pkgID')) {
             $schema->getTable('Workflows')->addColumn('pkgID', 'integer', array(
                 'unsigned' => true, 'notnull' => true, 'default' => 0
