7ee5fc9f8f8bd45a92554c40a0d52cdd2436ea56
Bernd Matzner <bernd@matznermatzner.de>
2016-11-17 18:29:47 +0100

    Adjust connect iframe height so that button sign in button is visible

--- concrete/single_pages/dashboard/extend/connect.php
+++ concrete/single_pages/dashboard/extend/connect.php
@@ -13,6 +13,6 @@
 		div.ccm-pane-body div.ccm-error { padding:15px 20px; };
 		</style>
 		<?php
-        echo $mi->getMarketplaceFrame('100%', '300', false, h($startStep));
+        echo $mi->getMarketplaceFrame('100%', '400', false, h($startStep));
     }
 ?>
