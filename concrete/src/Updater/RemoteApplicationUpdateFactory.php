14d5dffc0f947a8b4126d91d80fb715db11cf392
Michele Locati <michele@locati.it>
2016-11-23 12:08:56 +0100

    If the remote update info is not retrieved, correctly return null

--- concrete/src/Updater/RemoteApplicationUpdateFactory.php
+++ concrete/src/Updater/RemoteApplicationUpdateFactory.php
@@ -7,12 +7,16 @@ class RemoteApplicationUpdateFactory
     {
         $o = json_decode($json);
 
-        $update = new RemoteApplicationUpdate();
-        $update->setVersion($o->version);
-        $update->setIdentifier($o->identifier);
-        $update->setNotes($o->notes);
-        $update->setDate($o->date);
-        $update->setDirectDownloadURL($o->direct_download_url);
+        if (is_object($o)) {
+            $update = new RemoteApplicationUpdate();
+            $update->setVersion($o->version);
+            $update->setIdentifier($o->identifier);
+            $update->setNotes($o->notes);
+            $update->setDate($o->date);
+            $update->setDirectDownloadURL($o->direct_download_url);
+        } else {
+            $update = null;
+        }
 
         return $update;
     }
