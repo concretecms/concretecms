524589aa70b146002e14d0afc9a7a7da87678972
Michele Locati <michele@locati.it>
2016-12-01 18:18:52 +0100

    Add curl helper

--- concrete/src/Http/HttpServiceProvider.php
+++ concrete/src/Http/HttpServiceProvider.php
@@ -6,6 +6,7 @@ use Concrete\Core\Http\Middleware\DelegateInterface;
 use Concrete\Core\Http\Middleware\MiddlewareDelegate;
 use Concrete\Core\Http\Middleware\MiddlewareStack;
 use Concrete\Core\Http\Middleware\StackInterface;
+use Zend\Http\Client;
 
 class HttpServiceProvider extends ServiceProvider
 {
@@ -44,6 +45,46 @@ class HttpServiceProvider extends ServiceProvider
 
         // Response Factory
         $this->app->bind(ResponseFactoryInterface::class, ResponseFactory::class);
-    }
 
+        $this->app->bind('curl', function (\Concrete\Core\Application\Application $app, array $arguments) {
+            $config = $app->make('config');
+            $options = [
+                'adapter' => Curl::class,
+            ];
+            $proxyHost = $config->get('concrete.proxy.host');
+            if ($proxyHost) {
+                $options['proxyhost'] = $proxyHost;
+                $proxyPort = $config->get('concrete.proxy.port');
+                if ($proxyPort && is_numeric($proxyPort)) {
+                    $options['proxyport'] = $proxyPort;
+                }
+                $proxyUser = $config->get('concrete.proxy.user');
+                if (is_string($proxyUser) && $proxyUser !== '') {
+                    $options['proxyuser'] = $proxyUser;
+                    $options['proxypass'] = (string) $config->get('concrete.proxy.password');
+                }
+            }
+            $options['sslverifypeer'] = (bool) $config->get('app.curl.verifyPeer');
+            $connectionTimeout = $config->get('app.curl.connectionTimeout');
+            if (is_numeric($connectionTimeout)) {
+                $options['connectiontimeout'] = (int) $connectionTimeout;
+            }
+            $responseTimeout = $config->get('app.curl.responseTimeout');
+            if (is_numeric($responseTimeout)) {
+                $options['responsetimeout'] = (int) $responseTimeout;
+            }
+            $client = new Client(array_shift($arguments), $options);
+            $curlAdapter = $client->getAdapter();
+            $caInfo = $config->get('app.curl.caInfo');
+            if (is_string($caInfo) && $caInfo !== '') {
+                $curlAdapter->setCurlOption(CURLOPT_CAINFO, $caInfo);
+            }
+            $caPath = $config->get('app.curl.caPath');
+            if (is_string($caPath) && $caPath !== '') {
+                $curlAdapter->setCurlOption(CURLOPT_CAPATH, $caPath);
+            }
+
+            return $client;
+        });
+    }
 }
