9352d1c81cd6c44e651e59a09dd1784d47e6ce4d
Hamed <darragihamed@gmail.com>
2016-12-03 10:23:06 +0100

    Autorotate image on upload based on Exif Metadata

---
+++ concrete/src/File/ImportProcessor/AutorotateImageProcessor.php
@@ -0,0 +1,32 @@
+<?php
+namespace Concrete\Core\File\ImportProcessor;
+
+use Concrete\Core\Entity\File\Version;
+use Concrete\Core\File\Type\Type as FileType;
+use Concrete\Core\Support\Facade\Image;
+use Imagine\Filter\Basic\Autorotate;
+use Imagine\Filter\Transformation;
+use Imagine\Image\Metadata\ExifMetadataReader;
+
+class AutorotateImageProcessor implements ProcessorInterface
+{
+    public function shouldProcess(Version $version)
+    {
+        return function_exists('exif_read_data')
+                && $version->getTypeObject()->getGenericType() == FileType::T_IMAGE;
+    }
+
+    public function process(Version $version)
+    {
+        $fi = \Core::make('helper/file');
+        $ext = $fi->getExtension($version->getFileName());
+
+        $fr = $version->getFileResource();
+        $imagine = Image::getFacadeRoot()->setMetadataReader(new ExifMetadataReader());
+        $image = $imagine->load($fr->read());
+        $transformation = new Transformation($imagine);
+        $transformation->applyFilter($image, new Autorotate());
+
+        $version->updateContents($image->get($ext));
+    }
+}
