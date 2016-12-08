d5d1dbd5b5acccb2a8d959faa5fdfce587e5cb0b
Andrew Embler <andrew@concrete5.org>
2016-12-08 08:45:47 -0800

    upping version number

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -7,8 +7,8 @@ return array(
      *
      * @var string
      */
-    'version' => '8.0.1',
-    'version_installed' => '8.0.1',
+    'version' => '8.0.2',
+    'version_installed' => '8.0.2',
     'version_db' => '20161203000000', // the key of the latest database migration
 
     /*
