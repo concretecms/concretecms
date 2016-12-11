64759d850e97b296b10d56b801d5dd9a0ccff96d
Nour Akalay <mnakalay@users.noreply.github.com>
2016-12-11 19:08:54 +0000

    Update BlockView.php

--- concrete/src/Block/View/BlockView.php
+++ concrete/src/Block/View/BlockView.php
@@ -106,7 +106,7 @@ class BlockView extends AbstractView
                     urlencode($this->area->getAreaHandle()),
                     $this->blockType->getBlockTypeID(),
                     $task,
-                    'blockAdd'
+                    "blockAdd"
                 );
 
                 return call_user_func_array(array('\URL', 'to'), $arguments);
@@ -124,7 +124,7 @@ class BlockView extends AbstractView
                         urlencode($this->area->getAreaHandle()),
                         $b->getBlockID(),
                         $task,
-                        'blockEdit'
+                        "blockEdit"
                     );
 
                     return call_user_func_array(array('\URL', 'to'), $arguments);
