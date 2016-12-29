cb29896ef2ca1a5634cd4533ff6a710d4a4dec10
MrKarlDilkington <mrkarldilkington@outlook.com>
2016-12-29 00:50:39 -0500

    update the tables and add missing button style - switch to ccm-search-results-table.compact-results from table-striped - add missing spans - add missing thead - add missing tbody

--- concrete/single_pages/dashboard/users/points/actions.php
+++ concrete/single_pages/dashboard/users/points/actions.php
@@ -10,17 +10,17 @@
     ?>
     <div class="row">
         <div class="col-md-12">
-    
-            <?php 
+
+            <?php
                 echo $form->hidden('upaID', $upaID);
-    ?>               
-        	
+    ?>
+
         	<div class="checkbox">
                 <label>
                     <?=$form->checkbox('upaIsActive', 1, ($upaIsActive == 1 || (!$upaID)))?> <?=t('Enabled')?>
                 </label>
             </div>
-	
+
         	<div class="form-group">
         	    <?=$form->label('upaHandle', t('Action Handle'));
     ?>
@@ -35,7 +35,7 @@
     ?>
         		</div>
         	</div>
-	
+
         	<div class="form-group">
         	    <?=$form->label('upaName', t('Action Name'));
     ?>
@@ -44,7 +44,7 @@
     ?>
         		</div>
         	</div>
-	
+
         	<div class="form-group">
                 <?=$form->label('upaDefaultPoints', t('Default Points'));
     ?>
@@ -53,7 +53,7 @@
     ?>
         		</div>
         	</div>
-	
+
         	<div class="form-group">
         	    <?=$form->label('gBadgeID', t('Badge Associated'));
     ?>
@@ -69,7 +69,7 @@
         $label = t('Update Action');
     }
     ?>
-    
+
             <div class="ccm-dashboard-form-actions-wrapper">
                 <div class="ccm-dashboard-form-actions">
                     <a href="<?=$view->url('/dashboard/users/points/actions')?>" class="btn btn-default pull-left"><?=t('Back to List')?></a>
@@ -78,14 +78,14 @@
             </div>
         </div>
     </div>
-</form>		
-<?php 
+</form>
+<?php
 } else {
-    ?>	
+    ?>
 	<div class="ccm-dashboard-header-buttons">
 	    <a href="<?=$view->action('add')?>" class="btn btn-primary"><?=t('Add Action')?></a>
 	</div>
-	
+
 	<?php
         if (!$mode) {
             $mode = $_REQUEST['mode'];
@@ -94,59 +94,63 @@
     $keywords = $_REQUEST['keywords'];
 
     if (count($actions) > 0) {
-        ?>	
-			<table border="0" cellspacing="0" cellpadding="0" class="table table-striped">
-    			<tr>
-    				<th><?=t("Active")?></th>
+        ?>
+        <div class="table-responsive">
+			<table class="ccm-search-results-table compact-results">
+    			<thead>
+    				<th><span><?=t("Active")?></span></th>
     				<th class="<?=$actionList->getSearchResultsClass('upaName')?>"><a href="<?=$actionList->getSortByURL('upaName', 'asc')?>"><?=t('Action Name')?></a></th>
     				<th class="<?=$actionList->getSearchResultsClass('upaHandle')?>"><a href="<?=$actionList->getSortByURL('upaHandle', 'asc')?>"><?=t('Action Handle')?></a></th>
     				<th class="<?=$actionList->getSearchResultsClass('upaDefaultPoints')?>"><a href="<?=$actionList->getSortByURL('upaDefaultPoints', 'asc')?>"><?=t('Default Points')?></a></th>
     				<th class="<?=$actionList->getSearchResultsClass('upaBadgeGroupID')?>"><a href="<?=$actionList->getSortByURL('upaBadgeGroupID', 'asc')?>"><?=t('Group')?></a></th>
     				<th></th>
-    			</tr>
-    			
-        		<?php 
-                foreach ($actions as $upa) {
-                    ?>
-        		<tr class="">
-        			<td style="text-align: center"><?php if ($upa['upaIsActive']) {
-    ?><i class="fa fa-check"></i><?php 
-}
-                    ?></td>
-        			<td><?=h($upa['upaName'])?></td>
-        			<td><?=h($upa['upaHandle'])?></td>
-        			<td><?=number_format($upa['upaDefaultPoints'])?></td>
-        			<td><?php echo h($upa['gName']);
-                    ?></td>
-        			<td style="text-align: right">
-                        <?php
-                        $delete_url = \League\Url\Url::createFromUrl($view->action('delete', $upa['upaID']));
-                    $delete_url = $delete_url->setQuery(array(
-                            'ccm_token' => \Core::make('helper/validation/token')->generate('delete_action'),
-                        ));
-                    ?>
-        			    <a href="<?=$view->action($upa['upaID'])?>" class="btn btn-sm btn-default"><?=t('Edit')?></a>
-        			    <a href="<?=$delete_url?>" class="btn btn-sm btn-danger"><?=t('Delete')?></a>
-        			</td>
-        		</tr>
-        		<?php 
-                }
-        ?>
-		</table>
-		<?php 
+    			</thead>
+
+                <tbody>
+            		<?php
+                    foreach ($actions as $upa) {
+                        ?>
+            		<tr class="">
+            			<td style="text-align: center"><?php if ($upa['upaIsActive']) {
+        ?><i class="fa fa-check"></i><?php
+    }
+                        ?></td>
+            			<td><?=h($upa['upaName'])?></td>
+            			<td><?=h($upa['upaHandle'])?></td>
+            			<td><?=number_format($upa['upaDefaultPoints'])?></td>
+            			<td><?php echo h($upa['gName']);
+                        ?></td>
+            			<td style="text-align: right">
+                            <?php
+                            $delete_url = \League\Url\Url::createFromUrl($view->action('delete', $upa['upaID']));
+                        $delete_url = $delete_url->setQuery(array(
+                                'ccm_token' => \Core::make('helper/validation/token')->generate('delete_action'),
+                            ));
+                        ?>
+            			    <a href="<?=$view->action($upa['upaID'])?>" class="btn btn-sm btn-default"><?=t('Edit')?></a>
+            			    <a href="<?=$delete_url?>" class="btn btn-sm btn-danger"><?=t('Delete')?></a>
+            			</td>
+            		</tr>
+            		<?php
+                    }
+            ?>
+                </tbody>
+    		</table>
+        </div>
+		<?php
     } else {
         ?>
 			<p><?=t('No Actions found.')?></p>
-		<?php 
+		<?php
     }
     ?>
-	
+
 <div class="ccm-pane-footer">
 <?=$actionList->displayPagingV2();
     ?>
 </div>
 
-<?php 
+<?php
 } ?>
 
 <?=Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false)?>
