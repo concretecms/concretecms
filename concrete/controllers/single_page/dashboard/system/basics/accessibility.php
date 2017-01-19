959ac3ae8a40727bc605f5267b393e97f184e5b6
Michele Locati <michele@locati.it>
2017-01-19 10:57:31 +0100

    Modernize accessibility page controller

--- concrete/controllers/single_page/dashboard/system/basics/accessibility.php
+++ concrete/controllers/single_page/dashboard/system/basics/accessibility.php
@@ -9,29 +9,27 @@ class Accessibility extends DashboardPageController
 {
     public function view()
     {
-        $this->set('show_titles', Config::get('concrete.accessibility.toolbar_titles'));
-        $this->set('show_tooltips', Config::get('concrete.accessibility.toolbar_tooltips'));
-        $this->set('increase_font_size', Config::get('concrete.accessibility.toolbar_large_font'));
-        $this->set('display_help', Config::get('concrete.accessibility.display_help_system'));
-    }
-
-    public function saved()
-    {
-        $this->set('message', t('Successfully saved accessibility settings.'));
-        $this->view();
+        $config = $this->app->make('config');
+        $this->set('show_titles', $config->get('concrete.accessibility.toolbar_titles'));
+        $this->set('show_tooltips', $config->get('concrete.accessibility.toolbar_tooltips'));
+        $this->set('increase_font_size', $config->get('concrete.accessibility.toolbar_large_font'));
+        $this->set('display_help', $config->get('concrete.accessibility.display_help_system'));
     }
 
     public function save()
     {
         if (!$this->token->validate('accessibility')) {
             $this->error->add(t('Invalid CSRF token. Please refresh and try again.'));
-
-            return $this->view();
+            $this->view();
+        } else {
+            $config = $this->app->make('config');
+            $post = $this->request->request;
+            $config->save('concrete.accessibility.toolbar_titles', (bool) $post->get('show_titles', false));
+            $config->save('concrete.accessibility.toolbar_tooltips', (bool) $post->get('show_tooltips', false));
+            $config->save('concrete.accessibility.toolbar_large_font', (bool) $post->get('increase_font_size', false));
+            $config->save('concrete.accessibility.display_help_system', (bool) $post->get('display_help', false));
+            $this->flash('message', t('Successfully saved accessibility settings.'));
+            $this->redirect('/dashboard/system/basics/accessibility');
         }
-        Config::save('concrete.accessibility.toolbar_titles', (bool) Request::post('show_titles', false));
-        Config::save('concrete.accessibility.toolbar_tooltips', (bool) Request::post('show_tooltips', false));
-        Config::save('concrete.accessibility.toolbar_large_font', (bool) Request::post('increase_font_size', false));
-        Config::save('concrete.accessibility.display_help_system', (bool) Request::post('display_help', false));
-        $this->redirect('/dashboard/system/basics/accessibility', 'saved');
     }
 }
