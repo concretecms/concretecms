2dc22a29226bc3a8e5c1e59b5807a840ce2640c4
Michele Locati <michele@locati.it>
2017-01-19 10:23:22 +0100

    Fix indentation and remove an extra "</div>"

--- concrete/elements/account/menu.php
+++ concrete/elements/account/menu.php
@@ -22,35 +22,32 @@ if (!$cp->canRead()) {
 }
 ?>
 <div style="display: none">
-<div class="btn-group" id="ccm-account-menu">
-  <a class="btn btn-default" href="<?=$desktop->getCollectionLink()?>"><i class="fa fa-user"></i> <?=$ui->getUserDisplayName()?></a>
-  <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-	<span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu pull-right" role="menu">
-      <li><a href="<?=$desktop->getCollectionLink()?>"><?=t('My Account')?></a></li>
-      <li class="divider"></li>
-  <?php
-    $categories = [];
-    $children = $account->getCollectionChildrenArray(true);
-    foreach ($children as $cID) {
-        $nc = Page::getByID($cID, 'ACTIVE');
-        $ncp = new Permissions($nc);
-        if ($ncp->canRead() && (!$nc->getAttribute('exclude_nav'))) {
-            $categories[] = $nc;
-        }
-    }
-
-    foreach ($categories as $cc) {
-        ?>
-		<li><a href="<?=Core::make('helper/navigation')->getLinkToCollection($cc)?>"><?=h(t($cc->getCollectionName()))?></a></li><?php
-
-    }
-    ?>
-	<li class="divider"></li>
-	<li><a href="<?=URL::to('/')?>"><i class="fa fa-home"></i> <?=t("Home")?></a></li>
-	<li><a href="<?=URL::to('/login', 'logout', Loader::helper('validation/token')->generate('logout'))?>"><i class="fa fa-sign-out"></i> <?=t("Sign Out")?></a></li>
- </ul>
-</div>
-</div>
+    <div class="btn-group" id="ccm-account-menu">
+        <a class="btn btn-default" href="<?=$desktop->getCollectionLink()?>"><i class="fa fa-user"></i> <?=$ui->getUserDisplayName()?></a>
+        <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu pull-right" role="menu">
+            <li><a href="<?=$desktop->getCollectionLink()?>"><?=t('My Account')?></a></li>
+            <li class="divider"></li>
+            <?php
+                $categories = [];
+                $children = $account->getCollectionChildrenArray(true);
+                foreach ($children as $cID) {
+                    $nc = Page::getByID($cID, 'ACTIVE');
+                    $ncp = new Permissions($nc);
+                    if ($ncp->canRead() && (!$nc->getAttribute('exclude_nav'))) {
+                        $categories[] = $nc;
+                    }
+                }
+                foreach ($categories as $cc) {
+                    ?><li><a href="<?=Core::make('helper/navigation')->getLinkToCollection($cc)?>"><?=h(t($cc->getCollectionName()))?></a></li><?php
+                }
+            ?>
+            <li class="divider"></li>
+            <li><a href="<?=URL::to('/')?>"><i class="fa fa-home"></i> <?=t("Home")?></a></li>
+            <li><a href="<?=URL::to('/login', 'logout', Loader::helper('validation/token')->generate('logout'))?>"><i class="fa fa-sign-out"></i> <?=t("Sign Out")?></a></li>
+        </ul>
     </div>
+</div>
+
