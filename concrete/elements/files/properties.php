97f02505390d1b09aa736496b3e9a8ce2f078de2
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-01-27 22:53:03 -0500

    add editable attribute hover background and increase display name width

--- concrete/elements/files/properties.php
+++ concrete/elements/files/properties.php
@@ -10,13 +10,13 @@ if (!isset($mode) || !$mode) {
 <?php if ($mode == 'single') {
     ?>
 <div class="row">
-    <div class="col-md-2"><p><?= t('ID') ?></p></div>
-    <div class="col-md-10"><p><?= $fv->getFileID() ?> <span style="color: #afafaf">(<?= t(
+    <div class="col-md-3"><p><?= t('ID') ?></p></div>
+    <div class="col-md-9"><p><?= $fv->getFileID() ?> <span style="color: #afafaf">(<?= t(
                     'Version') ?> <?= $fv->getFileVersionID() ?>)</p></div>
 </div>
 <div class="row">
-    <div class="col-md-2"><p><?= t('Filename') ?></p></div>
-    <div class="col-md-10"><p><?= h($fv->getFileName()) ?></p></div>
+    <div class="col-md-3"><p><?= t('Filename') ?></p></div>
+    <div class="col-md-9"><p><?= h($fv->getFileName()) ?></p></div>
 </div>
 <?php
 } ?>
@@ -27,8 +27,8 @@ $url = $fv->getURL();
 $downloadUrl = $fv->getDownloadURL();
 ?>
 <div class="row">
-    <div class="col-md-2"><p><?= t('URL to File') ?></p></div>
-    <div class="col-md-10">
+    <div class="col-md-3"><p><?= t('URL to File') ?></p></div>
+    <div class="col-md-9">
         <p style="overflow: hidden">
             <a target="_blank" href="<?= $url ?>">
                 <?= $url ?>
@@ -40,8 +40,8 @@ $downloadUrl = $fv->getDownloadURL();
 if ($downloadUrl !== $url) {
     ?>
     <div class="row">
-        <div class="col-md-2"><p><?= t('Tracked URL') ?></p></div>
-        <div class="col-md-10">
+        <div class="col-md-3"><p><?= t('Tracked URL') ?></p></div>
+        <div class="col-md-9">
             <p style="overflow: hidden">
                 <a target="_blank" href="<?= $downloadUrl ?>">
                     <?= $downloadUrl ?>
@@ -66,8 +66,8 @@ if ($downloadUrl !== $url) {
         }
         ?>
         <div class="row">
-            <div class="col-md-2"><p><?= t('Page Added To') ?></p></div>
-            <div class="col-md-10"><p><a href="<?= Loader::helper('navigation')->getLinkToCollection($oc) ?>"
+            <div class="col-md-3"><p><?= t('Page Added To') ?></p></div>
+            <div class="col-md-9"><p><a href="<?= Loader::helper('navigation')->getLinkToCollection($oc) ?>"
                                         target="_blank"><?= $ocName ?></a></p></div>
         </div>
     <?php
@@ -75,8 +75,8 @@ if ($downloadUrl !== $url) {
     ?>
 
     <div class="row">
-        <div class="col-md-2"><p><?= t('Type') ?></p></div>
-        <div class="col-md-10"><p><?= $fv->getType() ?></p></div>
+        <div class="col-md-3"><p><?= t('Type') ?></p></div>
+        <div class="col-md-9"><p><?= $fv->getType() ?></p></div>
     </div>
 
 <?php
@@ -89,8 +89,8 @@ if ($downloadUrl !== $url) {
         ?>
         <div class="row">
 
-            <div class="col-md-2"><p><?= t('Thumbnails') ?></p></div>
-            <div class="col-md-10">
+            <div class="col-md-3"><p><?= t('Thumbnails') ?></p></div>
+            <div class="col-md-9">
                 <p style="color:#cc3333">
                     <?= t('Invalid file dimensions, please rescan this file.') ?>
                     <?php if ($mode != 'preview' && $fp->canEditFileContents()) {
@@ -109,8 +109,8 @@ if ($downloadUrl !== $url) {
         ?>
         <div class="row">
 
-            <div class="col-md-2"><p><?= t('Thumbnails') ?></p></div>
-            <div class="col-md-10">
+            <div class="col-md-3"><p><?= t('Thumbnails') ?></p></div>
+            <div class="col-md-9">
                 <p style="color:#cc3333">
                     <?= t('Unknown error retrieving thumbnails, please rescan this file.') ?>
                     <?php if ($mode != 'preview' && $fp->canEditFileContents()) {
@@ -129,8 +129,8 @@ if ($downloadUrl !== $url) {
     if ($thumbnails) {
         ?>
         <div class="row">
-            <div class="col-md-2"><p><?= t('Thumbnails') ?></p></div>
-            <div class="col-md-10"><p><a class="dialog-launch icon-link"
+            <div class="col-md-3"><p><?= t('Thumbnails') ?></p></div>
+            <div class="col-md-9"><p><a class="dialog-launch icon-link"
                                         dialog-title="<?= t('Thumbnail Images') ?>"
                                         dialog-width="90%" dialog-height="70%" href="<?= URL::to(
                         '/ccm/system/dialogs/file/thumbnails') ?>?fID=<?= $fv->getFileID() ?>&fvID=<?= $fv->getFileVersionID() ?>"><?= count(
@@ -145,16 +145,16 @@ if ($downloadUrl !== $url) {
     ?>
 
     <div class="row">
-        <div class="col-md-2"><p><?= t('Size') ?></p></div>
-        <div class="col-md-10"><p><?= $fv->getSize() ?> (<?= t2(/*i18n: %s is a number */
+        <div class="col-md-3"><p><?= t('Size') ?></p></div>
+        <div class="col-md-9"><p><?= $fv->getSize() ?> (<?= t2(/*i18n: %s is a number */
                     '%s byte',
                     '%s bytes',
                     $fv->getFullSize(),
                     Loader::helper('number')->format($fv->getFullSize())) ?>)</p></div>
     </div>
     <div class="row">
-        <div class="col-md-2"><p><?= t('Date Added') ?></p></div>
-        <div class="col-md-10"><p><?= t(
+        <div class="col-md-3"><p><?= t('Date Added') ?></p></div>
+        <div class="col-md-9"><p><?= t(
                     'Added by <strong>%s</strong> on %s',
                     $fv->getAuthorName(),
                     $dh->formatDateTime($f->getDateAdded(), true)) ?></p></div>
@@ -164,8 +164,8 @@ if ($downloadUrl !== $url) {
     if (is_object($fsl)) {
         ?>
         <div class="row">
-            <div class="col-md-2"><p><?= t('Storage Location') ?></p></div>
-            <div class="col-md-10"><p><?= $fsl->getDisplayName() ?></div>
+            <div class="col-md-3"><p><?= t('Storage Location') ?></p></div>
+            <div class="col-md-9"><p><?= $fsl->getDisplayName() ?></div>
         </div>
     <?php
     }
@@ -173,16 +173,16 @@ if ($downloadUrl !== $url) {
 <?php
 } ?>
 <div class="row">
-    <div class="col-md-2"><p><?= t('Title') ?></p></div>
-    <div class="col-md-10"><p><span
+    <div class="col-md-3"><p><?= t('Title') ?></p></div>
+    <div class="col-md-9"><p><span
                 <?php if ($fp->canEditFileProperties()) {
     ?>data-editable-field-type="xeditable"
                 data-type="text" data-name="fvTitle"<?php
 } ?>><?= h($fv->getTitle()) ?></span></p></div>
 </div>
 <div class="row">
-    <div class="col-md-2"><p><?= t('Description') ?></p></div>
-    <div class="col-md-10"><p><span
+    <div class="col-md-3"><p><?= t('Description') ?></p></div>
+    <div class="col-md-9"><p><span
                 <?php if ($fp->canEditFileProperties()) {
     ?>data-editable-field-type="xeditable"
                 data-type="textarea" data-name="fvDescription"<?php
@@ -190,8 +190,8 @@ if ($downloadUrl !== $url) {
                     $fv->getDescription()) ?></span></p></div>
 </div>
 <div class="row">
-    <div class="col-md-2"><p><?= t('Tags') ?></p></div>
-    <div class="col-md-10"><p><span
+    <div class="col-md-3"><p><?= t('Tags') ?></p></div>
+    <div class="col-md-9"><p><span
                 <?php if ($fp->canEditFileProperties()) {
     ?>data-editable-field-type="xeditable"
                 data-type="textarea" data-name="fvTags"<?php
