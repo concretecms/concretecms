e01785e25aa6da1c7569253a7b9d18363901af62
Andrew Embler <andrew@concrete5.org>
2017-02-17 08:19:15 -0800

    interface tweaks

---
+++ concrete/src/Site/Tree/LocalizableTreeInterface.php
@@ -0,0 +1,19 @@
+<?php
+namespace Concrete\Core\Site\Tree;
+
+use Concrete\Core\Entity\Site\Tree;
+use Concrete\Core\Localization\Locale\LocaleCollection;
+
+interface LocalizableTreeInterface extends TreeInterface
+{
+
+
+    function getLocale();
+
+    /**
+     * @return LocaleCollection
+     */
+    function getLocaleCollection();
+
+
+}
