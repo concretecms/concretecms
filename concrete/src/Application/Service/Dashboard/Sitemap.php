46f9159d0bbb3554701f6bf0b00b3e692a2a63ad
Katz Ueno <katz@concrete5.co.jp>
2017-02-24 23:34:37 +0900

    Bug fix

--- concrete/src/Application/Service/Dashboard/Sitemap.php
+++ concrete/src/Application/Service/Dashboard/Sitemap.php
@@ -171,7 +171,7 @@ class Sitemap
 
         $cvName = ($c->getCollectionName() !== '') ? $c->getCollectionName() : '(No Title)';
         $cvName = ($c->isSystemPage() || $cID == 1) ? t($cvName) : $cvName;
-        if (Config::get('concrete.misc.sitemap_show_permission'){
+        if (Config::get('concrete.misc.sitemap_show_permission')){
             $inheritance = $c->getCollectionInheritance();
             switch ($inheritance) {
                 case 'TEMPLATE':
