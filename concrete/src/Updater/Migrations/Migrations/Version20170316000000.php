41917ea7b631257f5a55f9a7470d774955c889cf
Michael Krasnow <mnkras@gmail.com>
2017-03-16 14:52:37 -0400

    Tabs->Spaces

--- concrete/src/Updater/Migrations/Migrations/Version20170316000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20170316000000.php
@@ -10,19 +10,19 @@ use Concrete\Core\Support\Facade\Application;
 
 class Version20170316000000 extends AbstractMigration
 {
-	public function up(Schema $schema)
-	{
-		$entities = \Core::make('express')->getEntities(true)->findAll();
-		foreach ($entities as $entity) {
-			/* @var $entity \Concrete\Core\Entity\Express\Entity */
-			$category = $entity->getAttributeKeyCategory();
-			foreach ($entity->getAttributes() as $key) {
-				$category->getSearchIndexer()->updateRepositoryColumns($category, $key);
-			}
-		}
-	}
+    public function up(Schema $schema)
+    {
+        $entities = \Core::make('express')->getEntities(true)->findAll();
+        foreach ($entities as $entity) {
+            /* @var $entity \Concrete\Core\Entity\Express\Entity */
+            $category = $entity->getAttributeKeyCategory();
+            foreach ($entity->getAttributes() as $key) {
+                $category->getSearchIndexer()->updateRepositoryColumns($category, $key);
+            }
+        }
+    }
 
-	public function down(Schema $schema)
-	{
-	}
+    public function down(Schema $schema)
+    {
+    }
 }
