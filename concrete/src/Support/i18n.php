df472c5df8a70e6f0ad51f623fec897e1399e8bf
Michele Locati <michele@locati.it>
2017-04-09 11:10:24 +0200

    Translate VERSION_INITIAL_COMMENT

---
+++ concrete/src/Support/i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * This file contains calls to t(), tc() and t2() functions to let the translation system know about strings to be translated.
+ */
+
+// To translate the value of the VERSION_INITIAL_COMMENT constant
+t('Initial Version');
