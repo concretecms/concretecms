1d666548b7083a1382198f133afd7722f663f29d
Michele Locati <michele@locati.it>
2017-04-12 11:06:21 +0200

    Better comment about Support/i18n.php

--- concrete/src/Support/i18n.php
+++ concrete/src/Support/i18n.php
@@ -2,7 +2,9 @@
 
 /*
  * This file contains calls to t(), tc() and t2() functions to let the translation system know about strings to be translated.
+ * It is never loaded or executed.
  */
+return;
 
 // To translate the value of the VERSION_INITIAL_COMMENT constant
 t('Initial Version');
