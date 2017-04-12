1ee2d61d27cd559dc2d008086fa7d7f87e2c48f1
Michele Locati <michele@locati.it>
2017-04-12 10:38:30 +0200

    Remove unneeded fieldset/legend

--- concrete/single_pages/dashboard/system/permissions/blacklist.php
+++ concrete/single_pages/dashboard/system/permissions/blacklist.php
@@ -16,31 +16,27 @@ $view->element('dashboard/system/permissions/blacklist/menu', ['type' => null]);
 <form method="post" id="ipblacklist-form" action="<?= $view->action('update_ipblacklist') ?>">
     <?php $token->output('update_ipblacklist') ?>
     <div class="ccm-pane-body">
-        <fieldset>
-            <legend><?= t('Smart IP Banning')?></legend>
+        <div class="form-group form-inline">
+            <?= $form->checkbox('banEnabled', 1, $banEnabled) ?> <?= t('Lock IP after') ?>
+            <?= $form->number('allowedAttempts', $allowedAttempts, ['style' => 'width:70px', 'min' => 1]) ?>
+            <?= t(/*i18n: before we have the number of failed logins, after we have a time duration */'failed login attempts in') ?>
+            <?= $form->number('attemptsTimeWindow', $attemptsTimeWindow, ['style' => 'width:90px', 'min' => 1]) ?>
+            <?= t('seconds') ?>
+        </div>
 
-            <div class="form-group form-inline">
-                <?= $form->checkbox('banEnabled', 1, $banEnabled) ?> <?= t('Lock IP after') ?>
-                <?= $form->number('allowedAttempts', $allowedAttempts, ['style' => 'width:70px', 'min' => 1]) ?>
-                <?= t(/*i18n: before we have the number of failed logins, after we have a time duration */'failed login attempts in') ?>
-                <?= $form->number('attemptsTimeWindow', $attemptsTimeWindow, ['style' => 'width:90px', 'min' => 1]) ?>
-                <?= t('seconds') ?>
-            </div>
-
-            <div class="form-inline form-group radio">
-                <?= t('Ban IP For') ?>
-                <br />
-                <label class="radio">
-                    <?= $form->radio('banDurationUnlimited', 0, $banDuration ? 0 : 1) ?>
-                    <?= $form->number('banDuration', $banDuration ?: 300, ['style' => 'width:90px', 'min' => 1]) ?>
-                    <?= t('minutes') ?>
-                </label>
-                <br />
-                <label class="radio">
-                    <?= $form->radio('banDurationUnlimited', 1, $banDuration ? 0 : 1) ?> <?= t('Forever') ?>
-                </label>
-            </div>
-        </fieldset>
+        <div class="form-inline form-group radio">
+            <?= t('Ban IP For') ?>
+            <br />
+            <label class="radio">
+                <?= $form->radio('banDurationUnlimited', 0, $banDuration ? 0 : 1) ?>
+                <?= $form->number('banDuration', $banDuration ?: 300, ['style' => 'width:90px', 'min' => 1]) ?>
+                <?= t('minutes') ?>
+            </label>
+            <br />
+            <label class="radio">
+                <?= $form->radio('banDurationUnlimited', 1, $banDuration ? 0 : 1) ?> <?= t('Forever') ?>
+            </label>
+        </div>
     </div>
 
     <div class="ccm-dashboard-form-actions-wrapper">
