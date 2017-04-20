d17e58c68df7d421728519f960c4b359cc970f4a
Michele Locati <michele@locati.it>
2017-04-20 19:38:39 +0200

    Inhibit moving guests/registered users/admins groups

--- concrete/src/Tree/Node/Type/Group.php
+++ concrete/src/Tree/Node/Type/Group.php
@@ -5,6 +5,7 @@ use Concrete\Core\Tree\Node\Node as TreeNode;
 use Concrete\Core\Tree\Node\Type\Menu\GroupMenu;
 use Loader;
 use Concrete\Core\User\Group\Group as UserGroup;
+use Exception;
 
 class Group extends TreeNode
 {
@@ -77,6 +78,14 @@ class Group extends TreeNode
 
     public function move(TreeNode $newParent)
     {
+        switch ($this->gID) {
+            case GUEST_GROUP_ID:
+                throw new Exception(t("The guest users groups can't me boved"));
+            case REGISTERED_GROUP_ID:
+                throw new Exception(t("The registered users group can't be moved"));
+            case ADMIN_GROUP_ID:
+                throw new Exception(t("The administrators group can't be moved"));
+        }
         parent::move($newParent);
         $g = $this->getTreeNodeGroupObject();
         if (is_object($g)) {
