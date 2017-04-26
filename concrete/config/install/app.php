a7918b32a130ec630efe33c085de9ed4f0e6f94c
Michele Locati <michele@locati.it>
2017-04-26 16:32:53 +0200

    Installation: update Country list upon language change

--- concrete/config/install/app.php
+++ concrete/config/install/app.php
@@ -6,6 +6,7 @@ return array(
         '/install' => array('\Concrete\Controller\Install::view'),
         '/install/select_language' => array('\Concrete\Controller\Install::select_language'),
         '/install/setup' => array('\Concrete\Controller\Install::setup'),
+        '/install/get_site_locale_countries/{viewLocaleID}/{languageID}/{preselectedCountryID}' => array('\Concrete\Controller\Install::get_site_locale_countries'),
         '/install/test_url/{num1}/{num2}' => array('\Concrete\Controller\Install::test_url'),
         '/install/configure' => array('\Concrete\Controller\Install::configure'),
         '/install/run_routine/{pkgHandle}/{routine}' => array('\Concrete\Controller\Install::run_routine'),
