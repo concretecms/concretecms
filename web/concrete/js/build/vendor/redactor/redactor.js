3482125462871f6a1a39adcdc9bdde656bfae017
Michele Locati <michele@locati.it>
2017-05-02 09:38:00 +0200

    Fix redactor issue on Chrome 58

--- web/concrete/js/build/vendor/redactor/redactor.js
+++ web/concrete/js/build/vendor/redactor/redactor.js
@@ -8200,6 +8200,12 @@
 					{
 						var node2 = this.selection.getMarker(2);
 						this.selection.setMarker(this.range, node2, false);
+						// concrete5 start - https://github.com/concrete5/concrete5/issues/5406
+						if (this.utils.browser('chrome'))
+						{
+						    this.caret.set(node1, 0, node2, 0);
+						}
+						// concrete5 end
 					}
 
 					this.savedSel = this.$editor.html();
