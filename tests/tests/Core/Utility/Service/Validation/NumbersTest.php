3b92dae514174c6acc40071224421d56b48c4b04
Michele Locati <michele@locati.it>
2017-05-04 11:11:20 +0200

    Improve number validation helper

--- tests/tests/Core/Utility/Service/Validation/NumbersTest.php
+++ tests/tests/Core/Utility/Service/Validation/NumbersTest.php
@@ -45,14 +45,67 @@ class NumbersTest extends PHPUnit_Framework_TestCase
             [false, ''],
             [false, '1.25'],
             [false, 1.25],
+            [false, 0, 1],
+            [true, 123, 0],
+            [true, 123, 123],
+            [true, '123', 123],
+            [false, 123, 124],
+            [true, '123', null, 123],
+            [false, '123', null, 122],
         ];
     }
 
     /**
      * @dataProvider integerDataProvider
      */
-    public function testInteger($expected, $input1)
+    public function testInteger($expected, $input1, $min = null, $max = null)
     {
-        $this->assertEquals($expected, $this->object->integer($input1));
+        $this->assertEquals($expected, $this->object->integer($input1, $min, $max));
+    }
+    
+    
+    public function numberDataProvider()
+    {
+        return [
+            [true, '0'],
+            [true, 0],
+            [true, '1'],
+            [true, 1],
+            [true, '-1456789445'],
+            [true, -1456789445],
+            [true, '123'],
+            [true, 123],
+            [false, '123,456'],
+            [false, 'a'],
+            [false, false],
+            [false, true],
+            [false, null],
+            [false, ''],
+            [true, '1.25'],
+            [true, 1.25],
+            [false, 0, 1],
+            [true, 123, 0],
+            [true, 123, 123],
+            [true, '123', 123],
+            [false, 123, 124],
+            [true, '123', null, 123],
+            [false, '123', null, 122],
+            [false, '.'],
+            [false, '-'],
+            [false, '-.'],
+            [true, '.0'],
+            [true, '0', 0, 1],
+            [true, '.1', 0, 1],
+            [true, '.999', 0, 1],
+            [true, .05, 0, 0.1],
+        ];
+    }
+    
+    /**
+     * @dataProvider numberDataProvider
+     */
+    public function testNumber($expected, $input1, $min = null, $max = null)
+    {
+        $this->assertEquals($expected, $this->object->number($input1, $min, $max));
     }
 }
