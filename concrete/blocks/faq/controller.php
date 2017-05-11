e584c4fac5c4e5fedc41796ba0a921c998581bfc
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-05-11 15:45:37 -0400

    update, indent, format

--- concrete/blocks/faq/controller.php
+++ concrete/blocks/faq/controller.php
@@ -2,36 +2,34 @@
 namespace Concrete\Block\Faq;
 
 use Concrete\Core\Block\BlockController;
-use Database;
 
 class Controller extends BlockController
 {
+    protected $btInterfaceWidth = 600;
+    protected $btInterfaceHeight = 465;
     protected $btTable = 'btFaq';
-    protected $btExportTables = array('btFaq', 'btFaqEntries');
-    protected $btInterfaceWidth = "600";
+    protected $btExportTables = ['btFaq', 'btFaqEntries'];
     protected $btWrapperClass = 'ccm-ui';
-    protected $btInterfaceHeight = "465";
-    protected $btCacheBlockRecord = true;
     protected $btCacheBlockOutput = true;
     protected $btCacheBlockOutputOnPost = true;
     protected $btCacheBlockOutputForRegisteredUsers = true;
 
-    public function getBlockTypeDescription()
-    {
-        return t("Frequently Asked Questions Block");
-    }
-
     public function getBlockTypeName()
     {
-        return t("FAQ");
+        return t('FAQ');
+    }
+
+    public function getBlockTypeDescription()
+    {
+        return t('Frequently Asked Questions Block');
     }
 
     public function getSearchableContent()
     {
         $content = '';
-        $db = Database::connection();
-        $v = array($this->bID);
-        $q = 'select * from btFaqEntries where bID = ?';
+        $db = $this->app->make('database')->connection();
+        $v = [$this->bID];
+        $q = 'SELECT * FROM btFaqEntries WHERE bID = ?';
         $r = $db->query($q, $v);
         foreach ($r as $row) {
             $content .= $row['title'] . ' ' . $row['linkTitle'] . ' ' . $row['description'];
@@ -40,72 +38,65 @@ class Controller extends BlockController
         return $content;
     }
 
-    public function add()
-    {
-        $this->requireAsset('core/file-manager');
-        $this->requireAsset('core/sitemap');
-    }
-
     public function edit()
     {
-        $this->add();
-
-        $db = Database::connection();
-        $query = $db->GetAll('SELECT * from btFaqEntries WHERE bID = ? ORDER BY sortOrder', array($this->bID));
+        $db = $this->app->make('database')->connection();
+        $query = $db->GetAll('SELECT * FROM btFaqEntries WHERE bID = ? ORDER BY sortOrder', [$this->bID]);
         $this->set('rows', $query);
     }
 
     public function view()
     {
-        $db = Database::connection();
-        $query = $db->GetAll('SELECT * from btFaqEntries WHERE bID = ? ORDER BY sortOrder', array($this->bID));
+        $db = $this->app->make('database')->connection();
+        $query = $db->GetAll('SELECT * FROM btFaqEntries WHERE bID = ? ORDER BY sortOrder', [$this->bID]);
         $this->set('rows', $query);
     }
 
     public function duplicate($newBID)
     {
-        $db = Database::connection();
-        $v = array($this->bID);
-        $q = 'select * from btFaqEntries where bID = ?';
+        $db = $this->app->make('database')->connection();
+        $v = [$this->bID];
+        $q = 'SELECT * FROM btFaqEntries WHERE bID = ?';
         $r = $db->query($q, $v);
         foreach ($r as $row) {
             $db->execute(
-                'INSERT INTO btFaqEntries (bID, title, linkTitle, description, sortOrder) values(?,?,?,?,?)',
-                array(
+                'INSERT INTO btFaqEntries (bID, title, linkTitle, description, sortOrder) VALUES(?,?,?,?,?)',
+                [
                     $newBID,
                     $row['title'],
                     $row['linkTitle'],
                     $row['description'],
                     $row['sortOrder'],
-                )
+                ]
             );
         }
     }
 
     public function delete()
     {
-        $db = Database::connection();
-        $db->execute('DELETE from btFaqEntries WHERE bID = ?', array($this->bID));
+        $db = $this->app->make('database')->connection();
+        $db->execute('DELETE FROM btFaqEntries WHERE bID = ?', [$this->bID]);
         parent::delete();
     }
 
     public function save($args)
     {
-        $db = Database::connection();
-        $db->execute('DELETE from btFaqEntries WHERE bID = ?', array($this->bID));
+        $db = $this->app->make('database')->connection();
+        $db->execute('DELETE FROM btFaqEntries WHERE bID = ?', [$this->bID]);
         $count = isset($args['sortOrder']) ? count($args['sortOrder']) : 0;
+
         $i = 0;
         parent::save($args);
         while ($i < $count) {
             $db->execute(
-                'INSERT INTO btFaqEntries (bID, title, linkTitle, description, sortOrder) values(?,?,?,?,?)',
-                array(
+                'INSERT INTO btFaqEntries (bID, title, linkTitle, description, sortOrder) VALUES(?,?,?,?,?)',
+                [
                     $this->bID,
                     $args['title'][$i],
                     $args['linkTitle'][$i],
                     $args['description'][$i],
                     $args['sortOrder'][$i],
-                )
+                ]
             );
             ++$i;
         }
