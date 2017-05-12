78095c58dc0d233c67070d4630f6db7b3e5818af
Daniel Gasser <software@toesslab.ch>
2017-05-12 16:28:42 +0200

    type array added to $datePickerOptions parameter

--- concrete/src/Form/Service/Widget/DateTime.php
+++ concrete/src/Form/Service/Widget/DateTime.php
@@ -93,7 +93,7 @@ class DateTime
      *
      * @return string
      */
-    public function datetime($field, $value = null, $includeActivation = false, $calendarAutoStart = true, $classes = null, $timeResolution = 60, $datePickerOptions = array())
+    public function datetime($field, $value = null, $includeActivation = false, $calendarAutoStart = true, $classes = null, $timeResolution = 60, array $datePickerOptions = array())
     {
         $app = Application::getFacadeApplication();
         $dh = $app->make('helper/date');
@@ -345,7 +345,7 @@ EOT;
      *
      * @return string
      */
-    public function date($field, $value = null, $calendarAutoStart = true, $datePickerOptions = array())
+    public function date($field, $value = null, $calendarAutoStart = true, array $datePickerOptions = array())
     {
         $app = Application::getFacadeApplication();
         $dh = $app->make('helper/date');
