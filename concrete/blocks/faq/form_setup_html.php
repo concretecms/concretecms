a61737aeec596b8eed4978a7a0ab3b1e231a398d
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-05-11 20:47:36 -0400

    make entries drag sortable and collapsible

--- concrete/blocks/faq/form_setup_html.php
+++ concrete/blocks/faq/form_setup_html.php
@@ -1,75 +1,102 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <style>
-    .ccm-faq-block-container input,
-    .ccm-faq-block-container textarea {
-        display: block;
-        width: 100%;
+    .ccm-faq-block-container {
+        position: relative;
     }
-
     .ccm-faq-block-container .btn-success {
         margin-bottom: 20px;
     }
-
     .ccm-faq-entry {
         position: relative;
     }
-
-    .ccm-faq-block-container i.fa-sort-asc {
+    .ccm-faq-entry.well {
+        margin-bottom: 10px;
+        padding: 28px 10px 10px;
+    }
+    .ccm-faq-entry.well.entry-closed {
+        height: 57px;
+        padding: 0 0 0 15px;
+    }
+    .ccm-faq-entry .entry-collapse-text {
+        display: none;
+    }
+    .ccm-faq-entry.entry-closed .entry-collapse-text {
+        display: block;
+        line-height: 57px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        width: 335px;
+    }
+    .ccm-faq-entry.entry-closed .form-group {
+        display: none;
+    }
+    .ccm-faq-entry .form-group:last-of-type {
+        margin-bottom: 0;
+    }
+    .ccm-edit-entry {
         position: absolute;
+        right: 127px;
         top: 10px;
-        right: 10px;
-        cursor: pointer;
     }
-
-    .ccm-faq-block-container i:hover {
-        color: #5cb85c;
-    }
-
-    .ccm-faq-block-container i.fa-sort-desc {
+    .ccm-delete-faq-entry {
         position: absolute;
-        top: 15px;
-        cursor: pointer;
-        right: 10px;
+        right: 41px;
+        top: 10px;
+    }
+    .ccm-faq-block-container i:hover {
+        color: #428bca;
+    }
+    .ccm-faq-block-container i.fa-arrows {
+        cursor: move;
+        font-size: 20px;
+        padding: 5px;
+        position: absolute;
+        right: 5px;
+        top: 6px;
+    }
+    .ccm-faq-block-container .ui-state-highlight {
+        height: 57px;
+        margin-bottom: 10px;
+    }
+    .ccm-faq-block-container .ui-sortable-helper {
+        box-shadow: 0 10px 18px 2px rgba(54,55,66,0.27);
     }
 </style>
 
 <div class="ccm-faq-block-container">
-    <span class="btn btn-success ccm-add-faq-entry"><?php echo t('Add Entry'); ?></span>
+    <button type="button" class="btn btn-success ccm-add-faq-entry"><?php echo t('Add Entry'); ?></button>
     <?php
     if ($rows) {
         foreach ($rows as $row) { ?>
-            <div class="ccm-faq-entry well">
-                <i class="fa-sort-asc fa"></i>
-                <i class="fa-sort-desc fa"></i>
+            <div class="ccm-faq-entry well entry-closed">
+                <p class="entry-collapse-text"><?php echo $row['linkTitle'] ? $row['linkTitle'] : ''; ?></p>
 
                 <div class="form-group">
                     <label class="control-label"><?php echo t('Navigation Link Text'); ?></label>
                     <input class="form-control ccm-input-text" type="text" name="linkTitle[]" value="<?php echo $row['linkTitle']; ?>">
                 </div>
-
                 <div class="form-group">
                     <label class="control-label"><?php echo t('Title Text'); ?></label>
                     <input class="form-control ccm-input-text" type="text" name="title[]" value="<?php echo $row['title']; ?>">
                 </div>
-
                 <div class="form-group">
                     <label class="control-label"><?php echo t('Description'); ?></label>
                     <textarea class='editor-content' name="description[]"><?php echo $row['description']; ?></textarea>
                 </div>
+                <button type="button" class="btn btn-sm btn-default ccm-edit-entry" data-entry-close-text="<?php echo t('Collapse Entry'); ?>" data-entry-edit-text="<?php echo t('Edit Entry'); ?>"><?php echo t('Edit Entry'); ?></button>
+                <button type="button" class="btn btn-sm btn-danger ccm-delete-faq-entry"><?php echo t('Remove'); ?></button>
+                <i class="fa fa-arrows"></i>
 
                 <input class="ccm-faq-entry-sort" type="hidden" name="sortOrder[]" value="<?php echo $row['sortOrder']; ?>">
-
-                <div class="form-group">
-                    <span class="btn btn-danger ccm-delete-faq-entry"><?php echo t('Delete Entry'); ?></span>
-                </div>
             </div>
         <?php
         }
     } else {
     ?>
         <script>
-        _.defer(function () {
+        _.defer(function() {
             $('.ccm-add-faq-entry').click();
         });
         </script>
@@ -78,28 +105,25 @@
     ?>
 
     <div class="ccm-faq-entry well ccm-faq-entry-template" style="display: none;">
-        <i class="fa-sort-asc fa"></i>
-        <i class="fa-sort-desc fa"></i>
+        <p class="entry-collapse-text"></p>
 
         <div class="form-group">
             <label class="control-label"><?php echo t('Navigation Link Text'); ?></label>
             <input class="form-control ccm-input-text" type="text" name="linkTitle[]" value="">
         </div>
-
         <div class="form-group">
             <label class="control-label"><?php echo t('Title Text'); ?></label>
             <input class="form-control ccm-input-text" type="text" name="title[]" value="">
         </div>
-
         <div class="form-group">
             <label class="control-label"><?php echo t('Description'); ?></label>
             <textarea class='editor-content' name="description[]"></textarea>
         </div>
-        <input class="ccm-faq-entry-sort" type="hidden" name="sortOrder[]" value="">
+        <button type="button" class="btn btn-sm btn-default ccm-edit-entry" data-entry-close-text="<?php echo t('Collapse Entry'); ?>" data-entry-edit-text="<?php echo t('Edit Entry'); ?>"><?php echo t('Edit Entry'); ?></button>
+        <button type="button" class="btn btn-sm btn-danger ccm-delete-faq-entry"><?php echo t('Remove'); ?></button>
+        <i class="fa fa-arrows"></i>
 
-        <div class="form-group">
-            <span class="btn btn-danger ccm-delete-faq-entry"><?php echo t('Delete Entry'); ?></span>
-        </div>
+        <input class="ccm-faq-entry-sort" type="hidden" name="sortOrder[]" value="">
     </div>
 </div>
 
@@ -110,18 +134,18 @@ $editorJavascript = $app->make('editor')->outputStandardEditorInitJSFunction();
 ?>
 var launchEditor = <?=$editorJavascript?>;
 
-(function() {
+$(document).ready(function() {
     var container = $('.ccm-faq-block-container');
 
-    var doSortCount = function () {
-        $('.ccm-faq-entry', container).each(function (index) {
+    var doSortCount = function() {
+        $('.ccm-faq-entry', container).each(function(index) {
             $(this).find('.ccm-faq-entry-sort').val(index);
         });
     };
     doSortCount();
 
-    var uniqueEntryID = function () {
-        $('.ccm-faq-entry', container).each(function () {
+    var uniqueEntryID = function() {
+        $('.ccm-faq-entry', container).each(function() {
             $(this).find('.editor-content').not('.ccm-faq-entry-template').attr('id', _.uniqueId());
         });
     };
@@ -131,9 +155,9 @@ var launchEditor = <?=$editorJavascript?>;
     cloneTemplate.removeClass('.ccm-faq-entry-template');
     $('.ccm-faq-entry-template').remove();
 
-    $(cloneTemplate).add($('.ccm-faq-entry', container)).find('.ccm-delete-faq-entry').click(function () {
+    $(cloneTemplate).add($('.ccm-faq-entry', container)).find('.ccm-delete-faq-entry').click(function() {
         var deleteIt = confirm('<?php echo t('Are you sure?'); ?>');
-        if (deleteIt == true) {
+        if (deleteIt === true) {
             entryID = $(this).closest('.ccm-faq-entry').find('.editor-content').attr('id');
             if (typeof CKEDITOR === 'object') {
                 CKEDITOR.instances[entryID].destroy();
@@ -148,38 +172,58 @@ var launchEditor = <?=$editorJavascript?>;
         launchEditor(container.find('.editor-content'));
     }
 
-    var attachSortDesc = function ($obj) {
-        $obj.click(function () {
-            var myContainer = $(this).closest('.ccm-faq-entry');
-            myContainer.insertAfter(myContainer.next('.ccm-faq-entry'));
-            doSortCount();
-        });
-    };
-
-    var attachSortAsc = function ($obj) {
-        $obj.click(function () {
-            var myContainer = $(this).closest('.ccm-faq-entry');
-            myContainer.insertBefore(myContainer.prev('.ccm-faq-entry'));
-            doSortCount();
-        });
-    };
-    $('i.fa-sort-desc', container).each(function () {
-        attachSortDesc($(this));
-    });
-    $('i.fa-sort-asc', container).each(function () {
-        attachSortAsc($(this));
-    });
-    $('.ccm-add-faq-entry', container).click(function () {
+    $('.ccm-add-faq-entry', container).click(function() {
         var newClone = cloneTemplate.clone(true);
         newClone.find('.editor-content').attr('id', _.uniqueId());
         launchEditor(newClone.show().find('.editor-content'));
         container.append(newClone);
-        attachSortAsc(newClone.find('i.fa-sort-asc'));
-        attachSortDesc(newClone.find('i.fa-sort-desc'));
+
+        $('.ccm-faq-entry').not('.entry-closed').each(function() {
+            $(this).addClass('entry-closed');
+            var thisEditButton = $(this).find('.ccm-edit-entry');
+            thisEditButton.text(thisEditButton.data('entryEditText'));
+
+            var linkTitleText = $(this).find('input[name="linkTitle[]"]').val();
+            if (linkTitleText) {
+                $(this).find('.entry-collapse-text').text(linkTitleText);
+            }
+        });
+
+        var newEntry = $('.ccm-faq-entry').last();
+        var closeText = newEntry.find('.ccm-edit-entry').data('entryCloseText');
+        newEntry.removeClass('entry-closed').find('.ccm-edit-entry').text(closeText);
+
         var thisModal = $(this).closest('.ui-dialog-content');
-        var newSlide = $('.ccm-faq-entry').last();
-        thisModal.scrollTop(newSlide.offset().top);
+        thisModal.scrollTop(newEntry.offset().top);
         doSortCount();
     });
-}());
+
+    $(container).on('click','.ccm-edit-entry', function() {
+        var closestEntry = $(this).closest('.ccm-faq-entry');
+        closestEntry.toggleClass('entry-closed');
+
+        var thisEditButton = $(this);
+        if (thisEditButton.data('entryEditText') === thisEditButton.text()) {
+            thisEditButton.text(thisEditButton.data('entryCloseText'));
+        } else if (thisEditButton.data('entryCloseText') === thisEditButton.text()) {
+            thisEditButton.text(thisEditButton.data('entryEditText'));
+
+            var linkTitleText = closestEntry.find('input[name="linkTitle[]"]').val();
+            if (linkTitleText) {
+                closestEntry.find('.entry-collapse-text').text(linkTitleText);
+            }
+        }
+    });
+
+    $(container).sortable({
+        placeholder: 'ui-state-highlight',
+        axis: 'y',
+        items: '.ccm-faq-entry',
+        handle: 'i.fa-arrows',
+        cursor: 'move',
+        update: function() {
+            doSortCount();
+        }
+    });
+});
 </script>
