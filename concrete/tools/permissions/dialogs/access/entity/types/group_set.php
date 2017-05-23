4849d5eaa1963395c5db6d6486587a34aa60823e
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-05-23 10:51:04 -0400

    replace image icon with Font Awesome icon

--- concrete/tools/permissions/dialogs/access/entity/types/group_set.php
+++ concrete/tools/permissions/dialogs/access/entity/types/group_set.php
@@ -13,46 +13,47 @@ if (!$tp->canAccessGroupSearch()) {
     $gl = new GroupSetList();
     ?>
 	<div id="ccm-list-wrapper">
-	
+
 	<?php if ($gl->getTotal() > 0) {
     foreach ($gl->get() as $gs) {
         ?>
-	
+
 		<div class="ccm-group">
-			<div style="background-image: url(<?=ASSETS_URL_IMAGES?>/icons/group.png)" class="ccm-group-inner-indiv">
+			<div style="padding: 8px 8px 8px 0;" class="ccm-group-inner-indiv">
+                <i class="fa fa-cubes"></i>
 				<a class="ccm-group-inner-atag" id="g<?=$g['gID']?>" href="javascript:void(0)" onclick="ccm_selectGroupSet(<?=$gs->getGroupSetID()?>)"><?=$gs->getGroupSetDisplayName()?></a>
 			</div>
 		</div>
-	
-	<?php 
+
+	<?php
     }
     ?>
-	
+
 	<?php
 
 } else {
     ?>
-	
+
 		<p><?=t('No group sets found.')?></p>
-		
-	<?php 
+
+	<?php
 }
     ?>
-	
+
 	</div>
-	
+
 	<script type="text/javascript">
-	ccm_selectGroupSet = function(gsID) {	
+	ccm_selectGroupSet = function(gsID) {
 		$('#ccm-permissions-access-entity-form .btn-group').removeClass('open');
 		$.getJSON('<?=$url?>', {
 			'gsID': gsID
 		}, function(r) {
-			jQuery.fn.dialog.hideLoader();			
+			jQuery.fn.dialog.hideLoader();
 			jQuery.fn.dialog.closeTop();
-			$('#ccm-permissions-access-entity-form input[name=peID]').val(r.peID);	
-			$('#ccm-permissions-access-entity-label').html('<div class="alert alert-info">' + r.label + '</div>');	
+			$('#ccm-permissions-access-entity-form input[name=peID]').val(r.peID);
+			$('#ccm-permissions-access-entity-label').html('<div class="alert alert-info">' + r.label + '</div>');
 		});
-	}	
+	}
 	</script>
-<?php 
+<?php
 } ?>
