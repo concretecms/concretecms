c62c9a6f3d7c45157fa34b544d06c57bd3260484
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-05-23 20:53:40 -0400

    add dashboard form actions wrapper for "Save Settings" button

--- concrete/single_pages/dashboard/system/multilingual/setup.php
+++ concrete/single_pages/dashboard/system/multilingual/setup.php
@@ -132,9 +132,11 @@ foreach ($locales as $locale) {
         </script>
     </div>
 
-    <div class="form-group">
-        <?php echo Loader::helper('validation/token')->output('set_default') ?>
-        <button class="btn btn-default" type="submit" name="save"><?= t('Save Settings') ?></button>
+    <div class="ccm-dashboard-form-actions-wrapper">
+        <div class="ccm-dashboard-form-actions">
+            <?php echo Loader::helper('validation/token')->output('set_default') ?>
+            <button class="pull-right btn btn-primary" type="submit" name="save"><?= t('Save Settings') ?></button>
+        </div>
     </div>
 </form>
 
