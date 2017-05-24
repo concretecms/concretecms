b605bda5aed00c973705b269c1c833847c7be15e
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-05-24 03:14:13 -0400

    indent and format

--- concrete/tools/files/replace.php
+++ concrete/tools/files/replace.php
@@ -1,10 +1,10 @@
-<?php
-defined('C5_EXECUTE') or die("Access Denied.");
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+
+use Concrete\Core\File\StorageLocation\StorageLocation;
 $u = new User();
 $ch = Loader::helper('concrete/file');
 $valt = Loader::helper('validation/token');
 $form = Loader::helper('form');
-use Concrete\Core\File\StorageLocation\StorageLocation;
 
 $f = File::getByID($_REQUEST['fID']);
 $fp = new Permissions($f);
@@ -16,27 +16,26 @@ $searchInstance = Loader::helper('text')->entities($_REQUEST['searchInstance']);
 ?>
 
 <div class="ccm-ui">
-
     <ul class="nav nav-tabs" id="ccm-file-import-tabs">
         <li class="active"><a href="javascript:void(0)" id="ccm-file-add-computer"><?=t('Add From Computer')?></a></li>
         <li><a href="javascript:void(0)" id="ccm-file-add-incoming"><?=t('Add From Incoming')?></a></li>
         <li><a href="javascript:void(0)" id="ccm-file-add-remote"><?=t('Add Remote Files')?></a></li>
     </ul>
-    <script type="text/javascript">
-        var ccm_fiActiveTab = "ccm-file-add-computer";
-        $("#ccm-file-import-tabs a").click(function() {
 
-            $("li.active").removeClass('active');
-            var activesection = ccm_fiActiveTab.substring(13);
+    <script>
+    var ccm_fiActiveTab = "ccm-file-add-computer";
+    $("#ccm-file-import-tabs a").click(function() {
+        $("li.active").removeClass('active');
+        var activesection = ccm_fiActiveTab.substring(13);
 
-            $("#" + ccm_fiActiveTab + "-tab").hide();
-            ccm_fiActiveTab = $(this).attr('id');
+        $("#" + ccm_fiActiveTab + "-tab").hide();
+        ccm_fiActiveTab = $(this).attr('id');
 
-            $(this).parent().addClass("active");
-            $("#" + ccm_fiActiveTab + "-tab").show();
-        });
-
+        $(this).parent().addClass("active");
+        $("#" + ccm_fiActiveTab + "-tab").show();
+    });
     </script>
+
     <div id="ccm-file-add-computer-tab">
         <form method="post" class="form-inline" id="ccm-file-manager-replace-upload" data-dialog-form="replace-file" action="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/files/importers/single">
             <h4><?=t('Add From Computer')?></h4>
@@ -54,7 +53,6 @@ $searchInstance = Loader::helper('text')->entities($_REQUEST['searchInstance']);
             </table>
         </form>
     </div>
-
 
     <div id="ccm-file-add-incoming-tab" style="display: none">
         <h4><?=t('Add from Incoming Directory')?></h4>
@@ -90,19 +88,19 @@ $searchInstance = Loader::helper('text')->entities($_REQUEST['searchInstance']);
                 </form>
             <?php
             } else {
-                if ($error) {
-                    ?>
+                if ($error) { ?>
                     <div class="alert alert-danger">
-                        <?php echo $error;
-                    ?>
+                        <?php echo $error; ?>
                     </div>
                 <?php
                 } else {
                     echo t('No files found in %s for the storage location "%s".', REL_DIR_FILES_INCOMING, StorageLocation::getDefault()->getName());
                 }
-            } ?>
+            }
+            ?>
         </div>
     </div>
+
     <div id="ccm-file-add-remote-tab" style="display: none">
         <h4><?=t("Add from Remote URL")?></h4>
         <form method="post" id="ccm-file-manager-replace-remote" class="form-inline" data-dialog-form="replace-file" action="<?=REL_DIR_FILES_TOOLS_REQUIRED?>/files/importers/remote">
@@ -123,13 +121,13 @@ $searchInstance = Loader::helper('text')->entities($_REQUEST['searchInstance']);
     </div>
 </div>
 
-<script type="text/javascript">
-    $(function() {
-        $('#ccm-file-manager-replace-incoming,#ccm-file-manager-replace-remote,#ccm-file-manager-replace-upload').concreteAjaxForm();
-        ConcreteEvent.subscribe('AjaxFormSubmitSuccess', function(e, data) {
-            if (data.form == 'replace-file') {
-                ConcreteEvent.publish('FileManagerReplaceFileComplete', {files: data.response.files});
-            }
-        });
+<script>
+$(function() {
+    $('#ccm-file-manager-replace-incoming,#ccm-file-manager-replace-remote,#ccm-file-manager-replace-upload').concreteAjaxForm();
+    ConcreteEvent.subscribe('AjaxFormSubmitSuccess', function(e, data) {
+        if (data.form == 'replace-file') {
+            ConcreteEvent.publish('FileManagerReplaceFileComplete', {files: data.response.files});
+        }
     });
+});
 </script>
