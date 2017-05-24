8642a830e5050ca7250576e6d46f4c4b1d00dc39
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-05-23 22:09:03 -0400

    indent and format

--- concrete/single_pages/dashboard/system/permissions/antispam.php
+++ concrete/single_pages/dashboard/system/permissions/antispam.php
@@ -1,36 +1,36 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");?>
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+
 <form method="post" id="site-form" action="<?=$view->action('update_library')?>">
     <div class="form-group">
-    <?=$form->label('group_id', t('Spam Whitelist Group'))?>
+        <?=$form->label('group_id', t('Spam Whitelist Group'))?>
 		<?=$form->select('group_id', (array) $groups, $whitelistGroup);?>
     </div>
+
 	<?=$this->controller->token->output('update_library')?>
-	<?php if (count($libraries) > 0) {
-    ?>
-
+	<?php if (count($libraries) > 0) { ?>
 		<div class="form-group">
-		<?=$form->label('activeLibrary', t('Active Library'))?>
-		<?php
-        $activeHandle = '';
-    if (is_object($activeLibrary)) {
-        $activeHandle = $activeLibrary->getSystemAntispamLibraryHandle();
-    }
-    ?>
+    		<?=$form->label('activeLibrary', t('Active Library'))?>
+    		<?php
+            $activeHandle = '';
+            if (is_object($activeLibrary)) {
+                $activeHandle = $activeLibrary->getSystemAntispamLibraryHandle();
+            }
+            ?>
 
-		<?=$form->select('activeLibrary', $libraries, $activeHandle, array('class' => 'form-control'))?>
+    		<?=$form->select('activeLibrary', $libraries, $activeHandle, array('class' => 'form-control'))?>
 		</div>
 
 		<?php if (is_object($activeLibrary)) {
-    if ($activeLibrary->hasOptionsForm()) {
-        if ($activeLibrary->getPackageID() > 0) {
-            Loader::packageElement('system/antispam/' . $activeLibrary->getSystemAntispamLibraryHandle() . '/form', $activeLibrary->getPackageHandle());
-        } else {
-            Loader::element('system/antispam/' . $activeLibrary->getSystemAntispamLibraryHandle() . '/form');
+            if ($activeLibrary->hasOptionsForm()) {
+                if ($activeLibrary->getPackageID() > 0) {
+                    Loader::packageElement('system/antispam/' . $activeLibrary->getSystemAntispamLibraryHandle() . '/form', $activeLibrary->getPackageHandle());
+                } else {
+                    Loader::element('system/antispam/' . $activeLibrary->getSystemAntispamLibraryHandle() . '/form');
+                }
+            }
         }
-    }
-}
-    if (is_object($activeLibrary)) {
-        ?>
+
+        if (is_object($activeLibrary)) { ?>
 			<fieldset>
 				<legend style="margin-bottom: 0"><?=t('Log Settings')?></legend>
 				<div class="checkbox">
@@ -46,27 +46,24 @@
 					<span class="help-block"><?=t('Any email address in this box will be notified when spam is detected.')?></span>
 				</div>
 			</fieldset>
-			<?php
-
+		<?php
+        }
+        ?>
+    <?php
+    } else { ?>
+        <p><?=t('You have no anti-spam libraries installed.')?></p>
+    <?php
     }
     ?>
-
-	<?php
-} else {
-    ?>
-		<p><?=t('You have no anti-spam libraries installed.')?></p>
-	<?php
-} ?>
 
 	<div class="ccm-dashboard-form-actions-wrapper">
         <div class="ccm-dashboard-form-actions">
 		    <?=Loader::helper('concrete/ui')->submit(t('Save'), 'submit', 'right', 'btn-primary')?>
         </div>
 	</div>
-
 </form>
 
-<script type="text/javascript">
+<script>
 $(function() {
 	$('select[name=activeLibrary]').change(function() {
 		$('#site-form').submit();
