a0e2db1b5e7a34f629f8e90f3e3adffd76419713
Andrew Embler <andrew@concrete5.org>
2017-06-15 12:56:59 -0700

    Forgot to commit the new form templates

---
+++ concrete/attributes/textarea/form.php
@@ -0,0 +1,19 @@
+<?php
+
+// switch display type here
+if ($akTextareaDisplayMode == 'text' || $akTextareaDisplayMode == '') { ?>
+
+    <?php
+    echo $form->textarea(
+        $view->controller->field('value'),
+        $value,
+        array('rows' => 5)
+    );
+    ?>
+
+<?php } else {
+    echo Core::make('editor')->outputStandardEditor(
+        $view->controller->field('value'),
+        $value
+    );
+}
