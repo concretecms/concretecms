4f8531301a20ddac4b70e7a7e51ba4608a779e60
Derek Cameron <derek@concrete5.co.jp>
2017-06-27 18:09:58 +0900

    fixing composer action causing the field to be rendered twice on some pages

--- concrete/attributes/telephone/controller.php
+++ concrete/attributes/telephone/controller.php
@@ -19,11 +19,7 @@ class Controller extends DefaultController
 
     public function composer()
     {
-        $value = null;
-        if (is_object($this->attributeValue)) {
-            $value = $this->app->make('helper/text')->entities($this->getAttributeValue()->getValue());
-        }
-        echo $this->app->make('helper/form')->telephone($this->field('value'), $value, array('class' => 'span5'));
+        $this->form();
     }
 
     public function getIconFormatter()
