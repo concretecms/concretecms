4ac028e20b23f254f81e33bc679594bdc189cc69
Korvin Szanto <korvinszanto@gmail.com>
2017-06-27 15:32:21 -0700

    Escape page names in page search

--- concrete/elements/pages/search.php
+++ concrete/elements/pages/search.php
@@ -6,14 +6,14 @@ $form = Loader::helper('form');
 
 <script type="text/template" data-template="search-results-table-body">
 <% _.each(items, function (page) {%>
-<tr data-launch-search-menu="<%=page.cID%>" data-page-id="<%=page.cID%>" data-page-name="<%=page.cvName%>">
+<tr data-launch-search-menu="<%=page.cID%>" data-page-id="<%=page.cID%>" data-page-name="<%-page.cvName%>">
 	<td><span class="ccm-search-results-checkbox"><input type="checkbox" class="ccm-flat-checkbox" data-search-checkbox="individual" value="<%=page.cID%>" /></span></td>
 	<% for (i = 0; i < page.columns.length; i++) {
 		var column = page.columns[i];
 		if (column.key == 'cv.cvName') { %>
-			<td class="ccm-search-results-name"><%=column.value%></td>
+			<td class="ccm-search-results-name"><%-column.value%></td>
 		<% } else { %>
-			<td><%=column.value%></td>
+			<td><%-column.value%></td>
 		<% } %>
 	<% } %>
 </tr>
