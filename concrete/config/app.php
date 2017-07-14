a7564ad99f72b5caa64279ba4294c6adaa740407
Andrew Embler <andrew@concrete5.org>
2017-07-14 10:14:36 -0700

    starting work on express search

--- concrete/config/app.php
+++ concrete/config/app.php
@@ -365,6 +365,9 @@ return [
         '/ccm/system/dialogs/user/advanced_search/submit' => ['\Concrete\Controller\Dialog\User\AdvancedSearch::submit'],
         '/ccm/system/dialogs/user/advanced_search/save_preset' => ['\Concrete\Controller\Dialog\User\AdvancedSearch::savePreset'],
 
+        '/ccm/system/dialogs/express/advanced_search/{exEntityID}' => ['\Concrete\Controller\Dialog\Express\AdvancedSearch::view'],
+        '/ccm/system/dialogs/express/advanced_search/add_field/' => ['\Concrete\Controller\Dialog\Express\AdvancedSearch::addField'],
+        '/ccm/system/dialogs/express/advanced_search/submit' => ['\Concrete\Controller\Dialog\Express\AdvancedSearch::submit'],
         /*
          * Conversations
          */
@@ -515,6 +518,10 @@ return [
         '/ccm/system/search/users/current' => ['\Concrete\Controller\Search\Users::searchCurrent'],
         '/ccm/system/search/users/preset/{presetID}' => ['\Concrete\Controller\Search\Users::searchPreset'],
         '/ccm/system/search/users/clear' => ['\Concrete\Controller\Search\Users::clearSearch'],
+
+        '/ccm/system/search/express/basic/{exEntityID}' => ['\Concrete\Controller\Search\Express::searchBasic'],
+        '/ccm/system/search/express/current/{exEntityID}' => ['\Concrete\Controller\Search\Express::searchCurrent'],
+        '/ccm/system/search/express/clear/{exEntityID}' => ['\Concrete\Controller\Search\Express::clearSearch'],
 
         '/ccm/system/search/groups/submit' => ['\Concrete\Controller\Search\Groups::submit'],
 
--- concrete/elements/dashboard/express/entries/header.php
+++ concrete/elements/dashboard/express/entries/header.php
@@ -1,8 +1,34 @@
-<?php
-defined('C5_EXECUTE') or die("Access Denied.");
-?>
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-<a class="btn btn-default" href="<?=$exportURL?>"><i class="fa fa-download"></i> <?=t('Export to CSV') ?></a>
-<a class="btn btn-primary" href="<?=$createURL?>">
-    <i class="fa fa-plus"></i> <?=t('New %s', $entity->getEntityDisplayName())?>
-</a>
+<div class="ccm-header-search-form ccm-ui" data-header="express-search">
+    <form method="get" action="<?php echo URL::to('/ccm/system/search/express/basic', $entity->getID()) ?>">
+        <div class="input-group">
+
+            <div class="ccm-header-search-form-input">
+                <a class="ccm-header-reset-search" href="#"
+                   data-button-action-url="<?= URL::to('/ccm/system/search/express/clear') ?>"
+                   data-button-action="clear-search"><?= t('Reset Search') ?></a>
+                <a class="ccm-header-launch-advanced-search"
+                   href="<?php echo URL::to('/ccm/system/dialogs/express/advanced_search', $entity->getID()) ?>"
+                   data-launch-dialog="advanced-search"><?= t('Advanced') ?></a>
+                <input type="text" class="form-control" autocomplete="off" name="uKeywords"
+                       placeholder="<?= t('Search') ?>">
+            </div>
+
+              <span class="input-group-btn">
+                <button class="btn btn-info" type="submit"><i class="fa fa-search"></i></button>
+              </span>
+        </div>
+
+        <ul class="ccm-header-search-navigation">
+            <li>
+                <a href="<?= $exportURL ?>" class="link-primary">
+                    <i class="fa fa-download"></i> <?= t('Export to CSV') ?>
+                </a>
+            </li>
+            <li><a href="<?= $createURL ?>" class="link-primary"><i class="fa fa-plus"></i> <?= t('New %s',
+                        $entity->getEntityDisplayName()) ?></a></li>
+        </ul>
+
+    </form>
+</div>
