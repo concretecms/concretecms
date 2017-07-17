86a8c6c2f13f353130e54eac4d35d929a58c8a57
Andrew Embler <andrew@concrete5.org>
2017-07-17 11:07:24 -0700

    adding method, more work

---
+++ concrete/controllers/dialog/express/advanced_search.php
@@ -0,0 +1,49 @@
+<?php
+namespace Concrete\Controller\Dialog\Express;
+
+use Concrete\Controller\Dialog\Search\AdvancedSearch as AdvancedSearchController;
+use Concrete\Core\Entity\Search\SavedSearch;
+use Concrete\Core\Search\Field\ManagerFactory;
+
+class AdvancedSearch extends AdvancedSearchController
+{
+
+    public function view($entityID = null)
+    {
+        $entity = \Express::getObjectByID($entityID);
+    }
+
+    protected $supportsSavedSearch = false;
+
+    protected function canAccess()
+    {
+        $dh = $this->app->make('helper/concrete/user');
+        return $dh->canAccessUserSearchInterface();
+    }
+
+    public function getSearchProvider()
+    {
+        return $this->app->make('Concrete\Core\User\Search\SearchProvider');
+    }
+
+    public function getFieldManager()
+    {
+        return ManagerFactory::get('user');
+    }
+
+    public function getSavedSearchBaseURL(SavedSearch $search)
+    {
+        return \URL::to('/ccm/system/search/users/preset', $search->getID());
+    }
+
+    public function getCurrentSearchBaseURL()
+    {
+        return \URL::to('/ccm/system/search/users/current');
+    }
+
+    public function getBasicSearchBaseURL()
+    {
+        return \URL::to('/ccm/system/search/users/basic');
+    }
+
+}
