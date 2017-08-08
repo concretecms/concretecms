93840dd588099dd3ba1048283938131b02ac3c52
MrKarlDilkington <mrkarldilkington@outlook.com>
2017-08-08 18:38:31 -0400

    consolidate add.php and edit.php into form.php

---
+++ concrete/blocks/external_form/form.php
@@ -0,0 +1,18 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+<?php $form = Loader::helper('form'); ?>
+<div class="form-group">
+    <?=$form->label('cstFilename', t('External Form to Include'))?>
+    <select name="filename" id="cstFilename" class="form-control">
+        <option value="">** <?=t('Select a form')?></option>
+    <?php foreach ($filenames as $ffilename) {
+    $selected = ($ffilename == $filename) ? " selected" : "";
+
+    echo '<option value="' . $ffilename . '"' . $selected . '>' . $file->unfilename($ffilename) . '</option>';
+} ?>
+    </select>
+</div>
+<div class="help-block">
+    <p style="word-break: break-all;"><?=t('This is a list of forms found in your external forms directory:')
+    . '<br>'
+    . DIR_FILES_BLOCK_TYPES_FORMS_EXTERNAL; ?></p>
+</div>
