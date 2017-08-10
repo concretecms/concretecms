dd356406d13b89c1e0680fcbc20b525d1d2bbcfe
Andrew Embler <andrew@concrete5.org>
2017-08-10 15:46:53 -0700

    Fix #5793

--- concrete/controllers/backend/get_remote_help.php
+++ concrete/controllers/backend/get_remote_help.php
@@ -12,15 +12,20 @@ class GetRemoteHelp extends UserInterface
     {
         session_write_close();
 
+        $config = \Core::make('config');
         $r = json_encode(array());
-        if ($_REQUEST['q']) {
-            $url = \Config::get('concrete.urls.concrete5') . \Config::get('concrete.urls.paths.menu_help_service');
-            $r = \Core::make("helper/file")->getContents($url . '?q=' . $_REQUEST['q']);
-            if (!$r) {
-                $r = json_encode(array());
+        if ($config->get('concrete.external.intelligent_search_help')) {
+            if ($_REQUEST['q']) {
+                $url = \Config::get('concrete.urls.concrete5') . \Config::get('concrete.urls.paths.menu_help_service');
+                $r = \Core::make("helper/file")->getContents($url . '?q=' . $_REQUEST['q']);
+                if (!$r) {
+                    $r = json_encode(array());
+                }
             }
         }
+
         echo $r;
+
         \Core::shutdown(array('jobs' => true));
     }
 }
