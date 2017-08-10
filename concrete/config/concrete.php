f658b88f01a08bb83241ab1aba906f2e9da19021
Andrew Embler <andrew@concrete5.org>
2017-08-10 15:31:56 -0700

    adding migration for image block update

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -6,9 +6,9 @@ return [
      *
      * @var string
      */
-    'version' => '8.2.2b1',
-    'version_installed' => '8.2.2b1',
-    'version_db' => '20170804000000', // the key of the latest database migration
+    'version' => '8.2.2b2',
+    'version_installed' => '8.2.2b2',
+    'version_db' => '20170810000000', // the key of the latest database migration
 
     /*
      * Installation status
