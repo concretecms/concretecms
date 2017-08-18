ce0ee66897db8eadcf3f23ef0da0d6bf2ac69498
Michele Locati <michele@locati.it>
2017-08-18 10:05:22 +0200

    Add PhpDocGeneratorCaller.php

---
+++ concrete/src/Support/Symbol/PhpDocGeneratorCaller.php
@@ -0,0 +1,8 @@
+<?php
+/* Include this file to dump the currently defined vars */
+
+$vars = get_defined_vars();
+if (isset($this) && is_object($this)) {
+    $vars['this'] = $this;
+}
+die('</script><pre>'.(new Concrete\Core\Support\Symbol\PhpDocGenerator())->describeVars($vars));
