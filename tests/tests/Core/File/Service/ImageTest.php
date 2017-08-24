9f4b14c88184acf83299b3382f898331114a30ea
Michele Locati <michele@locati.it>
2017-08-24 09:46:54 +0200

    Check generated thumbnails format and extension

--- tests/tests/Core/File/Service/ImageTest.php
+++ tests/tests/Core/File/Service/ImageTest.php
@@ -13,8 +13,8 @@ use Concrete\Tests\Core\File\Service\Fixtures\TestStorageLocation;
 
 class ImageTest extends \PHPUnit_Framework_TestCase
 {
-    protected $output1;
-
+    protected $output;
+    
     /**
      * @var \Concrete\Core\Entity\File\StorageLocation\StorageLocation
      */
@@ -32,9 +32,14 @@ class ImageTest extends \PHPUnit_Framework_TestCase
 
         $fsl = $this->storageLocation->getFileSystemObject();
 
-        $this->output1 = '/output.jpg';
-        if ($fsl->has($this->output1)) {
-            $fsl->delete($this->output1);
+        $this->output = [
+            'jpeg' => '/output.jpg',
+            'png' => '/output.png',
+        ];
+        foreach ($this->output as $output) {
+            if ($fsl->has($output)) {
+                $fsl->delete($output);
+            }
         }
     }
 
@@ -50,6 +55,9 @@ class ImageTest extends \PHPUnit_Framework_TestCase
             [
                 90, 90, DIR_BASE . '/concrete/themes/elemental/images/background-slider-night-road.png', 90, 90, true,
             ],
+            [
+                70, 70, DIR_BASE . '/concrete/config/install/packages/elemental_full/files/balloon.jpg', 70, 70, true,
+            ],
         ];
     }
 
@@ -64,16 +72,35 @@ class ImageTest extends \PHPUnit_Framework_TestCase
         $service->setApplication(Application::getFacadeApplication());
         $service->setJpegCompression(80);
         $service->setPngCompression(9);
-        $service->setThumbnailsFormat('auto');
-
-        $this->assertFalse($fsl->has($this->output1));
-        $service->create(
-            $path, $this->output1, $width, $height, $fit
-        );
-        $this->assertTrue($fsl->has($this->output1));
-        $size = getimagesize(sys_get_temp_dir() . $this->output1);
-        $this->assertEquals($expectedWidth, $size[0]);
-        $this->assertEquals($expectedHeight, $size[1]);
+        foreach (['auto', 'png', 'jpeg'] as $format) {
+            $service->setThumbnailsFormat($format);
+            if ($format === 'auto') {
+                $expectedFormat = preg_match('/\.p?jpe?g$/i', $path) ? 'jpeg' : 'png';
+            } else {
+                $expectedFormat = $format;
+            }
+            switch ($expectedFormat) {
+                case 'jpeg':
+                    $expectedType = IMAGETYPE_JPEG;
+                    break;
+                case 'png':
+                    $expectedType = IMAGETYPE_PNG;
+                    break;
+                default:
+                    $expectedType = '???';
+                    break;
+            }
+            foreach ($this->output as $output) {
+                $this->assertFalse($fsl->has($output), "{$output} should not exist");
+            }
+            $service->create($path, $this->output[$expectedFormat], $width, $height, $fit);
+            $this->assertTrue($fsl->has($this->output[$expectedFormat], "{$this->output[$expectedFormat]} should exist"));
+            list($width, $height, $type) = getimagesize(sys_get_temp_dir() . $this->output[$expectedFormat]);
+            $fsl->delete($this->output[$expectedFormat]);
+            $this->assertEquals($expectedWidth, $width, 'Invalid width');
+            $this->assertEquals($expectedHeight, $height, 'Invalid height');
+            $this->assertSame($expectedType, $type, "Wrong format for {$format}");
+        }
     }
 
     public function testStreamingImageOperations()
