cc35d3e814f5d4c785e6a58223a0a2e2e1c58a12
Andrew Embler <andrew@concrete5.org>
2017-09-05 10:24:24 -0700

    Updates to publish from defaults; verbiage tweaks, some bug fixes

--- concrete/single_pages/dashboard/pages/types/output.php
+++ concrete/single_pages/dashboard/pages/types/output.php
@@ -9,23 +9,24 @@
 
 
     <tr>
-        <td><a href="<?php echo $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID());
+        <td style="width: 1px"><a href="<?php echo $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID());
     ?>" target="_blank"><?php echo $pt->getPageTemplateIconImage();
     ?></a></td>
-        <td style="width: 100%; vertical-align: middle"><p class="lead" style="margin-bottom: 0px"><?php echo $pt->getPageTemplateDisplayName();
+        <td style="vertical-align: middle"><p class="lead" style="margin-bottom: 0px"><?php echo $pt->getPageTemplateDisplayName();
     ?></p></td>
-        <td style="vertical-align: middle">
-          <a class="btn btn-default dialog-launch" dialog-title="<?php echo t('Update Defaults');?>"
-            dialog-modal="true"
-            dialog-width="500"
-            dialog-height="auto"
-            href="<?php echo URL::to('/ccm/system/dialogs/type/update_from_type/'.$pagetype->getPageTypeID().'/'.$pt->getPageTemplateID()); ?>">
-            <?php echo t('Update Defaults');?>
-          </a>
+        <td style="width: 250px; vertical-align: middle">
+            <div class="btn-group pull-right">
+<a href="<?php echo $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID());
+    ?>" class="btn btn-default btn-sm"><?php echo t('Edit');
+    ?></a>
+                <a class="btn btn-sm btn-default dialog-launch" dialog-title="<?php echo t('Update Defaults');?>"
+                   dialog-modal="true"
+                   dialog-width="500"
+                   dialog-height="auto"
+                   href="<?php echo URL::to('/ccm/system/dialogs/type/update_from_type/'.$pagetype->getPageTypeID().'/'.$pt->getPageTemplateID()); ?>">
+                    <?php echo t('Publish to Child Pages');?>
+                </a></div>
         </td>
-        <td style="vertical-align: middle"><a href="<?php echo $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID());
-    ?>" class="btn btn-default"><?php echo t('Edit Defaults');
-    ?></a></td>
     </tr>
 
 <?php
--- concrete/views/dialogs/type/update_from_type.php
+++ concrete/views/dialogs/type/update_from_type.php
@@ -5,13 +5,14 @@ $dh = Core::make('helper/date'); /* @var $dh \Concrete\Core\Localization\Service
 
 <?php if ($total == 0) { ?>
 
-    <?php echo t("There are no pages of this type added to your website.")?>
-
+    <?php if (!$view->controller->getRequest()->isPost()) { ?>
+        <?php echo t("There are no pages of this type added to your website.")?>
+    <?php } ?>
 <?php } else { ?>
 
     <form method="post" id="ccmUpdateFromPageTypeForm" data-dialog-form-processing="progressive" data-dialog-form="update-from-page-type" action="<?php echo $controller->action('submit')?>">
 
-    <p><?php echo t('Are you sure? All child pages blocks will be set as in this type.')?></p>
+    <p><?php echo t('This will reset all blocks and their positions on child pages to those that are set in the defaults.')?></p>
 
     <div data-dialog-form-element="progress-bar"></div>
 
