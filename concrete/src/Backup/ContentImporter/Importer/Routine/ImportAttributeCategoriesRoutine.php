0bf3ff1be83168a8f8e316571c725870b9b4e52c
Michele Locati <michele@locati.it>
2017-10-24 09:12:42 +0200

    Associate attribute categories to existing attribute types

--- concrete/src/Backup/ContentImporter/Importer/Routine/ImportAttributeCategoriesRoutine.php
+++ concrete/src/Backup/ContentImporter/Importer/Routine/ImportAttributeCategoriesRoutine.php
@@ -1,6 +1,10 @@
 <?php
 namespace Concrete\Core\Backup\ContentImporter\Importer\Routine;
 
+use Concrete\Core\Attribute\Category\CategoryService;
+use Concrete\Core\Support\Facade\Application;
+use SimpleXMLElement;
+
 class ImportAttributeCategoriesRoutine extends AbstractRoutine
 {
     public function getHandle()
@@ -8,17 +12,41 @@ class ImportAttributeCategoriesRoutine extends AbstractRoutine
         return 'attribute_categories';
     }
 
-    public function import(\SimpleXMLElement $sx)
+    public function import(SimpleXMLElement $xRoot)
     {
-        if (isset($sx->attributecategories)) {
-            foreach ($sx->attributecategories->category as $akc) {
-                $pkg = static::getPackageObject($akc['package']);
-                $akx = \Concrete\Core\Attribute\Key\Category::getByHandle($akc['handle']);
-                if (!is_object($akx)) {
-                    $akx = \Concrete\Core\Attribute\Key\Category::add((string) $akc['handle'], (string) $akc['allow-sets'], $pkg);
+        if (isset($xRoot->attributecategories)) {
+            $app = Application::getFacadeApplication();
+
+            $categoryService = $app->make(CategoryService::class);
+            /* @var CategoryService $categoryService */
+
+            foreach ($xRoot->attributecategories->category as $xCategory) {
+                $acHandle = (string) $xCategory['handle'];
+                $category = $categoryService->getByHandle($acHandle);
+                if ($category === null) {
+                    $pkg = static::getPackageObject($xCategory['package']);
+                    $categoryService->add(
+                        $acHandle,
+                        (int) $xCategory['allow-sets'],
+                        $pkg
+                    );
+                    $category = $categoryService->getByHandle($acHandle);
+                }
+                if (isset($xCategory->attributetypes)) {
+                    $categoryTypes = $category->getAttributeTypes();
+                    $save = false;
+                    foreach ($xCategory->attributetypes->attributetype as $xAttributeType) {
+                        $type = $typeFactory->getByHandle((string) $xAttributeType['handle']);
+                        if ($type !== null && !$categoryTypes->contains($type)) {
+                            $categoryTypes->add($type);
+                            $save = true;
+                        }
+                    }
+                    if ($save === true) {
+                        $entityManager->flush($category);
+                    }
                 }
             }
         }
     }
-
 }
