0a03a7cc452fe962f49978b2d05cef28a21815f4
Michele Locati <michele@locati.it>
2017-10-26 22:13:40 +0200

    Add TextExportableAttributeInterfaces

---
+++ concrete/src/Attribute/MulticolumnTextExportableAttributeInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Concrete\Core\Attribute;
+
+use Concrete\Core\Error\ErrorList;
+
+/**
+ * Attribute controllers should implement this interface if they support importing/exporting to/from multiple plain text fields.
+ *
+ * For simple data see MulticolumnTextExportableAttributeInterface
+ */
+interface MulticolumnTextExportableAttributeInterface
+{
+    /**
+     * Get the handles of the columns that will contain parts of attribute values.
+     * The result must be independent of the specific attribute value instance.
+     *
+     * @return string[]
+     *
+     * @example An address attribute could return ['street', 'city', 'state', 'country']
+     */
+    public function getAttributeTextRepresentationHeaders();
+
+    /**
+     * Get the strings containing the text representation of the attribute value.
+     *
+     * @return string[]
+     *
+     * @example An address attribute could return ['10 Downing Street', 'London', '', 'GB']
+     */
+    public function getAttributeValueTextRepresentation();
+
+    /**
+     * Create an attribute value starting from its text representation.
+     *
+     * @param string[] $textRepresentation the text representation strings of the attribute value
+     * @param ErrorList $warnings An ErrorList instance that the method can add decoding warnings to
+     *
+     * @return \Concrete\Core\Entity\Attribute\Value\Value\AbstractValue|null
+     *
+     * @example In case of an address attributes, this may return
+     * - NULL if $textRepresentation contains only empty strings
+     * - an address attribute value with its parts extracted from $textRepresentation
+     * - add to the $warnings instance a message if the Country code is not valid
+     */
+    public function createAttributeValueFromTextRepresentation(array $textRepresentation, ErrorList $warnings);
+}
---
+++ concrete/src/Attribute/SimpleTextExportableAttributeInterface.php
@@ -0,0 +1,40 @@
+<?php
+namespace Concrete\Core\Attribute;
+
+use Concrete\Core\Error\ErrorList;
+
+/**
+ * Attribute controllers should implement this interface if they support importing/exporting to/from plain text.
+ *
+ * For complex data see MulticolumnTextExportableAttributeInterface
+ */
+interface SimpleTextExportableAttributeInterface
+{
+    /**
+     * Get the text representation of the attribute value.
+     *
+     * @return string
+     *
+     * @example A boolean attribute may return:
+     * - '0' if $value is false
+     * - '1' if $value is true
+     * - '' if $value is not set.
+     */
+    public function getAttributeValueTextRepresentation();
+
+    /**
+     * Create an attribute value starting from its text representation.
+     *
+     * @param string $textRepresentation the text representation of the attribute value
+     * @param ErrorList $warnings An ErrorList instance that the method can add decoding warnings to
+     *
+     * @return \Concrete\Core\Entity\Attribute\Value\Value\AbstractValue|null
+     *
+     * @example In case of booleans attributes, this may return
+     * - an attribute value with false if $textRepresentation is '0'
+     * - an attribute value with true if $textRepresentation is '1'
+     * - NULL if $textRepresentation is an empty string or it's not '0' or '1'
+     * - add to the $warnings instance a message $textRepresentation is not empty and it's not '0' or '1'
+     */
+    public function createAttributeValueFromTextRepresentation($textRepresentation, ErrorList $warnings);
+}
