86e93dda9eb5cfd85e1c513584b46b6755e5eca6
Remo Laubacher <remo.laubacher@gmail.com>
2017-11-09 14:25:53 +0100

    avoid left join in page path join when possible

--- concrete/src/Page/PageList.php
+++ concrete/src/Page/PageList.php
@@ -177,10 +177,13 @@ class PageList extends DatabaseItemList implements PagerProviderInterface, Pagin
 
     public function finalizeQuery(\Doctrine\DBAL\Query\QueryBuilder $query)
     {
+        // avoid slow left join if we have an active page version in which case we always have a page path
+        $joinOperation = $this->pageVersionToRetrieve == self::PAGE_VERSION_ACTIVE ? 'innerJoin' : 'leftJoin';
+
         if ($this->includeAliases) {
             $query->from('Pages', 'p')
                 ->leftJoin('p', 'Pages', 'pa', 'p.cPointerID = pa.cID')
-                ->leftJoin('p', 'PagePaths', 'pp', 'p.cID = pp.cID and pp.ppIsCanonical = true')
+                ->$joinOperation('p', 'PagePaths', 'pp', 'p.cID = pp.cID and pp.ppIsCanonical = true')
                 ->leftJoin('pa', 'PageSearchIndex', 'psi', 'psi.cID = if(pa.cID is null, p.cID, pa.cID)')
                 ->leftJoin('p', 'PageTypes', 'pt', 'pt.ptID = if(pa.cID is null, p.ptID, pa.ptID)')
                 ->leftJoin('p', 'CollectionSearchIndexAttributes', 'csi', 'csi.cID = if(pa.cID is null, p.cID, pa.cID)')
@@ -189,7 +192,7 @@ class PageList extends DatabaseItemList implements PagerProviderInterface, Pagin
                 ->andWhere('p.cIsTemplate = 0 or pa.cIsTemplate = 0');
         } else {
             $query->from('Pages', 'p')
-                ->leftJoin('p', 'PagePaths', 'pp', '(p.cID = pp.cID and pp.ppIsCanonical = true)')
+                ->$joinOperation('p', 'PagePaths', 'pp', '(p.cID = pp.cID and pp.ppIsCanonical = true)')
                 ->leftJoin('p', 'PageSearchIndex', 'psi', 'p.cID = psi.cID')
                 ->leftJoin('p', 'PageTypes', 'pt', 'p.ptID = pt.ptID')
                 ->leftJoin('c', 'CollectionSearchIndexAttributes', 'csi', 'c.cID = csi.cID')
