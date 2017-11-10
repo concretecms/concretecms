38a84123c51076a74d875043a9618107b3f6f79c
Andrew Embler <andrew@concrete5.org>
2017-11-09 20:40:51 -0800

    update migration

--- concrete/src/Updater/Migrations/Migrations/Version20171110032423.php
+++ concrete/src/Updater/Migrations/Migrations/Version20171110032423.php
@@ -2,9 +2,21 @@
 
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
+use Concrete\Core\Attribute\Key\EventKey;
 use Concrete\Core\Backup\ContentImporter;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Concrete\Core\Entity\Attribute\Value\EventValue;
+use Concrete\Core\Entity\Calendar\Calendar;
+use Concrete\Core\Entity\Calendar\CalendarEvent;
+use Concrete\Core\Entity\Calendar\CalendarEventOccurrence;
+use Concrete\Core\Entity\Calendar\CalendarEventRepetition;
+use Concrete\Core\Entity\Calendar\CalendarEventVersion;
+use Concrete\Core\Entity\Calendar\CalendarEventVersionOccurrence;
+use Concrete\Core\Entity\Calendar\CalendarEventVersionRepetition;
+use Concrete\Core\Entity\Calendar\CalendarEventWorkflowProgress;
+use Concrete\Core\Entity\Calendar\CalendarPermissionAssignment;
+use Concrete\Core\Entity\Calendar\CalendarRelatedEvent;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -16,6 +28,20 @@ class Version20171110032423 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
+        $this->refreshEntities([
+            EventKey::class,
+            EventValue::class,
+            Calendar::class,
+            CalendarEvent::class,
+            CalendarEventOccurrence::class,
+            CalendarEventRepetition::class,
+            CalendarEventVersion::class,
+            CalendarEventVersionOccurrence::class,
+            CalendarEventVersionRepetition::class,
+            CalendarEventWorkflowProgress::class,
+            CalendarRelatedEvent::class,
+            CalendarPermissionAssignment::class,
+        ]);
         $importer = new ContentImporter();
         $importer->importContentFile(DIR_BASE_CORE . '/config/install/upgrade/calendar.xml');
     }
