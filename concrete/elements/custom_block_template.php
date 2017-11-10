37d2ed041a0aff6d428b7c7075dac3551f86d4c8
Remo Laubacher <remo.laubacher@gmail.com>
2017-11-10 07:50:07 +0100

    move styles into less file

--- concrete/elements/custom_block_template.php
+++ concrete/elements/custom_block_template.php
@@ -2,16 +2,14 @@
 
 <form method="post" action="<?=$saveAction?>" id="ccm-inline-design-form">
     <ul class="ccm-inline-toolbar ccm-ui">
-        <li>
-            <div class="ccm-inline-select-container">
-                <select id="bFilename" name="bFilename" class="form-control" style="border: none; height: 27px; padding: 2px 28px 2px 2px; background-position: right 5px top 5px;">
-                    <option value="">(<?=t('None selected')?>)</option>
-                    <?php
-                    foreach ($templates as $tpl) { ?>
-                        <option value="<?=$tpl->getTemplateFileFilename()?>" <?php if ($bFilename == $tpl->getTemplateFileFilename()) { ?> selected <?php } ?>><?=$tpl->getTemplateFileDisplayName()?></option>
-                    <?php } ?>
-                </select>
-            </div>
+        <li class="ccm-inline-toolbar-select">
+            <select id="bFilename" name="bFilename" class="form-control">
+                <option value="">(<?=t('None selected')?>)</option>
+                <?php
+                foreach ($templates as $tpl) { ?>
+                    <option value="<?=$tpl->getTemplateFileFilename()?>" <?php if ($bFilename == $tpl->getTemplateFileFilename()) { ?> selected <?php } ?>><?=$tpl->getTemplateFileDisplayName()?></option>
+                <?php } ?>
+            </select>
         </li>
         <li class="ccm-inline-toolbar-button ccm-inline-toolbar-button-cancel">
             <button data-action="cancel-design" type="button" class="btn btn-mini"><?=t("Cancel")?></button>
