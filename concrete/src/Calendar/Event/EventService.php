76187013196c405bf0153884fd49aab51655de5b
Andrew Embler <andrew@concrete5.org>
2017-11-15 20:28:07 -0800

    making detail page change properly on duplicate

--- concrete/src/Calendar/Event/EventService.php
+++ concrete/src/Calendar/Event/EventService.php
@@ -3,6 +3,7 @@ namespace Concrete\Core\Calendar\Event;
 
 use Concrete\Core\Config\Repository\Repository;
 use Concrete\Core\Foundation\Repetition\Comparator;
+use Concrete\Core\Calendar\Event\Event\DuplicateEventEvent;
 use Concrete\Core\Page\Page;
 use Concrete\Core\Page\Type\Type;
 use Concrete\Core\User\User;
@@ -12,6 +13,7 @@ use Concrete\Core\Entity\Calendar\Calendar;
 use Concrete\Core\Entity\Calendar\CalendarEvent;
 use Concrete\Core\Entity\Calendar\CalendarEventVersion;
 use Concrete\Core\Entity\Calendar\CalendarRelatedEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class EventService
 {
@@ -19,17 +21,19 @@ class EventService
     protected $config;
     protected $occurrenceFactory;
     protected $eventCategory;
+    protected $dispatcher;
 
     const EVENT_VERSION_RECENT = 1;
     const EVENT_VERSION_APPROVED = 2;
     const INTERVAL_VERSION = 1200; // 20 minutes
 
-    public function __construct(EntityManagerInterface $entityManagerInterface, Repository $config, EventOccurrenceFactory $occurrenceFactory, EventCategory $eventCategory)
+    public function __construct(EntityManagerInterface $entityManagerInterface, Repository $config, EventOccurrenceFactory $occurrenceFactory, EventCategory $eventCategory, EventDispatcher $dispatcher)
     {
         $this->entityManager = $entityManagerInterface;
         $this->config = $config;
         $this->occurrenceFactory = $occurrenceFactory;
         $this->eventCategory = $eventCategory;
+        $this->dispatcher = $dispatcher;
     }
 
     public function getByID($id, $retrieveVersion = self::EVENT_VERSION_APPROVED)
@@ -197,7 +201,12 @@ class EventService
         
         $this->generateDefaultOccurrences($version);
 
-        return $event;
+        $duplicateEvent = new DuplicateEventEvent($this);
+        $duplicateEvent->setEntityManager($this->entityManager);
+        $duplicateEvent->setNewEventObject($new);
+        $this->dispatcher->dispatch('on_calendar_event_duplicate', $duplicateEvent);
+
+        return $new;
     }
 
     public function delete(CalendarEvent $event)
