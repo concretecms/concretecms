8e88b39dec20a2ae15c54aa9bdd67116d9d4f81f
Michele Locati <michele@locati.it>
2017-11-23 18:57:27 +0100

    Check that there are no unexpected executable files

---
+++ tests/tests/File/ExecutableFilesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Concrete\Tests\File;
+
+use PHPUnit_Framework_TestCase;
+
+class ExecutableFilesTest extends PHPUnit_Framework_TestCase
+{
+    public function testExecutableFiles()
+    {
+        if (DIRECTORY_SEPARATOR === '\\') {
+            $this->markTestSkipped('Testing executable files requires a Posix environment');
+        }
+        $rc = -1;
+        $output = [];
+        @exec('find ' . escapeshellarg(DIR_BASE) . ' -type f -executable', $output, $rc);
+        if ($rc !== 0) {
+            $this->markTestSkipped('Failed to retrieve the list of executable files (' . implode("\n", $output) . ')');
+        }
+        $output = array_map(function ($file) { return substr($file, strlen(DIR_BASE) + 1); }, $output);
+        $actual = array_filter($output, function ($file) {
+            return strpos($file, '.git/') !== 0
+                && strpos($file, 'concrete/vendor/') !== 0
+                && strpos($file, 'packages/') !== 0
+                && strpos($file, 'updates/') !== 0
+            ;
+        });
+        $actual = array_values($actual);
+        $expected = [
+            'concrete/bin/concrete5',
+        ];
+        $this->assertSame(
+            $expected,
+            $actual,
+            'Checking that only selected files are executable'
+        );
+    }
+}
