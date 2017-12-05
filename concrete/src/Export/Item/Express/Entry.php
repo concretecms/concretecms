22b323fdc8b9fe0295f340b3bc7d15bd3b8b4de6
Andrew Embler <andrew@concrete5.org>
2017-12-05 13:35:22 -0800

    removing unused code

--- concrete/src/Export/Item/Express/Entry.php
+++ concrete/src/Export/Item/Express/Entry.php
@@ -69,42 +69,6 @@ class Entry implements ItemInterface
                 }
             }
         }
-
-        /*
-        $node->addAttribute('id', $entity->getID());
-        $node->addAttribute('handle', $entity->getHandle());
-        $node->addAttribute('plural_handle', $entity->getPluralHandle());
-        $node->addAttribute('name', $entity->getName());
-        $node->addAttribute('include_in_public_list', $entity->getIncludeInPublicList() ? '1' : '');
-        $node->addAttribute('description', h($entity->getDescription()));
-        $node->addAttribute('default_view_form', $entity->getDefaultViewForm()->getID());
-        $node->addAttribute('default_edit_form', $entity->getDefaultEditForm()->getID());
-
-        $results = Node::getByID($entity->getEntityResultsNodeId());
-        if (is_object($results)) {
-            $parent = $results->getTreeNodeParentObject();
-            $path = $parent->getTreeNodeDisplayPath();
-            $node->addAttribute('results-folder', $path);
-        }
-
-        $associations = $entity->getAssociations();
-        if (count($associations)) {
-            $asn = $node->addChild('associations');
-            foreach($associations as $association) {
-                $exporter = $association->getExporter();
-                $exporter->export($association, $asn);
-            }
-        }
-
-        $forms = $entity->getForms();
-        if (count($forms)) {
-            $fsn = $node->addChild('forms');
-            foreach($forms as $form) {
-                $exporter = $form->getExporter();
-                $exporter->export($form, $fsn);
-            }
-        }
-        */
         return $node;
 
     }
