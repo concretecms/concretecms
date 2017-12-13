478e9bb5bce8ab70ac75274bfee7663b7a5c507c
Michele Locati <michele@locati.it>
2017-12-13 16:02:26 +0100

    Optimize Version::getObjectAttributeCategory()

--- concrete/src/Entity/File/Version.php
+++ concrete/src/Entity/File/Version.php
@@ -1168,7 +1168,9 @@ class Version implements ObjectInterface
      */
     public function getObjectAttributeCategory()
     {
-        return Core::make(FileCategory::class);
+        $app = Application::getFacadeApplication();
+
+        return $app->make(FileCategory::class);
     }
 
     /**
