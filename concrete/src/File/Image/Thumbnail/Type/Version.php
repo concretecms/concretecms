1d3136799b4c24563b0b637969244c73f98d35ba
Michele Locati <michele@locati.it>
2017-12-13 11:01:23 +0100

    Add PHPDoc and and property type checking

--- concrete/src/File/Image/Thumbnail/Type/Version.php
+++ concrete/src/File/Image/Thumbnail/Type/Version.php
@@ -13,59 +13,127 @@ use Concrete\Core\Support\Facade\Application;
  */
 class Version
 {
+    /**
+     * The name of the directory that contains the thumbnails.
+     *
+     * @var string
+     */
     protected $directoryName;
+
+    /**
+     * The handle of the thumbnail type version.
+     *
+     * @var string
+     */
     protected $handle;
+
+    /**
+     * The name of the thumbnail type version.
+     *
+     * @var string
+     */
     protected $name;
+
+    /**
+     * The thumbnail sizing mode (one of the \Concrete\Core\Entity\File\Image\Thumbnail\Type\Type::RESIZE_... constants).
+     *
+     * @var string
+     */
     protected $sizingMode;
+
+    /**
+     * The width of the thumbnails (or the maximum width in case of proportional sizing).
+     *
+     * @var int|null
+     */
     protected $width;
+
+    /**
+     * The height of the thumbnails (or the maximum height in case of proportional sizing).
+     *
+     * @var int|null
+     */
     protected $height;
+
+    /**
+     * Is this a high-DPI thumbnail type version?
+     *
+     * @var bool
+     */
     protected $isDoubledVersion;
 
+    /**
+     * Initialize the instance.
+     *
+     * @param string $directoryName the name of the directory that contains the thumbnails
+     * @param string $handle the handle of the thumbnail type version
+     * @param string $name the name of the thumbnail type versio
+     * @param int|null $width the width of the thumbnails (or the maximum width in case of proportional sizing)
+     * @param int|null $height the height of the thumbnails (or the maximum height in case of proportional sizing)
+     * @param bool $isDoubledVersion is this a high-DPI thumbnail type version?
+     * @param string $sizingMode the thumbnail sizing mode (one of the \Concrete\Core\Entity\File\Image\Thumbnail\Type\Type::RESIZE_... constants)
+     */
     public function __construct($directoryName, $handle, $name, $width, $height, $isDoubledVersion = false, $sizingMode = ThumbnailType::RESIZE_DEFAULT)
     {
-        $this->handle = $handle;
-        $this->name = $name;
-        $this->sizingMode = $sizingMode;
-        $this->width = $width;
-        $this->height = $height;
-        $this->directoryName = $directoryName;
+        $this->setHandle($handle);
+        $this->setName($name);
+        $this->setSizingMode($sizingMode);
+        $this->setWidth($width);
+        $this->setHeight($height);
+        $this->setDirectoryName($directoryName);
         $this->isDoubledVersion = (bool) $isDoubledVersion;
     }
 
     /**
-     * @param mixed $handle
+     * Set the handle of the thumbnail type version.
+     *
+     * @param string $handle
      */
     public function setHandle($handle)
     {
-        $this->handle = $handle;
+        $this->handle = (string) $handle;
     }
 
     /**
-     * @param mixed $name
+     * Set the name of the thumbnail type version.
+     *
+     * @param string $name
      */
     public function setName($name)
     {
-        $this->name = $name;
+        $this->name = (string) $name;
     }
 
     /**
-     * @param mixed $sizingMode
+     * Set the thumbnail sizing mode.
+     *
+     * @param string $sizingMode One of the \Concrete\Core\Entity\File\Image\Thumbnail\Type\Type::RESIZE_... constants
      */
     public function setSizingMode($sizingMode)
     {
-        $this->sizingMode = $sizingMode;
+        $this->sizingMode = (string) $sizingMode;
     }
 
     /**
-     * @param mixed $height
+     * Set the height of the thumbnails (or the maximum height in case of proportional sizing).
+     *
+     * @param int|null $height
      */
     public function setHeight($height)
     {
-        $this->height = $height;
+        $this->height = null;
+        if ($height && is_numeric($height)) {
+            $height = (int) $height;
+            if ($height > 0) {
+                $this->height = $height;
+            }
+        }
     }
 
     /**
-     * @return mixed
+     * Get the handle of the thumbnail type version.
+     *
+     * @return string
      */
     public function getHandle()
     {
@@ -73,6 +141,8 @@ class Version
     }
 
     /**
+     * Get the name of the thumbnail type version.
+     *
      * @return string
      */
     public function getName()
@@ -81,13 +151,20 @@ class Version
     }
 
     /**
-     * @return string
+     * Get the thumbnail sizing mode.
+     *
+     * @return string One of the \Concrete\Core\Entity\File\Image\Thumbnail\Type\Type::RESIZE_... constants
      */
     public function getSizingMode()
     {
         return $this->sizingMode;
     }
 
+    /**
+     * Get the display name of the thumbnail sizing mode.
+     *
+     * @return string
+     */
     public function getSizingModeDisplayName()
     {
         $sizingModeDisplayNames = [
@@ -98,7 +175,9 @@ class Version
         return $sizingModeDisplayNames[$this->getSizingMode()];
     }
 
-    /** Returns the display name for this thumbnail type version (localized and escaped accordingly to $format)
+    /**
+     * Get the display name for this thumbnail type version (localized and escaped accordingly to $format).
+     *
      * @param string $format = 'html'
      *    Escape the result in html format (if $format is 'html').
      *    If $format is 'text' or any other value, the display name won't be escaped.
@@ -121,15 +200,25 @@ class Version
     }
 
     /**
-     * @param mixed $width
+     * Set the width of the thumbnails (or the maximum width in case of proportional sizing).
+     *
+     * @param int|null $width
      */
     public function setWidth($width)
     {
-        $this->width = $width;
+        $this->width = null;
+        if ($width && is_numeric($width)) {
+            $width = (int) $width;
+            if ($width > 0) {
+                $this->width = $width;
+            }
+        }
     }
 
     /**
-     * @return mixed
+     * Get the width of the thumbnails (or the maximum width in case of proportional sizing).
+     *
+     * @return int|null
      */
     public function getWidth()
     {
@@ -137,7 +226,9 @@ class Version
     }
 
     /**
-     * @return mixed
+     * Get the height of the thumbnails (or the maximum height in case of proportional sizing).
+     *
+     * @return int|null
      */
     public function getHeight()
     {
@@ -145,21 +236,32 @@ class Version
     }
 
     /**
-     * @param mixed $directoryName
+     * Set the name of the directory that contains the thumbnails.
+     *
+     * @param string $directoryName
      */
     public function setDirectoryName($directoryName)
     {
-        $this->directoryName = $directoryName;
+        $this->directoryName = (string) $directoryName;
     }
 
     /**
-     * @return mixed
+     * Get the name of the directory that contains the thumbnails.
+     *
+     * @return string
      */
     public function getDirectoryName()
     {
         return $this->directoryName;
     }
 
+    /**
+     * Get a thumbnail type version given its handle.
+     *
+     * @param string $handle
+     *
+     * @return \Concrete\Core\File\Image\Thumbnail\Type\Version|null
+     */
     public static function getByHandle($handle)
     {
         $list = Type::getVersionList();
@@ -170,6 +272,15 @@ class Version
         }
     }
 
+    /**
+     * Get the path to the thumbnail of a file version (relative to the to the storage location root).
+     *
+     * @param FileVersion $fv the file version for which you want the path of the associated thumbnail
+     *
+     * @return string
+     *
+     * @example /thumbnails/file_manager_detail/0000/0000/0000/file.png
+     */
     public function getFilePath(FileVersion $fv)
     {
         $app = Application::getFacadeApplication();
