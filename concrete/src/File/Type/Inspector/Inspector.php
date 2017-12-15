6428d195b979b8f287f3e8b22be3aebccaffbfaa
Michele Locati <michele@locati.it>
2017-12-15 18:47:29 +0100

    File inspectors: improve, add PHPDoc, and get rid of deprecated methods

--- concrete/src/File/Type/Inspector/Inspector.php
+++ concrete/src/File/Type/Inspector/Inspector.php
@@ -4,7 +4,16 @@ namespace Concrete\Core\File\Type\Inspector;
 
 use Concrete\Core\Entity\File\Version;
 
+/**
+ * Abstract class that all the file inspectors must extend.
+ */
 abstract class Inspector
 {
+    /**
+     * This method is called when a File\Version class refreshes its attributes.
+     * This can be used to update the File\Version attributes as well as its contents.
+     *
+     * @param \Concrete\Core\Entity\File\Version $fv
+     */
     abstract public function inspect(Version $fv);
 }
