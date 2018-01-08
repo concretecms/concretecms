6c0edcd0ae4aae73c33fd462bc9f0811d24d79b7
Jeremy Rogers <info@jero.co.nz>
2018-01-08 15:16:22 +1300

    Fix nesting in topic list

--- concrete/blocks/topic_list/view.php
+++ concrete/blocks/topic_list/view.php
@@ -18,18 +18,19 @@
                 ?><ul class="ccm-block-topic-list-list"><?php
                 foreach ($node->getChildNodes() as $topic) {
                     if ($topic instanceof \Concrete\Core\Tree\Node\Type\Category) {
-                        ?><li><?php echo $topic->getTreeNodeDisplayName(); ?></li>
+                        ?><li><?php echo $topic->getTreeNodeDisplayName(); ?>
                         <?php
                     } else {
                         ?><li><a href="<?php echo $view->controller->getTopicLink($topic); ?>" <?php
                         if (isset($selectedTopicID) && $selectedTopicID == $topic->getTreeNodeID()) {
                             ?> class="ccm-block-topic-list-topic-selected"<?php
                         }
-                        ?>><?php echo $topic->getTreeNodeDisplayName(); ?></a></li><?php
+                        ?>><?php echo $topic->getTreeNodeDisplayName(); ?></a><?php
                     }
                     if (count($topic->getChildNodes())) {
                         $walk($topic);
                     } ?>
+                    </li>
                     <?php
                 }
                 ?></ul><?php
