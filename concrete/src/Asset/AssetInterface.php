c0e0ac61ad1e009439006ceaf7b63a80cedaa636
Michele Locati <michele@locati.it>
2018-01-12 11:33:46 +0100

    Reorder AssetInterface methods

--- concrete/src/Asset/AssetInterface.php
+++ concrete/src/Asset/AssetInterface.php
@@ -50,11 +50,32 @@ interface AssetInterface
     public function getAssetDefaultPosition();
 
     /**
+     * Set the position of this asset (\Concrete\Core\Asset\AssetInterface::ASSET_POSITION_HEADER or \Concrete\Core\Asset\AssetInterface::ASSET_POSITION_FOOTER).
+     *
+     * @param string $position
+     */
+    public function setAssetPosition($position);
+
+    /**
+     * Get the position of this asset (\Concrete\Core\Asset\AssetInterface::ASSET_POSITION_HEADER or \Concrete\Core\Asset\AssetInterface::ASSET_POSITION_FOOTER).
+     *
+     * @return string
+     */
+    public function getAssetPosition();
+
+    /**
      * Get the unique identifier of the asset type.
      *
      * @return string
      */
     public function getAssetType();
+
+    /**
+     * Get the handle of this asset (together with getAssetType, identifies this asset).
+     *
+     * @return string
+     */
+    public function getAssetHandle();
 
     /**
      * Get the resulting type of the asset (\Concrete\Core\Asset\AssetInterface::OUTPUTASSETTYPE_CSS, \Concrete\Core\Asset\AssetInterface::OUTPUTASSETTYPE_JAVASCRIPT or other values).
@@ -64,15 +85,25 @@ interface AssetInterface
     public function getOutputAssetType();
 
     /**
-     * Asset-type specific post-processing.
+     * Is this asset a locally available file (accessible with the getAssetPath method)?
      *
-     * @param \Concrete\Core\Asset\AssetInterface[] $assets The original assets
-     *
-     * @return \Concrete\Core\Asset\AssetInterface[] The final assets
-     *
-     * @example Compress JavaScripts, merge CSS files...
+     * @param bool $isLocal
      */
-    public static function process($assets);
+    public function setAssetIsLocal($isLocal);
+
+    /**
+     * Is this asset a locally available file (accessible with the getAssetPath method)?
+     *
+     * @return bool
+     */
+    public function isAssetLocal();
+
+    /**
+     * Does this asset support minification?
+     *
+     * @param bool $minify
+     */
+    public function setAssetSupportsMinification($minify);
 
     /**
      * Does this asset support minification?
@@ -84,93 +115,16 @@ interface AssetInterface
     /**
      * Can this asset be combined with other assets?
      *
-     * @return bool
-     */
-    public function assetSupportsCombination();
-
-    /**
-     * Does this asset support minification?
-     *
-     * @param bool $minify
-     */
-    public function setAssetSupportsMinification($minify);
-
-    /**
-     * Can this asset be combined with other assets?
-     *
      * @param bool $combine
      */
     public function setAssetSupportsCombination($combine);
 
     /**
-     * Get the URL of this asset.
+     * Can this asset be combined with other assets?
      *
-     * @return string
+     * @return bool
      */
-    public function getAssetURL();
-
-    /**
-     * Get a string that unambiguously identifies this asset.
-     *
-     * @return string
-     */
-    public function getAssetHashKey();
-
-    /**
-     * Get the path to this asset.
-     *
-     * @return string
-     */
-    public function getAssetPath();
-
-    /**
-     * Get the handle of this asset (together with getAssetType, identifies this asset).
-     *
-     * @return string
-     */
-    public function getAssetHandle();
-
-    /**
-     * Get the name of the file of this asset.
-     *
-     * @return string
-     */
-    public function getAssetFilename();
-
-    /**
-     * Set the version of this asset.
-     *
-     * @param string $version
-     */
-    public function setAssetVersion($version);
-
-    /**
-     * Set the URL of the source files this asset has been built from (useful to understand the origin of this asset).
-     *
-     * @param string[] $paths
-     */
-    public function setCombinedAssetSourceFiles($paths);
-
-    /**
-     * Get the version of this asset.
-     *
-     * @return string
-     */
-    public function getAssetVersion();
-
-    /**
-     * Set the position of this asset (\Concrete\Core\Asset\AssetInterface::ASSET_POSITION_HEADER or \Concrete\Core\Asset\AssetInterface::ASSET_POSITION_FOOTER).
-     *
-     * @param string $position
-     */
-    public function setAssetPosition($position);
-
-    /**
-     * Set the package that defines this asset.
-     *
-     * @param \Concrete\Core\Package\Package|\Concrete\Core\Entity\Package|null|false $pkg
-     */
-    public function setPackageObject($pkg);
+    public function assetSupportsCombination();
 
     /**
      * Set the URL of this asset.
@@ -180,6 +134,13 @@ interface AssetInterface
     public function setAssetURL($url);
 
     /**
+     * Get the URL of this asset.
+     *
+     * @return string
+     */
+    public function getAssetURL();
+
+    /**
      * Set the path to this asset.
      *
      * @param string $path
@@ -187,32 +148,11 @@ interface AssetInterface
     public function setAssetPath($path);
 
     /**
-     * Get the path of the parent "folder" that contains this asset.
+     * Get the path to this asset.
      *
      * @return string
      */
-    public function getAssetURLPath();
-
-    /**
-     * Is this asset a locally available file (accessible with the getAssetPath method)?
-     *
-     * @return bool
-     */
-    public function isAssetLocal();
-
-    /**
-     * Is this asset a locally available file (accessible with the getAssetPath method)?
-     *
-     * @param bool $isLocal
-     */
-    public function setAssetIsLocal($isLocal);
-
-    /**
-     * Get the position of this asset (\Concrete\Core\Asset\AssetInterface::ASSET_POSITION_HEADER or \Concrete\Core\Asset\AssetInterface::ASSET_POSITION_FOOTER).
-     *
-     * @return string
-     */
-    public function getAssetPosition();
+    public function getAssetPath();
 
     /**
      * If the asset is local, set its path and URL starting from the relative path. If it's not local, set its URL.
@@ -222,11 +162,60 @@ interface AssetInterface
     public function mapAssetLocation($path);
 
     /**
+     * Get the path of the parent "folder" that contains this asset.
+     *
+     * @return string
+     */
+    public function getAssetURLPath();
+
+    /**
+     * Get the name of the file of this asset.
+     *
+     * @return string
+     */
+    public function getAssetFilename();
+
+    /**
+     * Get a string that unambiguously identifies this asset.
+     *
+     * @return string
+     */
+    public function getAssetHashKey();
+
+    /**
+     * Set the version of this asset.
+     *
+     * @param string $version
+     */
+    public function setAssetVersion($version);
+
+    /**
+     * Get the version of this asset.
+     *
+     * @return string
+     */
+    public function getAssetVersion();
+
+    /**
+     * Set the package that defines this asset.
+     *
+     * @param \Concrete\Core\Package\Package|\Concrete\Core\Entity\Package|null|false $pkg
+     */
+    public function setPackageObject($pkg);
+
+    /**
      * Get the contents of the asset (if applicable).
      *
      * @return string|null
      */
     public function getAssetContents();
+
+    /**
+     * Set the URL of the source files this asset has been built from (useful to understand the origin of this asset).
+     *
+     * @param string[] $paths
+     */
+    public function setCombinedAssetSourceFiles($paths);
 
     /**
      * Register the asset properties.
@@ -244,4 +233,15 @@ interface AssetInterface
      * @param \Concrete\Core\Package\Package|\Concrete\Core\Entity\Package|string|null|false $pkg the package that defines this asset (or its handle)
      */
     public function register($filename, $args, $pkg = false);
+
+    /**
+     * Asset-type specific post-processing.
+     *
+     * @param \Concrete\Core\Asset\AssetInterface[] $assets The original assets
+     *
+     * @return \Concrete\Core\Asset\AssetInterface[] The final assets
+     *
+     * @example Compress JavaScripts, merge CSS files...
+     */
+    public static function process($assets);
 }
