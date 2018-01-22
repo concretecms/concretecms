5a566f34ab973b44f9e95fc369339d9d620603d8
Andrew Embler <andrew@concrete5.org>
2018-01-22 11:01:37 -0800

    tweaking API

--- concrete/src/Events/Broadcast/Driver/DriverInterface.php
+++ concrete/src/Events/Broadcast/Driver/DriverInterface.php
@@ -5,5 +5,7 @@ namespace Concrete\Core\Events\Broadcast\Driver;
 interface DriverInterface
 {
 
+    public function broadcast($channel, $message);
+
 
 }
--- concrete/src/Events/EventDispatcher.php
+++ concrete/src/Events/EventDispatcher.php
@@ -2,11 +2,10 @@
 
 namespace Concrete\Core\Events;
 
+use Concrete\Core\Events\Broadcast\BroadcastableEventInterface;
+use Concrete\Core\Events\Broadcast\Broadcaster;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcher as SymfonyEventDispatcher;
-use Concrete\Core\Events\Broadcast\BroadcasterFactory;
-use Concrete\Core\Events\Broadcast\BroadcastSerializer;
-
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class EventDispatcher extends SymfonyEventDispatcher
@@ -14,26 +13,35 @@ class EventDispatcher extends SymfonyEventDispatcher
 
     protected $broadcasters = [];
 
-    protected $broadcasterFactory;
+    /**
+     * @var Broadcaster
+     */
+    protected $broadcaster;
 
-    public function __construct(BroadcasterFactory $broadcasterFactory)
+    public function __construct(Broadcaster $broadcaster)
     {
-        $this->broadcasterFactory = $broadcasterFactory;
+        $this->broadcaster = $broadcaster;
     }
 
-    public function broadcast(array $events, BroadcastSerializer $serializer)
+    /**
+     * {@inheritdoc}
+     */
+    public function dispatch($eventName, Event $event = null)
     {
-        $broadcaster = $this->broadcasterFactory->createDriver();
-        foreach($events as $event) {
-            if (in_array($event, $this->broadcasters)) {
-                continue; // don't do it twice.
-            }
-
-            $this->addListener($event, function($object) use ($event, $serializer, $broadcaster) {
-                $json = $serializer->serializeForBroadcast($object);
-                $broadcaster->broadcast($event, $json);
-            });
+        if (null === $event) {
+            $event = new Event();
         }
+
+        if ($event instanceof BroadcastableEventInterface) {
+            $this->broadcaster->broadcast($event->getBroadcastChannel(), $event);
+        }
+
+        if ($listeners = $this->getListeners($eventName)) {
+            $this->doDispatch($listeners, $eventName, $event);
+        }
+
+        return $event;
     }
+
 
 }
