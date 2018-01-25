dde87648a0a3d4c88381d7d67dea63cff4f57805
Michele Locati <michele@locati.it>
2018-01-25 08:07:21 +0100

    Fix declaration of ManagedSchemaChangerInterface

--- concrete/src/Updater/Migrations/ManagedSchemaChangerInterface.php
+++ concrete/src/Updater/Migrations/ManagedSchemaChangerInterface.php
@@ -3,8 +3,8 @@
 namespace Concrete\Core\Updater\Migrations;
 
 /**
- * Interface that migrations should implement when database upgrades downgrades are performed using only a DBAL Schema object.
+ * Interface that migrations should implement when database upgrades and downgrades are performed using only a DBAL Schema object.
  */
-interface ManagedSchemaChangerInterface
+interface ManagedSchemaChangerInterface extends ManagedSchemaUpgraderInterface, ManagedSchemaDowngraderInterface
 {
 }
