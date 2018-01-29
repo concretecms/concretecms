35172e583bda968cd1d6935b0b0904fcbf89787d
Michele Locati <michele@locati.it>
2018-01-29 08:30:38 +0100

    Remove warning about dangerous execution of "--rerun"
    We introduced RepeatableMigrationInterface: the "--rerun" option only
    re-executes the migrations marked with that interface, so rerunning
    previous migrations is safe.

--- concrete/src/Console/Command/UpdateCommand.php
+++ concrete/src/Console/Command/UpdateCommand.php
@@ -10,7 +10,6 @@ use Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class UpdateCommand extends Command
 {
@@ -24,7 +23,6 @@ class UpdateCommand extends Command
             ->addOption('rerun', null, InputOption::VALUE_NONE, '(Re)apply already executed migrations')
             ->addOption('after', 'a', InputOption::VALUE_REQUIRED, '(Re)apply migrations after a specific version or migration (requires --rerun)')
             ->addOption('since', 's', InputOption::VALUE_REQUIRED, '(Re)apply migrations starting from a specific version or migration (requires --rerun)')
-            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force the update')
             ->setHelp(<<<EOT
 Examples:
 
@@ -45,8 +43,6 @@ Examples:
 
   ./concrete/bin/concrete5 c5:update --rerun --since=20171218000000
     Execute the migrations starting from the 20171218000000 migration (even if they have already been marked as executed)
-
-Please remark that re-executing old migrations (with the --after or --since options) may be dangerous.
 
 Returns codes:
   0 operation completed successfully
@@ -84,20 +80,6 @@ EOT
             if ($configuration->getForcedInitialMigration() !== null) {
                 if ($output->getVerbosity() >= OutputInterface::OUTPUT_NORMAL) {
                     $output->writeln(sprintf('Initial migration to be executed: %s', $configuration->getForcedInitialMigration()->getVersion()));
-                }
-                if (!$input->getOption('force')) {
-                    if (!$input->isInteractive()) {
-                        throw new Exception('You have to specify the --force option in order to run this command when you specify the --rerun option.');
-                    }
-                    $confirmQuestion = new ConfirmationQuestion(<<<'EOT'
-WARNING: re-running already executed migrations (with the --rerun option) may be dangerous!
-
-Are you sure you want to proceed?
-EOT
-                    );
-                    if (!$this->getHelper('question')->ask($input, $output, $confirmQuestion)) {
-                        throw new Exception('Operation aborted.');
-                    }
                 }
             }
         }
