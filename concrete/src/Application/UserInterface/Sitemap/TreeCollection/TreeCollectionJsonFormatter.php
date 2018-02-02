6ffb8d7591b1c57f0eece1203373d357fe42598b
Michele Locati <michele@locati.it>
2018-02-02 13:00:11 +0100

    php-cs-fixer

--- concrete/src/Application/UserInterface/Sitemap/TreeCollection/TreeCollectionJsonFormatter.php
+++ concrete/src/Application/UserInterface/Sitemap/TreeCollection/TreeCollectionJsonFormatter.php
@@ -1,13 +1,12 @@
 <?php
+
 namespace Concrete\Core\Application\UserInterface\Sitemap\TreeCollection;
 
-use Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\Entry\EntryGroupJsonFormatter;
 use Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\Entry\EntryJsonFormatter;
 use Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\Entry\Group\GroupJsonFormatter;
 
 final class TreeCollectionJsonFormatter implements \JsonSerializable
 {
-
     protected $collection;
 
     public function __construct(TreeCollectionInterface $collection)
@@ -15,23 +14,21 @@ final class TreeCollectionJsonFormatter implements \JsonSerializable
         $this->collection = $collection;
     }
 
-
     public function jsonSerialize()
     {
-        $response = array();
+        $response = [];
         $response['displayMenu'] = $this->collection->displayMenu();
-        $response['entries'] = array();
-        $response['entryGroups'] = array();
-        foreach($this->collection->getEntries() as $entry) {
+        $response['entries'] = [];
+        $response['entryGroups'] = [];
+        foreach ($this->collection->getEntries() as $entry) {
             $formatter = new EntryJsonFormatter($entry);
             $response['entries'][] = $formatter->jsonSerialize();
         }
-        foreach($this->collection->getEntryGroups() as $group) {
+        foreach ($this->collection->getEntryGroups() as $group) {
             $formatter = new GroupJsonFormatter($group);
             $response['entryGroups'][] = $formatter->jsonSerialize();
         }
+
         return $response;
     }
-
-
 }
