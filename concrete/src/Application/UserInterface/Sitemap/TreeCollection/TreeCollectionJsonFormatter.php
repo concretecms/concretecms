9528c185711beaece56737ff7db83ef87ab800fd
Michele Locati <michele@locati.it>
2018-02-02 13:01:56 +0100

    Add PHPDoc to sitemap TreeCollectionJsonFormatter

--- concrete/src/Application/UserInterface/Sitemap/TreeCollection/TreeCollectionJsonFormatter.php
+++ concrete/src/Application/UserInterface/Sitemap/TreeCollection/TreeCollectionJsonFormatter.php
@@ -4,16 +4,28 @@ namespace Concrete\Core\Application\UserInterface\Sitemap\TreeCollection;
 
 use Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\Entry\EntryJsonFormatter;
 use Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\Entry\Group\GroupJsonFormatter;
+use JsonSerializable;
 
-final class TreeCollectionJsonFormatter implements \JsonSerializable
+final class TreeCollectionJsonFormatter implements JsonSerializable
 {
+    /**
+     * @var \Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\TreeCollectionInterface
+     */
     protected $collection;
 
+    /**
+     * @param \Concrete\Core\Application\UserInterface\Sitemap\TreeCollection\TreeCollectionInterface $collection
+     */
     public function __construct(TreeCollectionInterface $collection)
     {
         $this->collection = $collection;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \JsonSerializable::jsonSerialize()
+     */
     public function jsonSerialize()
     {
         $response = [];
