8771df3723771185a7c3cf3f67745c8563740316
Michele Locati <michele@locati.it>
2018-02-03 09:42:18 +0100

    Mark migration 20180123000000 as repeatable

--- concrete/src/Updater/Migrations/Migrations/Version20180123000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20180123000000.php
@@ -3,8 +3,9 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\DirectSchemaUpgraderInterface;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-class Version20180123000000 extends AbstractMigration implements DirectSchemaUpgraderInterface
+class Version20180123000000 extends AbstractMigration implements RepeatableMigrationInterface, DirectSchemaUpgraderInterface
 {
     /**
      * {@inheritdoc}
