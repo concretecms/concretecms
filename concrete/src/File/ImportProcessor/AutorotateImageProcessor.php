6e5ea58b96d928df493ef788711ee73e1b1f70e2
Michele Locati <michele@locati.it>
2018-02-06 18:13:04 +0100

    Fix the AutorotateImageProcessor::shouldProcess
    Call AutorotateImageProcessor::process only if necessary, so that we don't
    load images in memory unnecessarily

--- concrete/src/File/ImportProcessor/AutorotateImageProcessor.php
+++ concrete/src/File/ImportProcessor/AutorotateImageProcessor.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace Concrete\Core\File\ImportProcessor;
 
 use Concrete\Core\Entity\File\Version;
-use Concrete\Core\Support\Facade\Image;
+use Concrete\Core\Support\Facade\Application;
+use Exception;
 use Imagine\Filter\Basic\Autorotate;
 use Imagine\Filter\Transformation;
+use Imagine\Image\ImagineInterface;
 use Imagine\Image\Metadata\ExifMetadataReader;
-use Concrete\Core\Support\Facade\Application;
+use Throwable;
 
 class AutorotateImageProcessor implements ProcessorInterface
 {
@@ -14,26 +17,59 @@ class AutorotateImageProcessor implements ProcessorInterface
      * @var \Concrete\Core\Application\Application
      */
     protected $app;
-    
+
     public function __construct()
     {
         $this->app = Application::getFacadeApplication();
     }
-    
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\File\ImportProcessor\ProcessorInterface::shouldProcess()
+     */
     public function shouldProcess(Version $version)
     {
-        return function_exists('exif_read_data')
-                && $version->getTypeObject()->getName() == 'JPEG';
+        $result = false;
+        if (ExifMetadataReader::isSupported()) {
+            if ($version->getTypeObject()->getName() == 'JPEG') {
+                try {
+                    $fr = $version->getFileResource();
+                    $medatadaReader = new ExifMetadataReader();
+                    $metadata = $medatadaReader->readStream($fr);
+                    switch (isset($metadata['ifd0.Orientation']) ? $metadata['ifd0.Orientation'] : null) {
+                        case 2: // top-right
+                        case 3: // bottom-right
+                        case 4: // bottom-left
+                        case 5: // left-top
+                        case 6: // right-top
+                        case 7: // right-bottom
+                        case 8: // left-bottom
+                            $result = true;
+                            break;
+                    }
+                } catch (Exception $x) {
+                } catch (Throwable $x) {
+                }
+            }
+        }
+
+        return $result;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\File\ImportProcessor\ProcessorInterface::process()
+     */
     public function process(Version $version)
     {
         $fr = $version->getFileResource();
-        
-        $imagine = $this->app->make(Image::getFacadeAccessor());
-        $imagine->setMetadataReader(new ExifMetadataReader);
+
+        $imagine = $this->app->make(ImagineInterface::class);
+        $imagine->setMetadataReader(new ExifMetadataReader());
         $image = $imagine->load($fr->read());
-        
+
         $transformation = new Transformation($imagine);
         $transformation->applyFilter($image, new Autorotate());
         $version->updateContents($image->get('jpg'));
