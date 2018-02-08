93151ef5bae14b9e7e423c0f1954c4fa875cc048
Michele Locati <michele@locati.it>
2018-02-08 16:06:47 +0100

    Even safer migration of btSearch table

--- concrete/src/Updater/Migrations/Migrations/Version20170926000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20170926000000.php
@@ -5,6 +5,7 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\DirectSchemaUpgraderInterface;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
+use Exception;
 
 class Version20170926000000 extends AbstractMigration implements RepeatableMigrationInterface, DirectSchemaUpgraderInterface
 {
@@ -15,7 +16,18 @@ class Version20170926000000 extends AbstractMigration implements RepeatableMigra
      */
     public function upgradeDatabase()
     {
-        $this->connection->executeQuery('UPDATE btSearch SET postTo_cID = NULL WHERE postTo_cID IS NOT NULL AND IFNULL(postTo_cID + 0, 0) < 1');
+        $this->connection->executeQuery("UPDATE btSearch SET postTo_cID = NULL WHERE postTo_cID IS NOT NULL AND postTo_cID = ''");
+        foreach ([
+            "postTo_cID NOT REGEXP '^[1-9][0-9]*$'",
+            'IFNULL(postTo_cID + 0, 0) < 1',
+            'IFNULL(CAST(postTo_cID AS SIGNED), 0) < 1',
+        ] as $try) {
+            try {
+                $this->connection->executeQuery('UPDATE btSearch SET postTo_cID = NULL WHERE postTo_cID IS NOT NULL AND ' . $try);
+                break;
+            } catch (Exception $foo) {
+            }
+        }
         $this->refreshBlockType('search');
     }
 }
