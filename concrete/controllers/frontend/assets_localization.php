5bc20eee0ffe86cbcc30678a63f19fe40a23e24e
Michele Locati <michele@locati.it>
2018-02-09 10:59:22 +0100

    Upgrade dropzone from 4.3.0 to 5.3.0
    Changelog
    https://gitlab.com/meno/dropzone/blob/master/CHANGELOG.md
    
    CSS didn't change from 4.3.0: see
    https://gitlab.com/meno/dropzone/commits/master/src/dropzone.scss

--- concrete/controllers/frontend/assets_localization.php
+++ concrete/controllers/frontend/assets_localization.php
@@ -597,10 +597,18 @@ jQuery.ui.fancytree.prototype.options.strings.loadError = ' . json_encode(t('Loa
      */
     public function getDropzoneJavascript()
     {
-        $content =
-'Dropzone.prototype.defaultOptions.dictDefaultMessage = ' . json_encode(t('Drop files here or click to upload.')) . ';
+        $content = ';
+Dropzone.prototype.defaultOptions.dictDefaultMessage = ' . json_encode(t('Drop files here or click to upload.')) . ';
 Dropzone.prototype.defaultOptions.dictFallbackMessage = ' . json_encode(t("Your browser does not support drag'n'drop file uploads.")) . ';
 Dropzone.prototype.defaultOptions.dictFallbackText = ' . json_encode(t('Please use the fallback form below to upload your files like in the olden days.')) . ';
+Dropzone.prototype.defaultOptions.dictFallbackText = ' . json_encode(t('Please use the fallback form below to upload your files like in the olden days.')) . ';
+Dropzone.prototype.defaultOptions.dictFileTooBig = ' . json_encode(t('File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.')) . ';
+Dropzone.prototype.defaultOptions.dictInvalidFileType = ' . json_encode(t('You can\'t upload files of this type.')) . ';
+Dropzone.prototype.defaultOptions.dictResponseError = ' . json_encode(t('Server responded with {{statusCode}} code.')) . ';
+Dropzone.prototype.defaultOptions.dictCancelUpload = ' . json_encode(t('Cancel upload')) . ';
+Dropzone.prototype.defaultOptions.dictCancelUploadConfirmation = ' . json_encode(t('Are you sure you want to cancel this upload?')) . ';
+Dropzone.prototype.defaultOptions.dictRemoveFile = ' . json_encode(t('Remove file')) . ';
+Dropzone.prototype.defaultOptions.dictMaxFilesExceeded = ' . json_encode(t('You can not upload any more files.')) . ';
 ';
 
         return $this->createJavascriptResponse($content);
