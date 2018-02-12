76edc4170499271fedb0f6f968b83af1bf5735a8
Michele Locati <michele@locati.it>
2018-02-12 14:38:20 +0100

    php-cs-fixer & fix indentation in dialogs/file/thumbnails view

--- concrete/views/dialogs/file/thumbnails.php
+++ concrete/views/dialogs/file/thumbnails.php
@@ -1,11 +1,10 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
+<?php
 
-use Concrete\Core\File\Image\Thumbnail\Type\Version;
+defined('C5_EXECUTE') or die('Access Denied.');
 
 /* @var Concrete\Core\Entity\File\Version $version */
 /* @var Concrete\Core\File\Image\Thumbnail\Type\Version[] $types */
 ?>
-
 <div class="ccm-ui">
     <?php
     $i = 1;
@@ -21,66 +20,68 @@ use Concrete\Core\File\Image\Thumbnail\Type\Version;
         $thumbnailPath = $type->getFilePath($version);
         $hasFile = $filesystem->has($thumbnailPath);
 
-        $query = http_build_query(array(
+        $query = http_build_query([
             'fID' => $version->getFileID(),
             'fvID' => $version->getFileVersionID(),
             'thumbnail' => $type->getHandle(),
-        ));
-    ?>
-
-        <?php if ($i % 3 === 1) { ?>
-            <div class="row">
-        <?php } ?>
-                <div class="col-md-4">
-                    <div class="ccm-image-thumbnail-card">
-                        <div class="ccm-image-thumbnail-display-name">
-                            <h4><?php echo $type->getDisplayName(); ?></h4>
-                        </div>
-                        <small class="ccm-image-thumbnail-dimensions"><?php echo t('%s x %s dimensions (%s)', $width, $height, $sizingMode); ?></small>
-                        <?php if ($fp->canEditFileContents() && $hasFile) { ?>
-                            <a href="<?php echo $url . '?' . $query; ?>"
-                                dialog-width="90%"
-                                dialog-height="70%"
-                                class="btn btn-sm btn-default dialog-launch"
-                                dialog-title="<?php echo t('Edit Thumbnail Images'); ?>">
-                                <?php echo t('Edit Thumbnail'); ?>
-                            </a>
-                        <?php } ?>
-                        <hr class="ccm-image-thumbnail-divider">
-                        <div class="ccm-file-manager-image-thumbnail">
-                            <?php if ($hasFile) { ?>
-                                <img class="ccm-file-manager-image-thumbnail-image"
-                                    data-handle='<?php echo $type->getHandle(); ?>'
-                                    data-fid="<?php echo $version->getFileID(); ?>"
-                                    data-fvid="<?php echo $version->getFileVersionID(); ?>"
-                                    style="max-width: 100%"
-                                    src="<?php echo $configuration->getPublicURLToFile($thumbnailPath); ?>"/>
-                            <?php
-                            } else {
-                                echo t(
-                                    'No thumbnail found. Usually this is because the ' .
-                                    'source file is smaller than this thumbnail configuration.');
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-        <?php if ($i % 3 === 0 || $i === count($types)) { ?>
-            </div>
-        <?php }
-
-        ++$i;
+        ]);
+        if ($i % 3 === 1) {
+            ?><div class="row"><?php
+        }
         ?>
-
-    <?php } ?>
-
+        <div class="col-md-4">
+            <div class="ccm-image-thumbnail-card">
+                <div class="ccm-image-thumbnail-display-name">
+                    <h4><?php echo $type->getDisplayName(); ?></h4>
+                </div>
+                <small class="ccm-image-thumbnail-dimensions"><?= t('%s x %s dimensions (%s)', $width, $height, $sizingMode) ?></small>
+                <?php
+                if ($fp->canEditFileContents() && $hasFile) {
+                    ?>
+                    <a href="<?= $url . '?' . $query ?>"
+                        dialog-width="90%"
+                        dialog-height="70%"
+                        class="btn btn-sm btn-default dialog-launch"
+                        dialog-title="<?= t('Edit Thumbnail Images') ?>"
+                    >
+                        <?= t('Edit Thumbnail') ?>
+                    </a>
+                    <?php
+                    }
+                ?>
+                <hr class="ccm-image-thumbnail-divider" />
+                <div class="ccm-file-manager-image-thumbnail">
+                    <?php
+                    if ($hasFile) {
+                        ?>
+                        <img class="ccm-file-manager-image-thumbnail-image"
+                            data-handle="<?= $type->getHandle() ?>"
+                            data-fid="<?= $version->getFileID() ?>"
+                            data-fvid="<?= $version->getFileVersionID() ?>"
+                            style="max-width: 100%"
+                            src="<?= $configuration->getPublicURLToFile($thumbnailPath) ?>"
+                        />
+                        <?php
+                    } else {
+                        echo t('No thumbnail found. Usually this is because the source file is smaller than this thumbnail configuration.');
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
+        if ($i % 3 === 0 || $i === count($types)) {
+            ?></div><?php
+        }
+        ++$i;
+    }
+    ?>
     <script>
         (function() {
             Concrete.event.unbind('ImageEditorDidSave.thumbnails');
             var thumbnails = $('img.ccm-file-manager-image-thumbnail-image');
             Concrete.event.bind('ImageEditorDidSave.thumbnails', function(event, data) {
                 if (data.isThumbnail) {
-
                     var thumbnail = thumbnails.filter('[data-handle=' + data.handle + '][data-fid=' + data.fID + '][data-fvid=' + data.fvID + ']').get(0);
                     thumbnail.src = data.imgData;
                     $.fn.dialog.closeTop();
@@ -88,5 +89,4 @@ use Concrete\Core\File\Image\Thumbnail\Type\Version;
             });
         }());
     </script>
-
 </div>
