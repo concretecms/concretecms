2861cea262de79b18751e07a65edf8ae27a566d6
Michele Locati <michele@locati.it>
2018-02-14 15:07:07 +0100

    Port of https://github.com/symfony/http-foundation/commit/04761ad618bc40375827737df8cb139ce54e665a

--- concrete/src/Http/FlysystemFileResponse.php
+++ concrete/src/Http/FlysystemFileResponse.php
@@ -128,7 +128,7 @@ class FlysystemFileResponse extends Response
      * Sets the Content-Disposition header with the given filename.
      *
      * @param string $disposition      ResponseHeaderBag::DISPOSITION_INLINE or ResponseHeaderBag::DISPOSITION_ATTACHMENT
-     * @param string $filename         Optionally use this filename instead of the real name of the file
+     * @param string $filename         Optionally use this UTF-8 encoded filename instead of the real name of the file
      * @param string $filenameFallback A fallback filename, containing only ASCII characters. Defaults to an automatically encoded filename
      *
      * @return $this
@@ -140,7 +140,7 @@ class FlysystemFileResponse extends Response
         }
 
         if ('' === $filenameFallback && (!preg_match('/^[\x20-\x7e]*$/', $filename) || false !== strpos($filename, '%'))) {
-            $encoding = mb_detect_encoding($filename, null, true);
+            $encoding = mb_detect_encoding($filename, null, true) ?: '8bit';
 
             for ($i = 0, $filenameLength = mb_strlen($filename, $encoding); $i < $filenameLength; ++$i) {
                 $char = mb_substr($filename, $i, 1, $encoding);
