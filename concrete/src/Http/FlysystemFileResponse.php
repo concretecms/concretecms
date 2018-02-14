ba564c9ecf19e9a149caa259ab5a01337e38dce7
Michele Locati <michele@locati.it>
2018-02-14 14:57:10 +0100

    Port of https://github.com/symfony/http-foundation/commit/235b1e4fa698797a94901caa2e29a01023c2e751

--- concrete/src/Http/FlysystemFileResponse.php
+++ concrete/src/Http/FlysystemFileResponse.php
@@ -166,7 +166,7 @@ class FlysystemFileResponse extends Response
 
         if (!$this->headers->has('Accept-Ranges')) {
             // Only accept ranges on safe HTTP methods
-            $this->headers->set('Accept-Ranges', $request->isMethodSafe() ? 'bytes' : 'none');
+            $this->headers->set('Accept-Ranges', $request->isMethodSafe(false) ? 'bytes' : 'none');
         }
 
         if (!$this->headers->has('Content-Type')) {
