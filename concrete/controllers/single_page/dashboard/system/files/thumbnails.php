8892667f4f21c54bb2d18435f29c679b1b4a11eb
Michele Locati <michele@locati.it>
2018-02-15 14:02:17 +0100

    Avoid accessing undefined vars in thumbnails dashboard page

--- concrete/controllers/single_page/dashboard/system/files/thumbnails.php
+++ concrete/controllers/single_page/dashboard/system/files/thumbnails.php
@@ -1,201 +1,192 @@
 <?php
+
 namespace Concrete\Controller\SinglePage\Dashboard\System\Files;
 
-use Concrete\Core\File\Image\Thumbnail\Type\Type;
+use Concrete\Core\Entity\File\Image\Thumbnail\Type\Type as TypeEntity;
+use Concrete\Core\File\Image\Thumbnail\Type\Type as TypeService;
+use Concrete\Core\Http\ResponseFactoryInterface;
 use Concrete\Core\Page\Controller\DashboardPageController;
-use Loader;
-use Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class Thumbnails extends DashboardPageController
 {
     public function view()
     {
-        $list = Type::getList();
+        $list = TypeService::getList();
         $this->set('types', $list);
     }
 
     public function edit($ftTypeID = false)
     {
-        $type = Type::getByID($ftTypeID);
+        if ($ftTypeID === 'new') {
+            $type = new TypeEntity();
+        } else {
+            $type = $ftTypeID ? $this->app->make(EntityManagerInterface::class)->find(TypeEntity::class, $ftTypeID) : null;
+            if ($type === null) {
+                $this->flash('error', t('Invalid thumbnail type object.'));
+
+                return $this->app->make(ResponseFactoryInterface::class)->redirect($this->action(''), 302);
+            }
+        }
         $this->set('type', $type);
-
-        $this->get_sizing_values();
+        $this->set('sizingModes', $this->getSizingModes());
+        $this->set('sizingModeHelps', $this->getSizingModeHelps());
     }
 
-    public function add()
+    public function save($ftTypeID = false)
     {
-        $this->get_sizing_values();
-    }
-
-    protected function get_sizing_values()
-    {
-        $sizingModes = [
-            Type::RESIZE_PROPORTIONAL => t('Resize Proportionally'),
-            Type::RESIZE_EXACT => t('Resize and Crop to the Exact Size')
-        ];
-        $this->set('sizingModes', $sizingModes);
-
-        $sizingModeHelp = [
-            Type::RESIZE_PROPORTIONAL => t("The original image will be scaled down so it is fully contained within the thumbnail dimensions. The specified width and height will be considered maximum limits. Unless the given dimensions are equal to the original image's aspect ratio, one dimension in the resulting thumbnail will be smaller than the given limit."),
-            Type::RESIZE_EXACT => t("The thumbnail will be scaled so that its smallest side will equal the length of the corresponding side in the original image. Any excess outside of the scaled thumbnail's area will be cropped, and the returned thumbnail will have the exact width and height specified. Both width and height must be specified.")
-        ];
-        $this->set('sizingModeHelp', $sizingModeHelp);
-
-        $this->set('ftTypeSizingMode', Type::RESIZE_DEFAULT);
-
-        $this->set('sizingHelpText', $sizingModeHelp[Type::RESIZE_DEFAULT]);
-    }
-
-    public function thumbnail_type_added()
-    {
-        $this->set('success', t('Thumbnail type added.'));
-        $this->view();
-    }
-
-    public function thumbnail_type_updated()
-    {
-        $this->set('success', t('Thumbnail type updated.'));
-        $this->view();
-    }
-
-    protected function validateThumbnailRequest()
-    {
-        $request = \Request::getInstance();
-        $valStrings = Loader::helper('validation/strings');
-        $valNumbers = Loader::helper('validation/numbers');
-
-        if (!$valStrings->notempty($request->request->get('ftTypeName'))) {
-            $this->error->add(t("Your thumbnail type must have a name."));
-        }
-
-        if (!$valStrings->handle($request->request->get('ftTypeHandle'))) {
-            $this->error->add(t("Your thumbnail type handle must only contain lowercase letters and underscores."));
-        }
-
-        $width = (int) $request->request->get('ftTypeWidth');
-        $height = (int) $request->request->get('ftTypeHeight');
-        if ($width < 1 && $height < 1) {
-            $this->error->add(t("Width and height can't both be empty or less than zero."));
-        }
-
-        if ($request->request->get('ftTypeSizingMode') === Type::RESIZE_EXACT && ($width < 1 || $height < 1)) {
-            $this->error->add(t("With the 'Exact' sizing mode (with cropping), both width and height must be specified and greater than zero."));
-        }
-
-        if ($valStrings->notempty($width)) {
-            if (!$valNumbers->integer($width)) {
-                $this->error->add(t("If used, width can only be an integer, with no units."));
+        if (!$this->token->validate('thumbnailtype-save-' . $ftTypeID)) {
+            $this->error->add($this->token->getErrorMessage());
+        } else {
+            $em = $this->app->make(EntityManagerInterface::class);
+            $repo = $em->getRepository(TypeEntity::class);
+            if ($ftTypeID === 'new') {
+                $type = new TypeEntity();
             } else {
-                if ($width < 1) {
-                    $this->error->add(t("If used, width must be greater than zero."));
+                $type = $ftTypeID ? $this->app->make(EntityManagerInterface::class)->find(TypeEntity::class, $ftTypeID) : null;
+                if ($type === null) {
+                    $this->flash('error', t('Invalid thumbnail type object.'));
+
+                    return $this->app->make(ResponseFactoryInterface::class)->redirect($this->action(''), 302);
                 }
             }
-        }
+            $post = $this->request->request;
+            $valNumbers = $this->app->make('helper/validation/numbers');
+            $valStrings = $this->app->make('helper/validation/strings');
 
-        if ($valStrings->notempty($height)) {
-            if (!$valNumbers->integer($height)) {
-                $this->error->add(t("If used, height can only be an integer, with no units."));
+            $handle = $post->get('ftTypeHandle');
+            $handle = is_string($handle) ? trim($handle) : '';
+            if ($handle === '') {
+                $this->error->add(t('Your thumbnail type must have a handle.'));
+            } elseif (!$valStrings->handle($handle)) {
+                $this->error->add(t('Your thumbnail type handle must only contain lowercase letters and underscores.'));
             } else {
-                if ($height < 1) {
-                    $this->error->add(t("If used, height must be greater than zero."));
+                $alreadyExists = false;
+                foreach ($repo->findBy(['ftTypeHandle' => $handle]) as $existingType) {
+                    if ($existingType !== $type) {
+                        $alreadyExists = true;
+                        break;
+                    }
+                }
+                if ($alreadyExists) {
+                    $this->error->add(t('That handle is in use.'));
+                } else {
+                    $type->setHandle($handle);
                 }
             }
-        }
 
-        return $request;
-    }
-
-    public function thumbnail_type_deleted()
-    {
-        $this->set('message', t('Thumbnail type removed.'));
-        $this->view();
-    }
-
-    public function delete()
-    {
-        $request = \Request::getInstance();
-
-        if (!Loader::helper('validation/token')->validate('delete')) {
-            $this->error->add(Loader::helper('validation/token')->getErrorMessage());
-        }
-        $type = Type::getByID($request->request->get('ftTypeID'));
-        if (!is_object($type)) {
-            $this->error->add(t('Invalid thumbnail type object.'));
-        }
-        if ($type->isRequired()) {
-            $this->error->add(t('You may not delete a required thumbnail type.'));
-        }
-
-        if (!$this->error->has()) {
-            $type->delete();
-            $this->redirect('/dashboard/system/files/thumbnails', 'thumbnail_type_deleted');
-        }
-        $this->edit($request->request->get('ftTypeID'));
-    }
-
-    public function update()
-    {
-        $request = $this->validateThumbnailRequest();
-
-        $type = Type::getByID($request->request->get('ftTypeID'));
-        if (!Loader::helper('validation/token')->validate('update')) {
-            $this->error->add(Loader::helper('validation/token')->getErrorMessage());
-        }
-        if (!is_object($type)) {
-            $this->error->add(t('Invalid thumbnail type object.'));
-        }
-        if (!$this->error->has()) {
-            $height = (int) $request->request->get('ftTypeHeight');
-            $width = (int) $request->request->get('ftTypeWidth');
-            if ($height > 0) {
-                $type->setHeight($height);
+            $name = $post->get('ftTypeName');
+            $name = is_string($name) ? trim($name) : '';
+            if ($name === '') {
+                $this->error->add(t('Your thumbnail type must have a name.'));
             } else {
-                $type->setHeight(null);
+                $alreadyExists = false;
+                foreach ($repo->findBy(['ftTypeName' => $name]) as $existingType) {
+                    if ($existingType !== $type) {
+                        $alreadyExists = true;
+                        break;
+                    }
+                }
+                if ($alreadyExists) {
+                    $this->error->add(t('Another thumbnail type exists with the name "%s".', h($name)));
+                } else {
+                    $type->setName($name);
+                }
             }
-            if ($width > 0) {
+            $width = $post->get('ftTypeWidth');
+            if ($valNumbers->integer($width, 1)) {
+                $width = (int) $width;
+            } else {
+                $width = null;
+            }
+            $height = $post->get('ftTypeHeight');
+            if ($valNumbers->integer($height, 1)) {
+                $height = (int) $height;
+            } else {
+                $height = null;
+            }
+            if ($width === null && $height === null) {
+                $this->error->add(t("Width and height can't both be empty or less than zero."));
+            } else {
                 $type->setWidth($width);
-            } else {
-                $type->setWidth(null);
-            }
-            $type->setName($request->request->get('ftTypeName'));
-            $type->setHandle($request->request->get('ftTypeHandle'));
-            $type->setSizingMode($request->request->get('ftTypeSizingMode'));
-            $type->save();
-            $this->redirect('/dashboard/system/files/thumbnails', 'thumbnail_type_updated');
-        }
-
-        $this->edit($request->request->get('ftTypeID'));
-    }
-
-    public function do_add()
-    {
-        $request = $this->validateThumbnailRequest();
-        if (!Loader::helper('validation/token')->validate('do_add')) {
-            $this->error->add(Loader::helper('validation/token')->getErrorMessage());
-        }
-        $thumbtype = Type::getByHandle($request->request->get('ftTypeHandle'));
-        if (is_object($thumbtype)) {
-            $this->error->add(t('That handle is in use.'));
-        }
-        if (!$this->error->has()) {
-            $type = new \Concrete\Core\Entity\File\Image\Thumbnail\Type\Type();
-            $height = (int) $request->request->get('ftTypeHeight');
-            $width = (int) $request->request->get('ftTypeWidth');
-            if ($height > 0) {
                 $type->setHeight($height);
             }
-            if ($width > 0) {
-                $type->setWidth($width);
+            $sizingMode = $post->get('ftTypeSizingMode');
+            if (!is_string($sizingMode) || !array_key_exists($sizingMode, $this->getSizingModes())) {
+                $this->error->add(t('Please specify the sizing mode.'));
+            } elseif ($sizingMode === TypeEntity::RESIZE_EXACT && ($width === null || $height === null)) {
+                $this->error->add(t("With the 'Exact' sizing mode (with cropping), both width and height must be specified and greater than zero."));
+            } else {
+                $type->setSizingMode($sizingMode);
             }
-            
-            $type->setName($request->request->get('ftTypeName'));
-            $type->setHandle($request->request->get('ftTypeHandle'));
-            $type->setSizingMode($request->request->get('ftTypeSizingMode'));
-            $type->save();
-            $this->redirect('/dashboard/system/files/thumbnails', 'thumbnail_type_added');
+
+            if (!$this->error->has()) {
+                if ($ftTypeID === 'new') {
+                    $em->persist($type);
+                }
+                $em->flush($type);
+                if ($ftTypeID === 'new') {
+                    $this->flash('success', t('Thumbnail type added.'));
+                } else {
+                    $this->flash('success', t('Thumbnail type updated.'));
+                }
+
+                return $this->app->make(ResponseFactoryInterface::class)->redirect($this->action(''), 302);
+            }
+        }
+        $this->edit($ftTypeID);
+    }
+
+    public function delete($ftTypeID = false)
+    {
+        if (!$this->token->validate('thumbnailtype-delete-' . $ftTypeID)) {
+            $this->error->add($this->token->getErrorMessage());
+        } else {
+            $em = $this->app->make(EntityManagerInterface::class);
+            $type = $ftTypeID ? $em->find(TypeEntity::class, $ftTypeID) : null;
+            if ($type === null) {
+                $this->flash('error', t('Invalid thumbnail type object.'));
+
+                return $this->app->make(ResponseFactoryInterface::class)->redirect($this->action(''), 302);
+            }
+            if ($type->isRequired()) {
+                $this->error->add(t('You may not delete a required thumbnail type.'));
+            }
+        }
+        if (!$this->error->has()) {
+            $em->remove($type);
+            $em->flush($type);
+            $this->flash('success', t('Thumbnail type removed.'));
+
+            return $this->app->make(ResponseFactoryInterface::class)->redirect($this->action(''));
+        }
+        $this->edit($ftTypeID);
+    }
+
+    protected function getSizingModes()
+    {
+        return [
+            TypeEntity::RESIZE_PROPORTIONAL => t('Resize Proportionally'),
+            TypeEntity::RESIZE_EXACT => t('Resize and Crop to the Exact Size'),
+        ];
+    }
+
+    protected function getSizingModeHelps()
+    {
+        $result = [];
+        foreach (array_keys($this->getSizingModes()) as $sizingMode) {
+            switch ($sizingMode) {
+                case TypeEntity::RESIZE_PROPORTIONAL:
+                    $result[$sizingMode] = t("The original image will be scaled down so it is fully contained within the thumbnail dimensions. The specified width and height will be considered maximum limits. Unless the given dimensions are equal to the original image's aspect ratio, one dimension in the resulting thumbnail will be smaller than the given limit.");
+                    break;
+                case TypeEntity::RESIZE_EXACT:
+                    $result[$sizingMode] = t("The thumbnail will be scaled so that its smallest side will equal the length of the corresponding side in the original image. Any excess outside of the scaled thumbnail's area will be cropped, and the returned thumbnail will have the exact width and height specified. Both width and height must be specified.");
+                    break;
+                default:
+                    $result[$sizingMode] = '';
+            }
         }
 
-        $this->set('type', $type);
-        $this->get_sizing_values();
+        return $result;
     }
 }
--- concrete/single_pages/dashboard/system/files/thumbnails.php
+++ concrete/single_pages/dashboard/system/files/thumbnails.php
@@ -1,157 +1,133 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php
 
-<?php if ($this->controller->getTask() == 'add'
-    || $this->controller->getTask() == 'do_add'
-    || $this->controller->getTask() == 'edit'
-    || $this->controller->getTask() == 'update'
-    || $this->controller->getTask() == 'delete') {
-    ?>
+/* @var Concrete\Core\Page\View\PageView $view */
+/* @var Concrete\Core\Form\Service\Form $form */
+/* @var Concrete\Core\Validation\CSRF\Token $token */
 
-    <?php
-    if (is_object($type)) {
-        $sizingHelpText = $sizingModeHelp[$type->getSizingMode()];
-        $ftTypeName = $type->getName();
-        $ftTypeSizingMode = $type->getSizingMode();
-        $ftTypeHandle = $type->getHandle();
-        $ftTypeWidth = $type->getWidth();
-        $ftTypeHeight = $type->getHeight();
-        $ftTypeIsRequired = $type->isRequired();
-        $method = 'update';
+defined('C5_EXECUTE') or die('Access Denied.');
 
-        if (!$ftTypeIsRequired) {
-            ?>
-
-            <div class="ccm-dashboard-header-buttons">
-                <form method="post" action="<?=$this->action('delete')?>">
-                    <input type="hidden" name="ftTypeID" value="<?=$type->getID()?>" />
-                    <?=Loader::helper('validation/token')->output('delete');
-            ?>
-                    <button type="button" class="btn btn-danger" data-action="delete-type"><?=t('Delete Type')?></button>
-                </form>
-            </div>
-
+if (isset($type)) {
+    /* @var Concrete\Core\Entity\File\Image\Thumbnail\Type\Type $type */
+    /* @var array $sizingModes */
+    /* @var array $sizingModeHelps */
+    if ($type->getID() !== null && !$type->isRequired()) {
+        ?>
+        <div class="ccm-dashboard-header-buttons">
+            <form method="post" action="<?= $view->action('delete', $type->getID())?> ">
+                <?php $token->output('thumbnailtype-delete-' . $type->getID()) ?>
+                <button type="button" class="btn btn-danger" data-action="delete-type"><?= t('Delete Type') ?></button>
+            </form>
+        </div>
         <?php
-
-        }
-    } else {
-        $method = 'do_add';
     }
     ?>
-
-    <form method="post" action="<?=$view->action($method)?>" id="ccm-attribute-key-form">
-        <?=Loader::helper('validation/token')->output($method);
-    ?>
-        <?php if (is_object($type)) {
-    ?>
-            <input type="hidden" name="ftTypeID" value="<?=$type->getID()?>" />
-        <?php
-}
-    ?>
+    <form method="POST" action="<?= $view->action('save', $type->getID() ?: 'new') ?>">
+        <?php $token->output('thumbnailtype-save-' . ($type->getID() ?: 'new')) ?>
         <fieldset>
             <div class="form-group">
-                <?=$form->label('ftTypeHandle', t('Handle'))?>
+                <?= $form->label('ftTypeHandle', t('Handle')) ?>
                 <div class="input-group">
-                    <?=$form->text('ftTypeHandle', $ftTypeHandle)?>
+                    <?= $form->text('ftTypeHandle', $type->getHandle(), ['required' => 'required', 'maxlength' => '255']) ?>
                     <span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
                 </div>
             </div>
             <div class="form-group">
-                <?=$form->label('ftTypeName', t('Name'))?>
+                <?= $form->label('ftTypeName', t('Name')) ?>
                 <div class="input-group">
-                    <?=$form->text('ftTypeName', $ftTypeName)?>
+                    <?=$form->text('ftTypeName', $type->getName(), ['required' => 'required', 'maxlength' => '255']) ?>
                     <span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
                 </div>
             </div>
             <div class="form-group">
-                <?=$form->label('ftTypeWidth', t('Width'))?>
+                <?= $form->label('ftTypeWidth', t('Width')) ?>
                 <div class="input-group">
-                    <?=$form->text('ftTypeWidth', $ftTypeWidth)?>
-                    <span class="input-group-addon"><?php echo t('px'); ?></span>
+                    <?= $form->number('ftTypeWidth', $type->getWidth() ?: '', ['min' => '1']) ?>
+                    <span class="input-group-addon"><?= t('px') ?></span>
                 </div>
             </div>
             <div class="form-group">
-                <?=$form->label('ftTypeHeight', t('Height'))?>
+                <?= $form->label('ftTypeHeight', t('Height')) ?>
                 <div class="input-group">
-                    <?=$form->text('ftTypeHeight', $ftTypeHeight)?>
-                    <span class="input-group-addon"><?php echo t('px'); ?></span>
+                    <?=$form->text('ftTypeHeight', $type->getHeight() ?: '', ['min' => '1']) ?>
+                    <span class="input-group-addon"><?= t('px') ?></span>
                 </div>
             </div>
             <div class="form-group">
-                <?=$form->label('ftTypeSizingMode', t('Sizing Mode'))?>
-                <?=$form->select('ftTypeSizingMode', $sizingModes, $ftTypeSizingMode)?>
-                <p class="sizingmode-help help-block"><span><?=$sizingHelpText?></span></p>
+                <?= $form->label('ftTypeSizingMode', t('Sizing Mode')) ?>
+                <?= $form->select('ftTypeSizingMode', $sizingModes, $type->getSizingMode()) ?>
+                <p class="help-block" id="sizingmode-help"><span><?= $sizingModeHelps[$type->getSizingMode()] ?></span></p>
             </div>
         </fieldset>
         <div class="ccm-dashboard-form-actions-wrapper">
             <div class="ccm-dashboard-form-actions">
-                <a href="<?=URL::page($c)?>" class="btn pull-left btn-default"><?=t('Back')?></a>
-                <?php if (is_object($type)) {
-    ?>
-                    <button type="submit" class="btn btn-primary pull-right"><?=t('Save')?></button>
+                <a href="<?= $view->action('') ?>" class="btn pull-left btn-default"><?= t('Back') ?></a>
                 <?php
-} else {
-    ?>
-                    <button type="submit" class="btn btn-primary pull-right"><?=t('Add')?></button>
-                <?php
-}
-    ?>
+                if ($type->getID() !== null) {
+                    ?>
+                    <button type="submit" class="btn btn-primary pull-right"><?= t('Save') ?></button>
+                    <?php
+                } else {
+                    ?>
+                    <button type="submit" class="btn btn-primary pull-right"><?= t('Add') ?></button>
+                    <?php
+                }
+                ?>
             </div>
         </div>
     </form>
-
-    <script type="text/javascript">
-        $(function() {
-            var sizingModeHelp = <?php echo json_encode($sizingModeHelp)?>;
-            $('button[data-action=delete-type]').on('click', function(e) {
-                e.preventDefault();
-                if (confirm('<?=t('Delete this thumbnail type?')?>')) {
-                    $(this).closest('form').submit();
-                }
-            });
-
-            $('#ftTypeSizingMode').on('change', function(e) {
-                $('.sizingmode-help').find('span').html(sizingModeHelp[$(this).val()]);
-            });
-
-            $('#ftTypeSizingMode').trigger('change');
-        })
+    <script>
+    $(document).ready(function() {
+        $('button[data-action=delete-type]').on('click', function(e) {
+            e.preventDefault();
+            if (window.confirm(<?= json_encode(t('Delete this thumbnail type?')) ?>)) {
+                $(this).closest('form').submit();
+            }
+        });
+        var sizingModeHelps = <?= json_encode($sizingModeHelps)?>;
+        $('#ftTypeSizingMode')
+            .on('change', function(e) {
+                var mode = $(this).val();
+                $('#sizingmode-help span').html(mode in sizingModeHelps ? sizingModeHelps[mode] : '');
+            })
+            .trigger('change')
+        ;
+    });
     </script>
-
-<?php
+    <?php
 } else {
+    /* @var Concrete\Core\Entity\File\Image\Thumbnail\Type\Type[] $types */
     ?>
-
     <div class="ccm-dashboard-header-buttons btn-group">
-        <a href="<?php echo $view->action('options')?>" class="btn btn-default"><?php echo t('Options')?></a>
-        <a href="<?php echo $view->action('add')?>" class="btn btn-primary"><?php echo t("Add Type")?></a>
+        <a href="<?= $view->action('options')?>" class="btn btn-default"><?= t('Options') ?></a>
+        <a href="<?= $view->action('edit', 'new')?>" class="btn btn-primary"><?= t('Add Type') ?></a>
     </div>
-
     <table class="table">
-    <thead>
-    <tr>
-        <th><?=t('Handle')?></th>
-        <th><?=t('Name')?></th>
-        <th><?=t('Width')?></th>
-        <th><?=t('Height')?></th>
-        <th><?=t('Sizing')?></th>
-        <th><?=t('Required')?></th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($types as $type) {
-    ?>
-    <tr>
-        <td><a href="<?=$view->action('edit', $type->getID())?>"><?=$type->getHandle()?></a></td>
-        <td><?=$type->getDisplayName()?></td>
-        <td><?=$type->getWidth() ? $type->getWidth() : '<span class="text-muted">' . t('Automatic') . '</span>' ?></td>
-        <td><?=$type->getHeight() ? $type->getHeight() : '<span class="text-muted">' . t('Automatic') . '</span>' ?></td>
-        <td><?=$type->getSizingModeDisplayName()?></td>
-        <td><?=$type->isRequired() ? t('Yes') : t('No')?></td>
-    </tr>
+        <thead>
+            <tr>
+                <th><?= t('Handle') ?></th>
+                <th><?= t('Name') ?></th>
+                <th><?= t('Width') ?></th>
+                <th><?= t('Height') ?></th>
+                <th><?= t('Sizing') ?></th>
+                <th><?= t('Required') ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($types as $type) {
+                ?>
+                <tr>
+                    <td><a href="<?= $view->action('edit', $type->getID()) ?>"><?= h($type->getHandle()) ?></a></td>
+                    <td><?= h($type->getDisplayName()) ?></td>
+                    <td><?= $type->getWidth() ?: '<span class="text-muted">' . t('Automatic') . '</span>' ?></td>
+                    <td><?= $type->getHeight() ?: '<span class="text-muted">' . t('Automatic') . '</span>' ?></td>
+                    <td><?= h($type->getSizingModeDisplayName()) ?></td>
+                    <td><?= $type->isRequired() ? t('Yes') : t('No') ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </tbody>
+    </table>
     <?php
 }
-    ?>
-    </tbody>
-    </table>
-<?php
-} ?>
