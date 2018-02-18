896b4b047e5ea5e4d5eb33877e602c52aafa216c
Sjors Snoeren <mail@sjorssnoeren.com>
2018-02-18 13:18:26 +0100

    Renders headerMenu and updates results when search query changed

--- concrete/views/dialogs/express/entry/search.php
+++ concrete/views/dialogs/express/entry/search.php
@@ -2,35 +2,39 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 ?>
 
-<div data-search="express_entries" class="ccm-ui">
-    <?php View::element('express/entries/search', array('controller' => $searchController, 'selectMode' => true)) ?>
+<div class="ccm-express-entry-search-dialog">
+  <?php $headerMenu->render(); ?>
+
+  <div data-search="express_entries" class="ccm-ui">
+      <?php View::element('express/entries/search', array('controller' => $searchController, 'selectMode' => true)) ?>
+  </div>
 </div>
 
 <script type="text/javascript">
-    $(function () {
-        $('div[data-search=express_entries]').concreteAjaxSearch({
-            result: <?=$result?>,
-            onUpdateResults: function(concreteSearch) {
-                concreteSearch.$element.on('mouseover', 'tr[data-entity-id]', function(e) {
-                    e.stopPropagation();
-                    $(this).addClass('ccm-search-select-hover');
-                });
-                concreteSearch.$element.on('mouseout', 'tr[data-entity-id]', function(e) {
-                    e.stopPropagation();
-                    $(this).removeClass('ccm-search-select-hover');
-                });
+      $(function () {
+          $('.ccm-express-entry-search-dialog').concreteAjaxSearch({
+              result: <?=$result?>,
+              onUpdateResults: function(concreteSearch) {
+                  concreteSearch.$element.on('mouseover', 'tr[data-entity-id]', function(e) {
+                      e.stopPropagation();
+                      $(this).addClass('ccm-search-select-hover');
+                  });
+                  concreteSearch.$element.on('mouseout', 'tr[data-entity-id]', function(e) {
+                      e.stopPropagation();
+                      $(this).removeClass('ccm-search-select-hover');
+                  });
 
-                concreteSearch.$element.unbind('click.expressEntries');
-                concreteSearch.$element.on('click.expressEntries', 'tr[data-entity-id]', function(e) {
-                    e.stopPropagation();
-                    ConcreteEvent.publish('SelectExpressEntry', {
-                        exEntryID: $(this).attr('data-entity-id'),
-                        event: e
-                    });
-                    return false;
-                });
-            }
+                  concreteSearch.$element.unbind('click.expressEntries');
+                  concreteSearch.$element.on('click.expressEntries', 'tr[data-entity-id]', function(e) {
+                      e.stopPropagation();
+                      ConcreteEvent.publish('SelectExpressEntry', {
+                          exEntryID: $(this).attr('data-entity-id'),
+                          event: e
+                      });
+                      return false;
+                  });
+              }
 
-        });
-    });
+          });
+      });
 </script>
