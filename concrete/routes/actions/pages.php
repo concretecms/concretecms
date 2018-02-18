57d7b31c1f1615cf50fff8426c2ad767e63620b4
Andrew Embler <andrew@concrete5.org>
2018-02-17 19:19:51 -0800

    Fixing missing route from merge

--- concrete/routes/actions/pages.php
+++ concrete/routes/actions/pages.php
@@ -14,3 +14,4 @@ $router->all('/multilingual/create_new', 'Page\Multilingual::create_new');
 $router->all('/multilingual/ignore', 'Page\Multilingual::ignore');
 $router->all('/multilingual/unmap', 'Page\Multilingual::unmap');
 $router->all('/select_sitemap', 'Page\SitemapSelector::view');
+$router->all('/sitemap_data', 'Page\SitemapData::view');
