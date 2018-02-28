64fcf0c27f038300cd5ada632aec139eaab82aeb
Andrew Embler <andrew@concrete5.org>
2018-02-28 09:54:12 -0800

    Implement repeatable migration interface

--- concrete/src/Updater/Migrations/Migrations/Version20180227035239.php
+++ concrete/src/Updater/Migrations/Migrations/Version20180227035239.php
@@ -3,12 +3,10 @@
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-class Version20180227035239 extends AbstractMigration
+class Version20180227035239 extends AbstractMigration implements RepeatableMigrationInterface
 {
     /**
      * @param Schema $schema
