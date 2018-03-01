6a6831d40faef01a0b56cda4bb6d8c55a24acd9e
Michele Locati <michele@locati.it>
2018-03-01 18:15:30 +0100

    Add c5:is-installed CLI command

--- concrete/src/Console/Application.php
+++ concrete/src/Console/Application.php
@@ -23,6 +23,7 @@ class Application extends SymfonyApplication
 
     public function setupDefaultCommands()
     {
+        $this->add(new Command\IsInstalledCommand());
         $this->add(new Command\InfoCommand());
         $this->add(new Command\InstallCommand());
         $this->add(new Command\InstallLanguageCommand());
