89e617163b60b4570b0b07445a8981c756cc6518
Michele Locati <michele@locati.it>
2018-03-02 11:00:59 +0100

    Avoid deprecated methods in user_interface backend controller
    And fix the URL built in the action() method

--- concrete/controllers/backend/user_interface.php
+++ concrete/controllers/backend/user_interface.php
@@ -3,57 +3,94 @@
 namespace Concrete\Controller\Backend;
 
 use Concrete\Core\Controller\Controller;
+use Concrete\Core\Support\Facade\Application;
 use Concrete\Core\View\DialogView;
-use Loader;
+use Exception;
 use Request;
 
 abstract class UserInterface extends Controller
 {
+    /**
+     * The current errors container.
+     *
+     * @var \Concrete\Core\Error\ErrorList\ErrorList
+     */
     protected $error;
+
+    /**
+     * An identifier to be used when checking tokens.
+     *
+     * @var string|null
+     */
     protected $validationToken;
 
     public function __construct()
     {
-        $this->error = \Core::make('helper/validation/error');
+        $this->app = Application::getFacadeApplication();
+        $this->error = $this->app->make('error');
         $this->view = new DialogView($this->viewPath);
         if (preg_match('/Concrete\\\Package\\\(.*)\\\Controller/i', get_class($this), $matches)) {
             $pkgHandle = uncamelcase($matches[1]);
             $this->view->setPackageHandle($pkgHandle);
         }
         $this->view->setController($this);
-        $request = Request::getInstance();
-        $this->request = $request;
+        $this->request = Request::getInstance();
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Controller\AbstractController::shouldRunControllerTask()
+     */
     public function shouldRunControllerTask()
     {
         return $this->canAccess();
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Controller\Controller::getViewObject()
+     */
     public function getViewObject()
     {
         if ($this->canAccess()) {
             return parent::getViewObject();
         }
-        throw new \Exception(t('Access Denied'));
+        throw new Exception(t('Access Denied'));
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Controller\Controller::action()
+     */
     public function action()
     {
-        $token = (isset($this->validationToken)) ? $this->validationToken : get_class($this);
-        $url = call_user_func_array('parent::action', func_get_args());
-        $url .= '?ccm_token=' . \Core::make('helper/validation/token')->generate($token);
+        $token = isset($this->validationToken) ? $this->validationToken : get_class($this);
+        $url = (string) call_user_func_array('parent::action', func_get_args());
+        $url .= (strpos($url, '?') === false ? '?' : '&') . $this->app->make('token')->getParameter($token);
 
         return $url;
     }
 
+    /**
+     * Can the current page be accessed?
+     *
+     * @return bool
+     */
     abstract protected function canAccess();
 
+    /**
+     * Check whether the token is valid and if the current page be accessed.
+     *
+     * @return bool
+     */
     protected function validateAction()
     {
         $token = (isset($this->validationToken)) ? $this->validationToken : get_class($this);
-        if (!Loader::helper('validation/token')->validate($token)) {
-            $this->error->add(\Core::make('helper/validation/token')->getErrorMessage());
+        if (!$this->app->make('token')->validate($token)) {
+            $this->error->add($this->app->make('token')->getErrorMessage());
 
             return false;
         }
