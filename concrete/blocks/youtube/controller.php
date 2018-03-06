c0630689233ae8b43ac96ae8f53dc67c44a1d79e
Jan Lucki <jlucki@gmail.com>
2018-03-06 21:51:50 +0000

    respects t parameter in url and added the option of setting start time in the block settings

--- concrete/blocks/youtube/controller.php
+++ concrete/blocks/youtube/controller.php
@@ -37,6 +37,7 @@ class Controller extends BlockController
     {
         $url = parse_url($this->videoURL);
         $pathParts = explode('/', rtrim($url['path'], '/'));
+        parse_str($url['query'], $params);
         $videoID = end($pathParts);
         $playListID = '';
 
@@ -55,8 +56,39 @@ class Controller extends BlockController
             $this->set('playlist', $videoID);
         }
 
+        if ($this->startTimeEnabled && !empty($this->startTime)) {
+            $this->set('startTime', $this->convertStringToSeconds($this->startTime));
+        }
+        elseif ($params['t']) {
+            $this->set('startTime', $this->convertStringToSeconds($params['t']));
+        }
+
         $this->set('videoID', $videoID);
         $this->set('playListID', $playListID);
+    }
+
+    public function convertStringToSeconds($string)
+    {
+        if (preg_match_all('/(\d+)(h|m|s)/i', $string, $matches)) {
+            $h = (($key = array_search('h', $matches[2])) !== false) ? (int)$matches[1][$key] : 0;
+            $m = (($key = array_search('m', $matches[2])) !== false) ? (int)$matches[1][$key] : 0;
+            $s = (($key = array_search('s', $matches[2])) !== false) ? (int)$matches[1][$key] : 0;
+            $seconds = ($h * 3600) + ($m * 60) + $s;
+        }
+        else {
+            $pieces = array_reverse(explode(':', $string));
+            $seconds = 0;
+            $multipliers = [1, 60, 3600];
+            foreach ($multipliers as $key => $multiplier) {
+                if (array_key_exists($key, $pieces)) {
+                    $seconds += (int)$pieces[$key] * $multiplier;
+                }
+            }
+        }
+        if ($seconds > 0) {
+            return $seconds;
+        }
+        return false;
     }
 
     public function save($data)
@@ -65,11 +97,14 @@ class Controller extends BlockController
         $args['videoURL'] = isset($data['videoURL']) ? trim($data['videoURL']) : '';
         $args['vHeight'] = isset($data['vHeight']) ? trim($data['vHeight']) : '';
         $args['vWidth'] = isset($data['vWidth']) ? trim($data['vWidth']) : '';
+        $args['startTime'] = isset($data['startTime']) ? trim($data['startTime']) : '';
 
         $args['rel'] = $data['rel'] ? 1 : 0;
         $args['showinfo'] = $data['showinfo'] ? 1 : 0;
         $args['autoplay'] = $data['autoplay'] ? 1 : 0;
         $args['loopEnd'] = $data['loopEnd'] ? 1 : 0;
+        $args['startTimeEnabled'] = $data['startTimeEnabled'] ? 1 : 0;
+
         $args['modestbranding'] = $data['modestbranding'] ? 1 : 0;
         $args['iv_load_policy'] = $data['iv_load_policy'] ? 3 : 1;
         $args['color'] = $data['color'];
