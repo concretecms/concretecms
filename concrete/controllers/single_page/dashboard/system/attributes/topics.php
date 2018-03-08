786171ff542fa2f76f6fca1208c6a24003f41744
Guti Grewal <ggrewal@rawnet.com>
2018-03-08 15:08:15 +0000

    editable topic tree names

--- concrete/controllers/single_page/dashboard/system/attributes/topics.php
+++ concrete/controllers/single_page/dashboard/system/attributes/topics.php
@@ -22,7 +22,7 @@ class Topics extends DashboardPageController
         $this->set('tree', $tree);
         $this->requireAsset('core/topics');
 
-        $trees = array();
+        $trees = [];
         if (is_object($defaultTree)) {
             $trees[] = $defaultTree;
             foreach (TopicTree::getList() as $ctree) {
@@ -54,7 +54,7 @@ class Topics extends DashboardPageController
             if (is_object($treeType)) {
                 $treeTypeHandle = $treeType->getTreeTypeHandle();
             }
-            if (is_object($tree) && $treeTypeHandle == 'topic') {
+            if (is_object($tree) && 'topic' == $treeTypeHandle) {
                 if (\PermissionKey::getByHandle('remove_topic_tree')->validate()) {
                     $tree->delete();
                     $this->redirect('/dashboard/system/attributes/topics', 'tree_deleted');
@@ -73,7 +73,7 @@ class Topics extends DashboardPageController
                 $this->error->add(t('Invalid node.'));
             }
 
-            if ($node->getTreeNodeParentID() == 0) {
+            if (0 == $node->getTreeNodeParentID()) {
                 $this->error->add(t('You may not remove the top level node.'));
             }
 
@@ -82,7 +82,7 @@ class Topics extends DashboardPageController
                 $this->error->add(t('You may not remove this node.'));
             }
 
-            if ($tree->getTreeTypeHandle() != 'topic') {
+            if ('topic' != $tree->getTreeTypeHandle()) {
                 $this->error->add(t('Invalid tree type.'));
             }
 
@@ -98,6 +98,38 @@ class Topics extends DashboardPageController
 
         if ($this->error->has()) {
             Loader::helper('ajax')->sendError($this->error);
+        }
+    }
+
+    public function tree_edit()
+    {
+        $vs = Loader::helper('validation/strings');
+
+        if (!$vs->notempty($_REQUEST['treeName'])) {
+            $this->error->add(t('You must specify a valid name for your tree.'));
+        }
+
+        if (!$this->token->validate('tree_edit')) {
+            $this->error->add($this->token->getErrorMessage());
+        }
+
+        if ($this->error->has()) {
+            return $this->view($_REQUEST['treeID']);
+        }
+
+        $tree = Tree::getByID(Loader::helper('security')->sanitizeInt($_REQUEST['treeID']));
+
+        $treeType = $tree->getTreeTypeObject();
+        if (is_object($treeType)) {
+            $treeTypeHandle = $treeType->getTreeTypeHandle();
+        }
+        if (is_object($tree) && 'topic' == $treeTypeHandle) {
+            if (\PermissionKey::getByHandle('edit_topic_tree')->validate()) {
+                $topic = new \Concrete\Core\Tree\Type\Topic();
+                $topic->setPropertiesFromArray($tree);
+                $topic->setTopicTreeName($_REQUEST['treeName']);
+                $this->redirect('/dashboard/system/attributes/topics/' . $_REQUEST['treeID']);
+            }
         }
     }
 }
