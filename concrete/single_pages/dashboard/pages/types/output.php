47e690cf915df1e20c9562476aa17aac7ef6ae48
MrKarlDilkington <mrkarldilkington@outlook.com>
2018-03-12 21:32:36 -0400

    add text label and table row styling for default page templates

--- concrete/single_pages/dashboard/pages/types/output.php
+++ concrete/single_pages/dashboard/pages/types/output.php
@@ -1,15 +1,19 @@
-<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+$pageTypeDefaultTemplateID = $pagetype->getPageTypeDefaultPageTemplateID();
+?>
 
 <p class="lead"><?php echo $pagetype->getPageTypeDisplayName(); ?></p>
     <table class="table table-striped">
-    <?php foreach ($pagetype->getPageTypePageTemplateObjects() as $pt) { ?>
-        <tr>
+    <?php foreach ($pagetype->getPageTypePageTemplateObjects() as $pt) {
+        $defaultTemplate = $pageTypeDefaultTemplateID == $pt->getPageTemplateID();
+        ?>
+        <tr <?php echo $defaultTemplate ? 'style="background: #d9edf7;"' : ''; ?>>
             <td style="width: 1px;">
                 <a href="<?php echo $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID()); ?>"
                     target="_blank"><?php echo $pt->getPageTemplateIconImage(); ?></a>
             </td>
             <td style="vertical-align: middle;">
-                <p class="lead" style="margin-bottom: 0;"><?php echo $pt->getPageTemplateDisplayName(); ?></p>
+                <p class="lead" style="margin-bottom: 0;"><?php echo $pt->getPageTemplateDisplayName() . ($defaultTemplate ? t(' (%s)', 'Default') : ''); ?></p>
             </td>
             <td style="vertical-align: middle;">
             <a href="<?php echo $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID()); ?>"
