423203261bd35d96f48b809d322194d58e30459a
Andrew Embler <andrew@concrete5.org>
2018-03-29 13:45:05 -0700

    repeatable

--- concrete/src/Updater/Migrations/Migrations/Version20180329183749.php
+++ concrete/src/Updater/Migrations/Migrations/Version20180329183749.php
@@ -4,9 +4,10 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Page\Page;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Doctrine\DBAL\Schema\Schema;
 
-class Version20180329183749 extends AbstractMigration
+class Version20180329183749 extends AbstractMigration implements RepeatableMigrationInterface
 {
 
     public function upgradeDatabase()
