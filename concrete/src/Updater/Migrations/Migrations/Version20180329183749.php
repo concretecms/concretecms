7b4134f2492b7693710d6529958579a0a2f86870
Andrew Embler <andrew@concrete5.org>
2018-03-29 13:19:36 -0700

    Add Force Check-in Dashboard page and command

---
+++ concrete/src/Updater/Migrations/Migrations/Version20180329183749.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Concrete\Core\Updater\Migrations\Migrations;
+
+use Concrete\Core\Page\Page;
+use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20180329183749 extends AbstractMigration
+{
+
+    public function upgradeDatabase()
+    {
+        $this->createSinglePage('/dashboard/system/basics/reset_edit_mode', 'Reset Edit Mode', ['meta_keywords' => 'checkin, check-in, check, force']);
+
+    }
+}
