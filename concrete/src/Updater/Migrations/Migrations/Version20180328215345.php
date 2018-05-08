66171d578d159cef9020d2c04174abb3eaae005b
Michele Locati <michele@locati.it>
2018-05-08 14:27:19 +0200

    Mark migration 20180328215345 as repeatable

--- concrete/src/Updater/Migrations/Migrations/Version20180328215345.php
+++ concrete/src/Updater/Migrations/Migrations/Version20180328215345.php
@@ -4,18 +4,17 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Page\Page;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
-use Doctrine\DBAL\Schema\Schema;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-class Version20180328215345 extends AbstractMigration
+class Version20180328215345 extends AbstractMigration implements RepeatableMigrationInterface
 {
-
     public function upgradeDatabase()
     {
         $c = Page::getByPath('/page_not_found');
         if ($c && !$c->isError()) {
             $db = $this->connection;
             $db->executeQuery('update Pages set siteTreeID = ? where cID = ?', [
-                0, $c->getCollectionID()
+                0, $c->getCollectionID(),
             ]);
         }
     }
