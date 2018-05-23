5f98ef59181f49d75a54c9592e45418edfca2456
Michele Locati <michele@locati.it>
2018-05-23 18:26:53 +0200

    php-cs-fixer

--- concrete/single_pages/dashboard/system/basics/icons.php
+++ concrete/single_pages/dashboard/system/basics/icons.php
@@ -1,4 +1,4 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <form method="post" class="form-horizontal" id="favicon-form" action="<?=$view->action('update_icons')?>" >
     <?=$this->controller->token->output('update_icons')?>
@@ -6,11 +6,11 @@
         <legend><?=t('Favicon')?></legend>
             <div class="help-block"><?=t('Your image should be 16x16 pixels, and should be a gif or a png with a .ico file extension.')?></div>
             <?php
-            $faviconFID = intval($config->get('misc.favicon_fid'));
+            $faviconFID = (int) $config->get('misc.favicon_fid');
             $f = File::getByID($faviconFID);
             ?>
             <div class="form-group">
-                <?=$concrete_asset_library->file('ccm-favicon-file', 'faviconFID', t('Choose File'), $f);?>
+                <?=$concrete_asset_library->file('ccm-favicon-file', 'faviconFID', t('Choose File'), $f); ?>
             </div>
     </fieldset>
 
@@ -18,11 +18,11 @@
         <legend><?=t('iPhone Thumbnail')?></legend>
         <div class="help-block"><?=t('iPhone home screen icons should be 57x57 and be in the .png format.')?></div>
         <?php
-        $iosHomeFID = intval($config->get('misc.iphone_home_screen_thumbnail_fid'));
+        $iosHomeFID = (int) $config->get('misc.iphone_home_screen_thumbnail_fid');
         $f = File::getByID($iosHomeFID);
         ?>
         <div class="form-group">
-            <?=$concrete_asset_library->file('ccm-iphone-file', 'iosHomeFID', t('Choose File'), $f);?>
+            <?=$concrete_asset_library->file('ccm-iphone-file', 'iosHomeFID', t('Choose File'), $f); ?>
         </div>
     </fieldset>
 
@@ -30,13 +30,13 @@
         <legend><?php echo t('Windows 8 Thumbnail'); ?></legend>
         <div class="help-block"><?=t('Windows 8 start screen tiles should be 144x144 and be in the .png format.'); ?></div>
         <?php
-        $modernThumbFID = intval($config->get('misc.modern_tile_thumbnail_fid'));
+        $modernThumbFID = (int) $config->get('misc.modern_tile_thumbnail_fid');
         $f = File::getByID($modernThumbFID);
-        $modernThumbBG = strval($config->get('misc.modern_tile_thumbnail_bgcolor'));
+        $modernThumbBG = (string) ($config->get('misc.modern_tile_thumbnail_bgcolor'));
         ?>
         <div class="form-group">
             <label class="control-label"><?=t('File')?></label>
-            <?=$concrete_asset_library->file('ccm-modern-file', 'modernThumbFID', t('Choose File'), $f);?>
+            <?=$concrete_asset_library->file('ccm-modern-file', 'modernThumbFID', t('Choose File'), $f); ?>
         </div>
         <div class="form-group">
             <label class="control-label"><?=t('Background Color')?></label>
