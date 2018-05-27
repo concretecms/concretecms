8403541e835148d368a22043d7a702b3e5ff12ca
a3020 <a3020@fastmail.com>
2018-05-27 10:19:01 +0200

    Remove records from ConversationSubscriptions when user is deleted

--- concrete/src/User/UserInfo.php
+++ concrete/src/User/UserInfo.php
@@ -229,6 +229,7 @@ class UserInfo extends ConcreteObject implements AttributeObjectInterface, Permi
         $r = $this->connection->executeQuery('DELETE FROM UserValidationHashes WHERE uID = ?', [(int) $this->getUserID()]);
         $r = $this->connection->executeQuery('DELETE FROM Piles WHERE uID = ?', [(int) $this->getUserID()]);
         $r = $this->connection->executeQuery('DELETE FROM ConfigStore WHERE uID = ?', [(int) $this->getUserID()]);
+        $r = $this->connection->executeQuery('DELETE FROM ConversationSubscriptions WHERE uID = ?', [(int) $this->getUserID()]);
         $r = $this->connection->executeQuery('UPDATE Blocks set uID = ? WHERE uID = ?', [(int) USER_SUPER_ID, (int) $this->getUserID()]);
         $r = $this->connection->executeQuery('UPDATE Pages set uID = ? WHERE uID = ?', [(int) USER_SUPER_ID, (int) $this->getUserID()]);
 
