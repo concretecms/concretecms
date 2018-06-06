21cce5b3d728b7979ed4ed92e84577d514cbf79d
Michele Locati <michele@locati.it>
2018-06-06 09:15:02 +0200

    Make editor preview sticky

--- concrete/single_pages/dashboard/system/basics/editor.php
+++ concrete/single_pages/dashboard/system/basics/editor.php
@@ -11,45 +11,63 @@
 /* @var string[] $selected_hidden */
 
 ?>
+<style>
+@media (min-width: 992px) {
+    #ccm-editor-preview.sticky {
+        position: fixed;
+    }
+}
+</style>
 <form id="ccm-editor-config" method="post" class="ccm-dashboard-content-form" action="<?= $view->action('submit') ?>">
     <?php $token->output('submit') ?>
-    <?= $form->label('', t('concrete5 Extensions')) ?>
-    <div class="checkbox">
-        <label>
-            <?= $form->checkbox('enable_filemanager', 1, $filemanager) ?>
-            <?= t('Enable file selection from file manager.') ?>
-        </label>
-    </div>
-    <div class="checkbox">
-        <label>
-            <?= $form->checkbox('enable_sitemap', 1, $sitemap) ?>
-            <?= t('Enable page selection from sitemap.') ?>
-        </label>
-    </div>
-    <?= $form->label('', t('Editor Plugins')) ?>
-    <?php
-    foreach ($plugins as $key => $plugin) {
-        if (!in_array($key, $selected_hidden)) {
-            $description = $plugin->getDescription();
-            ?>
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->label('', t('concrete5 Extensions')) ?>
             <div class="checkbox">
                 <label>
-                    <?php
-                    echo $form->checkbox('plugin[]', $key, $manager->isSelected($key));
-                    if ($description !== '') {
-                        echo '<span class="launch-tooltip" title="', h($description), '">';
-                    }
-                    echo h($plugin->getName());
-                    if ($description !== '') {
-                        echo '</span>';
-                    }
-                    ?>
+                    <?= $form->checkbox('enable_filemanager', 1, $filemanager) ?>
+                    <?= t('Enable file selection from file manager.') ?>
                 </label>
             </div>
+            <div class="checkbox">
+                <label>
+                    <?= $form->checkbox('enable_sitemap', 1, $sitemap) ?>
+                    <?= t('Enable page selection from sitemap.') ?>
+                </label>
+            </div>
+            <?= $form->label('', t('Editor Plugins')) ?>
             <?php
-        }
-    }
-    ?>
+            foreach ($plugins as $key => $plugin) {
+                if (!in_array($key, $selected_hidden)) {
+                    $description = $plugin->getDescription();
+                    ?>
+                    <div class="checkbox">
+                        <label>
+                            <?php
+                            echo $form->checkbox('plugin[]', $key, $manager->isSelected($key));
+                            if ($description !== '') {
+                                echo '<span class="launch-tooltip" title="', h($description), '">';
+                            }
+                            echo h($plugin->getName());
+                            if ($description !== '') {
+                                echo '</span>';
+                            }
+                            ?>
+                        </label>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+        <div class="col-md-6">
+            <div id="ccm-editor-preview" style="display: none">
+                <?= $form->label('', t('Editor Preview')) ?>
+                <div id="ccm-editor-preview-content"></div>
+            </div>
+        </div>
+    </div>
+            
     <div class="ccm-dashboard-form-actions-wrapper">
         <div class="ccm-dashboard-form-actions">
             <button class="pull-left btn btn-default" id="ccm-editor-preview-toggle"><?= t('Preview') ?></button>
@@ -58,13 +76,13 @@
     </div>
 </form>
 
-<div id="ccm-editor-preview-dialog" style="display: none" title="<?= t('Editor Preview') ?>">
-    <div id="ccm-editor-preview-dialog-content"></div>
-</div>
 <script>
 $(document).ready(function() {
-var $togglePreview = $('#ccm-editor-preview-toggle'),
-    $dialog = null,
+var $window = $(window),
+    $togglePreview = $('#ccm-editor-preview-toggle'),
+    $preview = $('#ccm-editor-preview'),
+    $previewContainer = $preview.closest('div:not(#ccm-editor-preview)');
+    $previewContent = $('#ccm-editor-preview-content'),
     previewEnabled = false;
 
 function showPreview(show) {
@@ -73,13 +91,14 @@ function showPreview(show) {
         return;
     }
     previewEnabled = show;
-    $togglePreview.removeClass(previewEnabled ? 'btn-default' : 'btn-success').addClass(previewEnabled ? 'btn-success' : 'btn-default');
+    $togglePreview
+        .removeClass(previewEnabled ? 'btn-default' : 'btn-success')
+        .addClass(previewEnabled ? 'btn-success' : 'btn-default')
+    ;
     if (previewEnabled) {
         updatePreview();
     } else {
-        if ($dialog) {
-            $dialog.dialog('close');
-        }
+        $preview.hide();
     }
 }
 
@@ -108,23 +127,30 @@ function updatePreview() {
         data: data
     })
     .success(function (data) {
-        if (!$dialog) {
-            $dialog = $('#ccm-editor-preview-dialog');
-            $dialog.dialog({
-                modal: false,
-                resizable: true,
-                height: 535,
-                width: Math.max(200, $(window).width() / 2 - 100),
-                close: function () {
-                    showPreview(false);
-                }
-            })
-        } else {
-            $dialog.dialog('open');
-        }
-        $('#ccm-editor-preview-dialog-content').html(data);
+        $preview.show();
+        $previewContent.html(data);
     })
 }
+
+var previewOffsetTop = $previewContainer.offset().top, previewOffsetLimit = $('.ccm-dashboard-page-header').offset().top;
+function updatePreviewView() {
+    if ($window.scrollTop() >= previewOffsetTop - previewOffsetLimit) {
+        $preview
+            .addClass('sticky')
+            .css('top', previewOffsetTop - $previewContainer.offset().top + previewOffsetLimit + 'px');
+    } else {
+        $preview.removeClass('sticky');
+    }
+    if ($preview.css('position') === 'fixed') {
+        $preview.width($previewContainer.width());
+    } else {
+        $preview.width('auto');
+    }
+}
+updatePreviewView();
+$window.on('scroll resize', function() {
+    updatePreviewView();
+});
     
 $togglePreview.on('click', function(e) {
     e.preventDefault();
