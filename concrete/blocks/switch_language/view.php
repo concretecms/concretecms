3db422bc5e77fd1e5a7a960f4654e855030fff90
Michele Locati <michele@locati.it>
2018-06-11 11:47:49 +0200

    Add PHPDoc to switch_language view

--- concrete/blocks/switch_language/view.php
+++ concrete/blocks/switch_language/view.php
@@ -1,4 +1,18 @@
-<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+
+/* @var Concrete\Core\Form\Service\Form $form */
+/* @var Concrete\Core\Block\View\BlockView $view */
+
+/* @var string $label The text label as configured by the user */
+
+/* @var array $languages Array keys are the multilingual section IDs, array values are the language names (in their own language) */
+/* @var Concrete\Core\Multilingual\Page\Section\Section[] $languageSections The list of multilanguage language sections */
+/* @var int|null $activeLanguage The ID of the currently active multilingual section (if available) */
+/* @var Concrete\Core\Multilingual\Page\Section\Section $defaultLocale The default multilingual section */
+/* @var string $locale The current language code (without Country code) */
+/* @var int $cID The ID of the current page */
+?>
 
 <div class="ccm-block-switch-language">
 
