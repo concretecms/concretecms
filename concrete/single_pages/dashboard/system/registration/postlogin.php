7f321fab325ce4e0e834bac3ca9d387d49a0a486
mesparza5 <37560903+mesparza5@users.noreply.github.com>
2018-07-03 12:54:38 -0700

    Update radio's labels with new descriptions in Login Destination page. #4098 (#6844)
    * Update radio's labels with new descriptions in Login Destination page.
    
    * Update on the title, switch to lowercase

--- concrete/single_pages/dashboard/system/registration/postlogin.php
+++ concrete/single_pages/dashboard/system/registration/postlogin.php
@@ -2,27 +2,27 @@
 <form method="post" id="login-redirect-form" action="<?php echo $view->url('/dashboard/system/registration/postlogin', 'update_login_redirect')?>">
     <?php echo $this->controller->token->output('update_login_redirect')?>
     <fieldset>
-    <legend><?php echo t('Redirect users to:')?></legend>
+    <legend><?php echo t('After login')?></legend>
     <div class="form-group">
         <div class="input">
             <div class="radio">
               <label>
                 <input type="radio" name="LOGIN_REDIRECT" value="HOMEPAGE"  <?php echo (!strlen($site_login_redirect) || $site_login_redirect == 'HOMEPAGE') ? 'checked' : ''?> />
-                <span><?php echo t('Homepage')?></span>
+                <span><?php echo t('Redirect to Home')?></span>
               </label>
             </div>
 
             <div class="radio">
               <label>
                 <input type="radio" name="LOGIN_REDIRECT" value="DESKTOP" <?php echo ($site_login_redirect == 'DESKTOP') ? 'checked' : ''?> />
-                <span><?php echo t('Desktop')?></span>
+                <span><?php echo t('Redirect to user\'s Desktop')?></span>
               </label>
             </div>
 
             <div class="radio">
               <label>
                 <input type="radio" name="LOGIN_REDIRECT" value="CUSTOM" <?php echo ($site_login_redirect == 'CUSTOM') ? 'checked' : ''?> />
-                <span><?php echo t('Custom page')?></span>
+                <span><?php echo t('Redirect to a specific page')?></span>
               </label>
                 <div id="login_redirect_custom_cid_wrap" style="display:<?php echo ($site_login_redirect == 'CUSTOM') ? 'block' : 'none'?>">
                 <?php
