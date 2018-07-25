79c30485e463ebef150761774e5041cf3eaaef18
Andrew Embler <andrew@concrete5.org>
2018-07-25 13:07:30 -0700

    preparing for 8.4.2

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -6,8 +6,8 @@ return [
      *
      * @var string
      */
-    'version' => '8.4.2b1',
-    'version_installed' => '8.4.2b1',
+    'version' => '8.4.2RC1',
+    'version_installed' => '8.4.2RC1',
     'version_db' => '20180716000000', // the key of the latest database migration
 
     /*
