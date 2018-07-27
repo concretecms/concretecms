e65f50d75126b2f284de34a6a314046da2f1208b
a3020 <a3020@fastmail.com>
2018-07-27 12:24:44 +0200

    Redirect user if welcome page is disabled

--- concrete/controllers/single_page/dashboard/welcome.php
+++ concrete/controllers/single_page/dashboard/welcome.php
@@ -3,11 +3,21 @@ namespace Concrete\Controller\SinglePage\Dashboard;
 
 use Concrete\Controller\Panel\Page\CheckIn;
 use Concrete\Core\Page\Controller\DashboardPageController;
+use Concrete\Core\Routing\Redirect;
 
 class Welcome extends DashboardPageController
 {
     public function view()
     {
+        /** @var \Concrete\Core\Config\Repository\Repository $config */
+        $config = $this->app->make('config');
+
+        // If the welcome page is disabled, the user is redirected to "Waiting for me" page.
+        // The welcome page can be disabled to prevent connections to concrete5.org.
+        if ($config->get('concrete.external.disable_welcome_page', false) === true) {
+            return Redirect::to('/dashboard/welcome/me');
+        }
+
         $this->setThemeViewTemplate('desktop.php');
     }
 }
