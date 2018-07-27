21651095e326c2c390bef4a36a4ed79468070f4f
a3020 <a3020@fastmail.com>
2018-07-27 12:36:23 +0200

    Remove unused import

--- concrete/controllers/single_page/dashboard/welcome.php
+++ concrete/controllers/single_page/dashboard/welcome.php
@@ -1,7 +1,6 @@
 <?php
 namespace Concrete\Controller\SinglePage\Dashboard;
 
-use Concrete\Controller\Panel\Page\CheckIn;
 use Concrete\Core\Page\Controller\DashboardPageController;
 use Concrete\Core\Routing\Redirect;
 
