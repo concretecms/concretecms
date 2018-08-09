8ed4e938d76a50fcf3476e9c05916dbb9598379b
Andrew Embler <andrew@concrete5.org>
2018-08-09 13:28:27 -0700

    Adding back files that shouldn't be gone yet

---
+++ concrete/src/Foundation/Processor/ProcessorInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Concrete\Core\Foundation\Processor;
+
+interface ProcessorInterface
+{
+    public function getTotalTasks();
+    public function process();
+    public function registerTask(TaskInterface $task, $priority = 0);
+    public function getTasks();
+    public function execute(ActionInterface $action);
+}
---
+++ concrete/src/Foundation/Processor/QueueAction.php
@@ -0,0 +1,17 @@
+<?php
+namespace Concrete\Core\Foundation\Processor;
+
+class QueueAction extends Action
+{
+    protected $queueItem;
+
+    public function setQueueItem($queueItem)
+    {
+        $this->queueItem = $queueItem;
+    }
+
+    public function getQueueItem()
+    {
+        return $this->queueItem;
+    }
+}
