01e36f0f14dcda1a9de11c3cc1a1e60dc7b30be1
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2018-08-21 06:04:43 +0900

    PHP CS Fix

--- concrete/controllers/backend/express/entry.php
+++ concrete/controllers/backend/express/entry.php
@@ -4,14 +4,12 @@ namespace Concrete\Controller\Backend\Express;
 use Concrete\Core\Controller\AbstractController;
 use Concrete\Core\Entity\Express\Entity;
 use Concrete\Core\Express\EntryList;
-use Concrete\Core\Legacy\Pagination;
 use Concrete\Core\Search\Pagination\PaginationFactory;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class Entry extends AbstractController
 {
-
     protected $entityManager;
 
     public function __construct(EntityManager $entityManager)
@@ -23,8 +21,8 @@ class Entry extends AbstractController
     protected function getRequestEntries()
     {
         $vals = $this->app->make('helper/validation/strings');
-        $entries = array();
-        $entryIDs = array();
+        $entries = [];
+        $entryIDs = [];
         if (isset($_REQUEST['exEntryID'])) {
             if (is_array($_REQUEST['exEntryID'])) {
                 $entryIDs = $_REQUEST['exEntryID'];
@@ -52,25 +50,25 @@ class Entry extends AbstractController
             }
         }
 
-        if (count($entries) == 0) {
+        if (0 == count($entries)) {
             $this->app->make('helper/ajax')->sendError(t('Entries not found.'));
         }
 
         return $entries;
     }
 
-
     public function getJSON()
     {
         $entries = $this->getRequestEntries();
-        foreach($entries as $entry) {
+        foreach ($entries as $entry) {
             $ep = new \Permissions($entry->getEntity());
             if (!$ep->canViewExpressEntries()) {
                 throw new \Exception(t('Access Denied.'));
             }
         }
-        $data = array();
+        $data = [];
         $data['entries'] = $entries;
+
         return new JsonResponse($data);
     }
 }
