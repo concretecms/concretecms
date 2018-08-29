7681a872f530c4869a84d6d1dee12a7639ae2817
a3020 <a3020@fastmail.com>
2018-08-29 13:45:40 +0200

    Collection object is automatically passed to view

--- concrete/blocks/content/view.php
+++ concrete/blocks/content/view.php
@@ -2,7 +2,9 @@
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
-$c = Page::getCurrentPage();
+/** @var string $content */
+/** @var \Concrete\Core\Page\Page|null $c */
+
 if (!$content && is_object($c) && $c->isEditMode()) {
     ?>
     <div class="ccm-edit-mode-disabled-item"><?=t('Empty Content Block.')?></div>
