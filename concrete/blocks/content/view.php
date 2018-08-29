6aea5576f0343f7694f8c32e448cd5d92a26b2d8
a3020 <a3020@fastmail.com>
2018-08-29 13:41:50 +0200

    Code style

--- concrete/blocks/content/view.php
+++ concrete/blocks/content/view.php
@@ -1,10 +1,12 @@
 <?php
-    defined('C5_EXECUTE') or die("Access Denied.");
-    $c = Page::getCurrentPage();
-    if (!$content && is_object($c) && $c->isEditMode()) {
-        ?>
-		<div class="ccm-edit-mode-disabled-item"><?=t('Empty Content Block.')?></div> 
-	<?php 
-    } else {
-        echo $content;
-    }
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+$c = Page::getCurrentPage();
+if (!$content && is_object($c) && $c->isEditMode()) {
+    ?>
+    <div class="ccm-edit-mode-disabled-item"><?=t('Empty Content Block.')?></div>
+    <?php
+} else {
+    echo $content;
+}
