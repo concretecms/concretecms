e8cabfe032affc011e9a4353788039a597d0660f
Andrew Embler <andrew@concrete5.org>
2018-08-30 23:08:53 +0000

    fixing an issue with doctrine orm and the league oauth2 server traits

--- concrete/src/Entity/OAuth/AccessToken.php
+++ concrete/src/Entity/OAuth/AccessToken.php
@@ -3,6 +3,8 @@
 namespace Concrete\Core\Entity\OAuth;
 
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
@@ -16,7 +18,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class AccessToken implements AccessTokenEntityInterface
 {
-    use AccessTokenTrait, EntityTrait, TokenEntityTrait;
+    use AccessTokenTrait, EntityTrait;
 
     /**
      * @ORM\Id @ORM\Column(type="guid")
@@ -40,7 +42,6 @@ class AccessToken implements AccessTokenEntityInterface
      */
     protected $scopes = [];
 
-
     /**
      * @var ScopeEntityInterface[]
      * @ORM\ManyToOne(targetEntity="Client")
@@ -48,9 +49,84 @@ class AccessToken implements AccessTokenEntityInterface
      */
     protected $client;
 
+    /**
+     * Associate a scope with the token.
+     *
+     * @param ScopeEntityInterface $scope
+     */
+    public function addScope(ScopeEntityInterface $scope)
+    {
+        $this->scopes[$scope->getIdentifier()] = $scope;
+    }
+
+    /**
+     * Return an array of scopes associated with the token.
+     *
+     * @return ScopeEntityInterface[]
+     */
     public function getScopes()
     {
-        return $this->scopes;
+        return array_values($this->scopes);
+    }
+
+    /**
+     * Get the token's expiry date time.
+     *
+     * @return \DateTime
+     */
+    public function getExpiryDateTime()
+    {
+        return $this->expiryDateTime;
+    }
+
+    /**
+     * Set the date time when the token expires.
+     *
+     * @param \DateTime $dateTime
+     */
+    public function setExpiryDateTime(\DateTime $dateTime)
+    {
+        $this->expiryDateTime = $dateTime;
+    }
+
+    /**
+     * Set the identifier of the user associated with the token.
+     *
+     * @param string|int $identifier The identifier of the user
+     */
+    public function setUserIdentifier($identifier)
+    {
+        $this->userIdentifier = $identifier;
+    }
+
+    /**
+     * Get the token user's identifier.
+     *
+     * @return string|int
+     */
+    public function getUserIdentifier()
+    {
+        return $this->userIdentifier;
+    }
+
+    /**
+     * Get the client that the token was issued to.
+     *
+     * @return ClientEntityInterface
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Set the client that the token was issued to.
+     *
+     * @param ClientEntityInterface $client
+     */
+    public function setClient(ClientEntityInterface $client)
+    {
+        $this->client = $client;
     }
 
 }
--- concrete/src/Entity/OAuth/AuthCode.php
+++ concrete/src/Entity/OAuth/AuthCode.php
@@ -3,6 +3,8 @@
 namespace Concrete\Core\Entity\OAuth;
 
 use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AuthCodeTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
@@ -17,7 +19,7 @@ use Doctrine\ORM\Mapping as ORM;
 class AuthCode implements AuthCodeEntityInterface
 {
 
-    use EntityTrait, TokenEntityTrait, AuthCodeTrait;
+    use EntityTrait, AuthCodeTrait;
 
     /**
      * @ORM\Id @ORM\Column(type="guid")
@@ -47,5 +49,85 @@ class AuthCode implements AuthCodeEntityInterface
      * @ORM\JoinColumn(name="client", referencedColumnName="identifier")
      */
     protected $client;
+
+    /**
+     * Associate a scope with the token.
+     *
+     * @param ScopeEntityInterface $scope
+     */
+    public function addScope(ScopeEntityInterface $scope)
+    {
+        $this->scopes[$scope->getIdentifier()] = $scope;
+    }
+
+    /**
+     * Return an array of scopes associated with the token.
+     *
+     * @return ScopeEntityInterface[]
+     */
+    public function getScopes()
+    {
+        return array_values($this->scopes);
+    }
+
+    /**
+     * Get the token's expiry date time.
+     *
+     * @return \DateTime
+     */
+    public function getExpiryDateTime()
+    {
+        return $this->expiryDateTime;
+    }
+
+    /**
+     * Set the date time when the token expires.
+     *
+     * @param \DateTime $dateTime
+     */
+    public function setExpiryDateTime(\DateTime $dateTime)
+    {
+        $this->expiryDateTime = $dateTime;
+    }
+
+    /**
+     * Set the identifier of the user associated with the token.
+     *
+     * @param string|int $identifier The identifier of the user
+     */
+    public function setUserIdentifier($identifier)
+    {
+        $this->userIdentifier = $identifier;
+    }
+
+    /**
+     * Get the token user's identifier.
+     *
+     * @return string|int
+     */
+    public function getUserIdentifier()
+    {
+        return $this->userIdentifier;
+    }
+
+    /**
+     * Get the client that the token was issued to.
+     *
+     * @return ClientEntityInterface
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Set the client that the token was issued to.
+     *
+     * @param ClientEntityInterface $client
+     */
+    public function setClient(ClientEntityInterface $client)
+    {
+        $this->client = $client;
+    }
 
 }
