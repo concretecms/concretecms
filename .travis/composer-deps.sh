acdb95ee01f77813d22bd608342b4ff260f7cd8d
Michele Locati <michele@locati.it>
2018-10-01 10:46:05 +0200

    Fix quote in composer-deps.sh

--- .travis/composer-deps.sh
+++ .travis/composer-deps.sh
@@ -6,7 +6,7 @@ echo 'Configuring PHP'
 phpenv config-add "$( dirname "${BASH_SOURCE[0]}" )/php.ini"
 phpenv config-rm xdebug.ini || true
 
-echo 'Installing Composer packages - Magento's composer merger'
+echo 'Installing Composer packages - Magento''s composer merger'
 travis_retry composer update --no-suggest --no-interaction $PREFER_LOWEST 
 
 echo 'Installing Composer packages - Merged dependencies'
