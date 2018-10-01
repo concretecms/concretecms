cd76b850159c9b796d84859a46ea79ddfb47554d
Michele Locati <michele@locati.it>
2018-10-01 09:25:19 +0200

    Simplify travis scripts

---
+++ .travis/setup-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+
+echo 'Installing Yarn'
+travis_retry npm install yarn -g
+
+echo 'Configuring Yarn'
+travis_retry yarn global add grunt
+
+echo 'Installing Node packages'
+cd "$TRAVIS_BUILD_DIR/build"
+travis_retry yarn
