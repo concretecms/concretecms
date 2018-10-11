aed755c8da57f3a2a4dcf6002b6c8805a697490c
Andrew Embler <andrew@concrete5.org>
2018-10-11 09:03:32 -0700

    updating version number

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -6,8 +6,8 @@ return [
      *
      * @var string
      */
-    'version' => '8.4.3',
-    'version_installed' => '8.4.3',
+    'version' => '8.4.4b1',
+    'version_installed' => '8.4.4b1',
     'version_db' => '20180716000000', // the key of the latest database migration
 
     /*
