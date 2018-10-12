c945c2fd554e6d4103d36c216b18a2ebf341ff05
Marvin <marvin@6tematik.fr>
2018-10-12 10:16:30 +0200

    Convert tabs indentation to spaces in sitemap_search_selector tool

--- concrete/tools/sitemap_search_selector.php
+++ concrete/tools/sitemap_search_selector.php
@@ -31,7 +31,7 @@ if ($callback) {
 
 <div class="ccm-ui" id="ccm-sitemap-search-selector">
 
-	<?php
+    <?php
         echo $app->make('helper/concrete/ui')->tabs([
             ['sitemap', t('Full Sitemap')],
             ['explore', t('Flat View')],
@@ -39,61 +39,61 @@ if ($callback) {
         ]);
     ?>
 
-	<div id="ccm-tab-content-sitemap" class="ccm-tab-content"></div>
+    <div id="ccm-tab-content-sitemap" class="ccm-tab-content"></div>
 
-	<div id="ccm-tab-content-explore" class="ccm-tab-content"></div>
+    <div id="ccm-tab-content-explore" class="ccm-tab-content"></div>
 
-	<div id="ccm-tab-content-search" class="ccm-tab-content"></div>
+    <div id="ccm-tab-content-search" class="ccm-tab-content"></div>
 
 </div>
 
 <script type="text/javascript">
-	ccm_sitemapSearchSelectorHideBottom = function() {
-		$('#ccm-sitemap-search-selector').parent().parent().find('.ui-dialog-buttonpane').hide();
-	}
+    ccm_sitemapSearchSelectorHideBottom = function() {
+        $('#ccm-sitemap-search-selector').parent().parent().find('.ui-dialog-buttonpane').hide();
+    }
 
-	ccm_sitemapSearchSelectorShowBottom = function() {
-		$('#ccm-sitemap-search-selector').parent().parent().find('.ui-dialog-buttonpane').show();
-	}
+    ccm_sitemapSearchSelectorShowBottom = function() {
+        $('#ccm-sitemap-search-selector').parent().parent().find('.ui-dialog-buttonpane').show();
+    }
 
-	$(function() {
-		var sst = jQuery.cookie('ccm-sitemap-selector-tab');
-		if (sst != 'explore' && sst != 'search') {
-			sst = 'sitemap';
-		}
-		$('a[data-tab=' + sst + ']').parent().addClass('active');
-		ccm_sitemapSearchSelectorHideBottom();
-		$('a[data-tab=sitemap]').click(function() {
-			jQuery.cookie('ccm-sitemap-selector-tab', 'sitemap', {path: '<?= DIR_REL; ?>/'});
-			ccm_sitemapSearchSelectorHideBottom();
-			if ($('#ccm-tab-content-sitemap').html() == '') {
-				jQuery.fn.dialog.showLoader();
-				$('#ccm-tab-content-sitemap').load('<?= REL_DIR_FILES_TOOLS_REQUIRED; ?>/sitemap_overlay', function() {
-					jQuery.fn.dialog.hideLoader();
-				});
-			}
-		});
-		$('a[data-tab=explore]').click(function() {
-			jQuery.cookie('ccm-sitemap-selector-tab', 'explore', {path: '<?=DIR_REL; ?>/'});
-			ccm_sitemapSearchSelectorHideBottom();
-			if ($('#ccm-tab-content-explore').html() == '') {
-				jQuery.fn.dialog.showLoader();
-				$('#ccm-tab-content-explore').load('<?= REL_DIR_FILES_TOOLS_REQUIRED; ?>/sitemap_overlay?display=flat', function() {
-					jQuery.fn.dialog.hideLoader();
-				});
-			}
-		});
-		$('a[data-tab=search]').click(function() {
-			jQuery.cookie('ccm-sitemap-selector-tab', 'search', {path: '<?= DIR_REL; ?>/'});
-			ccm_sitemapSearchSelectorShowBottom();
-			if ($('#ccm-tab-content-search').html() == '') {
-				jQuery.fn.dialog.showLoader();
-				$('#ccm-tab-content-search').load('<?= URL::to('/ccm/system/dialogs/page/search'); ?>', function() {
-					jQuery.fn.dialog.hideLoader();
-				});
-			}
-		});
+    $(function() {
+        var sst = jQuery.cookie('ccm-sitemap-selector-tab');
+        if (sst != 'explore' && sst != 'search') {
+            sst = 'sitemap';
+        }
+        $('a[data-tab=' + sst + ']').parent().addClass('active');
+        ccm_sitemapSearchSelectorHideBottom();
+        $('a[data-tab=sitemap]').click(function() {
+            jQuery.cookie('ccm-sitemap-selector-tab', 'sitemap', {path: '<?= DIR_REL; ?>/'});
+            ccm_sitemapSearchSelectorHideBottom();
+            if ($('#ccm-tab-content-sitemap').html() == '') {
+                jQuery.fn.dialog.showLoader();
+                $('#ccm-tab-content-sitemap').load('<?= REL_DIR_FILES_TOOLS_REQUIRED; ?>/sitemap_overlay', function() {
+                    jQuery.fn.dialog.hideLoader();
+                });
+            }
+        });
+        $('a[data-tab=explore]').click(function() {
+            jQuery.cookie('ccm-sitemap-selector-tab', 'explore', {path: '<?=DIR_REL; ?>/'});
+            ccm_sitemapSearchSelectorHideBottom();
+            if ($('#ccm-tab-content-explore').html() == '') {
+                jQuery.fn.dialog.showLoader();
+                $('#ccm-tab-content-explore').load('<?= REL_DIR_FILES_TOOLS_REQUIRED; ?>/sitemap_overlay?display=flat', function() {
+                    jQuery.fn.dialog.hideLoader();
+                });
+            }
+        });
+        $('a[data-tab=search]').click(function() {
+            jQuery.cookie('ccm-sitemap-selector-tab', 'search', {path: '<?= DIR_REL; ?>/'});
+            ccm_sitemapSearchSelectorShowBottom();
+            if ($('#ccm-tab-content-search').html() == '') {
+                jQuery.fn.dialog.showLoader();
+                $('#ccm-tab-content-search').load('<?= URL::to('/ccm/system/dialogs/page/search'); ?>', function() {
+                    jQuery.fn.dialog.hideLoader();
+                });
+            }
+        });
 
-		$('#ccm-sitemap-search-selector ul li.active a').click();
-	});
+        $('#ccm-sitemap-search-selector ul li.active a').click();
+    });
 </script>
