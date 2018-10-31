dcd3418c7546dc8d5481824c92a7b87f9b0427fa
Korvin Szanto <korvinszanto@gmail.com>
2018-10-31 10:29:15 -0700

    Document notification system

--- concrete/src/Notification/Subject/SubjectInterface.php
+++ concrete/src/Notification/Subject/SubjectInterface.php
@@ -4,7 +4,20 @@ namespace Concrete\Core\Notification\Subject;
 interface SubjectInterface
 {
 
-    function getNotificationDate();
-    function getUsersToExcludeFromNotification();
+    /**
+     * Get the date of this notification
+     *
+     * @return \DateTime
+     */
+    public function getNotificationDate();
+
+    /**
+     * Get the users that should be excluded from notifications
+     * Expected return value would be users involved in the creation of the notification, they may not need to be
+     * notified.
+     *
+     * @return \Concrete\Core\Entity\User\User[]
+     */
+    public function getUsersToExcludeFromNotification();
 
 }
