606e80470f6444092df8cf2b9613842f57e70e4d
Korvin Szanto <korvinszanto@gmail.com>
2018-10-31 15:12:50 -0700

    Fix tests

--- concrete/src/Updater/Migrations/Migrations/Version20181029223809.php
+++ concrete/src/Updater/Migrations/Migrations/Version20181029223809.php
@@ -5,12 +5,13 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Entity\Notification\UserDeactivatedNotification;
 use Concrete\Core\Entity\User\LoginAttempt;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20181029223809 extends AbstractMigration
+class Version20181029223809 extends AbstractMigration implements RepeatableMigrationInterface
 {
     /**
      * @param Schema $schema
--- tests/tests/User/Login/LoginAttemptServiceTest.php
+++ tests/tests/User/Login/LoginAttemptServiceTest.php
@@ -3,29 +3,40 @@
 namespace User\Login;
 
 use Concrete\Core\Config\Repository\Repository;
+use Concrete\Core\Entity\User\LoginAttempt;
 use Concrete\Core\Entity\User\LoginAttemptRepository;
 use Concrete\Core\User\Login\LoginAttemptService;
 use Doctrine\ORM\EntityManager;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit_Framework_TestCase;
 use Mockery as M;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class LoginAttemptServiceTest extends PHPUnit_Framework_TestCase
 {
 
     use MockeryPHPUnitIntegration;
 
-
     public function testPruneAttemptsFromConfig()
     {
-        $em = M::mock(EntityManager::class);
+
+        $fakeAttempt = M::mock(LoginAttempt::class);
+        $fakeAttempt2 = M::mock(LoginAttempt::class);
         $config = M::mock(Repository::class);
         $repository = M::mock(LoginAttemptRepository::class);
+        $director = M::mock(EventDispatcherInterface::class);
+
+        $em = M::mock(EntityManager::class);
+        $em->shouldReceive('remove')->once()->with($fakeAttempt);
+        $em->shouldReceive('remove')->once()->with($fakeAttempt2);
+        $em->shouldReceive('flush')->once();
+
+        $repository->shouldReceive('before')->andReturn([$fakeAttempt, $fakeAttempt2]);
 
         $config->shouldReceive('get')->andReturn(100);
         $em->shouldReceive('getRepository')->andReturn($repository);
 
-        $service = new LoginAttemptService($em, $config);
+        $service = new LoginAttemptService($em, $config, $director);
         $service->pruneAttempts();
     }
 
