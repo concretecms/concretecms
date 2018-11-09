06ecbefd72317566de71d3ea395d9f86bc081ec4
Michele Locati <michele@locati.it>
2018-11-09 19:41:30 +0100

    Improve tests

--- tests/tests/File/Image/Svg/SanitizerTest.php
+++ tests/tests/File/Image/Svg/SanitizerTest.php
@@ -4,6 +4,7 @@ namespace Concrete\Tests\File\Image\Svg;
 
 use Concrete\Core\File\Image\Svg\Sanitizer;
 use PHPUnit_Framework_TestCase;
+use Concrete\Core\File\Image\Svg\SanitizerOptions;
 
 class SanitizerTest extends PHPUnit_Framework_TestCase
 {
@@ -12,10 +13,21 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
      */
     protected static $sanitizer;
 
+    /**
+     * @var \Concrete\Core\File\Image\Svg\SanitizerOptions
+     */
+    protected static $sanitizerOptions;
+    
     public static function setupBeforeClass()
     {
         parent::setUpBeforeClass();
         self::$sanitizer = new Sanitizer();
+        self::$sanitizerOptions = SanitizerOptions::create()
+            ->setUnsafeElements('script script2')
+            ->setElementWhitelist('script2')
+            ->setUnsafeAttributes('onload onload2 onclick')
+            ->setAttributeWhitelist('onload2')
+        ;
     }
 
     /**
@@ -27,8 +39,10 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
             ['<svg/>', '<svg/>'],
             ['<svg good="1" />', '<svg good="1"/>'],
             ['<svg><script>alert(1);</script></svg>', '<svg/>'],
+            ['<svg><script2>alert(1);</script2></svg>', '<svg><script2>alert(1);</script2></svg>'],
             ['<svg onload="alert(1)" />', '<svg/>'],
             ['<svg foo="1" onload="alert(1)" bar="2" />', '<svg foo="1" bar="2"/>'],
+            ['<svg foo="1" OnLoad="alert(1)" OnLoad2="alert(1)" bar="2" />', '<svg foo="1" OnLoad2="alert(1)" bar="2"/>'],
             ['<svg><script></script><g onLoad="alert(1)"><rect /></g></svg>', '<svg><g><rect/></g></svg>'],
         ];
     }
@@ -40,7 +54,7 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
      */
     public function testSanitizeWithDefaultSettings($input, $expectedOutput)
     {
-        $sanitized = self::$sanitizer->sanitizeData($input);
+        $sanitized = self::$sanitizer->sanitizeData($input, self::$sanitizerOptions);
         $lines = explode("\n", $sanitized);
         $this->assertSame('<?xml version="1.0"?>', array_shift($lines));
         $xml = trim(implode('', $lines));
@@ -73,6 +87,6 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
      */
     public function testInvalidData($invalidSvgData)
     {
-        self::$sanitizer->sanitizeData($invalidSvgData);
+        self::$sanitizer->sanitizeData($invalidSvgData, self::$sanitizerOptions);
     }
 }
