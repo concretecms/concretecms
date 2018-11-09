40da71237e42b27f5a5980afaf81372f4d566369
Michele Locati <michele@locati.it>
2018-11-09 16:39:13 +0100

    Normalize HtmlObject use

--- concrete/elements/files/view/image.php
+++ concrete/elements/files/view/image.php
@@ -6,10 +6,10 @@ defined('C5_EXECUTE') or die('Access Denied.');
 /* @var Concrete\Core\Entity\File\Version $fv */
 
 $tag = new Image();
-$tag->src($fv->getURL());
+$tag->src = $fv->getURL();
 $tag->alt = h($fv->getTitle());
 if ($fv->getTypeObject()->isSVG()) {
     $tag->addClass('ccm-svg');
-    $tag->style('max-width: 100%');
+    $tag->style = 'max-width: 100%';
 }
 echo (string) $tag;
