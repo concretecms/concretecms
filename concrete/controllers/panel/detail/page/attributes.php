529bee581248c5aa7afc3af476482101fc2747a3
marvinde <29543909+marvinde@users.noreply.github.com>
2018-11-25 13:53:17 +0100

    Validate attributes in attributes panel

--- concrete/controllers/panel/detail/page/attributes.php
+++ concrete/controllers/panel/detail/page/attributes.php
@@ -5,15 +5,14 @@ use Concrete\Controller\Backend\UserInterface\Page as BackendInterfacePageContro
 use Concrete\Core\Attribute\Context\AttributePanelContext;
 use Concrete\Core\Http\ResponseAssetGroup;
 use Concrete\Core\Workflow\Request\ApprovePageRequest;
-use PageEditResponse;
-use PermissionKey;
-use stdClass;
-use Loader;
-use User;
+use Concrete\Core\Page\EditResponse as PageEditResponse;
+use Concrete\Core\Permission\Key\Key as PermissionKey;
+use Concrete\Core\User\User;
 use Concrete\Core\Page\Collection\Version\Version;
-use CollectionAttributeKey;
+use Concrete\Core\Attribute\Key\CollectionKey as CollectionAttributeKey;
 use Concrete\Core\Entity\Attribute\Key\PageKey;
 use Concrete\Core\Attribute\View as AttributeTypeView;
+use stdClass;
 
 class Attributes extends BackendInterfacePageController
 {
@@ -60,9 +59,9 @@ class Attributes extends BackendInterfacePageController
     public function view()
     {
         $this->set('assignment', $this->assignment);
-        $this->set('dt', Loader::helper('form/date_time'));
-        $this->set('uh', Loader::helper('form/user_selector'));
-        $selectedAttributes = array();
+        $this->set('dt', $this->app->make('helper/form/date_time'));
+        $this->set('uh', $this->app->make('helper/form/user_selector'));
+        $selectedAttributes = [];
         $allowed = $this->assignment->getAttributesAllowedArray();
         foreach ($this->page->getSetCollectionAttributes() as $ak) {
             if (is_object($ak) && in_array($ak->getAttributeKeyID(), $allowed)) {
@@ -70,7 +69,7 @@ class Attributes extends BackendInterfacePageController
                 $selectedAttributes[] = $obj;
             }
         }
-        $this->set('selectedAttributes', Loader::helper('json')->encode($selectedAttributes));
+        $this->set('selectedAttributes', $this->app->make('helper/json')->encode($selectedAttributes));
     }
 
     public function submit()
@@ -78,48 +77,29 @@ class Attributes extends BackendInterfacePageController
         if ($this->validateAction()) {
             $post = $this->request->request;
             $c = $this->page;
-            $cp = $this->permissions;
             $asl = $this->assignment;
-
             $nvc = $c->getVersionToModify();
-            $data = array();
-            if ($asl->allowEditName()) {
-                $data['cName'] = $post->get('cName');
-            }
-            if ($asl->allowEditDescription()) {
-                $data['cDescription'] = $post->get('cDescription');
-            }
-            if ($asl->allowEditDateTime()) {
-                $dt = Loader::helper('form/date_time');
-                $dh = Loader::helper('date');
-                $data['cDatePublic'] = $dt->translate('cDatePublic');
-            }
-            if ($asl->allowEditUserID()) {
-                $data['uID'] = $post->get('uID');
-            }
-
-            $nvc->update($data);
 
             // First, we check out the attributes we need to clear.
             $setAttribs = $nvc->getSetCollectionAttributes();
-            $processedAttributes = array();
+            $processedAttributes = [];
+            $attributesToValidate = [];
+            $attributesToClear = [];
             $selectedAKIDs = $post->get('selectedAKIDs');
             if (!is_array($selectedAKIDs)) {
-                $selectedAKIDs = array();
+                $selectedAKIDs = [];
             }
-            $selected = is_array($post->get('selectedAKIDs')) ? $post->get('selectedAKIDs') : array();
+            $selected = is_array($post->get('selectedAKIDs')) ? $post->get('selectedAKIDs') : [];
 
             foreach ($setAttribs as $ak) {
                 // do I have the ability to edit this attribute?
                 if (in_array($ak->getAttributeKeyID(), $asl->getAttributesAllowedArray())) {
                     // Is this item in the selectedAKIDs array? If so then it is being saved
                     if (in_array($ak->getAttributeKeyID(), $selected)) {
-                        $controller = $ak->getController();
-                        $value = $controller->createAttributeValueFromRequest();
-                        $nvc->setAttribute($ak, $value);
+                        $attributesToValidate[] = $ak;
                     } else {
                         // it is being removed
-                        $nvc->clearAttribute($ak);
+                        $attributesToClear[] = $ak;
                     }
                     $processedAttributes[] = $ak->getAttributeKeyID();
                 }
@@ -129,29 +109,73 @@ class Attributes extends BackendInterfacePageController
                 if ($akID > 0 && in_array($akID, $asl->getAttributesAllowedArray())) {
                     $ak = CollectionAttributeKey::getByID($akID);
                     $controller = $ak->getController();
-                    $value = $controller->createAttributeValueFromRequest();
-                    $nvc->setAttribute($ak, $value);
+                    $attributesToValidate[] = $controller;
                 }
             }
 
-            if ($this->request->request->get('sitemap')
-                && $this->permissions->canApprovePageVersions()
-                && \Config::get('concrete.misc.sitemap_approve_immediately')) {
-                $pkr = new ApprovePageRequest();
-                $u = new User();
-                $pkr->setRequestedPage($this->page);
-                $v = Version::get($this->page, "RECENT");
-                $pkr->setRequestedVersionID($v->getVersionID());
-                $pkr->setRequesterUserID($u->getUserID());
-                $response = $pkr->trigger();
-                $u->unloadCollectionEdit();
+            // Check if attributes have a valid value
+            $e = $this->app->make('error');
+            foreach ($attributesToValidate as $attribute) {
+                $controller = $attribute->getController();
+                $data = $controller->request();
+                $error = $controller->validateForm($data);
+                if ($error !== true) {
+                    $e->add($error);
+                }
             }
+            if ($e->has()) {
+                $r = new PageEditResponse();
+                $r->setError($e);
+                $r->outputJSON();
+            } else {
 
-            $r = new PageEditResponse();
-            $r->setPage($c);
-            $r->setTitle(t('Page Updated'));
-            $r->setMessage(t('Page Attributes have been saved.'));
-            $r->outputJSON();
+                // Update the page version after everything is validated
+                $data = [];
+                if ($asl->allowEditName()) {
+                    $data['cName'] = $post->get('cName');
+                }
+                if ($asl->allowEditDescription()) {
+                    $data['cDescription'] = $post->get('cDescription');
+                }
+                if ($asl->allowEditDateTime()) {
+                    $dt = $this->app->make('helper/form/date_time');
+                    $data['cDatePublic'] = $dt->translate('cDatePublic');
+                }
+                if ($asl->allowEditUserID()) {
+                    $data['uID'] = $post->get('uID');
+                }
+                $nvc->update($data);
+
+                // Save valid attributes
+                foreach ($attributesToValidate as $attribute) {
+                    $controller = $attribute->getController();
+                    $value = $controller->createAttributeValueFromRequest();
+                    $nvc->setAttribute($attribute, $value);
+                }
+
+                // Clear attributes that need to be removed
+                foreach ($attributesToClear as $attribute) {
+                    $nvc->clearAttribute($attribute);
+                }
+
+                $config = $this->app->make('config');
+                if ($this->request->request->get('sitemap') && $this->permissions->canApprovePageVersions() && $config->get('concrete.misc.sitemap_approve_immediately')) {
+                    $pkr = new ApprovePageRequest();
+                    $u = new User();
+                    $pkr->setRequestedPage($this->page);
+                    $v = Version::get($this->page, 'RECENT');
+                    $pkr->setRequestedVersionID($v->getVersionID());
+                    $pkr->setRequesterUserID($u->getUserID());
+                    $response = $pkr->trigger();
+                    $u->unloadCollectionEdit();
+                }
+
+                $r = new PageEditResponse();
+                $r->setPage($c);
+                $r->setTitle(t('Page Updated'));
+                $r->setMessage(t('Page Attributes have been saved.'));
+                $r->outputJSON();
+            }
         }
     }
 
@@ -165,7 +189,7 @@ class Attributes extends BackendInterfacePageController
         if (is_object($ak) && in_array($ak->getAttributeKeyID(), $allowed)) {
             $obj = $this->getAttributeJSONRepresentation($ak, 'add');
             $obj->pending = true;
-            $obj->assets = array();
+            $obj->assets = [];
             $ag = ResponseAssetGroup::get();
             foreach ($ag->getAssetsToOutput() as $position => $assets) {
                 foreach ($assets as $asset) {
@@ -175,7 +199,7 @@ class Attributes extends BackendInterfacePageController
                     }
                 }
             }
-            Loader::helper('ajax')->sendResult($obj);
+            $this->app->make('helper/ajax')->sendResult($obj);
         }
     }
 }
