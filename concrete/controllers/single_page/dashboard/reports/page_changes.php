3253b7bfc867dfcd6b612709821470716cc5c703
a3020 <a3020@fastmail.com>
2018-12-04 10:15:39 +0100

    Add single page and install it for fresh installs

---
+++ concrete/controllers/single_page/dashboard/reports/page_changes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Concrete\Controller\SinglePage\Dashboard\Reports;
+
+use Concrete\Core\Page\Controller\DashboardPageController;
+
+class PageChanges extends DashboardPageController
+{
+    public function view()
+    {
+
+    }
+}
---
+++ concrete/single_pages/dashboard/reports/page_changes.php
@@ -0,0 +1,8 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+
+<form role="form" action="<?=$controller->action('view')?>" class="ccm-search-fields">
+
+    <div class="ccm-search-fields-submit">
+        <button type="submit" class="btn btn-primary pull-right"><?=t('Search')?></button>
+    </div>
+</form>
