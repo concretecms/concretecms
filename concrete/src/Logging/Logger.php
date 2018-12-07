6e387e852bf8dcf49e9d66d2c1279e336a6d7725
Andrew Embler <andrew@concrete5.org>
2018-12-07 22:21:46 +0000

    adding back deprecated file

---
+++ concrete/src/Logging/Logger.php
@@ -0,0 +1,14 @@
+<?php
+namespace Concrete\Core\Logging;
+
+use Monolog\Logger as MonologLogger;
+
+/**
+ * @deprecated - Use the LoggerFactory to create this. Calling this directly will not load any handlers.
+ * Class Logger
+ * @package Concrete\Core\Logging
+ */
+class Logger extends MonologLogger
+{
+
+}
