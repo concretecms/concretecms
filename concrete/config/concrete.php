80506962a2f2793b6562b741381c6cce37758516
Andrew Embler <andrew@concrete5.org>
2018-12-07 17:52:31 +0000

    Removing databsae query log

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -6,9 +6,9 @@ return [
      *
      * @var string
      */
-    'version' => '8.5.0a2',
-    'version_installed' => '8.5.0a2',
-    'version_db' => '20181006212400', // the key of the latest database migration
+    'version' => '8.5.0a3',
+    'version_installed' => '8.5.0a3',
+    'version_db' => '20181207165730', // the key of the latest database migration
 
     /*
      * Installation status
--- concrete/src/Logging/LoggingServiceProvider.php
+++ concrete/src/Logging/LoggingServiceProvider.php
@@ -2,42 +2,32 @@
 namespace Concrete\Core\Logging;
 
 use Concrete\Core\Foundation\Service\Provider as ServiceProvider;
+use Concrete\Core\Logging\Configuration\ConfigurationFactory;
+use Monolog\Logger;
 
 class LoggingServiceProvider extends ServiceProvider
 {
     public function register()
     {
 
+        $this->app->singleton(LoggerFactory::class, function ($app) {
+            $logger = new LoggerFactory($app->make(ConfigurationFactory::class), $app->make('director'));
+            return $logger;
+        });
+
         $this->app->singleton('log/application', function($app) {
             $factory = $app->make(LoggerFactory::class);
             return $factory->createLogger(Channels::CHANNEL_APPLICATION);
         });
 
-
-        /*
-        $this->app->singleton(LoggerFactory::class, function ($app) {
-            $logger = new LoggerFactory($app->make('config'), $app->make('director'));
-            return $logger;
-        });
-
-        $this->app->singleton('log/factory', function($app) {
-            return $app->make(LoggerFactory::class);
-        });
-
-        $this->app->singleton(Logger::class, function ($app) {
-            $logger = new Logger(Logger::CHANNEL_APPLICATION);
-            return $logger;
-        });
-
         // Bind the PSR-3 logger interface against the singleton
-        $this->app->bind('Psr\Log\LoggerInterface', 'Concrete\Core\Logging\Logger');
-        $this->app->singleton('log', function($app) {
-            return $app->make(Logger::class);
-        });
-        $this->app->singleton('log/exceptions', function() {
-            $logger = new Logger(LOG_TYPE_EXCEPTIONS);
+        $this->app->bind('Psr\Log\LoggerInterface', 'log/application');
+
+        $this->app->singleton('log/exceptions', function($app) {
+            $factory = $app->make(LoggerFactory::class);
+            return $factory->createLogger(Channels::CHANNEL_EXCEPTIONS);
             return $logger;
         });
-        */
+
     }
 }
