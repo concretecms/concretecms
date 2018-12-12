38391fda3b431d514bc2ac5a37d66104beb89977
Michele Locati <michele@locati.it>
2018-12-12 12:14:23 +0100

    Don't require the redirect URL for API integrations

--- concrete/single_pages/dashboard/system/api/integrations/add.php
+++ concrete/single_pages/dashboard/system/api/integrations/add.php
@@ -20,7 +20,7 @@
         <div class="form-group">
             <label for="redirect" class="control-label"><?php echo t('Redirect'); ?></label>
             <div class="input-group">
-                <?php echo $form->url('redirect', array('autocomplete' => 'off', 'required' => 'required')); ?>
+                <?php echo $form->url('redirect', array('autocomplete' => 'off')); ?>
                 <span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
             </div>
         </div>
