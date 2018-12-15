3c6206da74885c965757ecd811c2dcf93b67d5b7
Hamed Darragi <hd@xanweb.com>
2018-12-15 23:39:49 +0100

    Remove unused uses

--- concrete/src/Updater/Migrations/Migrations/Version20181213000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20181213000000.php
@@ -2,12 +2,8 @@
 
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
-use Concrete\Core\Page\Page;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
-use Doctrine\DBAL\Schema\Schema;
-use Concrete\Core\Authentication\AuthenticationType;
-use Exception;
 
 class Version20181213000000 extends AbstractMigration implements RepeatableMigrationInterface
 {
