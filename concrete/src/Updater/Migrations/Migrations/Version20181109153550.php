5902f8f2cfd15501deafe6ac2190203216a4c5dc
Mike Esparza <mike@portlandlabs.com>
2018-12-18 14:09:07 -0800

    Removing automated comment from migration

--- concrete/src/Updater/Migrations/Migrations/Version20181109153550.php
+++ concrete/src/Updater/Migrations/Migrations/Version20181109153550.php
@@ -6,14 +6,9 @@ use Concrete\Core\Entity\Validator\UsedString;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 class Version20181109153550 extends AbstractMigration implements RepeatableMigrationInterface
 {
-    /**
-     * Handle upgrading the database
-     */
     public function upgradeDatabase()
     {
         $this->refreshEntities([
@@ -21,9 +16,6 @@ class Version20181109153550 extends AbstractMigration implements RepeatableMigra
         ]);
     }
 
-    /**
-     * Handle downgrading the database
-     */
     public function downgradeDatabase()
     {
         $this->refreshEntities();
