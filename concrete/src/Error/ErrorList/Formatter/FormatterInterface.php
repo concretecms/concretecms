3a73e370c7bd00c7283048664da786eeda6e92a7
Michele Locati <michele@locati.it>
2018-12-19 20:30:20 +0100

    Fix handling of html/text in error messages (#7431)
    * Support HtmlAwareErrorInterface errors in system_errors element
    
    * Don't html-escape the error in SMTP test
    
    * Error classes: phpdoc, php-cs-fixer
    
    * Remove HTML tags when rendering errors containing HTML as text
    
    * Add ErrorList::addError(), add ErrorList::addHtml(), add ThrowableError, save a flag in JSON to list errors that are in HTML format
    
    * Correctly handle HTML/text errors in ajax responses

--- concrete/src/Error/ErrorList/Formatter/FormatterInterface.php
+++ concrete/src/Error/ErrorList/Formatter/FormatterInterface.php
@@ -1,12 +1,22 @@
 <?php
+
 namespace Concrete\Core\Error\ErrorList\Formatter;
 
 use Concrete\Core\Error\ErrorList\ErrorList;
 
 interface FormatterInterface
 {
+    /**
+     * Initialize the instance.
+     *
+     * @param \Concrete\Core\Error\ErrorList\ErrorList $error the error list to be formatted
+     */
+    public function __construct(ErrorList $error);
 
-    function __construct(ErrorList $error);
-    function render();
-
+    /**
+     * Render the error list.
+     *
+     * @return string
+     */
+    public function render();
 }
