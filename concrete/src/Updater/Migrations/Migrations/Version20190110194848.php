7b33bd207e2a4f0a337e58dc8940c4e5b068a7d6
Korvin Szanto <korvinszanto@gmail.com>
2019-01-10 15:09:48 -0800

    Add openid OIDC compatible scope

---
+++ concrete/src/Updater/Migrations/Migrations/Version20190110194848.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Concrete\Core\Updater\Migrations\Migrations;
+
+use Concrete\Core\Entity\OAuth\Scope;
+use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190110194848 extends AbstractMigration
+{
+
+    public function upgradeDatabase()
+    {
+        $em = $this->connection->getEntityManager();
+        $scope = $em->find(Scope::class, 'openid');
+
+        if ($scope) {
+            // Scope already exists
+            return;
+        }
+
+        $scope = new Scope();
+        $scope->setIdentifier('openid');
+        $scope->setDescription('OpenID Connect authentication flow');
+
+        $em->persist($scope);
+        $em->flush();
+        $em->clear(Scope::class);
+    }
+
+    public function downgradeDatabase()
+    {
+        $em = $this->connection->getEntityManager();
+        $scope = $em->find(Scope::class, 'openid');
+
+        if ($scope) {
+            $em->remove($scope);
+        }
+    }
+
+}
