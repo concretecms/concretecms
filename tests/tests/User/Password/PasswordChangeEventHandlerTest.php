cf429372a0d2c7263b03cfe4487fdb2f384f1f72
Andrew Embler <andrew@concrete5.org>
2019-01-11 19:56:02 +0000

    Trying to fix password change tracker test

--- tests/tests/User/Password/PasswordChangeEventHandlerTest.php
+++ tests/tests/User/Password/PasswordChangeEventHandlerTest.php
@@ -3,8 +3,11 @@
 namespace Concrete\Tests\User\Password;
 
 use Concrete\Core\User\Event\UserInfoWithPassword;
+use Concrete\Core\User\Logger;
 use Concrete\Core\User\Password\PasswordChangeEventHandler;
 use Concrete\Core\User\Password\PasswordUsageTracker;
+use Concrete\Core\User\User;
+use Concrete\Core\User\UserInfo;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Symfony\Component\EventDispatcher\Event;
 use PHPUnit_Framework_TestCase;
@@ -18,10 +21,15 @@ class PasswordChangeEventHandlerTest extends PHPUnit_Framework_TestCase
     public function testHandlingSimpleEvent()
     {
         $password = 'blah';
-        $userinfo = 'someuserinfoobjectbutnotreally';
 
+        $user = M::mock(User::class);
+        $userinfo = M::mock(UserInfo::class);
         $tracker = M::mock(PasswordUsageTracker::class);
+        $logger = M::mock(Logger::class);
+        $logger->shouldReceive('logChangePassword');
         $tracker->shouldReceive('trackUse')->once()->with($password, $userinfo);
+
+        $userinfo->shouldReceive('getUserObject')->andReturn($user);
 
         $event = M::mock(UserInfoWithPassword::class);
         $event->shouldReceive([
@@ -29,7 +37,7 @@ class PasswordChangeEventHandlerTest extends PHPUnit_Framework_TestCase
             'getUserInfoObject' => $userinfo
         ]);
 
-        $handler = new PasswordChangeEventHandler($tracker);
+        $handler = new PasswordChangeEventHandler($logger, $tracker);
         $handler->handleEvent($event);
     }
 
@@ -40,9 +48,10 @@ class PasswordChangeEventHandlerTest extends PHPUnit_Framework_TestCase
     public function testInvalidEventType()
     {
         $tracker = M::mock(PasswordUsageTracker::class);
+        $logger = M::mock(Logger::class);
         $event = M::mock(Event::class);
 
-        $handler = new PasswordChangeEventHandler($tracker);
+        $handler = new PasswordChangeEventHandler($logger, $tracker);
         $handler->handleEvent($event);
     }
 
