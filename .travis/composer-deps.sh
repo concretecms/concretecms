88d882968d5b3ed53b3fbf30f7d62b977506306f
Michele Locati <michele@locati.it>
2019-02-08 10:28:02 +0100

    Define TravisCI job names

--- .travis/composer-deps.sh
+++ .travis/composer-deps.sh
@@ -2,14 +2,21 @@
 
 set -o errexit
 
-source "$( dirname "${BASH_SOURCE[0]}" )/travis_retry.sh"
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+source "$SCRIPT_DIR/travis_retry.sh"
 
 echo 'Configuring PHP'
-phpenv config-add "$( dirname "${BASH_SOURCE[0]}" )/php.ini"
+phpenv config-add "$SCRIPT_DIR/php.ini"
 phpenv config-rm xdebug.ini || true
 
+FLAGS=''
+if test -n "${COMPOSER_PREFER_LOWEST:-}"; then
+    FLAGS="$FLAGS --prefer-lowest"
+fi
+
 echo 'Installing Composer packages - Magento''s composer merger'
-travis_retry composer update --no-suggest --no-interaction $PREFER_LOWEST
+travis_retry composer update --no-suggest --no-interaction $FLAGS
 
 echo 'Installing Composer packages - Merged dependencies'
-travis_retry composer update --no-suggest --no-interaction $PREFER_LOWEST
+travis_retry composer update --no-suggest --no-interaction $FLAGS
