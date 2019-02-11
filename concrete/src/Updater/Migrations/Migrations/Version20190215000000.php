a4e580dcec59ab33047fc0254c1cdb01935a0bfd
Andrew Embler <andrew@concrete5.org>
2019-02-11 14:09:21 +0000

    Reworking old migrations to run after 8.5

--- concrete/src/Updater/Migrations/Migrations/Version20190215000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20190215000000.php
@@ -5,10 +5,7 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-class Version20180215212428 extends AbstractMigration
+class Version20190215000000 extends AbstractMigration
 {
     public function upgradeDatabase()
     {
