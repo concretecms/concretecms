65af62c7df2dd1ee0e7b14f9bd939362223a3da5
Michele Locati <michele@locati.it>
2019-03-01 15:22:12 +0100

    Fix creation of localized stacks/global areas

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -8,7 +8,7 @@ return [
      */
     'version' => '8.5.0RC2',
     'version_installed' => '8.5.0RC2',
-    'version_db' => '20190225184524', // the key of the latest database migration
+    'version_db' => '20190301133300', // the key of the latest database migration
 
     /*
      * Installation status
