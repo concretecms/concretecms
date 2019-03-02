a99f09591289ba6d6ed599a1208de831ebeefd98
Andrew Embler <andrew@concrete5.org>
2019-03-02 00:05:45 +0000

    Removing the mx record test because it's not reliable in all environments; fixing issue where tests couldn't run because entity manager tried to connect to the db too early.

--- concrete/src/Foundation/Queue/Batch/BatchProgressUpdater.php
+++ concrete/src/Foundation/Queue/Batch/BatchProgressUpdater.php
@@ -2,6 +2,7 @@
 
 namespace Concrete\Core\Foundation\Queue\Batch;
 
+use Concrete\Core\Application\Application;
 use Concrete\Core\Entity\Queue\Batch;;
 
 use Concrete\Core\Foundation\Queue\Batch\Command\BatchableCommandInterface;
@@ -11,31 +12,36 @@ class BatchProgressUpdater
 {
 
     /**
-     * @var EntityManager
+     * @var Application
      */
-    protected $entityManager;
+    protected $app;
 
     /**
-     * @param Driver     $driver
-     * @param Serializer $serializer
+     * BatchProgressUpdater constructor.
+     * @param Application $app
      */
-    public function __construct(EntityManager $entityManager)
+    public function __construct(Application $app)
     {
-        $this->entityManager = $entityManager;
+        // FYI - I know that the actual dependency here is the entity manager, not the application object. But
+        // unfortunately, we can't pass the entity manager in here because this dependency is created
+        // too early in the booting process, and it screws up the tests.
+        $this->app = $app;
     }
 
     public function incrementTotals(Batch $batch, $additional)
     {
+        $entityManager = $this->app->make(EntityManager::class);
         $total = $batch->getTotal();
         $total += $additional;
         $batch->setTotal($total);
-        $this->entityManager->persist($batch);
-        $this->entityManager->flush();
+        $entityManager->persist($batch);
+        $entityManager->flush();
     }
 
     public function incrementCommandProgress(BatchableCommandInterface $command)
     {
-        $r = $this->entityManager->getRepository(Batch::class);
+        $entityManager = $this->app->make(EntityManager::class);
+        $r = $entityManager->getRepository(Batch::class);
         $batch = $r->findOneByBatchHandle($command->getBatchHandle());
         if ($batch) {
             /**
@@ -44,8 +50,8 @@ class BatchProgressUpdater
             $completed = $batch->getCompleted();
             $completed++;
             $batch->setCompleted($completed);
-            $this->entityManager->persist($batch);
-            $this->entityManager->flush();
+            $entityManager->persist($batch);
+            $entityManager->flush();
         }
     }
 
