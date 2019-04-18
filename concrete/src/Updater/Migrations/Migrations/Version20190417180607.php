3703616e2cda2cf40795a47428af991824b90e45
Mike Esparza <mike@portlandlabs.com>
2019-04-18 13:28:01 -0700

    Quick fix on migration.

--- concrete/src/Updater/Migrations/Migrations/Version20190417180607.php
+++ concrete/src/Updater/Migrations/Migrations/Version20190417180607.php
@@ -3,37 +3,23 @@
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
-use Doctrine\DBAL\Schema\Schema;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Concrete\Core\Page\Page;
 
 class Version20190417180607 extends AbstractMigration implements RepeatableMigrationInterface
 {
-
     public function upgradeDatabase()
     {
-        $this->createSinglePage('/dashboard/users/groups/message', 'Send Message to Group', [
-            'meta_keywords' => implode(', ', [
-                'user',
-                'group',
-                'people',
-                'messages',
-            ])
-        ]);
+        $this->createSinglePage('/dashboard/users/groups/message', 'Send Message to Group',
+            ['meta_keywords' => 'user, group, people, messages']);
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function downgradeSchema(Schema $schema)
+    public function downgradeDatabase()
     {
         $page = Page::getByPath('/dashboard/users/groups/message');
 
-        if ($page && !$page->isError()) {
+        if ($page) {
             $page->delete();
         }
-
-        $this->refreshEntities();
-
     }
 }
