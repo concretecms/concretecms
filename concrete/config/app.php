2f126156f932c66e31a24328f8d27e0184e6a98b
Derek Cameron <derek@concrete5.co.jp>
2019-05-07 19:07:05 +0900

    Fixing Issue when adding new workflows, whose type belong to a package

--- concrete/config/app.php
+++ concrete/config/app.php
@@ -211,9 +211,9 @@ return [
         'storage_location_type',
         'theme',
         'user_point_action',
+        'workflow',
         'workflow_progress_category',
         'workflow_type',
-        'workflow',
     ],
 
     'importer_routines' => [
