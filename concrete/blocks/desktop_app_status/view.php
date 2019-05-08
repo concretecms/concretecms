378137789d7789a439e37692f81fbcd130495ba6
Michele Locati <michele@locati.it>
2019-05-08 08:21:54 +0200

    Don't check concrete.version vs APP_VERSION
    APP_VERSION is defined to the value of the concrete.version config key,
    so it's useless to check if APP_VERSION is greater than the value of
    the concrete.version config key.

--- concrete/blocks/desktop_app_status/view.php
+++ concrete/blocks/desktop_app_status/view.php
@@ -8,12 +8,6 @@ if (version_compare($latest_version, APP_VERSION, '>')) {
         <a class="pull-right btn btn-info btn-xs" href="<?php echo $view->url('/dashboard/system/update/update')?>"><?php echo t('Update')?></a>
     </div>
     <?php
-} elseif (version_compare(APP_VERSION, Config::get('concrete.version'), '>')) {
-    ?>
-    <div class="alert alert-warning">
-        <?php echo t('You have downloaded a new version of concrete5 but have not upgraded to it yet.'); ?>
-        <a href="<?php echo REL_DIR_FILES_TOOLS_REQUIRED ?>/upgrade" class="pull-right btn btn-warning btn-xs"><?php echo t('Update')?></a></div>
-    <?php
 }
 
 if ($updates > 0) {
