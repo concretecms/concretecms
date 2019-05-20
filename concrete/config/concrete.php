75ca2246792f34bde9020f6a65c5a25e5383332e
Michele Locati <michele@locati.it>
2019-05-20 10:50:57 +0200

    Add dashboard page to change database character set / collation

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -8,7 +8,7 @@ return [
      */
     'version' => '8.5.2a1',
     'version_installed' => '8.5.2a1',
-    'version_db' => '20190301133300', // the key of the latest database migration
+    'version_db' => '20190520070000', // the key of the latest database migration
 
     /*
      * Installation status
