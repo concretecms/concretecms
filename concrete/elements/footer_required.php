c39926902d666839f476dbd420bbbd550ea4cfe1
Michele Locati <michele@locati.it>
2019-05-24 12:26:19 +0200

    Don't show account menu if we can view the toolbar
    Fix #4588

--- concrete/elements/footer_required.php
+++ concrete/elements/footer_required.php
@@ -18,9 +18,8 @@ $c = Page::getCurrentPage();
 $site = $app->make('site')->getSite();
 $config = $site->getConfigRepository();
 $localization = Localization::getInstance();
-
-if (is_object($c)) {
-    $cp = new Permissions($c);
+$cp = is_object($c) ? new Permissions($c) : null;
+if ($cp !== null) {
     $localization->pushActiveContext(Localization::CONTEXT_UI);
     try {
         View::element('page_controls_footer', ['cp' => $cp, 'c' => $c]);
@@ -39,18 +38,20 @@ if (!isset($display_account_menu)) {
     $display_account_menu = $config->get('user.display_account_menu');
 }
 if ($display_account_menu) {
-    $u = $app->make(User::class);
-    if ($u->isRegistered()) {
-        $dh = $app->make('helper/concrete/dashboard');
-        if (!$dh->inDashboard($c)) {
-            $v = View::getRequestInstance();
-            $v->requireAsset('core/account');
-            $v->addFooterItem('<script>$(function() { if (window.ccm_enableUserProfileMenu) ccm_enableUserProfileMenu(); });</script>');
-            $localization->pushActiveContext(Localization::CONTEXT_UI);
-            try {
-                View::element('account/menu');
-            } finally {
-                $localization->popActiveContext();
+    if ($cp === null || !$cp->canViewToolbar()) {
+        $u = $app->make(User::class);
+        if ($u->isRegistered()) {
+            $dh = $app->make('helper/concrete/dashboard');
+            if (!$dh->inDashboard($c)) {
+                $v = View::getRequestInstance();
+                $v->requireAsset('core/account');
+                $v->addFooterItem('<script>$(function() { if (window.ccm_enableUserProfileMenu) ccm_enableUserProfileMenu(); });</script>');
+                $localization->pushActiveContext(Localization::CONTEXT_UI);
+                try {
+                    View::element('account/menu');
+                } finally {
+                    $localization->popActiveContext();
+                }
             }
         }
     }
