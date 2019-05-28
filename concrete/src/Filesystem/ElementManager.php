616a0e927fa091f98d81dbc2db8dd045302d1c37
Michele Locati <michele@locati.it>
2019-05-28 10:37:56 +0200

    Add phpdoc

--- concrete/src/Filesystem/ElementManager.php
+++ concrete/src/Filesystem/ElementManager.php
@@ -4,8 +4,19 @@ namespace Concrete\Core\Filesystem;
 
 class ElementManager
 {
+    /**
+     * @var \Concrete\Core\Filesystem\Element[]
+     */
     protected $registry = [];
 
+    /**
+     * @param string $element the element name
+     * @param \Concrete\Core\Page\Page $page
+     * @param array $elementArguments the arguments to be used when calling the constructor of the element controller
+     * @param string $pkgHandle the handle of the package defining this element
+     *
+     * @return \Concrete\Core\Filesystem\Element
+     */
     public function get($element)
     {
         if (isset($this->registry[$element])) {
@@ -25,11 +36,22 @@ class ElementManager
         }
     }
 
+    /**
+     * Register an element.
+     *
+     * @param string $element the element name
+     * @param \Concrete\Core\Filesystem\Element $object the element instance
+     */
     public function register($element, $object)
     {
         $this->registry[$element] = $object;
     }
 
+    /**
+     * Unregister an element.
+     *
+     * @param string $element the element name
+     */
     public function unregister($element)
     {
         unset($this->registry[$element]);
