5c275840935e9383d6f8a7795d3d5e7f57e4c776
Michele Locati <michele@locati.it>
2019-05-29 14:48:20 +0200

    Touch the session in the Heartbeat controller

--- concrete/controllers/frontend/heartbeat.php
+++ concrete/controllers/frontend/heartbeat.php
@@ -4,11 +4,17 @@ namespace Concrete\Controller\Frontend;
 
 use Concrete\Core\Controller\Controller;
 use Concrete\Core\Http\ResponseFactoryInterface;
+use Concrete\Core\Session\SessionValidator;
 
 class Heartbeat extends Controller
 {
     public function view()
     {
+        $sessionValidator = $this->app->make(SessionValidator::class);
+        if ($sessionValidator->hasActiveSession()) {
+            $this->app->make('session');
+        }
+
         return $this->app->make(ResponseFactoryInterface::class)->json(true);
     }
 }
