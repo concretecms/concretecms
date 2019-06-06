e0107ec030ef45a29b57faf76d60b7e2c9ee3339
Michele Locati <michele@locati.it>
2019-06-06 12:18:43 +0200

    Fix indentation

--- concrete/js/build/core/app/heartbeat.js
+++ concrete/js/build/core/app/heartbeat.js
@@ -2,27 +2,29 @@
 /* global CCM_DISPATCHER_FILENAME */
 
 ;(function(global, $) {
-'use strict';
+    'use strict';
+    
+    var lastUserInteraction = new Date(),
+        lastHeartbeat = new Date();
 
-var lastUserInteraction = new Date();
-var lastHeartbeat = new Date();
-$(window).on('mousemove keydown keyup', function() {
-    lastUserInteraction = new Date();
-});
-setInterval(
-    function() {
-        if(lastUserInteraction.getTime() > lastHeartbeat.getTime()) {
-            lastHeartbeat = new Date();
-            $.ajax({
-                async: true,
-                cache: false,
-                dataType: 'json',
-                type: 'GET',
-                url: CCM_DISPATCHER_FILENAME + '/ccm/system/heartbeat',
-            });
-        }
-    },
-    30 * 1000
-);
+    $(window).on('mousemove keydown keyup', function() {
+        lastUserInteraction = new Date();
+    });
+
+    setInterval(
+        function() {
+            if(lastUserInteraction.getTime() > lastHeartbeat.getTime()) {
+                lastHeartbeat = new Date();
+                $.ajax({
+                    async: true,
+                    cache: false,
+                    dataType: 'json',
+                    type: 'GET',
+                    url: CCM_DISPATCHER_FILENAME + '/ccm/system/heartbeat',
+                });
+            }
+        },
+        30 * 1000
+    );
 
 })(this, jQuery);
