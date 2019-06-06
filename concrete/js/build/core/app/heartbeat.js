950d08ee318238770ce41b451aab51bce78ca8fb
Michele Locati <michele@locati.it>
2019-06-06 12:28:10 +0200

    Avoid setInterval in heartbeat

--- concrete/js/build/core/app/heartbeat.js
+++ concrete/js/build/core/app/heartbeat.js
@@ -3,28 +3,24 @@
 
 ;(function(global, $) {
     'use strict';
-    
+
     var lastUserInteraction = new Date(),
-        lastHeartbeat = new Date();
+        lastHeartbeat = new Date(),
+        MIN_HEARTHBEAT_INTERVAL = 30 * 1000;
 
     $(window).on('mousemove keydown keyup', function() {
         lastUserInteraction = new Date();
-    });
-
-    setInterval(
-        function() {
-            if(lastUserInteraction.getTime() > lastHeartbeat.getTime()) {
-                lastHeartbeat = new Date();
-                $.ajax({
-                    async: true,
-                    cache: false,
-                    dataType: 'json',
-                    type: 'GET',
-                    url: CCM_DISPATCHER_FILENAME + '/ccm/system/heartbeat',
-                });
-            }
-        },
-        30 * 1000
+        if (lastUserInteraction.getTime() - lastHeartbeat.getTime() < MIN_HEARTHBEAT_INTERVAL) {
+            return;
+        }
+        lastHeartbeat = new Date();
+        $.ajax({
+            async: true,
+            cache: false,
+            dataType: 'json',
+            type: 'GET',
+            url: CCM_DISPATCHER_FILENAME + '/ccm/system/heartbeat',
+        });
     );
 
 })(this, jQuery);
