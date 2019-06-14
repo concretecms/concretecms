43f301bf2b30971449faf091cbb205ecd1675cb9
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-06-15 00:22:03 +0300

    Remove unnecessary constructor from the test class

--- tests/assets/Express/Form/Control/View/TestView.php
+++ tests/assets/Express/Form/Control/View/TestView.php
@@ -1,25 +1,11 @@
 <?php
 namespace Concrete\Tests\Express\Form\Control\View;
 
-use Concrete\Core\Entity\Express\Control\AttributeKeyControl;
-use Concrete\Core\Entity\Express\Control\Control;
 use Concrete\Core\Express\Form\Control\View\View;
-use Concrete\Core\Express\Form\Context\ContextInterface;
 use Concrete\Core\Filesystem\TemplateLocator;
 
 class TestView extends View
 {
-    /**
-     * AttributeKeyView constructor.
-     *
-     * @param ContextInterface $context
-     * @param AttributeKeyControl $control
-     */
-    public function __construct(ContextInterface $context, Control $control)
-    {
-        parent::__construct($context, $control);
-    }
-
     public function getControlID()
     {
         return 'tests';
