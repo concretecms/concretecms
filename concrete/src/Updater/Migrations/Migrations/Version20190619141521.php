d1b1104def12d4fb42e33d4a657a28e41bc3f21d
Andrew Embler <andrew@concrete5.org>
2019-06-20 04:38:54 +0000

    implementing repeatable migration interface to fix tests

--- concrete/src/Updater/Migrations/Migrations/Version20190619141521.php
+++ concrete/src/Updater/Migrations/Migrations/Version20190619141521.php
@@ -5,8 +5,9 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Entity\Attribute\Key\Settings\Settings;
 use Concrete\Core\Entity\Attribute\Key\Settings\UserGroupSettings;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-class Version20190619141521 extends AbstractMigration
+class Version20190619141521 extends AbstractMigration implements RepeatableMigrationInterface
 {
     public function upgradeDatabase()
     {
