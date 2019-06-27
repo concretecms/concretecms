cabf766a8589999494764dd76e1a53cedd8f4156
Michele Locati <michele@locati.it>
2019-06-27 16:34:32 +0200

    php-cs-fixer

--- concrete/src/Workflow/Workflow.php
+++ concrete/src/Workflow/Workflow.php
@@ -1,15 +1,16 @@
 <?php
+
 namespace Concrete\Core\Workflow;
 
-use \Concrete\Core\Foundation\ConcreteObject;
+use Concrete\Core\Foundation\ConcreteObject;
 use Concrete\Core\Package\Package;
-use \Concrete\Core\Workflow\Progress\Progress as WorkflowProgress;
-use Loader;
-use Core;
+use Concrete\Core\Workflow\Progress\Progress as WorkflowProgress;
 use Concrete\Core\Workflow\Request\Request as WorkflowRequest;
+use Core;
+use Loader;
 
 /**
- * \@package Workflow
+ * \@package Workflow.
  *
  * @author Andrew Embler <andrew@concrete5.org>
  * @copyright  Copyright (c) 2003-2012 concrete5. (http://www.concrete5.org)
@@ -18,8 +19,8 @@ use Concrete\Core\Workflow\Request\Request as WorkflowRequest;
 abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permission\ObjectInterface
 {
     protected $wfID = 0;
-    protected $allowedTasks = array('cancel', 'approve');
-    protected $restrictedToPermissionKeyHandles = array();
+    protected $allowedTasks = ['cancel', 'approve'];
+    protected $restrictedToPermissionKeyHandles = [];
 
     public function getAllowedTasks()
     {
@@ -90,12 +91,12 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
     public function delete()
     {
         $db = Loader::db();
-        $db->Execute('delete from Workflows where wfID = ?', array($this->wfID));
+        $db->Execute('delete from Workflows where wfID = ?', [$this->wfID]);
 
         foreach (
             $db->GetArray(
                 'select wpID from WorkflowProgress where wfID = ?',
-                array($this->wfID)
+                [$this->wfID]
             )
             as $row
         ) {
@@ -118,9 +119,9 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
 
     public static function getList()
     {
-        $workflows = array();
+        $workflows = [];
         $db = Loader::db();
-        $r = $db->Execute("select wfID from Workflows order by wfName asc");
+        $r = $db->Execute('select wfID from Workflows order by wfName asc');
         while ($row = $r->FetchRow()) {
             $wf = static::getByID($row['wfID']);
             if (is_object($wf)) {
@@ -133,9 +134,9 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
 
     public static function getListByPackage(\Concrete\Core\Entity\Package $pkg)
     {
-        $workflows = array();
+        $workflows = [];
         $db = Loader::db();
-        $r = $db->Execute("select wfID from Workflows where pkgID = ? order by wfName asc", [$pkg->getPackageID()]);
+        $r = $db->Execute('select wfID from Workflows where pkgID = ? order by wfName asc', [$pkg->getPackageID()]);
         while ($row = $r->FetchRow()) {
             $wf = static::getByID($row['wfID']);
             if (is_object($wf)) {
@@ -146,11 +147,10 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
         return $workflows;
     }
 
-
     public static function add(Type $wt, $name, \Concrete\Core\Entity\Package $pkg = null)
     {
         $db = Loader::db();
-        $wfID = $db->getOne('SELECT wfID FROM Workflows WHERE wfName=?', array($name));
+        $wfID = $db->getOne('SELECT wfID FROM Workflows WHERE wfName=?', [$name]);
         if (!$wfID) {
             $pkgID = $wt->getPackageID();
             if (empty($pkgID)) {
@@ -161,25 +161,18 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
                 $pkgID = $pkg->getPackageID();
             }
 
-            $db->Execute('insert into Workflows (wftID, wfName, pkgID) values (?, ?, ?)', array($wt->getWorkflowTypeID(), $name, $pkgID));
+            $db->Execute('insert into Workflows (wftID, wfName, pkgID) values (?, ?, ?)', [$wt->getWorkflowTypeID(), $name, $pkgID]);
             $wfID = $db->Insert_ID();
         }
 
         return self::getByID($wfID);
     }
 
-    protected function load($wfID)
-    {
-        $db = Loader::db();
-        $r = $db->GetRow('select Workflows.* from Workflows where Workflows.wfID = ?', array($wfID));
-        $this->setPropertiesFromArray($r);
-    }
-
     public static function getByID($wfID)
     {
         $db = Loader::db();
         $r = $db->GetRow('select WorkflowTypes.wftHandle, WorkflowTypes.pkgID from Workflows inner join WorkflowTypes on Workflows.wftID = WorkflowTypes.wftID where Workflows.wfID = ?',
-            array($wfID));
+            [$wfID]);
         if ($r['wftHandle']) {
             $prefix = null;
             $class = '\\Core\\Workflow\\' . Loader::helper('text')->camelcase($r['wftHandle']) . 'Workflow';
@@ -202,7 +195,7 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
     public static function getByName($wfName)
     {
         $db = Loader::db();
-        $wfID = $db->GetOne('select wfID from Workflows where wfName = ?', array($wfName));
+        $wfID = $db->GetOne('select wfID from Workflows where wfName = ?', [$wfName]);
         if ($wfID) {
             return static::getByID($wfID);
         }
@@ -221,7 +214,7 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
     public function updateName($wfName)
     {
         $db = Loader::db();
-        $db->Execute('update Workflows set wfName = ? where wfID = ?', array($wfName, $this->wfID));
+        $db->Execute('update Workflows set wfName = ? where wfID = ?', [$wfName, $this->wfID]);
     }
 
     abstract public function start(WorkflowProgress $wp);
@@ -256,5 +249,12 @@ abstract class Workflow extends ConcreteObject implements \Concrete\Core\Permiss
         }
 
         return false;
+    }
+
+    protected function load($wfID)
+    {
+        $db = Loader::db();
+        $r = $db->GetRow('select Workflows.* from Workflows where Workflows.wfID = ?', [$wfID]);
+        $this->setPropertiesFromArray($r);
     }
 }
