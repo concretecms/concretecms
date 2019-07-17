ac43f462a42cf3b08441fac0a21883cc355e9c71
Ed Beeny <edbeeny@gmail.com>
2019-07-17 09:06:30 +0100

    Add reCAPTCHA v3 update

---
+++ concrete/src/Updater/Migrations/Migrations/Version20190717090600.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Concrete\Core\Updater\Migrations\Migrations;
+
+use Concrete\Core\Backup\ContentImporter;
+use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
+
+class Version20190716000000 extends AbstractMigration implements RepeatableMigrationInterface
+{
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Updater\Migrations\AbstractMigration::upgradeDatabase()
+     */
+    public function upgradeDatabase()
+    {
+        $ci = new ContentImporter();
+        $ci->importContentFile(DIR_BASE_CORE . '/config/install/upgrade/captcha.xml');
+    }
+}
