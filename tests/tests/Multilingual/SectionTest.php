73f12417bb41d7c64d0327483dd2648368440759
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2019-07-30 10:11:31 +0900

    Add test for Section::registerDuplicate

--- tests/tests/Multilingual/SectionTest.php
+++ tests/tests/Multilingual/SectionTest.php
@@ -2,6 +2,7 @@
 
 namespace Concrete\Tests\Multilingual;
 
+use Concrete\Core\Multilingual\Page\Section\Section;
 use Concrete\TestHelpers\Page\PageTestCase;
 
 class SectionTest extends PageTestCase
@@ -10,6 +11,8 @@ class SectionTest extends PageTestCase
     {
         parent::setUp();
 
+        $this->app->make('cache/request')->disable();
+
         // get entity manager from database connection
         $em = $this->connection()->getEntityManager();
 
@@ -17,7 +20,7 @@ class SectionTest extends PageTestCase
         $service = new \Concrete\Core\Localization\Locale\Service($em);
 
         // get current site
-        $site = \Core::make('site')->getSite();
+        $site = $this->app->make('site')->getSite();
 
         // get page template "full"
         $template = \Concrete\Core\Page\Template::getByHandle('full');
@@ -25,6 +28,9 @@ class SectionTest extends PageTestCase
         // add locale with home page
         $locale = $service->add($site, 'de', 'CH');
         $service->addHomePage($locale, $template, 'Second language', 'chde');
+
+        // refresh site entity from the database
+        $em->refresh($site);
     }
 
     public function testGetByLocale()
@@ -45,5 +51,47 @@ class SectionTest extends PageTestCase
         // check section
         $this->assertNotFalse($section, 'Unable to load Section by language');
         $this->assertEquals('de_CH', $section->getLocale());
+    }
+
+    public function testMultilingualEnabled()
+    {
+        $enabled = $this->app->make('multilingual/detector')->isEnabled();
+        $this->assertTrue($enabled);
+    }
+
+    public function testRegisterDuplicate()
+    {
+        $default = Section::getDefaultSection();
+        $second = Section::getByLocale('de_CH');
+
+        // Create two pages in the same language section
+        $oldPage = self::createPage('Old Page', $default);
+        $newPage = self::createPage('New Page', $default);
+
+        Section::registerPage($oldPage);
+        Section::registerDuplicate($newPage, $oldPage);
+
+        // When duplicating a page within the same language tree, a new mpRelationID should created for a new page.
+        $this->assertNotEquals(
+            Section::getMultilingualPageRelationID($oldPage->getCollectionID()),
+            Section::getMultilingualPageRelationID($newPage->getCollectionID()),
+            'A page duplicated in the same locale, but did not created new mpRelationID.'
+        );
+
+        // Create a page in the second locale
+        $newPageInSecondLocale = self::createPage('New Page in Second Locale', $second);
+
+        Section::registerDuplicate($newPageInSecondLocale, $oldPage);
+
+        // If you duplicate a page into a different language tree, it should get the same mpRelationID.
+        $this->assertEquals(
+            Section::getMultilingualPageRelationID($oldPage->getCollectionID()),
+            Section::getMultilingualPageRelationID($newPageInSecondLocale->getCollectionID()),
+            'Two pages are related between two locales, but we could not get same mpRelationID.'
+        );
+
+        $oldPage->delete();
+        $newPage->delete();
+        $newPageInSecondLocale->delete();
     }
 }
