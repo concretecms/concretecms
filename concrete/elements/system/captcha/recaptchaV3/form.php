1ed1ebcb5dfe4a5e53efc1debd3fbde70f6572e8
Michele Locati <michele@locati.it>
2019-07-30 10:25:57 +0200

    Fix translations, warnings, improve logging, explicit configuration

--- concrete/elements/system/captcha/recaptchaV3/form.php
+++ concrete/elements/system/captcha/recaptchaV3/form.php
@@ -2,57 +2,58 @@
 defined('C5_EXECUTE') or die('Access denied.');
 
 use Concrete\Core\Support\Facade\Application;
-use Concrete\Core\Support\Facade\Config;
 
 $app = Application::getFacadeApplication();
 $form = $app->make('helper/form');
-
+$config = $app->make('config');
 ?>
 
-<p><?=  t('A site key and secret key must be provided. They can be obtained from the <a href="%s" target="_blank">reCAPTCHA website</a>.', 'https://www.google.com/recaptcha/admin') ?></p>
-
-<div class="form-group">
-    <?php  echo  $form->label('site', t('Site Key')) ?>
-    <?php  echo  $form->text('site', Config::get('recaptchaV3.site_key', '')) ?>
+<div class="alert alert-info">
+    <?= t('A site key and secret key must be provided. They can be obtained from the <a href="%s" target="_blank">reCAPTCHA website</a>.', h($config->get('captcha.recaptcha_v3.url.keys_source'))) ?>
 </div>
 
 <div class="form-group">
-    <?php  echo  $form->label('secret', t('Secret Key')) ?>
-    <?php  echo  $form->text('secret', Config::get('recaptchaV3.secret_key', '')) ?>
+    <?= $form->label('site_key', t('Site Key')) ?>
+    <?= $form->text('site_key', $config->get('captcha.recaptcha_v3.site_key')) ?>
 </div>
 
 <div class="form-group">
-    <p><?= t('Set the Score (1.0 is very likely a good interaction, 0.0 is very likely a bot)')?></p>
-    <?php  echo  $form->label('score', t('Score')) ?>
-    <?php  echo  $form->text('score', Config::get('recaptchaV3.score', '0.5')) ?>
+    <?= $form->label('secret_key', t('Secret Key')) ?>
+    <?= $form->text('secret_key', $config->get('captcha.recaptcha_v3.secret_key')) ?>
 </div>
 
 <div class="form-group">
-    <?php  echo  $form->label('logscore', t('Log failed score interactions')) ?>
-    <?= $form->checkbox('logscore', '1', Config::get('recaptchaV3.logscore') ? '1' : '0'); ?>
+    <?= $form->label('score', t('Score')) ?>
+    <?= $form->number('score', $config->get('captcha.recaptcha_v3.score', '0.5'), ['min' => '0', 'max' => '1', 'step' => '0.01']) ?>
+    <div class="small text-muted"><?= t('1.0 is very likely a good interaction, 0.0 is very likely a bot')?></div>
 </div>
 
 <div class="form-group">
-    <p><?= t('Set the position of the reCAPTCHA badge.')?></p>
-    <?php  echo  $form->label('position', t('Position')) ?>
-    <?php  echo  $form->select(
+    <?= $form->label('position', t('Position of the reCAPTCHA badge')) ?>
+    <?= $form->select(
             'position',
-             array(
-            'bottomright' => t('Bottom Right'),
-            'bottomleft' => t('Bottom Left'),
-            'inline' => t('Inline'),
-            ),
-            Config::get('recaptchaV3.position', 'bottomright')) ?>
+            [
+                'bottomright' => t('Bottom Right'),
+                'bottomleft' => t('Bottom Left'),
+                'inline' => t('Inline'),
+            ],
+            $config->get('captcha.recaptcha_v3.position')
+    ) ?>
 </div>
 
 <div class="form-group">
-    <p><?= t('For extra checks you can send the clients IP address to hCaptcha. (This will effect your GDPR compliance)'); ?></p>
-    <?php echo $form->label('sendip', t('Send IP')) ?>
-    <?php echo $form->select(
-        'sendip',
-        array(
-            'yes' => t('Yes'),
-            'no' => t('No'),
-        ),
-        Config::get('recaptchaV3.sendIP', 'no')) ?>
+    <?= $form->label('', t('Options')) ?>
+    <div class="checkbox">
+        <label>
+            <?= $form->checkbox('log_score', '1', $config->get('captcha.recaptcha_v3.log_score')) ?>
+            <?= t('Log failed score interactions') ?>
+        </label>
+    </div>
+    <div class="checkbox">
+        <label>
+            <?= $form->checkbox('send_ip', '1', (bool) $config->get('captcha.recaptcha_v3.send_ip')) ?>
+            <?= t('Send IP') ?>
+            <span class="small text-muted"><br /><?= t('For extra checks you can send the clients IP address to reCaptcha (this will effect your GDPR compliance).') ?></span>
+        </label>
+    </div>
 </div>
