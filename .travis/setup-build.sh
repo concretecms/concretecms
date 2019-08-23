a992399b1a320159f35c020fe40d68ebc33aee6f
Michele Locati <michele@locati.it>
2019-08-23 08:03:45 +0200

    Avoid using Yarn in TravisCI builds

--- .travis/setup-build.sh
+++ .travis/setup-build.sh
@@ -4,11 +4,8 @@ set -o errexit
 
 source "$( dirname "${BASH_SOURCE[0]}" )/travis_retry.sh"
 
-echo 'Installing Yarn'
-travis_retry npm install yarn -g
-
-echo 'Configuring Yarn'
-travis_retry yarn global add grunt
+echo 'Installing Grunt'
+travis_retry npm -g install grunt
 
 echo 'Installing Node packages'
 cd "$TRAVIS_BUILD_DIR/build"
