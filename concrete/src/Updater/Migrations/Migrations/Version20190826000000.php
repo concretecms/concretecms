589155b96e6571b98c59f5150d6ae99115d25a87
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-08-26 16:21:28 +0300

    PHPCS fixer

--- concrete/src/Updater/Migrations/Migrations/Version20190826000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20190826000000.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
@@ -7,7 +6,6 @@ use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
 class Version20190826000000 extends AbstractMigration implements RepeatableMigrationInterface
 {
-
     public function upgradeDatabase()
     {
         /* Refresh youtube block */
