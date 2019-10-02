53a4e72bba062dc43b492dcccf49e35475e6a3d2
中澤佑美 <n.yuumi@concrete5.co.jp>
2019-10-02 17:24:48 +0900

    Add migration version file for youtube block

--- concrete/src/Updater/Migrations/Migrations/Version20191002000000.php
+++ concrete/src/Updater/Migrations/Migrations/Version20191002000000.php
@@ -4,7 +4,7 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-class Version20190826000000 extends AbstractMigration implements RepeatableMigrationInterface
+class Version20191002000000 extends AbstractMigration implements RepeatableMigrationInterface
 {
     public function upgradeDatabase()
     {
