4e287f623546afff128a1281b4027c96c9b63c8d
Derek Cameron <derek@concrete5.co.jp>
2019-10-10 21:58:53 +0900

    fixing setup-redis.sh

--- .travis/setup-redis.sh
+++ .travis/setup-redis.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "Installing php-redis"
-pecl install $REDIS_VERSION <<< ""
+if [[$(pecl install $REDIS_VERSION <<< "") =~ "pecl/redis is already installed"]]; then
 echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+fi
 
 PORTS="6379 6380"
 
 function start_node() {
 	P=$1
 	CONFIG_FILE=`tempfile`
-	printf "port $P\nrequirepass 'randomredis'" > $CONFIG_FILE << CONFIG
-CONFIG
+	echo "port $P" >> $CONFIG_FILE
+	echo "requirepass 'randomredis'" >> $CONFIG_FILE
   echo "Starting Redis server on port: $P"
 	redis-server $CONFIG_FILE > /dev/null 2>/dev/null &
 	sleep 1
