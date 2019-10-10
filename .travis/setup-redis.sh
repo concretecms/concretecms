12fa355621e71ce3fe2b2edaf56414fc0a1c5dd7
Derek Cameron <derek@concrete5.co.jp>
2019-10-10 15:58:33 +0900

    trying alternative method for install

--- .travis/setup-redis.sh
+++ .travis/setup-redis.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
-php -m
+echo "Installing php-redis"
+PECL = "pecl$TRAVIS_PHP_VERSION-sp"
+echo  "$PECL for $TRAVIS_PHP_VERSION";
+if [\("$TRAVIS_PHP_VERSION" = "5.5"\) -o \("$TRAVIS_PHP_VERSION" = "5.6"\) ]: then
+  $PECL install redis-2.2.8 <<< ""
+else
+  pecl install redis <<< ""
+fi
 
 PORTS="6379 6380"
 
