101c9a6a6616c1e8aad197f6978fd510972c8b9e
Derek Cameron <derek@concrete5.co.jp>
2019-10-10 22:12:59 +0900

    2nd try

--- .travis/setup-redis.sh
+++ .travis/setup-redis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing php-redis"
-if [[ $("pecl install $REDIS_VERSION <<< ''") =~ "redis is already installed" ]]; then
+if [[ "pecl install $REDIS_VERSION <<< ''" =~ "redis is already installed" ]]; then
   echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
 
