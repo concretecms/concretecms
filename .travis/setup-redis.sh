741e19641b932f937baebb6eba22be429adead1d
Derek Cameron <derek@concrete5.co.jp>
2019-10-10 21:44:41 +0900

    enable redis as well

--- .travis/setup-redis.sh
+++ .travis/setup-redis.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "Installing php-redis"
 pecl install $REDIS_VERSION <<< ""
+echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 PORTS="6379 6380"
 
 function start_node() {
 	P=$1
 	CONFIG_FILE=`tempfile`
-	cat > $CONFIG_FILE << CONFIG
-port $P
-requirepass 'randomredis'
+	printf "port $P\nrequirepass 'randomredis'" > $CONFIG_FILE << CONFIG
 CONFIG
   echo "Starting Redis server on port: $P"
 	redis-server $CONFIG_FILE > /dev/null 2>/dev/null &
