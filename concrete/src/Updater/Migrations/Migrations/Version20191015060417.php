4aed80dc40a7a7f04baa919fae8346209a2a442a
Derek Cameron <derek@concrete5.co.jp>
2019-10-15 15:55:07 +0900

    php-cs-fixer

--- concrete/src/Updater/Migrations/Migrations/Version20191015060417.php
+++ concrete/src/Updater/Migrations/Migrations/Version20191015060417.php
@@ -1,13 +1,11 @@
 <?php
-
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * This migration updates topic category names imported from 5.x.
  */
 class Version20191015060417 extends AbstractMigration implements RepeatableMigrationInterface
 {
@@ -16,14 +14,13 @@ class Version20191015060417 extends AbstractMigration implements RepeatableMigra
      */
     public function upgradeDatabase()
     {
-        // Updates topic category names imported from 5.x
         $sm = $this->connection->getSchemaManager();
         if ($sm->tablesExist(['TreeCategoryNodes'])) {
             $sm->renameTable('TreeCategoryNodes', '_TreeCategoryNodes');
             $this->output(t('Updating Category Names...'));
             $categories = $this->connection->fetchAll('select * from _TreeCategoryNodes');
             $queryBuilder = $this->connection->createQueryBuilder();
-            $queryBuilder->update('TreeNodes')->set('treeNodeName',':nodeName')
+            $queryBuilder->update('TreeNodes')->set('treeNodeName', ':nodeName')
                 ->where('treeNodeID = :nodeID')
                 ->andWhere($queryBuilder->expr()->orX(
                     $queryBuilder->expr()->isNull('treeNodeName'),
@@ -31,8 +28,8 @@ class Version20191015060417 extends AbstractMigration implements RepeatableMigra
                 ));
             foreach ($categories as $category) {
                 $queryBuilder->setParameters([
-                        'nodeID'=>$category['treeNodeID'],
-                        'nodeName'=>$category['treeNodeCategoryName']
+                        'nodeID' => $category['treeNodeID'],
+                        'nodeName' => $category['treeNodeCategoryName'],
                     ]
                 );
                 $queryBuilder->execute();
