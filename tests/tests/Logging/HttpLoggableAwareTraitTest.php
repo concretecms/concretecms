b4145e6e7c5a0c8e1db458ed8344dde7bedc974e
Markus Liechti <kaapiii@hotmail.com>
2020-01-14 13:18:19 +0100

    Test for HttpLoggableAwareTrait

---
+++ tests/tests/Logging/HttpLoggableAwareTraitTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Concrete\Tests\Logging;
+
+use Concrete\Core\Logging\HttpLoggableAwareTrait;
+use PHPUnit_Framework_TestCase;
+
+class HttpLoggableAwareTraitTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider routeDestinationProvider
+     *
+     * @return array
+     */
+    public function contentTypesProvider()
+    {
+        return [
+            // contentType, expected
+            // true = loggable http body types
+            [['content-type' => 'text/plain'], true],
+            [['content-type' => 'text/css'], true],
+            [['content-type' => 'text/html'], true],
+            [['content-type' => 'text/csv'], true],
+            [['content-type' => 'application/json'], true],
+            [['content-type' => 'application/xml'], true],
+            [['content-type' => 'application/rss+xml'], true],
+            // false
+            [['content-type' => 'application/rtf'], false],
+            [['content-type' => 'application/javascript'], false],
+            [['content-type' => 'image/png'], false],
+            [['content-type' => 'image/jpeg'], false]
+        ];
+    }
+
+    /**
+     * Stub config values from log -> http -> content-types
+     *
+     * @return array
+     */
+    public function getLoggableContentTypes()
+    {
+        return [
+            'text/*',
+            'application/json',
+            'application/xml',
+            'application/*+xml'
+        ];
+    }
+
+    /**
+     * @dataProvider contentTypesProvider
+     */
+    public function testIsLoggable($contentType, $expected)
+    {
+        $mockTrait = $this->getMockForTrait(HttpLoggableAwareTrait::class);
+        $mockTrait->expects($this->any())
+            ->method('getLoggableContentTypes')
+            ->willReturn($this->getLoggableContentTypes());
+
+        $this->assertEquals($expected, $mockTrait->isLoggable($contentType, $mockTrait->getLoggableContentTypes()));
+    }
+}
