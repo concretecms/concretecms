23fcd05c700827136592e3cab685940759cb565d
Karl Dilkington <MrKarlDilkington@users.noreply.github.com>
2020-01-18 14:55:55 -0500

    Update migration date and filename

--- concrete/src/Updater/Migrations/Migrations/Version20200118043285.php
+++ concrete/src/Updater/Migrations/Migrations/Version20200118043285.php
@@ -4,7 +4,7 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-class Version20190423043285 extends AbstractMigration implements RepeatableMigrationInterface
+class Version20200118043285 extends AbstractMigration implements RepeatableMigrationInterface
 {
     public function upgradeDatabase()
     {
