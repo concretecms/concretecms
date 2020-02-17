ff92bb4cc4173bcb2d37063cf76b498ac730f778
Andrew Embler <andrew@concrete5.org>
2020-02-17 21:12:01 +0000

    Updating version number back to 8.5.3a1

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -6,8 +6,8 @@ return [
      *
      * @var string
      */
-    'version' => '8.6.0a3',
-    'version_installed' => '8.6.0a3',
+    'version' => '8.5.3a1',
+    'version_installed' => '8.5.3a1',
     'version_db' => '20200203000000', // the key of the latest database migration
  
     /*
