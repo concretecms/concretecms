7a1debad8fe938295a2b2df9ecf0d43aa81834aa
Michele Locati <michele@locati.it>
2020-02-20 10:37:53 +0100

    Restart MariaDB if first upgrade fails

--- tests/assets/Docker/run-update.sh
+++ tests/assets/Docker/run-update.sh
@@ -6,4 +6,10 @@ set -o errexit
 ccm-service start db
 
 # Update concrete5 to current version
+if sudo -u www-data -- /app/concrete/bin/concrete5 c5:update -vvv; then
+    exit 0
+fi
+
+# We may need to restart the database
+ccm-service restart db
 sudo -u www-data -- /app/concrete/bin/concrete5 c5:update -vvv
