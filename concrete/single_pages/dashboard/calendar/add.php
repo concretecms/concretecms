213a6f56670ef5eed70358e8e2c20be7454cf21c
Mike Esparza <mike@portlandlabs.com>
2020-04-01 14:15:53 -0700

    Updating styling and fixing the calendar template

--- concrete/single_pages/dashboard/calendar/add.php
+++ concrete/single_pages/dashboard/calendar/add.php
@@ -31,78 +31,88 @@ if ($calendar !== null) {
 }
 ?>
 
-<section>
-    <p>
-        <b><?= t('Add a calendar using the form. ') ?></b>
-        <?=t('You may add multiple calendars, but you must add at least one before you can use the Calendar add-on')?>
-    </p>
-</section>
+<div class="row">
+    <div class="col-9">
+        <section>
+            <p>
+                <b><?= t('Add a calendar using the form. ') ?></b>
+                <?=t('You may add multiple calendars, but you must add at least one before you can use the Calendar add-on')?>
+            </p>
+        </section>
 
-<form method="post" action="<?=$view->action('submit')?>">
-    <?=Loader::helper('validation/token')->output('submit')?>
-    <fieldset>
-        <legend><?=t('Summary')?></legend>
-        <?php if ($calendar !== null) {
-    ?>
-            <input type="hidden" name="caID" value="<?=$calendar->getID()?>" />
-        <?php
+        <form method="post" action="<?=$view->action('submit')?>">
+            <?=Loader::helper('validation/token')->output('submit')?>
+            <fieldset>
+                <legend><?=t('Summary')?></legend>
+                <?php if ($calendar !== null) {
+                    ?>
+                    <input type="hidden" name="caID" value="<?=$calendar->getID()?>" />
+                    <?php
 
-} ?>
+                } ?>
 
-        <div class="form-group">
-            <?=$form->label('calendarName', t('Calendar Name'))?>
-            <?=$form->text('calendarName', $calendarName)?>
-        </div>
+                <div class="form-group">
+                    <?=$form->label('calendarName', t('Calendar Name'))?>
+                    <?=$form->text('calendarName', $calendarName)?>
 
-        <div class="ccm-dashboard-form-actions-wrapper">
-            <div class="ccm-dashboard-form-actions ">
-                <a href="<?=$view->url($preferences->getPreferredViewPath())?>" class="btn btn-default float-left"><?=t("Cancel")?></a>
-                <button type="submit" class="btn btn-primary float-right"><?=$buttonText?></button>
-            </div>
-        </div>
-    </fieldset>
-    <fieldset>
-        <legend><?=t('More Details')?></legend>
-        <div class="form-group">
-            <?=$form->label('enableMoreDetails', t('Enable Additional Event Details'))?>
-            <?=$form->select('enableMoreDetails', [
-                '' => t('Do not link to a "More Details" page.'),
-                'create' => t('Create and link to a new "More Details" page.'),
-                'associate' => t('Link to an existing page')
-            ], $enableMoreDetails)
-            ?>
-        </div>
+                    <div class="help-block">
+                        <?= t('Each separate calendar gets a complete separate list of events. Each front-end block can display content from one or multiple calendars.') ?>
+                    </div>
+                </div>
 
-        <div data-more-details-group="create" style="display: none">
-            <div class="form-group">
-                <?=$form->label('eventPageParentID', t('Parent Page'))?>
-                <?=Core::make('helper/form/page_selector')->selectPage('eventPageParentID', $eventPageParentID)?>
-            </div>
+                <div class="ccm-dashboard-form-actions-wrapper">
+                    <div class="ccm-dashboard-form-actions ">
+                        <a href="<?=$view->url($preferences->getPreferredViewPath())?>" class="btn btn-default float-left"><?=t("Cancel")?></a>
+                        <button type="submit" class="btn btn-primary float-right"><?=$buttonText?></button>
+                    </div>
+                </div>
+            </fieldset>
+            <fieldset>
+                <legend><?=t('More Details')?></legend>
+                <div class="form-group">
+                    <?=$form->label('enableMoreDetails', t('Enable Additional Event Details'))?>
+                    <?=$form->select('enableMoreDetails', [
+                        '' => t('Do not link to a "More Details" page.'),
+                        'create' => t('Create and link to a new "More Details" page.'),
+                        'associate' => t('Link to an existing page')
+                    ], $enableMoreDetails)
+                    ?>
+                </div>
 
-            <div class="form-group">
-                <?=$form->label('eventPageTypeID', t('Page Type'))?>
-                <?=$form->select('eventPageTypeID', $types, $eventPageTypeID)?>
-            </div>
+                <div data-more-details-group="create" style="display: none">
+                    <div class="form-group">
+                        <?=$form->label('eventPageParentID', t('Parent Page'))?>
+                        <?=Core::make('helper/form/page_selector')->selectPage('eventPageParentID', $eventPageParentID)?>
+                    </div>
 
-            <div class="form-group">
-                <?=$form->label('eventPageAttributeKeyHandle', t('Calendar Event Attribute'))?>
-                <?=$form->select('eventPageAttributeKeyHandle', $attributeKeys, $eventPageAttributeKeyHandle)?>
-            </div>
+                    <div class="form-group">
+                        <?=$form->label('eventPageTypeID', t('Page Type'))?>
+                        <?=$form->select('eventPageTypeID', $types, $eventPageTypeID)?>
+                    </div>
 
-        </div>
+                    <div class="form-group">
+                        <?=$form->label('eventPageAttributeKeyHandle', t('Calendar Event Attribute'))?>
+                        <?=$form->select('eventPageAttributeKeyHandle', $attributeKeys, $eventPageAttributeKeyHandle)?>
+                    </div>
 
-        <div data-more-details-group="associate" style="display: none">
-            <div class="form-group">
-                <?=$form->label('eventPageAssociatedID', t('Link to Page'))?>
-                <?=Core::make('helper/form/page_selector')->selectPage('eventPageAssociatedID', isset($eventPageAssociatedID) ? $eventPageAssociatedID : null)?>
-            </div>
-            <div class="help-block">
-                <?=t('<strong>Important</strong>: The page that you link to should contain a Calendar Event block or custom code that can render a specific calendar occurrence.')?>
-            </div>
-        </div>
+                </div>
 
-    </fieldset>
-</form>
+                <div data-more-details-group="associate" style="display: none">
+                    <div class="form-group">
+                        <?=$form->label('eventPageAssociatedID', t('Link to Page'))?>
+                        <?=Core::make('helper/form/page_selector')->selectPage('eventPageAssociatedID', isset($eventPageAssociatedID) ? $eventPageAssociatedID : null)?>
+                    </div>
+                    <div class="help-block">
+                        <?=t('<strong>Important</strong>: The page that you link to should contain a Calendar Event block or custom code that can render a specific calendar occurrence.')?>
+                    </div>
+                </div>
+
+            </fieldset>
+        </form>
+
+    </div>
+</div>
+
 
 <script type="text/javascript">
     $(function () {
