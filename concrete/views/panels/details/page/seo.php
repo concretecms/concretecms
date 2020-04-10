7a4d83c2de39dc706e279bd93c7ccae99c1eedc3
Nour Akalay <mnakalay@gmail.com>
2020-04-10 11:04:21 +0100

    Modified spacing from tabs to spaces. Mostly to force travis to re-run tests

--- concrete/views/panels/details/page/seo.php
+++ concrete/views/panels/details/page/seo.php
@@ -3,56 +3,56 @@ defined('C5_EXECUTE') or die("Access Denied.");
 ?>
 
 <div class="row">
-	<section class="ccm-ui col-sm-9">
-		<form method="post" action="<?=$controller->action('submit')?>" class="ccm-panel-detail-content-form" data-dialog-form="seo" data-panel-detail-form="seo" data-action-after-save="reload">
-			<fieldset>
-				<legend class="pt-0"><?=t('SEO')?></legend>
+    <section class="ccm-ui col-sm-9">
+        <form method="post" action="<?=$controller->action('submit')?>" class="ccm-panel-detail-content-form" data-dialog-form="seo" data-panel-detail-form="seo" data-action-after-save="reload">
+            <fieldset>
+                <legend class="pt-0"><?=t('SEO')?></legend>
 
-				<?php if ($allowEditName) {
-				?>
-				<div class="form-group">
-					<label class="control-label" for="cName"><?=t('Name')?></label>
-					<div>
-						<input type="text" class="form-control" name="cName" id="cName" value="<?php echo h($c->getCollectionName())?>">
-					</div>
-				</div>
-				<?php
-			} ?>
+                <?php if ($allowEditName) {
+                ?>
+                <div class="form-group">
+                    <label class="control-label" for="cName"><?=t('Name')?></label>
+                    <div>
+                        <input type="text" class="form-control" name="cName" id="cName" value="<?php echo h($c->getCollectionName())?>">
+                    </div>
+                </div>
+                <?php
+            } ?>
 
-				<?php if ($allowEditPaths && !$c->isGeneratedCollection()) {
-				?>
-				<div class="form-group">
-					<label class="control-label launch-tooltip" data-placement="bottom" title="<?=t('This page must always be available from at least one URL. This is that URL.')?>" class="launch-tooltip"><?=t('URL Slug')?></label>
-					<div>
-						<input type="text" class="form-control" name="cHandle" value="<?php echo $c->getCollectionHandle()?>" id="cHandle"><input type="hidden" name="oldCHandle" id="oldCHandle" value="<?php echo $c->getCollectionHandle()?>">
-					</div>
-				</div>
-				<?php
-			} ?>
+                <?php if ($allowEditPaths && !$c->isGeneratedCollection()) {
+                ?>
+                <div class="form-group">
+                    <label class="control-label launch-tooltip" data-placement="bottom" title="<?=t('This page must always be available from at least one URL. This is that URL.')?>" class="launch-tooltip"><?=t('URL Slug')?></label>
+                    <div>
+                        <input type="text" class="form-control" name="cHandle" value="<?php echo $c->getCollectionHandle()?>" id="cHandle"><input type="hidden" name="oldCHandle" id="oldCHandle" value="<?php echo $c->getCollectionHandle()?>">
+                    </div>
+                </div>
+                <?php
+            } ?>
 
-				<?php foreach ($attributes as $ak) {
-					$av = $c->getAttributeValueObject($ak);
-					$view = $ak->getControlView(new \Concrete\Core\Attribute\Context\ComposerContext());
-					$renderer = $view->getControlRenderer();
-					$view->setValue($av);
-					print $renderer->render();
+                <?php foreach ($attributes as $ak) {
+                    $av = $c->getAttributeValueObject($ak);
+                    $view = $ak->getControlView(new \Concrete\Core\Attribute\Context\ComposerContext());
+                    $renderer = $view->getControlRenderer();
+                    $view->setValue($av);
+                    print $renderer->render();
 
-				?>
-				<?php
-			} ?>
+                ?>
+                <?php
+            } ?>
 
-					<?php if (isset($sitemap) && $sitemap) {
-				?>
-						<input type="hidden" name="sitemap" value="1" />
-					<?php
-			} ?>
-			</fieldset>
-		</form>
-		<div class="ccm-panel-detail-form-actions dialog-buttons d-flex justify-content-end">
-			<button class="btn btn-secondary mr-2" type="button" data-dialog-action="cancel" data-panel-detail-action="cancel"><?=t('Cancel')?></button>
-			<button class="btn btn-success" type="button" data-dialog-action="submit" data-panel-detail-action="submit"><?=t('Save Changes')?></button>
-		</div>
-	</section>
+                    <?php if (isset($sitemap) && $sitemap) {
+                ?>
+                        <input type="hidden" name="sitemap" value="1" />
+                    <?php
+            } ?>
+            </fieldset>
+        </form>
+        <div class="ccm-panel-detail-form-actions dialog-buttons d-flex justify-content-end">
+            <button class="btn btn-secondary mr-2" type="button" data-dialog-action="cancel" data-panel-detail-action="cancel"><?=t('Cancel')?></button>
+            <button class="btn btn-success" type="button" data-dialog-action="submit" data-panel-detail-action="submit"><?=t('Save Changes')?></button>
+        </div>
+    </section>
 </div>
 
 <script type="text/javascript">
@@ -60,17 +60,17 @@ defined('C5_EXECUTE') or die("Access Denied.");
         ConcreteEvent.unsubscribe('AjaxFormSubmitSuccess.saveSeo');
         ConcreteEvent.subscribe('AjaxFormSubmitSuccess.saveSeo', function(e, data) {
             if (data.form == 'seo') {
-				ConcreteToolbar.disableDirectExit();
+                ConcreteToolbar.disableDirectExit();
                 ConcreteEvent.publish('SitemapUpdatePageRequestComplete', {'cID': data.response.cID});
             }
         });
-		$('#ccm-panel-detail-page-seo .form-control').textcounter({
-			type: "character",
-			max: -1,
-			countSpaces: true,
-			stopInputAtMaximum: false,
-			counterText: '<?php echo t('Characters'); ?>: %d',
-			countContainerClass: 'form-text text-muted'
-		});
+        $('#ccm-panel-detail-page-seo .form-control').textcounter({
+            type: "character",
+            max: -1,
+            countSpaces: true,
+            stopInputAtMaximum: false,
+            counterText: '<?php echo t('Characters'); ?>: %d',
+            countContainerClass: 'form-text text-muted'
+        });
     });
 </script>
