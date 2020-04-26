b99ab094e940afe9e214adf5243d72ae6896ca5d
jordif <jordifontseca@gmail.com>
2020-04-26 17:54:30 +0200

    Improve display of caching page panel

--- concrete/views/panels/details/page/caching.php
+++ concrete/views/panels/details/page/caching.php
@@ -32,99 +32,107 @@ switch (Config::get('concrete.cache.full_page_lifetime')) {
     <header><h3><?= t('Page Caching') ?></h3></header>
     <form method="post" action="<?= $controller->action('submit') ?>" data-dialog-form="caching"
           data-panel-detail-form="caching">
-        <h3 class="font-weight-light"><?= t('Enable Cache') ?></h3>
 
-        <div class="form-check">
-            <?= $form->radio('cCacheFullPageContent', -1, $c->getCollectionFullPageCaching(), array('enable-cache' => $enableCache)) ?>
-            <label class="form-check-label" for="cCacheFullPageContent1">
-                <?= t('Use global setting - %s', $globalSetting) ?>
-            </label>
-        </div>
+        <div class="form-group">
+            <label class="col-form-label"><?= t('Enable Cache') ?></label>
 
-        <div class="form-check">
-            <?= $form->radio('cCacheFullPageContent', 0, $c->getCollectionFullPageCaching(), array('enable-cache' => 0)) ?>
-            <label class="form-check-label" for="cCacheFullPageContent2">
-                <?= t('Do not cache this page.') ?>
-            </label>
-        </div>
+            <div class="form-check">
+                <?= $form->radio('cCacheFullPageContent', -1, $c->getCollectionFullPageCaching(), array('enable-cache' => $enableCache)) ?>
+                <label class="form-check-label" for="cCacheFullPageContent1">
+                    <?= t('Use global setting - %s', $globalSetting) ?>
+                </label>
+            </div>
 
-        <div class="form-check">
-            <?= $form->radio('cCacheFullPageContent', 1, $c->getCollectionFullPageCaching(), array('enable-cache' => 1)) ?>
-            <label class="form-check-label" for="cCacheFullPageContent3">
-                <?= t('Cache this page.') ?>
-            </label>
+            <div class="form-check">
+                <?= $form->radio('cCacheFullPageContent', 0, $c->getCollectionFullPageCaching(), array('enable-cache' => 0)) ?>
+                <label class="form-check-label" for="cCacheFullPageContent2">
+                    <?= t('Do not cache this page.') ?>
+                </label>
+            </div>
+
+            <div class="form-check">
+                <?= $form->radio('cCacheFullPageContent', 1, $c->getCollectionFullPageCaching(), array('enable-cache' => 1)) ?>
+                <label class="form-check-label" for="cCacheFullPageContent3">
+                    <?= t('Cache this page.') ?>
+                </label>
+            </div>
         </div>
 
         <hr/>
 
-        <h3 class="font-weight-light"><?= t('Duration') ?></h3>
+        <div class="form-group">
+            <label class="col-form-label"><?= t('Duration') ?></label>
 
-        <div class="ccm-properties-cache-lifetime input">
-            <?php $val = ($c->getCollectionFullPageCachingLifetimeCustomValue() > 0 && $c->getCollectionFullPageCachingLifetime()) ? $c->getCollectionFullPageCachingLifetimeCustomValue() : ''; ?>
+            <div class="ccm-properties-cache-lifetime input">
+                <?php $val = ($c->getCollectionFullPageCachingLifetimeCustomValue() > 0 && $c->getCollectionFullPageCachingLifetime()) ? $c->getCollectionFullPageCachingLifetimeCustomValue() : ''; ?>
 
-            <div class="form-check">
-                <input type="radio" class="form-check-input" name="cCacheFullPageContentOverrideLifetime"
-                       value="0" <?php if ($c->getCollectionFullPageCachingLifetime() == '0') {
-                    ?> checked="checked" <?php
-                } ?> />
-                <label class="form-check-label" for="cCacheFullPageContentOverrideLifetime">
-                    <?= t('Use global setting - %s', $globalSettingLifetime) ?>
-                </label>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="cCacheFullPageContentOverrideLifetime"
+                        value="0" <?php if ($c->getCollectionFullPageCachingLifetime() == '0') {
+                        ?> checked="checked" <?php
+                    } ?> />
+                    <label class="form-check-label" for="cCacheFullPageContentOverrideLifetime">
+                        <?= t('Use global setting - %s', $globalSettingLifetime) ?>
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <?= $form->radio('cCacheFullPageContentOverrideLifetime', 'forever', $c->getCollectionFullPageCachingLifetime()) ?>
+                    <label class="form-check-label" for="cCacheFullPageContentOverrideLifetime1">
+                        <?= t('Until manually cleared') ?>
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <?= $form->radio('cCacheFullPageContentOverrideLifetime', 'custom', $c->getCollectionFullPageCachingLifetime()) ?>
+                    <label class="form-check-label" for="cCacheFullPageContentOverrideLifetime2">
+                        <?= t('Custom') ?>
+                    </label>
+                </div>
+
+                <div class="form-inline"><?= $form->number('cCacheFullPageContentLifetimeCustom', $val, array('style' => 'width: 110px', 'min' => 1)) ?> <span class="ml-1"><?= t('minutes') ?></span></div>
+
             </div>
-
-            <div class="form-check">
-                <?= $form->radio('cCacheFullPageContentOverrideLifetime', 'forever', $c->getCollectionFullPageCachingLifetime()) ?>
-                <label class="form-check-label" for="cCacheFullPageContentOverrideLifetime1">
-                    <?= t('Until manually cleared') ?>
-                </label>
-            </div>
-
-            <div class="form-check">
-                <?= $form->radio('cCacheFullPageContentOverrideLifetime', 'custom', $c->getCollectionFullPageCachingLifetime()) ?>
-                <label class="form-check-label" for="cCacheFullPageContentOverrideLifetime2">
-                    <?= t('Custom') ?>
-                </label>
-            </div>
-
-            <div class="form-inline"><?= $form->number('cCacheFullPageContentLifetimeCustom', $val, array('style' => 'width: 110px', 'min' => 1)) ?> <?= t('minutes') ?></div>
-
         </div>
 
         <hr/>
-        <h3 class="font-weight-light"><?= t('Cache Status') ?></h3>
 
-        <?php
-        $cache = PageCache::getLibrary();
-        $rec = $cache->getRecord($c);
-        if ($rec instanceof \Concrete\Core\Cache\Page\PageCacheRecord) {
-            ?>
-            <div class="alert alert-success">
-                <?= t('This page currently exists in the full page cache. It expires %s.', Core::make('date')->formatDateTime($rec->getCacheRecordExpiration())) ?>
-                &nbsp;&nbsp;<button type="button" class="btn btn-xs btn-default pull-right"
-                                    id="ccm-button-remove-page-from-cache"><?= t('Purge') ?></button>
-            </div>
-            <?php
-        } elseif ($rec instanceof \Concrete\Core\Cache\Page\UnknownPageCacheRecord) {
-            ?>
-            <div class="alert alert-info">
-                <?= t('This page <strong>may</strong> exist in the page cache.') ?>
-                &nbsp;&nbsp;<button type="button" class="btn btn-xs btn-default pull-right"
-                                    id="ccm-button-remove-page-from-cache"><?= t('Purge') ?></button>
-            </div>
-            <?php
-        } else {
-            ?>
-            <div class="alert alert-info"><?= t('This page is not currently in the full page cache.') ?></div>
-            <?php
-        } ?>
+        <div class="form-group">
+            <label class="col-form-label"><?= t('Cache Status') ?></label>
 
-        <small class="text-muted"><?= t('Note: You can enable site-wide caching from the System & Settings area of the Dashboard.') ?></small>
+            <?php
+            $cache = PageCache::getLibrary();
+            $rec = $cache->getRecord($c);
+            if ($rec instanceof \Concrete\Core\Cache\Page\PageCacheRecord) {
+                ?>
+                <div class="alert alert-success d-md-flex justify-content-between align-items-center">
+                    <?= t('This page currently exists in the full page cache. It expires %s.', Core::make('date')->formatDateTime($rec->getCacheRecordExpiration())) ?>
+                    <button type="button" class="btn btn-sm btn-secondary ml-2"
+                                        id="ccm-button-remove-page-from-cache"><?= t('Purge') ?></button>
+                </div>
+                <?php
+            } elseif ($rec instanceof \Concrete\Core\Cache\Page\UnknownPageCacheRecord) {
+                ?>
+                <div class="alert alert-info d-md-flex justify-content-between align-items-center">
+                    <?= t('This page <strong>may</strong> exist in the page cache.') ?>
+                    <button type="button" class="btn btn-sm btn-secondary ml-2"
+                                        id="ccm-button-remove-page-from-cache"><?= t('Purge') ?></button>
+                </div>
+                <?php
+            } else {
+                ?>
+                <div class="alert alert-info"><?= t('This page is not currently in the full page cache.') ?></div>
+                <?php
+            } ?>
+
+            <small class="text-muted"><?= t('Note: You can enable site-wide caching from the System & Settings area of the Dashboard.') ?></small>
+        </div>
 
     </form>
-    <div class="ccm-panel-detail-form-actions dialog-buttons">
-        <button class="pull-left btn btn-default" type="button" data-dialog-action="cancel"
+    <div class="ccm-panel-detail-form-actions dialog-buttons d-flex justify-content-end">
+        <button class="btn btn-secondary mr-2" type="button" data-dialog-action="cancel"
                 data-panel-detail-action="cancel"><?= t('Cancel') ?></button>
-        <button class="pull-right btn btn-success" type="button" data-dialog-action="submit"
+        <button class="btn btn-success" type="button" data-dialog-action="submit"
                 data-panel-detail-action="submit"><?= t('Save Changes') ?></button>
     </div>
 
