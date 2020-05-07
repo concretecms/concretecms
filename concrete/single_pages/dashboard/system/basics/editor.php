5eca219159f67c45d5b9e74bf17d5fcfb085fb55
Nour Akalay <mnakalay@gmail.com>
2020-05-07 13:54:27 +0000

    Added the loading indicator right away when clicking on checkboxes

--- concrete/single_pages/dashboard/system/basics/editor.php
+++ concrete/single_pages/dashboard/system/basics/editor.php
@@ -175,6 +175,7 @@ $togglePreview.on('click', function(e) {
 var debounced_updatePreview = _.debounce(updatePreview, 1500);
 
 $('#ccm-editor-config input').on('change', function() {
+    $previewContent.html('<p><?= t("Loading") ?>&nbsp;<i class="fa fa-circle-notch fa-spin fa-sm"></i></p>');
     debounced_updatePreview.cancel();
     debounced_updatePreview();
 });
