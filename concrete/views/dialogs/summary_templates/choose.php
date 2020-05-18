1f3f2c7206efe2bcf856d77937e39b5660b1efa7
Andrew Embler <andrew@concrete5.org>
2020-05-18 07:32:39 -0700

    reworking summary templates dialog as a custom vue app

--- concrete/views/dialogs/summary_templates/choose.php
+++ concrete/views/dialogs/summary_templates/choose.php
@@ -1,7 +1,7 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 ?>
-<section class="ccm-ui">
+<section class="ccm-ui" data-view="choose-summary-templates">
     <header><h3><?= t('Summary Templates') ?></h3></header>
     <form method="post" action="<?= $controller->action('submit') ?>" data-dialog-form="summary-templates"
           data-panel-detail-form="summary-templates">
@@ -11,30 +11,24 @@ defined('C5_EXECUTE') or die("Access Denied.");
                 <div class="col-8 pl-0">
                     <p class="text-muted"><small><?=t('Review your summary templates to make sure they look good at different breakpoints.')?></small></p>
                 </div>
-                <div class="col-4 text-right" vue-enabled>
-                    <?php
-                    $checked = "false";
-                    if ($object->hasCustomSummaryTemplates()) {
-                        $checked = "true";
-                    }
-                    ?>
-                    <toggle-switch class="pr-2 mb-0" :value="<?=$checked?>" name="hasCustomSummaryTemplates"></toggle-switch>
+                <div class="col-4 text-right">
+                    <toggle-button class="pr-2 mb-0" v-model="hasCustomSummaryTemplates" name="hasCustomSummaryTemplates"></toggle-button>
                     <span class="text-muted"><small><?= t('Use only specific templates.') ?></small></span>
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 pl-0">
-                    <ul class="nav nav-tabs nav-fill border-bottom mb-3" data-nav="summary-template-form-factors">
-                        <li class="nav-item"><a data-width="1140" class="nav-link active" href="#"><?=t('Extra Large')?></a></li>
-                        <li class="nav-item"><a data-width="992" class="nav-link" href="#"><?=t('Large')?></a></li>
-                        <li class="nav-item"><a data-width="768" class="nav-link" href="#"><?=t('Medium')?></a></li>
-                        <li class="nav-item"><a data-width="576" class="nav-link" href="#"><?=t('Small')?></a></li>
-                        <li class="nav-item"><a data-width="480" class="nav-link" href="#"><?=t('Extra Small')?></a></li>
+                    <ul class="nav nav-tabs nav-fill border-bottom mb-3">
+                        <li class="nav-item"><a @click="setWidth(1140)" :class="{'nav-link': true, 'active': previewWidth === 1140}" href="#"><?=t('Extra Large')?></a></li>
+                        <li class="nav-item"><a @click="setWidth(992)"  :class="{'nav-link': true, 'active': previewWidth === 992}" href="#"><?=t('Large')?></a></li>
+                        <li class="nav-item"><a @click="setWidth(768)"  :class="{'nav-link': true, 'active': previewWidth === 768}" href="#"><?=t('Medium')?></a></li>
+                        <li class="nav-item"><a @click="setWidth(576)"  :class="{'nav-link': true, 'active': previewWidth === 576}" href="#"><?=t('Small')?></a></li>
+                        <li class="nav-item"><a @click="setWidth(480)"  :class="{'nav-link': true, 'active': previewWidth === 480}" href="#"><?=t('Extra Small')?></a></li>
                     </ul>
                 </div>
             </div>
 
-            <div data-list="summary-templates">
+            <div>
                 <?php
                 foreach ($templates as $instanceTemplate) {
                     $template = $instanceTemplate->getTemplate();
@@ -48,14 +42,12 @@ defined('C5_EXECUTE') or die("Access Denied.");
                         <div class="col-6 pl-0">
                             <p class="text-muted"><?=$template->getName()?></p>
                         </div>
-                        <div class="col-6 text-right" data-wrapper="toggle-switch" vue-enabled>
-                            <toggle-switch name="template_<?=$template->getID()?>" :value="<?=$checked?>"></toggle-switch>
+                        <div class="col-6 text-right">
+                            <toggle-button v-show="hasCustomSummaryTemplates" name="template_<?=$template->getID()?>" :value="<?=$checked?>"></toggle-button>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12 pl-0 text-center">
-
-                            <div data-text="loading" class="text-left"><?=t('Loading...')?></div>
 
                             <iframe width="1140" class="ccm-summary-templates-preview" src="<?=
                             URL::to('/ccm/system/summary_template/render',
@@ -66,52 +58,12 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
                             <hr>
 
-
-
-
                         </div>
                     </div>
                 <?php } ?>
             </div>
         </div>
 
-
-
-
-        <?php
-        /*
-        ?>
-
-
-        <br>
-        <br><br><br><br><br>
-
-
-        <div class="form-group form-check">
-            <label class="form-check-label">
-                <input type="checkbox" <?php if ($object->hasCustomSummaryTemplates()) { ?>checked<?php } ?>
-                       name="hasCustomSummaryTemplates" class="form-check-input">
-            </label>
-        </div>
-
-        <hr>
-
-        <h3 class="font-weight-light"><?=t("All Available Templates")?></h3>
-        <p><?=t("Available summary templates are determined by the attributes and content.")?></p>
-
-        <div class="form-group" data-list="summary-templates">
-        <?php foreach($templates as $template) { ?>
-            <div class="form-check">
-                <label class="form-check-label">
-                    <input type="checkbox"
-                           name="templateIDs[]" <?php if (!$object->hasCustomSummaryTemplates() || in_array($template->getID(), $selectedTemplateIDs)) { ?>checked<?php } ?> value="<?=$template->getID()?>" class="form-check-input">
-                    <?=$template->getName()?>
-                </label>
-            </div>
-        <?php } ?>
-        </div>
-
-        */ ?>
 
     </form>
     <div class="ccm-panel-detail-form-actions dialog-buttons">
@@ -124,28 +76,34 @@ defined('C5_EXECUTE') or die("Access Denied.");
 </section>
 
 <script type="text/javascript">
-    $(function() {
-        $('input[name=hasCustomSummaryTemplates]').on('change', function() {
-            if ($(this).is(':checked')) {
-                $('div[data-list=summary-templates] [data-wrapper=toggle-switch]').show();
-            } else {
-                $('div[data-list=summary-templates] [data-wrapper=toggle-switch]').hide();
+
+    Concrete.Vue.activateContext('cms', function (Vue, config) {
+        new Vue({
+            el: 'section[data-view=choose-summary-templates]',
+            components: config.components,
+            mounted() {
+                $(this.$el).find('iframe').on('load', function() {
+                    var offsetHeight = this.contentWindow.document.body.offsetHeight,
+                        frameHeight = offsetHeight > 480 ? 480 : offsetHeight;
+
+                    $(this).css('width', $('[data-nav=summary-template-form-factors] a.active').attr('data-width'));
+                    $(this).css('height', frameHeight);
+
+                    $(this).parent().find('div[data-text=loading]').remove();
+                });
+            },
+            methods: {
+                setWidth(width) {
+                    this.previewWidth = width
+                    $(this.$el).find('iframe').css('width', width)
+                }
+            },
+            data: {
+                hasCustomSummaryTemplates: <?=$object->hasCustomSummaryTemplates() ? 'true' : 'false' ?>,
+                previewWidth: 1140
             }
-        }).trigger('change');
+        })
+    })
 
-        $('[data-list=summary-templates] iframe').on('load', function() {
-            var offsetHeight = this.contentWindow.document.body.offsetHeight,
-                frameHeight = offsetHeight > 480 ? 480 : offsetHeight;
 
-            $(this).css('width', $('[data-nav=summary-template-form-factors] a.active').attr('data-width'));
-            $(this).css('height', frameHeight);
-
-            $(this).parent().find('div[data-text=loading]').remove();
-        });
-        $('[data-nav=summary-template-form-factors]').on('click', 'a', function() {
-            $(this).closest('ul').find('a').removeClass('active');
-            $(this).addClass('active');
-            $('[data-list=summary-templates] iframe').css('width', $(this).attr('data-width'));
-        });
-    });
 </script>
