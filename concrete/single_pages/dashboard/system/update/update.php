c9c7211492cd888c26ba9a5687a639e896ea0054
Michele Locati <michele@locati.it>
2020-05-20 10:20:06 +0200

    Fix indentation, use already defined variables

--- concrete/single_pages/dashboard/system/update/update.php
+++ concrete/single_pages/dashboard/system/update/update.php
@@ -1,120 +1,118 @@
 <?php
-defined('C5_EXECUTE') or die("Access Denied.");
-$h = Loader::helper('concrete/dashboard');
-$ih = Loader::helper('concrete/ui');
-$form = Loader::helper('form');
-$view = View::getInstance();
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+/**
+ * @var Concrete\Core\Application\Service\Dashboard $dashboard
+ * @var Concrete\Core\Form\Service\Form $form
+ * @var Concrete\Core\Html\Service\Html $html
+ * @var Concrete\Core\Application\Service\UserInterface $interface
+ * @var Concrete\Controller\SinglePage\Dashboard\System\Update\Update $controller
+ * @var bool $canUpgrade
+ * @var bool $downloadableUpgradeAvailable
+ * @var bool $hideDashboardPanel
+ * @var Concrete\Core\Validation\CSRF\Token $token
+ * @var array $updates
+ */
+
 if ($canUpgrade) {
     ?>
-
     <div class="ccm-dashboard-header-buttons">
-        <a href="<?= $view->action('check_for_updates') ?>" class="btn btn-primary">
+        <a href="<?= $controller->action('check_for_updates') ?>" class="btn btn-primary">
             <?= t('Check For Updates') ?>
         </a>
     </div>
-
-   <?php if (isset($update) && is_object($update)) { ?>
+    <?php
+    if (isset($update) && is_object($update)) {
+        ?>
         <div class="alert alert-warning">
-            <p><?= t('Before updating, it is highly recommended to make a full site backup. A full site backup consists of site files and site database export. Please consult your hosting provider for guidance on backup processes.'); ?></p>
+            <p><?= t('Before updating, it is highly recommended to make a full site backup. A full site backup consists of site files and site database export. Please consult your hosting provider for guidance on backup processes.') ?></p>
         </div>
-
         <div class="ccm-dashboard-update-details-wrapper">
-
             <div class="ccm-dashboard-update-details">
-                <div class="ccm-dashboard-update-thumbnail"><img src="<?=ASSETS_URL_IMAGES?>/logo.svg" /></div>
-                <h2><?=t('Version %s', $update->getVersion())?></h2>
-                <div><i class="fa fa-cog"></i> <span class="ccm-dashboard-update-details-testing-text"><?=t('Testing System...')?></span></div>
+                <div class="ccm-dashboard-update-thumbnail"><img src="<?= ASSETS_URL_IMAGES ?>/logo.svg" /></div>
+                <h2><?=t('Version %s', $update->getVersion()) ?></h2>
+                <div><i class="fa fa-cog"></i> <span class="ccm-dashboard-update-details-testing-text"><?= t('Testing System...') ?></span></div>
             </div>
-
             <div class="ccm-dashboard-update-nav">
-                <form method="post" action="<?=$view->action('do_update')?>">
-                    <?=$token->output('do_update')?>
-                    <input type="hidden" name="version" value="<?=$update->getVersion()?>" />
+                <form method="post" action="<?= $controller->action('do_update') ?>">
+                    <?php $token->output('do_update') ?>
+                    <input type="hidden" name="version" value="<?= $update->getVersion() ?>" />
                     <div class="ccm-dashboard-update-apply">
-                        <button class="btn btn-primary" disabled="disabled" type="submit" name="update" value="1"><?=t('Checking...')?></button>
+                        <button class="btn btn-primary" disabled="disabled" type="submit" name="update" value="1"><?= t('Checking...') ?></button>
                     </div>
                 </form>
             </div>
         </div>
-
         <div class="ccm-dashboard-update-detail-columns">
             <div class="row">
                 <div class="col-md-4">
                     <ul class="list-group">
-                        <li class="list-group-item"><span data-href="#notes" class="text-muted"><?php echo t('Release Notes')?></span></li>
-                        <li class="list-group-item"><span data-href="#addons" class="text-muted"><?php echo t('Add-On Compatibility')?></span></li>
-                        <li class="list-group-item"><span data-href="#notices" class="text-muted"><?php echo t('Important Notices')?></span></li>
+                        <li class="list-group-item"><span data-href="#notes" class="text-muted"><?= t('Release Notes') ?></span></li>
+                        <li class="list-group-item"><span data-href="#addons" class="text-muted"><?= t('Add-On Compatibility') ?></span></li>
+                        <li class="list-group-item"><span data-href="#notices" class="text-muted"><?= t('Important Notices') ?></span></li>
                     </ul>
                 </div>
                 <div class="col-md-7 col-md-offset-1 ccm-dashboard-update-detail-main">
                     <a name="notes"></a>
-                    <a href="#" target="_blank" data-url="info" style="display: none" class="btn btn-default pull-right btn-xs "><?=t('View Full Release Notes')?></a>
-                    <h3><?=t('Release Notes')?></h3>
-                    <div class="ccm-dashboard-update-detail-release-notes"><?=t('Retrieving Release Notes...')?></div>
-
+                    <a href="#" target="_blank" data-url="info" style="display: none" class="btn btn-default pull-right btn-xs "><?= t('View Full Release Notes') ?></a>
+                    <h3><?= t('Release Notes') ?></h3>
+                    <div class="ccm-dashboard-update-detail-release-notes"><?= t('Retrieving Release Notes...') ?></div>
                     <div class="spacer-row-5"></div>
-
                     <a name="addons"></a>
-                    <a href="<?=URL::to('/dashboard/extend/update')?>" class="btn btn-default pull-right btn-xs "><?=t('Update Add-Ons')?></a>
-                    <h3><?=t('Add-On Compatibility')?></h3>
-                    <?php $list = \Package::getInstalledList();
-    $ci = Core::make('helper/concrete/urls');
-    if (count($list) == 0) {
-        ?>
-                        <p><?=t('No add-ons installed.')?></p>
-
+                    <a href="<?= URL::to('/dashboard/extend/update') ?>" class="btn btn-default pull-right btn-xs "><?= t('Update Add-Ons') ?></a>
+                    <h3><?= t('Add-On Compatibility') ?></h3>
                     <?php
-    }
-    foreach ($list as $pkg) {
-        ?>
-
-                        <div class="media" data-addon="<?=$pkg->getPackageHandle()?>">
-                            <div class="pull-left"><img style="width: 49px" src="<?= $ci->getPackageIconURL($pkg);
-        ?>" class"media-object" /></div>
+                    $list = Package::getInstalledList();
+                    $ci = Core::make('helper/concrete/urls');
+                    if (count($list) == 0) {
+                        ?>
+                        <p><?= t('No add-ons installed.') ?></p>
+                        <?php
+                    }
+                    foreach ($list as $pkg) {
+                        ?>
+                        <div class="media" data-addon="<?= $pkg->getPackageHandle() ?>">
+                            <div class="pull-left"><img style="width: 49px" src="<?= $ci->getPackageIconURL($pkg) ?>" class"media-object" /></div>
                             <div class="media-body">
                                 <i class="fa fa-question-circle text-muted pull-right"></i>
-                                <h4 class="media-heading"><?= $pkg->getPackageName();
-        ?> <span class="badge badge-info" style="margin-right: 10px"><?= tc('AddonVersion', 'v.%s', $pkg->getPackageVersion());
-        ?></span></h4>
+                                <h4 class="media-heading"><?= $pkg->getPackageName() ?> <span class="badge badge-info" style="margin-right: 10px"><?= tc('AddonVersion', 'v.%s', $pkg->getPackageVersion()) ?></span></h4>
                                 <div class="ccm-dashboard-update-detail-status-text"></div>
                             </div>
                         </div>
-
-                    <?php
-    }
-    ?>
-
+                        <?php
+                    }
+                    ?>
                     <div class="spacer-row-5"></div>
-
-                    <h3><?=t('Upgrade Notices')?></h3>
+                    <h3><?= t('Upgrade Notices') ?></h3>
                     <a name="notices"></a>
-                    <div class="ccm-dashboard-update-detail-notices"><?=t('Loading...')?></div>
+                    <div class="ccm-dashboard-update-detail-notices"><?= t('Loading...') ?></div>
                 </div>
             </div>
         </div>
 
-        <script type="text/javascript">
+        <script>
         $(function() {
 
             handleError = function(r) {
                 var $statusIcon = $('.ccm-dashboard-update-details i'),
                     $statusText = $('.ccm-dashboard-update-details-testing-text');
                 $statusIcon.removeClass().addClass('fa fa-warning text-info');
-                $statusText.removeClass().addClass('text-info').text(<?=json_encode(t('Unable to retrieve information about this update from concrete5.org. You may upgrade but do so with caution.'))?>);
-                $('.ccm-dashboard-update-detail-release-notes').html(<?=json_encode(t('Unable to retrieve release notes from concrete5.org.'))?>);
-                $('.ccm-dashboard-update-detail-notices').html(<?=json_encode(t('Unable to retrieve upgrade notices from concrete5.org.'))?>);
+                $statusText.removeClass().addClass('text-info').text(<?= json_encode(t('Unable to retrieve information about this update from concrete5.org. You may upgrade but do so with caution.')) ?>);
+                $('.ccm-dashboard-update-detail-release-notes').html(<?= json_encode(t('Unable to retrieve release notes from concrete5.org.')) ?>);
+                $('.ccm-dashboard-update-detail-notices').html(<?= json_encode(t('Unable to retrieve upgrade notices from concrete5.org.')) ?>);
             }
 
             $.ajax({
                 dataType: 'json',
                 type: 'post',
                 data: {
-                    'version': '<?=$update->getVersion()?>'
+                    'version': '<?= $update->getVersion() ?>'
                 },
                 complete: function() {
-                    $('.ccm-dashboard-update-apply button').prop('disabled', false).text(<?=json_encode(t('Install Update'))?>);
+                    $('.ccm-dashboard-update-apply button').prop('disabled', false).text(<?= json_encode(t('Install Update')) ?>);
                 },
-                url: '<?=$view->action('get_update_diagnostic_information')?>',
+                url: '<?= $controller->action('get_update_diagnostic_information') ?>',
                 error: function(r) {
                     handleError(r);
                 },
@@ -153,7 +151,7 @@ if ($canUpgrade) {
                             $wrapper.append('<div class="media"><div class="pull-left"><i class="' + className + '"></i></div><div class="media-body ' + textClassName + '">' + notice.status + '</div></div>');
                         });
                     } else {
-                        $wrapper.append(<?=json_encode(t('No upgrade notices found.'))?>);
+                        $wrapper.append(<?= json_encode(t('No upgrade notices found.')) ?>);
                     }
                     var $statusIcon = $('.ccm-dashboard-update-details i'),
                         $statusText = $('.ccm-dashboard-update-details-testing-text');
@@ -181,7 +179,7 @@ if ($canUpgrade) {
                         $statusText.removeClass().addClass(textClassName).text(r.status.status);
                     } else {
                         $statusIcon.removeClass().addClass('fa fa-arrow-circle-right');
-                        $statusText.removeClass().addClass(textClassName).text(<?=json_encode(t('Update Ready'))?>);
+                        $statusText.removeClass().addClass(textClassName).text(<?= json_encode(t('Update Ready')) ?>);
                     }
                     $('[data-addon]').each(function() {
                         var $addon = $(this);
@@ -217,102 +215,74 @@ if ($canUpgrade) {
                         if (item) {
                             $addon.find('.ccm-dashboard-update-detail-status-text').addClass(textClassName).html(item.status);
                         } else {
-                            $addon.find('.ccm-dashboard-update-detail-status-text').html(<?=json_encode(t('No information about this add-on available.'))?>);
+                            $addon.find('.ccm-dashboard-update-detail-status-text').html(<?= json_encode(t('No information about this add-on available.')) ?>);
                         }
                     });
                 }
             });
         });
         </script>
-
-
-    <?php
-} else {
-    ?>
-
-
-        <?php if ($downloadableUpgradeAvailable) {
-    ?>
-
+        <?php
+    } else {
+        if ($downloadableUpgradeAvailable) {
+            ?>
             <h2><?= t('Available Update for Download') ?></h2>
-            <form method="post" action="<?= $view->action('download_update') ?>" id="ccm-download-update-form">
-
-                <?= Loader::helper('validation/token')->output('download_update') ?>
-
+            <form method="post" action="<?= $controller->action('download_update') ?>" id="ccm-download-update-form">
+                <?php $token->output('download_update') ?>
                 <legend style="line-height:40px">
                     <?= t('Version: %s', $remoteUpdate->getVersion()) ?>.
                     <?= t('Release Date: %s', date(t('F d, Y'), strtotime($remoteUpdate->getDate()))) ?>
-                    <?= Loader::helper('concrete/ui')->submit(
-                        t('Download'),
-                        'ccm-download-update-form',
-                        'right',
-                        'btn-success') ?>
+                    <?= $interface->submit(t('Download'), 'ccm-download-update-form', 'right', 'btn-success') ?>
                 </legend>
                 <div id="ccm-release-notes">
                     <?= $remoteUpdate->getNotes() ?>
                 </div>
-                <hr/>
+                <hr />
                 <span class="help-block"><?= t('Note: Downloading an update will NOT automatically install it.') ?></span>
-
             </form>
             <script>
-                $('header.ccm-dashboard-page-header').children().text(<?=json_encode(t('Currently Running %s', config::get('concrete.version'))) ?>);
+                $('header.ccm-dashboard-page-header').children().text(<?= json_encode(t('Currently Running %s', Config::get('concrete.version'))) ?>);
             </script>
-
-        <?php
-
-}
-    ?>
-
-            <h2><?= t('Apply Downloaded Update') ?></h2>
-        <?php if (count($updates)) {
-    ?>
             <?php
-            $ih = Loader::helper('concrete/ui');
-    ?>
-
+        }
+        ?>
+        <h2><?= t('Apply Downloaded Update') ?></h2>
+        <?php
+        if (count($updates)) {
+            ?>
             <p><?= t('Several updates are available. Please choose the desired update from the list below.') ?></p>
-            <span class="label"><?= t('Current Version') ?> <?= config::get('concrete.version') ?></span>
-            <form method="post" class="form" action="<?= $view->action('start') ?>" id="ccm-update-form">
+            <span class="label"><?= t('Current Version') ?> <?= Config::get('concrete.version') ?></span>
+            <form method="post" class="form" action="<?= $controller->action('start') ?>" id="ccm-update-form">
                 <?php
                 $checked = true;
-    foreach ($updates as $upd) {
-        ?>
+                foreach ($updates as $upd) {
+                    ?>
                     <div class="radio">
                         <label>
-                            <input type="radio" name="updateVersion"
-                                   value="<?= $upd->getUpdateVersion() ?>" <?= (!$checked ? '' : "checked") ?> />
+                            <input type="radio" name="updateVersion" value="<?= $upd->getUpdateVersion() ?>" <?= (!$checked ? '' : 'checked') ?> />
                             <?= $upd->getUpdateVersion() ?>
                         </label>
                     </div>
                     <?php
                     $checked = false;
-    }
-    ?>
+                }
+                ?>
                 <div class="ccm-dashboard-form-actions-wrapper">
                     <div class="ccm-dashboard-form-actions">
-                        <?= $ih->submit(t('Update'), false, 'right', 'btn-primary') ?>
+                        <?= $interface->submit(t('Update'), false, 'right', 'btn-primary') ?>
                     </div>
                 </div>
             </form>
-            </div>
             <div class="clearfix">&nbsp;</div>
-        <?php
-
+            <?php
+        } else {
+            ?>
+            <p><?= t('No updates are ready to be installed.') ?></p>
+            <?php
+        }
+    }
 } else {
     ?>
-            <p><?=t('No updates are ready to be installed.')?></p>
-
-        <?php
-}
-    ?>
+    <p><?= t('You do not have permission to upgrade this installation of concrete5.') ?></p>
     <?php
 }
-    ?>
-
-<?php
-} else {
-    ?>
-    <p><?=t('You do not have permission to upgrade this installation of concrete5.')?></p>
-<?php
-} ?>
