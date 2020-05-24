50a9d52c2d7dbcdc59b113fbb77f5d7a5585f786
Michele Locati <michele@locati.it>
2020-05-24 16:13:52 +0200

    Add File Details dashboard page

---
+++ concrete/controllers/single_page/dashboard/files/details.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Concrete\Controller\SinglePage\Dashboard\Files;
+
+use Concrete\Core\Page\Controller\DashboardPageController;
+
+class Details extends DashboardPageController
+{
+    public function view($fID = '')
+    {
+    }
+}
