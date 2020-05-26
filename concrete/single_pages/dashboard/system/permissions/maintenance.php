7297e0ac779eabece8d0128b69129535a847841c
ob7 <dev@ob7.us>
2020-05-25 16:42:27 -0800

    update maintenance mode system page to use bootstrap 4 classes

--- concrete/single_pages/dashboard/system/permissions/maintenance.php
+++ concrete/single_pages/dashboard/system/permissions/maintenance.php
@@ -6,10 +6,13 @@ echo $h->getDashboardPaneHeaderWrapper(t('Maintenance Mode'), false, 'span6 offs
 <form id="maintenance-mode-form" action="<?php echo $view->action('')?>" method="post" role="form">
     <?php echo $this->controller->token->output('update_maintenance')?>
     <fieldset>
-        <div class="form-group">
-            <label class="control-label"><?= t('Only Dashboard Works') ?></label>
-            <div class="radio"><label><?=$form->radio('site_maintenance_mode', '1', $site_maintenance_mode)?> <span><?=t('Enabled - for emergencies')?></span></label></div>
-            <div class="radio"><label><?=$form->radio('site_maintenance_mode', '0', $site_maintenance_mode)?> <span><?=t('Disabled')?></span></label></div>
+        <div class="custom-control custom-radio">
+          <?=$form->radio('site_maintenance_mode', '1', $site_maintenance_mode)?>
+          <?=$form->label('site_maintenance_mode1', t('Enabled - for emergencies'), ['class'=>'custom-control-label'])?>
+        </div>
+        <div class="custom-control custom-radio">
+          <?=$form->radio('site_maintenance_mode', '0', $site_maintenance_mode)?>
+          <?=$form->label('site_maintenance_mode2', t('Disabled'), ['class'=>'custom-control-label'])?>
         </div>
     </fieldset>	
 
