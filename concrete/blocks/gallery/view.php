1777d515c52dfe3a4cf8f6d3270ba71c3c45cdab
Korvin Szanto <me@kor.vin>
2020-06-16 16:56:21 -0700

    Replace view with 3 wide list with empty block indicator

--- concrete/blocks/gallery/view.php
+++ concrete/blocks/gallery/view.php
@@ -1,5 +1,26 @@
-<div style="margin:20px;">
-    <?php foreach($images as $image) { ?>
-        <img src="<?= $image['thumbUrl'] ?>"  alt=""/>
-    <?php } ?>
+<?php
+$page = $controller->getCollectionObject();
+if (!$images && $page && $page->isEditMode()) {
+    ?>
+    <div class="ccm-edit-mode-disabled-item"><?=t('Empty Gallery Block.')?></div>
+    <?php
+
+    // Stop outputting
+    return;
+}
+?>
+
+<div class="d-flex flex-wrap">
+    <?php
+    /** @var \Concrete\Core\Entity\File\File $image */
+    foreach ($images as $image) {
+        $tag = (new \Concrete\Core\Html\Image($image))->getTag();
+        $tag->addClass('w-100 h-auto')
+        ?>
+        <div class="px-2 pb-4" style="width: calc(100% / 3)">
+            <?= $tag ?>
+        </div>
+        <?php
+    }
+    ?>
 </div>
