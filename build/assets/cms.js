0c32d08e1c0708ae2e3923ea7cd245edffc2b310
Andrew Embler <andrew@concrete5.org>
2020-06-22 14:03:50 -0700

    Making add panel fully work in themes not based on bootstrap

--- build/assets/cms.js
+++ build/assets/cms.js
@@ -7,3 +7,5 @@ import "bootstrap/js/dist/popover";
 import "bootstrap/js/dist/tab";
 import "bootstrap/js/dist/toast";
 import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/collapse";
