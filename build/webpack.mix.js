52945a283e3070da38b700f2cb4bfc1a70c68b48
Andrew Embler <andrew@concrete5.org>
2020-06-25 12:18:09 -0700

    fixing reorganization of webpack.mix.js

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -33,14 +33,14 @@ mix.copy('node_modules/ckeditor4', '../concrete/js/ckeditor');
  * Build shared assets
  */
 // Fullcalendar
-mix.
-    copy('node_modules/fullcalendar/dist/fullcalendar.min.css', '../concrete/css/fullcalendar.css')
-    .js('node_modules/@concretecms/bedrock/assets/ckeditor/js/concrete.js', 'js/ckeditor/concrete.js');
+mix
+    .copy('node_modules/fullcalendar/dist/fullcalendar.min.css', '../concrete/css/fullcalendar.css')
+    .js('node_modules/@concretecms/bedrock/assets/calendar/js/vendor/fullcalendar.js', 'js/fullcalendar.js');
 
 // CKEditor
 mix
     .sass('node_modules/@concretecms/bedrock/assets/ckeditor/scss/concrete.scss', 'css/ckeditor/concrete.css')
-    .js('node_modules/@concretecms/bedrock/assets/calendar/js/vendor/fullcalendar.js', 'js/fullcalendar.js');
+    .js('node_modules/@concretecms/bedrock/assets/ckeditor/js/concrete.js', 'js/ckeditor/concrete.js');
 
 // The CMS entry point
 mix
