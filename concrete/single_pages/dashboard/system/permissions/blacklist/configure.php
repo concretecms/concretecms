7933bdece4afdd570db00aa0ae90da99f1e1f845
ob7 <dev@ob7.us>
2020-06-25 18:13:18 -0800

    move div outside of translate function

--- concrete/single_pages/dashboard/system/permissions/blacklist/configure.php
+++ concrete/single_pages/dashboard/system/permissions/blacklist/configure.php
@@ -19,14 +19,18 @@ $view->element('dashboard/system/permissions/blacklist/menu', ['category' => $ca
             <?= $form->checkbox('banEnabled', 1, $category->isEnabled()) ?>
             <?php
             list($selectedUnit, $unitValue) = $controller->splitSeconds($category->getTimeWindow());
+            ?>
+            <div class="form-text">
+            <?php
             echo t(
                 /* i18n: %1$s is the number of events, %2$s is the number of seconds/minutes/hours/days, %3$s is "seconds", "minutes", "hours" or "days" */
-                '<div class="form-text">Lock IP after %1$s events in %2$s %3$s</div>',
+                'Lock IP after %1$s events in %2$s %3$s',
                 $form->number('maxEvents', $category->getMaxEvents(), ['style' => 'width:90px', 'required' => 'required', 'min' => 1]),
                 $form->number('timeWindowValue', $unitValue, ['style' => 'width:90px', 'min' => 1]),
                 $form->select('timeWindowUnit', $units, $selectedUnit)
             );
             ?>
+            </div>
         </div>
 
         <div class="form-group form-inline">
@@ -35,15 +39,17 @@ $view->element('dashboard/system/permissions/blacklist/menu', ['category' => $ca
             <div class="form-check">
                 <label>
                     <?= $form->radio('banDurationUnlimited', '0', $category->getBanDuration() === null ? '1' : '0') ?>
+                    <div class="form-text">
                     <?php
                     list($selectedUnit, $unitValue) = $controller->splitSeconds($category->getBanDuration() === null ? 300 : $category->getBanDuration());
                     echo t(
                         /* i18n: %1$s is the number of seconds/minutes/hours/days, %2$s is "seconds", "minutes", "hours" or "days" */
-                        '<div class="form-text">Ban IP for %1$s %2$s</div>',
+                        'Ban IP for %1$s %2$s',
                         $form->number('banDurationValue', $unitValue, ['style' => 'width:90px', 'min' => 1]),
                         $form->select('banDurationUnit', $units, $selectedUnit)
                     );
                     ?>
+                    </div>
                 </label>
             </div>
             <br />
