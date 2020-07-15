e99e25b7502688b27acec1f346480ad44bee01e7
Nour Akalay <mnakalay@gmail.com>
2020-07-15 17:08:16 +0100

    Fixed all checkboxes in concrete/single_pages/dashboard/system/basics/accessibility.php using proper BS4 class names

--- concrete/single_pages/dashboard/system/basics/accessibility.php
+++ concrete/single_pages/dashboard/system/basics/accessibility.php
@@ -5,20 +5,20 @@
     <fieldset>
         <div class="form-group">
             <div class="form-check">
-                <label>
-                    <input class="form-check-input" name="show_titles" value="1" type="checkbox" <?= $show_titles ? 'checked' : '' ?> />
+                <input class="form-check-input" name="show_titles" value="1" type="checkbox" <?= $show_titles ? 'checked' : '' ?> />
+                <label class="form-check-label" for="show_titles">
                     <?= t('Enable Toolbar Titles') ?>
                 </label>
             </div>
             <div class="form-check">
-                <label>
-                    <input class="form-check-input" name="show_tooltips" value="1" type="checkbox" <?= $show_tooltips ? 'checked' : '' ?> />
+                <input class="form-check-input" name="show_tooltips" value="1" type="checkbox" <?= $show_tooltips ? 'checked' : '' ?> />
+                <label class="form-check-label" for="show_tooltips">
                     <?= t('Enable Prominent Toolbar Tooltips') ?>
                 </label>
             </div>
             <div class="form-check">
-                <label>
-                    <input class="form-check-input" name="increase_font_size" value="1" type="checkbox" <?= $increase_font_size ? 'checked' : '' ?> />
+                <input class="form-check-input" name="increase_font_size" value="1" type="checkbox" <?= $increase_font_size ? 'checked' : '' ?> />
+                <label class="form-check-label" for="increase_font_size">
                     <?= t('Increase Toolbar Font Size') ?>
                 </label>
             </div>
@@ -29,14 +29,14 @@
         <legend><?=t('File Manager')?></legend>
         <div class="form-group">
             <div class="form-check">
-                <label>
-                    <input class="form-check-input" name="full_lisiting_thumbnails" value="1" type="checkbox" <?= $full_lisiting_thumbnails ? 'checked' : '' ?> />
+                <input class="form-check-input" name="full_lisiting_thumbnails" value="1" type="checkbox" <?= $full_lisiting_thumbnails ? 'checked' : '' ?> />
+                <label class="form-check-label" for="full_lisiting_thumbnails">
                     <?= t('Enable Full Size Image Thumbnails') ?>
                 </label>
             </div>
             <div class="form-check">
-                <label>
-                    <input class="form-check-input" name="preview_popover" value="1" type="checkbox" <?= $preview_popover ? 'checked' : '' ?> />
+                <input class="form-check-input" name="preview_popover" value="1" type="checkbox" <?= $preview_popover ? 'checked' : '' ?> />
+                <label class="form-check-label" for="preview_popover">
                     <?= t('Enable Preview Image Popover') ?>
                 </label>
             </div>
