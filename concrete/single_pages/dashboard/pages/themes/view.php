4c5c007915dd1e3aebfe6fe7c601aff2d38c0dd8
Hamed Darragi <hd@xanweb.com>
2020-07-27 11:33:16 +0100

    PageThemes section: Replace deprecated code and do more clean ups on controllers & views

--- concrete/single_pages/dashboard/pages/themes/view.php
+++ concrete/single_pages/dashboard/pages/themes/view.php
@@ -1,10 +1,12 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.');
-
+/**
+ * @var Concrete\Core\Form\Service\Form $form
+ * @var Concrete\Core\Validation\CSRF\Token $token
+ * @var int $siteThemeID
+ */
 // HELPERS
 $app = \Concrete\Core\Support\Facade\Application::getFacadeApplication();
-$form = $app->make('helper/form');
 $ih = $app->make('helper/concrete/ui');
-$valt = $app->make('helper/validation/token');
 
 $alreadyActiveMessage = t('This theme is currently active on your site.');
 
@@ -16,13 +18,13 @@ if (isset($activate_confirm)) {
     </div>
     <div class="ccm-dashboard-form-actions-wrapper">
         <div class="ccm-dashboard-form-actions">
-            <?=$ih->button(t('Ok'), $activate_confirm, 'right', 'btn btn-primary'); ?>
-            <?=$ih->button(t('Cancel'), $view->url('/dashboard/pages/themes/'), 'left'); ?>
+            <?= $ih->button(t('Ok'), $activate_confirm, 'right', 'btn btn-primary'); ?>
+            <?= $ih->button(t('Cancel'), URL::to('/dashboard/pages/themes/'), 'left'); ?>
         </div>
     </div>
     <?php
 } else {
-        // Themes listing / Themes landing page.
+    // Themes listing / Themes landing page.
     // Separate inclusion of dashboard header and footer helpers - no pane footer.
     ?>
     <h3><?=t('Currently Installed'); ?></h3>
@@ -47,17 +49,17 @@ if (isset($activate_confirm)) {
                         </div>
                     </td>
                     <td width="100%" style="vertical-align:middle;">
-                        <div class="btn-group" style="float: right"><?php
+                        <div class="btn-group float-right"><?php
                             if ($siteThemeID == $t->getThemeID()) {
                                 echo $ih->buttonJs(t('Activate'), "alert('" . $alreadyActiveMessage . "')", 'left', 'btn-secondary ccm-button-inactive', ['disabled' => 'disabled']);
                             } else {
-                                echo $ih->button(t('Activate'), $view->url('/dashboard/pages/themes', 'activate', $t->getThemeID()), 'left', 'btn-secondary');
+                                echo $ih->button(t('Activate'), $view->action('activate', $t->getThemeID()), 'left', 'btn-secondary');
                             }
-                echo $ih->button(t('Page Templates'), $view->url('/dashboard/pages/themes/inspect', $t->getThemeID()), 'left', 'btn-secondary');
+                echo $ih->button(t('Page Templates'), $view->action('inspect', $t->getThemeID()), 'left', 'btn-secondary');
                 if ($siteThemeID == $t->getThemeID()) {
                     echo $ih->button(t('Remove'), '', 'right', 'btn-danger', ['disabled' => 'disabled']);
                 } else {
-                    echo $ih->button(t('Remove'), $view->url('/dashboard/pages/themes', 'remove', $t->getThemeID(), $valt->generate('remove')), 'right', 'btn-danger');
+                    echo $ih->button(t('Remove'), $view->action('remove', $t->getThemeID(), $token->generate('remove')), 'right', 'btn-danger');
                 } ?></div>
                         <p class="ccm-themes-name"><strong><?=$t->getThemeDisplayName(); ?></strong></p>
                         <p class="ccm-themes-description"><em><?=$t->getThemeDisplayDescription(); ?></em></p>
@@ -67,17 +69,17 @@ if (isset($activate_confirm)) {
             } ?></tbody><?php
         } ?></table>
     <form method="post" action="<?=$view->action('save_mobile_theme'); ?>" >
-        <?php
-        $valt->output('save_mobile_theme'); ?>
+        <?php $token->output('save_mobile_theme'); ?>
         <h3><?=t('Mobile Theme'); ?></h3>
         <p><?=t('To use a separate theme for mobile browsers, specify it below.'); ?></p>
         <div class="form-group form-inline">
             <?=$form->label('MOBILE_THEME_ID', t('Mobile Theme'), ['class' => 'mr-3']); ?>
             <?php
-            $themes[0] = t('** Same as website (default)');
-        foreach ($tArray as $pt) {
-            $themes[$pt->getThemeID()] = $pt->getThemeDisplayName();
-        } ?>
+                $themes[0] = t('** Same as website (default)');
+                foreach ($tArray as $pt) {
+                    $themes[$pt->getThemeID()] = $pt->getThemeDisplayName();
+                }
+            ?>
             <?=$form->select('MOBILE_THEME_ID', $themes, Config::get('concrete.misc.mobile_theme_id'), ['class' => 'mr-1']); ?>
             <button class="btn btn-secondary" type="submit"><?=t('Save'); ?></button>
         </div>
@@ -105,7 +107,7 @@ if (isset($activate_confirm)) {
                             if (strlen($t->error) > 0) {
                                 ?><div class="alert alert-danger" role="alert"><?php echo nl2br(h($t->error)); ?></div><?php
                             } else {
-                                echo $ih->button(t('Install'), $view->url('/dashboard/pages/themes', 'install', $t->getThemeHandle()), 'left', 'btn-secondary');
+                                echo $ih->button(t('Install'), $view->action('install', $t->getThemeHandle()), 'left', 'btn-secondary');
                             } ?></div>
                     </td>
                 </tr>
@@ -121,7 +123,7 @@ if (isset($activate_confirm)) {
                          border: solid 1px rgba(231, 231, 231 ,0.5);" >
                 <h3 class="mt-2"><?=t('Want more themes?'); ?></h3>
                 <p><?=t('You can download themes and add-ons from the concrete5 marketplace.'); ?></p>
-                <p><a class="btn btn-success" href="<?=$view->url('/dashboard/extend/themes'); ?>"><?=t('Get More Themes'); ?></a></p>
+                <p><a class="btn btn-success" href="<?=URL::to('/dashboard/extend/themes'); ?>"><?=t('Get More Themes'); ?></a></p>
             </div>
         <?php
         }
