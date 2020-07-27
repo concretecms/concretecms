6bda45f3d45232dbd58dd6f51d9b28268b677290
Hamed Darragi <hd@xanweb.com>
2020-07-27 22:10:48 +0100

    Revert concrete\elements\permission\duration.php to commit 6bcc88aceb2c6f34f94a78e52c048afd523fb4d2 to solve merge conflicts

--- concrete/elements/permission/duration.php
+++ concrete/elements/permission/duration.php
@@ -1,21 +1,21 @@
 <?php
 // Arguments
-// Concrete\Core\Foundation\Repetition\RepetitionInterface|null $pd
+/* Concrete\Core\Foundation\Repetition\RepetitionInterface|null $pd */
 
-use Concrete\Core\Permission\Duration;
 use Concrete\Core\Support\Facade\Application;
+use Concrete\Core\Permission\Duration;
 use Punic\Calendar;
 
-defined('C5_EXECUTE') or die('Access Denied.');
+defined('C5_EXECUTE') or die("Access Denied.");
 
 $app = Application::getFacadeApplication();
 
 $service = $app->make('helper/date');
-// @var Concrete\Core\Localization\Service\Date $service
+/* @var Concrete\Core\Localization\Service\Date $service */
 $form = $app->make('helper/form');
-// @var Concrete\Core\Form\Service\Form $form
+/* @var Concrete\Core\Form\Service\Form $form */
 $dt = $app->make('helper/form/date_time');
-// @var Concrete\Core\Form\Service\Widget\DateTime $dt
+/* @var Concrete\Core\Form\Service\Widget\DateTime $dt */
 
 $pdRepeatPeriod = '';
 $pdRepeatPeriodDaysEvery = 1;
@@ -83,7 +83,7 @@ if (isset($pd) && is_object($pd)) {
 }
 $weekdays = [];
 $wd = Calendar::getFirstWeekday();
-for ($i = 0; $i < 7; $i++) {
+for ($i = 0; $i < 7; ++$i) {
     $weekdays[$wd] = Calendar::getWeekdayName($wd, 'wide', '', true);
     $wd = ($wd + 1) % 7;
 }
@@ -123,15 +123,15 @@ for ($i = 0; $i < 7; $i++) {
         <label for="pdRepeatPeriod" class="col-form-label"><?= t('Repeats')?></label>
         <div>
             <?= $form->select(
-    'pdRepeatPeriod',
-    [
-        '' => t('** Options'),
-        'daily' => t('Every Day'),
-        'weekly' => t('Every Week'),
-        'monthly' => t('Every Month'),
-    ],
-    $pdRepeatPeriod
-) ?>
+                'pdRepeatPeriod',
+                [
+                    '' => t('** Options'),
+                    'daily' => t('Every Day'),
+                    'weekly' => t('Every Week'),
+                    'monthly' => t('Every Month'),
+                ],
+                $pdRepeatPeriod
+            ) ?>
         </div>
     </div>
     <div id="ccm-permissions-access-entity-dates-repeat-daily" style="display: none">
@@ -255,148 +255,148 @@ for ($i = 0; $i < 7; $i++) {
     </div>
 </div>
 <script type="text/javascript">
-var ccm_accessEntityCalculateRepeatOptions = function () {
-    // get the difference between start date and end date
-    if (!$("#pdStartDate_activate").is(':checked')) {
-        return false;
-    }
+    var ccm_accessEntityCalculateRepeatOptions = function () {
+        // get the difference between start date and end date
+        if (!$("#pdStartDate_activate").is(':checked')) {
+            return false;
+        }
 
-    var sdf = ($("#pdStartDate_dt_pub").datepicker('option', 'altFormat'));
-    var sdfr = $.datepicker.parseDate(sdf, $("#pdStartDate_dt").val());
-    if (!sdfr) {
-        return;
-    }
-    var edf = ($("#pdEndDate_dt_pub").datepicker('option', 'altFormat'));
-    var edfr = $.datepicker.parseDate(edf, $("#pdEndDate_dt").val());
-    if (!edfr) {
-        return;
-    }
-    var sh = $("select[name=pdStartDate_h]").val();
-    var eh = $("select[name=pdEndDate_h]").val();
-    if ($("select[name=pdStartDate_a]").val() == 'PM' && (sh < 12)) {
-        sh = parseInt(sh) + 12;
-    } else if (sh == 12 && $("select[name=pdStartDate_a]").val() == 'AM') {
-        sh = 0;
-    }
-    if ($("select[name=pdEndDate_a]").val() == 'PM' && (eh < 12)) {
-        eh = parseInt(eh) + 12;
-    } else if (eh == 12 && $("select[name=pdEndDate_a]").val() == 'AM') {
-        eh = 0;
-    }
-    var startDate = new Date(sdfr.getFullYear(), sdfr.getMonth(), sdfr.getDate(), sh, $('select[name=pdStartDate_m]').val(), 0);
-    var endDate = new Date(edfr.getFullYear(), edfr.getMonth(), edfr.getDate(), eh, $('select[name=pdEndDate_m]').val(), 0);
-    var difference = ((endDate.getTime() / 1000) - (startDate.getTime() / 1000));
-    if (difference >= 60 * 60 * 24) {
-        $('select[name=pdRepeatPeriod] option[value=daily]').attr('disabled', true);
-        $("#ccm-permissions-access-entity-dates-repeat-weekly-dow").hide();
-    } else {
-        $('select[name=pdRepeatPeriod] option[value=daily]').attr('disabled', false);
-        $("#ccm-permissions-access-entity-dates-repeat-weekly-dow").show();
-    }
-    $('input[name=pdStartRepeatDate]').val($("#pdStartDate_dt_pub").val());
-    switch (sdfr.getDay()) {
-        case 0:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=0]").attr('checked', true);
-            break;
-        case 1:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=1]").attr('checked', true);
-            break;
-        case 2:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=2]").attr('checked', true);
-            break;
-        case 3:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=3]").attr('checked', true);
-            break;
-        case 4:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=4]").attr('checked', true);
-            break;
-        case 5:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=5]").attr('checked', true);
-            break;
-        case 6:
-            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=6]").attr('checked', true);
-            break;
-    }
-};
-var ccm_accessEntityCheckRepeat = function () {
-    if ($('input[name=pdRepeat]').is(':checked')) {
-        $("#ccm-permissions-access-entity-repeat-selector").show();
-    } else {
-        $("#ccm-permissions-access-entity-repeat-selector").hide();
-    }
-};
-var ccm_accessEntityOnActivateDates = function () {
-    if ($("#pdStartDate_activate").is(':checked') || $("#pdEndDate_activate").is(':checked')) {
+        var sdf = ($("#pdStartDate_dt_pub").datepicker('option', 'altFormat'));
+        var sdfr = $.datepicker.parseDate(sdf, $("#pdStartDate_dt").val());
+        if (!sdfr) {
+            return;
+        }
+        var edf = ($("#pdEndDate_dt_pub").datepicker('option', 'altFormat'));
+        var edfr = $.datepicker.parseDate(edf, $("#pdEndDate_dt").val());
+        if (!edfr) {
+            return;
+        }
+        var sh = $("select[name=pdStartDate_h]").val();
+        var eh = $("select[name=pdEndDate_h]").val();
+        if ($("select[name=pdStartDate_a]").val() == 'PM' && (sh < 12)) {
+            sh = parseInt(sh) + 12;
+        } else if (sh == 12 && $("select[name=pdStartDate_a]").val() == 'AM') {
+            sh = 0;
+        }
+        if ($("select[name=pdEndDate_a]").val() == 'PM' && (eh < 12)) {
+            eh = parseInt(eh) + 12;
+        } else if (eh == 12 && $("select[name=pdEndDate_a]").val() == 'AM') {
+            eh = 0;
+        }
+        var startDate = new Date(sdfr.getFullYear(), sdfr.getMonth(), sdfr.getDate(), sh, $('select[name=pdStartDate_m]').val(), 0);
+        var endDate = new Date(edfr.getFullYear(), edfr.getMonth(), edfr.getDate(), eh, $('select[name=pdEndDate_m]').val(), 0);
+        var difference = ((endDate.getTime() / 1000) - (startDate.getTime() / 1000));
+        if (difference >= 60 * 60 * 24) {
+            $('select[name=pdRepeatPeriod] option[value=daily]').attr('disabled', true);
+            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow").hide();
+        } else {
+            $('select[name=pdRepeatPeriod] option[value=daily]').attr('disabled', false);
+            $("#ccm-permissions-access-entity-dates-repeat-weekly-dow").show();
+        }
+        $('input[name=pdStartRepeatDate]').val($("#pdStartDate_dt_pub").val());
+        switch (sdfr.getDay()) {
+            case 0:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=0]").attr('checked', true);
+                break;
+            case 1:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=1]").attr('checked', true);
+                break;
+            case 2:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=2]").attr('checked', true);
+                break;
+            case 3:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=3]").attr('checked', true);
+                break;
+            case 4:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=4]").attr('checked', true);
+                break;
+            case 5:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=5]").attr('checked', true);
+                break;
+            case 6:
+                $("#ccm-permissions-access-entity-dates-repeat-weekly-dow input[value=6]").attr('checked', true);
+                break;
+        }
+    };
+    var ccm_accessEntityCheckRepeat = function () {
+        if ($('input[name=pdRepeat]').is(':checked')) {
+            $("#ccm-permissions-access-entity-repeat-selector").show();
+        } else {
+            $("#ccm-permissions-access-entity-repeat-selector").hide();
+        }
+    };
+    var ccm_accessEntityOnActivateDates = function () {
+        if ($("#pdStartDate_activate").is(':checked') || $("#pdEndDate_activate").is(':checked')) {
+            ccm_accessEntityCalculateRepeatOptions();
+        }
+        if ($("#pdStartDate_activate").is(':checked') && $("#pdEndDate_activate").is(':checked')) {
+            $("#ccm-permissions-access-entity-repeat").show();
+        } else {
+            $("#ccm-permissions-access-entity-repeat").hide();
+        }
+        if ($("#pdStartDate_activate").is(':checked')) {
+            $('#pdStartDateAllDayActivate').attr('disabled', false);
+        } else {
+            $('input[name=pdStartDateAllDayActivate]').attr('disabled', true);
+        }
+        if ($("#pdEndDate_activate").is(':checked')) {
+            $('#pdEndDateAllDayActivate').attr('disabled', false);
+        } else {
+            $('input[name=pdEndDateAllDayActivate]').attr('disabled', true);
+        }
+
+        if ($("input[name=pdStartDateAllDayActivate]").is(':checked')) {
+            $('span#pdStartDate_tw').hide();
+        } else {
+            $('span#pdStartDate_tw').show();
+        }
+
+        if ($("input[name=pdEndDateAllDayActivate]").is(':checked')) {
+            $('span#pdEndDate_tw').hide();
+        } else {
+            $('span#pdEndDate_tw').show();
+        }
+    };
+    var ccm_accessEntityOnRepeatPeriodChange = function () {
+        $("#ccm-permissions-access-entity-dates-repeat-daily").hide();
+        $("#ccm-permissions-access-entity-dates-repeat-weekly").hide();
+        $("#ccm-permissions-access-entity-dates-repeat-monthly").hide();
+        if ($('select[name=pdRepeatPeriod]').val() != '') {
+            $("#ccm-permissions-access-entity-dates-repeat-" + $('select[name=pdRepeatPeriod]').val()).show();
+            $("#ccm-permissions-access-entity-dates-repeat-dates").show();
+        }
+    };
+    var ccm_accessEntityCalculateRepeatEnd = function () {
+        if ($('input[name=pdEndRepeatDate]:checked').val() == 'date') {
+            $("#ccm-permissions-access-entity-dates-repeat-dates .ccm-input-date-wrapper input").attr('disabled', false);
+        } else {
+            $("#ccm-permissions-access-entity-dates-repeat-dates .ccm-input-date-wrapper input").attr('disabled', true);
+        }
+    };
+    $(function () {
+        $("#ccm-permissions-access-entity-dates input[type=checkbox]").click(function () {
+            ccm_accessEntityOnActivateDates();
+        });
+
+        $("select[name=pdRepeatPeriod]").change(function () {
+            ccm_accessEntityOnRepeatPeriodChange();
+        });
+
+        $("input[name=pdRepeat]").click(function () {
+            ccm_accessEntityCheckRepeat();
+        });
+
+        $("#ccm-permissions-access-entity-dates span.ccm-input-date-wrapper input, #ccm-permissions-access-entity-dates span.ccm-input-time-wrapper select").change(function () {
+            ccm_accessEntityCalculateRepeatOptions();
+        });
+        $("#ccm-permissions-access-entity-dates-repeat-dates input.ccm-input-date").attr('disabled', true);
+        $('input[name=pdEndRepeatDate]').change(function () {
+            ccm_accessEntityCalculateRepeatEnd();
+        });
         ccm_accessEntityCalculateRepeatOptions();
-    }
-    if ($("#pdStartDate_activate").is(':checked') && $("#pdEndDate_activate").is(':checked')) {
-        $("#ccm-permissions-access-entity-repeat").show();
-    } else {
-        $("#ccm-permissions-access-entity-repeat").hide();
-    }
-    if ($("#pdStartDate_activate").is(':checked')) {
-        $('#pdStartDateAllDayActivate').attr('disabled', false);
-    } else {
-        $('input[name=pdStartDateAllDayActivate]').attr('disabled', true);
-    }
-    if ($("#pdEndDate_activate").is(':checked')) {
-        $('#pdEndDateAllDayActivate').attr('disabled', false);
-    } else {
-        $('input[name=pdEndDateAllDayActivate]').attr('disabled', true);
-    }
-
-    if ($("input[name=pdStartDateAllDayActivate]").is(':checked')) {
-        $('span#pdStartDate_tw').hide();
-    } else {
-        $('span#pdStartDate_tw').show();
-    }
-
-    if ($("input[name=pdEndDateAllDayActivate]").is(':checked')) {
-        $('span#pdEndDate_tw').hide();
-    } else {
-        $('span#pdEndDate_tw').show();
-    }
-};
-var ccm_accessEntityOnRepeatPeriodChange = function () {
-    $("#ccm-permissions-access-entity-dates-repeat-daily").hide();
-    $("#ccm-permissions-access-entity-dates-repeat-weekly").hide();
-    $("#ccm-permissions-access-entity-dates-repeat-monthly").hide();
-    if ($('select[name=pdRepeatPeriod]').val() != '') {
-        $("#ccm-permissions-access-entity-dates-repeat-" + $('select[name=pdRepeatPeriod]').val()).show();
-        $("#ccm-permissions-access-entity-dates-repeat-dates").show();
-    }
-};
-var ccm_accessEntityCalculateRepeatEnd = function () {
-    if ($('input[name=pdEndRepeatDate]:checked').val() == 'date') {
-        $("#ccm-permissions-access-entity-dates-repeat-dates .ccm-input-date-wrapper input").attr('disabled', false);
-    } else {
-        $("#ccm-permissions-access-entity-dates-repeat-dates .ccm-input-date-wrapper input").attr('disabled', true);
-    }
-};
-$(function () {
-    $("#ccm-permissions-access-entity-dates input[type=checkbox]").click(function () {
         ccm_accessEntityOnActivateDates();
-    });
-
-    $("select[name=pdRepeatPeriod]").change(function () {
-        ccm_accessEntityOnRepeatPeriodChange();
-    });
-
-    $("input[name=pdRepeat]").click(function () {
         ccm_accessEntityCheckRepeat();
-    });
-
-    $("#ccm-permissions-access-entity-dates span.ccm-input-date-wrapper input, #ccm-permissions-access-entity-dates span.ccm-input-time-wrapper select").change(function () {
-        ccm_accessEntityCalculateRepeatOptions();
-    });
-    $("#ccm-permissions-access-entity-dates-repeat-dates input.ccm-input-date").attr('disabled', true);
-    $('input[name=pdEndRepeatDate]').change(function () {
+        ccm_accessEntityOnRepeatPeriodChange();
         ccm_accessEntityCalculateRepeatEnd();
     });
-    ccm_accessEntityCalculateRepeatOptions();
-    ccm_accessEntityOnActivateDates();
-    ccm_accessEntityCheckRepeat();
-    ccm_accessEntityOnRepeatPeriodChange();
-    ccm_accessEntityCalculateRepeatEnd();
-});
 </script>
