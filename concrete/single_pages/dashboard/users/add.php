fb157d169b0d6dfa592f216924376828a10d5edb
Bilel <bc@xanweb.com>
2020-07-27 15:48:31 +0100

    clean add user page

--- concrete/single_pages/dashboard/users/add.php
+++ concrete/single_pages/dashboard/users/add.php
@@ -1,91 +1,100 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
-$ih = Loader::helper('concrete/ui');
+<?php defined('C5_EXECUTE') or die('Access Denied.');
 ?>
 
 <form method="post" action="<?php echo $view->action('submit'); ?>">
-    <?= $form->getAutocompletionDisabler() ?>
+    <?= $form->getAutocompletionDisabler(); ?>
 	<fieldset>
-		<legend><?php echo t('Basic Details'); ?></legend>
+		<legend><?=  t('Basic Details'); ?></legend>
+		
 		<div class="form-group">
-			<label for="uName" class="control-label"><?php echo t('Username'); ?></label>
-			<div class="input-group">
-				<?php echo $form->text('uName', array('autofocus' => 'autofocus', 'autocomplete' => 'off')); ?>
-				<span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
+			<label for="uName" class="col-form-label"><?php echo t('Username'); ?></label>
+			<div class="input-group align-items-center" >
+				<?php echo $form->text('uName', ['autofocus' => 'autofocus', 'autocomplete' => 'off']); ?>
+				<small  class="text-muted ml-1" >
+			      	* <?=  t('Required'); ?>
+				</small>
+			</div>
+		</div>
+		
+
+		<div class="form-group">
+			<label for="uPassword" class="col-form-label"><?php echo t('Password'); ?></label>
+			<div class="input-group align-items-center">
+				<?php echo $form->password('uPassword', ['autocomplete' => 'off']); ?>
+				<small  class="text-muted ml-1" >
+			      	* <?=  t('Required'); ?>
+				</small>
 			</div>
 		</div>
 
 		<div class="form-group">
-			<label for="uPassword" class="control-label"><?php echo t('Password'); ?></label>
-			<div class="input-group">
-				<?php echo $form->password('uPassword', array('autocomplete' => 'off')); ?>
-				<span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
-			</div>
-		</div>
-
-		<div class="form-group">
-			<label for="uEmail" class="control-label"><?php echo t('Email Address'); ?></label>
-			<div class="input-group">
+			<label for="uEmail" class="col-form-label"><?php echo t('Email Address'); ?></label>
+			<div class="input-group align-items-center">
 				<?php echo $form->email('uEmail'); ?>
-				<span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
+				<small  class="text-muted ml-1" >
+			     	* <?=  t('Required'); ?>
+				</small>
 			</div>
 		</div>
 
-		<?php if (count($locales)) { // "> 1" because en_US is always available ?>
+		<?php if (count($locales)) { // "> 1" because en_US is always available?>
 		<div class="form-group">
-			<label for="uEmail" class="control-label"><?php echo t('Language'); ?></label>
+			<label for="uEmail" class="col-form-label"><?php echo t('Language'); ?></label>
 			<div>
 				<?php echo $form->select('uDefaultLanguage', $locales, Localization::activeLocale()); ?>
 			</div>
 		</div>
-		<?php } ?>
+		<?php
+} ?>
 	</fieldset>
 
-<?php if (count($attribs) > 0) { ?>
+<?php if (count($attribs) > 0) {
+    ?>
 	<fieldset>
-		<legend><?php echo t('Registration Data'); ?></legend>
+		<legend><?=  t('Registration Data'); ?></legend>
 
 	<?php foreach ($attribs as $ak) {
-		if (in_array($ak->getAttributeKeyID(), $assignment->getAttributesAllowedArray())) { ?>
+        if (in_array($ak->getAttributeKeyID(), $assignment->getAttributesAllowedArray())) {
+            ?>
 		<div class="form-group">
-        	<label class="control-label"><?php echo $ak->getAttributeKeyDisplayName(); ?></label>
+        	<label class="col-form-label"><?php echo $ak->getAttributeKeyDisplayName(); ?></label>
         	<div>
                 <?php $ak->render(new \Concrete\Core\Attribute\Context\DashboardFormContext(), null, false); ?>
             </div>
         </div>
-        <?php } ?>
-    <?php } ?>
+        <?php
+        } ?>
+    <?php
+    } ?>
 
 	</fieldset>
-<?php } ?>
+<?php
+} ?>
 
 	<fieldset>
-		<legend><?php echo t('Groups'); ?></legend>
+		<legend><?= t('Groups'); ?></legend>
 		<div class="form-group">
-			<label class="control-label"><?php echo t('Place this user into groups'); ?></label>
+			<label class="col-form-label"><?= t('Place this user into groups'); ?></label>
 
 		<?php foreach ($gArray as $g) {
-			$gp = new Permissions($g);
-			if ($gp->canAssignGroup()) { ?>
-			<div class="checkbox">
-				<label>
-					<input type="checkbox" name="gID[]" value="<?php echo $g->getGroupID(); ?>"
-					<?php if (isset($_POST['gID']) && is_array($_POST['gID']) && in_array($g->getGroupID(), $_POST['gID'])) {
-					?> checked <?php } ?>>
-
-					<?php echo $g->getGroupDisplayName(); ?>
-				</label>
+        $gp = new Permissions($g);
+        if ($gp->canAssignGroup()) {
+            ?>
+			<div class="form-check">
+				    <?php  echo $form->checkbox('gID[]', $g->getGroupID(), isset($_POST['gID']) && is_array($_POST['gID']) && in_array($g->getGroupID(), $_POST['gID'])); ?>
+					<label class="form-check-label"  > <?=  $g->getGroupDisplayName(); ?> <label> 
 			</div>
-		<?php }
-		} ?>
+		<?php
+        }
+    } ?>
 
 		</div>
     </fieldset>
-	<?php echo $token->output('submit');?>
-
+	<?php echo $token->output('submit'); ?>
 	<div class="ccm-dashboard-form-actions-wrapper">
 		<div class="ccm-dashboard-form-actions">
-			<a href="<?php echo View::url('/dashboard/users/search'); ?>" class="btn btn-default pull-left"><?php echo t('Cancel'); ?></a>
-			<?php echo Loader::helper("form")->submit('add', t('Add'), array('class' => 'btn btn-primary pull-right')); ?>
+			<a href="<?= URL::to('/dashboard/users/search'); ?>" class="btn btn-secondary float-left"><?=  t('Cancel'); ?></a>
+			<?php echo $form->submit('add', t('Add'), ['class' => 'btn btn-primary float-right']); ?>
 		</div>
 	</div>
 </form>
