af92956eff823730d763ab0cf89b59cce58c30d1
Nour Akalay <mnakalay@gmail.com>
2020-08-01 21:51:24 +0100

    In single pages: - Replaced deprecated BS3 class names with proper BS4 ones - Fixed all checkboxes - Fixed all buttons
    In editor page:
    - made preview work
    - used debounce to avoid too many refresh
    - made window scroll to preview after clicking on the button
    
    In multilingual\update page made the detail badge better looking with BS4 classes
    
    In Timezone page replace deprecated FontAwesome icon with new ones
    
    Removed both "mages" single page and controller

--- concrete/single_pages/dashboard/system/basics/name.php
+++ concrete/single_pages/dashboard/system/basics/name.php
@@ -1,7 +1,7 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");?>
 
 <div class="ccm-dashboard-header-buttons">
-	<a href="<?php echo View::url('/dashboard/system/basics/attributes')?>" class="btn btn-default"><?php echo t("Manage Attributes")?></a>
+	<a href="<?php echo View::url('/dashboard/system/basics/attributes')?>" class="btn btn-secondary"><?php echo t("Manage Attributes")?></a>
 </div>
 
 <form method="post" class="ccm-dashboard-content-form" action="<?=$view->action('update_sitename')?>">
@@ -23,14 +23,15 @@
 				echo $renderer->render($ak);
 			}
 		} else { ?>
-			<p><?=t('You have not defined any <a href="%s">custom attributes</a> for this site.', URL::to('/dashboard/system/basics/attributes'))?></p>
+			<div class="ml-3">
+				<p><?=t('You have not defined any <a href="%s">custom attributes</a> for this site.', URL::to('/dashboard/system/basics/attributes'))?></p>
+			</div>
 		<?php } ?>
-
 	</fieldset>
 
 	<div class="ccm-dashboard-form-actions-wrapper">
 	<div class="ccm-dashboard-form-actions">
-		<button class="pull-right btn btn-primary" type="submit" ><?=t('Save')?></button>
+		<button class="float-right btn btn-primary" type="submit" ><?=t('Save')?></button>
 	</div>
 	</div>
 
