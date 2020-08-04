ade98769bd224516298a483a1bda774772acd352
Korvin Szanto <Korvinszanto@gmail.com>
2020-08-04 11:08:32 -0700

    Prevent error from container not being set

--- concrete/blocks/core_container/view.php
+++ concrete/blocks/core_container/view.php
@@ -6,19 +6,20 @@ defined('C5_EXECUTE') or die('Access Denied.');
  * @var $c Concrete\Core\Page\Page
  * @var $fileToRender string The file containing the container template.
  */
+if ($container) {
+    $container->startRender();
 
-$container->startRender();
+    $c = Page::getCurrentPage();
+    if ($fileToRender) {
+        include($fileToRender);
+    } else {
+        if ($c->isEditMode()) { ?>
+            <div class="ccm-edit-mode-disabled-item">
+               <?php echo t('Container: %s – no container template file found.', 
+                   $container->getInstance()->getContainer()->getContainerName()); ?>
+            </div>
+        <?php }
+    }
 
-$c = Page::getCurrentPage();
-if ($fileToRender) {
-    include($fileToRender);
-} else {
-    if ($c->isEditMode()) { ?>
-        <div class="ccm-edit-mode-disabled-item">
-           <?php echo t('Container: %s – no container template file found.', 
-               $container->getInstance()->getContainer()->getContainerName()); ?>
-        </div>
-    <?php }
+    $container->endRender();
 }
-
-$container->endRender();
