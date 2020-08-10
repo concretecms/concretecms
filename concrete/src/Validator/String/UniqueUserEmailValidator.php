c84732f105090f66f65f85f21d3a5fe3f3a144aa
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2020-08-10 13:00:03 +0900

    Change the message of email address is already in use

--- concrete/src/Validator/String/UniqueUserEmailValidator.php
+++ concrete/src/Validator/String/UniqueUserEmailValidator.php
@@ -60,13 +60,13 @@ class UniqueUserEmailValidator extends EmailValidator implements ValidatorForSub
         $this->setErrorString(
             self::E_EMAIL_IN_USE,
             function (UniqueUserEmailValidator $validator, $code, $mixed) {
-                return t('The email address "%s" is already in use.', $mixed);
+                return t('The email address "%s" is already in use. Please contact the administrator of this site for more information.', $mixed);
             }
         );
         $this->setErrorString(
             self::E_EMAIL_USED_BY_ANOTHER_USER,
             function (UniqueUserEmailValidator $validator, $code, $mixed) {
-                return t('The email address "%s" is already in use.', $mixed);
+                return t('The email address "%s" is already in use. Please contact the administrator of this site for more information.', $mixed);
             }
         );
     }
