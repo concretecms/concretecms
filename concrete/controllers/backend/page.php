ffda59d314bd381e10665725cd76275b6cdad60a
Michele Locati <michele@locati.it>
2020-08-15 23:01:55 +0200

    Modernize page backend controller

--- concrete/controllers/backend/page.php
+++ concrete/controllers/backend/page.php
@@ -2,29 +2,29 @@
 
 namespace Concrete\Controller\Backend;
 
-use Concrete\Core\Page\EditResponse as PageEditResponse;
+use Concrete\Core\Controller\Controller;
+use Concrete\Core\Error\UserMessageException;
+use Concrete\Core\Http\ResponseFactoryInterface;
+use Concrete\Core\Page\EditResponse;
+use Concrete\Core\Page\Page as ConcretePage;
+use Concrete\Core\Page\Type\Type as PageType;
+use Concrete\Core\Permission\Checker;
 use Concrete\Core\User\User as ConcreteUser;
-use Controller;
-use Core;
-use Loader;
-use Page as ConcretePage;
-use PageType;
-use Permissions;
-use Redirect;
+use Concrete\Core\Validation\SanitizeService;
 
 class Page extends Controller
 {
     public function create($ptID, $parentID = false)
     {
-        $pagetype = PageType::getByID(Loader::helper('security')->sanitizeInt($ptID));
-        if (is_object($pagetype)) {
+        $pagetype = PageType::getByID($this->app->make(SanitizeService::class)->sanitizeInt($ptID));
+        if (!$pagetype) {
             $proceed = false;
             $parent = $parentID ? ConcretePage::getByID($parentID) : null;
-            if (is_object($parent) && !$parent->isError()) {
-                $pp = new Permissions($parent);
+            if ($parent && !$parent->isError()) {
+                $pp = new Checker($parent);
                 $proceed = $pp->canAddSubCollection($pagetype);
             } else {
-                $ptp = new Permissions($pagetype);
+                $ptp = new Checker($pagetype);
                 $proceed = $ptp->canAddPageType();
                 $parent = null;
             }
@@ -35,42 +35,41 @@ class Page extends Controller
                     $d->setPageDraftTargetParentPageID($parent->getCollectionID());
                 }
 
-                return Redirect::url(\Core::getApplicationURL() . '/' . DISPATCHER_FILENAME . '?cID=' . $d->getCollectionID() . '&ctask=check-out-first&' . Loader::helper('validation/token')->getParameter());
+                return $this->buildRedirect('/' . DISPATCHER_FILENAME . '?cID=' . $d->getCollectionID() . '&ctask=check-out-first&' . $this->app->make('token')->getParameter());
             }
         }
     }
 
     public function exitEditMode($cID, $token)
     {
-        if (Loader::helper('validation/token')->validate('', $token)) {
-            $c = ConcretePage::getByID($cID);
-            $cp = new Permissions($c);
-            if ($cp->canViewToolbar()) {
-                $u = $this->app->make(ConcreteUser::class);
-                $u->unloadCollectionEdit();
-            }
-
-            return Redirect::page($c);
+        $valt = $this->app->make('token');
+        if (!$valt->validate('', $token)) {
+            throw new UserMessageException(t('Access Denied'));
+        }
+        $c = ConcretePage::getByID($cID);
+        $cp = new Checker($c);
+        if ($cp->canViewToolbar()) {
+            $u = $this->app->make(ConcreteUser::class);
+            $u->unloadCollectionEdit();
         }
 
-        return new \Response(t('Access Denied'));
+        return $this->buildRedirect([$c]);
     }
 
     public function getJSON()
     {
         $h = $this->app->make('helper/concrete/dashboard/sitemap');
-        if ($h->canRead()) {
-            $c = ConcretePage::getByID((int) $this->request->request->get('cID'));
-            $cp = new Permissions($c);
-            if ($cp->canViewPage()) {
-                $r = new PageEditResponse();
-                $r->setPage($c);
-                $r->outputJSON();
-            } else {
-                Core::make('helper/ajax')->sendError(t('You are not allowed to access this page.'));
-            }
-        } else {
-            Core::make('helper/ajax')->sendError(t('You do not have access to the sitemap.'));
+        if (!$h->canRead()) {
+            throw new UserMessageException(t('You do not have access to the sitemap.'));
         }
+        $c = ConcretePage::getByID((int) $this->request->request->get('cID'));
+        $cp = new Checker($c);
+        if (!$cp->canViewPage()) {
+            throw new UserMessageException(t('You are not allowed to access this page.'));
+        }
+        $r = new EditResponse();
+        $r->setPage($c);
+
+        return $this->app->make(ResponseFactoryInterface::class)->json($r);
     }
 }
