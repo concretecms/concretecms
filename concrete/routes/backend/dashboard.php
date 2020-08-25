43618c9dbd3044f237fab9b12957122085d7406c
Michele Locati <michele@locati.it>
2020-08-25 08:44:19 +0200

    Prepare router for dashboard routes

---
+++ concrete/routes/backend/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+/**
+ * @var \Concrete\Core\Routing\Router $router
+ */
+/*
+ * Base path: /ccm/system/backend/dashboard
+ * Namespace: Concrete\Controller\Backend\Dashboard
+ */
