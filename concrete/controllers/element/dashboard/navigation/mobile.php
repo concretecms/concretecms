bcd20935e5d19d2ec18fee935c7993584bb57405
ob7 <dev@ob7.us>
2020-09-03 10:49:26 -0800

    dashboard/navigation/mobile view and controller

---
+++ concrete/controllers/element/dashboard/navigation/mobile.php
@@ -0,0 +1,38 @@
+<?php
+namespace Concrete\Controller\Element\Dashboard\Navigation;
+
+use Concrete\Core\Application\UserInterface\Dashboard\Navigation\FullNavigationFactory;
+use Concrete\Core\Controller\ElementController;
+//use Concrete\Core\Navigation\Modifier\TopLevelOnlyModifier;
+use Concrete\Core\Navigation\Modifier\AppendHTMLModifier;
+use Concrete\Core\Navigation\NavigationModifier;
+
+class Mobile extends ElementController
+{
+
+    /**
+     * @var FullNavigationFactory
+     */
+    protected $factory;
+
+    public function getElement()
+    {
+        return 'dashboard/navigation/mobile';
+    }
+
+    public function __construct(FullNavigationFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+
+    public function view()
+    {
+        $navigation = $this->factory->createNavigation();
+        $modifier = new NavigationModifier();
+        //$modifier->addModifier(new TopLevelOnlyModifier());
+        //$modifier->addModifier(new AppendHTMLModifier());
+        $this->set('navigation', $modifier->process($navigation));
+    }
+
+}
