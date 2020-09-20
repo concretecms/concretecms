df57de3edc540791b6bf173985f3d584af763b8e
Michele Locati <michele@locati.it>
2020-09-21 01:12:30 +0200

    Page Type tools to routes: composer/form/edit_control

--- concrete/routes/backend/page_types.php
+++ concrete/routes/backend/page_types.php
@@ -13,3 +13,6 @@ defined('C5_EXECUTE') or die('Access Denied.');
 
 $router->all('composer/form/add_control', 'Composer\Form\AddControl::view');
 $router->post('composer/form/add_control/pick', 'Composer\Form\AddControl\Pick::view');
+
+$router->all('composer/form/edit_control', 'Composer\Form\EditControl::view');
+$router->post('composer/form/edit_control/save', 'Composer\Form\EditControl\Save::view');
