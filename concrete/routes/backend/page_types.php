8e43324b44f7d37fd3fe320457d227c44ab84096
Michele Locati <michele@locati.it>
2020-09-20 16:48:23 +0200

    Prepare router for page type routes

---
+++ concrete/routes/backend/page_types.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+/**
+ * @var Concrete\Core\Routing\Router $router
+ */
+
+/*
+ * Base path: /ccm/system/page/type
+ * Namespace: Concrete\Controller\Backend\Page\Type
+ */
+
