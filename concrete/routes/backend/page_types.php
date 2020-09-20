6a2ef597fc343382d77aee554bf2cfc0580d692f
Michele Locati <michele@locati.it>
2020-09-20 18:05:53 +0200

    Page Type tools to routes: composer/form/add_control

--- concrete/routes/backend/page_types.php
+++ concrete/routes/backend/page_types.php
@@ -11,3 +11,5 @@ defined('C5_EXECUTE') or die('Access Denied.');
  * Namespace: Concrete\Controller\Backend\Page\Type
  */
 
+$router->all('composer/form/add_control', 'Composer\Form\AddControl::view');
+$router->post('composer/form/add_control/pick', 'Composer\Form\AddControl\Pick::view');
