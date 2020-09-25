945a1a7e0877cb5649438d5d0fa7908033401221
Michele Locati <michele@locati.it>
2020-09-25 14:09:05 +0200

    Permissions tools to routes: categories/notification

---
+++ concrete/src/Permission/Category/TaskHandler/Notification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Concrete\Core\Permission\Category\TaskHandler;
+
+use Concrete\Core\Error\UserMessageException;
+use Concrete\Core\Permission\Category\DefaultTaskHandler;
+use Concrete\Core\Permission\Checker;
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+class Notification extends DefaultTaskHandler
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Permission\Category\DefaultTaskHandler::checkAccess()
+     */
+    protected function checkAccess(): void
+    {
+        $p = new Checker();
+        if (!$p->canAccessTaskPermissions()) {
+            throw new UserMessageException(t('Access Denied.'));
+        }
+    }
+}
