91e484befc5b0fb7c64f8e5202fd5d9912f40468
Michele Locati <michele@locati.it>
2020-09-27 10:32:24 +0200

    Fix phpdoc

--- concrete/routes/dialogs/permissions.php
+++ concrete/routes/dialogs/permissions.php
@@ -1,9 +1,14 @@
 <?php
 
 defined('C5_EXECUTE') or die('Access Denied.');
+
 /**
- * @var \Concrete\Core\Routing\Router
+ * @var \Concrete\Core\Routing\Router $router
+ */
+
+/*
  * Base path: /ccm/system/dialogs/permissions
  * Namespace: Concrete\Controller\Dialog\Permissions\
  */
+
 $router->all('/access/entity/site_group', 'Access\Entity\SiteGroup::view');
