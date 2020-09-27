067383246775655f2ebd6943919241b49c5bddc1
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2020-09-27 14:02:25 +0900

    Mark deprecated to unused methods in Banned Word Service
    And PHP CS Fix

--- concrete/src/Validation/BannedWord/Service.php
+++ concrete/src/Validation/BannedWord/Service.php
@@ -1,27 +1,48 @@
 <?php
+
 namespace Concrete\Core\Validation\BannedWord;
 
 class Service
 {
+    /** @deprecated */
     const CASE_FIRST_LOWER = 0;
+    /** @deprecated */
     const CASE_FIRST_UPPER = 1;
+    /** @deprecated */
     const CASE_HAS_LOWER = 2;
+    /** @deprecated */
     const CASE_HAS_UPPER = 4;
+    /** @deprecated */
     const CASE_HAS_NONALPH = 8;
+    /** @deprecated */
     const CASE_MIXED = 6;
-
+    /** @deprecated */
     const TRUNCATE_CHARS = '';
+    /** @deprecated */
     const TRUNCATE_WORDS = '\s+';
+    /** @deprecated */
     const TRUNCATE_SENTS = '[!.?]\s+';
+    /** @deprecated */
     const TRUNCATE_PARS = '\n{2,}';
 
+    /** @var string[] From version 9, this will be private. */
     public $bannedWords;
 
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $file
+     *
+     * @return false
+     */
     public function getCSV_simple($file)
     {
         return false;
     }
 
+    /**
+     * @deprecated this method will be removed from version 9
+     */
     public function loadBannedWords()
     {
         if ($this->bannedWords) {
@@ -29,16 +50,23 @@ class Service
         }
         $bw = new BannedWordList();
         $bannedWords = $bw->get();
-        $this->bannedWords = array();
+        $this->bannedWords = [];
         foreach ($bannedWords as $word) {
             $this->bannedWords[] = $word->getWord();
         }
     }
 
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $word
+     *
+     * @return int
+     */
     public function wordCase($word)
     {
-        $lower = "abcdefghijklmnopqrstuvwxyz";
-        $UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $lower = 'abcdefghijklmnopqrstuvwxyz';
+        $UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $i = 0;
         $case = 0;
         while ($c = $word[$i]) {
@@ -63,6 +91,12 @@ class Service
         return $case;
     }
 
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $case
+     * @param $word
+     */
     public function forceCase($case, &$word)
     {
         $word = strtolower($word);
@@ -81,6 +115,13 @@ class Service
         }
     }
 
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $word
+     *
+     * @return bool
+     */
     public function isBannedWord(&$word)
     {
         $case = self::wordCase($word);
@@ -99,7 +140,7 @@ class Service
     public function setBannedWords($bannedWords)
     {
         $this->bannedWords = $bannedWords;
-                }
+    }
 
     /**
      * @return string[]
@@ -109,11 +150,13 @@ class Service
         if (!is_array($this->bannedWords) || count($this->bannedWords) === 0) {
             $this->loadBannedWords();
         }
+
         return $this->bannedWords;
-            }
+    }
 
     /**
      * @param string $string String to check it has banned words. From version 9, it won't be passed by reference.
+     *
      * @return int Return 1 if string has banned words, otherwise 0. From version 9, it will return boolean.
      */
     public function hasBannedWords(&$string)
@@ -124,12 +167,19 @@ class Service
             if (mb_stripos($string, $bannedWord) !== false) {
                 $out = 1;
                 break;
-        }
+            }
         }
 
         return $out;
     }
 
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $string
+     *
+     * @return bool
+     */
     public function hasBannedPart($string)
     {
         $this->loadBannedWords();
@@ -143,24 +193,41 @@ class Service
         return false;
     }
 
-    public function truncate($string, $num, $which = self::TRUNCATE_CHARS, $ellipsis = "&#8230;")
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $string
+     * @param $num
+     * @param string $which
+     * @param string $ellipsis
+     *
+     * @return string
+     */
+    public function truncate($string, $num, $which = self::TRUNCATE_CHARS, $ellipsis = '&#8230;')
     {
         $parts = preg_split("/($which)/", $string, -1, PREG_SPLIT_DELIM_CAPTURE);
         $i = 0;
-        $out = "";
+        $out = '';
         while (count($parts) && ++$i < $num) {
-            $out .= array_shift($parts).array_shift($parts);
+            $out .= array_shift($parts) . array_shift($parts);
         }
         if (count($parts)) {
-            $out = trim($out).$ellipsis;
+            $out = trim($out) . $ellipsis;
         }
 
         return $out;
     }
 
+    /**
+     * @deprecated this method will be removed from version 9
+     *
+     * @param $inputArray
+     *
+     * @return array
+     */
     public function getBannedKeys($inputArray)
     {
-        $error_keys = array();
+        $error_keys = [];
         if (is_array($inputArray) && count($inputArray)) {
             foreach (array_keys($inputArray) as $k) {
                 if (is_string($inputArray[$k]) && $this->hasBannedWords($inputArray[$k])) {
