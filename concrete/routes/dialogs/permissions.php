60ac8c117f930e2515e64e1342915cab0b2030be
Michele Locati <michele@locati.it>
2020-09-27 11:00:30 +0200

    Permissions tools to routes: dialogs/block_type

--- concrete/routes/dialogs/permissions.php
+++ concrete/routes/dialogs/permissions.php
@@ -15,3 +15,4 @@ $router->all('/access/entity/site_group', 'Access\Entity\SiteGroup::view');
 $router->all('/access/entity/types/group_combination', 'Access\Entity\Types\GroupCombination::view');
 $router->all('/access/entity/types/group_set', 'Access\Entity\Types\GroupSet::view');
 $router->all('/basic_workflow', 'BasicWorkflow::view');
+$router->all('/block_type', 'BlockType::view');
