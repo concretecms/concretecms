654bd3b2181d566cf3b6256140f68a87c8d8b261
Michele Locati <michele@locati.it>
2020-09-29 19:32:21 +0200

    Fix Redis prefix for session cookies

--- concrete/src/Session/SessionFactory.php
+++ concrete/src/Session/SessionFactory.php
@@ -275,10 +275,10 @@ class SessionFactory implements SessionFactoryInterface
 
         // In case of anyone setting prefix on the redis server directly
         // Similar to how we do it on cache
-        $prefix = array_get($options, 'prefix', 'CCM_SESSION');
+        $prefix = array_get($options, 'prefix') ?: array_get($config, 'name') ?: 'CCM_SESSION';
 
         // We pass the prefix to the Redis Handler when we build it
-        return $this->app->make(RedisSessionHandler::class, [$redis, ['prefix' => array_get($config, 'name') ?: $prefix]]);
+        return $this->app->make(RedisSessionHandler::class, [$redis, ['prefix' => $prefix]]);
     }
 
     /**
