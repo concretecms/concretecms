bc9bac12da3a416be56c2b40620ba1ce6bd21d25
Michele Locati <michele@locati.it>
2020-10-01 09:51:10 +0200

    Fix phpdoc

--- concrete/routes/actions/blocks.php
+++ concrete/routes/actions/blocks.php
@@ -1,17 +1,24 @@
 <?php
 
 defined('C5_EXECUTE') or die('Access Denied.');
+
 /**
  * @var \Concrete\Core\Routing\Router $router
- * Base path: /ccm/system/block
- * Namespace: Concrete\Controller\Backend\
  */
+
+ /*
+  * Base path: /ccm/system/block
+  * Namespace: Concrete\Controller\Backend\
+  */
+
 $router->all('/preview', 'Block\Preview::render');
 $router->all('/render/', 'Block::render');
 $router->all('/action/add/{cID}/{arHandle}/{btID}/{action}', 'Block\Action::add')
-    ->setRequirements(['action' => '.+']);
+    ->setRequirements(['action' => '.+'])
+;
 $router->all('/action/edit/{cID}/{arHandle}/{bID}/{action}', 'Block\Action::edit')
-    ->setRequirements(['action' => '.+']);
+    ->setRequirements(['action' => '.+'])
+;
 $router->all('/action/add_composer/{ptComposerFormLayoutSetControlID}/{action}', 'Block\Action::add_composer');
 $router->all('/action/edit_composer/{cID}/{arHandle}/{ptComposerFormLayoutSetControlID}/{action}', 'Block\Action::edit_composer');
 $router->all('/process/alias/{cID}/{arHandle}/{pcID}/{dragAreaBlockID}', 'Block\Process::alias');
