15b6c9728e43c6253ff8ebcdd2d5380e251eb94a
Michele Locati <michele@locati.it>
2020-10-01 10:07:11 +0200

    Root tools to routes: pile_manager

--- concrete/routes/actions/blocks.php
+++ concrete/routes/actions/blocks.php
@@ -22,3 +22,11 @@ $router->all('/action/edit/{cID}/{arHandle}/{bID}/{action}', 'Block\Action::edit
 $router->all('/action/add_composer/{ptComposerFormLayoutSetControlID}/{action}', 'Block\Action::add_composer');
 $router->all('/action/edit_composer/{cID}/{arHandle}/{ptComposerFormLayoutSetControlID}/{action}', 'Block\Action::edit_composer');
 $router->all('/process/alias/{cID}/{arHandle}/{pcID}/{dragAreaBlockID}', 'Block\Process::alias');
+$router
+    ->all('/process/copy/{cID}/{arHandle}/{bID}', 'Block\Process::copy')
+    ->setRequirements([
+        'cID' => '[1-9]\d*',
+        'arHandle' => '.+',
+        'bID' => '[1-9]\d*',
+    ])
+;
