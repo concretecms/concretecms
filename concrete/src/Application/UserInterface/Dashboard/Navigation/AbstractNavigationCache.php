a8cafe4b3aa2c35957acc027342851ea991dead8
Michele Locati <michele@locati.it>
2020-10-20 09:04:20 +0200

    Make the navigation cache localization-aware

--- concrete/src/Application/UserInterface/Dashboard/Navigation/AbstractNavigationCache.php
+++ concrete/src/Application/UserInterface/Dashboard/Navigation/AbstractNavigationCache.php
@@ -1,6 +1,7 @@
 <?php
 namespace Concrete\Core\Application\UserInterface\Dashboard\Navigation;
 
+use Concrete\Core\Localization\Localization;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 abstract class AbstractNavigationCache
@@ -13,29 +14,44 @@ abstract class AbstractNavigationCache
      */
     protected $session;
 
-    public function __construct(Session $session)
+    /**
+     * @var \Concrete\Core\Localization\Localization
+     */
+    protected $localization;
+
+    public function __construct(Session $session, Localization $localization)
     {
         $this->session = $session;
+        $this->localization = $localization;
     }
 
     public function has()
     {
-        return $this->session->has($this->getIdentifier());
+        return $this->session->has($this->getSessionIdentifier());
     }
 
     public function set(Navigation $navigation)
     {
-        $this->session->set($this->getIdentifier(), $navigation);
+        $this->session->set($this->getSessionIdentifier(), $navigation);
     }
 
     public function get()
     {
-        return $this->session->get($this->getIdentifier());
+        return $this->session->get($this->getSessionIdentifier());
     }
 
     public function clear()
     {
-        $this->session->remove($this->getIdentifier());
+        $prefix = $this->getIdentifier() . '@';
+        foreach (array_keys($this->session->all()) as $sessionKey) {
+            if (strpos($sessionKey, $prefix) === 0) {
+                $this->session->remove($sessionKey);
+            }
+        }
     }
 
+    protected function getSessionIdentifier(): string
+    {
+        return $this->getIdentifier() . '@' . $this->localization->getLocale();
+    }
 }
