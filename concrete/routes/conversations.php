f5573c7a5fa0d5d9f49c6c1df982a42044ff390d
Michele Locati <michele@locati.it>
2020-10-23 22:02:07 +0200

    Prepare router for conversations routes

---
+++ concrete/routes/conversations.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('C5_EXECUTE') or die('Access Denied.');
+/**
+ * @var \Concrete\Core\Routing\Router $router
+ * Base path: /ccm/frontend/conversations
+ * Namespace: Concrete\Controller\Frontend\Conversations
+ */
