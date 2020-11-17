1071aa2823459ccfae38449da2dd72e2807a46ac
Andrew Embler <andrew@concrete5.org>
2020-11-17 12:50:53 -0800

    improvements to dropdown workflow menu

--- concrete/src/Application/Service/UserInterface.php
+++ concrete/src/Application/Service/UserInterface.php
@@ -407,6 +407,7 @@ class UserInterface
             } else {
                 $text .= '<div class="dropup"><button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' . t('Action') . '</button><div class="dropdown-menu">';
                 foreach ($arguments['buttons'] as $button) {
+                    $button->addClass('dropdown-item');
                     $text .= $button;
                 }
                 $text .= '</div></div>';
