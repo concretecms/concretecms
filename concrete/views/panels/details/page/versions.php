5ace8e04fc4dfd90e921b7f45de6bc8c1d94de33
Andrew Embler <andrew@concrete5.org>
2020-11-16 18:25:00 -0800

    Making page versions not show toolbar anymore when previewing, fixes

--- concrete/views/panels/details/page/versions.php
+++ concrete/views/panels/details/page/versions.php
@@ -13,26 +13,32 @@ if (!isset($_REQUEST['cvID']) || !is_array($_REQUEST['cvID'])) {
     die(t('Invalid Request.'));
 }
 
-?><div style="height: 100%"><?php
+?><div class="h-100 pt-4"><?php
     $tabs = array();
     $checked = true;
     foreach ($_REQUEST['cvID'] as $key => $cvID) {
         if (!$val->integer($cvID)) {
             unset($_REQUEST['cvID'][$key]);
         } else {
-            $tabs[] = array('view-version-' . $cvID, t('Version %s', $cvID), $checked);
+            $tabs[] = array('ccm-tab-content-view-version-' . $cvID, t('Version %s', $cvID), $checked);
             $checked = false;
         }
     }
     echo $ih->tabs($tabs);
 
-    $display = 'block';
+    ?>
+    <div class="tab-content h-100">
+    <?php
+    $i = 0;
     $resolverManager = app(ResolverManagerInterface::class);
     foreach ($_REQUEST['cvID'] as $cvID) {
-        ?><div id="ccm-tab-content-view-version-<?=$cvID?>" style="display: <?=$display?>; height: 100%">
-            <iframe border="0" frameborder="0" height="100%" width="100%" src="<?= h($resolverManager->resolve(['/ccm/system/page/preview_version']) . "?cvID={$cvID}&cID={$cID}") ?>"></iframe>
-        </div>
+        ?>
+            <div class="tab-pane <?php if ($i == 0) { ?>active<?php } ?> h-100" id="ccm-tab-content-view-version-<?=$cvID?>">
+                <iframe border="0" frameborder="0" height="100%" width="100%" src="<?= h($resolverManager->resolve(['/ccm/system/page/preview_version']) . "?cvID={$cvID}&cID={$cID}") ?>"></iframe>
+            </div>
         <?php
-        $display = 'none';
+        $i++;
     }
-?></div><?php
+    ?>
+    </div>
+</div><?php
