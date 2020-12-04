5fcb5a1cdc062bd33e8fb1f62c4cbd5906f6806c
Andrew Embler <andrew@concrete5.org>
2020-12-04 07:39:05 -0800

    start on processes

--- concrete/single_pages/dashboard/system/automation/processes.php
+++ concrete/single_pages/dashboard/system/automation/processes.php
@@ -1,3 +1,33 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php
 
-processes
+defined('C5_EXECUTE') or die("Access Denied.");
+
+?>
+
+<div v-cloak id="processes">
+
+
+</div>
+
+<script type="text/javascript">
+    $(function() {
+        Concrete.Vue.activateContext('backend', function (Vue, config) {
+            new Vue({
+                el: '#proceses',
+                components: config.components,
+                data: {
+                    
+
+                },
+
+                computed: {
+                },
+
+                watch: {
+                },
+                methods: {
+                }
+            })
+        })
+    });
+</script>
