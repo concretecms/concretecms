13394530621a2d2590aa18ca69efa897395c037b
Andrew Embler <andrew@concrete5.org>
2021-02-02 14:45:28 -0800

    fix tests

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -8,7 +8,7 @@ return [
      */
     'version' => '9.0.0a3',
     'version_installed' => '9.0.0a3',
-    'version_db' => '20201231153200', // the key of the latest database migration
+    'version_db' => '20210128224608', // the key of the latest database migration
 
     /*
      * Installation status
