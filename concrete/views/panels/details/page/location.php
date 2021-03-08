b6162e0a19044bfe56717e1c6d6dd6961415d3f2
arielkamoyedji <58799870+arielkamoyedji@users.noreply.github.com>
2021-03-08 16:48:48 +0900

    Update location.php
    Initializing $sitemap variable to fix warning/error "Exception Occurred: /Applications/MAMP/htdocs/c855/concrete/views/panels/details/page/location.php:9 Undefined variable: sitemap (8)" for PHP 7.3 and 7.4.

--- concrete/views/panels/details/page/location.php
+++ concrete/views/panels/details/page/location.php
@@ -1,5 +1,7 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
+
+$sitemap = Loader::helper('concrete/dashboard/sitemap');
 ?>
 <section class="ccm-ui">
 	<header><?=t('Location')?></header>
