6d9bfe5c1f0ccbaffeb58825ef4f62eccc6aad4f
Andrew Embler <andrew@concrete5.org>
2021-03-20 07:31:23 -0700

    making remove old page versions task work again

--- concrete/src/Command/Task/Controller/RemoveOldPageVersionsController.php
+++ concrete/src/Command/Task/Controller/RemoveOldPageVersionsController.php
@@ -1,16 +1,30 @@
 <?php
 namespace Concrete\Core\Command\Task\Controller;
 
+use Concrete\Core\Command\Batch\Batch;
+use Concrete\Core\Command\Task\Input\Definition\Definition;
+use Concrete\Core\Command\Task\Input\Definition\Field;
 use Concrete\Core\Command\Task\Input\InputInterface;
-use Concrete\Core\Command\Task\Runner\ProcessTaskRunner;
+use Concrete\Core\Command\Task\Runner\BatchProcessTaskRunner;
 use Concrete\Core\Command\Task\Runner\TaskRunnerInterface;
 use Concrete\Core\Command\Task\TaskInterface;
-use Concrete\Core\Page\Sitemap\Command\GenerateSitemapCommand;
+use Concrete\Core\Database\Connection\Connection;
+use Concrete\Core\Page\Command\RemoveOldPageVersionsTaskCommand;
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
 class RemoveOldPageVersionsController extends AbstractController
 {
+
+    /**
+     * @var Connection
+     */
+    protected $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
 
     public function getName(): string
     {
@@ -22,14 +36,32 @@ class RemoveOldPageVersionsController extends AbstractController
         return t('Removes all except the 10 most recent page versions for each page.');
     }
 
+    public function getInputDefinition(): ?Definition
+    {
+        $definition = new Definition();
+        $definition->addField(new Field('after', t('After Page ID'), t('Scan pages for versions to remove after a particular page ID.')));
+        return $definition;
+    }
+
+
     public function getTaskRunner(TaskInterface $task, InputInterface $input): TaskRunnerInterface
     {
-        return new ProcessTaskRunner(
-            $task,
-            new GenerateSitemapCommand(),
-            $input,
-            t('Generation of sitemap.xml started.')
-        );
+        $query = $this->db->createQueryBuilder();
+        $query->select('cID')->from('Pages', 'p');
+        if ($input->hasField('after')) {
+            $after = $input->getField('after');
+            $query->andWhere('p.cID > :after');
+            $query->setParameter('after', $after->getValue());
+        }
+
+        $query->orderBy('p.cID', 'asc');
+
+        $batch = Batch::create();
+        foreach($query->execute()->fetchAll() as $result) {
+            $batch->add(new RemoveOldPageVersionsTaskCommand($result['cID']));
+        }
+
+        return new BatchProcessTaskRunner($task, $batch, $input, t('Page version removal beginning...'));
     }
 
 
