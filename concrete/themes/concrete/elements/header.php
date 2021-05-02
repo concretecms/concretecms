0b620943ef525da8ff694bc68a30b6f68a06b46a
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2021-05-02 22:55:07 +0900

    Add/fix lang attribute of internal themes

--- concrete/themes/concrete/elements/header.php
+++ concrete/themes/concrete/elements/header.php
@@ -1,6 +1,6 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 <!DOCTYPE html>
-<html>
+<html lang="<?php echo Localization::activeLanguage() ?>">
 <head>
     <link rel="stylesheet" type="text/css" href="<?=$this->getThemePath()?>/main.css" />
     <?php
