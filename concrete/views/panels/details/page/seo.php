7281b08c5c25fb4fbbbd7dcca110379fc8b3d1fe
Hamed Darragi <hd@xanweb.com>
2021-06-18 16:11:44 +0100

    fix seo page display

--- concrete/views/panels/details/page/seo.php
+++ concrete/views/panels/details/page/seo.php
@@ -9,7 +9,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
             <?php if ($allowEditName) {
             ?>
             <div class="form-group">
-                <label class="control-label form-label" for="cName"><?=t('Name')?></label>
+                <label class="form-label" for="cName"><?=t('Name')?></label>
                 <div>
                     <input type="text" class="form-control" name="cName" id="cName" value="<?php echo h($c->getCollectionName())?>">
                 </div>
@@ -20,7 +20,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
             <?php if ($allowEditPaths && !$c->isGeneratedCollection()) {
             ?>
             <div class="form-group">
-                <label class="control-label launch-tooltip" data-bs-placement="bottom" title="<?=t('This page must always be available from at least one URL. This is that URL.')?>" class="launch-tooltip"><?=t('URL Slug')?></label>
+                <label class="form-label launch-tooltip" data-bs-placement="bottom" title="<?=t('This page must always be available from at least one URL. This is that URL.')?>" class="launch-tooltip"><?=t('URL Slug')?></label>
                 <div>
                     <input type="text" class="form-control" name="cHandle" value="<?php echo $c->getCollectionHandle()?>" id="cHandle"><input type="hidden" name="oldCHandle" id="oldCHandle" value="<?php echo $c->getCollectionHandle()?>">
                 </div>
