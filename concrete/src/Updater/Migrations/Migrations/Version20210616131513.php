7661a2f41c45c2b6a9339a9ad5b41509c0f9627b
Derek Cameron <derek@concrete5.co.jp>
2021-06-24 18:53:19 +0900

    fix tests

--- concrete/src/Updater/Migrations/Migrations/Version20210616131513.php
+++ concrete/src/Updater/Migrations/Migrations/Version20210616131513.php
@@ -3,9 +3,10 @@
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Doctrine\DBAL\Schema\Schema;
 
-final class Version20210616131513 extends AbstractMigration
+final class Version20210616131513 extends AbstractMigration  implements RepeatableMigrationInterface
 {
 
     public function upgradeDatabase()
