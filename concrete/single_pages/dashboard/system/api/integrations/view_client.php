39d403e4d9d1d2dc64b3c9a22704d6ba09d18384
Bitter Digital Solutions UG (haftungsbeschränkt) <fabian@bitter.de>
2021-06-26 10:57:18 +0200

    Fix

--- concrete/single_pages/dashboard/system/api/integrations/view_client.php
+++ concrete/single_pages/dashboard/system/api/integrations/view_client.php
@@ -12,13 +12,13 @@ if ($clientSecret && !password_verify($clientSecret, $client->getClientSecret())
     $clientSecret = null;
 }
 
-// Get the consent type from the cclient
+// Get the consent type from the client
 $consentType = $client->getConsentType();
 ?>
 
 <div class="ccm-dashboard-header-buttons">
     <div class="btn-group">
-        <a href="<?=URL::to('/dashboard/system/api/settings'); ?>" class="btn btn-default"><?php echo t('Back to Settings'); ?></a>
+        <a href="<?=URL::to('/dashboard/system/api/settings'); ?>" class="btn btn-secondary"><?php echo t('Back to Settings'); ?></a>
         <button class="btn btn-danger" data-dialog="delete-client"><?=t("Delete")?></button>
         <a href="<?=URL::to('/dashboard/system/api/integrations', 'edit', $client->getIdentifier()); ?>" class="btn btn-primary"><?php echo t('Edit'); ?></a>
     </div>
@@ -28,22 +28,28 @@ $consentType = $client->getConsentType();
     <legend><?=t('Integration Details')?></legend>
 
     <div class="form-group">
-        <label class="control-label"><?=t('Name')?></label>
+        <label><?=t('Name')?></label>
         <div><?=$client->getName()?></div>
     </div>
 
     <div class="form-group">
-        <label class="control-label"><?=t('Redirect URI')?></label>
+        <label><?=t('Redirect URI')?></label>
         <div><?=$client->getRedirectUri() ?: t('None provided') ?></div>
+        <?php
+        $redirectUri = $client->getRedirectUri() ?: t('None provided');
+        ?>
+        <ul>
+            <li><?= implode('</li><li>', (array) $redirectUri) ?></li>
+        </ul>
     </div>
 
     <div class="form-group">
-        <label class="control-label"><?=t('Client ID')?></label>
+        <label><?=t('Client ID')?></label>
         <input type="text" class="form-control" onclick="this.select()" value="<?=$client->getClientKey()?>">
     </div>
 
     <div class="form-group <?= $clientSecret ? 'has-warning' : '' ?>">
-        <label class="control-label"><?=t('Client Secret')?></label>
+        <label><?=t('Client Secret')?></label>
         <input type="<?= $clientSecret ? 'text' : 'password' ?>" autocomplete="off" class="form-control" onclick="this.select()" value="<?= $clientSecret ?: str_repeat('*', 96) ?>" <?= $clientSecret ? '' : 'disabled' ?>>
         <div class="help-block">
             <?php
@@ -57,23 +63,22 @@ $consentType = $client->getConsentType();
     </div>
 
     <div class="form-group">
-        <label class="control-label"><?=t('User Consent Level')?></label>
-        <div class="checkbox">
-            <div class="form-check">
-                <label class="radio">
-                    <input disabled type="radio" name="consentLevel" value="<?= Client::CONSENT_SIMPLE ?>" <?= $consentType === Client::CONSENT_SIMPLE ? 'checked' : '' ?> />
-                    <?= t('Standard') ?>
-                </label>
-            </div>
-            <div class="form-check">
-                <label class="radio">
-                    <input disabled type="radio" name="consentLevel" value="<?= Client::CONSENT_NONE ?>" <?= $consentType === Client::CONSENT_NONE ? 'checked' : '' ?> />
-                    <?= t('None') ?>
-                </label>
-            </div>
+        <label><?=t('User Consent Level')?></label>
+        <div class="form-check">
+            <input disabled type="radio" class="form-check-input" name="consentLevel"  value="<?= Client::CONSENT_SIMPLE ?>" <?= $consentType === Client::CONSENT_SIMPLE ? 'checked' : '' ?> >
+            <label class="form-check-label">
+                <?= t('Standard') ?>
+            </label>
         </div>
 
-        <div class="consent-warning alert alert-danger <?= $consentType !== Client::CONSENT_NONE ? 'hidden' : '' ?>" >
+        <div class="form-check">
+            <input disabled type="radio" class="form-check-input"  name="consentLevel" value="<?= Client::CONSENT_NONE ?>" <?= $consentType === Client::CONSENT_NONE ? 'checked' : '' ?> >
+            <label class="form-check-label">
+                <?= t('None') ?>
+            </label>
+        </div>
+
+        <div class="consent-warning alert alert-danger mt-3 <?= $consentType !== Client::CONSENT_NONE ? 'd-none' : '' ?>" >
             <?= t("Only disable user consent if you trust this integration fully. By disabling user consent, you remove the user's ability to deny access.") ?>
         </div>
     </div>
@@ -85,10 +90,10 @@ $consentType = $client->getConsentType();
         <form method="post" action="<?php echo $view->action('delete'); ?>">
             <?php echo Loader::helper('validation/token')->output('delete'); ?>
             <input type="hidden" name="clientID" value="<?php echo $client->getIdentifier(); ?>">
-            <p><?=t('Are you sure you want to delete this credentials set? This cannot be undone.')?></p>
+            <p><?=t('Are you sure you want to delete this credentials set ? This cannot be undone.')?></p>
             <div class="dialog-buttons">
-                <button class="btn btn-default pull-left" onclick="jQuery.fn.dialog.closeTop()"><?=t('Cancel')?></button>
-                <button class="btn btn-danger pull-right" onclick="$('div[data-dialog-wrapper=delete-client] form').submit()"><?=t('Delete')?></button>
+                <button class="btn btn-secondary float-left" onclick="jQuery.fn.dialog.closeTop()"><?=t('Cancel')?></button>
+                <button class="btn btn-danger float-right" onclick="$('div[data-dialog-wrapper=delete-client] form').submit()"><?=t('Delete')?></button>
             </div>
         </form>
     </div>
