3dcb7a50753deafb5058eb20d4d1b0c2cf5f260e
Bitter Digital Solutions UG (haftungsbeschränkt) <fabian@bitter.de>
2021-06-29 14:41:35 +0200

    implement RepeatableMigrationInterface interface to fix auto test failure

--- concrete/src/Updater/Migrations/Migrations/Version20210622145600.php
+++ concrete/src/Updater/Migrations/Migrations/Version20210622145600.php
@@ -3,13 +3,13 @@
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-final class Version20210622145600 extends AbstractMigration
+final class Version20210622145600 extends AbstractMigration implements RepeatableMigrationInterface
 {
 
     public function upgradeDatabase()
     {
-
         $this->createSinglePage('/dashboard/system/registration/session_options', 'Session Options');
     }
 }
