780049caae1fe9dc27ca4de25ea49c0c50c58b7b
Andrew Embler <andrew@concrete5.org>
2021-07-13 15:51:25 -0700

    Updates

--- concrete/css/bedrock/@concretecms/bedrock/assets/cms/components/customizer/ColorPageCustomizerWidget.vue
+++ concrete/css/bedrock/@concretecms/bedrock/assets/cms/components/customizer/ColorPageCustomizerWidget.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="d-flex">
-    <label class="control-label align-self-center">{{ styleValue.style.name }}</label>
-    <div class="ml-auto d-flex">
-      <input :id="'color-picker-' + styleValue.style.variable" />
+    <div class="d-flex">
+        <label class="control-label align-self-center">{{ styleValue.style.name }}</label>
+        <div class="ml-auto d-flex">
+            <input :id="'color-picker-' + styleValue.style.variable"/>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 
 export default {
     data() {
-      return {
-        color: this.styleValue.value
-      }
+        return {
+            color: this.styleValue.value
+        }
     },
     mounted() {
-      var my = this
-      $("#color-picker-" + this.styleValue.style.variable).spectrum({
-        showAlpha: true,
-        preferredFormat: 'rgb',
-        allowEmpty: true,
-        color: this.color,
-        change: function(r) {
-          var color = r.toRgb()
-          my.$emit('update', {
-            variable: my.styleValue.style.variable,
-            value: {
-              r: color.r,
-              g: color.g,
-              b: color.b,
-              a: color.a
+        var my = this
+        $('#color-picker-' + this.styleValue.style.variable).spectrum({
+            showAlpha: true,
+            preferredFormat: 'rgb',
+            allowEmpty: true,
+            color: this.color,
+            change: function (r) {
+                var color = r.toRgb()
+                my.$emit('update', {
+                    variable: my.styleValue.style.variable,
+                    value: {
+                        r: color.r,
+                        g: color.g,
+                        b: color.b,
+                        a: color.a
+                    }
+                })
             }
-          })
-        }
-      });
+        })
     },
     props: {
         styleValue: {
--- concrete/css/bedrock/@concretecms/bedrock/assets/cms/components/customizer/FontFamilyPageCustomizerWidget.vue
+++ concrete/css/bedrock/@concretecms/bedrock/assets/cms/components/customizer/FontFamilyPageCustomizerWidget.vue
@@ -1,46 +1,47 @@
 <template>
-  <div>
-    <label class="control-label">{{ styleValue.style.name }}</label>
-    <select @change="componentUpdated" class="form-select form-control" v-model="selectedFont" :style="{'font-family': selectedFont}">
-      <option v-for="font in fonts" :value="font">{{ font }}</option>
-    </select>
-  </div>
+    <div>
+        <label class="control-label">{{ styleValue.style.name }}</label>
+        <select @change="componentUpdated" class="form-select form-control" v-model="selectedFont"
+                :style="{'font-family': selectedFont}">
+            <option v-for="font in fonts" :value="font">{{ font }}</option>
+        </select>
+    </div>
 </template>
 
 <script>
 
 export default {
-  components: {},
-  data() {
-    return {
-      selectedFont: this.styleValue.value.fontFamily
-    }
-  },
-  methods: {
-    componentUpdated: function() {
-      this.$emit('update', {
-        variable: this.styleValue.style.variable,
-        value: {
-          fontFamily: this.selectedFont
+    components: {},
+    data() {
+        return {
+            selectedFont: this.styleValue.value.fontFamily
         }
-      })
-    }
-  },
-  computed: {
-    fonts: function() {
-      var fonts = [
-        this.styleValue.value.fontFamily,
-        'Helvetica',
-        'Verdana',
-        'Georgia'
-      ];
-      return fonts;
     },
-  },
-  props: {
-    styleValue: {
-      type: Object
+    methods: {
+        componentUpdated: function () {
+            this.$emit('update', {
+                variable: this.styleValue.style.variable,
+                value: {
+                    fontFamily: this.selectedFont
+                }
+            })
+        }
+    },
+    computed: {
+        fonts: function () {
+            var fonts = [
+                this.styleValue.value.fontFamily,
+                'Helvetica',
+                'Verdana',
+                'Georgia'
+            ]
+            return fonts
+        }
+    },
+    props: {
+        styleValue: {
+            type: Object
+        }
     }
-  }
 }
 </script>
