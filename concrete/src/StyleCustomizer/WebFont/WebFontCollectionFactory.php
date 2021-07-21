a78b0cab1481d5bf1ad749e525f64941d6a9028e
Andrew Embler <andrew@concrete5.org>
2021-07-20 22:04:20 -0700

    making custom skins work with the new font approach

--- concrete/src/StyleCustomizer/WebFont/WebFontCollectionFactory.php
+++ concrete/src/StyleCustomizer/WebFont/WebFontCollectionFactory.php
@@ -2,6 +2,8 @@
 
 namespace Concrete\Core\StyleCustomizer\WebFont;
 
+use Concrete\Core\Entity\Page\Theme\CustomSkin;
+use Concrete\Core\StyleCustomizer\Skin\PresetSkin;
 use Concrete\Core\StyleCustomizer\Skin\SkinInterface;
 
 class WebFontCollectionFactory
@@ -13,13 +15,18 @@ class WebFontCollectionFactory
      */
     public function createFromSkin(SkinInterface $skin): WebFontCollection
     {
+        if ($skin instanceof CustomSkin) {
+            $skinIdentifier = $skin->getPresetSkinStartingPoint();
+        } else {
+            $skinIdentifier = $skin->getIdentifier();
+        }
         $collection = new WebFontCollection();
         $record = $skin->getTheme()->getStyleConfigurationFileRecord();
         if ($record->exists()) {
             $xml = simplexml_load_file($record->getFile());
             if ($xml->webfonts) {
                 foreach ($xml->webfonts->skin as $skinNode) {
-                    if ((string) $skinNode['identifier'] == $skin->getIdentifier()) {
+                    if ((string) $skinNode['identifier'] == $skinIdentifier) {
                         foreach ($skinNode->font as $fontNode) {
                             $font = new WebFont((string) $fontNode['name'], (string) $fontNode['type']);
                             $collection->add($font);
