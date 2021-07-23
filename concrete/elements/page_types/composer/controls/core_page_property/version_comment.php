019b42280dc8f6b706bed328843d2b6416d045af
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2021-07-23 18:35:28 +0900

    Add Version Comment Core Page Property as Composer Control

---
+++ concrete/elements/page_types/composer/controls/core_page_property/version_comment.php
@@ -0,0 +1,22 @@
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+/**
+ * @var Concrete\Core\Page\Type\Composer\Control\Control $control
+ * @var Concrete\Core\Form\Service\Form $form
+ * @var string $label
+ * @var string $description
+ */
+?>
+
+<div class="form-group" data-group="composer-description">
+    <?= $form->label('', $label) ?>
+    <?php if ($control->isPageTypeComposerControlRequiredByDefault() || $control->isPageTypeComposerFormControlRequiredOnThisRequest()) { ?>
+        <span class="label label-info"><?= t('Required') ?></span>
+    <?php } ?>
+
+	<?php if ($description) { ?>
+	    <i class="fas fa-question-circle launch-tooltip" title="" data-original-title="<?= $description ?>"></i>
+	<?php } ?>
+
+	<?= $form->textarea($this->field('version_comment'), $control->getPageTypeComposerControlDraftValue(), ['rows' => 2]) ?>
+</div>
