e5b13347edb067a101f48ae8c70f94780408889a
Bitter Digital Solutions UG (haftungsbeschränkt) <fabian@bitter.de>
2021-08-11 21:27:22 +0200

    fix tests #2

--- concrete/js/build/core/file-manager/menu.js
+++ concrete/js/build/core/file-manager/menu.js
@@ -1,5 +1,5 @@
 /* jshint unused:vars, undef:true, browser:true, jquery:true */
-/* global _, CCM_DISPATCHER_FILENAME, ConcreteMenu, ConcreteMenuManager */
+/* global _, CCM_DISPATCHER_FILENAME, CCM_SECURITY_TOKEN, ConcreteMenu, ConcreteMenuManager */
 
 ;(function(global, $) {
     'use strict';
