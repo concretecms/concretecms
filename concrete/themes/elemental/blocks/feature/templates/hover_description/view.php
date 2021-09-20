6b90c843d52bc19f2169331d774ddd9caf79cfbb
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2021-09-21 05:59:12 +0900

    Use new $iconTag

--- concrete/themes/elemental/blocks/feature/templates/hover_description/view.php
+++ concrete/themes/elemental/blocks/feature/templates/hover_description/view.php
@@ -1,4 +1,7 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+
+$iconTag = $iconTag ?? '';
+?>
 
 <?php if ($linkURL) {
     ?>
@@ -7,7 +10,7 @@
 } ?>
 <div class="ccm-block-feature-item-hover-wrapper" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?=h(strip_tags($paragraph))?>">
     <div class="ccm-block-feature-item-hover">
-        <div class="ccm-block-feature-item-hover-icon"><i class="fa fa-<?=$icon?>"></i></div>
+        <div class="ccm-block-feature-item-hover-icon"><?=$iconTag?></div>
     </div>
     <div class="ccm-block-feature-item-hover-title"><?=h($title)?></div>
 </div>
