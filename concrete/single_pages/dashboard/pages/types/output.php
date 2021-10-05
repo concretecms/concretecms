df441efefc68d340665e170c98e63b1044b19518
Andrew Embler <andrew@concrete5.org>
2021-10-05 06:58:22 -0700

    Fix #9866 by removing Update from Type from page type template list page; the functionality does not work

--- concrete/single_pages/dashboard/pages/types/output.php
+++ concrete/single_pages/dashboard/pages/types/output.php
@@ -5,13 +5,29 @@ $pageTypePageTemplateObjects = $pagetype->getPageTypePageTemplateObjects();
 
 <p class="lead"><?= $pagetype->getPageTypeDisplayName(); ?></p>
 
+<ul class="item-select-list">
+    <?php
+    foreach ($pageTypePageTemplateObjects as $pt) {
+        ?>
+        <li><a href="<?= $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID()); ?>" target="_blank">
+                <?= $pt->getPageTemplateIconImage(); ?>
+                <?= $pt->getPageTemplateDisplayName(); ?>
+            </a>
+        </li>
+        <?php
+    }
+    ?>
+</ul>
+<?php /*
+ * The table here is outmoded, but I'm keeping it around in case we ever try to fix the update_from_type functionality
+  * Update from type is currently very broken, it came from a pull request that was half-finished that I merged in foolishly.
 <table class="table table-striped">
 <?php
 foreach ($pageTypePageTemplateObjects as $pt) {
     ?>
     <tr>
-        <td style="width: 1px"><a href="<?= $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID()); ?>" target="_blank"><?= $pt->getPageTemplateIconImage(); ?></a></td>
-        <td style="vertical-align: middle"><p class="lead" style="margin-bottom: 0px"><?= $pt->getPageTemplateDisplayName(); ?></p></td>
+        <td style="width: 1px">></a></td>
+        <td style="vertical-align: middle"><p class="lead" style="margin-bottom: 0px"></p></td>
         <td style="width: 250px; vertical-align: middle">
             <div class="btn-group float-end">
                 <a href="<?= $view->action('edit_defaults', $pagetype->getPageTypeID(), $pt->getPageTemplateID()); ?>" class="btn btn-secondary btn-sm"><?= t('Edit'); ?></a>
@@ -29,6 +45,7 @@ foreach ($pageTypePageTemplateObjects as $pt) {
 }
 ?>
 </table>
+ */ ?>
 
 <div class="ccm-dashboard-header-buttons">
     <a href="<?= URL::to('/dashboard/pages/types'); ?>" class="btn btn-secondary"><?= t('Back to List'); ?></a>
