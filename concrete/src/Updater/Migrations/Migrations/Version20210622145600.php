5e827fffd406e70aa5623e52cecc15d5f57f4417
Andrew Embler <andrew@concrete5.org>
2021-10-12 13:14:10 -0700

    Refresh site entity

--- concrete/src/Updater/Migrations/Migrations/Version20210622145600.php
+++ concrete/src/Updater/Migrations/Migrations/Version20210622145600.php
@@ -3,6 +3,7 @@
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Application\UserInterface\Dashboard\Navigation\NavigationCache;
+use Concrete\Core\Entity\Site\Site;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
@@ -11,7 +12,10 @@ final class Version20210622145600 extends AbstractMigration implements Repeatabl
 
     public function upgradeDatabase()
     {
-
+        // This migration was failing without this being present.
+        $this->refreshEntities([
+            Site::class
+        ]);
         $this->createSinglePage('/dashboard/system/registration/session_options', 'Session Options');
 
         /** @var NavigationCache $navigationCache */
