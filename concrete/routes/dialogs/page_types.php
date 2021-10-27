cbd643590d05ec7491ea24b038184559c8192ea6
Andrew Embler <andrew@concrete5.org>
2021-10-27 06:53:19 -0700

    remove public vestiges of half-completed update from type defaults functionality

--- concrete/routes/dialogs/page_types.php
+++ concrete/routes/dialogs/page_types.php
@@ -12,9 +12,12 @@ defined('C5_EXECUTE') or die('Access Denied.');
  * Namespace: Concrete\Controller\Dialog\Type
  */
 
+/*
+ * These routes are broken, but I'm keeping them around in case we decide to fix this functionality in the future. It
+ * came from a pull request that had good intentions but was never bug free.
 $router->all('/update_from_type/{ptID}/{pTemplateID}', 'UpdateFromType::view');
 $router->all('/update_from_type/{ptID}/{pTemplateID}/submit', 'UpdateFromType::submit');
-
+*/
 $router->all('/attributes/{ptID}', 'Attributes::view');
 $router->all('/attributes/{ptID}/get_attribute', 'Attributes::getAttribute');
 $router->all('/attributes/{ptID}/submit', 'Attributes::submit');
