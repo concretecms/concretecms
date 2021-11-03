637842bec2ee2cd8940ba8c0c22b8802ae62c66b
Andrew Embler <andrew@concrete5.org>
2021-11-03 14:55:58 -0700

    Fix #10032

--- concrete/src/StyleCustomizer/Style/StyleValueList.php
+++ concrete/src/StyleCustomizer/Style/StyleValueList.php
@@ -47,8 +47,16 @@ class StyleValueList
             foreach ($set->getStyles() as $style) {
                 // Find the style in our value list that matches the one in the original nested set
                 foreach($this->getValues() as $styleValue) {
-                    if ($styleValue->getStyle()->getVariable() === $style->getVariable()) {
-                        $groupedSet->addValue($styleValue);
+                    if (($styleValue->getStyle()->getVariable() === $style->getVariable()) &&
+                        (get_class($styleValue->getStyle()) === get_class($style))) {
+                            // This get_class() check shouldn't be necessary, but some legacy styles
+                            // actually have multiple instances of the SAME variable within the style, because
+                            // if the type is different you can technically do that. In the legacy customizer
+                            // different types will let you use different computed variables. e.g.
+                            // if my style variable is "body-text" I could have a variable of the type color and use
+                            // "body-text-color" and a variable of the type "type" and use "body-text-type" both
+                            // in the same styles.xml.
+                            $groupedSet->addValue($styleValue);
                     }
                 }
             }
