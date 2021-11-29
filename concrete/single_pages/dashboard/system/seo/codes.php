286cdca371af1238ab855bf898e79240ece6a62a
Ryan Hewitt <ryan@mesuva.com.au>
2021-11-29 11:10:49 +0000

    Use base64 encoding and decoding on submitted tracking codes, to avoid Mod Security related blocks

--- concrete/single_pages/dashboard/system/seo/codes.php
+++ concrete/single_pages/dashboard/system/seo/codes.php
@@ -19,17 +19,42 @@ defined('C5_EXECUTE') or die('Access Denied.');
 
     <div class="form-group">
         <?= $form->label('tracking_code_header', t('Header Tracking Codes')) ?>
-        <?= $form->textarea('tracking_code_header', $tracking_code_header, ['style' => 'height: 250px;', 'class' => 'form-control font-monospace', 'spellcheck' => 'false']) ?>
+        <?= $form->textarea('tracking_code_header_input', $tracking_code_header, ['style' => 'height: 250px;', 'class' => 'form-control font-monospace', 'spellcheck' => 'false']) ?>
+        <?= $form->hidden('tracking_code_header', '') ?>
     </div>
 
     <div class="form-group">
         <?= $form->label('tracking_code_footer', t('Footer Tracking Codes')) ?>
-        <?= $form->textarea('tracking_code_footer', $tracking_code_footer, ['style' => 'height: 250px;', 'class' => 'form-control font-monospace', 'spellcheck' => 'false']) ?>
+        <?= $form->textarea('tracking_code_footer_input', $tracking_code_footer, ['style' => 'height: 250px;', 'class' => 'form-control font-monospace', 'spellcheck' => 'false']) ?>
+        <?= $form->hidden('tracking_code_footer', '') ?>
     </div>
 
     <div class="ccm-dashboard-form-actions-wrapper">
         <div class="ccm-dashboard-form-actions">
-            <button type="submit" class="btn btn-primary float-end"><?= t('Save') ?></button>
+            <button type="submit" id="tracking-code-form-button" class="btn btn-primary float-end"><?= t('Save') ?></button>
         </div>
     </div>
 </form>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function(event) {
+        var trackingCodeForm = document.getElementById("tracking-code-form-button");
+        var trackingCodeHeaderInput = document.getElementById("tracking_code_header_input");
+        var trackingCodeFooterInput = document.getElementById("tracking_code_footer_input");
+        var trackingCodeHeader = document.getElementById("tracking_code_header");
+        var trackingCodeFooter = document.getElementById("tracking_code_footer");
+
+        trackingCodeForm.addEventListener("click", function(e) {
+            trackingCodeHeader.value = b64EncodeUnicode(trackingCodeHeaderInput.value);
+            trackingCodeHeaderInput.setAttribute("disabled", "disabled");
+            trackingCodeFooter.value = b64EncodeUnicode(trackingCodeFooterInput.value);
+            trackingCodeFooterInput.setAttribute("disabled", "disabled");
+        });
+    });
+
+    function b64EncodeUnicode(str) {
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+            return String.fromCharCode('0x' + p1);
+        }));
+    }
+</script>
