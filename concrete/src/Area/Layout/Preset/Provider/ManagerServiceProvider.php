d8f349af444f95d689a981317985352ded706cc0
Michele Locati <michele@locati.it>
2021-12-17 09:49:47 +0100

    Make the layout preset provider a singleton

--- concrete/src/Area/Layout/Preset/Provider/ManagerServiceProvider.php
+++ concrete/src/Area/Layout/Preset/Provider/ManagerServiceProvider.php
@@ -1,18 +1,23 @@
 <?php
+
 namespace Concrete\Core\Area\Layout\Preset\Provider;
 
 use Concrete\Core\Foundation\Service\Provider as ServiceProvider;
+use Concrete\Core\Application\Application;
 
 class ManagerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton('manager/area_layout_preset_provider', function($app) {
-            $manager = new Manager($app);
-            $manager->register(new UserProvider());
-            $manager->register(new ActiveThemeProvider());
-
-            return $manager;
-        });
+        $this->app->singleton(
+            Manager::class,
+            static function(Application $app): Manager {
+                $manager = new Manager($app);
+                $manager->register(new UserProvider());
+                $manager->register(new ActiveThemeProvider());
+                return $manager;
+            }
+        );
+        $this->app->alias(Manager::class, 'manager/area_layout_preset_provider');
     }
 }
