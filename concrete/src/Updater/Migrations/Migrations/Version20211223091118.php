edad537601a24eab693d743ae89f1e37fe0e8e90
Glyn Jenkins <glyn@katalysis.net>
2021-12-23 09:13:27 +0000

    Update Migration

--- concrete/src/Updater/Migrations/Migrations/Version20211223091118.php
+++ concrete/src/Updater/Migrations/Migrations/Version20211223091118.php
@@ -6,7 +6,7 @@ use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Doctrine\DBAL\Schema\Schema;
 
-final class Version20212312085830 extends AbstractMigration  implements RepeatableMigrationInterface
+final class Version20211223091118 extends AbstractMigration  implements RepeatableMigrationInterface
 {
 
     public function upgradeDatabase()
