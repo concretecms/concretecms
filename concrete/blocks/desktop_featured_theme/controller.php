48c612b62e984602e2c58848c9c81463e5e286fc
Derek Cameron <derek@macareux.co.jp>
2022-02-10 17:30:00 +0900

    update php-doc on desktop_featured_addon,desktop_featured_theme blocks phpcs-fixer on  desktop_featured_addon,desktop_featured_theme blocks

--- concrete/blocks/desktop_featured_theme/controller.php
+++ concrete/blocks/desktop_featured_theme/controller.php
@@ -1,40 +1,71 @@
 <?php
+
 namespace Concrete\Block\DesktopFeaturedTheme;
 
 use Concrete\Core\Block\BlockController;
 use Concrete\Core\Marketplace\RemoteItemList as MarketplaceRemoteItemList;
 
-/**
+    /**
      * The controller for the block that displays featured themes in the dashboard news overlay.
      *
      * @package Blocks
      * @subpackage Dashboard Featured Theme
      *
-     * @author Andrew Embler <andrew@concrete5.org>
-     * @copyright  Copyright (c) 2003-2012 Concrete5. (http://www.concrete5.org)
-     * @license    http://www.concrete5.org/license/     MIT License
+     * @author Andrew Embler <andrew@concretecms.org>
+     * @copyright  Copyright (c) 2003-2022 concreteCMS. (http://www.concretecms.org)
+     * @license    http://www.concretecms.org/license/     MIT License
      */
     class Controller extends BlockController
     {
-        protected $btCacheBlockRecord = true;
+        /**
+         * @var bool
+         */
         protected $btCacheBlockOutput = true;
+
+        /**
+         * @var bool
+         */
         protected $btCacheBlockOutputOnPost = true;
+
+        /**
+         * @var bool
+         */
         protected $btCacheBlockOutputForRegisteredUsers = true;
+
+        /**
+         * @var int
+         */
         protected $btCacheBlockOutputLifetime = 7200;
 
+        /**
+         * @var int
+         */
         protected $btInterfaceWidth = 300;
+
+        /**
+         * @var int
+         */
         protected $btInterfaceHeight = 100;
 
+        /**
+         * @return string
+         */
         public function getBlockTypeDescription()
         {
-            return t("Features a theme from marketplace.concretecms.com.");
+            return t('Features a theme from marketplace.concretecms.com.');
         }
 
+        /**
+         * @return string
+         */
         public function getBlockTypeName()
         {
-            return t("Dashboard Featured Theme");
+            return t('Dashboard Featured Theme');
         }
 
+        /**
+         * @return void
+         */
         public function view()
         {
             $mri = new MarketplaceRemoteItemList();
