5120acd1a961f4071df6b4751eefcd08f836afda
Derek Cameron <derek@macareux.co.jp>
2022-02-10 17:12:02 +0900

    update php-doc on desktop_concrete_latest block phpcs-fixer on desktop_concrete_latest

--- concrete/blocks/desktop_concrete_latest/controller.php
+++ concrete/blocks/desktop_concrete_latest/controller.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Concrete\Block\DesktopConcreteLatest;
 
 use Concrete\Core\Block\BlockController;
@@ -6,29 +7,62 @@ use Concrete\Core\ConcreteCms\ActivityService;
 
 class Controller extends BlockController
 {
-    protected $btCacheBlockRecord = true;
+    /**
+     * @var bool
+     */
     protected $btCacheBlockOutput = true;
+
+    /**
+     * @var bool
+     */
     protected $btCacheBlockOutputOnPost = true;
+
+    /**
+     * @var int
+     */
     protected $btCacheBlockOutputLifetime = 7200;
+
+    /**
+     * @var string
+     */
     protected $btTable = 'btDesktopConcreteLatest';
+
+    /**
+     * @var bool
+     */
     protected $btCacheBlockOutputForRegisteredUsers = true;
+
+    /**
+     * @var string|null
+     */
     protected $slot;
 
+    /**
+     * @return string
+     */
     public function getBlockTypeDescription()
     {
-        return t("Grabs the latest information about Concrete from concretecms.com.");
+        return t('Grabs the latest information about Concrete from concretecms.com.');
     }
 
+    /**
+     * @return string
+     */
     public function getBlockTypeName()
     {
-        return t("Desktop Latest News");
+        return t('Desktop Latest News');
     }
 
+    /**
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     *
+     * @return void
+     */
     public function view()
     {
         $service = $this->app->make(ActivityService::class);
         $slots = $service->getSlotContents();
-        $this->set('slot', isset($slots[$this->slot]) ? $slots[$this->slot] : null);
+        $this->set('slot', $slots[$this->slot] ?? null);
         $this->set('key', $this->slot);
     }
 }
