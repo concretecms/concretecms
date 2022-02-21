bafe99e475a5ffc5db9039ae66bb490a89a2d174
Andrew Embler <andrew@concrete5.org>
2022-02-20 16:27:15 -0800

    Trying again

--- .gp/bash/patch-preview.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Hotfix for https://github.com/apolopena/gitpod-laravel-starter/issues/19
-msg="\e[38;5;208mGitpod was not able to open the preview.\e[0m
-You may open the preview by running the command:\e[38;5;183m op\e[0m
-You may also pass in an additional path to open such as:\e[38;5;183m op phpmyadmin\e[0m"
-echo -e "$msg"
--- .gp/bash/spinner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Shortcut so that other files don't have to use such a long path when loading the spinner
-. .gp/bash/third-party/bash-spinner/spinner.sh
