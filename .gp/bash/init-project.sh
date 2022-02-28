3fe795e4f7959398a80f4fca0d5b378420e63bae
Andrew Embler <andrew@concrete5.org>
2022-02-28 06:46:11 -0800

    updating init project

--- .gp/bash/init-project.sh
+++ .gp/bash/init-project.sh
@@ -7,18 +7,15 @@
 # Load logger
 . .gp/bash/workspace-init-logger.sh
 
-# BEGIN example code block - migrate database
-# . .gp/bash/spinner.sh # COMMENT: Load spinner
-# __migrate_msg="Migrating database"
-# log_silent "$__migrate_msg" && start_spinner "$__migrate_msg"
-# php artisan migrate
-# err_code=$?
-# if [ $err_code != 0 ]; then
-#  stop_spinner $err_code
-#  log -e "ERROR: Failed to migrate database"
-# else
-#  stop_spinner $err_code
-#  log "SUCCESS: migrated database"
-# fi
-# END example code block - migrate database
+# Install Composer Dependencies
+composer install
 
+# Create Concrete database
+mysql -u root -e "drop database if exists concretecms; create database concretecms"
+
+echo "Installing Concrete CMS with canonical URL $GITPOD_WORKSPACE_URL"
+
+# Install Concrete
+concrete/bin/concrete c5:install --db-server=localhost --db-username=root \
+  --db-database=c5 --admin-email=demo@concretecms.com --admin-password=password \
+  --canonical-url=$GITPOD_WORKSPACE_URL
