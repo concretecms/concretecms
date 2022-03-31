6eb640836729218c9840836fb676216c7b521150
Andrew Embler <andrew@concrete5.org>
2022-03-31 15:59:54 -0700

    lol good grief

--- concrete/src/Updater/Migrations/Migrations/Version20220321122800.php
+++ concrete/src/Updater/Migrations/Migrations/Version20220321122800.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Entity\Attribute\Key\ExpressKey;
+use Concrete\Core\Entity\Express\Entity;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 use Doctrine\DBAL\Schema\Schema;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
@@ -18,8 +19,11 @@ final class Version20220321122800 extends AbstractMigration implements Repeatabl
         $this->refreshEntities(
             [
                 ExpressKey::class,
+                Entity::class,
             ]
         );
+        // Default the boolean to true
+        $this->connection->executeStatement('update ExpressEntities set is_published = 1');
     }
 
 }
