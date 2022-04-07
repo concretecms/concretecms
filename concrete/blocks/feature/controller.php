bca704022bb54f7f3710c12124c3916eb361db8d
Andrew Embler <andrew@concrete5.org>
2022-04-07 09:16:34 -0700

    Fix google maps for php8

--- concrete/blocks/feature/controller.php
+++ concrete/blocks/feature/controller.php
@@ -25,7 +25,7 @@ class Controller extends BlockController implements UsesFeatureInterface
     protected $icon;
 
     public $paragraph;
-    
+
     public function getBlockTypeDescription()
     {
         return t("Displays an icon, a title, and a short paragraph description.");
