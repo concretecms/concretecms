9901acd761441f8d598fe44682003a6fee74b7ac
Md Biplob Hossain <biplob.ice@gmail.com>
2022-05-24 12:58:34 +0900

    Avoid null pointer exceptions on core area layout block

--- concrete/blocks/core_area_layout/view.php
+++ concrete/blocks/core_area_layout/view.php
@@ -1,7 +1,8 @@
 <?php
-    defined('C5_EXECUTE') or die('Access Denied.');
-    $a = $b->getBlockAreaObject();
+defined('C5_EXECUTE') or die('Access Denied.');
+$a = $b->getBlockAreaObject();
 
+if (isset($formatter) && is_object($formatter)) {
     $rootContainer = $formatter->getLayoutContainerHtmlObject();
     if (!empty($rootContainer)) {
         $container = $rootContainer;
@@ -9,14 +10,17 @@
             $container = $container->getChildren()[0];
         }
     }
+}
 
-    foreach ($columns as $column) {
-        $html = $column->getColumnHtmlObject();
-        if (!empty($container)) {
-            $container->appendChild($html);
-        } else {
-            echo $html;
-        }
+foreach ($columns as $column) {
+    $html = $column->getColumnHtmlObject();
+    if (!empty($container)) {
+        $container->appendChild($html);
+    } else {
+        echo $html;
     }
+}
 
+if (isset($rootContainer)) {
     echo $rootContainer;
+}
