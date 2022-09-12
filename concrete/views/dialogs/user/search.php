017323c5e613909926e9023528016ed7a5f87c93
Michele Locati <michele@locati.it>
2022-09-12 17:44:23 +0200

    Add an option to the user picker to hide usernames
    Websites that use the email address as login may want to completely ignore usernames

--- concrete/views/dialogs/user/search.php
+++ concrete/views/dialogs/user/search.php
@@ -2,11 +2,15 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 /**
  * @var bool $multipleSelection
+ * @var bool $hideUsername
  */
 ?>
 
 <div data-choose="user" class="h-100">
-    <concrete-user-chooser :multiple-selection="<?= json_encode($multipleSelection); ?>"></concrete-user-chooser>
+    <concrete-user-chooser
+        :multiple-selection="<?= json_encode($multipleSelection) ?>"
+        :hide-username="<?= json_encode($hideUsername) ?>"
+    ></concrete-user-chooser>
 </div>
 <script type="text/javascript">
 
