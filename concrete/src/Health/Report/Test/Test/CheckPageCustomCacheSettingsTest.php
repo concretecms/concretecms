898aa3651c9c53818cecb4302c9e4416f3b5f08a
Michele Locati <michele@locati.it>
2022-09-14 09:29:18 +0200

    Don't concatenate translatable strings in cache report

--- concrete/src/Health/Report/Test/Test/CheckPageCustomCacheSettingsTest.php
+++ concrete/src/Health/Report/Test/Test/CheckPageCustomCacheSettingsTest.php
@@ -11,15 +11,26 @@ class CheckPageCustomCacheSettingsTest extends AbstractPageTest
     {
         $page = Page::getByID($this->getPageId());
         if ($page->getCollectionFullPageCaching() !== -1) {
-            $enabled = $page->getCollectionFullPageCaching() == 1 ? t('enabled') : 'disabled';
+            $message = t(/* i18n: %1$s is a path, %2$s is a number */'Custom settings detected for page: %1$s (ID: %2$s).', $page->getCollectionPath(), $page->getCollectionID());
+            $cachingDescription = $page->getCollectionFullPageCaching() == 1 ? t('Caching enabled') : t('Caching disabled');
             $lifetime = $page->getCollectionFullPageCachingLifetime();
-            if ($lifetime === '0' || $lifetime === 0) {
-                $lifetime = t('default');
-            }
             $lifetimeValue = $page->getCollectionFullPageCachingLifetimeValue();
-            $report->info(t('Custom settings detected for page: %s (ID: %s). Caching %s - lifetime %s (value: %s)',
-                $page->getCollectionPath(), $page->getCollectionID(), $enabled, $lifetime, $lifetimeValue
-            ));
+            switch ($lifetime) {
+                case 'default':
+                    $lifetimeDescription = t('Default lifetime (value: %s)', $lifetimeValue);
+                    break;
+                case 'forever':
+                    $lifetimeDescription = t('Unlimited lifetime (value: %s)', $lifetimeValue);
+                    break;
+                case 'custom':
+                    $lifetimeDescription = t('Custom lifetime (value: %s)', $lifetimeValue);
+                    break;
+                default: // '0', 0, other...
+                    $lifetimeDescription = t('Default lifetime (value: %s)', $lifetimeValue);
+                    break;
+            }
+            $message .= " {$cachingDescription} - {$lifetimeDescription}";
+            $report->info($message);
         }
     }
 
