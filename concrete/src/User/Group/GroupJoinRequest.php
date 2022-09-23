9fe490398c3b353df1fdf922bb263c092fb9edcc
Michele Locati <michele@locati.it>
2022-09-23 15:32:15 +0200

    Add new GroupSignupRequestAcceptNotification instances to the GroupSignupRequestAccept notifications

--- concrete/src/User/Group/GroupJoinRequest.php
+++ concrete/src/User/Group/GroupJoinRequest.php
@@ -68,6 +68,7 @@ class GroupJoinRequest extends ConcreteObject implements SubjectInterface
                 $subscription = $type->getSubscription($subject);
                 $users = $notifier->getUsersToNotify($subscription, $subject);
                 $notification = new GroupSignupRequestAcceptNotification($subject);
+                $subject->getNotifications()->add($notification);
                 $notifier->notify($users, $notification);
             }
 
