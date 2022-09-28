5d0c4a652ee3a4e6ad09fda8f1c2a9cf3d1828c8
Michele Locati <michele@locati.it>
2022-09-28 14:17:24 +0200

    Group nodes: avoid deprecated methods, add phpdoc

--- concrete/src/Tree/Node/Type/Group.php
+++ concrete/src/Tree/Node/Type/Group.php
@@ -1,102 +1,176 @@
 <?php
+
 namespace Concrete\Core\Tree\Node\Type;
 
+use Concrete\Core\Database\Connection\Connection;
+use Concrete\Core\Error\UserMessageException;
 use Concrete\Core\Tree\Node\Node as TreeNode;
-use Concrete\Core\Tree\Node\Type\Formatter\CategoryListFormatter;
 use Concrete\Core\Tree\Node\Type\Formatter\GroupFormatter;
 use Concrete\Core\Tree\Node\Type\Formatter\LegacyGroupFormatter;
 use Concrete\Core\Tree\Node\Type\Menu\GroupMenu;
-use Loader;
 use Concrete\Core\User\Group\Group as UserGroup;
-use Exception;
+use Concrete\Core\User\Group\GroupRepository;
 
 class Group extends TreeNode
 {
-    protected $gID = null;
+    /**
+     * @var int|null
+     */
+    protected $gID;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Permission\ObjectInterface::getPermissionResponseClassName()
+     */
     public function getPermissionResponseClassName()
     {
         return '\\Concrete\\Core\\Permission\\Response\\GroupTreeNodeResponse';
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Permission\ObjectInterface::getPermissionAssignmentClassName()
+     */
     public function getPermissionAssignmentClassName()
     {
         return '\\Concrete\\Core\\Permission\\Assignment\\GroupTreeNodeAssignment';
     }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Permission\ObjectInterface::getPermissionObjectKeyCategoryHandle()
+     */
     public function getPermissionObjectKeyCategoryHandle()
     {
         return 'group_tree_node';
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::getTreeNodeTypeName()
+     */
     public function getTreeNodeTypeName()
     {
         return 'Group';
     }
 
+    /**
+     * @return int|null
+     */
     public function getTreeNodeGroupID()
     {
         return $this->gID;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::getTreeNodeMenu()
+     */
     public function getTreeNodeMenu()
     {
         return new GroupMenu($this);
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::getListFormatter()
+     */
     public function getListFormatter()
     {
         if (count($this->getTreeNodeGroupObject()->getChildGroups()) > 0) {
             return new LegacyGroupFormatter();
-        } else {
-            return new GroupFormatter();
         }
+
+        return new GroupFormatter();
     }
 
+    /**
+     * @return \Concrete\Core\User\Group\Group|null
+     */
     public function getTreeNodeGroupObject()
     {
-        return UserGroup::getByID($this->gID);
+        if (!$this->gID) {
+            return null;
+        }
+        $repository = app(GroupRepository::class);
+
+        return $repository->getGroupByID($this->gID);
     }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::getTreeNodeName()
+     */
     public function getTreeNodeName()
     {
-        $g = UserGroup::getByID($this->gID);
-        if (is_object($g)) {
-            return $g->getGroupName();
-        }
+        $group = $this->getTreeNodeGroupObject();
+
+        return $group === null ? null : $group->getGroupName();
     }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::getTreeNodeDisplayName()
+     */
     public function getTreeNodeDisplayName($format = 'html')
     {
         if ($this->treeNodeParentID == 0) {
             return t('All Groups');
         }
 
-        $g = UserGroup::getByID($this->gID);
-        if (is_object($g)) {
-            $gName = $g->getGroupDisplayName(false, false);
-            switch ($format) {
-                case 'html':
-                    return h($gName);
-                case 'text':
-                default:
-                    return $gName;
-            }
+        $group = $this->getTreeNodeGroupObject();
+        if ($group === null) {
+            return null;
+        }
+        $gName = $group->getGroupDisplayName(false, false);
+        switch ($format) {
+            case 'html':
+                return h($gName);
+            case 'text':
+            default:
+                return $gName;
         }
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::loadDetails()
+     */
     public function loadDetails()
     {
-        $db = Loader::db();
-        $row = $db->GetRow('select * from TreeGroupNodes where treeNodeID = ?', array($this->treeNodeID));
-        $this->setPropertiesFromArray($row);
+        $db = app(Connection::class);
+        $row = $db->fetchAssociative(
+            'select * from TreeGroupNodes where treeNodeID = ?',
+            [$this->treeNodeID]
+        );
+        if ($row !== false) {
+            $this->setPropertiesFromArray($row);
+        }
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::move()
+     */
     public function move(TreeNode $newParent)
     {
         switch ($this->gID) {
             case GUEST_GROUP_ID:
-                throw new Exception(t("The guest users group can't be moved."));
+                throw new UserMessageException(t("The guest users group can't be moved."));
             case REGISTERED_GROUP_ID:
-                throw new Exception(t("The registered users group can't be moved."));
+                throw new UserMessageException(t("The registered users group can't be moved."));
             case ADMIN_GROUP_ID:
-                throw new Exception(t("The administrators group can't be moved."));
+                throw new UserMessageException(t("The administrators group can't be moved."));
         }
         parent::move($newParent);
         $g = $this->getTreeNodeGroupObject();
@@ -105,23 +179,36 @@ class Group extends TreeNode
         }
     }
 
+    /**
+     * @param int $gID
+     *
+     * @return \Concrete\Core\Tree\Node\Type\Group|null
+     */
     public static function getTreeNodeByGroupID($gID)
     {
-        $db = Loader::db();
-        $treeNodeID = $db->GetOne('select treeNodeID from TreeGroupNodes where gID = ?', array($gID));
+        $db = app(Connection::class);
+        $treeNodeID = $db->fetchOne('select treeNodeID from TreeGroupNodes where gID = ?', [$gID]);
         if ($treeNodeID) {
-            $tn = TreeNode::getByID($treeNodeID);
-
-            return $tn;
+            return TreeNode::getByID($treeNodeID);
         }
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::deleteDetails()
+     */
     public function deleteDetails()
     {
-        $db = Loader::db();
-        $db->Execute('delete from TreeGroupNodes where treeNodeID = ?', array($this->treeNodeID));
+        $db = app(Connection::class);
+        $db->delete('TreeGroupNodes', ['treeNodeID' => $this->treeNodeID]);
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Tree\Node\Node::getTreeNodeJSON()
+     */
     public function getTreeNodeJSON()
     {
         $obj = parent::getTreeNodeJSON();
@@ -132,7 +219,7 @@ class Group extends TreeNode
             } else {
                 $obj->icon = 'fas fa-users';
             }
-            if (isset($this->gID)) {
+            if ($this->gID) {
                 $obj->title = $this->getTreeNodeDisplayName('text');
             }
 
@@ -142,14 +229,19 @@ class Group extends TreeNode
 
     public function setTreeNodeGroup(UserGroup $g)
     {
-        $db = Loader::db();
-        $db->Replace('TreeGroupNodes', array('treeNodeID' => $this->getTreeNodeID(), 'gID' => $g->getGroupID()), array('treeNodeID'), true);
+        $db = app(Connection::class);
+        $db->replace('TreeGroupNodes', ['treeNodeID' => $this->getTreeNodeID(), 'gID' => $g->getGroupID()], ['treeNodeID'], true);
         $this->gID = $g->getGroupID();
     }
 
+    /**
+     * @param \Concrete\Core\User\Group\Group|false|null $group
+     * @param \Concrete\Core\Tree\Node\Node|false|null $parent
+     *
+     * @return \Concrete\Core\Tree\Node\Type\Group
+     */
     public static function add($group = false, $parent = false)
     {
-        $db = Loader::db();
         $node = parent::add($parent);
         if (is_object($group)) {
             $node->setTreeNodeGroup($group);
