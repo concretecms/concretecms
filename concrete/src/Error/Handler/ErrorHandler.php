7ef42d570736064fabb8afd921a4906848204023
Andrew Embler <andrew@concrete5.org>
2022-10-06 07:39:11 -0700

    Merge pull request #10975 from KorvinSzanto/feature/masked-env-server-and-cookie
    Improve whoops error output

--- concrete/src/Error/Handler/ErrorHandler.php
+++ concrete/src/Error/Handler/ErrorHandler.php
@@ -94,22 +94,47 @@ class ErrorHandler extends PrettyPageHandler
             [
                 'Version' => Config::get('concrete.version'),
                 'Installed Version' => Config::get('concrete.version_installed'),
+                'Database Version' => Config::get('concrete.version_db'),
             ]
         );
 
+        $extensions = collect(array_flip(get_loaded_extensions()))->map(function($value, string $ext) {
+            return phpversion($ext) ?: 'Unknown version';
+        });
         $this->addDataTable(
             'PHP',
             [
                 'Version' => PHP_VERSION,
+                'Extensions' => $extensions->all(),
             ]
         );
-        
-        /*
-         * Config
-         */
-        $this->addDataTable('Concrete Configuration', $this->flatConfig(Config::get('concrete'), 'concrete'));
+
+        $this->addDataTable('Concrete Configuration', [
+            'concrete' => $this->cleanedConfig(Config::get('concrete'), 'concrete'),
+            'app' => $this->cleanedConfig(Config::get('app'), 'app')
+        ]);
     }
 
+    protected function cleanedConfig(array $config, $group): array
+    {
+        $clean = [];
+        foreach ($config as $key => $value) {
+            $assembled = "{$group}.{$key}";
+            if (is_array($value)) {
+                $clean[$key] = $this->cleanedConfig($value, $assembled);
+            } elseif ($this->isKeyHidden($assembled)) {
+                $clean[$key] = str_repeat('*', is_string($value) ? strlen($value) : 3);
+            } else {
+                $clean[$key] = $value;
+            }
+        }
+
+        return $clean;
+    }
+
+    /**
+     * @deprecated Will be removed
+     */
     protected function flatConfig(array $config, $group)
     {
         $flat = [];
