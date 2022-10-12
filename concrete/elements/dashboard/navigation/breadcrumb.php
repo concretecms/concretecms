abadf3f751ab76edbf1bb7940aa241a57b73d992
Korvin Szanto <korvin@portlandlabs.com>
2022-10-12 12:59:44 -0700

    Clean up dashboard breadcrumbs element

--- concrete/elements/dashboard/navigation/breadcrumb.php
+++ concrete/elements/dashboard/navigation/breadcrumb.php
@@ -6,37 +6,46 @@
 $items = $breadcrumb->getItems();
 $total = count($items);
 if ($total > 1) {
-?>
-<ol class="breadcrumb">
-    <?php
-    for ($i = 0; $i < $total; $i++) {
-        $item = $items[$i];
-        $isActive = ($total - $i) == 1 ? true : false;
-        ?>
-        <li class="breadcrumb-item <?=$isActive ? 'active' : ''?>">
-            <?php if (count($item->getChildren())) { ?>
-                <span class="dropdown">
-                <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
-                    <?= $item->getName() ?>
-                    <span class="caret"></span>
-                </a>
-                    <ul class="dropdown-menu" role="menu">
-                        <?php
-                        foreach ($item->getChildren() as $child) {
-                            ?>
-                            <li><a class="dropdown-item" href="<?= h($child->getUrl()); ?>"><?= $child->getName(); ?></a></li><?php
-                        } ?>
-                    </ul>
-                </span>
-            <?php } else { ?>
-                <?php if ($isActive) { ?>
-                    <?=$item->getName()?>
-                <?php } else { ?>
-                    <a href="<?=$item->getURL()?>"><?=$item->getName()?></a>
-                <?php } ?>
-            <?php } ?>
-        </li>
-    <?php }
     ?>
-</ol>
-<?php } ?>
+    <ol class="breadcrumb">
+        <?php
+        for ($i = 0; $i < $total; $i++) {
+            $item = $items[$i];
+            $isActive = ($total - $i) == 1;
+            ?>
+            <li class="breadcrumb-item <?= $isActive ? 'active' : '' ?>">
+                <?php
+                if (count($item->getChildren())) {
+                    ?>
+                    <span class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
+                        <?= h($item->getName()) ?>
+                        <span class="caret"></span>
+                    </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <?php
+                            foreach ($item->getChildren() as $child) {
+                                ?>
+                                <li><a class="dropdown-item" href="<?= h($child->getUrl()); ?>"><?= h($child->getName()); ?></a></li>
+                                <?php
+                            }
+                            ?>
+                        </ul>
+                    </span>
+                <?php
+                } elseif ($isActive) {
+                    echo h($item->getName());
+                } else {
+                    ?>
+                    <a href="<?= h($item->getURL()) ?>"><?= h($item->getName()) ?></a>
+                    <?php
+                }
+                ?>
+            </li>
+            <?php
+        }
+        ?>
+    </ol>
+    <?php
+}
+?>
