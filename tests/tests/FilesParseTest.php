111129772cd96cfe71357502693efc3c8b5c402e
Korvin Szanto <korvin@portlandlabs.com>
2022-11-03 10:07:32 -0700

    Test for scalar type hints

--- tests/tests/FilesParseTest.php
+++ tests/tests/FilesParseTest.php
@@ -2,7 +2,12 @@
 
 namespace Concrete\Tests;
 
+use Doctrine\Common\Annotations\PhpParser;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\NameResolver;
+use PhpParser\Parser;
 use PhpParser\ParserFactory;
+use PhpParser\Node\Stmt;
 
 class FilesParseTest extends \PHPUnit_Framework_TestCase
 {
@@ -22,7 +27,53 @@ class FilesParseTest extends \PHPUnit_Framework_TestCase
     public function testSupportsPhp5($name, $file)
     {
         try {
-            $this->parser->parse(file_get_contents($file));
+            /** @var \PhpParser\Node[] $tree */
+            $tree = $this->parser->parse(file_get_contents($file));
+
+            // Traverse the tree and look for scalar type hints
+            $iterate = [$tree];
+            while (is_array($nodes = array_shift($iterate))) {
+                /** @var \PhpParser\Node $node */
+                foreach ($nodes as $node) {
+                    $type = $node->getType();
+                    if (
+                        $node instanceof Stmt\Namespace_
+                    ) {
+                        $iterate[] = $node->stmts;
+                        continue;
+                    }
+
+                    if ($node instanceof Stmt\Class_) {
+                        $iterate[] = $node->getMethods();
+                        continue;
+                    }
+
+                    if (
+                        $node instanceof Stmt\Function_ ||
+                        $node instanceof Stmt\ClassMethod
+                    ) {
+                        $iterate[] = $node->getParams();
+                        continue;
+                    }
+
+                    // Check for scalar type hints
+                    if ($node instanceof \PhpParser\Node\Param) {
+                        // If the parameter has no type
+                        if (!$node->type) {
+                            continue;
+                        }
+
+                        $paramType = is_string($node->type) ? $node->type : $node->type->toString();
+                        switch ($paramType) {
+                            case 'string':
+                            case 'int':
+                            case 'bool':
+                            case 'float':
+                                return $this->fail('Scalar type hints detected: ' . $name . ':' . $node->getLine());
+                        }
+                    }
+                }
+            }
         } catch (\Exception $e) {
             throw new \RuntimeException('Failed to parse file: .' . $name, 0, $e);
         }
