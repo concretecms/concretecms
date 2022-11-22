ba923d2c49d9835bd99d3d223a8058560b50568d
Andrew Embler <andrew@concrete5.org>
2022-11-22 15:05:18 -0800

    Resolve #11026

--- concrete/elements/icons.php
+++ concrete/elements/icons.php
@@ -1,2 +1,2 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
-<?= file_get_contents(DIR_BASE_CORE . '/images/icons/bedrock/sprites.svg') ?>
+<div style="display: none"><?= file_get_contents(DIR_BASE_CORE . '/images/icons/bedrock/sprites.svg') ?></div>
