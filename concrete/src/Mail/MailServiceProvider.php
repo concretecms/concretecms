5e9a96fcd13f59094b953655cf7bd9abfdc1d505
Michele Locati <michele@locati.it>
2022-12-20 17:43:00 +0100

    Add notes in the System Email Addresses dashboard page

--- concrete/src/Mail/MailServiceProvider.php
+++ concrete/src/Mail/MailServiceProvider.php
@@ -32,6 +32,7 @@ class MailServiceProvider extends ServiceProvider
 
     private function configureSenders(SenderConfiguration $configuration): SenderConfiguration
     {
+        $fallbackToDefaultNotes = t("If not specified, we'll use the email address specified in the %s section.", '<code>' . tc('EmailAddress', 'Default') . '</code>');
         return $configuration->addEntries([
             (new SenderConfiguration\Entry(tc('EmailAddress', 'Default'), 'concrete.email.default.address'))
                 ->setNameKey('concrete.email.default.name')
@@ -40,21 +41,27 @@ class MailServiceProvider extends ServiceProvider
             ,
             (new SenderConfiguration\Entry(t('Forgot Password'), 'concrete.email.forgot_password.address'))
                 ->setNameKey('concrete.email.forgot_password.name')
+                ->setNotes($fallbackToDefaultNotes)
             ,
             (new SenderConfiguration\Entry(t('Form Block'), 'concrete.email.form_block.address'))
                 ->setNameKey('')
+                ->setNotes($fallbackToDefaultNotes)
             ,
             (new SenderConfiguration\Entry(t('Spam Notification'), 'concrete.spam.notify_email'))
                 ->setNameKey('')
+                ->setNotes(t("If not specified, spam notifications won't be sent."))
             ,
             (new SenderConfiguration\Entry(t('Website Registration Notification'), 'concrete.email.register_notification.address'))
                 ->setNameKey('concrete.email.register_notification.name')
+                ->setNotes($fallbackToDefaultNotes)
             ,
             (new SenderConfiguration\Entry(t('Validate Registration'), 'concrete.email.validate_registration.address'))
                 ->setNameKey('concrete.email.validate_registration.name')
+                ->setNotes($fallbackToDefaultNotes)
             ,
             (new SenderConfiguration\Entry(t('Workflow Notification'), 'concrete.email.workflow_notification.address'))
                 ->setNameKey('concrete.email.workflow_notification.name')
+                ->setNotes($fallbackToDefaultNotes)
             ,
         ]);
     }
