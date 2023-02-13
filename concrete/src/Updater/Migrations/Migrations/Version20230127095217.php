8590fa17c84ef79e8aeda836031fe56b41a496af
Jan Lucki <janlucki@planet-krypton.local>
2023-02-13 11:38:22 +0000

    remove unused namespace

--- concrete/src/Updater/Migrations/Migrations/Version20230127095217.php
+++ concrete/src/Updater/Migrations/Migrations/Version20230127095217.php
@@ -6,7 +6,6 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Entity\Attribute\Key\Key;
 use Concrete\Core\Entity\Attribute\Value\Value\AddressValue;
-use Concrete\Core\Entity\Page\PagePath;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
