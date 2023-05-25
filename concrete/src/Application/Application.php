995b01d153a31614fa30b546dacfa9e30d320be3
Michele Locati <michele@locati.it>
2023-05-25 16:48:10 +0200

    Let C5_ENVIRONMENT_ONLY work in web context too

--- concrete/src/Application/Application.php
+++ concrete/src/Application/Application.php
@@ -302,7 +302,7 @@ class Application extends Container
      */
     public static function isRunThroughCommandLineInterface()
     {
-        return defined('C5_ENVIRONMENT_ONLY') && C5_ENVIRONMENT_ONLY || PHP_SAPI == 'cli' || PHP_SAPI === 'phpdbg';
+        return PHP_SAPI == 'cli' || PHP_SAPI === 'phpdbg';
     }
 
     /**
