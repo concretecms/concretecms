a42b240e2dcfe5176340c61919245cfc6cf22fca
Michele Locati <michele@locati.it>
2023-08-25 09:49:23 +0200

    Fix Tree\Group::ensureGroupNodes

--- concrete/src/Tree/Type/Group.php
+++ concrete/src/Tree/Type/Group.php
@@ -71,9 +71,9 @@ class Group extends Tree
     public static function ensureGroupNodes()
     {
         $db = Database::connection();
-        $tree = GroupTree::get();
+        $tree = static::get();
         $rootNode = $tree->getRootTreeNodeObject();
-        $rows = $db->GetCol('select Groups.gID from ' . $db->getDatabasePlatform()->quoteSingleIdentifier('Groups') . ' left join TreeGroupNodes on Groups.gID = TreeGroupNodes.gID where TreeGroupNodes.gID is null');
+        $rows = $db->fetchFirstColumn('select Groups.gID from ' . $db->getDatabasePlatform()->quoteSingleIdentifier('Groups') . ' left join TreeGroupNodes on Groups.gID = TreeGroupNodes.gID where TreeGroupNodes.gID is null');
         foreach ($rows as $gID) {
             $g = static::getByID($gID);
             GroupTreeNode::add($g, $rootNode);
