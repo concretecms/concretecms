c88ddd1f8f1373d6c15c2d6e015c3a783d77d5e5
Andrew Embler <andrew@concrete5.org>
2023-11-09 15:35:00 -0800

    Sanitize layout preset name on output

--- concrete/views/dialogs/area/layout/presets/manage.php
+++ concrete/views/dialogs/area/layout/presets/manage.php
@@ -18,7 +18,7 @@ use Concrete\Core\Area\Layout\Preset\UserPreset;
             <?php foreach ($presets as $preset) { ?>
                 <li data-preset-row="<?php echo $preset->getAreaLayoutPresetID() ?>">
                     <span>
-                        <?php echo $preset->getAreaLayoutPresetName() ?>
+                        <?php echo h($preset->getAreaLayoutPresetName()) ?>
 
                         <a href="javascript:void(0)" class="float-end icon-link delete-area-layout-preset"
                            data-action="delete-area-layout-preset">
