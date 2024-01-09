0294598dbd4f83bcda61105da5c863df7f807f99
shahroq <shahroq@yahoo.com>
2024-01-09 15:16:21 +0000

    Using Null Coalescing Operator

--- concrete/blocks/express_entry_list/controller.php
+++ concrete/blocks/express_entry_list/controller.php
@@ -486,18 +486,12 @@ class Controller extends BlockController implements UsesFeatureInterface
         }
         $data['linkedProperties'] = json_encode($linkedProperties);
 
-        if (empty($data['enableKeywordSearch'])) {
-            $data['enableKeywordSearch'] = 0;
-        }
+        $data['enableKeywordSearch'] = $data['enableKeywordSearch'] ?? 0;
 
-        if (empty($data['enablePagination'])) {
-            $data['enablePagination'] = 0;
-        }
+        $data['enablePagination'] = $data['enablePagination'] ?? 0;
 
-        if (empty($data['enableItemsPerPageSelection'])) {
-            $data['enableItemsPerPageSelection'] = 0;
-        }
-
+        $data['enableItemsPerPageSelection'] = $data['enableItemsPerPageSelection'] ?? 0;
+        
         $data['displayLimit'] = (int) $data['displayLimit'];
 
         $entity = $this->entityManager->find('Concrete\Core\Entity\Express\Entity', $data['exEntityID']);
