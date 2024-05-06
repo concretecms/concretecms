9763e4941876c3791c3e4f0c2628a3f8c281aeee
Andrew Embler <andrew@concrete5.org>
2024-05-06 11:58:30 -0700

    reworking styles on desktop to look nicer

--- concrete/blocks/desktop_featured_addon/view.php
+++ concrete/blocks/desktop_featured_addon/view.php
@@ -1,16 +1,15 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <div class="ccm-block-desktop-featured-addon">
-	<div class="ccm-block-desktop-featured-addon-inner" style="padding: 1rem">
+	<div class="ccm-block-desktop-featured-addon-inner">
 
-        <h6><?=t('Featured Add-On')?></h6/>
+	<h6><?=t('Featured Add-On')?></h6/>
 
-
-        <div style="margin-bottom: 1rem; text-align: left"><b><?=t('No Extensions Available')?></b></div>
-        <?php
-            View::element('dashboard/marketplace_upgrade');
-        ?>
-
+	<img src="<?=ASSETS_URL_IMAGES?>/marketplace_upgrade_addon.png" width="80" height="80" />
+	<h3><?=t('No Extensions Available')?></h3>
+	<p><?=t('You are using an older version of Concrete that no longer supports direct marketplace integration. You should explore upgrading to version 9 of Concrete CMS.')?></p>
+	<a target="_blank" href="https://www.concretecms.com/about/new-marketplace" class="btn btn-default btn-lg"><?=t('Learn More')?></a>
 
 	</div>
 </div>
+
--- concrete/blocks/desktop_featured_theme/view.php
+++ concrete/blocks/desktop_featured_theme/view.php
@@ -1,13 +1,18 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <div class="ccm-block-desktop-featured-theme">
-    <div class="ccm-block-desktop-featured-theme-inner" style="padding: 1rem">
+    <div class="ccm-block-desktop-featured-theme-inner" style="display: flex">
 
-    <h6><?=t('Featured Theme')?></h6/>
-        <div style="margin-bottom: 1rem; text-align: left"><b><?=t('No Extensions Available')?></b></div>
-    <?php
-            View::element('dashboard/marketplace_upgrade');
-    ?>
+        <img src="<?=ASSETS_URL_IMAGES?>/marketplace_upgrade_theme.png" style="height: 250px; border-right: 0"/>
+
+        <div class="ccm-block-desktop-featured-theme-description">
+            <h6><?=t('Featured Theme')?></h6/>
+
+            <h3><?=t('No Extensions Available')?></h3>
+            <p><?=t('You are using an older version of Concrete that no longer supports direct marketplace integration. You should explore upgrading to version 9 of Concrete CMS.')?></p>
+            <a target="_blank" href="https://www.concretecms.com/about/new-marketplace" class="btn btn-info"><?=t('Learn More')?></a>
+        </div>
 
     </div>
 </div>
+
