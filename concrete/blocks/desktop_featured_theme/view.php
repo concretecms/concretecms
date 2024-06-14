f024c561114cffe0410ed19307b26e1a86c1db18
Andrew Embler <andrew@concrete5.org>
2024-06-14 11:35:42 -0700

    Fix #12094

--- concrete/blocks/desktop_featured_theme/view.php
+++ concrete/blocks/desktop_featured_theme/view.php
@@ -1,5 +1,9 @@
-<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+<?php defined('C5_EXECUTE') or die('Access Denied.');
 
+/**
+ * @var $connection ?\Concrete\Core\Marketplace\ConnectionInterface
+ */
+?>
 <div class="ccm-block-desktop-featured-theme">
     <div class="ccm-block-desktop-featured-theme-inner" style="display: flex">
 
@@ -8,11 +12,19 @@
         <div class="ccm-block-desktop-featured-theme-description">
             <h6><?=t('Featured Theme')?></h6/>
 
-            <h3><?=t('Connect to the new marketplace!')?></h3>
-            <p><?=t("This version of Concrete can't connect to the latest marketplace. You should update to version 9.3 to enjoy one-click marketplace integration.")?></p>
-            <a target="_blank" href="https://www.concretecms.com/about/new-marketplace" class="btn btn-info"><?=t('Learn More')?></a>
+            <?php if ($connection) { ?>
+                <h3><?=t('Browse Themes')?></h3>
+                <p><?=t("Get access to hundreds of Concrete CMS themes for your site from the Concrete marketplace.")?></p>
+                <a target="_blank" href="https://market.concretecms.com/themes/" class="btn btn-info"><?=t('Browse Themes')?></a>
+
+            <?php } else { ?>
+
+                <h3><?=t('Connect to the Marketplace')?></h3>
+                <p><?=t("Connect your site to the Concrete CMS marketplace to browse themes.")?></p>
+                <a target="_blank" href="<?=URL::to('/dashboard/system/basics/marketplace')?>" class="btn btn-info"><?=t('Connect Site')?></a>
+
+            <?php } ?>
         </div>
 
     </div>
 </div>
-
