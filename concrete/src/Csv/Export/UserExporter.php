0b5e951da24987d0d9854be202d50519e72716cd
aleksandr <sasha@macareus.co.jp>
2024-10-02 13:28:01 +0900

    Feature concretecms#12242: use format from config

--- concrete/src/Csv/Export/UserExporter.php
+++ concrete/src/Csv/Export/UserExporter.php
@@ -3,6 +3,7 @@ namespace Concrete\Core\Csv\Export;
 
 use Concrete\Core\Attribute\Category\UserCategory;
 use Concrete\Core\Attribute\ObjectInterface;
+use Concrete\Core\Config\Repository\Repository;
 use Concrete\Core\Localization\Service\Date;
 use League\Csv\Writer;
 
@@ -16,16 +17,28 @@ class UserExporter extends AbstractExporter
     protected $appTimezone;
 
     /**
+     * @var Date
+     */
+    protected $dateService;
+
+    /**
+     * @var string
+     */
+    protected $format;
+
+    /**
      * Initialize the instance.
      *
      * @param Writer $writer
      * @param UserCategory $userCategory
      * @param Date $dateService
      */
-    public function __construct(Writer $writer, UserCategory $userCategory, Date $dateService)
+    public function __construct(Writer $writer, UserCategory $userCategory, Date $dateService, Repository $config)
     {
         parent::__construct($writer, $userCategory);
         $this->appTimezone = $dateService->getTimezone('app');
+        $this->dateService = $dateService;
+        $this->format = $this->getFormat($config->get('concrete.export.csv.datetime_format', 'ATOM'));
     }
 
     /**
@@ -58,12 +71,22 @@ class UserExporter extends AbstractExporter
         $dateTime = $userInfo->getUserDateAdded();
         if ($dateTime) {
             $dateTime = clone $dateTime;
-            $dateTime->setTimezone($this->appTimezone);
-            yield $dateTime->format('Y-m-d H:i:s');
+            yield $this->dateService->formatCustom($this->format, $dateTime, 'app');
         } else {
             yield '';
         }
         yield $userInfo->isActive() ? '1' : '0';
         yield (string) (int) $userInfo->getNumLogins();
+    }
+
+    protected function getFormat(string $formatName = 'ATOM')
+    {
+        $datetime_format_constant = sprintf('DATE_%s', $formatName);
+
+        if (defined($datetime_format_constant)) {
+            return constant($datetime_format_constant);
+        }
+
+        return DATE_ATOM;
     }
 }
