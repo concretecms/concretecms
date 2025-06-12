3fb0830e741c6e1073f3b095c457f0fb66a02777
Diskibilus <diskusbilus@yahoo.com>
2025-06-12 12:25:10 +0200

    Update top navigation menu view.php - Scroll listed items
    On mobile in landscape rotation if there are more listed items they get cliped. This enables scroll action in that situation.

--- concrete/blocks/top_navigation_bar/view.php
+++ concrete/blocks/top_navigation_bar/view.php
@@ -69,28 +69,41 @@ $c = Page::getCurrentPage();
                             </ul>
                         </div>
                     <?php } ?>
-                    <?php if ($includeNavigation) { ?>
-                    <ul class="navbar-nav">
-                        <?php foreach ($navigation->getItems() as $item) {
-                            /**
-                             * @var $item \Concrete\Core\Navigation\Item\PageItem
-                             */
-                            if (count($item->getChildren()) > 0) { ?>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link<?= $item->isActiveParent() ? " nav-path-selected" : ""; ?> dropdown-toggle<?= $item->isActive() ? " active" : ""; ?>" data-concrete-toggle="dropdown" target="<?=$controller->getPageItemNavTarget($item)?>" href="<?= $item->getUrl() ?>">
-                                        <?=$item->getName()?>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <?php foreach ($item->getChildren() as $dropdownChild) { ?>
-                                            <li><a class="dropdown-item<?= $dropdownChild->isActive() ? " active" : ""; ?><?= $dropdownChild->isActiveParent() ? " nav-path-selected" : ""; ?>" target="<?=$controller->getPageItemNavTarget($dropdownChild)?>" href="<?=$dropdownChild->getUrl()?>"><?=$dropdownChild->getName()?></a></li>
-                                        <?php } ?>
-                                    </ul>
-                                </li>
-                            <?php } else { ?>
-                                <li class="nav-item"><a class="nav-link<?= $item->isActiveParent() ? " nav-path-selected" : ""; ?><?= $item->isActive() ? " active" : ""; ?>" target="<?=$controller->getPageItemNavTarget($item)?>" href="<?=$item->getUrl()?>"><?=$item->getName()?></a></li>
+                    <div class="navbar-scroll">
+                    /* If list is to long its scrollable. Because on mobile on landscape rotation the listed items get cliped and cant be reached.
+                        
+                        CSS Style needed
+                        @media (max-height: 700px), (max-width: 991px) {
+                            .navbar-scroll {
+                                max-height: calc(100vh - 180px);
+                                overflow-y: auto;
+                            }
+                        }
+                        */
+                        
+                        <?php if ($includeNavigation) { ?>
+                        <ul class="navbar-nav">
+                            <?php foreach ($navigation->getItems() as $item) {
+                                /**
+                                 * @var $item \Concrete\Core\Navigation\Item\PageItem
+                                 */
+                                if (count($item->getChildren()) > 0) { ?>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link<?= $item->isActiveParent() ? " nav-path-selected" : ""; ?> dropdown-toggle<?= $item->isActive() ? " active" : ""; ?>" data-concrete-toggle="dropdown" target="<?=$controller->getPageItemNavTarget($item)?>" href="<?= $item->getUrl() ?>">
+                                            <?=$item->getName()?>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <?php foreach ($item->getChildren() as $dropdownChild) { ?>
+                                                <li><a class="dropdown-item<?= $dropdownChild->isActive() ? " active" : ""; ?><?= $dropdownChild->isActiveParent() ? " nav-path-selected" : ""; ?>" target="<?=$controller->getPageItemNavTarget($dropdownChild)?>" href="<?=$dropdownChild->getUrl()?>"><?=$dropdownChild->getName()?></a></li>
+                                            <?php } ?>
+                                        </ul>
+                                    </li>
+                                <?php } else { ?>
+                                    <li class="nav-item"><a class="nav-link<?= $item->isActiveParent() ? " nav-path-selected" : ""; ?><?= $item->isActive() ? " active" : ""; ?>" target="<?=$controller->getPageItemNavTarget($item)?>" href="<?=$item->getUrl()?>"><?=$item->getName()?></a></li>
+                                <?php } ?>
                             <?php } ?>
-                        <?php } ?>
-                    </ul>
+                        </ul>
+                    </div>
                     <?php } ?>
                 </div>
             <?php } ?>
