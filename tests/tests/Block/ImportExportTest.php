b17be36869c74410845ef2a231dcc0ab2c33ba2f
Michele Locati <michele@locati.it>
2025-06-27 15:27:22 +0200

    Test desktop_draft_list block type

--- tests/tests/Block/ImportExportTest.php
+++ tests/tests/Block/ImportExportTest.php
@@ -255,7 +255,6 @@ class ImportExportTest extends PageTestCase
         );
         $expectedUncoveredHandles = [
             'core_board_slot', // Does it make sense to test it?
-            'desktop_draft_list',
             'desktop_featured_addon',
             'desktop_featured_theme',
             'desktop_latest_form',
