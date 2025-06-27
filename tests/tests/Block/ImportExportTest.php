055082a48fad4b0120cb862cf35e382fb6c48814
Michele Locati <michele@locati.it>
2025-06-27 18:53:12 +0200

    Test document_library block type

--- tests/tests/Block/ImportExportTest.php
+++ tests/tests/Block/ImportExportTest.php
@@ -257,7 +257,6 @@ class ImportExportTest extends PageTestCase
         );
         $expectedUncoveredHandles = [
             'core_board_slot', // Does it make sense to test it?
-            'document_library',
             'event_list',
             'express_entry_detail',
             'express_entry_list',
