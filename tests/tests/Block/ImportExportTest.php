4ac22237e49c656ea09d91d726e68cc2e4612321
Michele Locati <michele@locati.it>
2025-06-30 15:13:56 +0200

    Skip tests for express_entry_list

--- tests/tests/Block/ImportExportTest.php
+++ tests/tests/Block/ImportExportTest.php
@@ -326,7 +326,6 @@ class ImportExportTest extends PageTestCase
         );
         $expectedUncoveredHandles = [
             'core_board_slot', // Does it make sense to test it?
-            'express_entry_list',
             'express_form',
             'external_form',
             'faq',
