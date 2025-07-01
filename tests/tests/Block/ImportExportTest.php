50ca29acd1a391b10854ba53e295c20cbaa9c6bb
Michele Locati <michele@locati.it>
2025-07-01 18:00:40 +0200

    Test next_previous block type

--- tests/tests/Block/ImportExportTest.php
+++ tests/tests/Block/ImportExportTest.php
@@ -391,7 +391,6 @@ class ImportExportTest extends PageTestCase
         $expectedUncoveredHandles = [
             'core_board_slot', // does it make sense to test it?
             'form', // old stuff that's not worth working on
-            'next_previous',
             'page_attribute_display',
             'page_list',
             'page_title',
