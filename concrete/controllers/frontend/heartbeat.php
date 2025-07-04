c6eaf1f256788c106cf1b6c9e7ef4982b0d7928d
Michele Locati <michele@locati.it>
2025-07-04 09:53:07 +0200

    Keep current page in edit mode while user is active in the page

--- concrete/controllers/frontend/heartbeat.php
+++ concrete/controllers/frontend/heartbeat.php
@@ -3,7 +3,9 @@
 namespace Concrete\Controller\Frontend;
 
 use Concrete\Core\Controller\Controller;
+use Concrete\Core\Database\Connection\Connection;
 use Concrete\Core\Http\ResponseFactoryInterface;
+use Concrete\Core\Page\Page;
 use Concrete\Core\Session\SessionValidator;
 use Concrete\Core\User\User;
 
@@ -17,9 +19,30 @@ class Heartbeat extends Controller
             $user = $this->app->make(User::class);
             if ($user->isRegistered()) {
                 $user->updateOnlineCheck();
+                $this->refreshPageEditMode($user);
             }
         }
 
         return $this->app->make(ResponseFactoryInterface::class)->json(true);
+    }
+
+    private function refreshPageEditMode(User $loggedInUser): void
+    {
+        if (($cID = $this->request->query->getInt('cID')) <= 0) {
+            return;
+        }
+        $page = Page::getByID($cID);
+        if (!$page || $page->isError() || !$page->isCheckedOutByMe()) {
+            return;
+        }
+        $cn = $this->app->make(Connection::class);
+        $cn->executeStatement(
+            'UPDATE Pages SET cCheckedOutDatetimeLastEdit = ? WHERE cID = ? AND cCheckedOutUID = ? LIMIT 1',
+            [
+                $this->app->make('helper/date')->getOverridableNow(),
+                $cID,
+                (int) $loggedInUser->getUserID(),
+            ]
+        );
     }
 }
