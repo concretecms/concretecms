e84f71a06ac37e43fcc717328b7b03afd8d0ef36
Andrew Embler <andrew@concrete5.org>
2025-08-19 09:34:21 -0700

    filename weird filename relative path parsing under certain web server conditions

--- concrete/src/Application/Application.php
+++ concrete/src/Application/Application.php
@@ -344,13 +344,30 @@ class Application extends Container
     public function detectEnvironment($environments)
     {
         $r = Request::getInstance();
-        $pos = stripos($r->server->get('SCRIPT_NAME'), DISPATCHER_FILENAME);
-        if ($pos > 0) {
-            //we do this because in CLI circumstances (and some random ones) we would end up with index.ph instead of index.php
-            $pos = $pos - 1;
+        $relativePath = '';
+        $scriptName = $r->server->get('SCRIPT_NAME');
+        // First normalize the directory separator
+        $scriptName = str_replace(DIRECTORY_SEPARATOR, '/', $scriptName);
+        foreach (explode('/', $scriptName) as $part) {
+            if (empty($part)) {
+                continue; // Continue on weird parts or empty segments
+            }
+            // Loop through all path segments, discarding any that look like filenames.
+            // If a filename is encountered, the path is considered done.
+            if (strpos($part, '.') > -1) {
+                break;
+            } else {
+                $relativePath .= '/' . $part;
+            }
         }
-        $home = substr($r->server->get('SCRIPT_NAME'), 0, $pos);
-        $this['app_relative_path'] = rtrim(str_replace(DIRECTORY_SEPARATOR, '/', $home), '/');
+        // Strip off any trailing or ending slashes
+        $relativePath = trim($relativePath, '/');
+        // Finally, make sure that if there is a relative path, it does, in fact, start with
+        // a slash
+        if ($relativePath !== '') {
+            $relativePath = '/' . $relativePath;
+        }
+        $this['app_relative_path'] = $relativePath;
 
         $args = $this->isRunThroughCommandLineInterface() && isset($_SERVER['argv']) ? $_SERVER['argv'] : null;
 
