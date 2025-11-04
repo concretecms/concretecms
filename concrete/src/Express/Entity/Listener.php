692f567cda114f5619bdccb582403f273d2e1923
Andrew Embler <andrew@concrete5.org>
2025-11-04 09:50:00 -0800

    adding logging to Express

--- concrete/src/Express/Entity/Listener.php
+++ concrete/src/Express/Entity/Listener.php
@@ -3,6 +3,8 @@ namespace Concrete\Core\Express\Entity;
 
 use Concrete\Core\Entity\Attribute\Key\Key;
 use Concrete\Core\Entity\Express\Entity;
+use Concrete\Core\Logging\Channels;
+use Concrete\Core\Logging\LoggerFactory;
 use Concrete\Core\Tree\Node\Node;
 use Concrete\Core\Tree\Type\ExpressEntryResults as ExpressEntryResultsTree;
 use Concrete\Core\Tree\Node\Type\ExpressEntryResults as ExpressEntryResultsNode;
@@ -11,6 +13,16 @@ use Concrete\Core\Entity\Express\Entry;
 
 class Listener
 {
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct()
+    {
+        $this->logger = app(LoggerFactory::class)->createLogger(Channels::CHANNEL_EXPRESS);
+    }
 
     public function preRemove(Entity $entity, LifecycleEventArgs $event)
     {
@@ -24,6 +36,8 @@ class Listener
         $entity->setDefaultEditForm(null);
         $entity->setDefaultViewForm(null);
         $em->persist($entity);
+
+        $this->logger->info(t('Deleting Express entity: %s (%s)', $entity->getName(), $entity->getID()));
 
         foreach ($entity->getForms() as $form) {
             $em->remove($form);
@@ -78,6 +92,12 @@ class Listener
             $indexer->createRepository($entity->getAttributeKeyCategory());
         }
 
+        $this->logger->info(t('Creating new Express entity: %s', $entity->getName()));
+    }
+
+    public function postUpdate(Entity $entity, LifecycleEventArgs $event)
+    {
+        $this->logger->info(t('Express entity %s (%s) saved successfully.', $entity->getName(), $entity->getID()));
     }
 
     public function postRemove(Entity $entity, LifecycleEventArgs $event)
@@ -86,7 +106,12 @@ class Listener
         if (is_object($node)) {
             $node->delete();
         }
+        $this->logger->info(t('Express entity %s deleted successfully.', $entity->getName()));
     }
 
+    public function postPersist(Entity $entity, LifecycleEventArgs $event)
+    {
+        $this->logger->info(t('Express entity %s (%s) created successfully.', $entity->getName(), $entity->getID()));
+    }
 
 }
