6e94f46d222c1a4ede1d5d2a49abb855adcc68ad
Andrew Embler <andrew@concrete5.org>
2026-01-06 08:32:25 -0800

    Adding namespace for use with themes

--- concrete/src/Filesystem/TwigFactory.php
+++ concrete/src/Filesystem/TwigFactory.php
@@ -4,9 +4,12 @@ namespace Concrete\Core\Filesystem;
 
 use Concrete\Core\Application\ApplicationAwareInterface;
 use Concrete\Core\Application\ApplicationAwareTrait;
+use Concrete\Core\Page\Page;
+use Concrete\Core\Page\View\PageView;
 use Twig\Cache\CacheInterface;
 use Twig\Environment;
 use Twig\Extension\ExtensionInterface;
+use Twig\Loader\FilesystemLoader;
 use Twig\Loader\LoaderInterface;
 
 class TwigFactory implements ApplicationAwareInterface
@@ -32,10 +35,28 @@ class TwigFactory implements ApplicationAwareInterface
         $this->debug = $debug;
     }
 
+    private function addNamespaces(FilesystemLoader $loader): void
+    {
+        // Adds @theme as a twig namespace for the current theme. Not sure if there's
+        // a better place to do this. I'd like to encapsulate this functionality in
+        // the core exstension but I don't think it has access to the Loader object.
+        $c = Page::getCurrentPage();
+        if ($c) {
+            $theme = $c->getCollectionThemeObject();
+            if ($theme) {
+                $loader->addPath((string) $theme->getThemeDirectory(), 'theme');
+            }
+        }
+    }
+
     public function create(LoaderInterface $loader, array $options = []): Environment
     {
         $options = $options === [] ? $this->defaultOptions() : array_merge($this->defaultOptions(), $options);
         $twig = new \Twig\Environment($loader, $options);
+
+        if ($loader instanceof FilesystemLoader) {
+            $this->addNamespaces($loader);
+        }
 
         foreach ($this->globals as $name => $value) {
             $twig->addGlobal($name, $value);
