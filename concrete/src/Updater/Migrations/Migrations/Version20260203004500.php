436dc4e0abd803b4175b762805b46b9abae20c9d
Andrew Embler <andrew@concrete5.org>
2026-02-03 10:05:48 -0800

    Renaming migration since it only just merged

--- concrete/src/Updater/Migrations/Migrations/Version20260203004500.php
+++ concrete/src/Updater/Migrations/Migrations/Version20260203004500.php
@@ -7,7 +7,7 @@ namespace Concrete\Core\Updater\Migrations\Migrations;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
-final class Version20250302004500 extends AbstractMigration implements RepeatableMigrationInterface
+final class Version20260203004500 extends AbstractMigration implements RepeatableMigrationInterface
 {
     public function upgradeDatabase()
     {
