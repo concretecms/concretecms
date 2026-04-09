e84f80f8dbfed466bae5d3600ab4cbcd4c354e07
wordish <42463480+wordish@users.noreply.github.com>
2026-04-08 23:08:49 -0700

    Update StandardController.php

--- concrete/src/Express/Controller/StandardController.php
+++ concrete/src/Express/Controller/StandardController.php
@@ -6,8 +6,6 @@ use Concrete\Core\Express\Entry\Manager as EntryManager;
 use Concrete\Core\Express\Entry\Notifier\NotificationProviderInterface;
 use Concrete\Core\Express\Entry\Notifier\StandardNotifier;
 use Concrete\Core\Express\Form\Processor\StandardProcessor;
-use Concrete\Core\Logging\Channels;
-use Concrete\Core\Logging\LoggerFactory;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -32,11 +30,7 @@ class StandardController implements ControllerInterface
 
     public function getEntryManager(Request $request)
     {
-        $manager = $this->app->make(EntryManager::class, ['request' => $request]);
-        $manager->setLogger(
-            $this->app->make(LoggerFactory::class)->createLogger(Channels::CHANNEL_EXPRESS)
-        );
-        return $manager;
+        return $this->app->make(EntryManager::class, ['request' => $request]);
     }
 
     public function getNotifier(?NotificationProviderInterface $provider = null)
