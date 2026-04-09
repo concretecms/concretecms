c612ba3129b1583b6d8b77c3e7619ab88dce870d
wordish <42463480+wordish@users.noreply.github.com>
2026-04-08 20:21:56 -0700

    Update Manager.php

--- concrete/src/Express/Entry/Manager.php
+++ concrete/src/Express/Entry/Manager.php
@@ -15,7 +15,6 @@ use Concrete\Core\Express\Form\Control\SaveHandler\SaveHandlerInterface;
 use Concrete\Core\Logging\Channels;
 use Concrete\Core\Logging\LoggerAwareInterface;
 use Concrete\Core\Logging\LoggerAwareTrait;
-use Concrete\Core\Logging\LoggerFactory;
 use Concrete\Core\User\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query;
@@ -38,7 +37,6 @@ class Manager implements EntryManagerInterface, LoggerAwareInterface
         $this->request = $request;
         $this->entityManager = $entityManager;
         $this->app = $app;
-        $this->logger = $app->make(LoggerFactory::class)->createLogger(Channels::CHANNEL_EXPRESS);     
     }
 
     public function getLoggerChannel()
