39b123e4be1916fd946aee9e94ae14f8362b9aac
Nick Ratering <nick.ratering@framna.com>
2026-08-05 15:01:13 +0200

    fix: issue 12993

--- concrete/views/dialogs/page/drag_request.php
+++ concrete/views/dialogs/page/drag_request.php
@@ -22,10 +22,10 @@ if (isset($showProgressBar) && $showProgressBar) {
 }
 if (!$dragRequestData->canDoAnyOf([$dragRequestData::OPERATION_MOVE, $dragRequestData::OPERATION_ALIAS, $dragRequestData::OPERATION_COPY, $dragRequestData::OPERATION_COPYVERSION])) {
     ?>
-    <div class="alert alert-danger">
-        <?= t('You are not allowed to perform any operation with the selected pages.') ?>
-    </div>
-    <?php
+<div class="alert alert-danger">
+    <?= t('You are not allowed to perform any operation with the selected pages.') ?>
+</div>
+<?php
     return;
 }
 $singleOriginalPage = $dragRequestData->getSingleOriginalPage();
@@ -55,8 +55,9 @@ $singleOriginalPageName = (is_object($singleOriginalPage)) ? $singleOriginalPage
     <?php
     if ($dragRequestData->getDestinationSibling() !== null) {
         ?>
-        <input type="hidden" name="destSibling" value="<?= $dragRequestData->getDestinationSibling()->getCollectionID() ?>" />
-        <?php
+    <input type="hidden" name="destSibling"
+        value="<?= $dragRequestData->getDestinationSibling()->getCollectionID() ?>" />
+    <?php
     }
     ?>
     <input type="hidden" name="origCID" value="<?= $originalPageIDs ?>" />
@@ -64,116 +65,123 @@ $singleOriginalPageName = (is_object($singleOriginalPage)) ? $singleOriginalPage
     <?php
     if ($dragRequestData->canDo($dragRequestData::OPERATION_MOVE)) {
         ?>
-        <div class="form-group">
-            <div class="form-check">
-                <input type="radio" id="ctask1" name="ctask" class="form-check-input" value="<?= $dragRequestData::OPERATION_MOVE ?>" />
-                <label class="form-check-label" for="ctask1">
-                    <?php
+    <div class="form-group">
+        <div class="form-check">
+            <input type="radio" id="ctask1" name="ctask" class="form-check-input"
+                value="<?= $dragRequestData::OPERATION_MOVE ?>" />
+            <label class="form-check-label" for="ctask1">
+                <?php
                     if ($singleOriginalPage !== null) {
                         echo t('<strong>Move</strong> "%1$s" beneath "%2$s"', h($singleOriginalPageName), h($dragRequestData->getDestinationPage()->getCollectionName()));
                      } else {
                          echo t('<strong>Move</strong> pages beneath "%s"', h($dragRequestData->getDestinationPage()->getCollectionName()));
                      }
                      ?>
-                </label>
-                <?php
+            </label>
+            <?php
                 if ($singleOriginalPage && !$singleOriginalPage->isExternalLink()) {
                     ?>
-                    <div class="form-check" style="margin: 0 0 0 20px">
-                        <input type="checkbox" class="form-check-input" name="saveOldPagePath" id="saveOldPagePath" value="1" />
-                        <label class="form-check-label" for="saveOldPagePath">
-                            <?= t('Save old page path') ?>
-                        </label>
-                    </div>
-                    <?php
+            <div class="form-check" style="margin: 0 0 0 20px">
+                <input type="checkbox" class="form-check-input" name="saveOldPagePath" id="saveOldPagePath" value="1" />
+                <label class="form-check-label" for="saveOldPagePath">
+                    <?= t('Save old page path') ?>
+                </label>
+            </div>
+            <?php
                 }
                 ?>
-            </div>
         </div>
-        <?php
+    </div>
+    <?php
     }
 
     if ($dragRequestData->canDo($dragRequestData::OPERATION_ALIAS)) {
         ?>
-        <div class="form-group">
-            <div class="form-check">
-                <input type="radio" name="ctask" id="ctask2" class="form-check-input" value="<?= $dragRequestData::OPERATION_ALIAS ?>" />
-                <label class="form-check-label" for="ctask2">
-                    <?php
+    <div class="form-group">
+        <div class="form-check">
+            <input type="radio" name="ctask" id="ctask2" class="form-check-input"
+                value="<?= $dragRequestData::OPERATION_ALIAS ?>" />
+            <label class="form-check-label" for="ctask2">
+                <?php
                     if ($singleOriginalPage !== null) {
                         echo t('<strong>Alias</strong> "%1$s" beneath "%2$s"', h($singleOriginalPageName), h($dragRequestData->getDestinationPage()->getCollectionName()));
                     } else {
                         echo t('<strong>Alias</strong> pages beneath "%s"', h($dragRequestData->getDestinationPage()->getCollectionName()));
                     }
                     ?>
-                </label>
-                <div class="text-muted" style="margin: 0 0 0 20px">
-                    <?= t('Pages appear in both locations; all edits to originals will be reflected in their alias.') ?>
-                </div>
+            </label>
+            <div class="text-muted" style="margin: 0 0 0 20px">
+                <?= t('Pages appear in both locations; all edits to originals will be reflected in their alias.') ?>
             </div>
         </div>
-        <?php
+    </div>
+    <?php
     }
 
     if ($dragRequestData->canDo($dragRequestData::OPERATION_COPY) || $dragRequestData->canDo($dragRequestData::OPERATION_COPYVERSION)) {
         ?>
-        <div class="form-group">
-            <div class="form-check">
-                <input type="radio" name="ctask" id="ctask3" class="form-check-input" value="a-copy-operation" />
-                <label class="form-check-label" for="ctask3">
-                    <?= sprintf('<strong>%s</strong>', $singleOriginalPage !== null ? t('Copy Page') : t('Copy Pages')) ?>
-                </label>
-                <div style="margin: 0 0 0 20px">
-                    <?php
+    <div class="form-group">
+        <div class="form-check">
+            <input type="radio" name="ctask" id="ctask3" class="form-check-input" value="a-copy-operation" />
+            <label class="form-check-label" for="ctask3">
+                <?= sprintf('<strong>%s</strong>', $singleOriginalPage !== null ? t('Copy Page') : t('Copy Pages')) ?>
+            </label>
+            <div style="margin: 0 0 0 20px">
+                <?php
                     if ($dragRequestData->canDo($dragRequestData::OPERATION_COPY)) {
                         ?>
-                        <div class="form-check">
-                            <input type="radio" class="form-check-input" id="dtask1" name="dtask" value="<?= $dragRequestData::OPERATION_COPY ?>" />
-                            <label class="form-check-label" for="dtask1">
-                                <?php
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" id="dtask1" name="dtask"
+                        value="<?= $dragRequestData::OPERATION_COPY ?>" />
+                    <label class="form-check-label" for="dtask1">
+                        <?php
                                 if ($singleOriginalPage !== null) {
                                     echo h(t('Copy "%1$s" beneath "%2$s"', $singleOriginalPageName, $dragRequestData->getDestinationPage()->getCollectionName()));
                                 } else {
                                     echo h(t('Copy pages beneath "%s"', $dragRequestData->getDestinationPage()->getCollectionName()));
                                 }
                                 ?>
-                            </label>
-                        </div>
-                        <?php
+                    </label>
+                </div>
+                <?php
                     }
                     if ($dragRequestData->canDo($dragRequestData::OPERATION_COPYALL)) {
                         ?>
-                        <div class="form-check">
+                <div class="form-check">
 
-                            <input type="radio" class="form-check-input" id="dtask2"  name="dtask"  value="<?= $dragRequestData::OPERATION_COPYALL ?>" />
-                            <label class="form-check-label" for="dtask2">
-                                <?= h(t('Copy "%1$s" and all its children beneath "%2$s"', $singleOriginalPageName, $dragRequestData->getDestinationPage()->getCollectionName())) ?>
-                            </label>
-                        </div>
-                        <?php
+                    <input type="radio" class="form-check-input" id="dtask2" name="dtask"
+                        value="<?= $dragRequestData::OPERATION_COPYALL ?>" />
+                    <label class="form-check-label" for="dtask2">
+                        <?= h(t('Copy "%1$s" and all its children beneath "%2$s"', $singleOriginalPageName, $dragRequestData->getDestinationPage()->getCollectionName())) ?>
+                    </label>
+                </div>
+                <?php
                     }
                     if ($dragRequestData->canDo($dragRequestData::OPERATION_COPYVERSION)) {
                         ?>
-                        <div class="form-check">
+                <div class="form-check">
 
-                            <input type="radio" class="form-check-input" id="dtask3"  name="dtask"  value="<?= $dragRequestData::OPERATION_COPYVERSION ?>" />
-                            <label class="form-check-label" for="dtask3">
-                                <?= h(t('Replace "%1$s" with a copy of "%2$s"', $dragRequestData->getDestinationPage()->getCollectionName(), $singleOriginalPageName)) ?>
-                            </label>
-                        </div>
-                    </div>
-                    <?php
+                    <input type="radio" class="form-check-input" id="dtask3" name="dtask"
+                        value="<?= $dragRequestData::OPERATION_COPYVERSION ?>" />
+                    <label class="form-check-label" for="dtask3">
+                        <?= h(t('Replace "%1$s" with a copy of "%2$s"', $dragRequestData->getDestinationPage()->getCollectionName(), $singleOriginalPageName)) ?>
+                    </label>
+                </div>
+            </div>
+            <?php
                 }
                 ?>
-            </div>
         </div>
-        <?php
+    </div>
+    <?php
     }
     ?>
 
     <div class="dialog-buttons">
         <button onclick="$.fn.dialog.closeTop()" class="btn btn-secondary"><?= t('Cancel') ?></button>
-        <button data-form-selector="#<?= $formID ?>" onclick="ConcreteSitemap.submitDragRequest($($(this).data('form-selector')))" class="ms-auto btn btn-primary"><?= t('Go') ?></button>
+        <button data-form-selector="#<?= $formID ?>"
+            onclick="ConcreteSitemap.submitDragRequest($($(this).data('form-selector')))"
+            class="ms-auto btn btn-primary"><?= t('Go') ?></button>
     </div>
 </form>
 
@@ -192,6 +200,7 @@ $(document).ready(function() {
             }
         });
     }
+
     function muteInput(selector, muted) {
         var $input = $form.find(selector);
         if (muted) {
@@ -200,25 +209,26 @@ $(document).ready(function() {
             $input.attr('disabled', false);
         }
     }
+
     function updateState() {
         var ctask = $form.find('input[name="ctask"]:checked').val();
-        muteInput('input[name="saveOldPagePath"]', ctask !== <?= json_encode($dragRequestData::OPERATION_MOVE) ?>);
+        muteInput('input[name="saveOldPagePath"]', ctask !==
+            <?= json_encode($dragRequestData::OPERATION_MOVE) ?>);
         muteInput('input[name="dtask"]', ctask !== 'a-copy-operation');
     }
     $form.find('input[name="ctask"]').on('change', function() {
-    	updateState();
+        updateState();
     });
     $form.find('input[name="saveOldPagePath"]').on('change click', function() {
-        $form.find('input[name="ctask"][value="' + <?= json_encode($dragRequestData::OPERATION_MOVE) ?> + '"]')
+        $form.find('input[name="ctask"][value="' +
+                <?= json_encode($dragRequestData::OPERATION_MOVE) ?> + '"]')
             .prop('checked', true)
-            .trigger('change')
-        ;
+            .trigger('change');
     });
     $form.find('input[name="dtask"]').on('change click', function() {
         $form.find('input[name="ctask"][value="a-copy-operation"]')
             .prop('checked', true)
-            .trigger('change')
-        ;
+            .trigger('change');
     });
     $form.find('input[name="dtask"]:first').prop('checked', 'checked');
     $form.find('input[name="ctask"]:first').prop('checked', 'checked');
