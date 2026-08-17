fb62d03ae5cf1b93139a78e76338ef2016009a14
Michele Locati <michele@locati.it>
2026-08-17 16:22:09 +0200

    Let's accept <concrete-picture> placeholders with IDs, maintain additional attributes

--- concrete/src/Backup/ContentImporter/ValueInspector/InspectionRoutine/PictureRoutine.php
+++ concrete/src/Backup/ContentImporter/ValueInspector/InspectionRoutine/PictureRoutine.php
@@ -23,7 +23,37 @@ class PictureRoutine extends AbstractRegularExpressionRoutine
      */
     public function getRegularExpression()
     {
-        return '/\<concrete-picture\s[^>]*?file\s*=\s*[\'"]([^\'"]*?)[\'"][^>]*?>/i';
+        // anything except ">", but quoted attribute values may contain ">"
+        $upToClosing = '(?:[^>"\']|"[^"]*"|\'[^\']*\')*';
+
+        return implode('', [
+            // delimiter
+            '~',
+            // group 1: the complete <concrete-picture> element
+            '(',
+            // element name
+            '<concrete-picture',
+            // followed by whitespace (but don't consume it)
+            '(?=\s)',
+            // followed by anything except ">"
+            $upToClosing,
+            // a "file" or "file-id" attribute, preceded by whitespace
+            '\s(file|file-id)\s*=\s*',
+            // opening quote
+            '(?<quote>["\'])',
+            // at least one character (but not the char used to quote)
+            '((?!\k<quote>).)+',
+            // closing quote
+            '\k<quote>',
+            // followed by anything except ">"
+            $upToClosing,
+            // until the closing ">"
+            '>',
+            // end of group 1
+            ')',
+            // case-insensitive, "." also matches newlines
+            '~is',
+        ]);
     }
 
     /**
@@ -33,13 +63,36 @@ class PictureRoutine extends AbstractRegularExpressionRoutine
      */
     public function getItem($identifier)
     {
-        if (str_contains($identifier, ':')) {
-            [$prefix, $filename] = explode(':', $identifier);
-        } else {
-            $filename = $identifier;
-            $prefix = null;
-        }
+        $filename = '';
+        $prefix = null;
+        $fileID = null;
+        // strip the element name and the final ">" (or "/>")
+        $attributes = preg_replace(['~^<concrete-picture~i', '~/?>$~'], '', $identifier);
+        // extract (and remove) the "file" and "file-id" attributes
+        $attributes = preg_replace_callback(
+            '~\s+(?<name>file|file-id)\s*=\s*(?<quote>["\'])(?<value>(?:(?!\k<quote>).)*)\k<quote>~si',
+            static function (array $m) use (&$filename, &$prefix, &$fileID) {
+                switch (strtolower($m['name'])) {
+                    case 'file':
+                        $filename = $m['value'];
+                        if ($filename !== '') {
+                            $fileID = null;
+                            if (str_contains($m['value'], ':')) {
+                                [$prefix, $filename] = explode(':', $m['value'], 2);
+                            }
+                        }
+                        break;
+                    case 'file-id':
+                        if ($filename === '') {
+                            $fileID = ((int) $m['value']) ?: null;
+                        }
+                        break;
+                }
+                return '';
+            },
+            $attributes
+        );
 
-        return new PictureItem($filename, $prefix);
+        return new PictureItem($filename, $prefix, $fileID, trim($attributes));
     }
 }
