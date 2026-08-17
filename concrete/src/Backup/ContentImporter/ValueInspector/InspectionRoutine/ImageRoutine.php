49060ae799374d5b630df4f9f31f913961ff888d
Michele Locati <michele@locati.it>
2026-08-17 15:08:31 +0200

    Let's accept {ccm:export:image:...} placeholders with IDs

--- concrete/src/Backup/ContentImporter/ValueInspector/InspectionRoutine/ImageRoutine.php
+++ concrete/src/Backup/ContentImporter/ValueInspector/InspectionRoutine/ImageRoutine.php
@@ -33,13 +33,18 @@ class ImageRoutine extends AbstractRegularExpressionRoutine
      */
     public function getItem($identifier)
     {
-        if (str_contains($identifier, ':')) {
-            [$prefix, $filename] = explode(':', $identifier);
-        } else {
-            $filename = $identifier;
-            $prefix = null;
+        $filename = '';
+        $prefix = null;
+        $id = null;
+        if (preg_match('/^(.*):id=([1-9]\d*)$/D', $identifier, $m)) {
+            $identifier = $m[1];
+            $id = (int) $m[2];
+        }
+        if ($identifier !== '') {
+            [$prefix, $filename] = str_contains($identifier, ':') ? explode(':', $identifier, 2) : [null, $identifier];
+            $id = null;
         }
 
-        return new ImageItem($filename, $prefix);
+        return new ImageItem($filename, $prefix, $id);
     }
 }
