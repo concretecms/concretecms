bdf4d05da6af4143fe0d5924bfefce3c54fdf21a
Michele Locati <michele@locati.it>
2026-08-18 12:15:52 +0200

    Synchronize the OAuth scopes when upgrading

--- concrete/config/concrete.php
+++ concrete/config/concrete.php
@@ -8,7 +8,7 @@ return [
      */
     'version' => '9.5.2',
     'version_installed' => '9.5.2',
-    'version_db' => '20260811120000', // the key of the latest database migration
+    'version_db' => '20260818120000', // the key of the latest database migration
 
     /*
      * Installation status
---
+++ concrete/src/Updater/Migrations/Migrations/Version20260818120000.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Concrete\Core\Updater\Migrations\Migrations;
+
+use Concrete\Core\Api\Command\SynchronizeScopesCommand;
+use Concrete\Core\Updater\Migrations\AbstractMigration;
+use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
+
+final class Version20260818120000 extends AbstractMigration implements RepeatableMigrationInterface
+{
+    public function upgradeDatabase()
+    {
+    	// Persist the OAuth scopes declared in the API specification (block_types:read is a new one)
+        $this->app->executeCommand(new SynchronizeScopesCommand());
+    }
+}
